8a372d202f92c371aab86e289539d3e37b0314b9
Marc G. Fournier <scrappy@hub.org>
1996-07-25 20:45:05 +0000

    Add in port to DG/UX
    Submitted by: "Brian E. Gallew" <geek+@cmu.edu>

---
+++ src/backend/port/dgux/machine.h
@@ -0,0 +1,18 @@
+/*-------------------------------------------------------------------------
+ *
+ * machine.h--
+ *    
+ *
+ *
+ * Copyright (c) 1994, Regents of the University of California
+ *
+ * $Id: machine.h,v 1.1 1996/07/25 20:43:59 scrappy Exp $
+ *
+ *-------------------------------------------------------------------------
+ */
+#ifndef MACHINE_H
+#define MACHINE_H
+
+#define BLCKSZ	8192
+
+#endif
---
+++ src/backend/port/dgux/port.c
@@ -0,0 +1,13 @@
+/*-------------------------------------------------------------------------
+ *
+ * port.c--
+ *    Linux-specific routines
+ *
+ * Copyright (c) 1994, Regents of the University of California
+ *
+ *
+ * IDENTIFICATION
+ *    $Header: /cvsroot/pgsql/src/backend/port/dgux/Attic/port.c,v 1.1 1996/07/25 20:44:00 scrappy Exp $
+ *
+ *-------------------------------------------------------------------------
+ */
