eadf5dc754f68457499e65815bff47f9152c581d
Marc G. Fournier <scrappy@hub.org>
1996-08-27 22:00:21 +0000

    #include "postgres.h" exists in most .c files in system, so adding #include "config.h" here will (should?) ensure that any platform dependencies defined in config.h should be reflected in all .c files...

--- src/backend/include/postgres.h
+++ src/backend/include/postgres.h
@@ -6,7 +6,7 @@
  *
  * Copyright (c) 1995, Regents of the University of California
  *
- * $Id: postgres.h,v 1.1.1.1 1996/07/09 06:21:28 scrappy Exp $
+ * $Id: postgres.h,v 1.2 1996/08/27 22:00:21 scrappy Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -36,6 +36,7 @@
 #ifndef POSTGRES_H
 #define POSTGRES_H
 
+#include "config.h"
 #include "c.h"
 
 /* ----------------------------------------------------------------
