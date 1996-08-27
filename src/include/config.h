1da12ffb120cc414eddde459ac4aeb05b7bc07b1
Marc G. Fournier <scrappy@hub.org>
1996-08-27 06:14:29 +0000

    Its a start...

---
+++ src/include/config.h
@@ -0,0 +1,10 @@
+
+/* the purpose of this file is to reduce the use of #ifdef's through
+   the code base by those porting the software, an dto facilitate the
+   eventual use of autoconf to build the server 
+*/
+
+/* un-Define this if your system does not have <unistd.h> */
+#define HAVE_UNISTD_H
+
+
