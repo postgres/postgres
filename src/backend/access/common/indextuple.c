5dedad99a11ead58828951dba9d6f91876fdadfc
Marc G. Fournier <scrappy@hub.org>
1996-10-19 03:28:20 +0000

    One at a time...another one is complete...

--- src/backend/access/common/indextuple.c
+++ src/backend/access/common/indextuple.c
@@ -8,23 +8,25 @@
  *
  *
  * IDENTIFICATION
- *    $Header: /cvsroot/pgsql/src/backend/access/common/indextuple.c,v 1.1.1.1 1996/07/09 06:21:09 scrappy Exp $
+ *    $Header: /cvsroot/pgsql/src/backend/access/common/indextuple.c,v 1.2 1996/10/19 03:28:20 scrappy Exp $
  *
  *-------------------------------------------------------------------------
  */
-#include <string.h>
 
-#include "c.h"
-#include "access/ibit.h"
-#include "access/itup.h"	/* where the declarations go */
-#include "access/heapam.h"
-#include "access/genam.h"	
-#include "access/tupdesc.h"
-#include "access/tupmacs.h"
+#include "postgres.h"
 
+#include "storage/block.h"
+#include "storage/off.h"
 #include "storage/itemptr.h"
-#include "utils/elog.h"
-#include "utils/palloc.h"
+#include "nodes/nodes.h"
+#include "access/attnum.h"
+#include "nodes/pg_list.h"
+#include "catalog/pg_attribute.h"
+#include "access/tupdesc.h"
+#include "access/itup.h"
+
+#include "utils/memutils.h"
+#include "access/ibit.h"
 
 static Size IndexInfoFindDataOffset(unsigned short t_info);
 
