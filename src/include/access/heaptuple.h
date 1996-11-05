54c2aa99bd192f67edb91a908578735aa01896cc
Marc G. Fournier <scrappy@hub.org>
1996-11-05 07:18:05 +0000

    And another...

--- src/include/access/heaptuple.h
+++ src/include/access/heaptuple.h
@@ -6,16 +6,16 @@
  *
  * Copyright (c) 1994, Regents of the University of California
  *
- * $Id: heaptuple.h,v 1.2 1996/11/03 08:17:19 scrappy Exp $
+ * $Id: heaptuple.h,v 1.3 1996/11/05 07:18:05 scrappy Exp $
  *
  *-------------------------------------------------------------------------
  */
 #ifndef	HEAPTUPLE_H
 #define HEAPTUPLE_H
 
-#include "access/htup.h"
-#include "storage/buf.h"
-#include "access/tupdesc.h"
+#include <access/tupdesc.h>
+#include <storage/buf.h>
+#include <access/htup.h>
 
 extern char *heap_getattr(HeapTuple tup,
                           Buffer b,
