90113e2847b7eabcace2d8c66a6f0dbcdc3645ca
Marc G. Fournier <scrappy@hub.org>
1996-11-05 11:29:45 +0000

    another one quickly done up

--- src/include/lib/hasht.h
+++ src/include/lib/hasht.h
@@ -7,14 +7,14 @@
  *
  * Copyright (c) 1994, Regents of the University of California
  *
- * $Id: hasht.h,v 1.1 1996/08/28 07:22:38 scrappy Exp $
+ * $Id: hasht.h,v 1.2 1996/11/05 11:29:45 scrappy Exp $
  *
  *-------------------------------------------------------------------------
  */
 #ifndef	HASHT_H
 #define HASHT_H
 
-#include "utils/hsearch.h"
+#include <utils/hsearch.h>
 
 typedef void (*HashtFunc)();
 
