51716fdd8d449a6deae585a2405bca59a5058450
Marc G. Fournier <scrappy@hub.org>
1996-11-05 07:24:03 +0000

    Another

--- src/include/access/ibit.h
+++ src/include/access/ibit.h
@@ -6,14 +6,14 @@
  *
  * Copyright (c) 1994, Regents of the University of California
  *
- * $Id: ibit.h,v 1.3 1996/11/03 08:17:20 scrappy Exp $
+ * $Id: ibit.h,v 1.4 1996/11/05 07:24:03 scrappy Exp $
  *
  *-------------------------------------------------------------------------
  */
 #ifndef	IBIT_H
 #define IBIT_H
 
-#include "utils/memutils.h" 
+#include <utils/memutils.h>
 
 typedef struct IndexAttributeBitMapData {
 	char	bits[(MaxIndexAttributeNumber + MaxBitsPerByte - 1)
