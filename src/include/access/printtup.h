e0b772cc006a3168a23995b0411624fb4d367409
Marc G. Fournier <scrappy@hub.org>
1996-11-06 08:52:04 +0000

    another gone

--- src/include/access/printtup.h
+++ src/include/access/printtup.h
@@ -6,15 +6,15 @@
  *
  * Copyright (c) 1994, Regents of the University of California
  *
- * $Id: printtup.h,v 1.1 1996/08/27 21:50:19 scrappy Exp $
+ * $Id: printtup.h,v 1.2 1996/11/06 08:52:04 scrappy Exp $
  *
  *-------------------------------------------------------------------------
  */
 #ifndef	PRINTTUP_H
 #define PRINTTUP_H
 
-#include "access/htup.h"
-#include "access/tupdesc.h"
+#include <access/htup.h>
+#include <access/tupdesc.h>
 
 extern Oid typtoout(Oid type);
 extern void printtup(HeapTuple tuple, TupleDesc typeinfo);
