1d0e30839514f2802299b75e6488ba56e9ca5f7e
Bruce Momjian <bruce@momjian.us>
1996-11-09 05:48:28 +0000

    Added new include file.

---
+++ src/include/utils/dynahash.h
@@ -0,0 +1,19 @@
+/*-------------------------------------------------------------------------
+ *
+ * dynahash--
+ *    POSTGRES dynahash.h file definitions
+ *
+ *
+ * Copyright (c) 1994, Regents of the University of California
+ *
+ * $Id: dynahash.h,v 1.1 1996/11/09 05:48:28 momjian Exp $
+ *
+ *-------------------------------------------------------------------------
+ */
+#ifndef	DYNAHASH_H
+#define DYNAHASH_H
+
+extern int my_log2(long num);
+
+#endif DYNAHASH_H /* DYNAHASH_H */
+
