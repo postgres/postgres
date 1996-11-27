a38365e0ebca0297d4909cb25cccff52cfa735ec
Vadim B. Mikheev <vadim4o@yahoo.com>
1996-11-27 07:35:06 +0000

    New structures for new vacuum.

--- src/include/commands/vacuum.h
+++ src/include/commands/vacuum.h
@@ -6,7 +6,7 @@
  *
  * Copyright (c) 1994, Regents of the University of California
  *
- * $Id: vacuum.h,v 1.2 1996/10/18 08:15:58 vadim Exp $
+ * $Id: vacuum.h,v 1.3 1996/11/27 07:35:06 vadim Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -21,19 +21,26 @@ typedef struct VAttListData {
 typedef VAttListData	*VAttList;
 
 typedef struct VPageDescrData {
-    BlockNumber			vpd_blkno;	/* BlockNumber of this Page */
-    Size			vpd_free;	/* FreeSpace on this Page */
-    uint16			vpd_noff;	/* Number of dead tids */
-    OffsetNumber		vpd_voff[1];	/* Array of its OffNums */
+    BlockNumber		vpd_blkno;	/* BlockNumber of this Page */
+    Size		vpd_free;	/* FreeSpace on this Page */
+    uint16		vpd_nusd;	/* Number of OffNums used by vacuum */
+    uint16		vpd_noff;	/* Number of OffNums free or to be free */
+    OffsetNumber	vpd_voff[1];	/* Array of its OffNums */
 } VPageDescrData;
 
 typedef VPageDescrData	*VPageDescr;
 
+typedef struct VPageListData {
+    int			vpl_nemend;	/* Number of "empty" end-pages */
+    int			vpl_npages;	/* Number of pages in vpl_pgdesc */
+    VPageDescr		*vpl_pgdesc;	/* Descriptions of pages */
+} VPageListData;
+
+typedef VPageListData	*VPageList;
+
 typedef struct VRelListData {
     Oid			vrl_relid;
     VAttList		vrl_attlist;
-    VPageDescr		*vrl_pgdsc;
-    int			vrl_nrepg;
     int			vrl_ntups;
     int			vrl_npages;
     bool		vrl_hasindex;
