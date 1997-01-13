2de3d96f6923791e580967cb55b1a44c9685bb62
Bryan Henderson <bryanh@giraffe.netgate.net>
1997-01-13 01:26:29 +0000

    Add nextstep port, courtesy of Ovidiu Predescu.

---
+++ src/backend/port/nextstep/port-protos.h
@@ -0,0 +1,27 @@
+/*-------------------------------------------------------------------------
+ *
+ * port-protos.h--
+ *    port-specific prototypes for NeXT
+ *
+  
+-------------------------------------------------------------------------
+ */
+#ifndef PORT_PROTOS_H
+#define PORT_PROTOS_H
+
+#include "fmgr.h"			/* for func_ptr */
+#include "utils/dynamic_loader.h"
+
+void* next_dlopen(char* name);
+int next_dlclose(void* handle);
+void* next_dlsym (void *handle, char *symbol);
+char* next_dlerror(void);
+
+#define	pg_dlopen(f)	next_dlopen
+#define	pg_dlsym	next_dlsym
+#define	pg_dlclose	next_dlclose
+#define	pg_dlerror	next_dlerror
+
+/* port.c */
+
+#endif /* PORT_PROTOS_H */
---
+++ src/backend/port/nextstep/port.c
@@ -0,0 +1,58 @@
+#ifndef _POSIX_SOURCE
+# include <libc.h>
+#else
+# include <unistd.h>
+# include <stdlib.h>
+#endif
+
+#include <string.h>
+#include <sys/signal.h>
+
+
+void putenv(char* name)
+{
+    extern char** environ;
+    static int was_mallocated = 0;
+    int size;
+
+    /* Compute the size of environ array including the final NULL */
+    for (size = 1; environ[size++];)
+	/* nothing */;
+
+    if (!was_mallocated) {
+	char** tmp = environ;
+	int i;
+
+	was_mallocated = 1;
+	environ = malloc (size * sizeof(char*));
+	for (i = 0; i < size; i++)
+	    environ[i] = tmp[i];
+    }
+
+    environ = realloc (environ, (size + 1) * sizeof (char*));
+    environ[size - 1] = strcpy (malloc (strlen (name) + 1), name);
+    environ[size] = NULL;
+}
+
+char* strdup (const char* string)
+{
+    return strcpy (malloc (strlen (string) + 1), string);
+}
+
+#ifndef _POSIX_SOURCE
+int sigaddset(int *set, int signo)
+{
+    *set |= sigmask(signo);
+    return *set;
+}
+
+int sigemptyset(int *set)
+{
+    return (*set = 0);
+}
+
+char *getcwd(char *buf, size_t size)
+{
+    return getwd (buf);
+}
+#endif
