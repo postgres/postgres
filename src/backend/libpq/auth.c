678cd5c6b6e5c1dd8be236f43d11ecdeb0bb94f3
Marc G. Fournier <scrappy@hub.org>
1997-03-18 21:46:31 +0000

    From: Jun Kuwamura <juk@rccm.co.jp> Subject: [HACKERS] auth.c for kerberos.
      I made pgsql with eBones(international version of Kerberos4).  The
    following modification was needed.  And I added read permition for
    group to srvtab instead of running postmaster as root.

--- src/backend/libpq/auth.c
+++ src/backend/libpq/auth.c
@@ -7,7 +7,7 @@
  *
  *
  * IDENTIFICATION
- *    $Header: /cvsroot/pgsql/src/backend/libpq/auth.c,v 1.9 1997/03/12 21:17:48 scrappy Exp $
+ *    $Header: /cvsroot/pgsql/src/backend/libpq/auth.c,v 1.10 1997/03/18 21:46:31 scrappy Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -438,10 +438,15 @@ be_recvauth(MsgType msgtype_arg, Port *port, char *username, StartupInfo* sp)
        what used to be the only choice, but installation may choose "hba"
        instead.
        */
-    if (msgtype_arg == STARTUP_MSG && useHostBasedAuth)
-        msgtype = STARTUP_HBA_MSG;
-    else
-        msgtype = msgtype_arg;
+    if (msgtype_arg != STARTUP_KRB4_MSG && msgtype_arg != STARTUP_KRB5_MSG) {
+        if (msgtype_arg == STARTUP_MSG && useHostBasedAuth)
+            msgtype = STARTUP_HBA_MSG;
+        else 
+            msgtype = STARTUP_UNAUTH_MSG;
+    } else {
+            msgtype = msgtype_arg;
+    }
+
 
     if (!username) {
         (void) sprintf(PQerrormsg,
