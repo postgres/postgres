6ffd26d8eb11c28bdca0fc76290cd5b431be46f5
Marc G. Fournier <scrappy@hub.org>
1997-03-19 02:37:42 +0000

    Add a check for strerr, and add in D'Arcy's strerror() code in case not found

---
+++ src/backend/port/strerror.c
@@ -0,0 +1,30 @@
+/*
+ * strerror - map error number to descriptive string
+ *
+ * This version is obviously somewhat Unix-specific.
+ *
+ * based on code by Henry Spencer
+ * modified for ANSI by D'Arcy J.M. Cain
+ */
+
+#include        <string.h>
+#include        <stdio.h>
+#include        <errno.h>
+
+extern const char * const sys_errlist[];
+extern int sys_nerr;
+
+const char *
+strerror(int errnum)
+{
+        static char     buf[24];
+
+        if (errnum < 0 || errnum > sys_nerr)
+        {
+                sprintf(buf, "unknown error %d", errnum);
+                return(buf);
+        }
+
+        return(sys_errlist[errnum]);
+}
+
