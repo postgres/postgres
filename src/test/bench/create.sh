cfe0d67dc11377abf1befd302c1fce63a006c52a
Marc G. Fournier <scrappy@hub.org>
1997-04-17 13:48:52 +0000

    Some slight changes to the Wisconsin Benchmark tests since postgres requires a -D<datadir> option

--- src/test/bench/create.sh
+++ src/test/bench/create.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
-# $Header: /cvsroot/pgsql/src/test/bench/Attic/create.sh,v 1.1.1.1 1996/07/09 06:22:21 scrappy Exp $
+# $Header: /cvsroot/pgsql/src/test/bench/Attic/create.sh,v 1.2 1997/04/17 13:48:49 scrappy Exp $
 # 
+if [ ! -d $1 ]; then
+	echo " you must specify a valid data directory "
+	exit
+fi
 if [ -d ./obj ]; then
 	cd ./obj
 fi
 
 echo =============== destroying old bench database... =================
-echo "drop database bench" | postgres template1 > /dev/null
+echo "drop database bench" | postgres -D${1} template1 > /dev/null
 
 echo =============== creating new bench database... =================
-echo "create database bench" | postgres template1 > /dev/null
+echo "create database bench" | postgres -D${1} template1 > /dev/null
 if [ $? -ne 0 ]; then
 	echo createdb failed
 	exit 1
 fi
 
-postgres -Q bench < create.sql > /dev/null
+postgres -D${1} -Q bench < create.sql > /dev/null
 if [ $? -ne 0 ]; then
 	echo initial database load failed
 	exit 1
--- src/test/bench/runwisc.sh
+++ src/test/bench/runwisc.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
-# $Header: /cvsroot/pgsql/src/test/bench/Attic/runwisc.sh,v 1.1.1.1 1996/07/09 06:22:23 scrappy Exp $
+# $Header: /cvsroot/pgsql/src/test/bench/Attic/runwisc.sh,v 1.2 1997/04/17 13:48:52 scrappy Exp $
 # 
 # Note that in our published benchmark numbers, we executed the command in the
 # following fashion:
 #
 # time $POSTGRES -texecutor -tplanner -f hashjoin -Q bench
 #
+if [ ! -d $1 ]; then
+        echo " you must specify a valid data directory "
+        exit
+fi
+
 if [ -d ./obj ]; then
 	cd ./obj
 fi
 
 echo =============== vacuuming benchmark database... =================
-echo "vacuum" | postgres -Q bench > /dev/null
+echo "vacuum" | postgres -D${1} -Q bench > /dev/null
 
 echo =============== running benchmark... =================
-time postgres -texecutor -tplanner -Q bench < bench.sql
+time postgres -D${1} -texecutor -tplanner -Q bench < bench.sql
