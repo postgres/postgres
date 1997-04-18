d94c7fc4c5d38fa8925de029306dd654c71daf3d
Vadim B. Mikheev <vadim4o@yahoo.com>
1997-04-18 02:59:26 +0000

    1. Declaration static const char *num_word(Cash value); moved to cash.c. 2. 'extern ' added to funcs prototypes.

--- src/include/utils/cash.h
+++ src/include/utils/cash.h
@@ -11,25 +11,24 @@
 
 typedef long int Cash;
 
-const char *cash_out(Cash *value);
-Cash *cash_in(const char *str);
+extern const char *cash_out(Cash *value);
+extern Cash *cash_in(const char *str);
 
-bool cash_eq(Cash *c1, Cash *c2);
-bool cash_ne(Cash *c1, Cash *c2);
-bool cash_lt(Cash *c1, Cash *c2);
-bool cash_le(Cash *c1, Cash *c2);
-bool cash_gt(Cash *c1, Cash *c2);
-bool cash_ge(Cash *c1, Cash *c2);
+extern bool cash_eq(Cash *c1, Cash *c2);
+extern bool cash_ne(Cash *c1, Cash *c2);
+extern bool cash_lt(Cash *c1, Cash *c2);
+extern bool cash_le(Cash *c1, Cash *c2);
+extern bool cash_gt(Cash *c1, Cash *c2);
+extern bool cash_ge(Cash *c1, Cash *c2);
 
-Cash *cash_pl(Cash *c1, Cash *c2);
-Cash *cash_mi(Cash *c1, Cash *c2);
-Cash *cash_mul(Cash *c, float8 *f);
-Cash *cash_div(Cash *c, float8 *f);
+extern Cash *cash_pl(Cash *c1, Cash *c2);
+extern Cash *cash_mi(Cash *c1, Cash *c2);
+extern Cash *cash_mul(Cash *c, float8 *f);
+extern Cash *cash_div(Cash *c, float8 *f);
 
-Cash *cashlarger(Cash *c1, Cash *c2);
-Cash *cashsmaller(Cash *c1, Cash *c2);
+extern Cash *cashlarger(Cash *c1, Cash *c2);
+extern Cash *cashsmaller(Cash *c1, Cash *c2);
 
-const char *cash_words_out(Cash *value);
-static const char *num_word(Cash value);
+extern const char *cash_words_out(Cash *value);
 
 #endif /* CASH_H */
