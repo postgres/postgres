c2ae46730098ead3df4261f502c576b84e6f7dfc
Marc G. Fournier <scrappy@hub.org>
1997-04-26 05:07:12 +0000

    clean up the bin/*/Makefiles...up version.h to v6.1 instead of v6.0
    Remove bin/Makefile.global since it wasn't actually *doing* anything
    that Makefile.global hadn't already done

--- src/include/version.h
+++ src/include/version.h
@@ -4,7 +4,7 @@
  *    this file contains the interface to version.c.
  *    Also some parameters.
  *
- * $Id: version.h,v 1.1 1996/11/11 14:24:38 bryanh Exp $
+ * $Id: version.h,v 1.2 1997/04/26 05:07:12 scrappy Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -18,7 +18,7 @@ void
 SetPgVersion(const char *path, char **reason_p);
 
 #define	PG_RELEASE	6
-#define PG_VERSION	0
+#define PG_VERSION	1
 #define	PG_VERFILE	"PG_VERSION"
 
 #endif
