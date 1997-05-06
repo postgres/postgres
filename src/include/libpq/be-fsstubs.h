94123219bfd5f9e688764a2125d36f206bf89704
Thomas G. Lockhart <lockhart@fourpalms.org>
1997-05-06 07:14:34 +0000

    Rename LOread() and LOwrite() to be lower case to allow use  in case-insensitive SQL. Define LOread() and LOwrite() as macros  to avoid having to update calls everywhere.

--- src/include/libpq/be-fsstubs.h
+++ src/include/libpq/be-fsstubs.h
@@ -6,12 +6,20 @@
  *
  * Copyright (c) 1994, Regents of the University of California
  *
- * $Id: be-fsstubs.h,v 1.1 1996/08/28 07:22:56 scrappy Exp $
+ * $Id: be-fsstubs.h,v 1.2 1997/05/06 07:14:34 thomas Exp $
  *
  *-------------------------------------------------------------------------
  */
 #ifndef	BE_FSSTUBS_H
 #define	BE_FSSTUBS_H
+
+/* Redefine names LOread() and LOwrite() to be lowercase to allow calling
+ *  using the new v6.1 case-insensitive SQL parser. Define macros to allow
+ *  the existing code to stay the same. - tgl 97/05/03
+ */
+
+#define LOread(f,l) loread(f,l)
+#define LOwrite(f,b) lowrite(f,b)
 
 extern Oid lo_import(text *filename);
 extern int4 lo_export(Oid lobjId, text *filename);
@@ -26,7 +34,7 @@ extern int lo_lseek(int fd, int offset, int whence);
 extern int lo_tell(int fd);
 extern int lo_unlink(Oid lobjId);
 
-extern struct varlena *LOread(int fd, int len);
-extern int LOwrite(int fd, struct varlena *wbuf);
-     
+extern struct varlena *loread(int fd, int len);
+extern int lowrite(int fd, struct varlena *wbuf);
+
 #endif	/* BE_FSSTUBS_H */
