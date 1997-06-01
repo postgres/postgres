187e0f240567ff5e3100b06f7d6235a183085056
Marc G. Fournier <scrappy@hub.org>
1997-06-01 15:51:10 +0000

    From: Phil Nelson <phil@cs.wwu.edu> Subject: [PATCHES] Support for NetBSD/sparc
    The following is a patch to allow BSD44_derived to support NetBSD/sparc.

--- src/include/port/BSD44_derived.h
+++ src/include/port/BSD44_derived.h
@@ -1,5 +1,10 @@
 #  define USE_POSIX_TIME
-#  define NEED_I386_TAS_ASM
+#  if defined(i386)
+#    define NEED_I386_TAS_ASM
+#  endif
+#  if defined(sparc)
+#    define NEED_SPARC_TAS_ASM
+#  endif
 #  define HAS_TEST_AND_SET
 #  if defined(__mips__)
 /* #    undef HAS_TEST_AND_SET */
