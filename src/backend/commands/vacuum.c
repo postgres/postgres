858f35510e8a4b25cd8fc7719d545fe35838b6e0
Bruce Momjian <bruce@momjian.us>
1997-06-07 17:34:35 +0000

    Cleanup on xinv# table names.

--- src/backend/commands/vacuum.c
+++ src/backend/commands/vacuum.c
@@ -7,7 +7,7 @@
  *
  *
  * IDENTIFICATION
- *    $Header: /cvsroot/pgsql/src/backend/commands/vacuum.c,v 1.35 1997/06/07 05:18:47 momjian Exp $
+ *    $Header: /cvsroot/pgsql/src/backend/commands/vacuum.c,v 1.36 1997/06/07 17:34:35 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -313,7 +313,7 @@ vc_getrels(NameData *VacRelP)
 	}
 
 	/* don't vacuum large objects for now - something breaks when we do */
-	if ( (strlen(rname) > 5) && rname[0] == 'x' &&
+	if ( (strlen(rname) >= 5) && rname[0] == 'x' &&
 		rname[1] == 'i' && rname[2] == 'n' &&
 		(rname[3] == 'v' || rname[3] == 'x') &&
 		rname[4] >= '0' && rname[4] <= '9')
