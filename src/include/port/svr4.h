31fba1d97416814d2ab863a7964fa0eced44e391
Bruce Momjian <bruce@momjian.us>
1997-07-14 16:52:13 +0000

    Mips sysv4 support.

--- src/include/port/svr4.h
+++ src/include/port/svr4.h
@@ -2,3 +2,9 @@
 #  define USE_POSIX_SIGNALS
 #  define NO_EMPTY_STMTS
 #  define SYSV_DIRENT
+
+#ifndef         BYTE_ORDER
+#ifdef		MIPSEB
+#define         BYTE_ORDER      BIG_ENDIAN
+#endif
+#endif 
