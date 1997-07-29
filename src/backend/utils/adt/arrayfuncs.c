9d8ae797748d1bafb0c215d8c0f05c27f1ccd5df
Thomas G. Lockhart <lockhart@fourpalms.org>
1997-07-29 15:51:33 +0000

    Add debugging statements.

--- src/backend/utils/adt/arrayfuncs.c
+++ src/backend/utils/adt/arrayfuncs.c
@@ -7,7 +7,7 @@
  *
  *
  * IDENTIFICATION
- *    $Header: /cvsroot/pgsql/src/backend/utils/adt/arrayfuncs.c,v 1.11 1997/07/24 20:15:45 momjian Exp $
+ *    $Header: /cvsroot/pgsql/src/backend/utils/adt/arrayfuncs.c,v 1.12 1997/07/29 15:51:33 thomas Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -33,13 +33,13 @@
 #define ASSGN    "="
 
 /* An array has the following internal structure:
- *    <nbytes>            - total number of bytes 
- *     <ndim>                - number of dimensions of the array 
- *    <flags>                - bit mask of flags
- *    <dim>                - size of each array axis 
- *    <dim_lower>            - lower boundary of each dimension 
- *    <actual data>        - whatever is the stored data
-*/
+ *    <nbytes>      - total number of bytes 
+ *    <ndim>        - number of dimensions of the array 
+ *    <flags>       - bit mask of flags
+ *    <dim>         - size of each array axis 
+ *    <dim_lower>   - lower boundary of each dimension 
+ *    <actual data> - whatever is the stored data
+ */
 
 /*-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-*/
 static int _ArrayCount(char *str, int dim[], int typdelim);
@@ -148,6 +148,14 @@ array_in(char *string,		/* input array in external form */
         while (isspace(*p)) p++;
     }        
     
+#ifdef ARRAYDEBUG
+printf( "array_in- ndim %d (", ndim);
+for (i = 0; i < ndim; i++) {
+    printf(" %d", dim[i]);
+};
+printf( ") for %s\n", string);
+#endif
+
     nitems = getNitems( ndim, dim);
     if (nitems == 0) {
         char *emptyArray = palloc(sizeof(ArrayType));
