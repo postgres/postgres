4e9a3f4ef008900be30bd62fdadf7303cd1734fa
Vadim B. Mikheev <vadim4o@yahoo.com>
1997-08-31 11:40:13 +0000

    Workplace for CREATE/DROP TRIGGER

---
+++ src/backend/commands/trigger.c
@@ -0,0 +1,27 @@
+/*-------------------------------------------------------------------------
+ *
+ * trigger.c--
+ *    PostgreSQL TRIGGERs support code.
+ *
+ *-------------------------------------------------------------------------
+ */
+#include "postgres.h"
+
+#include "nodes/parsenodes.h"
+#include "commands/trigger.h"
+#include "catalog/pg_trigger.h"
+#include "utils/builtins.h"
+
+void
+CreateTrigger (CreateTrigStmt *stmt)
+{
+
+    return;
+}
+
+void
+DropTrigger (DropTrigStmt *stmt)
+{
+
+    return;
+}
