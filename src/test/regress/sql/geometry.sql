2d0e6b0ff7dabe0222e24a33af2874e063b82820
Thomas G. Lockhart <lockhart@fourpalms.org>
1997-09-01 06:18:45 +0000

    Test new intersection and closest point operators. Fix up results labels on queries.

--- src/test/regress/sql/geometry.sql
+++ src/test/regress/sql/geometry.sql
@@ -2,19 +2,19 @@
 -- Points
 --
 
-SELECT '' AS xxx, center(f1) AS center
+SELECT '' AS four, center(f1) AS center
    FROM BOX_TBL;
 
-SELECT '' AS xxx, (@@ f1) AS center
+SELECT '' AS four, (@@ f1) AS center
    FROM BOX_TBL;
 
-SELECT '' AS xxx, point(f1) AS center
+SELECT '' AS six, point(f1) AS center
    FROM CIRCLE_TBL;
 
-SELECT '' AS xxx, (@@ f1) AS center
+SELECT '' AS six, (@@ f1) AS center
    FROM CIRCLE_TBL;
 
-SELECT '' AS xxx, (@@ f1) AS center
+SELECT '' AS two, (@@ f1) AS center
    FROM POLYGON_TBL
    WHERE (# f1) > 2;
 
@@ -43,11 +43,11 @@ SELECT '' AS one, p1.f1
 --
 
 -- intersection
-SELECT '' AS xxx, p.f1, l.s, l.s # p.f1 AS intersection
+SELECT '' AS count, p.f1, l.s, l.s # p.f1 AS intersection
    FROM LSEG_TBL l, POINT_TBL p;
 
 -- closest point
-SELECT '' AS xxx, p.f1, l.s, p.f1 ## l.s AS closest
+SELECT '' AS thirty, p.f1, l.s, p.f1 ## l.s AS closest
    FROM LSEG_TBL l, POINT_TBL p;
 
 --
@@ -58,20 +58,20 @@ SELECT '' AS xxx, p.f1, l.s, p.f1 ## l.s AS closest
 -- Boxes
 --
 
-SELECT box(f1) AS box FROM CIRCLE_TBL;
+SELECT '' as six, box(f1) AS box FROM CIRCLE_TBL;
 
 -- translation
-SELECT '' AS count, b.f1 + p.f1 AS translation
+SELECT '' AS twentyfour, b.f1 + p.f1 AS translation
    FROM BOX_TBL b, POINT_TBL p;
 
-SELECT '' AS count, b.f1 - p.f1 AS translation
+SELECT '' AS twentyfour, b.f1 - p.f1 AS translation
    FROM BOX_TBL b, POINT_TBL p;
 
 -- scaling and rotation
-SELECT '' AS count, b.f1 * p.f1 AS rotation
+SELECT '' AS twentyfour, b.f1 * p.f1 AS rotation
    FROM BOX_TBL b, POINT_TBL p;
 
-SELECT '' AS count, b.f1 / p.f1 AS rotation
+SELECT '' AS twenty, b.f1 / p.f1 AS rotation
    FROM BOX_TBL b, POINT_TBL p
    WHERE (p.f1 <-> '(0,0)'::point) >= 1;
 
@@ -81,9 +81,9 @@ SELECT '' AS count, b.f1 / p.f1 AS rotation
 
 SET geqo TO 'off';
 
-SELECT '' AS xxx, points(f1) AS npoints, f1 AS path FROM PATH_TBL;
+SELECT '' AS eight, points(f1) AS npoints, f1 AS path FROM PATH_TBL;
 
-SELECT '' AS xxx, path(f1) FROM POLYGON_TBL;
+SELECT '' AS four, path(f1) FROM POLYGON_TBL;
 
 -- translation
 SELECT '' AS eight, p1.f1 + '(10,10)'::point AS dist_add
@@ -100,46 +100,46 @@ RESET geqo;
 --
 
 -- containment
-SELECT '' AS xxx, p.f1, poly.f1, poly.f1 ~ p.f1 AS contains
+SELECT '' AS twentyfour, p.f1, poly.f1, poly.f1 ~ p.f1 AS contains
    FROM POLYGON_TBL poly, POINT_TBL p;
 
-SELECT '' AS xxx, p.f1, poly.f1, p.f1 @ poly.f1 AS contained
+SELECT '' AS twentyfour, p.f1, poly.f1, p.f1 @ poly.f1 AS contained
    FROM POLYGON_TBL poly, POINT_TBL p;
 
-SELECT '' AS xxx, points(f1) AS npoints, f1 AS polygon
+SELECT '' AS four, points(f1) AS npoints, f1 AS polygon
    FROM POLYGON_TBL;
 
-SELECT '' AS xxx, polygon(f1)
+SELECT '' AS four, polygon(f1)
    FROM BOX_TBL;
 
-SELECT '' AS xxx, polygon(f1)
+SELECT '' AS four, polygon(f1)
    FROM PATH_TBL WHERE isclosed(f1);
 
-SELECT '' AS xxx, f1 AS open_path, polygon( pclose(f1)) AS polygon
+SELECT '' AS four, f1 AS open_path, polygon( pclose(f1)) AS polygon
    FROM PATH_TBL
    WHERE isopen(f1);
 
 -- convert circles to polygons using the default number of points
-SELECT '' AS xxx, polygon(f1)
+SELECT '' AS six, polygon(f1)
    FROM CIRCLE_TBL;
 
 -- convert the circle to an 8-point polygon
-SELECT '' AS xxx, polygon(8, f1)
+SELECT '' AS six, polygon(8, f1)
    FROM CIRCLE_TBL;
 
 --
 -- Circles
 --
 
-SELECT '' AS xxx, circle(f1, 50.0)
+SELECT '' AS six, circle(f1, 50.0)
    FROM POINT_TBL;
 
-SELECT '' AS xxx, circle(f1)
+SELECT '' AS four, circle(f1)
    FROM BOX_TBL;
 
-SELECT '' AS xxx, circle(f1)
+SELECT '' AS two, circle(f1)
    FROM POLYGON_TBL
-   WHERE (# f1) >= 2;
+   WHERE (# f1) >= 3;
 
 SELECT '' AS twentyfour, c1.f1 AS circle, p1.f1 AS point, (p1.f1 <-> c1.f1) AS distance
    FROM CIRCLE_TBL c1, POINT_TBL p1
