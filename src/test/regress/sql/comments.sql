36b54847cbbe0bf393b8dc3a03b3eb76a3e4788e
Thomas G. Lockhart <lockhart@fourpalms.org>
1997-09-18 03:54:19 +0000

    Add explicit test for various comment syntaxes.

---
+++ src/test/regress/sql/comments.sql
@@ -0,0 +1,16 @@
+--
+-- Comments
+--
+
+SELECT 'trailing' AS first; -- trailing single line
+SELECT /* embedded single line */ 'embedded' AS second;
+SELECT /* both embedded and trailing single line */ 'both' AS third; -- trailing single line
+
+SELECT 'before multi-line' AS fourth;
+/* This is an example of SQL which should not execute:
+ * select 'multi-line';
+ */
+SELECT 'after multi-line' AS fifth;
+
+/* and this is the end of the file */
+
