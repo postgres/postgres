f1bfc520c0f51c2a2fd5433916946bca892ebd61
Marc G. Fournier <scrappy@hub.org>
1997-10-03 17:31:29 +0000

    We never do remember to up the PG_VERSION number, do we? :(

--- src/include/version.h
+++ src/include/version.h
@@ -4,7 +4,7 @@
  *	  this file contains the interface to version.c.
  *	  Also some parameters.
  *
- * $Id: version.h,v 1.4 1997/09/08 02:33:57 momjian Exp $
+ * $Id: version.h,v 1.5 1997/10/03 17:31:29 scrappy Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -18,7 +18,7 @@ void
 			SetPgVersion(const char *path, char **reason_p);
 
 #define PG_RELEASE		6
-#define PG_VERSION		1
+#define PG_VERSION		2
 #define PG_VERFILE		"PG_VERSION"
 
 #endif
