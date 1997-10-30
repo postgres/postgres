868859b7e31c9ea3c6a1f76b5801b76156fad405
Bruce Momjian <bruce@momjian.us>
1997-10-30 03:37:28 +0000

    AIX patch from Darren King.

--- src/backend/port/univel/port.c
+++ src/backend/port/univel/port.c
@@ -70,23 +70,6 @@ getrusage(int who, struct rusage * rusage)
 	return (0);
 }
 
-/*
- * Copyright (c) 1987 Regents of the University of California.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms are permitted
- * provided that this notice is preserved and that due credit is given
- * to the University of California at Berkeley. The name of the University
- * may not be used to endorse or promote products derived from this
- * software without specific written prior permission. This software
- * is provided ``as is'' without express or implied warranty.
- */
-
-#if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)strcasecmp.c	5.5 (Berkeley) 11/24/87";
-
-#endif							/* LIBC_SCCS and not lint */
-
 #include <sys/types.h>
 #include <string.h>
 
