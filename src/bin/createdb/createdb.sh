c97f6fd197f38943b7b59e490b5c21c623ad93c7
Thomas G. Lockhart <lockhart@fourpalms.org>
1997-11-07 06:25:25 +0000

    Add "-D location" to specify alternate database location.

--- src/bin/createdb/createdb.sh
+++ src/bin/createdb/createdb.sh
@@ -11,60 +11,80 @@
 #
 #
 # IDENTIFICATION
-#    $Header: /cvsroot/pgsql/src/bin/createdb/Attic/createdb.sh,v 1.6 1996/11/17 03:54:44 bryanh Exp $
+#    $Header: /cvsroot/pgsql/src/bin/createdb/Attic/createdb.sh,v 1.7 1997/11/07 06:25:25 thomas Exp $
 #
 #-------------------------------------------------------------------------
 
 CMDNAME=`basename $0`
 
 if [ -z "$USER" ]; then
-    if [ -z "$LOGNAME" ]; then
-	if [ -z "`whoami`" ]; then
-	    echo "$CMDNAME: cannot determine user name"
-	    exit 1
+	if [ -z "$LOGNAME" ]; then
+		if [ -z "`whoami`" ]; then
+			echo "$CMDNAME: cannot determine user name"
+			exit 1
+		fi
+	else
+		USER=$LOGNAME
+		export USER
 	fi
-    else
-	USER=$LOGNAME
-	export USER
-    fi
 fi
 
 dbname=$USER
 
 while test -n "$1"
 do
-    case $1 in
-	-a) AUTHSYS=$2; shift;;
-        -h) PGHOST=$2; shift;;
-        -p) PGPORT=$2; shift;;
-         *) dbname=$1;;
-    esac
-    shift;
+	case $1 in
+		--help) usage=1;;
+
+		-a) AUTHSYS=$2; shift;;
+		-h) PGHOST=$2; shift;;
+		-p) PGPORT=$2; shift;;
+		-D) dbpath=$2; shift;;
+		-*) echo "$CMDNAME: unrecognized parameter $1"; usage=1;;
+		 *) dbname=$1;;
+	esac
+	shift;
 done
 
+if [ "$usage" ]; then
+	echo "Usage: $CMDNAME -a <authtype> -h <server> -p <portnumber> -D <location> [dbname]"
+	exit 1
+fi
+
 if [ -z "$AUTHSYS" ]; then
-  AUTHOPT=""
+	AUTHOPT=""
 else
-  AUTHOPT="-a $AUTHSYS"
+	AUTHOPT="-a $AUTHSYS"
 fi
 
 if [ -z "$PGHOST" ]; then
-  PGHOSTOPT=""
+	PGHOSTOPT=""
 else
-  PGHOSTOPT="-h $PGHOST"
+	PGHOSTOPT="-h $PGHOST"
 fi
 
 if [ -z "$PGPORT" ]; then
-  PGPORTOPT=""
+	PGPORTOPT=""
 else
-  PGPORTOPT="-p $PGPORT"
+	PGPORTOPT="-p $PGPORT"
 fi
 
-psql -tq $AUTHOPT $PGHOSTOPT $PGPORTOPT -c "create database $dbname" template1
+if [ -z "$dbpath" ]; then
+	location=""
+else
+#	if [ ! -d "$dbpath"/base ]; then
+#		echo "$CMDNAME: database creation failed on $dbname."
+#		echo "directory $dbpath/base not found."
+#		exit 1
+#	fi
+	location="with location = '$dbpath'"
+fi
+
+psql -tq $AUTHOPT $PGHOSTOPT $PGPORTOPT -c "create database $dbname $location" template1
 
 if [ $? -ne 0 ]; then
-    echo "$CMDNAME: database creation failed on $dbname."
-    exit 1
+	echo "$CMDNAME: database creation failed on $dbname."
+	exit 1
 fi
 
 exit 0
