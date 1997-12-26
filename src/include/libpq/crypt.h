c06ecfb456339036d7a79121f30271c5a638eb41
Vadim B. Mikheev <vadim4o@yahoo.com>
1997-12-26 05:51:51 +0000

    extern char* crypt_getpwdfilename(void);                                   ^^^^ gcc likes this

--- src/include/libpq/crypt.h
+++ src/include/libpq/crypt.h
@@ -13,7 +13,7 @@
 
 #define CRYPT_PWD_FILE	"pg_pwd"
 
-extern char* crypt_getpwdfilename();
+extern char* crypt_getpwdfilename(void);
 extern MsgType crypt_salt(const char* user);
 extern int crypt_verify(Port* port, const char* user, const char* pgpass);
 
