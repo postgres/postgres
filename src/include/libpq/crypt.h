e8291f7ce2a20fd8a69e0c0f359fec0e58d00ad4
Vadim B. Mikheev <vadim4o@yahoo.com>
1998-01-13 14:53:36 +0000

    extern char* crypt_getpwdreloadfilename(void);                                         ^^^^

--- src/include/libpq/crypt.h
+++ src/include/libpq/crypt.h
@@ -20,7 +20,7 @@ extern char**     pwd_cache;
 extern int        pwd_cache_count;
 
 extern char* crypt_getpwdfilename(void);
-extern char* crypt_getpwdreloadfilename();
+extern char* crypt_getpwdreloadfilename(void);
 extern MsgType crypt_salt(const char* user);
 extern int crypt_verify(Port* port, const char* user, const char* pgpass);
 
