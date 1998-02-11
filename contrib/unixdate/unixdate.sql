243a9137666717fc239beb4c79dbe6c0b294057b
Thomas G. Lockhart <lockhart@fourpalms.org>
1998-02-11 04:24:20 +0000

    Remove some definitions which could not work; conversions from datetime  to integer unix system time conflict on the input types.  Leave in the conversions from integer unix system time to datetime.

--- contrib/unixdate/unixdate.sql
+++ contrib/unixdate/unixdate.sql
@@ -1,6 +1,6 @@
 -- unixdate
 -- Routines to convert int4 (Unix system time) to datetime
---  and int4 (delta time) to timespan
+--  and int4 (delta time in seconds) to timespan
 --
 -- Thomas Lockhart (lockhart@alumni.caltech.edu)
 -- 1997-11-25
@@ -8,7 +8,7 @@
 -- This cheats and reuses existing code in the standard package.
 -- Can not include this directly because built-in functions are optimized
 --  into a cache and the duplicate function names abstime_datetime() and
---  reltime_timespan() result in duplicate constants.
+--  reltime_timespan() would result in duplicate constants.
 --
 -- This works with Postgres v6.2 and higher.
 
@@ -31,24 +31,4 @@ CREATE FUNCTION reltime_timespan(int4)
 CREATE FUNCTION timespan(int4)
  RETURNS timespan
  AS 'select reltime_timespan($1)' LANGUAGE 'SQL';
-
---
--- Conversions back to integer
---
-
-CREATE FUNCTION datetime_abstime(datetime)
- RETURNS int4
- AS '-' LANGUAGE 'internal';
-
-CREATE FUNCTION utime(datetime)
- RETURNS int4
- AS 'select datetime_abstime($1)' LANGUAGE 'SQL';
-
-CREATE FUNCTION timespan_reltime(timespan)
- RETURNS int4
- AS '-' LANGUAGE 'internal';
-
-CREATE FUNCTION uspan(timespan)
- RETURNS int4
- AS 'select timespan_reltime($1)' LANGUAGE 'SQL';
 
