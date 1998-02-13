b6d5fce95117b72b2d4d3f4eacf8ab5ca2c22215
Marc G. Fournier <scrappy@hub.org>
1998-02-13 13:15:23 +0000

    Add missing extern.h file

---
+++ src/interfaces/ecpg/preproc/extern.h
@@ -0,0 +1,13 @@
+/* variables */
+
+extern int debugging, braces_open;
+extern char * yytext;
+extern int yylineno, yyleng;
+extern FILE *yyin, *yyout;
+
+
+/* functions */
+
+extern void lex_init(void);
+extern char * input_filename;
+extern int yyparse(void);
