30f737f6b1fc7f4d2ff685230ef97add5f457810
Marc G. Fournier <scrappy@hub.org>
1998-02-17 01:43:04 +0000

    Totally forgot to add these files...

---
+++ src/interfaces/ecpg/test/mm.sql
@@ -0,0 +1,8 @@
+create table meskes(name char8, born int4);
+
+insert into meskes(name, born) values ('Petra', 19661202);
+insert into meskes(name, born) values ('Michael', 19660117);
+insert into meskes(name, born) values ('Carsten', 19910103);
+insert into meskes(name, born) values ('Marc', 19930907);
+insert into meskes(name, born) values ('Chris', 19970923);
+
