f0da7129f514dfb9873a999f1370609ac986e922
Marc G. Fournier <scrappy@hub.org>
1998-02-24 15:56:45 +0000

    Change -c to -u for regress.sh...should shrink regression.<portname> somewhat at the same time...
    Suggested by: Frank Ridderbusch <ridderbusch.pad@sni.de>

--- src/test/regress/regress.sh
+++ src/test/regress/regress.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Header: /cvsroot/pgsql/src/test/regress/Attic/regress.sh,v 1.15 1998/02/01 11:46:10 scrappy Exp $
+# $Header: /cvsroot/pgsql/src/test/regress/Attic/regress.sh,v 1.16 1998/02/24 15:56:45 scrappy Exp $
 #
 if echo '\c' | grep -s c >/dev/null 2>&1
 then
@@ -55,7 +55,7 @@ do
   
 	if [ `diff ${EXPECTED} results/${i}.out | wc -l` -ne 0 ]
 	then
-		( diff -c ${EXPECTED} results/${i}.out; \
+		( diff -u ${EXPECTED} results/${i}.out; \
 		echo "";  \
 		echo "----------------------"; \
 		echo "" ) >> regression.diffs
