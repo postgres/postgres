2a677424122012d2eaa4c6d1113fc6b9b121720b
Vadim B. Mikheev <vadim4o@yahoo.com>
1998-07-22 05:48:59 +0000

    Fix _lo_commit: core if LO memmory context was not created (LO were not used?)

--- src/backend/libpq/be-fsstubs.c
+++ src/backend/libpq/be-fsstubs.c
@@ -7,7 +7,7 @@
  *
  *
  * IDENTIFICATION
- *	  $Header: /cvsroot/pgsql/src/backend/libpq/be-fsstubs.c,v 1.22 1998/07/21 04:17:23 momjian Exp $
+ *	  $Header: /cvsroot/pgsql/src/backend/libpq/be-fsstubs.c,v 1.23 1998/07/22 05:48:59 vadim Exp $
  *
  * NOTES
  *	  This should be moved to a more appropriate place.  It is here
@@ -375,14 +375,18 @@ lo_export(Oid lobjId, text *filename)
 void 
 _lo_commit(void)
 {
-        int i;
+	int i;
 	MemoryContext currentContext;
 
+	if (fscxt == NULL)
+		return;
+	
 	currentContext = MemoryContextSwitchTo((MemoryContext) fscxt);
 
-        for (i = 0; i < MAX_LOBJ_FDS; i++) {
-                if (cookies[i] != NULL) inv_cleanindex(cookies[i]);
-        }
+	for (i = 0; i < MAX_LOBJ_FDS; i++)
+	{
+		if (cookies[i] != NULL) inv_cleanindex(cookies[i]);
+	}
 
 	MemoryContextSwitchTo(currentContext);
 
