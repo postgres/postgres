620c2c362f636d786f10ad010d58d2ed595f72d3
Thomas G. Lockhart <lockhart@fourpalms.org>
1998-08-17 16:11:35 +0000

    Update the random test so it should succeed most of the time. Instead of directly showing the random results, test the results  for the expected behavior (range and randomness).

--- src/test/regress/sql/random.sql
+++ src/test/regress/sql/random.sql
@@ -5,8 +5,22 @@
 SELECT count(*) FROM onek;
 
 -- select roughly 1/10 of the tuples
-SELECT count(*) FROM onek where oidrand(onek.oid, 10);
+-- Assume that the "onek" table has 1000 tuples
+--  and try to bracket the correct number so we
+--  have a regression test which can pass/fail
+-- - thomas 1998-08-17
+SELECT count(*) AS random INTO RANDOM_TBL
+  FROM onek WHERE oidrand(onek.oid, 10);
 
 -- select again, the count should be different
-SELECT count(*) FROM onek where oidrand(onek.oid, 10);
+INSERT INTO RANDOM_TBL (random)
+  SELECT count(*)
+  FROM onek WHERE oidrand(onek.oid, 10);
+
+-- now test the results for randomness in the correct range
+SELECT random, count(random) FROM RANDOM_TBL
+  GROUP BY random HAVING count(random) > 1;
+
+SELECT random FROM RANDOM_TBL
+  WHERE random NOT BETWEEN 80 AND 120;
 
