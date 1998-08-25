dd70e439dfc79af38cc1d488ffe3174b55315f91
Marc G. Fournier <scrappy@hub.org>
1998-08-25 14:07:13 +0000

    re-integrate nextstep dynloader functionality
    From: Jacek Lasecki <jacek@sound.eti.pg.gda.pl>

--- src/backend/port/dynloader/nextstep.h
+++ src/backend/port/dynloader/nextstep.h
@@ -12,10 +12,10 @@
 #include "fmgr.h"				/* for func_ptr */
 #include "utils/dynamic_loader.h"
 
-void	   *next_dlopen(char *name);
-int			next_dlclose(void *handle);
-void	   *next_dlsym(void *handle, char *symbol);
-char	   *next_dlerror(void);
+void	*next_dlopen(char *name);
+int	next_dlclose(void *handle);
+void	*next_dlsym(void *handle, char *symbol);
+char	*next_dlerror(void);
 
 #define pg_dlopen(f)	next_dlopen
 #define pg_dlsym		next_dlsym
