d2d919f7b79a5f1e9025610bde4382f4119650b3
Bruce Momjian <bruce@momjian.us>
1998-10-12 04:32:24 +0000

    tcl cleanup.

--- src/pl/plpgsql/src/mklang.sql
+++ src/pl/plpgsql/src/mklang.sql
@@ -1,11 +1,11 @@
 --
 -- PL/pgSQL language declaration
 --
--- $Header: /cvsroot/pgsql/src/pl/plpgsql/src/Attic/mklang.sql,v 1.1 1998/08/24 19:14:48 momjian Exp $
+-- $Header: /cvsroot/pgsql/src/pl/plpgsql/src/Attic/mklang.sql,v 1.2 1998/10/12 04:32:24 momjian Exp $
 --
 
 create function plpgsql_call_handler() returns opaque
-	as '/usr/local/pgsql/lib/plpgsql.so'
+	as '${exec_prefix}/lib/plpgsql.so'
 	language 'C';
 
 create trusted procedural language 'plpgsql'
