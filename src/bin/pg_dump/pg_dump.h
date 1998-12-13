e18d900da4f1ecf2a1fd50e6727891c4ac1c1775
Thomas G. Lockhart <lockhart@fourpalms.org>
1998-12-13 23:41:32 +0000

    Adjust handling of data type names to suppress double quotes  unless necessary. Label internal bpchar types as "character" and varchar types as  "character varying" to be less Postgres-specific. These types map to  the SQL92 definitions anyway. Redefine g_force_quotes to be the local variable force_quotes. Pass this as an argument to fmtId(). These should help with handling the single-byte internal "char" type.

--- src/bin/pg_dump/pg_dump.h
+++ src/bin/pg_dump/pg_dump.h
@@ -5,7 +5,7 @@
  *
  * Copyright (c) 1994, Regents of the University of California
  *
- * $Id: pg_dump.h,v 1.36 1998/12/05 22:09:56 tgl Exp $
+ * $Id: pg_dump.h,v 1.37 1998/12/13 23:41:32 thomas Exp $
  *
  * Modifications - 6/12/96 - dave@bensoft.com - version 1.13.dhb.2
  *
@@ -226,7 +226,7 @@ extern void dumpIndices(FILE *fout, IndInfo *indinfo, int numIndices,
 			TableInfo *tbinfo, int numTables, const char *tablename);
 
 extern const char *
-			fmtId(const char *identifier);
+			fmtId(const char *identifier, bool force_quotes);
 
 /* largest query string size */
 #define MAXQUERYLEN  5000
