909c51973363acebf7a33a46a77663cdb64e25a4
Bruce Momjian <bruce@momjian.us>
1999-01-18 12:43:55 +0000

    Add missing Windows files.

---
+++ src/backend/port/dynloader/win.h
@@ -0,0 +1,34 @@
+/*-------------------------------------------------------------------------
+ *
+ * win.h--
+ *	  port-specific prototypes for Intel x86/Window NT
+ *
+ *
+ * Copyright (c) 1994, Regents of the University of California
+ *
+ * win.h,v 1.2 1995/03/17 06:40:18 andrew Exp
+ *
+ *-------------------------------------------------------------------------
+  */
+#ifndef PORT_PROTOS_H
+#define PORT_PROTOS_H
+
+#include <dlfcn.h>
+#include "fmgr.h"				/* for func_ptr */
+#include "utils/dynamic_loader.h"
+
+ /* dynloader.c */
+/*
+ * Dynamic Loader on Intel x86/Windows NT
+ *
+ * this dynamic loader uses the system dynamic loading interface for shared
+ * libraries (ie. dlopen/dlsym/dlclose). The user must specify a shared
+ * library as the file to be dynamically loaded.
+ *
+  */
+#define pg_dlopen(f)		dlopen(f,1)
+#define pg_dlsym		dlsym
+#define pg_dlclose		dlclose
+#define pg_dlerror		dlerror
+
+#endif	 /* PORT_PROTOS_H */
---
+++ src/include/port/win.h
@@ -0,0 +1,7 @@
+#define JMP_BUF
+#define HAS_TEST_AND_SET
+typedef unsigned char slock_t;
+
+#ifndef O_DIROPEN
+#define O_DIROPEN	0x100000	/* should be in sys/fcntl.h */
+#endif
