c84ea433d725db190e6e2e51344ec055422414fe
Tatsuo Ishii <ishii@postgresql.org>
1999-04-26 04:40:17 +0000

    Fix from Yutaka Tanida <yutaka@marin.or.jp>

--- src/include/port/win.h
+++ src/include/port/win.h
@@ -5,3 +5,7 @@ typedef unsigned char slock_t;
 #ifndef O_DIROPEN
 #define O_DIROPEN	0x100000	/* should be in sys/fcntl.h */
 #endif
+
+#define tzname _tzname /* should be in time.h?*/
+#define USE_POSIX_TIME
+#define HAVE_INT_TIMEZONE /* has int _timezone */
