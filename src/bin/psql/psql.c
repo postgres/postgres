2b8c9e793db33e3378c61d71f76852264282d2fb
Bruce Momjian <bruce@momjian.us>
1999-07-19 16:46:53 +0000

    Set the application name, used for parsing readline's .inputrc -- dz
    Massimo

--- src/bin/psql/psql.c
+++ src/bin/psql/psql.c
@@ -7,7 +7,7 @@
  *
  *
  * IDENTIFICATION
- *	  $Header: /cvsroot/pgsql/src/bin/psql/Attic/psql.c,v 1.184 1999/07/19 02:27:10 momjian Exp $
+ *	  $Header: /cvsroot/pgsql/src/bin/psql/Attic/psql.c,v 1.185 1999/07/19 16:46:53 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -2876,6 +2876,13 @@ main(int argc, char **argv)
 		pqsignal(SIGINT, handle_sigint);		/* control-C => cancel */
 #ifdef USE_READLINE
 		settings.useReadline = 1;
+		{
+			/*
+			 * Set the application name, used for parsing .inputrc -- dz
+			 */
+			char *progname = strrchr(argv[0], SEP_CHAR);
+			rl_readline_name = (progname ? progname+1 : argv[0]);
+		}
 #endif
 	}
 #ifdef PSQL_ALWAYS_GET_PASSWORDS
