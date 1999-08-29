04ffd2bc0accb80a6bf1e2a46c86ff1b289be9cb
Tom Lane <tgl@sss.pgh.pa.us>
1999-08-29 01:39:57 +0000

    Back-patch fix for timestamp(datetime) into 6.5.*. I believe this is not a violation of our policy against requiring initdb for minor-version bugfixes, since users don't *have* to initdb; they just won't see any effect from the fix if they don't.  But a user who does do a clean install or initdb upgrade to 6.5.2 will get the fix, and that seems worthwhile.

--- src/include/catalog/pg_proc.h
+++ src/include/catalog/pg_proc.h
@@ -6,7 +6,7 @@
  *
  * Copyright (c) 1994, Regents of the University of California
  *
- * $Id: pg_proc.h,v 1.99.2.1 1999/07/30 18:52:53 scrappy Exp $
+ * $Id: pg_proc.h,v 1.99.2.2 1999/08/29 01:39:57 tgl Exp $
  *
  * NOTES
  *	  The script catalog/genbki.sh reads this file and generates .bki
@@ -1641,7 +1641,7 @@ DATA(insert OID = 1362 (  time				 PGUID 14 f t f 1 f 1083 "1083" 100 0 0 100  "
 DESCR("convert (noop)");
 DATA(insert OID = 1363 (  time				 PGUID 11 f t f 1 f 1083 "1184" 100 0 0 100  datetime_time - ));
 DESCR("convert datetime to time");
-DATA(insert OID = 1364 (  time				 PGUID 14 f t f 1 f 1083 "702" 100 0 0 100	"select abstime_time($1)" - ));
+DATA(insert OID = 1364 (  time				 PGUID 14 f t f 1 f 1083 "702" 100 0 0 100	"select time(datetime($1))" - ));
 DESCR("convert abstime to time");
 DATA(insert OID = 1365 (  abstime			 PGUID 14 f t f 1 f  702 "702" 100 0 0 100	"select $1" - ));
 DESCR("convert (noop)");
@@ -1653,7 +1653,7 @@ DATA(insert OID = 1368 (  reltime			 PGUID 11 f t f 1 f  703 "1186" 100 0 0 100 
 DESCR("convert timespan to reltime");
 DATA(insert OID = 1369 (  timestamp			 PGUID 14 f t f 1 f 1296 "1296" 100 0 0 100  "select $1" - ));
 DESCR("convert (noop)");
-DATA(insert OID = 1370 (  timestamp			 PGUID 14 f t f 1 f 1296 "1184" 100 0 0 100  "select datetime_stamp($1)" - ));
+DATA(insert OID = 1370 (  timestamp			 PGUID 11 f t f 1 f 1296 "1184" 100 0 0 100  datetime_timestamp - ));
 DESCR("convert datetime to timestamp");
 DATA(insert OID = 1371 (  length			 PGUID 11 f t f 1 f   23   "25" 100 0 0 100  textlen - ));
 DESCR("character length");
