3c44a132ea94894cb4e0e138fc4e3d5523eae6da
Bruce Momjian <bruce@momjian.us>
1999-10-12 14:06:49 +0000

    Update pgeasy for missing files.

---
+++ src/interfaces/libpgeasy/libpgeasy.h
@@ -0,0 +1,22 @@
+/*
+ * pglib.h
+ *
+*/
+
+PGresult   *doquery(char *query);
+PGconn	   *connectdb(char *dbName,
+				char *pghost,
+				char *pgport,
+				char *pgoptions,
+				char *pgtty);
+void		disconnectdb(void);
+int			fetch(void *param,...);
+int			fetchwithnulls(void *param,...);
+void		on_error_continue(void);
+void		on_error_stop(void);
+PGresult   *get_result(void);
+void		set_result(PGresult *newres);
+void		unset_result(PGresult *oldres);
+void		reset_fetch(void);
+
+#define END_OF_TUPLES	(-1)
