f4db5c3c88bc588bf34dd95c95bfd738bd33b527
Bruce Momjian <bruce@momjian.us>
1999-12-10 00:48:32 +0000

    Update regression for new dropdb command.

--- src/test/regress/regress.sh
+++ src/test/regress/regress.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Header: /cvsroot/pgsql/src/test/regress/Attic/regress.sh,v 1.32 1999/11/19 18:51:49 wieck Exp $
+# $Header: /cvsroot/pgsql/src/test/regress/Attic/regress.sh,v 1.33 1999/12/10 00:48:32 momjian Exp $
 #
 if [ $# -eq 0 ]
 then
@@ -47,8 +47,8 @@ echo "   ports@postgresql.org"
 echo "See regress/README for more information."
 echo ""
 
-echo "=============== destroying old regression database... ================="
-destroydb $HOST regression
+echo "=============== dropping old regression database... ================="
+dropdb $HOST regression
 
 echo "=============== creating new regression database...   ================="
 if [ -n "$MULTIBYTE" ];then
@@ -119,17 +119,17 @@ then
 echo Skipping clearing and deletion of the regression database
 else
 echo "=============== clearing regression database...       ================="
-$FRONTEND regression < destroy.sql
+$FRONTEND regression < drop.sql
 if [ $? -ne 0 ]; then
-     echo the destroy script has an error
+     echo the drop script has an error
      exit 1
 fi
 
 exit 0
-echo "=============== destroying regression database...     ================="
-destroydb regression
+echo "=============== dropping regression database...     ================="
+dropdb regression
 if [ $? -ne 0 ]; then
-     echo destroydb failed
+     echo dropdb failed
      exit 1
 fi
 
