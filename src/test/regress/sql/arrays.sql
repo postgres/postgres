6ce5e0abb67add892c5d726a5fdfe7aed686c1af
Tom Lane <tgl@sss.pgh.pa.us>
2000-01-15 19:11:40 +0000

    Update arrays regress test to reflect fact that several things work now that did not work in 6.5.

--- src/test/regress/sql/arrays.sql
+++ src/test/regress/sql/arrays.sql
@@ -10,14 +10,14 @@ SELECT arrtest.a[1],
           arrtest.d[1][1], 
           arrtest.e[0]
    FROM arrtest;
--- ??? what about
--- SELECT a[1], b[1][1][1], c[1], d[1][1], e[0]
---    FROM arrtest;
 
-SELECT arrtest.a[1:3],
-          arrtest.b[1:1][1:2][1:2],
-          arrtest.c[1:2], 
-          arrtest.d[1:1][1:2]
+SELECT a[1], b[1][1][1], c[1], d[1][1], e[0]
+   FROM arrtest;
+
+SELECT a[1:3],
+          b[1:1][1:2][1:2],
+          c[1:2], 
+          d[1:1][1:2]
    FROM arrtest;
 
 -- returns three different results--
@@ -26,18 +26,16 @@ SELECT array_dims(arrtest.b) AS x;
 -- returns nothing 
 SELECT *
    FROM arrtest
-   WHERE arrtest.a[1] < 5 and 
-         arrtest.c = '{"foobar"}'::_name;
+   WHERE a[1] < 5 and 
+         c = '{"foobar"}'::_name;
 
--- updating array subranges seems to be broken
--- 
--- UPDATE arrtest
---   SET a[1:2] = '{16,25}',
---       b[1:1][1:1][1:2] = '{113, 117}', 
---       c[1:1] = '{"new_word"}';
+UPDATE arrtest
+  SET a[1:2] = '{16,25}',
+      b[1:1][1:1][1:2] = '{113, 117}', 
+      c[1:1] = '{"new_word"}';
 
-SELECT arrtest.a[1:3],
-          arrtest.b[1:1][1:2][1:2],
-          arrtest.c[1:2], 
-          arrtest.d[1:1][1:2]
+SELECT a[1:3],
+          b[1:1][1:2][1:2],
+          c[1:2], 
+          d[1:1][2:2]
    FROM arrtest;
