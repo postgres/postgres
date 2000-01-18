6c25ea23422a200b8b4394b9f95220999dbf9fa9
Tatsuo Ishii <ishii@postgresql.org>
2000-01-18 13:46:10 +0000

    Fix minor comple error

--- src/backend/utils/init/miscinit.c
+++ src/backend/utils/init/miscinit.c
@@ -7,7 +7,7 @@
  *
  *
  * IDENTIFICATION
- *	  $Header: /cvsroot/pgsql/src/backend/utils/init/miscinit.c,v 1.40 2000/01/18 05:10:29 ishii Exp $
+ *	  $Header: /cvsroot/pgsql/src/backend/utils/init/miscinit.c,v 1.41 2000/01/18 13:46:10 ishii Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -85,7 +85,7 @@ const char *pg_encoding_to_char(int encoding)
 int	pg_char_to_encoding(const char *encoding_string)
 {
 	elog(ERROR, "MultiByte support must be enabled to use this function");
-	return ("");
+	return (0);
 }
 
 #endif
