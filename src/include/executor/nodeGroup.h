91250f3854ad3dbb07097816c03c9c031707d536
Tom Lane <tgl@sss.pgh.pa.us>
2000-01-26 23:48:05 +0000

    Remove duplicate extern declaration.

--- src/include/executor/nodeGroup.h
+++ src/include/executor/nodeGroup.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2000, PostgreSQL, Inc
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $Id: nodeGroup.h,v 1.13 2000/01/26 05:58:05 momjian Exp $
+ * $Id: nodeGroup.h,v 1.14 2000/01/26 23:48:05 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -21,8 +21,5 @@ extern bool ExecInitGroup(Group *node, EState *estate, Plan *parent);
 extern int	ExecCountSlotsGroup(Group *node);
 extern void ExecEndGroup(Group *node);
 extern void ExecReScanGroup(Group *node, ExprContext *exprCtxt, Plan *parent);
-
-extern void ExecReScanGroup(Group *node, ExprContext *exprCtxt, Plan *parent);
-
 
 #endif	 /* NODEGROUP_H */
