b2d867fb086c2b8c504387d177b80b9d09b76370
Bruce Momjian <bruce@momjian.us>
2000-03-23 17:27:36 +0000

    Some points for portability improvements:
    1. C++ style comments in C source for ecpg ( // comment )
    2. compiler finds wrong include file extern.h in ecpg/lib/descriptor.c
    from
    include path instead of workdir (rename it ?)
    3. fe-connect getsockopt takes a socklen_t as fifth arg not int (use
    SOCKET_SIZE_TYPE instead)
    4. char vs unsigned char in psql calls to libpq
    5. empty define that results in an empty but terminated line ( ; )
    
    Now for all but point 3 I can supply changes to the
    compiler flags, to make the compiler less pedantic.
    Or is someone interested in the complications ?
    
    in the meantime can someone apply the attached patch ?
    
    Andreas

--- src/interfaces/libpq/fe-connect.c
+++ src/interfaces/libpq/fe-connect.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $Header: /cvsroot/pgsql/src/interfaces/libpq/fe-connect.c,v 1.123 2000/03/11 03:08:36 tgl Exp $
+ *	  $Header: /cvsroot/pgsql/src/interfaces/libpq/fe-connect.c,v 1.124 2000/03/23 17:27:29 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -1080,7 +1080,7 @@ PQconnectPoll(PGconn *conn)
 #else
 			char optval;
 #endif
-			int optlen = sizeof(optval);
+			SOCKET_SIZE_TYPE optlen = sizeof(optval);
 
 			/* Write ready, since we've made it here, so the connection
 			 * has been made. */
