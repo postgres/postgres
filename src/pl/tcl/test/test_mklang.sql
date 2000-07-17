139f19c30221968e7d3bf64fe303cb41517e4601
Tom Lane <tgl@sss.pgh.pa.us>
2000-07-17 02:22:50 +0000

    update pltcl test to have at least some chance of running ... still seems to be busted though ...

--- src/pl/tcl/test/test_mklang.sql
+++
@@ -1,9 +0,0 @@
-
-create function pltcl_call_handler() returns opaque
-	as '/usr/local/pgsql/lib/pltcl.so'
-	language 'C';
-
-create trusted procedural language 'pltcl'
-	handler pltcl_call_handler
-	lancompiler 'PL/Tcl';
-
