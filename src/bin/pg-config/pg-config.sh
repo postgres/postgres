9314141116e7851a87e760def912fda65b2ddca0
Peter Eisentraut <peter_e@gmx.net>
2000-08-26 13:08:16 +0000

    Add pg-config utility that stores some configuration parameters other packages can use to configure their build.
    E.g.,
    $ pg-config --libdir
    /usr/local/pgsql/lib
    
    `pg-config --configure' stores the configure command line.

---
+++ src/bin/pg-config/pg-config.sh
@@ -0,0 +1,70 @@
+#! /bin/sh
+
+# This shell script saves various pieces of information about the
+# installed version of PostgreSQL.  Packages that interface to
+# PostgreSQL can use it to configure their build.
+#
+# Author:  Peter Eisentraut <peter_e@gmx.net> 
+# Public domain
+
+# $Header: /cvsroot/pgsql/src/bin/pg-config/Attic/pg-config.sh,v 1.1 2000/08/26 13:08:16 petere Exp $
+
+me=`basename $0`
+
+# stored configuration values
+val_bindir='@bindir@'
+val_includedir='@includedir@'
+val_libdir='@libdir@'
+val_configure='@configure@'
+val_version='@version@'
+
+help="\
+$me provides information about the installed version of PostgreSQL.
+
+Usage: $me --bindir | --includedir | --libdir | --configure | --version
+
+Operation modes:
+  --bindir      show location of user executables
+  --includedir  show location of C header files
+  --libdir      show location of object code libraries
+  --configure   show options given to \`configure' script when
+                PostgreSQL was built
+  --version     show PostgreSQL version and exit
+
+Report bugs to <pgsql-bugs@postgresql.org>."
+
+advice="\
+Try \`$me --help' for more information."
+
+if test $# -eq 0 ; then
+    echo "$me: argument required"
+    echo "$advice"
+    exit 1
+fi
+
+show=
+
+for opt
+do
+    case $opt in
+        --bindir)       show="$show \$val_bindir";;
+        --includedir)   show="$show \$val_includedir";;
+        --libdir)       show="$show \$val_libdir";;
+        --configure)    show="$show \$val_configure";;
+
+	--version)      echo "PostgreSQL $val_version"
+                        exit 0;;
+	--help|-\?)     echo "$help"
+                        exit 0;;
+        *)              echo "$me: invalid argument: $opt"
+                        echo "$advice"
+                        exit 1;;
+    esac
+done
+
+for thing in $show
+do
+    eval "echo $thing"
+done
+
+# end of pg-config
