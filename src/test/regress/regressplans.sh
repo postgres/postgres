dc3fb8b152d2f697bdc656175b88d514930cd917
Peter Eisentraut <peter_e@gmx.net>
2000-11-22 16:40:22 +0000

    Fix output file names for new driver

--- src/test/regress/regressplans.sh
+++ src/test/regress/regressplans.sh
@@ -32,67 +32,67 @@ MAKE=${MAKE:-gmake}
 mkdir planregress
 
 PGOPTIONS="                   " $MAKE runtest
-mv -f regress.out planregress/out.normal
+mv -f regression.out planregress/out.normal
 mv -f regression.diffs planregress/diffs.normal
 PGOPTIONS="                -fh" $MAKE runtest
-mv -f regress.out planregress/out.h
+mv -f regression.out planregress/out.h
 mv -f regression.diffs planregress/diffs.h
 PGOPTIONS="            -fm    " $MAKE runtest
-mv -f regress.out planregress/out.m
+mv -f regression.out planregress/out.m
 mv -f regression.diffs planregress/diffs.m
 PGOPTIONS="            -fm -fh" $MAKE runtest
-mv -f regress.out planregress/out.mh
+mv -f regression.out planregress/out.mh
 mv -f regression.diffs planregress/diffs.mh
 PGOPTIONS="        -fn        " $MAKE runtest
-mv -f regress.out planregress/out.n
+mv -f regression.out planregress/out.n
 mv -f regression.diffs planregress/diffs.n
 PGOPTIONS="        -fn     -fh" $MAKE runtest
-mv -f regress.out planregress/out.nh
+mv -f regression.out planregress/out.nh
 mv -f regression.diffs planregress/diffs.nh
 PGOPTIONS="        -fn -fm    " $MAKE runtest
-mv -f regress.out planregress/out.nm
+mv -f regression.out planregress/out.nm
 mv -f regression.diffs planregress/diffs.nm
 PGOPTIONS="    -fi            " $MAKE runtest
-mv -f regress.out planregress/out.i
+mv -f regression.out planregress/out.i
 mv -f regression.diffs planregress/diffs.i
 PGOPTIONS="    -fi         -fh" $MAKE runtest
-mv -f regress.out planregress/out.ih
+mv -f regression.out planregress/out.ih
 mv -f regression.diffs planregress/diffs.ih
 PGOPTIONS="    -fi     -fm    " $MAKE runtest
-mv -f regress.out planregress/out.im
+mv -f regression.out planregress/out.im
 mv -f regression.diffs planregress/diffs.im
 PGOPTIONS="    -fi     -fm -fh" $MAKE runtest
-mv -f regress.out planregress/out.imh
+mv -f regression.out planregress/out.imh
 mv -f regression.diffs planregress/diffs.imh
 PGOPTIONS="    -fi -fn        " $MAKE runtest
-mv -f regress.out planregress/out.in
+mv -f regression.out planregress/out.in
 mv -f regression.diffs planregress/diffs.in
 PGOPTIONS="    -fi -fn     -fh" $MAKE runtest
-mv -f regress.out planregress/out.inh
+mv -f regression.out planregress/out.inh
 mv -f regression.diffsregression.planregress/inh
 PGOPTIONS="    -fi -fn -fm    " $MAKE runtest
-mv -f regress.out planregress/out.inm
+mv -f regression.out planregress/out.inm
 mv -f regression.diffs planregress/diffs.inm
 PGOPTIONS="-fs                " $MAKE runtest
-mv -f regress.out planregress/out.s
+mv -f regression.out planregress/out.s
 mv -f regression.diffs planregress/diffs.s
 PGOPTIONS="-fs             -fh" $MAKE runtest
-mv -f regress.out planregress/out.sh
+mv -f regression.out planregress/out.sh
 mv -f regression.diffs planregress/diffs.sh
 PGOPTIONS="-fs         -fm    " $MAKE runtest
-mv -f regress.out planregress/out.sm
+mv -f regression.out planregress/out.sm
 mv -f regression.diffs planregress/diffs.sm
 PGOPTIONS="-fs         -fm -fh" $MAKE runtest
-mv -f regress.out planregress/out.smh
+mv -f regression.out planregress/out.smh
 mv -f regression.diffs planregress/diffs.smh
 PGOPTIONS="-fs     -fn        " $MAKE runtest
-mv -f regress.out planregress/out.sn
+mv -f regression.out planregress/out.sn
 mv -f regression.diffs planregress/diffs.sn
 PGOPTIONS="-fs     -fn     -fh" $MAKE runtest
-mv -f regress.out planregress/out.snh
+mv -f regression.out planregress/out.snh
 mv -f regression.diffs planregress/diffs.snh
 PGOPTIONS="-fs     -fn -fm    " $MAKE runtest
-mv -f regress.out planregress/out.snm
+mv -f regression.out planregress/out.snm
 mv -f regression.diffs planregress/diffs.snm
 
 exit 0
