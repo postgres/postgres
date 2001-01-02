25d88e4c53e4249f3ba033d7f1b424396c5b65b0
Tom Lane <tgl@sss.pgh.pa.us>
2001-01-02 02:13:48 +0000

    Tweak temporary-installation setup so that it doesn't break when the configured install --prefix begins with /data/...

--- src/test/regress/pg_regress.sh
+++ src/test/regress/pg_regress.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-# $Header: /cvsroot/pgsql/src/test/regress/Attic/pg_regress.sh,v 1.15 2000/12/31 18:38:44 tgl Exp $
+# $Header: /cvsroot/pgsql/src/test/regress/Attic/pg_regress.sh,v 1.16 2001/01/02 02:13:48 tgl Exp $
 
 me=`basename $0`
 : ${TMPDIR=/tmp}
@@ -269,10 +269,11 @@ then
         temp_install="`pwd`/$temp_install"
     fi
 
-    bindir=$temp_install/$bindir
-    libdir=$temp_install/$libdir
-    datadir=$temp_install/$datadir
+    bindir=$temp_install/install/$bindir
+    libdir=$temp_install/install/$libdir
+    datadir=$temp_install/install/$datadir
     PGDATA=$temp_install/data
+
     if [ "$unix_sockets" = no ]; then
         PGHOST=$hostname
         export PGHOST
@@ -305,7 +306,7 @@ then
     if [ ! -d "$LOGDIR" ]; then
         mkdir -p "$LOGDIR" || { (exit 2); exit; }
     fi
-    $GMAKE -C "$top_builddir" DESTDIR="$temp_install" install with_perl=no with_python=no >"$LOGDIR/install.log" 2>&1
+    $GMAKE -C "$top_builddir" DESTDIR="$temp_install/install" install with_perl=no with_python=no >"$LOGDIR/install.log" 2>&1
 
     if [ $? -ne 0 ]
     then
