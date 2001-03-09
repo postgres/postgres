5490195f045a54e7a33220c3dadcd76f999ac0cf
Tom Lane <tgl@sss.pgh.pa.us>
2001-03-09 22:10:13 +0000

    Don't choke on superuser names containing random punctuation.

--- src/include/catalog/pg_shadow.h
+++ src/include/catalog/pg_shadow.h
@@ -9,7 +9,7 @@
  * Portions Copyright (c) 1996-2001, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $Id: pg_shadow.h,v 1.10 2001/01/24 19:43:22 momjian Exp $
+ * $Id: pg_shadow.h,v 1.11 2001/03/09 22:10:13 tgl Exp $
  *
  * NOTES
  *	  the genbki.sh script reads this file and generates .bki
@@ -73,6 +73,6 @@ typedef FormData_pg_shadow *Form_pg_shadow;
  * user choices.
  * ----------------
  */
-DATA(insert OID = 0 ( POSTGRES PGUID t t t t _null_ _null_ ));
+DATA(insert OID = 0 ( "POSTGRES" PGUID t t t t _null_ _null_ ));
 
 #endif	 /* PG_SHADOW_H */
