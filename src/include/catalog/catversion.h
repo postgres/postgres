61ba9e685bd94290a4bf1b7effbaf85013c9f94f
Bruce Momjian <bruce@momjian.us>
2001-05-14 21:59:19 +0000

    Update catalog version for fix.

--- src/include/catalog/catversion.h
+++ src/include/catalog/catversion.h
@@ -37,7 +37,7 @@
  * Portions Copyright (c) 1996-2001, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $Id: catversion.h,v 1.74 2001/05/14 21:53:16 momjian Exp $
+ * $Id: catversion.h,v 1.75 2001/05/14 21:59:19 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -53,6 +53,6 @@
  */
 
 /*							yyyymmddN */
-#define CATALOG_VERSION_NO	2001051402
+#define CATALOG_VERSION_NO	2001051403
 
 #endif
