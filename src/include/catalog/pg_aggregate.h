1945096324d16489c71e04139c4b4191b93f2f1f
Tom Lane <tgl@sss.pgh.pa.us>
2001-05-18 15:59:04 +0000

    Correct recently-broken avg(interval) definition.

--- src/include/catalog/pg_aggregate.h
+++ src/include/catalog/pg_aggregate.h
@@ -8,7 +8,7 @@
  * Portions Copyright (c) 1996-2001, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $Id: pg_aggregate.h,v 1.29 2001/03/22 04:00:36 momjian Exp $
+ * $Id: pg_aggregate.h,v 1.30 2001/05/18 15:59:04 tgl Exp $
  *
  * NOTES
  *	  the genbki.sh script reads this file and generates .bki
@@ -87,7 +87,7 @@ DATA(insert OID = 0 ( avg	PGUID int2_accum	numeric_avg		21	 1231 1700 "{0,0,0}" 
 DATA(insert OID = 0 ( avg	PGUID numeric_accum  numeric_avg	1700 1231 1700 "{0,0,0}" ));
 DATA(insert OID = 0 ( avg	PGUID float4_accum	float8_avg		700  1022 701 "{0,0,0}" ));
 DATA(insert OID = 0 ( avg	PGUID float8_accum	float8_avg		701  1022 701 "{0,0,0}" ));
-DATA(insert OID = 0 ( avg	PGUID interval_accum interval_avg	1186 1187 1186 "{0,0}" ));
+DATA(insert OID = 0 ( avg	PGUID interval_accum interval_avg	1186 1187 1186 "{0 second,0 second}" ));
 
 DATA(insert OID = 0 ( sum	PGUID int8_sum			-	20 1700 1700 _null_ ));
 DATA(insert OID = 0 ( sum	PGUID int4_sum			-	23 1700 1700 _null_ ));
