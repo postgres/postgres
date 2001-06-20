f44528978196fa22b67a540d73d6bf66762d3fee
Peter Eisentraut <peter_e@gmx.net>
2001-06-20 18:25:26 +0000

    Swedish translation for psql (my interpretation of Swedish, surely)

--- src/bin/psql/nls.mk
+++ src/bin/psql/nls.mk
@@ -1,6 +1,6 @@
-# $Header: /cvsroot/pgsql/src/bin/psql/nls.mk,v 1.2 2001/06/11 18:23:33 petere Exp $
+# $Header: /cvsroot/pgsql/src/bin/psql/nls.mk,v 1.3 2001/06/20 18:25:26 petere Exp $
 CATALOG_NAME	:= psql
-AVAIL_LANGUAGES	:= de fr
+AVAIL_LANGUAGES	:= de fr sv
 GETTEXT_FILES	:= command.c common.c copy.c help.c input.c large_obj.c \
                    mainloop.c print.c startup.c
                    # describe.c needs work
