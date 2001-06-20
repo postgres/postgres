c5f7a94f3c7294e7a6ee649c81a4cf51e0850baf
Peter Eisentraut <peter_e@gmx.net>
2001-06-20 18:33:31 +0000

    There is no RTLD_GLOBAL on OpenBSD, says Alex Pilosov <alex@pilosoft.com>.

--- src/backend/port/dynloader/openbsd.h
+++ src/backend/port/dynloader/openbsd.h
@@ -1,13 +1,11 @@
 /*-------------------------------------------------------------------------
  *
- * port_protos.h
- *	  port-specific prototypes for NetBSD 1.0
- *
+ * Dynamic loader for OpenBSD
  *
  * Portions Copyright (c) 1996-2001, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $Id: openbsd.h,v 1.5 2001/05/15 16:55:27 petere Exp $
+ * $Id: openbsd.h,v 1.6 2001/06/20 18:33:31 petere Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -34,7 +32,7 @@
  * begin with an underscore is fairly tricky, and some versions of
  * NetBSD (like 1.0, and 1.0A pre June 1995) have no dlerror.)
  */
-#define		   pg_dlopen(f)    BSD44_derived_dlopen((f), RTLD_LAZY | RTLD_GLOBAL)
+#define		   pg_dlopen(f)    BSD44_derived_dlopen((f), RTLD_LAZY)
 #define		   pg_dlsym		   BSD44_derived_dlsym
 #define		   pg_dlclose	   BSD44_derived_dlclose
 #define		   pg_dlerror	   BSD44_derived_dlerror
