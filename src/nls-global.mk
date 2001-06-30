b1c5b3459d0710a8a8fa718f2b7c424fbcd98532
Peter Eisentraut <peter_e@gmx.net>
2001-06-30 21:58:06 +0000

    Add missing dependency and fix thinko.

--- src/nls-global.mk
+++ src/nls-global.mk
@@ -1,4 +1,4 @@
-# $Header: /cvsroot/pgsql/src/nls-global.mk,v 1.1 2001/06/02 18:25:17 petere Exp $
+# $Header: /cvsroot/pgsql/src/nls-global.mk,v 1.2 2001/06/30 21:58:06 petere Exp $
 
 # Common rules for Native Language Support (NLS)
 #
@@ -35,7 +35,9 @@ endif
 PO_FILES = $(addsuffix .po, $(LANGUAGES))
 MO_FILES = $(addsuffix .mo, $(LANGUAGES))
 
-XGETTEXT += --foreign-user
+ifdef XGETTEXT
+XGETTEXT += --foreign-user -ctranslator
+endif
 
 
 all-po: $(MO_FILES)
@@ -48,10 +50,10 @@ distprep: $(srcdir)/$(CATALOG_NAME).pot
 ifdef XGETTEXT
 ifeq ($(word 1,$(GETTEXT_FILES)),+)
 $(srcdir)/$(CATALOG_NAME).pot: $(word 2, $(GETTEXT_FILES))
-	$(XGETTEXT) -D $(srcdir) -ctranslator -n $(addprefix -k, $(GETTEXT_TRIGGERS)) -f $<
+	$(XGETTEXT) -D $(srcdir) -n $(addprefix -k, $(GETTEXT_TRIGGERS)) -f $<
 else
 $(srcdir)/$(CATALOG_NAME).pot: $(GETTEXT_FILES)
-	$(XGETTEXT) -ctranslator -n $(addprefix -k, $(GETTEXT_TRIGGERS)) $^
+	$(XGETTEXT) -n $(addprefix -k, $(GETTEXT_TRIGGERS)) $^
 endif
 	mv messages.po $@
 else # not XGETTEXT
@@ -75,7 +77,7 @@ clean-po:
 	rm -f $(MO_FILES)
 	@rm -f $(addsuffix .po.old, $(AVAIL_LANGUAGES))
 
-maintainer-clean-po:
+maintainer-clean-po: clean-po
 	rm -f $(srcdir)/$(CATALOG_NAME).pot
 
 
