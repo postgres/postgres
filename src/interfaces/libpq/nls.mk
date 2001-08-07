7739cde4f6727615378092876a79ac6ef6d6e96a
Peter Eisentraut <peter_e@gmx.net>
2001-08-07 11:41:17 +0000

    Russian translation by Serguei Mokhov

--- src/interfaces/libpq/nls.mk
+++ src/interfaces/libpq/nls.mk
@@ -1,5 +1,5 @@
-# $Header: /cvsroot/pgsql/src/interfaces/libpq/nls.mk,v 1.1 2001/07/15 13:45:04 petere Exp $
+# $Header: /cvsroot/pgsql/src/interfaces/libpq/nls.mk,v 1.2 2001/08/07 11:41:17 petere Exp $
 CATALOG_NAME	:= libpq
-AVAIL_LANGUAGES	:= de
+AVAIL_LANGUAGES	:= de ru
 GETTEXT_FILES	:= fe-auth.c fe-connect.c fe-exec.c fe-lobj.c fe-misc.c
 GETTEXT_TRIGGERS:= libpq_gettext
