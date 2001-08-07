957eea278ed4ce42afd194588a9ec76bdbb87536
Peter Eisentraut <peter_e@gmx.net>
2001-08-07 11:28:18 +0000

    Czech translation for psql from Karel Zak

--- src/bin/psql/nls.mk
+++ src/bin/psql/nls.mk
@@ -1,6 +1,6 @@
-# $Header: /cvsroot/pgsql/src/bin/psql/nls.mk,v 1.4 2001/06/30 17:26:12 petere Exp $
+# $Header: /cvsroot/pgsql/src/bin/psql/nls.mk,v 1.5 2001/08/07 11:28:18 petere Exp $
 CATALOG_NAME	:= psql
-AVAIL_LANGUAGES	:= de fr sv
+AVAIL_LANGUAGES	:= cs de fr sv
 GETTEXT_FILES	:= command.c common.c copy.c help.c input.c large_obj.c \
                    mainloop.c print.c startup.c describe.c
 GETTEXT_TRIGGERS:= _ psql_error simple_prompt
