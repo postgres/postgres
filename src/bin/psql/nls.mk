88371647302c89ca128e4eae5d4638e918e3d4de
Peter Eisentraut <peter_e@gmx.net>
2001-09-06 11:10:47 +0000

    Russian translation from Serguei Mokhov

--- src/bin/psql/nls.mk
+++ src/bin/psql/nls.mk
@@ -1,6 +1,6 @@
-# $Header: /cvsroot/pgsql/src/bin/psql/nls.mk,v 1.5 2001/08/07 11:28:18 petere Exp $
+# $Header: /cvsroot/pgsql/src/bin/psql/nls.mk,v 1.6 2001/09/06 11:10:46 petere Exp $
 CATALOG_NAME	:= psql
-AVAIL_LANGUAGES	:= cs de fr sv
+AVAIL_LANGUAGES	:= cs de fr ru sv
 GETTEXT_FILES	:= command.c common.c copy.c help.c input.c large_obj.c \
                    mainloop.c print.c startup.c describe.c
 GETTEXT_TRIGGERS:= _ psql_error simple_prompt
