583096a7f9320e8e7b55845884c1958b042f7067
Bruce Momjian <bruce@momjian.us>
2001-10-05 15:47:48 +0000

    > > I am quite sure that all AIX Versions accept the CLOBBER method, > > thus I ask you to apply the following patch, to make it work.
    Zeugswetter Andreas SB SD

--- src/backend/utils/misc/ps_status.c
+++ src/backend/utils/misc/ps_status.c
@@ -5,7 +5,7 @@
  * to contain some useful information. Differs wildly across
  * platforms.
  *
- * $Header: /cvsroot/pgsql/src/backend/utils/misc/ps_status.c,v 1.4 2001/03/22 04:00:06 momjian Exp $
+ * $Header: /cvsroot/pgsql/src/backend/utils/misc/ps_status.c,v 1.5 2001/10/05 15:47:48 momjian Exp $
  *
  * Copyright 2000 by PostgreSQL Global Development Group
  * various details abducted from various places
@@ -62,7 +62,7 @@ extern char **environ;
 #define PS_USE_PS_STRINGS
 #elif defined(BSD) || defined(__bsdi__) || defined(__hurd__)
 #define PS_USE_CHANGE_ARGV
-#elif defined(__linux__) || defined(_AIX4) || defined(_AIX3) || defined(__sgi) || (defined(sun) && !defined(BSD)) || defined(ultrix) || defined(__ksr__) || defined(__osf__) || defined(__QNX__) || defined(__svr4__) || defined(__svr5__)
+#elif defined(__linux__) || defined(_AIX) || defined(__sgi) || (defined(sun) && !defined(BSD)) || defined(ultrix) || defined(__ksr__) || defined(__osf__) || defined(__QNX__) || defined(__svr4__) || defined(__svr5__)
 #define PS_USE_CLOBBER_ARGV
 #else
 #define PS_USE_NONE
@@ -70,7 +70,7 @@ extern char **environ;
 
 
 /* Different systems want the buffer padded differently */
-#if defined(_AIX3) || defined(__linux__) || defined(__QNX__) || defined(__svr4__)
+#if defined(_AIX) || defined(__linux__) || defined(__QNX__) || defined(__svr4__)
 #define PS_PADDING '\0'
 #else
 #define PS_PADDING ' '
