c781600618317188c81b0ba636b0b3120ce73854
Tom Lane <tgl@sss.pgh.pa.us>
2001-10-09 14:00:22 +0000

    Fix typo in error message.  Noted by laser@zhengmai.com.cn.

--- src/backend/tcop/utility.c
+++ src/backend/tcop/utility.c
@@ -10,7 +10,7 @@
  *
  *
  * IDENTIFICATION
- *	  $Header: /cvsroot/pgsql/src/backend/tcop/utility.c,v 1.118 2001/09/18 01:59:06 tgl Exp $
+ *	  $Header: /cvsroot/pgsql/src/backend/tcop/utility.c,v 1.119 2001/10/09 14:00:22 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -310,7 +310,7 @@ ProcessUtility(Node *parsetree,
 					elog(ERROR, "TRUNCATE cannot be used on sequences. '%s' is a sequence",
 						 relname);
 				if (rel->rd_rel->relkind == RELKIND_VIEW)
-					elog(ERROR, "TRUNCATE cannot be used on views. '%s' is a sequence",
+					elog(ERROR, "TRUNCATE cannot be used on views. '%s' is a view",
 						 relname);
 				heap_close(rel, NoLock);
 
