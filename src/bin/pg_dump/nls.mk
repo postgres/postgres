3aaaf5aeee87a0f1072fb1adf5142b7e6ba47dd2
Peter Eisentraut <peter_e@gmx.net>
2001-12-13 22:06:33 +0000

    Add Swedish

--- src/bin/pg_dump/nls.mk
+++ src/bin/pg_dump/nls.mk
@@ -1,6 +1,6 @@
-# $Header: /cvsroot/pgsql/src/bin/pg_dump/nls.mk,v 1.4 2001/11/29 18:59:28 petere Exp $
+# $Header: /cvsroot/pgsql/src/bin/pg_dump/nls.mk,v 1.5 2001/12/13 22:06:33 petere Exp $
 CATALOG_NAME	:= pg_dump
-AVAIL_LANGUAGES	:= de ru zh_CN zh_TW
+AVAIL_LANGUAGES	:= de ru sv zh_CN zh_TW
 GETTEXT_FILES	:= pg_dump.c common.c pg_backup_archiver.c pg_backup_custom.c \
                    pg_backup_db.c pg_backup_files.c pg_backup_null.c \
                    pg_backup_tar.c pg_restore.c
--- src/interfaces/libpq/nls.mk
+++ src/interfaces/libpq/nls.mk
@@ -1,5 +1,5 @@
-# $Header: /cvsroot/pgsql/src/interfaces/libpq/nls.mk,v 1.4 2001/12/10 13:03:55 petere Exp $
+# $Header: /cvsroot/pgsql/src/interfaces/libpq/nls.mk,v 1.5 2001/12/13 22:05:57 petere Exp $
 CATALOG_NAME	:= libpq
-AVAIL_LANGUAGES	:= de fr ru zh_CN zh_TW
+AVAIL_LANGUAGES	:= de fr ru sv zh_CN zh_TW
 GETTEXT_FILES	:= fe-auth.c fe-connect.c fe-exec.c fe-lobj.c fe-misc.c
 GETTEXT_TRIGGERS:= libpq_gettext
