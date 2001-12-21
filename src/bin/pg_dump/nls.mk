cb85a628074f51b8846c3ddb3804623c2e7b926a
Peter Eisentraut <peter_e@gmx.net>
2001-12-21 22:30:49 +0000

    Czech translation updates from Karel Zak

--- src/bin/pg_dump/nls.mk
+++ src/bin/pg_dump/nls.mk
@@ -1,6 +1,6 @@
-# $Header: /cvsroot/pgsql/src/bin/pg_dump/nls.mk,v 1.5 2001/12/13 22:06:33 petere Exp $
+# $Header: /cvsroot/pgsql/src/bin/pg_dump/nls.mk,v 1.6 2001/12/21 22:30:49 petere Exp $
 CATALOG_NAME	:= pg_dump
-AVAIL_LANGUAGES	:= de ru sv zh_CN zh_TW
+AVAIL_LANGUAGES	:= cs de ru sv zh_CN zh_TW
 GETTEXT_FILES	:= pg_dump.c common.c pg_backup_archiver.c pg_backup_custom.c \
                    pg_backup_db.c pg_backup_files.c pg_backup_null.c \
                    pg_backup_tar.c pg_restore.c
