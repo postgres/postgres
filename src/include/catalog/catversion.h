f5bbcfb0b238abf27dd885cad84c86b5bb538b73
Tom Lane <tgl@sss.pgh.pa.us>
2002-01-12 18:09:04 +0000

    Bump catversion to ensure people apply recent timestamp and sequence fixes.

--- src/include/catalog/catversion.h
+++ src/include/catalog/catversion.h
@@ -37,7 +37,7 @@
  * Portions Copyright (c) 1996-2001, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $Id: catversion.h,v 1.102 2001/10/28 06:25:59 momjian Exp $
+ * $Id: catversion.h,v 1.103 2002/01/12 18:09:04 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -53,6 +53,6 @@
  */
 
 /*							yyyymmddN */
-#define CATALOG_VERSION_NO	200110251
+#define CATALOG_VERSION_NO	200201121
 
 #endif
