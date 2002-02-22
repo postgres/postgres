326191c9b097169cc98f4dc286a43459e6c34385
Bruce Momjian <bruce@momjian.us>
2002-02-22 15:40:28 +0000

    We had a problem with to compile pgsql-7.2 under SW-8.0. In the mailing lists I found no informations. See note for further informations.
    Add missing AuthBlockSig.
    
    regards Heiko

--- src/include/libpq/pqsignal.h
+++ src/include/libpq/pqsignal.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2001, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $Id: pqsignal.h,v 1.19 2001/11/05 17:46:33 momjian Exp $
+ * $Id: pqsignal.h,v 1.19.2.1 2002/02/22 15:40:28 momjian Exp $
  *
  * NOTES
  *	  This shouldn't be in libpq, but the monitor and some other
@@ -28,7 +28,8 @@ extern sigset_t UnBlockSig,
 #define PG_SETMASK(mask)	sigprocmask(SIG_SETMASK, mask, NULL)
 #else
 extern int	UnBlockSig,
-			BlockSig;
+			BlockSig,
+			AuthBlockSig;
 
 #define PG_SETMASK(mask)	sigsetmask(*((int*)(mask)))
 #endif
