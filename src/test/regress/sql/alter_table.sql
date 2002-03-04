26c246036dd22f9c27f992bae42f5d667b9d793e
Tom Lane <tgl@sss.pgh.pa.us>
2002-03-04 05:17:55 +0000

    Fix very ancient breakage in alter-table tests: apparently, there was a type named 'dt' back in Postgres 4.2, and the regression test wasn't updated when it was removed.  Per report from Patricia Holben of Great Bridge.

--- src/test/regress/sql/alter_table.sql
+++ src/test/regress/sql/alter_table.sql
@@ -25,7 +25,7 @@ ALTER TABLE tmp ADD COLUMN i char;
 
 ALTER TABLE tmp ADD COLUMN j abstime[];
 
-ALTER TABLE tmp ADD COLUMN k dt;
+ALTER TABLE tmp ADD COLUMN k int4;
 
 ALTER TABLE tmp ADD COLUMN l tid;
 
@@ -60,10 +60,10 @@ INSERT INTO tmp (a, b, c, d, e, f, g, h, i, j, k, l, m, n, p, q, r, s, t, u,
 	v, w, x, y, z)
    VALUES (4, 'name', 'text', 4.1, 4.1, 2, '(4.1,4.1,3.1,3.1)', 
         'Mon May  1 00:30:30 1995', 'c', '{Mon May  1 00:30:30 1995, Monday Aug 24 14:43:07 1992, epoch}', 
-	314159, '(1,1)', 512,
+	314159, '(1,1)', '512',
 	'1 2 3 4 5 6 7 8', 'magnetic disk', '(1.1,1.1)', '(4.1,4.1,3.1,3.1)',
-	'(0,2,4.1,4.1,3.1,3.1)', '(4.1,4.1,3.1,3.1)', '["current" "infinity"]',
-	'1/3', '1,name', '{1.0,2.0,3.0,4.0}', '{1.0,2.0,3.0,4.0}', '{1,2,3,4}');
+	'(0,2,4.1,4.1,3.1,3.1)', '(4.1,4.1,3.1,3.1)', '["epoch" "infinity"]',
+	'epoch', '01:00:10', '{1.0,2.0,3.0,4.0}', '{1.0,2.0,3.0,4.0}', '{1,2,3,4}');
 
 SELECT * FROM tmp;
 
@@ -94,7 +94,7 @@ ALTER TABLE tmp ADD COLUMN i char;
 
 ALTER TABLE tmp ADD COLUMN j abstime[];
 
-ALTER TABLE tmp ADD COLUMN k dt;
+ALTER TABLE tmp ADD COLUMN k int4;
 
 ALTER TABLE tmp ADD COLUMN l tid;
 
@@ -128,11 +128,11 @@ ALTER TABLE tmp ADD COLUMN z int2[];
 INSERT INTO tmp (a, b, c, d, e, f, g, h, i, j, k, l, m, n, p, q, r, s, t, u,
 	v, w, x, y, z)
    VALUES (4, 'name', 'text', 4.1, 4.1, 2, '(4.1,4.1,3.1,3.1)', 
-	'Mon May  1 00:30:30 1995', 'c', '{Mon May  1 00:30:30 1995, Monday Aug 24 14:43:07 1992, epoch}',
-	 314159, '(1,1)', 512,
+        'Mon May  1 00:30:30 1995', 'c', '{Mon May  1 00:30:30 1995, Monday Aug 24 14:43:07 1992, epoch}', 
+	314159, '(1,1)', '512',
 	'1 2 3 4 5 6 7 8', 'magnetic disk', '(1.1,1.1)', '(4.1,4.1,3.1,3.1)',
-	'(0,2,4.1,4.1,3.1,3.1)', '(4.1,4.1,3.1,3.1)', '["current" "infinity"]',
-	'1/3', '1,name', '{1.0,2.0,3.0,4.0}', '{1.0,2.0,3.0,4.0}', '{1,2,3,4}');
+	'(0,2,4.1,4.1,3.1,3.1)', '(4.1,4.1,3.1,3.1)', '["epoch" "infinity"]',
+	'epoch', '01:00:10', '{1.0,2.0,3.0,4.0}', '{1.0,2.0,3.0,4.0}', '{1,2,3,4}');
 
 SELECT * FROM tmp;
 
