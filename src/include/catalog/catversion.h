a4b361b747b462a924c2a3115d0c06fd407543f8
Bruce Momjian <bruce@momjian.us>
2002-04-24 03:09:09 +0000

    Add missing columns for UNKNOWN patch.  Increment catalog version.

--- src/include/catalog/catversion.h
+++ src/include/catalog/catversion.h
@@ -37,7 +37,7 @@
  * Portions Copyright (c) 1996-2001, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $Id: catversion.h,v 1.123 2002/04/24 02:12:53 momjian Exp $
+ * $Id: catversion.h,v 1.124 2002/04/24 03:09:09 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -53,6 +53,6 @@
  */
 
 /*							yyyymmddN */
-#define CATALOG_VERSION_NO	200204231
+#define CATALOG_VERSION_NO	200204232
 
 #endif
