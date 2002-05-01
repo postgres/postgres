61446e0927ab31bf4227c7eb3de95b72540f051a
Tom Lane <tgl@sss.pgh.pa.us>
2002-05-01 17:12:08 +0000

    Improve lexer's error reporting.  You get the whole token mentioned now in parse error messages, not just the part scanned by the last flex rule. For example, 	select "foo" "bar"; used to draw 	ERROR:  parser: parse error at or near """ which was rather unhelpful.  Now it gives 	ERROR:  parser: parse error at or near ""bar"" Also, error messages concerning bitstring literals and suchlike will quote the source text at you, not the processed internal form of the literal.

--- src/backend/po/nls.mk
+++ src/backend/po/nls.mk
@@ -1,4 +1,4 @@
 CATALOG_NAME	:= postgres
 AVAIL_LANGUAGES	:= cs de hu ru zh_CN zh_TW
 GETTEXT_FILES	:= + gettext-files
-GETTEXT_TRIGGERS:= elog:2 postmaster_error
+GETTEXT_TRIGGERS:= elog:2 postmaster_error yyerror
