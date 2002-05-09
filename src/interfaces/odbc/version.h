16e22de53ab530843237121fa4daf12da24f1d41
Hiroshi Inoue <inoue@tpf.co.jp>
2002-05-09 04:10:22 +0000

    Add a file for version control.

---
+++ src/interfaces/odbc/version.h
@@ -0,0 +1,16 @@
+/* File:			version.h
+ *
+ * Description:		This file defines the driver version.
+ *
+ * Comments:		See "notice.txt" for copyright and license information.
+ *
+ */
+
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+#define POSTGRESDRIVERVERSION		"07.02.0002"
+#define POSTGRES_RESOURCE_VERSION	"07.02.0002\0"
+#define PG_DRVFILE_VERSION		7,2,0,02
+
+#endif
