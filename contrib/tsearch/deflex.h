2860041bf0c132602c1bc805386984faa2a26e55
Bruce Momjian <bruce@momjian.us>
2002-08-15 03:02:08 +0000

    August 13, 2002          Use parser of OpenFTS v0.33.
    --
    Teodor Sigaev

--- contrib/tsearch/deflex.h
+++ contrib/tsearch/deflex.h
@@ -2,28 +2,33 @@
 #define __DEFLEX_H__
 
 /* rememder !!!! */
-#define LASTNUM		19
+#define LASTNUM		23
 
 #define LATWORD		1
-#define NONLATINWORD	2
+#define CYRWORD		2
 #define UWORD		3
 #define EMAIL		4
 #define FURL		5
 #define HOST		6
-#define FLOAT		7
-#define FINT		8
-#define PARTWORD	9
-#define NONLATINPARTWORD	10
-#define LATPARTWORD		11
-#define SPACE		12
-#define SYMTAG		13
-#define HTTP		14
-#define DEFISWORD	15
-#define DEFISLATWORD	16
-#define DEFISNONLATINWORD	17
+#define SCIENTIFIC	7
+#define VERSIONNUMBER	8
+#define PARTHYPHENWORD 	9	
+#define CYRPARTHYPHENWORD 	10	
+#define LATPARTHYPHENWORD 	11	
+#define SPACE 		12
+#define TAG 		13
+#define HTTP 		14
+#define HYPHENWORD	15
+#define LATHYPHENWORD	16
+#define CYRHYPHENWORD	17
 #define URI		18
 #define FILEPATH	19
+#define DECIMAL		20
+#define SIGNEDINT	21
+#define UNSIGNEDINT	22
+#define HTMLENTITY	23
 
 extern const char *descr[];
 
 #endif
+
