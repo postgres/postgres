50edd63b988799412c49b1ecd42d1f66f41f13dc
Bruce Momjian <bruce@momjian.us>
2002-08-20 04:48:36 +0000

    - test for indexes on a domain - don't fail type-sanity checks if a domain exists
    Rod Taylor

--- src/test/regress/sql/domain.sql
+++ src/test/regress/sql/domain.sql
@@ -111,11 +111,11 @@ create table defaulttest
             ( col1 ddef1
             , col2 ddef2
             , col3 ddef3
-            , col4 ddef4
+            , col4 ddef4 PRIMARY KEY
             , col5 ddef1 NOT NULL DEFAULT NULL
             , col6 ddef2 DEFAULT '88'
             , col7 ddef4 DEFAULT 8000
-		, col8 ddef5
+            , col8 ddef5
             );
 insert into defaulttest default values;
 insert into defaulttest default values;
