b28b05317de33d1ddb5f91d93f178bcabb28b801
Peter Eisentraut <peter_e@gmx.net>
2002-09-02 22:22:12 +0000

    Translation updates

--- src/bin/pg_controldata/nls.mk
+++ src/bin/pg_controldata/nls.mk
@@ -1,5 +1,5 @@
-# $Header: /cvsroot/pgsql/src/bin/pg_controldata/nls.mk,v 1.1 2002/08/21 22:24:34 petere Exp $
+# $Header: /cvsroot/pgsql/src/bin/pg_controldata/nls.mk,v 1.2 2002/09/02 22:22:12 petere Exp $
 CATALOG_NAME	:= pg_controldata
-AVAIL_LANGUAGES	:= de
+AVAIL_LANGUAGES	:= de es ru sv
 GETTEXT_FILES	:= pg_controldata.c
 GETTEXT_TRIGGERS:= _
--- src/interfaces/libpq/nls.mk
+++ src/interfaces/libpq/nls.mk
@@ -1,5 +1,5 @@
-# $Header: /cvsroot/pgsql/src/interfaces/libpq/nls.mk,v 1.6 2001/12/21 22:30:49 petere Exp $
+# $Header: /cvsroot/pgsql/src/interfaces/libpq/nls.mk,v 1.7 2002/09/02 22:22:12 petere Exp $
 CATALOG_NAME	:= libpq
-AVAIL_LANGUAGES	:= cs de fr ru sv zh_CN zh_TW
+AVAIL_LANGUAGES	:= cs de es fr pt_BR ru sv zh_CN zh_TW
 GETTEXT_FILES	:= fe-auth.c fe-connect.c fe-exec.c fe-lobj.c fe-misc.c
 GETTEXT_TRIGGERS:= libpq_gettext
