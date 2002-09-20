24bebf0b725e965ed2b725dcb31a46f787e98dbf
Bruce Momjian <bruce@momjian.us>
2002-09-20 03:47:22 +0000

    I have included fixes to declare some floating point constants as double instead of int, change the calculation method to use the haversine formula which is more accurrate for short distances, added a grant to public for geo_distance and added a regression test.
    I will resubmit the earth distance stuff based on cube after 7.3 is
    released.
    
    Bruno Wolff III

--- contrib/earthdistance/earthdistance.c
+++ contrib/earthdistance/earthdistance.c
@@ -6,8 +6,8 @@
 
 
 /* Earth's radius is in statute miles. */
-const int	EARTH_RADIUS = 3958.747716;
-const int	TWO_PI = 2.0 * M_PI;
+const double	EARTH_RADIUS = 3958.747716;
+const double	TWO_PI = 2.0 * M_PI;
 
 double	   *geo_distance(Point *pt1, Point *pt2);
 
@@ -50,6 +50,7 @@ geo_distance(Point *pt1, Point *pt2)
 				long2,
 				lat2;
 	double		longdiff;
+        double		sino;
 	double	   *resultp = palloc(sizeof(double));
 
 	/* convert degrees to radians */
@@ -65,8 +66,10 @@ geo_distance(Point *pt1, Point *pt2)
 	if (longdiff > M_PI)
 		longdiff = TWO_PI - longdiff;
 
-	*resultp = EARTH_RADIUS * acos
-		(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(longdiff));
+        sino = sqrt(sin(fabs(lat1-lat2)/2.)*sin(fabs(lat1-lat2)/2.) +
+                cos(lat1) * cos(lat2) * sin(longdiff/2.)*sin(longdiff/2.));
+        if (sino > 1.) sino = 1.;
+        *resultp = 2. * EARTH_RADIUS * asin(sino);
 
 	return resultp;
 }
