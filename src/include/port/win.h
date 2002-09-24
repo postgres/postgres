12bb3679dbb556d050f084335b364844a3bc4837
Peter Eisentraut <peter_e@gmx.net>
2002-09-24 20:20:10 +0000

    Remove useless O_DIROPEN stuff.

--- src/include/port/win.h
+++ src/include/port/win.h
@@ -1,10 +1,6 @@
 #define HAS_TEST_AND_SET
 typedef unsigned char slock_t;
 
-#ifndef O_DIROPEN
-#define O_DIROPEN	0x100000	/* should be in sys/fcntl.h */
-#endif
-
 #define tzname _tzname			/* should be in time.h? */
 #define HAVE_INT_TIMEZONE		/* has int _timezone */
 
