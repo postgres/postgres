fd972e7f7edeee5eb410070e204a11a197703cf9
Dave Cramer <davec@fastcrypt.com>
2002-10-01 16:11:30 +0000

    test files added by Kris Jurka

---
+++ src/interfaces/jdbc/org/postgresql/test/jdbc2/SerializeObject.java
@@ -0,0 +1,11 @@
+package org.postgresql.test.jdbc2;
+
+import java.io.Serializable;
+
+public class SerializeObject implements Serializable {
+
+	public int intcol;
+	public double doublecol;
+	public String stringcol;
+
+}
