b825a8f0255f5925721a00a7bfb391886ba04e13
Bruce Momjian <bruce@momjian.us>
2002-10-21 20:33:21 +0000

    When I removed the cube based stuff from eathdistance I accidentally left a reference to cube in a comment in the regression test (that also shows up in the expected output). This doesn't cause any real problem, but people who read the comment might be confused. Attached is a diff to remove the reference.
    Bruno Wolff III

--- contrib/earthdistance/sql/earthdistance.sql
+++ contrib/earthdistance/sql/earthdistance.sql
@@ -4,7 +4,7 @@
 
 --
 -- first, define the datatype.  Turn off echoing so that expected file
--- does not depend on contents of earthdistance.sql or cube.sql.
+-- does not depend on contents of earthdistance.sql.
 --
 \set ECHO none
 \i earthdistance.sql
