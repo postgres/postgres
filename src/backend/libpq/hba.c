bab9c3d5551db2e2c5620671f5679cfaec8584b9
Bruce Momjian <bruce@momjian.us>
2002-12-11 22:17:31 +0000

    Fix line count error reporting in config files, like pg_hba.conf, per report from Oliver Elphick.
    Backpatch to 7.3.

--- src/backend/libpq/hba.c
+++ src/backend/libpq/hba.c
@@ -10,7 +10,7 @@
  *
  *
  * IDENTIFICATION
- *	  $Header: /cvsroot/pgsql/src/backend/libpq/hba.c,v 1.87 2002/09/04 20:31:19 momjian Exp $
+ *	  $Header: /cvsroot/pgsql/src/backend/libpq/hba.c,v 1.87.2.1 2002/12/11 22:17:31 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -93,6 +93,7 @@ void
 next_token(FILE *fp, char *buf, const int bufsz)
 {
 	int			c;
+	char	   *start_buf = buf;
 	char	   *end_buf = buf + (bufsz - 1);
 	bool		in_quote = false;
 	bool		was_quote = false;
@@ -115,7 +116,10 @@ next_token(FILE *fp, char *buf, const int bufsz)
 			{
 				while ((c = getc(fp)) != EOF && c != '\n')
 					;
-				continue;
+				/* If only comment, consume EOL too; return EOL */
+				if (c != EOF && buf == start_buf)
+					c = getc(fp);
+				break;
 			}
 
 			if (buf >= end_buf)
