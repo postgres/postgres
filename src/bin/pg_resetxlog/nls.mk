b19a0c70d26d971094092e0567c116d019c3f307
Peter Eisentraut <peter_e@gmx.net>
2003-01-04 10:26:28 +0000

    Translation updates

--- src/bin/pg_resetxlog/nls.mk
+++ src/bin/pg_resetxlog/nls.mk
@@ -1,5 +1,5 @@
-# $Header: /cvsroot/pgsql/src/bin/pg_resetxlog/nls.mk,v 1.4 2002/10/14 19:04:28 petere Exp $
+# $Header: /cvsroot/pgsql/src/bin/pg_resetxlog/nls.mk,v 1.4.2.1 2003/01/04 10:26:20 petere Exp $
 CATALOG_NAME	:= pg_resetxlog
-AVAIL_LANGUAGES	:= de pt_BR ru sv zh_CN
+AVAIL_LANGUAGES	:= de hu pt_BR ru sv zh_CN
 GETTEXT_FILES	:= pg_resetxlog.c
 GETTEXT_TRIGGERS:= _
