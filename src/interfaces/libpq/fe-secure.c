671231f58853fa6e6bdcbbd62d4496bb574bd572
Bruce Momjian <bruce@momjian.us>
2003-01-08 22:57:05 +0000

    I was playing around with 7.3.1 and found some more SSL problems.  The first, that I missed when checking over 7.3.1, was that the client method was switched to SSLv23 along with the server.  The SSLv23 client method does SSLv2 by default, but can also understand SSLv3.  In our situation the SSLv2 backwords compatibility is really only needed on the server.  This is the first patch.
    The last thing is that I found a way for the server to understand SSLv2
    HELLO messages (sent by pre-7.3 clients) but then get them to talk
    SSLv3.  This is the last one.
    
    Nathan Mueller

--- src/interfaces/libpq/fe-secure.c
+++ src/interfaces/libpq/fe-secure.c
@@ -11,7 +11,7 @@
  *
  *
  * IDENTIFICATION
- *	  $Header: /cvsroot/pgsql/src/interfaces/libpq/fe-secure.c,v 1.15.2.2 2002/12/18 13:20:03 pgsql Exp $
+ *	  $Header: /cvsroot/pgsql/src/interfaces/libpq/fe-secure.c,v 1.15.2.3 2003/01/08 22:57:05 momjian Exp $
  *
  * NOTES
  *	  The client *requires* a valid server certificate.  Since
@@ -712,7 +712,7 @@ initialize_SSL(PGconn *conn)
 	{
 		SSL_library_init();
 		SSL_load_error_strings();
-		SSL_context = SSL_CTX_new(SSLv23_method());
+		SSL_context = SSL_CTX_new(TLSv1_method());
 		if (!SSL_context)
 		{
 			printfPQExpBuffer(&conn->errorMessage,
