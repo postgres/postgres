6f49703f0e6688748f0252d7eaf0c9e76a83ae52
Tom Lane <tgl@sss.pgh.pa.us>
2003-01-31 22:25:14 +0000

    Clean up plpython error reporting so that its regression test passes with some amount of cleanliness.  I see no need to report the internal Python name rather than the SQL procedure name in error tracebacks.

--- src/pl/plpython/test.sh
+++ src/pl/plpython/test.sh
@@ -30,8 +30,8 @@ echo -n "*** Running error handling tests."
 psql -q -e $DBNAME < plpython_error.sql > error.output 2>&1
 echo " Done. ***"
 
-echo -n "*** Checking the results of the feature tests"
-if diff -u feature.expected feature.output > feature.diff 2>&1 ; then
+echo -n "*** Checking the results of the feature tests."
+if diff -c feature.expected feature.output > feature.diff 2>&1 ; then
   echo -n " passed!"
 else
   echo -n " failed!  Please examine feature.diff."
@@ -39,10 +39,9 @@ fi
 echo " Done. ***"
 
 echo -n "*** Checking the results of the error handling tests."
-diff -u error.expected error.output > error.diff 2>&1
+if diff -c error.expected error.output > error.diff 2>&1 ; then
+  echo -n " passed!"
+else
+  echo -n " failed!  Please examine error.diff."
+fi
 echo " Done. ***"
-echo "*** You need to check the file error.diff and make sure that"
-echo "    any differences are due only to the oid encoded in the "
-echo "    python function name. ***"
-
-# or write a fancier error checker...
