73e3fad1e8a7d67eb7107b7d28fe7332e8d2ab08
Tom Lane <tgl@sss.pgh.pa.us>
2003-03-02 20:45:59 +0000

    Repair memory leak introduced by recent change to make SPI return a tupdesc even with zero tuples returned: some plpgsql routines assumed they didn't need to do SPI_freetuptable() after retrieving no tuples.

--- src/pl/plpgsql/src/pl_exec.c
+++ src/pl/plpgsql/src/pl_exec.c
@@ -3,7 +3,7 @@
  *			  procedural language
  *
  * IDENTIFICATION
- *	  $Header: /cvsroot/pgsql/src/pl/plpgsql/src/pl_exec.c,v 1.65.2.1 2003/01/21 22:06:36 tgl Exp $
+ *	  $Header: /cvsroot/pgsql/src/pl/plpgsql/src/pl_exec.c,v 1.65.2.2 2003/03/02 20:45:59 tgl Exp $
  *
  *	  This software is copyrighted by Jan Wieck - Hamburg.
  *
@@ -1369,11 +1369,12 @@ exec_stmt_fors(PLpgSQL_execstate * estate, PLpgSQL_stmt_fors * stmt)
 			if (rc != PLPGSQL_RC_OK)
 			{
 				/*
-				 * We're aborting the loop, so cleanup and set FOUND
+				 * We're aborting the loop, so cleanup and set FOUND.
+				 * (This code should match the code after the loop.)
 				 */
-				exec_set_found(estate, found);
 				SPI_freetuptable(tuptab);
 				SPI_cursor_close(portal);
+				exec_set_found(estate, found);
 
 				if (rc == PLPGSQL_RC_EXIT)
 				{
@@ -1409,6 +1410,11 @@ exec_stmt_fors(PLpgSQL_execstate * estate, PLpgSQL_stmt_fors * stmt)
 		n = SPI_processed;
 		tuptab = SPI_tuptable;
 	}
+
+	/*
+	 * Release last group of tuples
+	 */
+	SPI_freetuptable(tuptab);
 
 	/*
 	 * Close the implicit cursor
@@ -2363,7 +2369,7 @@ exec_stmt_dynfors(PLpgSQL_execstate * estate, PLpgSQL_stmt_dynfors * stmt)
 	if (n == 0)
 		exec_move_row(estate, rec, row, NULL, tuptab->tupdesc);
 	else
-		found = true;
+		found = true;			/* processed at least one tuple */
 
 	/*
 	 * Now do the loop
@@ -2382,14 +2388,15 @@ exec_stmt_dynfors(PLpgSQL_execstate * estate, PLpgSQL_stmt_dynfors * stmt)
 			 */
 			rc = exec_stmts(estate, stmt->body);
 
-			/*
-			 * We're aborting the loop, so cleanup and set FOUND
-			 */
 			if (rc != PLPGSQL_RC_OK)
 			{
-				exec_set_found(estate, found);
+				/*
+				 * We're aborting the loop, so cleanup and set FOUND.
+				 * (This code should match the code after the loop.)
+				 */
 				SPI_freetuptable(tuptab);
 				SPI_cursor_close(portal);
+				exec_set_found(estate, found);
 
 				if (rc == PLPGSQL_RC_EXIT)
 				{
@@ -2427,7 +2434,12 @@ exec_stmt_dynfors(PLpgSQL_execstate * estate, PLpgSQL_stmt_dynfors * stmt)
 	}
 
 	/*
-	 * Close the cursor
+	 * Release last group of tuples
+	 */
+	SPI_freetuptable(tuptab);
+
+	/*
+	 * Close the implicit cursor
 	 */
 	SPI_cursor_close(portal);
 
@@ -2736,24 +2748,15 @@ exec_stmt_fetch(PLpgSQL_execstate * estate, PLpgSQL_stmt_fetch * stmt)
 	pfree(curname);
 
 	/* ----------
-	 * Initialize the global found variable to false
-	 * ----------
-	 */
-	exec_set_found(estate, false);
-
-	/* ----------
 	 * Determine if we fetch into a record or a row
 	 * ----------
 	 */
 	if (stmt->rec != NULL)
 		rec = (PLpgSQL_rec *) (estate->datums[stmt->rec->recno]);
+	else if (stmt->row != NULL)
+		row = (PLpgSQL_row *) (estate->datums[stmt->row->rowno]);
 	else
-	{
-		if (stmt->row != NULL)
-			row = (PLpgSQL_row *) (estate->datums[stmt->row->rowno]);
-		else
-			elog(ERROR, "unsupported target in exec_stmt_select()");
-	}
+		elog(ERROR, "unsupported target in exec_stmt_fetch()");
 
 	/* ----------
 	 * Fetch 1 tuple from the cursor
@@ -2764,22 +2767,19 @@ exec_stmt_fetch(PLpgSQL_execstate * estate, PLpgSQL_stmt_fetch * stmt)
 	n = SPI_processed;
 
 	/* ----------
-	 * If the FETCH didn't return a row, set the target
-	 * to NULL and return with FOUND = false.
+	 * Set the target and the global FOUND variable appropriately.
 	 * ----------
 	 */
 	if (n == 0)
 	{
 		exec_move_row(estate, rec, row, NULL, tuptab->tupdesc);
-		return PLPGSQL_RC_OK;
+		exec_set_found(estate, false);
 	}
-
-	/* ----------
-	 * Put the result into the target and set found to true
-	 * ----------
-	 */
-	exec_move_row(estate, rec, row, tuptab->vals[0], tuptab->tupdesc);
-	exec_set_found(estate, true);
+	else
+	{
+		exec_move_row(estate, rec, row, tuptab->vals[0], tuptab->tupdesc);
+		exec_set_found(estate, true);
+	}
 
 	SPI_freetuptable(tuptab);
 
