5e8499d995679f6d8e4519da816ba14eb8d7396f
Bruce Momjian <bruce@momjian.us>
2003-03-27 16:55:11 +0000

    Add new file.

---
+++ src/include/executor/tstoreReceiver.h
@@ -0,0 +1,22 @@
+/*-------------------------------------------------------------------------
+ *
+ * tstoreReceiver.h
+ *	  prototypes for tstoreReceiver.c
+ *
+ *
+ * Portions Copyright (c) 1996-2002, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1994, Regents of the University of California
+ *
+ * $Id: tstoreReceiver.h,v 1.1 2003/03/27 16:55:11 momjian Exp $
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#ifndef TSTORE_RECEIVER_H
+#define TSTORE_RECEIVER_H
+
+#include "tcop/dest.h"
+
+extern DestReceiver *tstoreReceiverCreateDR(void);
+
+#endif	/* TSTORE_RECEIVER_H */
