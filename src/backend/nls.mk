2a4a0c4d71dcbc1820de85c4576aadae7bc17c70
Tom Lane <tgl@sss.pgh.pa.us>
2003-07-19 20:32:12 +0000

    Add ereport-related functions to GETTEXT_TRIGGERS list.

--- src/backend/nls.mk
+++ src/backend/nls.mk
@@ -1,8 +1,9 @@
-# $Header: /cvsroot/pgsql/src/backend/nls.mk,v 1.4 2003/06/28 22:30:59 petere Exp $
+# $Header: /cvsroot/pgsql/src/backend/nls.mk,v 1.5 2003/07/19 20:32:12 tgl Exp $
 CATALOG_NAME	:= postgres
 AVAIL_LANGUAGES	:= cs de es hr hu ru sv tr zh_CN zh_TW
 GETTEXT_FILES	:= + gettext-files
-GETTEXT_TRIGGERS:= elog:2 postmaster_error yyerror
+# elog should eventually be removed from this list:
+GETTEXT_TRIGGERS:= elog:2 errmsg errdetail errhint errcontext postmaster_error yyerror
 
 gettext-files:
 	find $(srcdir)/ -name '*.c' -print >$@
