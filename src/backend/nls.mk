b7489ac445a4719f3bc7b0268c8ad7161cec4137
Tom Lane <tgl@sss.pgh.pa.us>
2003-07-28 00:25:21 +0000

    elog() is no longer a gettext trigger --- all remaining uses are considered internal errors.

--- src/backend/nls.mk
+++ src/backend/nls.mk
@@ -1,9 +1,10 @@
-# $Header: /cvsroot/pgsql/src/backend/nls.mk,v 1.5 2003/07/19 20:32:12 tgl Exp $
+# $Header: /cvsroot/pgsql/src/backend/nls.mk,v 1.6 2003/07/28 00:25:21 tgl Exp $
 CATALOG_NAME	:= postgres
 AVAIL_LANGUAGES	:= cs de es hr hu ru sv tr zh_CN zh_TW
 GETTEXT_FILES	:= + gettext-files
-# elog should eventually be removed from this list:
-GETTEXT_TRIGGERS:= elog:2 errmsg errdetail errhint errcontext postmaster_error yyerror
+# you can add "elog:2" and "errmsg_internal" to this list if you want to
+# include internal messages in the translation list.
+GETTEXT_TRIGGERS:= errmsg errdetail errhint errcontext postmaster_error yyerror
 
 gettext-files:
 	find $(srcdir)/ -name '*.c' -print >$@
