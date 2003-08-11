7078441af0f16e02255cc8e52b00be5352e2465a
Peter Eisentraut <peter_e@gmx.net>
2003-08-11 15:19:58 +0000

    Translation updates

--- src/bin/scripts/nls.mk
+++ src/bin/scripts/nls.mk
@@ -1,6 +1,6 @@
-# $Header: /cvsroot/pgsql/src/bin/scripts/nls.mk,v 1.5 2003/08/01 16:19:14 petere Exp $
+# $Header: /cvsroot/pgsql/src/bin/scripts/nls.mk,v 1.6 2003/08/11 15:19:58 petere Exp $
 CATALOG_NAME    := pgscripts
-AVAIL_LANGUAGES := cs de ru
+AVAIL_LANGUAGES := cs de ru sv
 GETTEXT_FILES   := createdb.c createlang.c createuser.c \
                    dropdb.c droplang.c dropuser.c \
                    clusterdb.c vacuumdb.c \
