903fe8fc6fb0297198782e451c6b6d4396eeaa53
Peter Eisentraut <peter_e@gmx.net>
2003-08-24 21:18:53 +0000

    Translation updates

--- src/bin/psql/nls.mk
+++ src/bin/psql/nls.mk
@@ -1,6 +1,6 @@
-# $Header: /cvsroot/pgsql/src/bin/psql/nls.mk,v 1.8 2003/06/28 22:31:01 petere Exp $
+# $Header: /cvsroot/pgsql/src/bin/psql/nls.mk,v 1.9 2003/08/24 21:18:52 petere Exp $
 CATALOG_NAME	:= psql
-AVAIL_LANGUAGES	:= cs de fr hu ru sv zh_CN zh_TW
+AVAIL_LANGUAGES	:= cs de es fr hu ru sv zh_CN zh_TW
 GETTEXT_FILES	:= command.c common.c copy.c help.c input.c large_obj.c \
                    mainloop.c print.c startup.c describe.c
 GETTEXT_TRIGGERS:= _ psql_error simple_prompt
