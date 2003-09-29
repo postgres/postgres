b994b143a6dca59e654cafc5f80571d82085f7d6
Peter Eisentraut <peter_e@gmx.net>
2003-09-29 09:51:29 +0000

    New translations

--- src/backend/nls.mk
+++ src/backend/nls.mk
@@ -1,6 +1,6 @@
-# $Header: /cvsroot/pgsql/src/backend/nls.mk,v 1.7 2003/09/14 22:40:38 petere Exp $
+# $Header: /cvsroot/pgsql/src/backend/nls.mk,v 1.8 2003/09/29 09:51:28 petere Exp $
 CATALOG_NAME	:= postgres
-AVAIL_LANGUAGES	:= cs de es hr hu ru sv tr zh_CN zh_TW
+AVAIL_LANGUAGES	:= cs de es hr hu it ru sv tr zh_CN zh_TW
 GETTEXT_FILES	:= + gettext-files
 # you can add "elog:2" and "errmsg_internal" to this list if you want to
 # include internal messages in the translation list.
--- src/bin/pg_controldata/nls.mk
+++ src/bin/pg_controldata/nls.mk
@@ -1,5 +1,5 @@
-# $Header: /cvsroot/pgsql/src/bin/pg_controldata/nls.mk,v 1.5 2003/07/23 09:34:54 petere Exp $
+# $Header: /cvsroot/pgsql/src/bin/pg_controldata/nls.mk,v 1.6 2003/09/29 09:51:29 petere Exp $
 CATALOG_NAME	:= pg_controldata
-AVAIL_LANGUAGES	:= cs de es fr hu pt_BR ru sv zh_CN
+AVAIL_LANGUAGES	:= cs de es fr hu it pt_BR ru sv zh_CN
 GETTEXT_FILES	:= pg_controldata.c
 GETTEXT_TRIGGERS:= _
