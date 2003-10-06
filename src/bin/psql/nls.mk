34e9ab027be6cde28e013741474778503c3e8499
Peter Eisentraut <peter_e@gmx.net>
2003-10-06 21:50:45 +0000

    New Italian translation

--- src/bin/psql/nls.mk
+++ src/bin/psql/nls.mk
@@ -1,6 +1,6 @@
-# $Header: /cvsroot/pgsql/src/bin/psql/nls.mk,v 1.13 2003/10/06 17:37:39 petere Exp $
+# $Header: /cvsroot/pgsql/src/bin/psql/nls.mk,v 1.14 2003/10/06 21:50:44 petere Exp $
 CATALOG_NAME	:= psql
-AVAIL_LANGUAGES	:= cs de es fr hu nb pt_BR ru sl sv zh_CN zh_TW
+AVAIL_LANGUAGES	:= cs de es fr hu it nb pt_BR ru sl sv zh_CN zh_TW
 GETTEXT_FILES	:= command.c common.c copy.c help.c input.c large_obj.c \
                    mainloop.c print.c startup.c describe.c sql_help.h
 GETTEXT_TRIGGERS:= _ N_ psql_error simple_prompt
