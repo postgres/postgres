037468eff2e1fddefd0ece3e8208e70dc0a380e7
Peter Eisentraut <peter_e@gmx.net>
2003-10-12 19:16:36 +0000

    New translation

--- src/interfaces/libpq/nls.mk
+++ src/interfaces/libpq/nls.mk
@@ -1,5 +1,5 @@
-# $Header: /cvsroot/pgsql/src/interfaces/libpq/nls.mk,v 1.12 2003/10/06 16:31:16 petere Exp $
+# $Header: /cvsroot/pgsql/src/interfaces/libpq/nls.mk,v 1.13 2003/10/12 19:16:36 petere Exp $
 CATALOG_NAME	:= libpq
-AVAIL_LANGUAGES	:= cs de es fr hr nb pt_BR ru sl sv zh_CN zh_TW
+AVAIL_LANGUAGES	:= cs de es fr hr it nb pt_BR ru sl sv zh_CN zh_TW
 GETTEXT_FILES	:= fe-auth.c fe-connect.c fe-exec.c fe-lobj.c fe-misc.c fe-secure.c
 GETTEXT_TRIGGERS:= libpq_gettext pqInternalNotice:2
