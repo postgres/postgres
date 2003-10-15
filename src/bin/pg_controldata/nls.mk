3992f1c50bb62dc228c70041f85fc752e7717666
Peter Eisentraut <peter_e@gmx.net>
2003-10-15 10:00:20 +0000

    Translation updates

--- src/bin/pg_controldata/nls.mk
+++ src/bin/pg_controldata/nls.mk
@@ -1,5 +1,5 @@
-# $Header: /cvsroot/pgsql/src/bin/pg_controldata/nls.mk,v 1.7 2003/09/29 10:57:05 petere Exp $
+# $Header: /cvsroot/pgsql/src/bin/pg_controldata/nls.mk,v 1.8 2003/10/15 10:00:20 petere Exp $
 CATALOG_NAME	:= pg_controldata
-AVAIL_LANGUAGES	:= cs de es fr hu it nb pt_BR ru sv zh_CN
+AVAIL_LANGUAGES	:= cs de es fr hu it nb pt_BR ru sl sv zh_CN
 GETTEXT_FILES	:= pg_controldata.c
 GETTEXT_TRIGGERS:= _
--- src/bin/pg_resetxlog/nls.mk
+++ src/bin/pg_resetxlog/nls.mk
@@ -1,5 +1,5 @@
-# $Header: /cvsroot/pgsql/src/bin/pg_resetxlog/nls.mk,v 1.9 2003/10/08 20:35:39 petere Exp $
+# $Header: /cvsroot/pgsql/src/bin/pg_resetxlog/nls.mk,v 1.10 2003/10/15 10:00:20 petere Exp $
 CATALOG_NAME	:= pg_resetxlog
-AVAIL_LANGUAGES	:= cs de es hu it nb pt_BR ru sv zh_CN
+AVAIL_LANGUAGES	:= cs de es hu it nb pt_BR ru sl sv zh_CN
 GETTEXT_FILES	:= pg_resetxlog.c
 GETTEXT_TRIGGERS:= _
