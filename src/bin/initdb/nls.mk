dc69cc109749e2b16cf8fb3818cd13c78e970801
Peter Eisentraut <peter_e@gmx.net>
2003-11-25 19:19:21 +0000

    New translation

--- src/bin/initdb/nls.mk
+++ src/bin/initdb/nls.mk
@@ -1,5 +1,5 @@
-# $Header: /cvsroot/pgsql/src/bin/initdb/nls.mk,v 1.1 2003/11/23 21:41:30 petere Exp $
+# $Header: /cvsroot/pgsql/src/bin/initdb/nls.mk,v 1.2 2003/11/25 19:19:20 petere Exp $
 CATALOG_NAME	:= initdb
-AVAIL_LANGUAGES	:= pt_BR
+AVAIL_LANGUAGES	:= de pt_BR
 GETTEXT_FILES	:= initdb.c
 GETTEXT_TRIGGERS:= _ simple_prompt
