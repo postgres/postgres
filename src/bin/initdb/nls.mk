15a5440dc5c497ca9389046b2f018d02c9aa7379
Dennis Bjorklund <db@zigo.dhs.org>
2004-01-10 09:09:40 +0000

    Translation updates

--- src/bin/initdb/nls.mk
+++ src/bin/initdb/nls.mk
@@ -1,5 +1,5 @@
-# $PostgreSQL: pgsql/src/bin/initdb/nls.mk,v 1.4 2003/12/11 22:03:05 petere Exp $
+# $PostgreSQL: pgsql/src/bin/initdb/nls.mk,v 1.5 2004/01/10 09:09:40 dennis Exp $
 CATALOG_NAME	:= initdb
-AVAIL_LANGUAGES	:= de it pt_BR
+AVAIL_LANGUAGES	:= de it pt_BR sv
 GETTEXT_FILES	:= initdb.c
 GETTEXT_TRIGGERS:= _ simple_prompt
