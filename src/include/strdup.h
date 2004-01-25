11c7bdb4e410102af298943526a48fffa9a9eb19
Neil Conway <neilc@samurai.com>
2004-01-25 22:19:42 +0000

    Fix an obsolete file path mentioned in a comment.

--- src/include/strdup.h
+++ src/include/strdup.h
@@ -2,7 +2,7 @@
    with Postgres.  That strdup() is only needed on those systems that
    don't already have strdup() in their system libraries.
 
-   The Postgres strdup() is in src/utils/strdup.c.
+   The Postgres strdup() is in src/port/strdup.c.
 */
 
 extern char *strdup(char const *);
