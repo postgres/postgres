2510c867d6c7f5db1dc721542d8f7ebe9e110d9a
Peter Eisentraut <peter_e@gmx.net>
2004-04-15 08:13:00 +0000

    Translation updates

--- src/bin/pg_resetxlog/nls.mk
+++ src/bin/pg_resetxlog/nls.mk
@@ -1,5 +1,5 @@
-# $Header: /cvsroot/pgsql/src/bin/pg_resetxlog/nls.mk,v 1.10 2003/10/15 10:00:20 petere Exp $
+# $Header: /cvsroot/pgsql/src/bin/pg_resetxlog/nls.mk,v 1.10.2.1 2004/04/15 08:13:00 petere Exp $
 CATALOG_NAME	:= pg_resetxlog
-AVAIL_LANGUAGES	:= cs de es hu it nb pt_BR ru sl sv zh_CN
+AVAIL_LANGUAGES	:= cs de es fr hu it nb pt_BR ru sl sv zh_CN
 GETTEXT_FILES	:= pg_resetxlog.c
 GETTEXT_TRIGGERS:= _
--- src/bin/scripts/nls.mk
+++ src/bin/scripts/nls.mk
@@ -1,6 +1,6 @@
-# $Header: /cvsroot/pgsql/src/bin/scripts/nls.mk,v 1.11 2003/10/08 20:35:39 petere Exp $
+# $Header: /cvsroot/pgsql/src/bin/scripts/nls.mk,v 1.11.2.1 2004/04/15 08:13:00 petere Exp $
 CATALOG_NAME    := pgscripts
-AVAIL_LANGUAGES := cs de es it pt_BR ru sl sv zh_CN
+AVAIL_LANGUAGES := cs de es fr it pt_BR ru sl sv zh_CN
 GETTEXT_FILES   := createdb.c createlang.c createuser.c \
                    dropdb.c droplang.c dropuser.c \
                    clusterdb.c vacuumdb.c \
