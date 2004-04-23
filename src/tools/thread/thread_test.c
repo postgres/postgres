f6646efa4c4a5a3e5d935842f71e9e26422f8f4e
Bruce Momjian <bruce@momjian.us>
2004-04-23 23:58:12 +0000

    Hook thread_test program run at the end of configure run.
    Add test for cross-compiles that they have to run the thread_test
    program on the target machine.

--- src/tools/thread/thread_test.c
+++ src/tools/thread/thread_test.c
@@ -6,7 +6,7 @@
  * Portions Copyright (c) 1996-2003, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- *	$PostgreSQL: pgsql/src/tools/thread/thread_test.c,v 1.22 2004/04/23 22:21:49 momjian Exp $
+ *	$PostgreSQL: pgsql/src/tools/thread/thread_test.c,v 1.23 2004/04/23 23:58:12 momjian Exp $
  *
  *	This program tests to see if your standard libc functions use
  *	pthread_setspecific()/pthread_getspecific() to be thread-safe.
@@ -109,7 +109,7 @@ main(int argc, char *argv[])
 		sched_yield();			/* if this is a portability problem,
 								 * remove it */
 
-	fprintf(stderr, "Your errno is thread-safe.\n");
+	printf("Your errno is thread-safe.\n");
 
 #ifndef HAVE_STRERROR_R
 	if (strerror_p1 != strerror_p2)
