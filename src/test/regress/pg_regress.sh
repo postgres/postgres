e4a03f9e75873394d3fcddd58c9a568829a76a03
Bruce Momjian <bruce@momjian.us>
2004-05-02 10:24:51 +0000

    Set PWDFLAGS based on test platform, not target platform.
    Andrew Dunstan

--- src/test/regress/pg_regress.sh
+++ src/test/regress/pg_regress.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-# $PostgreSQL: pgsql/src/test/regress/pg_regress.sh,v 1.39 2004/05/02 04:27:37 momjian Exp $
+# $PostgreSQL: pgsql/src/test/regress/pg_regress.sh,v 1.40 2004/05/02 10:24:51 momjian Exp $
 
 me=`basename $0`
 : ${TMPDIR=/tmp}
@@ -211,8 +211,8 @@ esac
 # Set up pwd to give a win32 happy pathname
 # ----------
 
-case $host_platform in
-    *-*-mingw32*)
+case `uname` in
+     MINGW32*)
         PWDFLAGS=-W;;
     *)
         PWDFLAGS=;;
