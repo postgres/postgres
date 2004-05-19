14531e0c44c1a06c209ddccdffbb56c1562bf129
Bruce Momjian <bruce@momjian.us>
2004-05-19 21:37:43 +0000

    Rename irix5 port to irix.

--- src/backend/port/dynloader/irix.h
+++ src/backend/port/dynloader/irix.h
@@ -1,13 +1,13 @@
 /*-------------------------------------------------------------------------
  *
- * irix5.h
- *	  port-specific prototypes for Irix 5
+ * irix.h
+ *	  port-specific prototypes for Irix
  *
  *
  * Portions Copyright (c) 1996-2003, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/backend/port/dynloader/irix5.h,v 1.15 2003/11/29 22:39:55 pgsql Exp $
+ * $PostgreSQL: pgsql/src/backend/port/dynloader/irix.h,v 1.1 2004/05/19 21:37:43 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
