254df82b4061ac63d7aa04b37f2a1d2f21d6d4e2
Bruce Momjian <bruce@momjian.us>
2004-05-20 01:47:01 +0000

    Add ctype.h include for new macro in port.h.

--- src/include/port.h
+++ src/include/port.h
@@ -6,7 +6,7 @@
  * Portions Copyright (c) 1996-2003, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/port.h,v 1.33 2004/05/19 04:21:49 momjian Exp $
+ * $PostgreSQL: pgsql/src/include/port.h,v 1.34 2004/05/20 01:47:01 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -16,6 +16,7 @@
 #include <pwd.h>
 #include <netdb.h>
 #endif
+#include <ctype.h>
 
 /* non-blocking */
 bool set_noblock(int sock);
