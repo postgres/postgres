6d1347b36fd1416c47fea9d6777d11bab6ae764e
Bruce Momjian <bruce@momjian.us>
2004-06-10 02:59:22 +0000

    Add pg_ctl native language files for Russian.
    Serguei A. Mokhov

---
+++ src/bin/pg_ctl/nls.mk
@@ -0,0 +1,5 @@
+# $PostgreSQL: pgsql/src/bin/pg_ctl/nls.mk,v 1.1 2004/06/10 02:59:18 momjian Exp $
+CATALOG_NAME	:= pg_ctl
+AVAIL_LANGUAGES	:= ru
+GETTEXT_FILES	:= pg_ctl.c
+GETTEXT_TRIGGERS:= _ simple_prompt
