bc4c2ac44ffb3979ad3b2ce9f0187f69bfe5c423
Dennis Bjorklund <db@zigo.dhs.org>
2004-06-25 09:49:36 +0000

    Swedish translation of pg_ctl.

--- src/bin/pg_ctl/nls.mk
+++ src/bin/pg_ctl/nls.mk
@@ -1,5 +1,5 @@
-# $PostgreSQL: pgsql/src/bin/pg_ctl/nls.mk,v 1.1 2004/06/10 02:59:18 momjian Exp $
+# $PostgreSQL: pgsql/src/bin/pg_ctl/nls.mk,v 1.2 2004/06/25 09:49:36 dennis Exp $
 CATALOG_NAME	:= pg_ctl
-AVAIL_LANGUAGES	:= ru
+AVAIL_LANGUAGES	:= ru sv
 GETTEXT_FILES	:= pg_ctl.c
 GETTEXT_TRIGGERS:= _ simple_prompt
