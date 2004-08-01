ee2bfaa3aa519b42c1e10a6b3e1d8a8ff9b9cf6a
Peter Eisentraut <peter_e@gmx.net>
2004-08-01 17:54:22 +0000

    Translation updates (zh_TW converted to UTF-8)

--- src/bin/scripts/nls.mk
+++ src/bin/scripts/nls.mk
@@ -1,6 +1,6 @@
-# $PostgreSQL: pgsql/src/bin/scripts/nls.mk,v 1.13 2004/04/15 08:15:09 petere Exp $
+# $PostgreSQL: pgsql/src/bin/scripts/nls.mk,v 1.14 2004/08/01 17:54:21 petere Exp $
 CATALOG_NAME    := pgscripts
-AVAIL_LANGUAGES := cs de es fr it pt_BR ru sl sv zh_CN
+AVAIL_LANGUAGES := cs de es fr it pt_BR ru sl sv zh_CN zh_TW
 GETTEXT_FILES   := createdb.c createlang.c createuser.c \
                    dropdb.c droplang.c dropuser.c \
                    clusterdb.c vacuumdb.c \
