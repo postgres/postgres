640d0dc0f02c1853fcf991b51d23e91c19af40b1
Bruce Momjian <bruce@momjian.us>
2004-08-08 07:00:22 +0000

    Fix compiler warning in the proper way.

--- src/bin/pg_dump/pg_dumpall.c
+++ src/bin/pg_dump/pg_dumpall.c
@@ -6,7 +6,7 @@
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  *
- * $PostgreSQL: pgsql/src/bin/pg_dump/pg_dumpall.c,v 1.48 2004/08/08 06:58:00 momjian Exp $
+ * $PostgreSQL: pgsql/src/bin/pg_dump/pg_dumpall.c,v 1.49 2004/08/08 07:00:22 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -870,9 +870,10 @@ runPgDump(const char *dbname)
 	appendPQExpBufferChar(cmd, '\'');
 #else
 	appendPQExpBufferChar(cmd, '"');
-	appendPQExpBuffer(cmd, SYSTEMQUOTE);
 #endif
 	
+	appendPQExpBuffer(cmd, "%s", SYSTEMQUOTE);
+
 	if (verbose)
 		fprintf(stderr, _("%s: running \"%s\"\n"), progname, cmd->data);
 
