3ae65316736e95cab8bf1e1c67aa4bbba28e2e0c
Peter Eisentraut <peter_e@gmx.net>
2004-08-11 09:00:27 +0000

    Add translation from 7.4 branch to head.

--- src/backend/nls.mk
+++ src/backend/nls.mk
@@ -1,6 +1,6 @@
-# $PostgreSQL: pgsql/src/backend/nls.mk,v 1.14 2004/08/10 19:10:21 petere Exp $
+# $PostgreSQL: pgsql/src/backend/nls.mk,v 1.15 2004/08/11 09:00:27 petere Exp $
 CATALOG_NAME	:= postgres
-AVAIL_LANGUAGES	:= af cs de es hr hu it nb pt_BR ru sk sv tr zh_CN zh_TW
+AVAIL_LANGUAGES	:= af cs de es fr hr hu it nb pt_BR ru sk sv tr zh_CN zh_TW
 GETTEXT_FILES	:= + gettext-files
 # you can add "elog:2" and "errmsg_internal" to this list if you want to
 # include internal messages in the translation list.
