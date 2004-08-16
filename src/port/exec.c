23717760d1eb9441f672a2d01b52bddfceb8c2e2
Bruce Momjian <bruce@momjian.us>
2004-08-16 02:46:36 +0000

    Fix for adding \n for zero-length win32 read_pipe return

--- src/port/exec.c
+++ src/port/exec.c
@@ -7,7 +7,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/port/exec.c,v 1.22 2004/08/16 01:26:31 momjian Exp $
+ *	  $PostgreSQL: pgsql/src/port/exec.c,v 1.23 2004/08/16 02:46:36 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -402,7 +402,7 @@ static char *pipe_read_line(char *cmd, char *line, int maxsize)
 			 *	We emulate fgets() behaviour. So if there is no newline
 			 *	at the end, we add one...
 			 */
-			if (line[len-1] != '\n')
+			if (len == 0 || line[len-1] != '\n')
 				strcat(line,"\n");
 		}
 
