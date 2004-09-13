77697dcaa3f43f0319f97f9b81c35d24b86a5585
Peter Eisentraut <peter_e@gmx.net>
2004-09-13 12:20:24 +0000

    New translations

--- src/bin/initdb/nls.mk
+++ src/bin/initdb/nls.mk
@@ -1,5 +1,5 @@
-# $PostgreSQL: pgsql/src/bin/initdb/nls.mk,v 1.10 2004/08/10 19:06:18 petere Exp $
+# $PostgreSQL: pgsql/src/bin/initdb/nls.mk,v 1.11 2004/09/13 12:20:21 petere Exp $
 CATALOG_NAME	:= initdb
-AVAIL_LANGUAGES	:= de fr it pt_BR ru sk sv zh_CN zh_TW
+AVAIL_LANGUAGES	:= de es fr it pt_BR ru sk sv zh_CN zh_TW
 GETTEXT_FILES	:= initdb.c
 GETTEXT_TRIGGERS:= _ simple_prompt
--- src/bin/pg_ctl/nls.mk
+++ src/bin/pg_ctl/nls.mk
@@ -1,5 +1,5 @@
-# $PostgreSQL: pgsql/src/bin/pg_ctl/nls.mk,v 1.5 2004/08/10 19:06:20 petere Exp $
+# $PostgreSQL: pgsql/src/bin/pg_ctl/nls.mk,v 1.6 2004/09/13 12:20:23 petere Exp $
 CATALOG_NAME	:= pg_ctl
-AVAIL_LANGUAGES	:= ru sk sv zh_CN zh_TW
+AVAIL_LANGUAGES	:= es ru sk sv zh_CN zh_TW
 GETTEXT_FILES	:= pg_ctl.c
 GETTEXT_TRIGGERS:= _ simple_prompt
