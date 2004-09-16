9f6df6c4a6b76be47be645a4041f93eac87c3c95
Tom Lane <tgl@sss.pgh.pa.us>
2004-09-16 21:59:17 +0000

    Fix oversight: there's no reason for PG_TRY to use sigsetjmp(buf,1) since we don't change the signal mask during normal backend operations. Use sigsetjmp(buf,0) to avoid many unnecessary kernel calls.

--- src/include/utils/elog.h
+++ src/include/utils/elog.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2004, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/utils/elog.h,v 1.76 2004/09/05 03:42:13 tgl Exp $
+ * $PostgreSQL: pgsql/src/include/utils/elog.h,v 1.77 2004/09/16 21:59:17 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -204,7 +204,7 @@ extern DLLIMPORT ErrorContextCallback *error_context_stack;
 		sigjmp_buf *save_exception_stack = PG_exception_stack; \
 		ErrorContextCallback *save_context_stack = error_context_stack; \
 		sigjmp_buf local_sigjmp_buf; \
-		if (sigsetjmp(local_sigjmp_buf, 1) == 0) \
+		if (sigsetjmp(local_sigjmp_buf, 0) == 0) \
 		{ \
 			PG_exception_stack = &local_sigjmp_buf
 
