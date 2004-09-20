fbbdfa413edb97557292b2e9f46fed0ea6ede717
Peter Eisentraut <peter_e@gmx.net>
2004-09-20 08:15:29 +0000

    Translation update

--- src/bin/pg_ctl/nls.mk
+++ src/bin/pg_ctl/nls.mk
@@ -1,5 +1,5 @@
-# $PostgreSQL: pgsql/src/bin/pg_ctl/nls.mk,v 1.7 2004/09/13 12:26:45 petere Exp $
+# $PostgreSQL: pgsql/src/bin/pg_ctl/nls.mk,v 1.8 2004/09/20 08:13:07 petere Exp $
 CATALOG_NAME	:= pg_ctl
-AVAIL_LANGUAGES	:= cs es ru sk sv zh_CN zh_TW
+AVAIL_LANGUAGES	:= cs es fr ru sk sv zh_CN zh_TW
 GETTEXT_FILES	:= pg_ctl.c
 GETTEXT_TRIGGERS:= _ simple_prompt
