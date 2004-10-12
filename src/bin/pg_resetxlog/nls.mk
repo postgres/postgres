893094415b3ce6dec51043c6922815fd51cf55f4
Peter Eisentraut <peter_e@gmx.net>
2004-10-12 18:01:20 +0000

    Translation updates

--- src/bin/pg_resetxlog/nls.mk
+++ src/bin/pg_resetxlog/nls.mk
@@ -1,5 +1,5 @@
-# $PostgreSQL: pgsql/src/bin/pg_resetxlog/nls.mk,v 1.14 2004/08/13 16:47:25 petere Exp $
+# $PostgreSQL: pgsql/src/bin/pg_resetxlog/nls.mk,v 1.15 2004/10/12 18:01:13 petere Exp $
 CATALOG_NAME	:= pg_resetxlog
-AVAIL_LANGUAGES	:= cs de es fr hu it nb pt_BR ru sk sl sv zh_CN zh_TW
+AVAIL_LANGUAGES	:= cs de es fr hu it nb pt_BR ru sk sl sv tr zh_CN zh_TW
 GETTEXT_FILES	:= pg_resetxlog.c
 GETTEXT_TRIGGERS:= _
