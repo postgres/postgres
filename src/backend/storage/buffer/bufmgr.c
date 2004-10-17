4347cc2392a75dec1d5e83f8b60911597fc602d9
Tom Lane <tgl@sss.pgh.pa.us>
2004-10-17 22:01:51 +0000

    Allow background writing to be shut down by setting limit values to zero. This does not disable the bgwriter process: it still has to wake up often enough to collect fsync requests from backends in a timely fashion.  But it responds to the recent gripe about not being able to prevent the disk from being spun up constantly.

--- src/backend/storage/buffer/bufmgr.c
+++ src/backend/storage/buffer/bufmgr.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/storage/buffer/bufmgr.c,v 1.180 2004/10/16 18:57:23 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/storage/buffer/bufmgr.c,v 1.181 2004/10/17 22:01:50 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -671,8 +671,10 @@ UnpinBuffer(BufferDesc *buf, bool fixOwner)
  *
  * This is called at checkpoint time to write out all dirty shared buffers,
  * and by the background writer process to write out some of the dirty blocks.
- * percent/maxpages should be zero in the former case, and nonzero limit
- * values in the latter.
+ * percent/maxpages should be -1 in the former case, and limit values (>= 0)
+ * in the latter.
+ *
+ * Returns the number of buffers written.
  */
 int
 BufferSync(int percent, int maxpages)
@@ -681,6 +683,10 @@ BufferSync(int percent, int maxpages)
 	BufferTag  *buftags;
 	int			num_buffer_dirty;
 	int			i;
+
+	/* If either limit is zero then we are disabled from doing anything... */
+	if (percent == 0 || maxpages == 0)
+		return 0;
 
 	/*
 	 * Get a list of all currently dirty buffers and how many there are.
--- src/backend/utils/misc/guc.c
+++ src/backend/utils/misc/guc.c
@@ -10,7 +10,7 @@
  * Written by Peter Eisentraut <peter_e@gmx.net>.
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/utils/misc/guc.c,v 1.244 2004/10/16 19:08:38 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/utils/misc/guc.c,v 1.245 2004/10/17 22:01:51 tgl Exp $
  *
  *--------------------------------------------------------------------
  */
@@ -1248,7 +1248,7 @@ static struct config_int ConfigureNamesInt[] =
 			NULL
 		},
 		&BgWriterDelay,
-		200, 10, 5000, NULL, NULL
+		200, 10, 10000, NULL, NULL
 	},
 
 	{
@@ -1257,7 +1257,7 @@ static struct config_int ConfigureNamesInt[] =
 			NULL
 		},
 		&BgWriterPercent,
-		1, 1, 100, NULL, NULL
+		1, 0, 100, NULL, NULL
 	},
 
 	{
@@ -1266,7 +1266,7 @@ static struct config_int ConfigureNamesInt[] =
 			NULL
 		},
 		&BgWriterMaxPages,
-		100, 1, 1000, NULL, NULL
+		100, 0, 1000, NULL, NULL
 	},
 
 	{
