f4eb44027910da50541a44a7231354f8c97e199c
Dennis Bjorklund <db@zigo.dhs.org>
2004-10-22 14:13:04 +0000

    New swedish translation of pg_config

--- src/bin/pg_config/nls.mk
+++ src/bin/pg_config/nls.mk
@@ -1,5 +1,5 @@
-# $PostgreSQL: pgsql/src/bin/pg_config/nls.mk,v 1.5 2004/10/18 22:19:15 petere Exp $
+# $PostgreSQL: pgsql/src/bin/pg_config/nls.mk,v 1.6 2004/10/22 14:13:04 dennis Exp $
 CATALOG_NAME	:= pg_config
-AVAIL_LANGUAGES	:= de pt_BR ru tr zh_TW
+AVAIL_LANGUAGES	:= de pt_BR ru sv tr zh_TW
 GETTEXT_FILES	:= pg_config.c
 GETTEXT_TRIGGERS:= _
