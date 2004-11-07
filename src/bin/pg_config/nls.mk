58b33f2e854af1ba3cbabeaacbeb92cf00178b42
Peter Eisentraut <peter_e@gmx.net>
2004-11-07 23:28:20 +0000

    Translation updates

--- src/bin/pg_config/nls.mk
+++ src/bin/pg_config/nls.mk
@@ -1,5 +1,5 @@
-# $PostgreSQL: pgsql/src/bin/pg_config/nls.mk,v 1.8 2004/11/02 09:28:28 petere Exp $
+# $PostgreSQL: pgsql/src/bin/pg_config/nls.mk,v 1.9 2004/11/07 23:27:50 petere Exp $
 CATALOG_NAME	:= pg_config
-AVAIL_LANGUAGES	:= cs de fr pt_BR ru sv tr zh_TW
+AVAIL_LANGUAGES	:= cs de es fr pt_BR ru sv tr zh_TW
 GETTEXT_FILES	:= pg_config.c
 GETTEXT_TRIGGERS:= _
