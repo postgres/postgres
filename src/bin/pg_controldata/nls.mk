11ca6d580ce00ac18886e17e88762e387b74ca31
Peter Eisentraut <peter_e@gmx.net>
2005-01-09 17:07:27 +0000

    Translation updates

--- src/bin/pg_controldata/nls.mk
+++ src/bin/pg_controldata/nls.mk
@@ -1,5 +1,5 @@
-# $PostgreSQL: pgsql/src/bin/pg_controldata/nls.mk,v 1.14 2004/12/15 17:50:15 petere Exp $
+# $PostgreSQL: pgsql/src/bin/pg_controldata/nls.mk,v 1.15 2005/01/09 17:07:24 petere Exp $
 CATALOG_NAME	:= pg_controldata
-AVAIL_LANGUAGES	:= cs de es fa fr hu it ko nb pt_BR ru sk sl sv tr zh_CN zh_TW
+AVAIL_LANGUAGES	:= cs de es fa fr hu it ko nb pt_BR ro ru sk sl sv tr zh_CN zh_TW
 GETTEXT_FILES	:= pg_controldata.c
 GETTEXT_TRIGGERS:= _
