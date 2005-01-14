75112d4b5d67f6c15726db7609517216d72e559b
Peter Eisentraut <peter_e@gmx.net>
2005-01-14 08:57:06 +0000

    Translation updates

--- src/bin/pg_controldata/nls.mk
+++ src/bin/pg_controldata/nls.mk
@@ -1,5 +1,5 @@
-# $PostgreSQL: pgsql/src/bin/pg_controldata/nls.mk,v 1.15 2005/01/09 17:07:24 petere Exp $
+# $PostgreSQL: pgsql/src/bin/pg_controldata/nls.mk,v 1.16 2005/01/14 08:57:05 petere Exp $
 CATALOG_NAME	:= pg_controldata
-AVAIL_LANGUAGES	:= cs de es fa fr hu it ko nb pt_BR ro ru sk sl sv tr zh_CN zh_TW
+AVAIL_LANGUAGES	:= cs de es fa fr hu it ko nb pl pt_BR ro ru sk sl sv tr zh_CN zh_TW
 GETTEXT_FILES	:= pg_controldata.c
 GETTEXT_TRIGGERS:= _
--- src/interfaces/libpq/nls.mk
+++ src/interfaces/libpq/nls.mk
@@ -1,5 +1,5 @@
-# $PostgreSQL: pgsql/src/interfaces/libpq/nls.mk,v 1.19 2004/12/13 16:30:51 petere Exp $
+# $PostgreSQL: pgsql/src/interfaces/libpq/nls.mk,v 1.20 2005/01/14 08:57:06 petere Exp $
 CATALOG_NAME	:= libpq
-AVAIL_LANGUAGES	:= af cs de es fr hr it ko nb pt_BR ru sk sl sv tr zh_CN zh_TW
+AVAIL_LANGUAGES	:= af cs de es fr hr it ko nb pl pt_BR ru sk sl sv tr zh_CN zh_TW
 GETTEXT_FILES	:= fe-auth.c fe-connect.c fe-exec.c fe-lobj.c fe-misc.c fe-protocol2.c fe-protocol3.c fe-secure.c
 GETTEXT_TRIGGERS:= libpq_gettext pqInternalNotice:2
