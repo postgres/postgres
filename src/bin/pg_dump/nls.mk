5ca9687fe4e5f44cc4d23ed4263236aec6e6764d
Peter Eisentraut <peter_e@gmx.net>
2005-01-25 17:32:00 +0000

    Add missing "ko".

--- src/bin/pg_dump/nls.mk
+++ src/bin/pg_dump/nls.mk
@@ -1,6 +1,6 @@
-# $PostgreSQL: pgsql/src/bin/pg_dump/nls.mk,v 1.18 2004/11/27 22:44:13 petere Exp $
+# $PostgreSQL: pgsql/src/bin/pg_dump/nls.mk,v 1.19 2005/01/25 17:32:00 petere Exp $
 CATALOG_NAME	:= pg_dump
-AVAIL_LANGUAGES	:= cs de es fr it nb pt_BR ro ru sk sl sv tr zh_CN zh_TW
+AVAIL_LANGUAGES	:= cs de es fr it ko nb pt_BR ro ru sk sl sv tr zh_CN zh_TW
 GETTEXT_FILES	:= pg_dump.c common.c pg_backup_archiver.c pg_backup_custom.c \
                    pg_backup_db.c pg_backup_files.c pg_backup_null.c \
                    pg_backup_tar.c pg_restore.c pg_dumpall.c \
