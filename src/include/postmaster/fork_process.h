bb4c88c29ab312e2be118ac857daa7c93399d6e1
Tom Lane <tgl@sss.pgh.pa.us>
2005-03-13 23:32:26 +0000

    Add missing identification comment, remove entirely inappropriate include of postgres.h.

--- src/include/postmaster/fork_process.h
+++ src/include/postmaster/fork_process.h
@@ -1,8 +1,17 @@
+/*-------------------------------------------------------------------------
+ *
+ * fork_process.h
+ *	  Exports from postmaster/fork_process.c.
+ *
+ * Copyright (c) 1996-2005, PostgreSQL Global Development Group
+ *
+ * $PostgreSQL: pgsql/src/include/postmaster/fork_process.h,v 1.2 2005/03/13 23:32:26 tgl Exp $
+ *
+ *-------------------------------------------------------------------------
+ */
 #ifndef FORK_PROCESS_H
 #define FORK_PROCESS_H
 
-#include "postgres.h"
-
 extern pid_t fork_process(void);
 
-#endif /* ! FORK_PROCESS_H */
+#endif /* FORK_PROCESS_H */
