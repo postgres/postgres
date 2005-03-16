f4021259269815f9a897cf0836c15c5d542351e4
Bruce Momjian <bruce@momjian.us>
2005-03-16 22:59:42 +0000

    Force initdb cause of encoding additions.

--- src/include/catalog/catversion.h
+++ src/include/catalog/catversion.h
@@ -37,7 +37,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/catalog/catversion.h,v 1.257 2005/02/28 03:45:21 neilc Exp $
+ * $PostgreSQL: pgsql/src/include/catalog/catversion.h,v 1.258 2005/03/16 22:59:42 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -53,6 +53,6 @@
  */
 
 /*							yyyymmddN */
-#define CATALOG_VERSION_NO	200502281
+#define CATALOG_VERSION_NO	200503161
 
 #endif
