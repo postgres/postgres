1a6ad669fbe5f5bd5bd75988ebd6efa19ed05c26
Bruce Momjian <bruce@momjian.us>
2005-04-17 03:04:29 +0000

    Fix comment typo.

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/backend/access/transam/xlog.c,v 1.186 2005/04/15 22:19:48 tgl Exp $
+ * $PostgreSQL: pgsql/src/backend/access/transam/xlog.c,v 1.187 2005/04/17 03:04:29 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -5194,7 +5194,7 @@ assign_xlog_sync_method(const char *method, bool doit, GucSource source)
 #ifndef FSYNC_IS_WRITE_THROUGH
 	if (pg_strcasecmp(method, "fsync") == 0)
 #else
-	/* Win32 fsync() == _commit(0, which writes through a write cache */
+	/* Win32 fsync() == _commit(), which writes through a write cache */
 	if (pg_strcasecmp(method, "fsync_writethrough") == 0)
 #endif
 	{
