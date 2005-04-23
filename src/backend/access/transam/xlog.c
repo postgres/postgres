19d127548cf7d27a0e09ab8d698ec9e3e0bb21a1
Tom Lane <tgl@sss.pgh.pa.us>
2005-04-23 18:49:54 +0000

    Add comment about checkpoint panic behavior during shutdown, per suggestion from Qingqing Zhou.

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/backend/access/transam/xlog.c,v 1.187 2005/04/17 03:04:29 momjian Exp $
+ * $PostgreSQL: pgsql/src/backend/access/transam/xlog.c,v 1.188 2005/04/23 18:49:54 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -4925,7 +4925,10 @@ CreateCheckPoint(bool shutdown, bool force)
 	 *
 	 * This I/O could fail for various reasons.  If so, we will fail to
 	 * complete the checkpoint, but there is no reason to force a system
-	 * panic.  Accordingly, exit critical section while doing it.
+	 * panic.  Accordingly, exit critical section while doing it.  (If
+	 * we are doing a shutdown checkpoint, we probably *should* panic ---
+	 * but that will happen anyway because we'll still be inside the
+	 * critical section established by ShutdownXLOG.)
 	 */
 	END_CRIT_SECTION();
 
