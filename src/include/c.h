5394771580977364b1994e0a2fb302c9ceb00212
Tom Lane <tgl@sss.pgh.pa.us>
2005-07-18 15:54:30 +0000

    MemSet() must not cast its pointer argument to int32* until after it has checked that the pointer is actually word-aligned.  Casting a non-aligned pointer to int32* is technically illegal per the C spec, and some recent versions of gcc actually generate bad code for the memset() when given such a pointer.  Per report from Andrew Morrow.

--- src/include/c.h
+++ src/include/c.h
@@ -12,7 +12,7 @@
  * Portions Copyright (c) 1996-2002, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $Id: c.h,v 1.130 2002/10/24 03:11:05 momjian Exp $
+ * $Id: c.h,v 1.130.2.1 2005/07/18 15:54:30 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -582,21 +582,22 @@ typedef NameData *Name;
 #define MemSet(start, val, len) \
 	do \
 	{ \
-		int32 * _start = (int32 *) (start); \
+		void   *_vstart = (void *) (start); \
 		int		_val = (val); \
 		Size	_len = (len); \
 \
-		if ((((long) _start) & INT_ALIGN_MASK) == 0 && \
+		if ((((long) _vstart) & INT_ALIGN_MASK) == 0 && \
 			(_len & INT_ALIGN_MASK) == 0 && \
 			_val == 0 && \
 			_len <= MEMSET_LOOP_LIMIT) \
 		{ \
-			int32 * _stop = (int32 *) ((char *) _start + _len); \
+			int32 *_start = (int32 *) _vstart; \
+			int32 *_stop = (int32 *) ((char *) _start + _len); \
 			while (_start < _stop) \
 				*_start++ = 0; \
 		} \
 		else \
-			memset((char *) _start, _val, _len); \
+			memset(_vstart, _val, _len); \
 	} while (0)
 
 #define MEMSET_LOOP_LIMIT  1024
