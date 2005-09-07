694da2897be2db929d7dc76df5b085114b2c8ab8
Tom Lane <tgl@sss.pgh.pa.us>
2005-09-07 21:39:41 +0000

    Update timezone data files to release 2005m of the zic database. Among other changes, this reflects the recently passed change in USA daylight savings rules.

--- src/timezone/data/yearistype.sh
+++ src/timezone/data/yearistype.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-: '@(#)yearistype.sh	7.7'
+: '@(#)yearistype.sh	7.8'
 
 case $#-$1 in
 	2-|2-0*|2-*[!0-9]*)
@@ -9,7 +9,7 @@ case $#-$1 in
 esac
 
 case $#-$2 in
-	2-even)	
+	2-even)
 		case $1 in
 			*[24680])			exit 0 ;;
 			*)				exit 1 ;;
@@ -19,7 +19,7 @@ case $#-$2 in
 			*[02468][048]|*[13579][26])	exit 1 ;;
 			*)				exit 0 ;;
 		esac ;;
-	2-odd)	
+	2-odd)
 		case $1 in
 			*[13579])			exit 0 ;;
 			*)				exit 1 ;;
@@ -29,7 +29,7 @@ case $#-$2 in
 			*[02468][048]|*[13579][26])	exit 0 ;;
 			*)				exit 1 ;;
 		esac ;;
-	2-*)	
+	2-*)
 		echo "$0: wild type - $2" >&2 ;;
 esac
 
