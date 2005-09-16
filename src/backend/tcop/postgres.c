098d204a6b8db56acc05e21b0a62ab1b6f9d90ec
Bruce Momjian <bruce@momjian.us>
2005-09-16 19:31:04 +0000

    Update query cancel message:
       errmsg("canceling query due to user request or statement timeout")));

--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/tcop/postgres.c,v 1.458 2005/09/02 21:50:54 momjian Exp $
+ *	  $PostgreSQL: pgsql/src/backend/tcop/postgres.c,v 1.459 2005/09/16 19:31:04 momjian Exp $
  *
  * NOTES
  *	  this is the "main" module of the postgres backend and
@@ -2205,7 +2205,7 @@ ProcessInterrupts(void)
 		DisableCatchupInterrupt();
 		ereport(ERROR,
 				(errcode(ERRCODE_QUERY_CANCELED),
-				 errmsg("canceling query due to user request")));
+				 errmsg("canceling query due to user request or statement timeout")));
 	}
 	/* If we get here, do nothing (probably, QueryCancelPending was reset) */
 }
