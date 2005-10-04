9c76af4a5ef3b27a4cdd344c5e7f73f5466d9423
Peter Eisentraut <peter_e@gmx.net>
2005-10-04 11:14:03 +0000

    Add _ to gettext triggers. Must have been forgotten here when it was introduced.

--- src/backend/nls.mk
+++ src/backend/nls.mk
@@ -1,10 +1,10 @@
-# $PostgreSQL: pgsql/src/backend/nls.mk,v 1.19 2005/01/09 17:10:28 petere Exp $
+# $PostgreSQL: pgsql/src/backend/nls.mk,v 1.20 2005/10/04 11:14:03 petere Exp $
 CATALOG_NAME	:= postgres
 AVAIL_LANGUAGES	:= af cs de es fr hr hu it ko nb pt_BR ro ru sk sl sv tr zh_CN zh_TW
 GETTEXT_FILES	:= + gettext-files
 # you can add "elog:2" and "errmsg_internal" to this list if you want to
 # include internal messages in the translation list.
-GETTEXT_TRIGGERS:= errmsg errdetail errhint errcontext write_stderr yyerror
+GETTEXT_TRIGGERS:= _ errmsg errdetail errhint errcontext write_stderr yyerror
 
 gettext-files: distprep
 	find $(srcdir)/ $(srcdir)/../port/ -name '*.c' -print >$@
