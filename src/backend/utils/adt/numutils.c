8c8c0108e8f39b291286eaf3cc5c482ecc305f48
Bruce Momjian <bruce@momjian.us>
2005-12-01 21:16:13 +0000

    Add comment to pg_atoi.

--- src/backend/utils/adt/numutils.c
+++ src/backend/utils/adt/numutils.c
@@ -10,7 +10,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/utils/adt/numutils.c,v 1.70 2005/11/30 23:10:08 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/utils/adt/numutils.c,v 1.71 2005/12/01 21:16:13 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -73,7 +73,7 @@ pg_atoi(char *s, int size, int c)
 				 errmsg("invalid input syntax for integer: \"%s\"",
 						s)));
 
-	errno = 0;
+	errno = 0;	/* avoid having to check the result for failure */
 	l = strtol(s, &badp, 10);
 
 	/* We made no progress parsing the string, so bail out */
