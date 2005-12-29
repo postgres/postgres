eb29d89ffdcead29ab243acb523404e580fe9666
Andrew Dunstan <andrew@dunslane.net>
2005-12-29 14:28:31 +0000

    Move declaration of check_function_bodies to where the perl headers haven't had a chance to mangle the definition of DLLIMPORT (thanks again, perl guys).

--- src/pl/plperl/plperl.c
+++ src/pl/plperl/plperl.c
@@ -33,7 +33,7 @@
  *	  ENHANCEMENTS, OR MODIFICATIONS.
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/pl/plperl/plperl.c,v 1.97 2005/12/28 18:34:16 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/pl/plperl/plperl.c,v 1.98 2005/12/29 14:28:31 adunstan Exp $
  *
  **********************************************************************/
 
@@ -56,6 +56,9 @@
 #include "miscadmin.h"
 #include "mb/pg_wchar.h"
 
+/* define this before the perl headers get a chance to mangle DLLIMPORT */
+extern DLLIMPORT bool check_function_bodies;
+
 /* perl stuff */
 #include "EXTERN.h"
 #include "perl.h"
@@ -68,8 +71,6 @@
 #define pTHX_
 #define pTHX void
 #endif
-
-extern DLLIMPORT bool check_function_bodies;
 
 
 /**********************************************************************
