c20a9f25afb488146bdae9d8ab86b69e9f48e8e2
Neil Conway <neilc@samurai.com>
2006-01-01 01:41:42 +0000

    Trim an obsolete comment: it is no longer relevant that "SELECT" was formerly "RETRIEVE", and so forth.

--- src/include/nodes/nodes.h
+++ src/include/nodes/nodes.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/nodes/nodes.h,v 1.180 2005/12/28 01:30:01 tgl Exp $
+ * $PostgreSQL: pgsql/src/include/nodes/nodes.h,v 1.181 2006/01/01 01:41:42 neilc Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -425,9 +425,9 @@ typedef double Cost;			/* execution cost (in page-access units) */
 typedef enum CmdType
 {
 	CMD_UNKNOWN,
-	CMD_SELECT,					/* select stmt (formerly retrieve) */
-	CMD_UPDATE,					/* update stmt (formerly replace) */
-	CMD_INSERT,					/* insert stmt (formerly append) */
+	CMD_SELECT,					/* select stmt */
+	CMD_UPDATE,					/* update stmt */
+	CMD_INSERT,					/* insert stmt */
 	CMD_DELETE,
 	CMD_UTILITY,				/* cmds like create, destroy, copy, vacuum,
 								 * etc. */
