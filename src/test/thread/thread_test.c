9e66c3e6fe4f36e061a4d265f8e514c80ec11ef8
Bruce Momjian <bruce@momjian.us>
2006-02-04 01:00:02 +0000

    Move thread_test directory from /tools to /test so source-only tarballs have the directory for the configure test.

--- src/test/thread/thread_test.c
+++ src/test/thread/thread_test.c
@@ -6,7 +6,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- *	$PostgreSQL: pgsql/src/tools/thread/thread_test.c,v 1.41 2005/10/15 02:49:52 momjian Exp $
+ *	$PostgreSQL: pgsql/src/test/thread/thread_test.c,v 1.1 2006/02/04 01:00:02 momjian Exp $
  *
  *	This program tests to see if your standard libc functions use
  *	pthread_setspecific()/pthread_getspecific() to be thread-safe.
