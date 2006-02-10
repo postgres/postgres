9878d9a1f1fb64dea76531955ec5c5c69d97aded
Tom Lane <tgl@sss.pgh.pa.us>
2006-02-10 21:52:43 +0000

    Check that SID is enabled while checking for Windows admin privileges. Magnus

--- src/backend/port/win32/security.c
+++ src/backend/port/win32/security.c
@@ -6,7 +6,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/port/win32/security.c,v 1.8 2004/12/31 22:00:37 pgsql Exp $
+ *	  $PostgreSQL: pgsql/src/backend/port/win32/security.c,v 1.8.4.1 2006/02/10 21:52:43 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -78,8 +78,8 @@ pgwin32_is_admin(void)
 
 	for (x = 0; x < Groups->GroupCount; x++)
 	{
-		if (EqualSid(AdministratorsSid, Groups->Groups[x].Sid) ||
-			EqualSid(PowerUsersSid, Groups->Groups[x].Sid))
+		if ((EqualSid(AdministratorsSid, Groups->Groups[x].Sid) && (Groups->Groups[x].Attributes & SE_GROUP_ENABLED)) ||
+			(EqualSid(PowerUsersSid, Groups->Groups[x].Sid) && (Groups->Groups[x].Attributes & SE_GROUP_ENABLED)))
 		{
 			success = TRUE;
 			break;
