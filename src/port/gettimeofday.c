295615a6ca64f38d278bdd7c6aeda03ab452ac51
Bruce Momjian <bruce@momjian.us>
2006-03-03 23:59:14 +0000

    This patch fixes this warning.
    gettimeofday.c:35: warning: integer constant is too large for "long"
    type
    
    Kris Jurka

--- src/port/gettimeofday.c
+++ src/port/gettimeofday.c
@@ -2,7 +2,7 @@
  * gettimeofday.c
  *	  Win32 gettimeofday() replacement
  *
- * $PostgreSQL: pgsql/src/port/gettimeofday.c,v 1.7 2005/12/16 21:55:27 alvherre Exp $
+ * $PostgreSQL: pgsql/src/port/gettimeofday.c,v 1.8 2006/03/03 23:59:14 momjian Exp $
  *
  * Copyright (c) 2003 SRA, Inc.
  * Copyright (c) 2003 SKC, Inc.
@@ -32,7 +32,7 @@
 
 
 /* FILETIME of Jan 1 1970 00:00:00. */
-static const unsigned __int64 epoch = 116444736000000000L;
+static const unsigned __int64 epoch = 116444736000000000LL;
 
 /*
  * timezone information is stored outside the kernel so tzp isn't used anymore.
