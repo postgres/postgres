b7a870ccc7c3d5b174806b0305bbd57c56724298
Bruce Momjian <bruce@momjian.us>
2006-03-03 23:54:52 +0000

    Avoid trying to open /dev/tty on Win32.  Some Win32 systems have /dev/tty, but it isn't a device file and doesn't work as expected.
    This fixes a known bug where psql does not prompt for a password on some
    Win32 systems.
    
    Backpatch to 8.0.X too.
    
    Robert Kinberg

--- src/port/sprompt.c
+++ src/port/sprompt.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/port/sprompt.c,v 1.10 2004/12/31 22:03:53 pgsql Exp $
+ *	  $PostgreSQL: pgsql/src/port/sprompt.c,v 1.10.4.1 2006/03/03 23:54:52 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -40,8 +40,8 @@ simple_prompt(const char *prompt, int maxlen, bool echo)
 {
 	int			length;
 	char	   *destination;
-	FILE	   *termin,
-			   *termout;
+	FILE	   *termin = NULL,
+			   *termout = NULL;
 
 #ifdef HAVE_TERMIOS_H
 	struct termios t_orig,
@@ -64,8 +64,14 @@ simple_prompt(const char *prompt, int maxlen, bool echo)
 	 * Do not try to collapse these into one "w+" mode file. Doesn't work
 	 * on some platforms (eg, HPUX 10.20).
 	 */
-	termin = fopen("/dev/tty", "r");
-	termout = fopen("/dev/tty", "w");
+#ifndef WIN32
+	/*
+	 *	Some win32 platforms actually have a /dev/tty file, but it isn't
+	 *	a device file, and it doesn't work as expected, so we avoid trying.
+	 */
+  	termin = fopen("/dev/tty", "r");
+  	termout = fopen("/dev/tty", "w");
+#endif
 	if (!termin || !termout)
 	{
 		if (termin)
