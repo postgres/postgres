c574106a665c9364a95d6d2ddfc2033b8914824b
Bruce Momjian <bruce@momjian.us>
2006-03-08 04:01:29 +0000

    Adjust plpython for escape_string_warning.

--- src/pl/plpython/sql/plpython_function.sql
+++ src/pl/plpython/sql/plpython_function.sql
@@ -318,11 +318,11 @@ LANGUAGE plpythonu;
 -- Unicode error handling
 --
 
-CREATE FUNCTION unicode_return_error() RETURNS text AS '
+CREATE FUNCTION unicode_return_error() RETURNS text AS E'
 return u"\\x80"
 ' LANGUAGE plpythonu;
 
-CREATE FUNCTION unicode_trigger_error() RETURNS trigger AS '
+CREATE FUNCTION unicode_trigger_error() RETURNS trigger AS E'
 TD["new"]["testvalue"] = u"\\x80"
 return "MODIFY"
 ' LANGUAGE plpythonu;
@@ -330,13 +330,13 @@ return "MODIFY"
 CREATE TRIGGER unicode_test_bi BEFORE INSERT ON unicode_test
   FOR EACH ROW EXECUTE PROCEDURE unicode_trigger_error();
 
-CREATE FUNCTION unicode_plan_error1() RETURNS text AS '
+CREATE FUNCTION unicode_plan_error1() RETURNS text AS E'
 plan = plpy.prepare("SELECT $1 AS testvalue", ["text"])
 rv = plpy.execute(plan, [u"\\x80"], 1)
 return rv[0]["testvalue"]
 ' LANGUAGE plpythonu;
 
-CREATE FUNCTION unicode_plan_error2() RETURNS text AS '
+CREATE FUNCTION unicode_plan_error2() RETURNS text AS E'
 plan = plpy.prepare("SELECT $1 AS testvalue1, $2 AS testvalue2", ["text", "text"])
 rv = plpy.execute(plan, u"\\x80", 1)
 return rv[0]["testvalue1"]
