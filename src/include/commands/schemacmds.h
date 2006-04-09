848692131a7081d789aa4e58e4cc5ec94c4f520a
Tom Lane <tgl@sss.pgh.pa.us>
2006-04-09 22:01:19 +0000

    Fix another const-decoration mismatch, per Magnus.

--- src/include/commands/schemacmds.h
+++ src/include/commands/schemacmds.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2006, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/commands/schemacmds.h,v 1.13 2006/03/05 15:58:55 momjian Exp $
+ * $PostgreSQL: pgsql/src/include/commands/schemacmds.h,v 1.14 2006/04/09 22:01:19 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -24,6 +24,6 @@ extern void RemoveSchemaById(Oid schemaOid);
 
 extern void RenameSchema(const char *oldname, const char *newname);
 extern void AlterSchemaOwner(const char *name, Oid newOwnerId);
-extern void AlterSchemaOwner_oid(const Oid schemaOid, Oid newOwnerId);
+extern void AlterSchemaOwner_oid(Oid schemaOid, Oid newOwnerId);
 
 #endif   /* SCHEMACMDS_H */
