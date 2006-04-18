5bd59b9c0afd81e5ec636b7b475ef55ad16b12fb
Bruce Momjian <bruce@momjian.us>
2006-04-18 00:52:23 +0000

    Document that errors are not output by log_statement (was they were in 8.0), and add as suggestion to use log_min_error_statement for this purpose.  I also fixed the code so the first EXECUTE has it's prepare, rather than the last which is what was in the current code.  Also remove "protocol" prefix for SQL EXECUTE output because it is not accurate.
    Backpatch to 8.1.X.

--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/tcop/postgres.c,v 1.483 2006/04/04 19:35:35 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/tcop/postgres.c,v 1.484 2006/04/18 00:52:23 momjian Exp $
  *
  * NOTES
  *	  this is the "main" module of the postgres backend and
@@ -586,19 +586,21 @@ log_after_parse(List *raw_parsetree_list, const char *query_string,
 
 		/*
 		 * For the first EXECUTE we find, record the client statement used by
-		 * the PREPARE.
+		 * the PREPARE.  PREPARE doesn't save the parse tree so we have no
+		 * way to conditionally output based on the type of query prepared.
 		 */
 		if (IsA(parsetree, ExecuteStmt))
 		{
 			ExecuteStmt *stmt = (ExecuteStmt *) parsetree;
 			PreparedStatement *entry;
 
-			if ((entry = FetchPreparedStatement(stmt->name, false)) != NULL &&
+			if (*prepare_string == NULL &&
+				(entry = FetchPreparedStatement(stmt->name, false)) != NULL &&
 				entry->query_string)
 			{
 				*prepare_string = palloc(strlen(entry->query_string) +
-									  strlen("  [protocol PREPARE:  %s]") - 1);
-				sprintf(*prepare_string, "  [protocol PREPARE:  %s]",
+									  strlen("  [PREPARE:  %s]") - 2 + 1);
+				sprintf(*prepare_string, "  [PREPARE:  %s]",
 						entry->query_string);
 			}
 		}
