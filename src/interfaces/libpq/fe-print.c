8f52496a05e83666bcf8b32c6e525cc6730fd78b
Tom Lane <tgl@sss.pgh.pa.us>
2006-04-19 16:15:29 +0000

    Fix ancient memory leak in PQprintTuples(); our code no longer uses this routine, but perhaps some applications do.  Found by Martijn van Oosterhout using Coverity.

--- src/interfaces/libpq/fe-print.c
+++ src/interfaces/libpq/fe-print.c
@@ -10,7 +10,7 @@
  * didn't really belong there.
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/interfaces/libpq/fe-print.c,v 1.67 2006/03/05 15:59:09 momjian Exp $
+ *	  $PostgreSQL: pgsql/src/interfaces/libpq/fe-print.c,v 1.68 2006/04/19 16:15:29 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -751,8 +751,10 @@ PQprintTuples(const PGresult *res,
 				fprintf(fout, "|\n%s\n", tborder);
 		}
 	}
-}
 
+	if (tborder)
+		free(tborder);
+}
 
 
 /* simply send out max-length number of filler characters to fp */
