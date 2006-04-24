7384e95b0c013be325888d8954f9edea6ded6c4a
Bruce Momjian <bruce@momjian.us>
2006-04-24 22:17:04 +0000

    Add one more paren to macro.

--- src/include/access/heapam.h
+++ src/include/access/heapam.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2006, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/access/heapam.h,v 1.108 2006/04/24 22:06:32 momjian Exp $
+ * $PostgreSQL: pgsql/src/include/access/heapam.h,v 1.109 2006/04/24 22:17:04 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -100,7 +100,7 @@ extern Datum fastgetattr(HeapTuple tup, int attnum, TupleDesc tupleDesc,
 		( \
 			((attnum) > (int) (tup)->t_data->t_natts) ? \
 			( \
-				((isnull) != NULL) ? (*(isnull) = true) : (dummyret)NULL), \
+				(((isnull) != NULL) ? (*(isnull) = true) : (dummyret)NULL), \
 				(Datum)NULL \
 			) \
 			: \
