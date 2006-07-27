596b858fcbc40256c5ee5c855f1a238ab51ab962
Andrew Dunstan <andrew@dunslane.net>
2006-07-27 16:12:36 +0000

    fix most regression tests for new cube code.

--- contrib/cube/sql/cube.sql
+++ contrib/cube/sql/cube.sql
@@ -6,9 +6,11 @@
 -- first, define the datatype.  Turn off echoing so that expected file
 -- does not depend on contents of cube.sql.
 --
+SET client_min_messages = warning;
 \set ECHO none
 \i cube.sql
 \set ECHO all
+RESET client_min_messages;
 
 --
 -- testing the input and output functions
--- contrib/earthdistance/sql/earthdistance.sql
+++ contrib/earthdistance/sql/earthdistance.sql
@@ -6,10 +6,12 @@
 -- first, define the datatype.  Turn off echoing so that expected file
 -- does not depend on contents of earthdistance.sql or cube.sql.
 --
+SET client_min_messages = warning;
 \set ECHO none
 \i ../cube/cube.sql
 \i earthdistance.sql
 \set ECHO all
+RESET client_min_messages;
 
 --
 -- The radius of the Earth we are using.
