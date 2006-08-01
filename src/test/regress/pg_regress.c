9a4eaa944020cbbfaf0f64f886eac36ab052c79b
Bruce Momjian <bruce@momjian.us>
2006-08-01 18:01:36 +0000

    Remove extra argument to printf().

--- src/test/regress/pg_regress.c
+++ src/test/regress/pg_regress.c
@@ -11,7 +11,7 @@
  * Portions Copyright (c) 1996-2006, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/test/regress/pg_regress.c,v 1.18 2006/08/01 14:56:29 adunstan Exp $
+ * $PostgreSQL: pgsql/src/test/regress/pg_regress.c,v 1.19 2006/08/01 18:01:36 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -921,7 +921,7 @@ results_differ(const char *testname)
 	if (strcmp(expectname, testname) != 0)
 	{
 		snprintf(expectfile, sizeof(expectfile), "%s/expected/%s.out",
-				 inputdir, testname, i);
+				 inputdir, testname);
 
 		snprintf(cmd, sizeof(cmd),
 				 SYSTEMQUOTE "diff %s \"%s\" \"%s\" > \"%s\"" SYSTEMQUOTE,
