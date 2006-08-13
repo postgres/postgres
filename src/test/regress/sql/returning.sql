d2ccc707af4d0eb45b907383f6b274060d1f6e16
Tom Lane <tgl@sss.pgh.pa.us>
2006-08-13 17:58:49 +0000

    Original coding of 'returning' regression test inadvertently chose a nonunique join value, leading to plan-choice-dependent results ... and it seems some platforms will choose a different plan.  Tweak the test so that it has well-defined results.  Per report from Olivier Prenant.

--- src/test/regress/sql/returning.sql
+++ src/test/regress/sql/returning.sql
@@ -70,7 +70,7 @@ SELECT * FROM foochild;
 
 UPDATE foo SET f3 = f3*2
   FROM int8_tbl i
-  WHERE foo.f1 = i.q1
+  WHERE foo.f1 = i.q2
   RETURNING *;
 
 SELECT * FROM foo;
@@ -78,7 +78,7 @@ SELECT * FROM foochild;
 
 DELETE FROM foo
   USING int8_tbl i
-  WHERE foo.f1 = i.q1
+  WHERE foo.f1 = i.q2
   RETURNING *;
 
 SELECT * FROM foo;
