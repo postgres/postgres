d22164d4c4228b420e68adb6f18a96e43b694b9f
Bruce Momjian <bruce@momjian.us>
2006-08-31 00:35:32 +0000

    Correct attibution:
    COPY SELECT work done by Zoltan Boszormenyi

--- src/backend/commands/copy.c
+++ src/backend/commands/copy.c
@@ -1,14 +1,14 @@
 /*-------------------------------------------------------------------------
  *
  * copy.c
- *		Implements the COPY utility command.
+ *		Implements the COPY utility command
  *
  * Portions Copyright (c) 1996-2006, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/commands/copy.c,v 1.269 2006/08/30 23:34:21 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/commands/copy.c,v 1.270 2006/08/31 00:35:32 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
