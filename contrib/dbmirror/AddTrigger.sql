af7d257e21aae3d75c46977482309b658b3a29d7
Tom Lane <tgl@sss.pgh.pa.us>
2006-09-05 17:20:29 +0000

    Remove contrib modules that have been migrated to pgfoundry: adddepend, dbase, dbmirror, fulltextindex, mac, userlock; or abandoned: mSQL-interface, tips.

--- contrib/dbmirror/AddTrigger.sql
+++
@@ -1,7 +0,0 @@
--- Adjust this setting to control where the objects get created.
-SET search_path = public;
-
-CREATE TRIGGER "MyTableName_Trig" 
-AFTER INSERT OR DELETE OR UPDATE ON "MyTableName"
-FOR EACH ROW EXECUTE PROCEDURE "recordchange" ();
-
--- contrib/fulltextindex/uninstall.sql
+++
@@ -1,4 +0,0 @@
--- Adjust this setting to control where the objects get created.
-SET search_path = public;
-
-DROP FUNCTION fti() CASCADE; 
