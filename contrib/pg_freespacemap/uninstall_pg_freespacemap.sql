ad5031feac9064ed9d62d5515c1dbd003fbfc07d
Tom Lane <tgl@sss.pgh.pa.us>
2006-09-30 18:15:48 +0000

    uninstall script for pg_freespacemap

---
+++ contrib/pg_freespacemap/uninstall_pg_freespacemap.sql
@@ -0,0 +1,7 @@
+SET search_path = public;
+
+DROP VIEW pg_freespacemap_pages;
+DROP VIEW pg_freespacemap_relations;
+
+DROP FUNCTION pg_freespacemap_pages();
+DROP FUNCTION pg_freespacemap_relations();
