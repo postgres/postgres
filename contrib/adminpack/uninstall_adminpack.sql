8fd0e43c4e9746fb61dcb6196aefad3fd0ab8ae8
Tom Lane <tgl@sss.pgh.pa.us>
2006-09-30 18:03:06 +0000

    Add uninstall script for adminpack - Josh Drake

---
+++ contrib/adminpack/uninstall_adminpack.sql
@@ -0,0 +1,8 @@
+DROP FUNCTION pg_catalog.pg_file_write(text, text, bool) ;
+DROP FUNCTION pg_catalog.pg_file_rename(text, text, text) ;
+DROP FUNCTION pg_catalog.pg_file_rename(text, text) ;
+DROP FUNCTION pg_catalog.pg_file_unlink(text) ;
+DROP FUNCTION pg_catalog.pg_logdir_ls() ;
+DROP FUNCTION pg_catalog.pg_file_read(text, bigint, bigint) ;
+DROP FUNCTION pg_catalog.pg_file_length(text) ;
+DROP FUNCTION pg_catalog.pg_logfile_rotate() ;
