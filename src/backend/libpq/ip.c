62fc3b9929fc59ae964cc40da42330ee69b454e4
Tom Lane <tgl@sss.pgh.pa.us>
2006-10-19 17:26:37 +0000

    Work around reported problem that AIX's getaddrinfo() doesn't seem to zero sin_port in the returned IP address struct when servname is NULL.  This has been observed to cause failure to bind the stats collection socket, and could perhaps cause other issues too.  Per reports from Brad Nicholson and Chris Browne.

--- src/backend/libpq/ip.c
+++ src/backend/libpq/ip.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/libpq/ip.c,v 1.32.2.1 2005/11/22 18:23:09 momjian Exp $
+ *	  $PostgreSQL: pgsql/src/backend/libpq/ip.c,v 1.32.2.2 2006/10/19 17:26:37 tgl Exp $
  *
  * This file and the IPV6 implementation were initially provided by
  * Nigel Kukard <nkukard@lbsd.net>, Linux Based Systems Design
@@ -73,6 +73,15 @@ pg_getaddrinfo_all(const char *hostname, const char *servname,
 #ifdef HAVE_UNIX_SOCKETS
 	if (hintp->ai_family == AF_UNIX)
 		return getaddrinfo_unix(servname, hintp, result);
+#endif
+
+#ifdef _AIX
+	/*
+	 * It seems AIX's getaddrinfo doesn't reliably zero sin_port when servname
+	 * is NULL, so force the issue.
+	 */
+	if (servname == NULL)
+		servname = "0";
 #endif
 
 	/* NULL has special meaning to getaddrinfo(). */
