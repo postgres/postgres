8964b41c7b88bb1acc7bb9811ca8cb4938c7a410
Neil Conway <neilc@samurai.com>
2006-11-08 00:45:30 +0000

    Remove a 15-year old comment questioning behavior that is now well- established: referencing an undefined parameter should result in an error, not NULL.

--- src/backend/executor/execQual.c
+++ src/backend/executor/execQual.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/executor/execQual.c,v 1.197 2006/11/06 18:21:31 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/executor/execQual.c,v 1.198 2006/11/08 00:45:30 neilc Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -595,11 +595,6 @@ ExecEvalConst(ExprState *exprstate, ExprContext *econtext,
  *		something like ($.name) and the expression context contains
  *		the current parameter bindings (name = "sam") (age = 34)...
  *		so our job is to find and return the appropriate datum ("sam").
- *
- *		Q: if we have a parameter ($.foo) without a binding, i.e.
- *		   there is no (foo = xxx) in the parameter list info,
- *		   is this a fatal error or should this be a "not available"
- *		   (in which case we could return NULL)?	-cim 10/13/89
  * ----------------------------------------------------------------
  */
 static Datum
