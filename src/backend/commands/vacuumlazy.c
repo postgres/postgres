c29a0bd52c6361c65d8d5bbe0ce80f21adac7829
Bruce Momjian <bruce@momjian.us>
2007-02-04 03:10:55 +0000

    Change vacuum lazy "compacting" warning message to:
      errhint("Consider using VACUUM FULL on this relation or increasing the configuration parameter \"max_fsm_pages\".")));

--- src/backend/commands/vacuumlazy.c
+++ src/backend/commands/vacuumlazy.c
@@ -36,7 +36,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/commands/vacuumlazy.c,v 1.82 2007/01/05 22:19:27 momjian Exp $
+ *	  $PostgreSQL: pgsql/src/backend/commands/vacuumlazy.c,v 1.83 2007/02/04 03:10:55 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -513,7 +513,7 @@ lazy_scan_heap(Relation onerel, LVRelStats *vacrelstats,
 				(errmsg("relation \"%s.%s\" contains more than \"max_fsm_pages\" pages with useful free space",
 						get_namespace_name(RelationGetNamespace(onerel)),
 						relname),
-				 errhint("Consider compacting this relation or increasing the configuration parameter \"max_fsm_pages\".")));
+				 errhint("Consider using VACUUM FULL on this relation or increasing the configuration parameter \"max_fsm_pages\".")));
 }
 
 
