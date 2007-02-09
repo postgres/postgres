6be40def01a1ca5c673427b2aa47936e6fcba93f
Tom Lane <tgl@sss.pgh.pa.us>
2007-02-09 20:17:59 +0000

    Minor tweak to make rowtypes regression test run faster.  We don't currently have any better strategy for this query than re-running the sub-select over and over; it seems unlikely that doing so 10000 times is a more useful test than doing it a few dozen times.

--- src/test/regress/sql/rowtypes.sql
+++ src/test/regress/sql/rowtypes.sql
@@ -100,7 +100,8 @@ select ROW('ABC','DEF') ~~ ROW('DEF','ABC') as fail;
 
 -- Check row comparison with a subselect
 select unique1, unique2 from tenk1
-where (unique1, unique2) < any (select ten, ten from tenk1 where hundred < 3);
+where (unique1, unique2) < any (select ten, ten from tenk1 where hundred < 3)
+  and unique1 <= 20;
 
 -- Also check row comparison with an indexable condition
 select thousand, tenthous from tenk1
