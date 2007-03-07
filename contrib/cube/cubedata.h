2b1abc8756e973a098943c255c3cbeb2568cfb91
Teodor Sigaev <teodor@sigaev.ru>
2007-03-07 21:25:18 +0000

    Athough cube is a varlena type, nowhere was a detoasting of cube's value, so fix it.  Add macroses DatumGetNDBOX, PG_GETARG_NDBOX and PG_RETURN_NDBOX. Backpatch for 8.2 too.
    Previous versions use version 0 calling conventions. And fmgr code detoast
    values for user-defined functions.

--- contrib/cube/cubedata.h
+++ contrib/cube/cubedata.h
@@ -1,4 +1,4 @@
-/* $PostgreSQL: pgsql/contrib/cube/cubedata.h,v 1.7 2006/03/11 04:38:28 momjian Exp $ */
+/* $PostgreSQL: pgsql/contrib/cube/cubedata.h,v 1.7.2.1 2007/03/07 21:25:18 teodor Exp $ */
 
 #define CUBE_MAX_DIM (100)
 typedef struct NDBOX
@@ -7,3 +7,7 @@ typedef struct NDBOX
 	unsigned int dim;
 	double		x[1];
 }	NDBOX;
+
+#define DatumGetNDBOX(x)	((NDBOX*)DatumGetPointer(x))
+#define PG_GETARG_NDBOX(x)	DatumGetNDBOX( PG_DETOAST_DATUM(PG_GETARG_DATUM(x)) )
+#define PG_RETURN_NDBOX(x)	PG_RETURN_POINTER(x)
