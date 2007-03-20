5e96b04a7cb1f35aa1b75680f8bfbeac7cedc178
Jan Wieck <JanWieck@Yahoo.com>
2007-03-20 03:53:26 +0000

    Bumping catversion due to changes to pg_trigger and pg_rewrite.
    BTW, the comment in this file says that we hope we never have more than
    10 catversion changes per day, but to even make this possible we should
    start counting at zero, shouldn't we?
    
    
    Jan

--- src/include/catalog/catversion.h
+++ src/include/catalog/catversion.h
@@ -37,7 +37,7 @@
  * Portions Copyright (c) 1996-2007, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/catalog/catversion.h,v 1.391 2007/03/17 00:11:05 tgl Exp $
+ * $PostgreSQL: pgsql/src/include/catalog/catversion.h,v 1.392 2007/03/20 03:53:26 wieck Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -53,6 +53,6 @@
  */
 
 /*							yyyymmddN */
-#define CATALOG_VERSION_NO	200703162
+#define CATALOG_VERSION_NO	200703190
 
 #endif
