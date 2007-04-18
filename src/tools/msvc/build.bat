62d25256f13d067f3150200efba38b7ec5102327
Magnus Hagander <magnus@hagander.net>
2007-04-18 10:14:06 +0000

    Change default build to release, to be consistent with unix build. Make it possible to set the default from builenv.bat.
    Per request from Dave Page

--- src/tools/msvc/build.bat
+++ src/tools/msvc/build.bat
@@ -1,8 +1,9 @@
 @echo off
-REM $PostgreSQL: pgsql/src/tools/msvc/build.bat,v 1.7 2007/03/17 14:01:01 mha Exp $
+REM $PostgreSQL: pgsql/src/tools/msvc/build.bat,v 1.8 2007/04/18 10:14:06 mha Exp $
 
 SETLOCAL
 SET STARTDIR=%CD%
+SET CONFIG=
 if exist src\tools\msvc\buildenv.bat call src\tools\msvc\buildenv.bat
 if exist buildenv.bat call buildenv.bat
 
@@ -10,12 +11,20 @@ perl mkvcbuild.pl
 if errorlevel 1 goto :eof
 
 if exist ..\msvc if exist ..\..\..\src cd ..\..\..
-SET CONFIG=
-if "%1" == "" set CONFIG=Debug
-if "%CONFIG%" == "" if "%1" == "DEBUG" set CONFIG=Debug
-if "%CONFIG%" == "" if "%1" == "RELEASE" set CONFIG=Release
-if not "%CONFIG%" == "" shift
-if "%CONFIG%" == "" set CONFIG=Debug
+set CFG=
+if "%1" == "DEBUG" (
+ set CONFIG=Debug
+ set CFG=1
+)
+if "%1" == "RELEASE" (
+ set CONFIG=Release
+ set CFG=1
+)
+if "%CONFIG%" == "" set CONFIG=Release
+
+if "%CFG%" == "1" shift
+
+echo Building %CONFIG%
 
 if "%1" == "" msbuild pgsql.sln /verbosity:detailed /p:Configuration=%CONFIG%
 if not "%1" == "" vcbuild %1.vcproj %CONFIG%
