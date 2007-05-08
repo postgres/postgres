5b7cf08d33ebf220916b0bb16fed4c788a030cca
Tom Lane <tgl@sss.pgh.pa.us>
2007-05-08 19:13:52 +0000

    Reserve some pg_statistic "kind" codes for use by the ESRI ST_Geometry datatype project.  Per request from Ale Raza (araza at esri.com).

--- src/include/catalog/pg_statistic.h
+++ src/include/catalog/pg_statistic.h
@@ -8,7 +8,7 @@
  * Portions Copyright (c) 1996-2007, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/catalog/pg_statistic.h,v 1.32 2007/01/05 22:19:53 momjian Exp $
+ * $PostgreSQL: pgsql/src/include/catalog/pg_statistic.h,v 1.33 2007/05/08 19:13:52 tgl Exp $
  *
  * NOTES
  *	  the genbki.sh script reads this file and generates .bki
@@ -184,7 +184,9 @@ typedef FormData_pg_statistic *Form_pg_statistic;
  *				(values in this range will be documented in this file)
  *	100-199:	reserved for assignment by the PostGIS project
  *				(values to be documented in PostGIS documentation)
- *	200-9999:	reserved for future public assignments
+ *	200-299:	reserved for assignment by the ESRI ST_Geometry project
+ *				(values to be documented in ESRI ST_Geometry documentation)
+ *	300-9999:	reserved for future public assignments
  *
  * For private use you may choose a "kind" code at random in the range
  * 10000-30000.  However, for code that is to be widely disseminated it is
