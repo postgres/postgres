ab04f4de7a187c4715b871675b3afa17fc59c9e1
Tom Lane <tgl@sss.pgh.pa.us>
2007-07-17 17:45:40 +0000

    Fix incorrect optimization of foreign-key checks.  When an UPDATE on the referencing table does not change the tuple's FK column(s), we don't bother to check the PK table since the constraint was presumably already valid. However, the check is still necessary if the tuple was inserted by our own transaction, since in that case the INSERT trigger will conclude it need not make the check (since its version of the tuple has been deleted).  We got this right for simple cases, but not when the insert and update are in different subtransactions of the current top-level transaction; in such cases the FK check would never be made at all.  (Hence, problem dates back to 8.0 when subtransactions were added --- it's actually the subtransaction version of a bug fixed in 7.3.5.)  Fix, and add regression test cases.  Report and fix by Affan Salman.

--- src/backend/commands/trigger.c
+++ src/backend/commands/trigger.c
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/commands/trigger.c,v 1.210.2.2 2007/07/01 17:45:49 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/commands/trigger.c,v 1.210.2.3 2007/07/17 17:45:40 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -3338,8 +3338,7 @@ AfterTriggerSaveEvent(ResultRelInfo *relinfo, int event, bool row_trigger,
 					 * anything, so we have to do the check for the UPDATE
 					 * anyway.
 					 */
-					if (HeapTupleHeaderGetXmin(oldtup->t_data) !=
-						GetCurrentTransactionId() &&
+					if (!TransactionIdIsCurrentTransactionId(HeapTupleHeaderGetXmin(oldtup->t_data)) &&
 						RI_FKey_keyequal_upd_fk(trigger, rel, oldtup, newtup))
 					{
 						continue;
