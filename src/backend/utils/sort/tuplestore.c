3ca3c71141e8259810c6f46fd04892463dadffe9
Neil Conway <neilc@samurai.com>
2007-08-02 17:49:01 +0000

    Fix a memory leak in tuplestore_end(). Unlikely to be significant during normal operation, but tuplestore_end() ought to do what it claims to do.

--- src/backend/utils/sort/tuplestore.c
+++ src/backend/utils/sort/tuplestore.c
@@ -36,7 +36,7 @@
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  * IDENTIFICATION
- *	  $Header: /cvsroot/pgsql/src/backend/utils/sort/tuplestore.c,v 1.16 2003/08/04 02:40:09 momjian Exp $
+ *	  $Header: /cvsroot/pgsql/src/backend/utils/sort/tuplestore.c,v 1.16.4.1 2007/08/02 17:49:01 neilc Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -284,6 +284,7 @@ tuplestore_end(Tuplestorestate *state)
 			pfree(state->memtuples[i]);
 		pfree(state->memtuples);
 	}
+	pfree(state);
 }
 
 /*
