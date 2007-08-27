3b1e04c3e9b63281062aa07ed6eefdd998c0f475
Magnus Hagander <magnus@hagander.net>
2007-08-27 10:29:49 +0000

    Fix generation of snowball_create.sql on msvc builds.

--- src/tools/msvc/Install.pm
+++ src/tools/msvc/Install.pm
@@ -3,7 +3,7 @@ package Install;
 #
 # Package that provides 'make install' functionality for msvc builds
 #
-# $PostgreSQL: pgsql/src/tools/msvc/Install.pm,v 1.16 2007/06/12 11:07:34 mha Exp $
+# $PostgreSQL: pgsql/src/tools/msvc/Install.pm,v 1.17 2007/08/27 10:29:49 mha Exp $
 #
 use strict;
 use warnings;
@@ -64,6 +64,7 @@ sub Install
     );
     GenerateConversionScript($target);
     GenerateTimezoneFiles($target,$conf);
+    GenerateTsearchFiles($target);
     CopyContribFiles($config,$target);
     CopyIncludeFiles($target);
 
@@ -213,6 +214,45 @@ sub GenerateTimezoneFiles
     unshift @tzfiles,'';
     print "Generating timezone files...";
     system("$conf\\zic\\zic -d \"$target/share/timezone\" " . join(" src/timezone/data/", @tzfiles));
+    print "\n";
+}
+
+sub GenerateTsearchFiles
+{
+    my $target = shift;
+
+    print "Generating tsearch script...";
+    my $F;
+    my $tmpl = read_file('src/backend/snowball/snowball.sql.in');
+    my $mf = read_file('src/backend/snowball/Makefile');
+    $mf =~ s{\\\s*[\r\n]+}{}mg;
+    $mf =~ /^LANGUAGES\s*=\s*(.*)$/m
+      || die "Could not find LANGUAGES line in snowball Makefile\n";
+    my @pieces = split /\s+/,$1;
+    open($F,">$target/share/snowball_create.sql")
+      || die "Could not write snowball_create.sql";
+    print $F read_file('src/backend/snowball/snowball_func.sql.in');
+    while ($#pieces > 0)
+    {
+        my $lang = shift @pieces || last;
+        my $latlang = shift @pieces || last;
+        my $txt = $tmpl;
+        my $stop = '';
+
+        if (-s "src/backend/snowball/stopwords/$lang.stop") {
+            $stop = ", StopWords=$lang";
+        }
+
+        $txt =~ s#_LANGNAME_#${lang}#gs;
+        $txt =~ s#_DICTNAME_#${lang}_stem#gs;
+        $txt =~ s#_CFGNAME_#${lang}#gs;
+        $txt =~ s#_LATDICTNAME_#${latlang}_stem#gs;
+        $txt =~ s#_NONLATDICTNAME_#${lang}_stem#gs;
+        $txt =~ s#_STOPWORDS_#$stop#gs;
+        print $F $txt;
+        print ".";
+    }
+    close($F);
     print "\n";
 }
 
