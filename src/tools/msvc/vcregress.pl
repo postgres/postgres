c0a8276061f57d85a5fe11dd13cefd1182c4bc80
Andrew Dunstan <andrew@dunslane.net>
2007-09-28 00:14:58 +0000

    exclude contrib/tsearch2 from regression tests.

--- src/tools/msvc/vcregress.pl
+++ src/tools/msvc/vcregress.pl
@@ -1,7 +1,7 @@
 
 # -*-perl-*- hey - emacs - this is a perl file
 
-# $PostgreSQL: pgsql/src/tools/msvc/vcregress.pl,v 1.3 2007/09/24 21:42:34 adunstan Exp $
+# $PostgreSQL: pgsql/src/tools/msvc/vcregress.pl,v 1.4 2007/09/28 00:14:58 adunstan Exp $
 
 use strict;
 
@@ -188,6 +188,7 @@ sub contribcheck
     my $mstat = 0;
     foreach my $module (glob("*"))
     {
+		next if $module eq 'tsearch2';
         next unless -d "$module/sql" && 
 			-d "$module/expected" && 
 			(-f "$module/Makefile" || -f "$module/GNUmakefile");
