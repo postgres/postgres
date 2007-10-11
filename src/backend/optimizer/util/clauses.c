9bc1ea970d081d6e0c6f9aca3a34d4a0b696f1f3
Tom Lane <tgl@sss.pgh.pa.us>
2007-10-11 21:28:20 +0000

    Ensure that the result of evaluating a function during constant-expression simplification gets detoasted before it is incorporated into a Const node. Otherwise, if an immutable function were to return a TOAST pointer (an unlikely case, but it can be made to happen), we would end up with a plan that depends on the continued existence of the out-of-line toast datum.

--- src/backend/optimizer/util/clauses.c
+++ src/backend/optimizer/util/clauses.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/optimizer/util/clauses.c,v 1.186.4.7 2007/05/01 18:54:16 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/optimizer/util/clauses.c,v 1.186.4.8 2007/10/11 21:28:20 tgl Exp $
  *
  * HISTORY
  *	  AUTHOR			DATE			MAJOR EVENT
@@ -2506,9 +2506,20 @@ evaluate_expr(Expr *expr, Oid result_type)
 	/* Get back to outer memory context */
 	MemoryContextSwitchTo(oldcontext);
 
-	/* Must copy result out of sub-context used by expression eval */
+	/*
+	 * Must copy result out of sub-context used by expression eval.
+	 *
+	 * Also, if it's varlena, forcibly detoast it.  This protects us against
+	 * storing TOAST pointers into plans that might outlive the referenced
+	 * data.
+	 */
 	if (!const_is_null)
-		const_val = datumCopy(const_val, resultTypByVal, resultTypLen);
+	{
+		if (resultTypLen == -1)
+			const_val = PointerGetDatum(PG_DETOAST_DATUM_COPY(const_val));
+		else
+			const_val = datumCopy(const_val, resultTypByVal, resultTypLen);
+	}
 
 	/* Release all the junk we just created */
 	FreeExecutorState(estate);
