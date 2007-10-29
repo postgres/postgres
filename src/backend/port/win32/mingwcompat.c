811be893fa743ef553950a5d31b7e68230d3cf11
Magnus Hagander <magnus@hagander.net>
2007-10-29 12:35:41 +0000

    Add compat file for dynamically loading the functions that MinGW is missing the imports for. Add RegisterWaitForSingleObject() to the list of such functions, which should take care of the current buildfarm breakage.

---
+++ src/backend/port/win32/mingwcompat.c
@@ -0,0 +1,77 @@
+/*-------------------------------------------------------------------------
+ *
+ * mingwcompat.c
+ *	  MinGW compatibility functions
+ *
+ * Portions Copyright (c) 1996-2007, PostgreSQL Global Development Group
+ *
+ * IDENTIFICATION
+ *	  $PostgreSQL: pgsql/src/backend/port/win32/mingwcompat.c,v 1.1 2007/10/29 12:35:41 mha Exp $
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#include "postgres.h"
+
+/*
+ * This file contains loaders for functions that are missing in the MinGW 
+ * import libraries. It's only for actual Win32 API functions, so they are
+ * all present in proper Win32 compilers.
+ */
+#ifndef WIN32_ONLY_COMPILER
+
+static HMODULE kernel32 = NULL;
+
+/*
+ * Load DLL file just once regardless of how many functions
+ * we load/call in it.
+ */
+static void
+LoadKernel32()
+{
+	if (kernel32 != NULL)
+		return;
+
+	kernel32 = LoadLibraryEx("kernel32.dll", NULL, 0);
+	if (kernel32 == NULL)
+		ereport(FATAL,
+				(errmsg_internal("could not load kernel32.dll: %d",
+								 (int)GetLastError())));
+}
+
+
+/*
+ * Replacement for RegisterWaitForSingleObject(), which lives in
+ * kernel32.dll·
+ */
+typedef BOOL (WINAPI * __RegisterWaitForSingleObject)
+	(PHANDLE, HANDLE, WAITORTIMERCALLBACK, PVOID, ULONG, ULONG);
+__RegisterWaitForSingleObject _RegisterWaitForSingleObject = NULL;
+
+BOOL WINAPI 
+RegisterWaitForSingleObject(PHANDLE phNewWaitObject,
+							HANDLE hObject,
+							WAITORTIMERCALLBACK Callback,
+							PVOID Context,
+							ULONG dwMilliseconds,
+							ULONG dwFlags)
+{
+	if (_RegisterWaitForSingleObject == NULL)
+	{
+		LoadKernel32();
+
+		_RegisterWaitForSingleObject = (__RegisterWaitForSingleObject)
+			GetProcAddress(kernel32, "RegisterWaitForSingleObject");
+
+		if (_RegisterWaitForSingleObject == NULL)
+			ereport(FATAL,
+					(errmsg_internal("could not locate RegisterWaitForSingleObject in kernel32.dll: %d",
+									 (int)GetLastError())));
+	}
+
+	return (_RegisterWaitForSingleObject)
+		(phNewWaitObject, hObject, Callback, Context, dwMilliseconds, dwFlags);
+}
+
+#endif
+
