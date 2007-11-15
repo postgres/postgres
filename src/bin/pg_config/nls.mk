3adc760fb92eab1a8720337a8bf9b66486609eb3
Peter Eisentraut <peter_e@gmx.net>
2007-11-15 20:38:15 +0000

    Translation updates

--- src/bin/pg_config/nls.mk
+++ src/bin/pg_config/nls.mk
@@ -1,5 +1,5 @@
-# $PostgreSQL: pgsql/src/bin/pg_config/nls.mk,v 1.16 2006/10/21 21:03:01 petere Exp $
+# $PostgreSQL: pgsql/src/bin/pg_config/nls.mk,v 1.17 2007/11/15 20:38:15 petere Exp $
 CATALOG_NAME	:= pg_config
-AVAIL_LANGUAGES	:= cs de es fr ko nb pl pt_BR ro ru sl sv tr zh_CN zh_TW
+AVAIL_LANGUAGES	:= cs de es fr ko nb pl pt_BR ro ru sl sv ta tr zh_CN zh_TW
 GETTEXT_FILES	:= pg_config.c ../../port/exec.c
 GETTEXT_TRIGGERS:= _
--- src/bin/pg_resetxlog/nls.mk
+++ src/bin/pg_resetxlog/nls.mk
@@ -1,5 +1,5 @@
-# $PostgreSQL: pgsql/src/bin/pg_resetxlog/nls.mk,v 1.17 2004/12/16 11:27:42 petere Exp $
+# $PostgreSQL: pgsql/src/bin/pg_resetxlog/nls.mk,v 1.18 2007/11/15 20:38:15 petere Exp $
 CATALOG_NAME	:= pg_resetxlog
-AVAIL_LANGUAGES	:= cs de es fr hu it ko nb pt_BR ro ru sk sl sv tr zh_CN zh_TW
+AVAIL_LANGUAGES	:= cs de es fr hu it ko nb pt_BR ro ru sk sl sv ta tr zh_CN zh_TW
 GETTEXT_FILES	:= pg_resetxlog.c
 GETTEXT_TRIGGERS:= _
