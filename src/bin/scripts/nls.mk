42a0c88eadb18c90252c2d25e6918bd03931bbff
Peter Eisentraut <peter_e@gmx.net>
2007-11-29 08:50:36 +0000

    Translation updates

--- src/bin/scripts/nls.mk
+++ src/bin/scripts/nls.mk
@@ -1,6 +1,6 @@
-# $PostgreSQL: pgsql/src/bin/scripts/nls.mk,v 1.20 2006/09/22 18:50:41 petere Exp $
+# $PostgreSQL: pgsql/src/bin/scripts/nls.mk,v 1.21 2007/11/29 08:50:35 petere Exp $
 CATALOG_NAME    := pgscripts
-AVAIL_LANGUAGES := cs de es fr it ko pt_BR ro ru sk sl sv tr zh_CN zh_TW
+AVAIL_LANGUAGES := cs de es fr it ko pt_BR ro ru sk sl sv ta tr zh_CN zh_TW
 GETTEXT_FILES   := createdb.c createlang.c createuser.c \
                    dropdb.c droplang.c dropuser.c \
                    clusterdb.c vacuumdb.c reindexdb.c \
