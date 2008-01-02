ba340917129f5f6a22c3525c6b21eb85dd7b7a88
Tom Lane <tgl@sss.pgh.pa.us>
2008-01-02 22:05:28 +0000

    Insert ARST into the list of known timezone abbreviations.

--- src/backend/utils/adt/datetime.c
+++ src/backend/utils/adt/datetime.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/utils/adt/datetime.c,v 1.137.4.3 2005/12/01 17:56:54 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/utils/adt/datetime.c,v 1.137.4.4 2008/01/02 22:05:28 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -123,8 +123,8 @@ static datetkn datetktbl[] = {
 #if 0
 	aqtst
 	aqtt
-	arst
 #endif
+	{"arst", DTZ, NEG(8)},		/* Argentina Summer Time */
 	{"art", TZ, NEG(12)},		/* Argentina Time */
 #if 0
 	ashst
