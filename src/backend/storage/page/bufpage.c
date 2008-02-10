082aca9ec2854f8e25730c37411685f51a6277de
Tom Lane <tgl@sss.pgh.pa.us>
2008-02-10 20:39:08 +0000

    Fix PageGetExactFreeSpace() so that it actually behaves sensibly if pd_lower > pd_upper, rather than merely claiming to.  This would only matter if the page header were corrupt, which shouldn't occur, but ...

--- src/backend/storage/page/bufpage.c
+++ src/backend/storage/page/bufpage.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/storage/page/bufpage.c,v 1.77 2008/01/01 19:45:52 momjian Exp $
+ *	  $PostgreSQL: pgsql/src/backend/storage/page/bufpage.c,v 1.78 2008/02/10 20:39:08 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -488,6 +488,9 @@ PageGetExactFreeSpace(Page page)
 	 */
 	space = (int) ((PageHeader) page)->pd_upper -
 		(int) ((PageHeader) page)->pd_lower;
+
+	if (space < 0)
+		return 0;
 
 	return (Size) space;
 }
