bea0466d9dc099f1fda28f5714e83ad9a1b23034
Michael Meskes <meskes@postgresql.org>
2008-02-15 12:11:02 +0000

    Changed the way symbols are defined in C in INFORMIX mode. Added SQLSTATE macro closing bug #3961. EXECUTE can return NOT FOUND so it should be checked here too.

--- src/interfaces/ecpg/include/decimal.h
+++ src/interfaces/ecpg/include/decimal.h
@@ -1,10 +1,11 @@
-/* $PostgreSQL: pgsql/src/interfaces/ecpg/include/decimal.h,v 1.14 2006/03/11 04:38:39 momjian Exp $ */
+/* $PostgreSQL: pgsql/src/interfaces/ecpg/include/decimal.h,v 1.14.4.1 2008/02/15 12:11:02 meskes Exp $ */
 
 #ifndef _ECPG_DECIMAL_H
 #define _ECPG_DECIMAL_H
 
 #include <ecpg_informix.h>
 
-typedef decimal dec_t;
+/* brought in by ecpg_informix.h nowadays
+ * typedef decimal dec_t; */
 
 #endif   /* ndef _ECPG_DECIMAL_H */
