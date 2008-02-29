da956ae76fa08924bb6c1bcd7323ef0566a0194b
Andrew Dunstan <andrew@dunslane.net>
2008-02-29 23:31:42 +0000

    Don't call AddUserToDacl on Cygwin

--- src/bin/initdb/initdb.c
+++ src/bin/initdb/initdb.c
@@ -42,7 +42,7 @@
  * Portions Copyright (c) 1994, Regents of the University of California
  * Portions taken from FreeBSD.
  *
- * $PostgreSQL: pgsql/src/bin/initdb/initdb.c,v 1.152.2.1 2008/02/29 15:31:40 mha Exp $
+ * $PostgreSQL: pgsql/src/bin/initdb/initdb.c,v 1.152.2.2 2008/02/29 23:31:42 adunstan Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -2349,7 +2349,9 @@ CreateRestrictedProcess(char *cmd, PROCESS_INFORMATION * processInfo)
 		return 0;
 	}
 
+#ifndef __CYGWIN__
 	AddUserToDacl(processInfo->hProcess);
+#endif
 
 	return ResumeThread(processInfo->hThread);
 }
