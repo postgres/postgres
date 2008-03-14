c666edb5eb8c9f96788f9cf5fbd74f7727ba46d7
Peter Eisentraut <peter_e@gmx.net>
2008-03-14 04:46:30 +0000

    Translation updates

--- src/backend/nls.mk
+++ src/backend/nls.mk
@@ -1,6 +1,6 @@
-# $PostgreSQL: pgsql/src/backend/nls.mk,v 1.21 2008/01/30 11:05:37 petere Exp $
+# $PostgreSQL: pgsql/src/backend/nls.mk,v 1.21.2.1 2008/03/14 04:46:29 petere Exp $
 CATALOG_NAME	:= postgres
-AVAIL_LANGUAGES	:= af cs de es fr hr hu it ko nb nl pt_BR ro ru sk sl sv tr zh_CN zh_TW
+AVAIL_LANGUAGES	:= af cs de es fr hr hu it ko nb nl pl pt_BR ro ru sk sl sv tr zh_CN zh_TW
 GETTEXT_FILES	:= + gettext-files
 # you can add "elog:2" and "errmsg_internal" to this list if you want to
 # include internal messages in the translation list.
