7f3d1583f8bc9f3bf6866e38f28c4671a0cb56f6
Tom Lane <tgl@sss.pgh.pa.us>
2008-04-24 20:18:23 +0000

    Fix ALTER TABLE ADD COLUMN ... PRIMARY KEY so that the new column is correctly checked to see if it's been initialized to all non-nulls.  The implicit NOT NULL constraint was not being checked during the ALTER (in fact, not even if there was an explicit NOT NULL too), because ATExecAddColumn neglected to set the flag needed to make the test happen.  This has been broken since the capability was first added, in 8.0.
    Brendan Jurd, per a report from Kaloyan Iliev.

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/commands/tablecmds.c,v 1.142.4.7 2007/02/02 00:08:01 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/commands/tablecmds.c,v 1.142.4.8 2008/04/24 20:18:23 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -3134,6 +3134,11 @@ ATExecAddColumn(AlteredTableInfo *tab, Relation rel,
 
 		tab->newvals = lappend(tab->newvals, newval);
 	}
+
+	/*
+	 * If the new column is NOT NULL, tell Phase 3 it needs to test that.
+	 */
+	tab->new_notnull |= colDef->is_not_null;
 
 	/*
 	 * Add needed dependency entries for the new column.
