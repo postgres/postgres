c2c9fbef941adfae894eac69a7ea9734caf712a0
Andrew Dunstan <andrew@dunslane.net>
2008-05-04 03:46:08 +0000

    Place statement timeout more appropriately, per gripe from Tom.

--- src/bin/pg_dump/pg_backup_archiver.c
+++ src/bin/pg_dump/pg_backup_archiver.c
@@ -15,7 +15,7 @@
  *
  *
  * IDENTIFICATION
- *		$PostgreSQL: pgsql/src/bin/pg_dump/pg_backup_archiver.c,v 1.155 2008/05/03 23:32:32 adunstan Exp $
+ *		$PostgreSQL: pgsql/src/bin/pg_dump/pg_backup_archiver.c,v 1.156 2008/05/04 03:46:08 adunstan Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -218,11 +218,6 @@ RestoreArchive(Archive *AHX, RestoreOptions *ropt)
 		else
 			ahprintf(AH, "BEGIN;\n\n");
 	}
-
-	/*
-	 * Disable statement_timeout in archive for pg_restore/psql
-	 */
-	ahprintf(AH, "SET statement_timeout = 0;\n");
 
 	/*
 	 * Establish important parameter values right away.
@@ -2140,6 +2135,9 @@ _tocEntryRequired(TocEntry *te, RestoreOptions *ropt, bool include_acls)
 static void
 _doSetFixedOutputState(ArchiveHandle *AH)
 {
+	/* Disable statement_timeout in archive for pg_restore/psql	 */
+	ahprintf(AH, "SET statement_timeout = 0;\n")
+
 	/* Select the correct character set encoding */
 	ahprintf(AH, "SET client_encoding = '%s';\n",
 			 pg_encoding_to_char(AH->public.encoding));
