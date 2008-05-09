1a107363e2b0acbdc5da2ab2ddb1376927d91435
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2008-05-09 15:28:01 +0000

    Fix Assert introduced in previous patch.

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2008, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/backend/access/transam/xlog.c,v 1.292.2.2 2008/05/09 14:28:08 heikki Exp $
+ * $PostgreSQL: pgsql/src/backend/access/transam/xlog.c,v 1.292.2.3 2008/05/09 15:28:01 heikki Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -2506,7 +2506,7 @@ RestoreArchivedFile(char *path, const char *xlogfname,
 					 ControlFile->checkPointCopy.ThisTimeLineID,
 					 restartLog, restartSeg);
 		/* we shouldn't need anything earlier than last restart point */
-		Assert(strcmp(lastRestartPointFname, xlogfname) < 0);
+		Assert(strcmp(lastRestartPointFname, xlogfname) <= 0);
 	}
 	else
 		XLogFileName(lastRestartPointFname, 0, 0, 0);
