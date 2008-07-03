2c2aff6acd20710d59baed54fd1c20e47a5cfb53
Bruce Momjian <bruce@momjian.us>
2008-07-03 02:49:54 +0000

    Update source code comment about when to use gettext_noop().

--- src/include/c.h
+++ src/include/c.h
@@ -12,7 +12,7 @@
  * Portions Copyright (c) 1996-2008, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/c.h,v 1.228 2008/06/24 17:58:27 tgl Exp $
+ * $PostgreSQL: pgsql/src/include/c.h,v 1.229 2008/07/03 02:49:54 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -99,8 +99,12 @@
 #endif
 
 /*
- *	Use this to mark strings to be translated by gettext, in places where
- *	you don't want an actual function call to occur (eg, constant tables).
+ *	Use this to mark string constants as needing translation at some later
+ *	time, rather than immediately.  This is useful for cases where you need
+ *	access to the original string and translated string, and for cases where
+ *	immediate translation is not possible, like when initializing global
+ *	variables.
+ *		http://www.gnu.org/software/autoconf/manual/gettext/Special-cases.html
  */
 #define gettext_noop(x) (x)
 
