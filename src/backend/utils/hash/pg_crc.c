d96d7be2b5652461510d7a4c1f04d5e6c2bacccd
Alvaro Herrera <alvherre@alvh.no-ip.org>
2008-08-25 17:37:40 +0000

    Update URL to Ross William's paper.
    Devrim Gunduz.

--- src/backend/utils/hash/pg_crc.c
+++ src/backend/utils/hash/pg_crc.c
@@ -5,7 +5,7 @@
  *
  * See Ross Williams' excellent introduction
  * A PAINLESS GUIDE TO CRC ERROR DETECTION ALGORITHMS, available from
- * ftp://ftp.rocksoft.com/papers/crc_v3.txt or several other net sites.
+ * http://www.ross.net/crc/download/crc_v3.txt or several other net sites.
  *
  * We use a normal (not "reflected", in Williams' terms) CRC, using initial
  * all-ones register contents and a final bit inversion.
@@ -19,7 +19,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/utils/hash/pg_crc.c,v 1.19 2008/03/27 03:57:33 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/utils/hash/pg_crc.c,v 1.20 2008/08/25 17:37:40 alvherre Exp $
  *
  *-------------------------------------------------------------------------
  */
