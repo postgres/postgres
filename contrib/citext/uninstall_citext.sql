5bc561a16a8515736d4ed59f5871dc78d1a75f75
Tom Lane <tgl@sss.pgh.pa.us>
2008-09-05 18:25:17 +0000

    Add some additional casts and regression tests for the citext data type. David Wheeler

--- contrib/citext/uninstall_citext.sql
+++ contrib/citext/uninstall_citext.sql
@@ -1,4 +1,4 @@
-/* $PostgreSQL: pgsql/contrib/citext/uninstall_citext.sql,v 1.2 2008/07/30 17:08:52 tgl Exp $ */
+/* $PostgreSQL: pgsql/contrib/citext/uninstall_citext.sql,v 1.3 2008/09/05 18:25:16 tgl Exp $ */
 
 -- Adjust this setting to control where the objects get dropped.
 SET search_path = public;
@@ -40,8 +40,12 @@ DROP CAST (citext AS bpchar);
 DROP CAST (text AS citext);
 DROP CAST (varchar AS citext);
 DROP CAST (bpchar AS citext);
+DROP CAST (boolean AS citext);
+DROP CAST (inet AS citext);
 
 DROP FUNCTION citext(bpchar);
+DROP FUNCTION citext(boolean);
+DROP FUNCTION citext(inet);
 DROP FUNCTION citext_eq(citext, citext);
 DROP FUNCTION citext_ne(citext, citext);
 DROP FUNCTION citext_lt(citext, citext);
@@ -60,5 +64,17 @@ DROP FUNCTION texticlike(citext, text);
 DROP FUNCTION texticnlike(citext, text);
 DROP FUNCTION texticregexeq(citext, text);
 DROP FUNCTION texticregexne(citext, text);
+DROP FUNCTION regexp_matches( citext, citext );
+DROP FUNCTION regexp_matches( citext, citext, text );
+DROP FUNCTION regexp_replace( citext, citext, text );
+DROP FUNCTION regexp_replace( citext, citext, text, text );
+DROP FUNCTION regexp_split_to_array( citext, citext );
+DROP FUNCTION regexp_split_to_array( citext, citext, text );
+DROP FUNCTION regexp_split_to_table( citext, citext );
+DROP FUNCTION regexp_split_to_table( citext, citext, text );
+DROP FUNCTION strpos( citext, citext );
+DROP FUNCTION replace( citext, citext, citext );
+DROP FUNCTION split_part( citext, citext, int );
+DROP FUNCTION translate( citext, citext, text );
 
 DROP TYPE citext CASCADE;
