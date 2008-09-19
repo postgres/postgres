90d910866e44ac009028dc75747f1463db8c528d
Peter Eisentraut <peter_e@gmx.net>
2008-09-19 13:08:08 +0000

    Add additional rules so that make init-po for ecpg gets up to date flex and bison output.  Without these, make can sometimes be tempted to invoke its built-in rules using lex and yacc, which can fail if those commands are not available.
    This was a main cause for the NLS web site breakage.

--- src/interfaces/ecpg/nls.mk
+++ src/interfaces/ecpg/nls.mk
@@ -18,3 +18,14 @@ GETTEXT_FILES	= \
 	preproc/type.c \
 	preproc/variable.c
 GETTEXT_TRIGGERS = _ mmerror:3 ecpg_gettext ecpg_log:1
+
+
+# Without these rules, make will invoke predefined implicit rules using "lex" 
+# and "yacc", which we don't want.  The prerequisites should be exactly the 
+# same as in preproc/Makefile.
+
+$(srcdir)/preproc/preproc.c: preproc/preproc.y
+	$(MAKE) -C preproc preproc.c
+
+$(srcdir)/preproc/pgc.c: preproc/pgc.l
+	$(MAKE) -C preproc pgc.c
