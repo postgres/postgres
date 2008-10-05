128849875f33953cc19a7238f15c9398c0490171
Peter Eisentraut <peter_e@gmx.net>
2008-10-05 14:20:03 +0000

    Additional test coverage for boolean type (bool.c)

--- src/test/regress/sql/boolean.sql
+++ src/test/regress/sql/boolean.sql
@@ -10,11 +10,47 @@ SELECT 1 AS one;
 
 -- ******************testing built-in type bool********************
 
--- check bool type-casting as well as and, or, not in qualifications--
+-- check bool input syntax
+
+SELECT true AS true;
+
+SELECT false AS false;
 
 SELECT bool 't' AS true;
 
 SELECT bool '   f           ' AS false;
+
+SELECT bool 'true' AS true;
+
+SELECT bool 'test' AS error;
+
+SELECT bool 'false' AS false;
+
+SELECT bool 'foo' AS error;
+
+SELECT bool 'y' AS true;
+
+SELECT bool 'yes' AS true;
+
+SELECT bool 'yeah' AS error;
+
+SELECT bool 'n' AS false;
+
+SELECT bool 'no' AS false;
+
+SELECT bool 'nay' AS error;
+
+SELECT bool '1' AS true;
+
+SELECT bool '11' AS error;
+
+SELECT bool '0' AS false;
+
+SELECT bool '000' AS error;
+
+SELECT bool '' AS error;
+
+-- and, or, not in qualifications
 
 SELECT bool 't' or bool 'f' AS true;
 
@@ -25,6 +61,14 @@ SELECT not bool 'f' AS true;
 SELECT bool 't' = bool 'f' AS false;
 
 SELECT bool 't' <> bool 'f' AS true;
+
+SELECT bool 't' > bool 'f' AS true;
+
+SELECT bool 't' >= bool 'f' AS true;
+
+SELECT bool 'f' < bool 't' AS true;
+
+SELECT bool 'f' <= bool 't' AS true;
 
 -- explicit casts to/from text
 SELECT 'TrUe'::text::boolean AS true, 'fAlse'::text::boolean AS false;
