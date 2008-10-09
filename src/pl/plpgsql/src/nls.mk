cbe99a97a8343c36ff7d9ce37381edd75c19e47b
Alvaro Herrera <alvherre@alvh.no-ip.org>
2008-10-09 18:15:28 +0000

    Add initial plpgsql translation (with lots of fuzzies)

--- src/pl/plpgsql/src/nls.mk
+++ src/pl/plpgsql/src/nls.mk
@@ -1,6 +1,6 @@
-# $PostgreSQL: pgsql/src/pl/plpgsql/src/nls.mk,v 1.1 2008/10/09 17:24:05 alvherre Exp $
+# $PostgreSQL: pgsql/src/pl/plpgsql/src/nls.mk,v 1.2 2008/10/09 18:15:28 alvherre Exp $
 CATALOG_NAME	:= plpgsql
-AVAIL_LANGUAGES	:=
+AVAIL_LANGUAGES	:= es
 GETTEXT_FILES	:= pl_comp.c pl_exec.c pl_gram.c pl_funcs.c pl_handler.c pl_scan.c
 GETTEXT_TRIGGERS:= _ errmsg errdetail errdetail_log errhint errcontext write_stderr yyerror
 
