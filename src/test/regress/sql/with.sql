1f238e569acbc2a41be497d4fe1b1b797e5ae902
Tom Lane <tgl@sss.pgh.pa.us>
2008-10-14 00:12:44 +0000

    Eliminate unnecessary array[] decoration in examples of recursive cycle detection.

--- src/test/regress/sql/with.sql
+++ src/test/regress/sql/with.sql
@@ -266,7 +266,7 @@ insert into graph values
 with recursive search_graph(f, t, label, path, cycle) as (
 	select *, array[row(g.f, g.t)], false from graph g
 	union all
-	select g.*, path || array[row(g.f, g.t)], row(g.f, g.t) = any(path)
+	select g.*, path || row(g.f, g.t), row(g.f, g.t) = any(path)
 	from graph g, search_graph sg
 	where g.f = sg.t and not cycle
 )
