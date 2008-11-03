48cbe59150de7f8ba8a065b575bb389f0c5c2d7d
Tom Lane <tgl@sss.pgh.pa.us>
2008-11-03 21:09:17 +0000

    Dept of second thoughts: seems it'd be safer if pg_typeof is marked stable not immutable, since it depends on system catalog contents.

--- src/include/catalog/pg_proc.h
+++ src/include/catalog/pg_proc.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2008, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/catalog/pg_proc.h,v 1.522 2008/11/03 20:17:20 adunstan Exp $
+ * $PostgreSQL: pgsql/src/include/catalog/pg_proc.h,v 1.523 2008/11/03 21:09:17 tgl Exp $
  *
  * NOTES
  *	  The script catalog/genbki.sh reads this file and generates .bki
@@ -2293,7 +2293,7 @@ DESCR("result type of a function");
 DATA(insert OID = 1686 (  pg_get_keywords		PGNSP PGUID 12 10 400 0 f f t t s 0 2249 "" "{25,18,25}" "{o,o,o}" "{word,catcode,catdesc}" pg_get_keywords _null_ _null_ _null_ ));
 DESCR("list of SQL keywords");
 
-DATA(insert OID = 1619 (  pg_typeof				PGNSP PGUID 12 1 0 0 f f f f i 1 2206  "2276" _null_ _null_ _null_  pg_typeof _null_ _null_ _null_ ));
+DATA(insert OID = 1619 (  pg_typeof				PGNSP PGUID 12 1 0 0 f f f f s 1 2206  "2276" _null_ _null_ _null_  pg_typeof _null_ _null_ _null_ ));
 DESCR("returns the type of the argument");
 
 /* Generic referential integrity constraint triggers */
