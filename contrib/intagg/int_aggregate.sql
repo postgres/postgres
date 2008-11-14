32cc9e553323c40beba4a98a5859fafe6e4e1855
Tom Lane <tgl@sss.pgh.pa.us>
2008-11-14 19:58:45 +0000

    Reduce contrib/intagg to a thin wrapper around the new core functions array_agg() and unnest().  We could drop it entirely in the future, but let's keep it for a release or two as a compatibility assist.

--- contrib/intagg/int_aggregate.sql
+++ contrib/intagg/int_aggregate.sql
@@ -1,28 +1,28 @@
-/* $PostgreSQL: pgsql/contrib/intagg/int_aggregate.sql.in,v 1.9 2007/11/13 04:24:28 momjian Exp $ */
+/* $PostgreSQL: pgsql/contrib/intagg/int_aggregate.sql,v 1.1 2008/11/14 19:58:45 tgl Exp $ */
 
 -- Adjust this setting to control where the objects get created.
 SET search_path = public;
 
 -- Internal function for the aggregate
 -- Is called for each item in an aggregation
-CREATE OR REPLACE FUNCTION int_agg_state (int4[], int4)
-RETURNS int4[]
-AS 'MODULE_PATHNAME','int_agg_state'
-LANGUAGE C;
+CREATE OR REPLACE FUNCTION int_agg_state (internal, int4)
+RETURNS internal
+AS 'array_agg_transfn'
+LANGUAGE INTERNAL;
 
 -- Internal function for the aggregate
 -- Is called at the end of the aggregation, and returns an array.
-CREATE OR REPLACE FUNCTION int_agg_final_array (int4[])
+CREATE OR REPLACE FUNCTION int_agg_final_array (internal)
 RETURNS int4[]
-AS 'MODULE_PATHNAME','int_agg_final_array'
-LANGUAGE C;
+AS 'array_agg_finalfn'
+LANGUAGE INTERNAL;
 
 -- The aggregate function itself
 -- uses the above functions to create an array of integers from an aggregation.
 CREATE AGGREGATE int_array_aggregate (
 	BASETYPE = int4,
 	SFUNC = int_agg_state,
-	STYPE = int4[],
+	STYPE = internal,
 	FINALFUNC = int_agg_final_array
 );
 
@@ -31,5 +31,5 @@ CREATE AGGREGATE int_array_aggregate (
 -- as a row.
 CREATE OR REPLACE FUNCTION int_array_enum(int4[])
 RETURNS setof integer
-AS 'MODULE_PATHNAME','int_enum'
-LANGUAGE C IMMUTABLE STRICT;
+AS 'array_unnest'
+LANGUAGE INTERNAL IMMUTABLE STRICT;
--- contrib/intagg/uninstall_int_aggregate.sql
+++ contrib/intagg/uninstall_int_aggregate.sql
@@ -1,4 +1,4 @@
-/* $PostgreSQL: pgsql/contrib/intagg/uninstall_int_aggregate.sql,v 1.3 2007/11/13 04:24:28 momjian Exp $ */
+/* $PostgreSQL: pgsql/contrib/intagg/uninstall_int_aggregate.sql,v 1.4 2008/11/14 19:58:45 tgl Exp $ */
 
 -- Adjust this setting to control where the objects get dropped.
 SET search_path = public;
@@ -7,6 +7,6 @@ DROP FUNCTION int_array_enum(int4[]);
 
 DROP AGGREGATE int_array_aggregate (int4);
 
-DROP FUNCTION int_agg_final_array (int4[]);
+DROP FUNCTION int_agg_final_array (internal);
 
-DROP FUNCTION int_agg_state (int4[], int4);
+DROP FUNCTION int_agg_state (internal, int4);
