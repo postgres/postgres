667685ca7da3b895fe362863c3da00b010543399
Magnus Hagander <magnus@hagander.net>
2008-11-18 21:17:26 +0000

    Add required include to build with cygwin.
    Andrew Chernow

--- src/test/thread/thread_test.c
+++ src/test/thread/thread_test.c
@@ -6,7 +6,7 @@
  * Portions Copyright (c) 1996-2008, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- *	$PostgreSQL: pgsql/src/test/thread/thread_test.c,v 1.5 2008/01/01 19:46:01 momjian Exp $
+ *	$PostgreSQL: pgsql/src/test/thread/thread_test.c,v 1.6 2008/11/18 21:17:26 mha Exp $
  *
  *	This program tests to see if your standard libc functions use
  *	pthread_setspecific()/pthread_getspecific() to be thread-safe.
@@ -47,6 +47,10 @@ typedef char bool;
 #include <fcntl.h>
 #include <errno.h>
 
+/* CYGWIN requires this for MAXHOSTNAMELEN */
+#ifdef __CYGWIN__
+#include <sys/param.h>
+#endif
 
 /******************************************************************
  * Windows Hacks
