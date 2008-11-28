4cee672177974062d20c6a2f7b51ed57501d79b7
Tom Lane <tgl@sss.pgh.pa.us>
2008-11-28 21:19:13 +0000

    Fix an oversight in uninstall_isn.sql: must drop operator families explicitly.

--- contrib/isn/uninstall_isn.sql
+++ contrib/isn/uninstall_isn.sql
@@ -1,7 +1,11 @@
-/* $PostgreSQL: pgsql/contrib/isn/uninstall_isn.sql,v 1.3 2007/11/13 04:24:28 momjian Exp $ */
+/* $PostgreSQL: pgsql/contrib/isn/uninstall_isn.sql,v 1.4 2008/11/28 21:19:13 tgl Exp $ */
 
 -- Adjust this setting to control where the objects get dropped.
 SET search_path = public;
+
+-- Drop the operator families (which don't depend on the types)
+DROP OPERATOR FAMILY isn_ops USING btree CASCADE;
+DROP OPERATOR FAMILY isn_ops USING hash CASCADE;
 
 --
 --	Drop the actual types (in cascade):
@@ -15,6 +19,6 @@ DROP TYPE ismn CASCADE;
 DROP TYPE issn CASCADE;
 DROP TYPE upc CASCADE;
 
+-- and clean up a couple miscellaneous functions
 DROP FUNCTION isn_weak();
 DROP FUNCTION isn_weak(boolean);
-
