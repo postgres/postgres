d0fe3df745ac0f05546046fa1732cf5c002d575c
Peter Eisentraut <peter_e@gmx.net>
2009-01-15 12:59:11 +0000

    Remove unneeded GETTEXT_TRIGGERS.

--- src/pl/tcl/nls.mk
+++ src/pl/tcl/nls.mk
@@ -1,5 +1,5 @@
-# $PostgreSQL: pgsql/src/pl/tcl/nls.mk,v 1.1 2008/10/09 17:24:05 alvherre Exp $
+# $PostgreSQL: pgsql/src/pl/tcl/nls.mk,v 1.2 2009/01/15 12:59:11 petere Exp $
 CATALOG_NAME	:= pltcl
 AVAIL_LANGUAGES	:=
 GETTEXT_FILES	:= pltcl.c
-GETTEXT_TRIGGERS:= _ errmsg errdetail errdetail_log errhint errcontext write_stderr yyerror
+GETTEXT_TRIGGERS:= errmsg errdetail errdetail_log errhint errcontext
