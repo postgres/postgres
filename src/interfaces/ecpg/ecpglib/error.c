19afb4e80535ba8d0ce797029a1ff448541e93f2
Peter Eisentraut <peter_e@gmx.net>
2009-01-16 09:15:03 +0000

    Wordsmithing

--- src/interfaces/ecpg/ecpglib/error.c
+++ src/interfaces/ecpg/ecpglib/error.c
@@ -1,4 +1,4 @@
-/* $PostgreSQL: pgsql/src/interfaces/ecpg/ecpglib/error.c,v 1.21 2009/01/15 11:52:55 petere Exp $ */
+/* $PostgreSQL: pgsql/src/interfaces/ecpg/ecpglib/error.c,v 1.22 2009/01/16 09:15:03 petere Exp $ */
 
 #define POSTGRES_ECPG_INTERNAL
 #include "postgres_fe.h"
@@ -106,7 +106,7 @@ ecpg_raise(int line, int code, const char *sqlstate, const char *str)
 			snprintf(sqlca->sqlerrm.sqlerrmc, sizeof(sqlca->sqlerrm.sqlerrmc),
 				/* translator: this string will be truncated at 149 
 				   characters expanded.  */
-					 ecpg_gettext("variable is not an array on line %d"), line);
+					 ecpg_gettext("variable does not have an array type on line %d"), line);
 			break;
 
 		case ECPG_DATA_NOT_ARRAY:
@@ -120,7 +120,7 @@ ecpg_raise(int line, int code, const char *sqlstate, const char *str)
 			snprintf(sqlca->sqlerrm.sqlerrmc, sizeof(sqlca->sqlerrm.sqlerrmc),
 				/* translator: this string will be truncated at 149 
 				   characters expanded.  */
-				 ecpg_gettext("trying to insert an array of variables on line %d"), line);
+				 ecpg_gettext("inserting an array of variables is not supported on line %d"), line);
 			break;
 
 		case ECPG_NO_CONN:
@@ -134,7 +134,7 @@ ecpg_raise(int line, int code, const char *sqlstate, const char *str)
 			snprintf(sqlca->sqlerrm.sqlerrmc, sizeof(sqlca->sqlerrm.sqlerrmc),
 				/* translator: this string will be truncated at 149 
 				   characters expanded.  */
-				 ecpg_gettext("not connected to \"%s\" on line %d"), str, line);
+				 ecpg_gettext("not connected to connection \"%s\" on line %d"), str, line);
 			break;
 
 		case ECPG_INVALID_STMT:
@@ -169,14 +169,14 @@ ecpg_raise(int line, int code, const char *sqlstate, const char *str)
 			snprintf(sqlca->sqlerrm.sqlerrmc, sizeof(sqlca->sqlerrm.sqlerrmc),
 				/* translator: this string will be truncated at 149 
 				   characters expanded.  */
-				 ecpg_gettext("variable is not a numeric type on line %d"), line);
+				 ecpg_gettext("variable does not have a numeric type on line %d"), line);
 			break;
 
 		case ECPG_VAR_NOT_CHAR:
 			snprintf(sqlca->sqlerrm.sqlerrmc, sizeof(sqlca->sqlerrm.sqlerrmc),
 				/* translator: this string will be truncated at 149 
 				   characters expanded.  */
-				 ecpg_gettext("variable is not a character type on line %d"), line);
+				 ecpg_gettext("variable does not have a character type on line %d"), line);
 			break;
 
 		case ECPG_TRANS:
