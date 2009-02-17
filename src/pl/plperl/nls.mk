acee2f6fa4e5a34b696d4d91b663d7b57afbb6e5
Peter Eisentraut <peter_e@gmx.net>
2009-02-17 09:24:57 +0000

    Remove gettext trigger write_stderr(), which isn't used by PLs.

--- src/pl/plperl/nls.mk
+++ src/pl/plperl/nls.mk
@@ -1,5 +1,5 @@
-# $PostgreSQL: pgsql/src/pl/plperl/nls.mk,v 1.1 2008/10/09 17:24:05 alvherre Exp $
+# $PostgreSQL: pgsql/src/pl/plperl/nls.mk,v 1.2 2009/02/17 09:24:57 petere Exp $
 CATALOG_NAME	:= plperl
 AVAIL_LANGUAGES	:=
 GETTEXT_FILES	:= plperl.c SPI.xs
-GETTEXT_TRIGGERS:= _ errmsg errdetail errdetail_log errhint errcontext write_stderr croak Perl_croak
+GETTEXT_TRIGGERS:= _ errmsg errdetail errdetail_log errhint errcontext croak Perl_croak
--- src/pl/plpgsql/src/nls.mk
+++ src/pl/plpgsql/src/nls.mk
@@ -1,8 +1,8 @@
-# $PostgreSQL: pgsql/src/pl/plpgsql/src/nls.mk,v 1.2 2008/10/09 18:15:28 alvherre Exp $
+# $PostgreSQL: pgsql/src/pl/plpgsql/src/nls.mk,v 1.3 2009/02/17 09:24:57 petere Exp $
 CATALOG_NAME	:= plpgsql
 AVAIL_LANGUAGES	:= es
 GETTEXT_FILES	:= pl_comp.c pl_exec.c pl_gram.c pl_funcs.c pl_handler.c pl_scan.c
-GETTEXT_TRIGGERS:= _ errmsg errdetail errdetail_log errhint errcontext write_stderr yyerror
+GETTEXT_TRIGGERS:= _ errmsg errdetail errdetail_log errhint errcontext yyerror
 
 .PHONY: gettext-files
 gettext-files: distprep
