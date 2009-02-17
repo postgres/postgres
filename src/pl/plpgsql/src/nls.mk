207b4e74611d695a47983d2318ea9ccb57f2e95b
Peter Eisentraut <peter_e@gmx.net>
2009-02-17 12:59:35 +0000

    Add plpgsql_yyerror to gettext triggers

--- src/pl/plpgsql/src/nls.mk
+++ src/pl/plpgsql/src/nls.mk
@@ -1,8 +1,8 @@
-# $PostgreSQL: pgsql/src/pl/plpgsql/src/nls.mk,v 1.4 2009/02/17 12:51:59 petere Exp $
+# $PostgreSQL: pgsql/src/pl/plpgsql/src/nls.mk,v 1.5 2009/02/17 12:59:35 petere Exp $
 CATALOG_NAME	:= plpgsql
 AVAIL_LANGUAGES	:= es
 GETTEXT_FILES	:= pl_comp.c pl_exec.c pl_gram.c pl_funcs.c pl_handler.c pl_scan.c
-GETTEXT_TRIGGERS:= _ errmsg errdetail errdetail_log errhint errcontext validate_tupdesc_compat:3 yyerror
+GETTEXT_TRIGGERS:= _ errmsg errdetail errdetail_log errhint errcontext validate_tupdesc_compat:3 yyerror plpgsql_yyerror
 
 .PHONY: gettext-files
 gettext-files: distprep
