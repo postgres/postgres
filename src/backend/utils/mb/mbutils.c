5ae9f82783e9da3f5cc46f2924aa872f4067fc6d
Peter Eisentraut <peter_e@gmx.net>
2009-04-06 19:34:52 +0000

    Add entry in the encoding number to OS name table for KOI8-U.

--- src/backend/utils/mb/mbutils.c
+++ src/backend/utils/mb/mbutils.c
@@ -4,7 +4,7 @@
  *
  * Tatsuo Ishii
  *
- * $PostgreSQL: pgsql/src/backend/utils/mb/mbutils.c,v 1.83 2009/04/02 17:30:53 tgl Exp $
+ * $PostgreSQL: pgsql/src/backend/utils/mb/mbutils.c,v 1.84 2009/04/06 19:34:52 petere Exp $
  */
 #include "postgres.h"
 
@@ -911,6 +911,7 @@ static const struct codeset_map {
 	{PG_LATIN9, "LATIN-9"},
 	{PG_LATIN10, "LATIN10"},
 	{PG_KOI8R, "KOI8-R"},
+	{PG_KOI8U, "KOI8-U"},
 	{PG_WIN1250, "CP1250"},
 	{PG_WIN1251, "CP1251"},
 	{PG_WIN1252, "CP1252"},
