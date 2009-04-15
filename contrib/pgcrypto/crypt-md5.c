9785f7382e44af6b6473d70158f3e0a5d1eab469
Magnus Hagander <magnus@hagander.net>
2009-04-15 18:58:28 +0000

    Remove beer-ware license from crypt-md5.c, per approval from Poul-Henning Kamp.
    This makes the file the same standard 2-clause BSD as the
    rest of PostgreSQL.

--- contrib/pgcrypto/crypt-md5.c
+++ contrib/pgcrypto/crypt-md5.c
@@ -1,15 +1,10 @@
 /*
- * ----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 42):
- * <phk@login.dknet.dk> wrote this file.  As long as you retain this notice you
- * can do whatever you want with this stuff. If we meet some day, and you think
- * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
- * ----------------------------------------------------------------------------
+ * File imported from FreeBSD, original by Poul-Henning Kamp.
  *
  * $FreeBSD: src/lib/libcrypt/crypt-md5.c,v 1.5 1999/12/17 20:21:45 peter Exp $
  *
  */
-/* $PostgreSQL: pgsql/contrib/pgcrypto/crypt-md5.c,v 1.3 2003/11/29 22:39:28 pgsql Exp $ */
+/* $PostgreSQL: pgsql/contrib/pgcrypto/crypt-md5.c,v 1.3.4.1 2009/04/15 18:58:28 mha Exp $ */
 
 #include <postgres.h>
 #include "px.h"
