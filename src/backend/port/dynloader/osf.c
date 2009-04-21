fea168823acd6c1c50d3d366798a4314c79e6dc9
Tom Lane <tgl@sss.pgh.pa.us>
2009-04-21 21:05:25 +0000

    Fix obsolete cross-reference (this file isn't called alpha.c anymore)

--- src/backend/port/dynloader/osf.c
+++ src/backend/port/dynloader/osf.c
@@ -1,5 +1,7 @@
-/* Dummy file used for nothing at this point
+/*
+ * $PostgreSQL: pgsql/src/backend/port/dynloader/osf.c,v 1.3 2009/04/21 21:05:25 tgl Exp $
  *
- * see alpha.h
- * $PostgreSQL: pgsql/src/backend/port/dynloader/osf.c,v 1.2 2006/03/11 04:38:31 momjian Exp $
+ * Dummy file used for nothing at this point
+ *
+ * see osf.h
  */
