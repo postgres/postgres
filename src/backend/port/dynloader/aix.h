0fca584ac8f3cf59ae82ea897a33b9d6b75d324c
Bruce Momjian <bruce@momjian.us>
2009-04-25 15:54:02 +0000

    Remove HELIOS Software GmbH name and copyright from AIX dynloader files, per approval from Helmut Tschemernjak, President.
    Only back branches;  files removed from CVS HEAD.

--- src/backend/port/dynloader/aix.h
+++ src/backend/port/dynloader/aix.h
@@ -1,9 +1,5 @@
 /*
- * $PostgreSQL: pgsql/src/backend/port/dynloader/aix.h,v 1.13 2005/10/15 02:49:23 momjian Exp $
- *
- * @(#)dlfcn.h	1.4 revision of 95/04/25  09:36:52
- * This is an unpublished work copyright (c) 1992 HELIOS Software GmbH
- * 30159 Hannover, Germany
+ * $PostgreSQL: pgsql/src/backend/port/dynloader/aix.h,v 1.13.6.1 2009/04/25 15:54:02 momjian Exp $
  */
 
 #ifndef PORT_PROTOS_H
