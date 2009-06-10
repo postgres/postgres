0b7b908882b18c6b1c34bb9e386c4f2fdb21f0db
Peter Eisentraut <peter_e@gmx.net>
2009-06-10 23:42:44 +0000

    Translation updates

--- src/interfaces/ecpg/ecpglib/nls.mk
+++ src/interfaces/ecpg/ecpglib/nls.mk
@@ -1,5 +1,5 @@
-# $PostgreSQL: pgsql/src/interfaces/ecpg/ecpglib/nls.mk,v 1.4 2009/05/14 21:41:53 alvherre Exp $
+# $PostgreSQL: pgsql/src/interfaces/ecpg/ecpglib/nls.mk,v 1.5 2009/06/10 23:42:43 petere Exp $
 CATALOG_NAME	 = ecpglib
-AVAIL_LANGUAGES	 = de es fr pt_BR tr
+AVAIL_LANGUAGES	 = de es fr ja pt_BR tr
 GETTEXT_FILES	 = connect.c error.c execute.c misc.c
 GETTEXT_TRIGGERS = ecpg_gettext
--- src/pl/tcl/nls.mk
+++ src/pl/tcl/nls.mk
@@ -1,5 +1,5 @@
-# $PostgreSQL: pgsql/src/pl/tcl/nls.mk,v 1.5 2009/06/04 18:33:08 tgl Exp $
+# $PostgreSQL: pgsql/src/pl/tcl/nls.mk,v 1.6 2009/06/10 23:42:44 petere Exp $
 CATALOG_NAME	:= pltcl
-AVAIL_LANGUAGES	:= de es fr pt_BR tr
+AVAIL_LANGUAGES	:= de es fr ja pt_BR tr
 GETTEXT_FILES	:= pltcl.c
 GETTEXT_TRIGGERS:= errmsg errmsg_plural:1,2 errdetail errdetail_log errdetail_plural:1,2 errhint errcontext
