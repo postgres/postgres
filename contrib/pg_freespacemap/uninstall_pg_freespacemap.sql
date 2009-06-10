15a3fe6d6343ae46b9e28a093b8dea422c31af83
Tom Lane <tgl@sss.pgh.pa.us>
2009-06-10 22:12:28 +0000

    Properly mark pg_freespace() function as strict.  Also update uninstall script to match reality.

--- contrib/pg_freespacemap/uninstall_pg_freespacemap.sql
+++ contrib/pg_freespacemap/uninstall_pg_freespacemap.sql
@@ -1,10 +1,7 @@
-/* $PostgreSQL: pgsql/contrib/pg_freespacemap/uninstall_pg_freespacemap.sql,v 1.4 2008/09/30 11:17:07 heikki Exp $ */
+/* $PostgreSQL: pgsql/contrib/pg_freespacemap/uninstall_pg_freespacemap.sql,v 1.5 2009/06/10 22:12:28 tgl Exp $ */
 
 -- Adjust this setting to control where the objects get dropped.
 SET search_path = public;
 
-DROP VIEW pg_freespacemap_pages;
-DROP VIEW pg_freespacemap_relations;
-
-DROP FUNCTION pg_freespacemap_pages();
-DROP FUNCTION pg_freespacemap_relations();
+DROP FUNCTION pg_freespace(regclass, bigint);
+DROP FUNCTION pg_freespace(regclass);
