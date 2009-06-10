746c28a8e46be060d0461b1c642a9b147fe71a5c
Peter Eisentraut <peter_e@gmx.net>
2009-06-10 20:02:15 +0000

    Improve capitalization and punctuation in recently added GiST message.

--- src/backend/access/gist/gistsplit.c
+++ src/backend/access/gist/gistsplit.c
@@ -8,7 +8,7 @@
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/access/gist/gistsplit.c,v 1.8 2009/04/06 14:27:27 teodor Exp $
+ *	  $PostgreSQL: pgsql/src/backend/access/gist/gistsplit.c,v 1.9 2009/06/10 20:02:15 petere Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -369,9 +369,9 @@ gistUserPicksplit(Relation r, GistEntryVector *entryvec, int attno, GistSplitVec
 	{
 		ereport(DEBUG1,
 				(errcode(ERRCODE_INTERNAL_ERROR),
-				 errmsg("Picksplit method for %d column of index \"%s\" failed",
+				 errmsg("picksplit method for %d column of index \"%s\" failed",
 											attno+1, RelationGetRelationName(r)),
-				 errhint("Index is not optimal, to optimize it contact developer or try to use the column as a second one in create index command")));
+				 errhint("The index is not optimal. To optimize it, contact a developer, or try to use the column as the second one in the CREATE INDEX command.")));
 
 		/*
 		 * Reinit GIST_SPLITVEC. Although that fields are not used
