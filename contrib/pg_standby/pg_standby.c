8fd733bd19b2a0a8cde5a9e9a247ef361f167387
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2009-06-25 12:03:11 +0000

    Disable pg_standby -l option because the backend doesn't expect the recovered file to be a symlink. We tried to fix this issue with an earlier server-side patch, but it didn't fix the whole issue.
    The same bug is present in older releases as well, but the 8.4 train is
    about to leave the station, and I'm not sure if have consensus on whether
    we can remove the -l option in back-branches or do we need to attempt a
    server-side fix to make symlinking safe.
    
    Patch by Simon Riggs, per discussion on bug identified by Fujii Masao.

--- contrib/pg_standby/pg_standby.c
+++ contrib/pg_standby/pg_standby.c
@@ -1,5 +1,5 @@
 /*
- * $PostgreSQL: pgsql/contrib/pg_standby/pg_standby.c,v 1.24 2009/06/18 10:08:08 heikki Exp $
+ * $PostgreSQL: pgsql/contrib/pg_standby/pg_standby.c,v 1.25 2009/06/25 12:03:10 heikki Exp $
  *
  *
  * pg_standby.c
@@ -523,7 +523,7 @@ usage(void)
 	printf("  -d                 generate lots of debugging output (testing only)\n");
 	printf("  -k NUMFILESTOKEEP  if RESTARTWALFILE not used, removes files prior to limit\n"
 		   "                     (0 keeps all)\n");
-	printf("  -l                 links into archive (leaves file in archive)\n");
+	printf("  -l                 does nothing; use of link is now deprecated\n");
 	printf("  -r MAXRETRIES      max number of times to retry, with progressive wait\n"
 		   "                     (default=3)\n");
 	printf("  -s SLEEPTIME       seconds to wait between file checks (min=1, max=60,\n"
@@ -610,7 +610,12 @@ main(int argc, char **argv)
 				}
 				break;
 			case 'l':			/* Use link */
-				restoreCommandType = RESTORE_COMMAND_LINK;
+				/*
+				 * Link feature disabled, possibly permanently. Linking
+				 * causes a problem after recovery ends that is not currently
+				 * resolved by PostgreSQL. 25 Jun 2009
+					restoreCommandType = RESTORE_COMMAND_LINK;
+				*/
 				break;
 			case 'r':			/* Retries */
 				maxretries = atoi(optarg);
