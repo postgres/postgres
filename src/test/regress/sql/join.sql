d7bc5e307714b5643a4e5ceb9d5a449ffa0b7213
Tom Lane <tgl@sss.pgh.pa.us>
2009-07-17 23:20:15 +0000

    Repair bug #4926 "too few pathkeys for mergeclauses".  This example shows that the sanity checking I added to create_mergejoin_plan() in 8.3 was a few bricks shy of a load: the mergeclauses could reference pathkeys in a noncanonical order such as x,y,x, not only cases like x,x,y which is all that the code had allowed for.  The odd cases only turn up when using redundant clauses in an outer join condition, which is why no one had noticed before.

--- src/test/regress/sql/join.sql
+++ src/test/regress/sql/join.sql
@@ -495,3 +495,20 @@ select a.unique2, a.ten, b.tenthous, b.unique2, b.hundred
 from tenk1 a left join tenk1 b on a.unique2 = b.tenthous
 where a.unique1 = 42 and
       ((b.unique2 is null and a.ten = 2) or b.hundred = 3);
+
+--
+-- test for sane behavior with noncanonical merge clauses, per bug #4926
+--
+
+begin;
+
+set enable_mergejoin = 1;
+set enable_hashjoin = 0;
+set enable_nestloop = 0;
+
+create temp table a (i integer);
+create temp table b (x integer, y integer);
+
+select * from a left join b on i = x and i = y and x = i;
+
+rollback;
