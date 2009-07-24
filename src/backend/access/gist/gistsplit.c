4b284aff655f6150cf4791151c249d33b9bb3c02
Peter Eisentraut <peter_e@gmx.net>
2009-07-24 19:54:25 +0000

    Fix the fix for the gist error message

--- src/backend/access/gist/gistsplit.c
+++ src/backend/access/gist/gistsplit.c
@@ -8,7 +8,7 @@
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/access/gist/gistsplit.c,v 1.3.2.2 2009/06/10 19:59:17 petere Exp $
+ *	  $PostgreSQL: pgsql/src/backend/access/gist/gistsplit.c,v 1.3.2.3 2009/07/24 19:54:25 petere Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -368,7 +368,7 @@ gistUserPicksplit(Relation r, GistEntryVector *entryvec, int attno, GistSplitVec
 	{
 		ereport(DEBUG1,
 				(errcode(ERRCODE_INTERNAL_ERROR),
-				 errmsg("picksplit method for %d column of index \"%s\" failed",
+				 errmsg("picksplit method for column %d of index \"%s\" failed",
 											attno+1, RelationGetRelationName(r)),
 				 errhint("The index is not optimal. To optimize it, contact a developer, or try to use the column as the second one in the CREATE INDEX command.")));
 
