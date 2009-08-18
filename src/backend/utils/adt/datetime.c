ed398d1957835d71f9135ec6161036a3174a394f
Tom Lane <tgl@sss.pgh.pa.us>
2009-08-18 21:23:42 +0000

    Fix overflow for INTERVAL 'x ms' where x is more than a couple million, and integer datetimes are in use.  Per bug report from Hubert Depesz Lubaczewski.
    Alex Hunsaker

--- src/backend/utils/adt/datetime.c
+++ src/backend/utils/adt/datetime.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/utils/adt/datetime.c,v 1.160.2.9 2009/05/01 19:29:27 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/utils/adt/datetime.c,v 1.160.2.10 2009/08/18 21:23:42 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -3135,6 +3135,9 @@ DecodeInterval(char **field, int *ftype, int nf, int *dtype, struct pg_tm * tm, 
 						break;
 
 					case DTK_MILLISEC:
+						/* avoid overflowing the fsec field */
+						tm->tm_sec += val / 1000;
+						val -= (val / 1000) * 1000;
 #ifdef HAVE_INT64_TIMESTAMP
 						*fsec += (val + fval) * 1000;
 #else
