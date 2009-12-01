d85cb27293a2bafb2f6f8c5d965cc8af33c4bf48
Bruce Momjian <bruce@momjian.us>
2009-12-01 01:08:46 +0000

    ProcessUtility_hook:
    Add ProcessUtility_hook() to handle all DDL to contrib/pg_stat_statements.
    
    Itagaki Takahiro

--- src/include/tcop/utility.h
+++ src/include/tcop/utility.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2009, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/tcop/utility.h,v 1.35 2009/01/01 17:24:01 momjian Exp $
+ * $PostgreSQL: pgsql/src/include/tcop/utility.h,v 1.36 2009/12/01 01:08:46 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -17,7 +17,16 @@
 #include "tcop/tcopprot.h"
 
 
+/* Hook for plugins to get control in ProcessUtility() */
+typedef void (*ProcessUtility_hook_type) (Node *parsetree,
+			   const char *queryString, ParamListInfo params, bool isTopLevel,
+			   DestReceiver *dest, char *completionTag);
+extern PGDLLIMPORT ProcessUtility_hook_type ProcessUtility_hook;
+
 extern void ProcessUtility(Node *parsetree, const char *queryString,
+			   ParamListInfo params, bool isTopLevel,
+			   DestReceiver *dest, char *completionTag);
+extern void standard_ProcessUtility(Node *parsetree, const char *queryString,
 			   ParamListInfo params, bool isTopLevel,
 			   DestReceiver *dest, char *completionTag);
 
