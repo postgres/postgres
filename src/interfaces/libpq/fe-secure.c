d724237551b2b43c9c6f217de44bf210b0f50fdd
Magnus Hagander <magnus@hagander.net>
2009-12-09 06:37:25 +0000

    Reject certificates with embedded NULLs in the commonName field. This stops attacks where an attacker would put <attack>\0<propername> in the field and trick the validation code that the certificate was for <attack>.
    This is a very low risk attack since it reuqires the attacker to trick the
    CA into issuing a certificate with an incorrect field, and the common
    PostgreSQL deployments are with private CAs, and not external ones. Also,
    default mode in 8.4 does not do any name validation, and is thus also not
    vulnerable - but the higher security modes are.
    
    Backpatch all the way. Even though versions 8.3.x and before didn't have
    certificate name validation support, they still exposed this field for
    the user to perform the validation in the application code, and there
    is no way to detect this problem through that API.
    
    Security: CVE-2009-4034

--- src/interfaces/libpq/fe-secure.c
+++ src/interfaces/libpq/fe-secure.c
@@ -11,7 +11,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/interfaces/libpq/fe-secure.c,v 1.102.2.1 2009/01/28 15:06:57 mha Exp $
+ *	  $PostgreSQL: pgsql/src/interfaces/libpq/fe-secure.c,v 1.102.2.2 2009/12/09 06:37:25 mha Exp $
  *
  * NOTES
  *	  [ Most of these notes are wrong/obsolete, but perhaps not all ]
@@ -1087,9 +1087,28 @@ open_client_SSL(PGconn *conn)
 					  conn->peer_dn, sizeof(conn->peer_dn));
 	conn->peer_dn[sizeof(conn->peer_dn) - 1] = '\0';
 
-	X509_NAME_get_text_by_NID(X509_get_subject_name(conn->peer),
+	r = X509_NAME_get_text_by_NID(X509_get_subject_name(conn->peer),
 							  NID_commonName, conn->peer_cn, SM_USER);
-	conn->peer_cn[SM_USER] = '\0';
+	conn->peer_cn[SM_USER] = '\0'; /* buffer is SM_USER+1 chars! */
+	if (r == -1)
+	{
+		/* Unable to get the CN, set it to blank so it can't be used */
+		conn->peer_cn[0] = '\0';
+	}
+	else
+	{
+		/*
+		 * Reject embedded NULLs in certificate common name to prevent attacks like
+		 * CVE-2009-4034.
+		 */
+		if (r != strlen(conn->peer_cn))
+		{
+			printfPQExpBuffer(&conn->errorMessage,
+							  libpq_gettext("SSL certificate's common name contains embedded null\n"));
+			close_SSL(conn);
+			return PGRES_POLLING_FAILED;
+		}
+	}
 
 	/* verify that the common name resolves to peer */
 
