baab7a0427a05c2726c39c130693f6a7147ea565
Peter Eisentraut <peter_e@gmx.net>
2009-12-19 20:23:26 +0000

    Translation updates

--- src/bin/pg_dump/nls.mk
+++ src/bin/pg_dump/nls.mk
@@ -1,6 +1,6 @@
-# $PostgreSQL: pgsql/src/bin/pg_dump/nls.mk,v 1.21 2009/06/26 19:33:50 petere Exp $
+# $PostgreSQL: pgsql/src/bin/pg_dump/nls.mk,v 1.22 2009/12/19 20:23:25 petere Exp $
 CATALOG_NAME	:= pg_dump
-AVAIL_LANGUAGES	:= de es fr ja pt_BR sv tr
+AVAIL_LANGUAGES	:= de es fr it ja pt_BR sv tr
 GETTEXT_FILES	:= pg_dump.c common.c pg_backup_archiver.c pg_backup_custom.c \
                    pg_backup_db.c pg_backup_files.c pg_backup_null.c \
                    pg_backup_tar.c pg_restore.c pg_dumpall.c \
