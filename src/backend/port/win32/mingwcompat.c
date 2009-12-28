d2225da79fd6ab40d3416621c4c82bbe64a7fa37
Bruce Momjian <bruce@momjian.us>
2009-12-28 18:09:44 +0000

    Remove non-ascii characters from source code.

--- src/backend/port/win32/mingwcompat.c
+++ src/backend/port/win32/mingwcompat.c
@@ -6,7 +6,7 @@
  * Portions Copyright (c) 1996-2009, PostgreSQL Global Development Group
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/port/win32/mingwcompat.c,v 1.6 2009/06/11 14:49:00 momjian Exp $
+ *	  $PostgreSQL: pgsql/src/backend/port/win32/mingwcompat.c,v 1.7 2009/12/28 18:09:44 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -42,7 +42,7 @@ LoadKernel32()
 
 /*
  * Replacement for RegisterWaitForSingleObject(), which lives in
- * kernel32.dll·
+ * kernel32.dll
  */
 typedef
 BOOL		(WINAPI * __RegisterWaitForSingleObject)
