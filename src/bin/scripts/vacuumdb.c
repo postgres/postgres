36f6b7ca38ccadffc654745780344ae77796e038
Bruce Momjian <bruce@momjian.us>
2010-01-06 16:04:05 +0000

    Corrected CVS entry:
    --only-analyze mode is for _vacuumdb_, not pg_dump.

--- src/bin/scripts/vacuumdb.c
+++ src/bin/scripts/vacuumdb.c
@@ -5,7 +5,7 @@
  * Portions Copyright (c) 1996-2010, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/bin/scripts/vacuumdb.c,v 1.30 2010/01/06 05:31:14 itagaki Exp $
+ * $PostgreSQL: pgsql/src/bin/scripts/vacuumdb.c,v 1.31 2010/01/06 16:04:05 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -169,7 +169,7 @@ main(int argc, char *argv[])
 					progname);
 			exit(1);
 		}
-		/* ignore 'and_analyze' */
+		/* allow 'and_analyze' with 'only_analyze' */
 	}
 
 	setup_cancel_handler();
