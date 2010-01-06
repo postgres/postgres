2391396b3d9304765aea50d220180892b6937fb6
Bruce Momjian <bruce@momjian.us>
2010-01-06 03:07:24 +0000

    Update catalog version for recent relfilenode patch, so pg_migrator can identify the new API.

--- src/include/catalog/catversion.h
+++ src/include/catalog/catversion.h
@@ -37,7 +37,7 @@
  * Portions Copyright (c) 1996-2010, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/catalog/catversion.h,v 1.567 2010/01/05 21:53:59 rhaas Exp $
+ * $PostgreSQL: pgsql/src/include/catalog/catversion.h,v 1.568 2010/01/06 03:07:24 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -53,6 +53,6 @@
  */
 
 /*							yyyymmddN */
-#define CATALOG_VERSION_NO	201001051
+#define CATALOG_VERSION_NO	201001052
 
 #endif
