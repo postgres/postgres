c9dc53be77664952ebd3b50a1e920e92306484de
Tom Lane <tgl@sss.pgh.pa.us>
2010-01-16 05:52:29 +0000

    Huh, apparently on cygwin we HAVE_SIGPROCMASK, so both variants of the BlockSig/UnBlockSig declaration have to be PGDLLIMPORT'ified. Per buildfarm results.

--- src/include/libpq/pqsignal.h
+++ src/include/libpq/pqsignal.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2010, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/libpq/pqsignal.h,v 1.38 2010/01/16 00:04:41 tgl Exp $
+ * $PostgreSQL: pgsql/src/include/libpq/pqsignal.h,v 1.39 2010/01/16 05:52:29 tgl Exp $
  *
  * NOTES
  *	  This shouldn't be in libpq, but the monitor and some other
@@ -21,7 +21,7 @@
 #include <signal.h>
 
 #ifdef HAVE_SIGPROCMASK
-extern sigset_t UnBlockSig,
+extern PGDLLIMPORT sigset_t UnBlockSig,
 			BlockSig,
 			StartupBlockSig;
 
