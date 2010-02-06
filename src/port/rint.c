7fc30c488fc6e9674564206193c29b1a657a818f
Tom Lane <tgl@sss.pgh.pa.us>
2010-02-06 05:42:49 +0000

    Avoid changing the sign of zero.  Per buildfarm failures.

--- src/port/rint.c
+++ src/port/rint.c
@@ -4,16 +4,16 @@
  *	  rint() implementation
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/port/rint.c,v 1.3 2010/02/05 03:20:56 momjian Exp $
+ *	  $PostgreSQL: pgsql/src/port/rint.c,v 1.4 2010/02/06 05:42:49 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
-
 #include "c.h"
+
 #include <math.h>
 
 double
 rint(double x)
 {
-	return (x > 0.0) ? floor(x + 0.5) : ceil(x - 0.5);
+	return (x >= 0.0) ? floor(x + 0.5) : ceil(x - 0.5);
 }
