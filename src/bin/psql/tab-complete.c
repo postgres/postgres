714f279457de23cbc81efc6961f4b5d611723db6
Itagaki Takahiro <itagaki.takahiro@gmail.com>
2010-02-15 02:55:01 +0000

    Add psql tab completion for DO blocks. Also adjust documentation of DO.
    Patch from David Fetter and subsequent discussion.

--- src/bin/psql/tab-complete.c
+++ src/bin/psql/tab-complete.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 2000-2010, PostgreSQL Global Development Group
  *
- * $PostgreSQL: pgsql/src/bin/psql/tab-complete.c,v 1.192 2010/01/25 18:23:09 heikki Exp $
+ * $PostgreSQL: pgsql/src/bin/psql/tab-complete.c,v 1.193 2010/02/15 02:55:01 itagaki Exp $
  */
 
 /*----------------------------------------------------------------------
@@ -618,7 +618,7 @@ psql_completion(char *text, int start, int end)
 	static const char *const sql_commands[] = {
 		"ABORT", "ALTER", "ANALYZE", "BEGIN", "CHECKPOINT", "CLOSE", "CLUSTER",
 		"COMMENT", "COMMIT", "COPY", "CREATE", "DEALLOCATE", "DECLARE",
-		"DELETE FROM", "DISCARD", "DROP", "END", "EXECUTE", "EXPLAIN", "FETCH",
+		"DELETE FROM", "DISCARD", "DO", "DROP", "END", "EXECUTE", "EXPLAIN", "FETCH",
 		"GRANT", "INSERT", "LISTEN", "LOAD", "LOCK", "MOVE", "NOTIFY", "PREPARE",
 		"REASSIGN", "REINDEX", "RELEASE", "RESET", "REVOKE", "ROLLBACK",
 		"SAVEPOINT", "SELECT", "SET", "SHOW", "START", "TABLE", "TRUNCATE", "UNLISTEN",
@@ -1530,6 +1530,18 @@ psql_completion(char *text, int start, int end)
 		{"ALL", "PLANS", "TEMP", NULL};
 
 		COMPLETE_WITH_LIST(list_DISCARD);
+	}
+
+/* DO */
+	/*
+	 * Complete DO with LANGUAGE.
+	 */
+	else if (pg_strcasecmp(prev_wd, "DO") == 0)
+	{
+		static const char *const list_DO[] =
+		{"LANGUAGE", NULL};
+
+		COMPLETE_WITH_LIST(list_DO);
 	}
 
 /* DROP (when not the previous word) */
