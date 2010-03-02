96ae23a8b34fd129181c41f8715b7ab491f0a7b9
Andrew Dunstan <andrew@dunslane.net>
2010-03-02 15:44:03 +0000

    Backpatch MSVC build fix for XSLT

--- src/tools/msvc/Solution.pm
+++ src/tools/msvc/Solution.pm
@@ -3,7 +3,7 @@ package Solution;
 #
 # Package that encapsulates a Visual C++ solution file generation
 #
-# $PostgreSQL: pgsql/src/tools/msvc/Solution.pm,v 1.47 2009/01/06 18:37:50 mha Exp $
+# $PostgreSQL: pgsql/src/tools/msvc/Solution.pm,v 1.47.2.1 2010/03/02 15:44:03 adunstan Exp $
 #
 use Carp;
 use strict;
@@ -173,6 +173,11 @@ s{PG_VERSION_STR "[^"]+"}{__STRINGIFY(x) #x\n#define __STRINGIFY2(z) __STRINGIFY
         {
             print O "#define HAVE_LIBXML2\n";
             print O "#define USE_LIBXML\n";
+        }
+        if ($self->{options}->{xslt})
+        {
+            print O "#define HAVE_LIBXSLT\n";
+            print O "#define USE_LIBXSLT\n";
         }
         if ($self->{options}->{krb5})
         {
