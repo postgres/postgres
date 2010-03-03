b4631ca0bb91fb9a3681e9f2460392dbe57d6112
Andrew Dunstan <andrew@dunslane.net>
2010-03-03 03:29:02 +0000

    Make iconv work like other optional libraries for MSVC.

--- src/tools/msvc/Solution.pm
+++ src/tools/msvc/Solution.pm
@@ -3,7 +3,7 @@ package Solution;
 #
 # Package that encapsulates a Visual C++ solution file generation
 #
-# $PostgreSQL: pgsql/src/tools/msvc/Solution.pm,v 1.47.2.2 2010/03/02 23:51:17 adunstan Exp $
+# $PostgreSQL: pgsql/src/tools/msvc/Solution.pm,v 1.47.2.3 2010/03/03 03:29:02 adunstan Exp $
 #
 use Carp;
 use strict;
@@ -378,10 +378,14 @@ sub AddProject
         $proj->AddLibrary($self->{options}->{krb5} . '\lib\i386\comerr32.lib');
         $proj->AddLibrary($self->{options}->{krb5} . '\lib\i386\gssapi32.lib');
     }
+    if ($self->{options}->{iconv})
+    {
+        $proj->AddIncludeDir($self->{options}->{iconv} . '\include');
+        $proj->AddLibrary($self->{options}->{iconv} . '\lib\iconv.lib');
+	}
     if ($self->{options}->{xml})
     {
         $proj->AddIncludeDir($self->{options}->{xml} . '\include');
-        $proj->AddIncludeDir($self->{options}->{iconv} . '\include');
         $proj->AddLibrary($self->{options}->{xml} . '\lib\libxml2.lib');
     }
 	if ($self->{options}->{xslt})
