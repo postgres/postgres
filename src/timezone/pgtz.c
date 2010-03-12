d75f7a01bf646b3ebe71688c31ffe56fc6fde9a8
Tom Lane <tgl@sss.pgh.pa.us>
2010-03-12 21:40:36 +0000

    Fix a commented-out block of code that pgindent didn't understand properly; it's been making that comment uglier with each run.

--- src/timezone/pgtz.c
+++ src/timezone/pgtz.c
@@ -6,7 +6,7 @@
  * Portions Copyright (c) 1996-2010, PostgreSQL Global Development Group
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/timezone/pgtz.c,v 1.66 2010/02/26 02:01:39 momjian Exp $
+ *	  $PostgreSQL: pgsql/src/timezone/pgtz.c,v 1.67 2010/03/12 21:40:36 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -974,9 +974,13 @@ static const struct
 		"W. Australia Standard Time", "W. Australia Daylight Time",
 		"Australia/Perth"
 	},							/* (GMT+08:00) Perth */
-/*	{"W. Central Africa Standard Time", "W. Central Africa Daylight Time",
-	 *	 *	 *	 *	 *	 *	 *	 *	 *	""}, Could not find a match for this one. Excluded for now. *//* (
-	 * G MT+01:00) West Central Africa */
+#ifdef NOT_USED
+	/* Could not find a match for this one. Excluded for now. */
+	{
+		"W. Central Africa Standard Time", "W. Central Africa Daylight Time",
+		""
+	},							/* (GMT+01:00) West Central Africa */
+#endif
 	{
 		"W. Europe Standard Time", "W. Europe Daylight Time",
 		"CET"
