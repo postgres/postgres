45d9d90c83a577a1b241369ccc1a49243619d14c
Bruce Momjian <bruce@momjian.us>
2010-03-13 15:35:46 +0000

    Move TCP keepalive macro definition
    Jaime Casanova

--- src/backend/libpq/pqcomm.c
+++ src/backend/libpq/pqcomm.c
@@ -30,7 +30,7 @@
  * Portions Copyright (c) 1996-2010, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- *	$PostgreSQL: pgsql/src/backend/libpq/pqcomm.c,v 1.205 2010/02/26 02:00:43 momjian Exp $
+ *	$PostgreSQL: pgsql/src/backend/libpq/pqcomm.c,v 1.206 2010/03/13 15:35:46 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -1418,10 +1418,10 @@ pq_getkeepalivesinterval(Port *port)
 int
 pq_setkeepalivesinterval(int interval, Port *port)
 {
+#ifdef TCP_KEEPINTVL
 	if (port == NULL || IS_AF_UNIX(port->laddr.addr.ss_family))
 		return STATUS_OK;
 
-#ifdef TCP_KEEPINTVL
 	if (interval == port->keepalives_interval)
 		return STATUS_OK;
 
