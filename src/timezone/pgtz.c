f93f91393847260c807aa59b90a7721a57c74e2d
Magnus Hagander <magnus@hagander.net>
2010-04-08 11:26:06 +0000

    Proceed to look for the next timezone when matching a localized Windows timezone name where the information in the registry is incomplete, instead of aborting.
    This fixes cases when the registry information is incomplete for
    a timezone that is alphabetically before the one that is in use.
    
    Per report from Alexander Forschner

--- src/timezone/pgtz.c
+++ src/timezone/pgtz.c
@@ -6,7 +6,7 @@
  * Portions Copyright (c) 1996-2009, PostgreSQL Global Development Group
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/timezone/pgtz.c,v 1.63.2.1 2010/04/06 20:35:17 mha Exp $
+ *	  $PostgreSQL: pgsql/src/timezone/pgtz.c,v 1.63.2.2 2010/04/08 11:26:06 mha Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -1090,7 +1090,7 @@ identify_system_timezone(void)
 					(errmsg_internal("could not query value for 'std' to identify Windows timezone \"%s\": %i",
 									 keyname, (int) r)));
 			RegCloseKey(key);
-			break;
+			continue; /* Proceed to look at the next timezone */
 		}
 		if (strcmp(tzname, zonename) == 0)
 		{
@@ -1107,7 +1107,7 @@ identify_system_timezone(void)
 					(errmsg_internal("could not query value for 'dlt' to identify Windows timezone \"%s\": %i",
 									 keyname, (int) r)));
 			RegCloseKey(key);
-			break;
+			continue; /* Proceed to look at the next timezone */
 		}
 		if (strcmp(tzname, zonename) == 0)
 		{
