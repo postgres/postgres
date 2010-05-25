238d21d7d28612bfebc2989ba649a1b137c29cbe
Bruce Momjian <bruce@momjian.us>
2010-05-25 15:55:28 +0000

    Modify pg_standby, pgbench, and pg_upgrade manual pages to be consistent in their display of command-line options with other client applications.

--- contrib/pg_upgrade/option.c
+++ contrib/pg_upgrade/option.c
@@ -226,21 +226,21 @@ usage(migratorContext *ctx)
 	printf(_("\nUsage: pg_upgrade [OPTIONS]...\n\
 \n\
 Options:\n\
- -b, --old-bindir=OLDBINDIR      old cluster executable directory\n\
- -B, --new-bindir=NEWBINDIR      new cluster executable directory\n\
- -c, --check                     check clusters only, don't change any data\n\
- -d, --old-datadir=OLDDATADIR    old cluster data directory\n\
- -D, --new-datadir=NEWDATADIR    new cluster data directory\n\
- -g, --debug                     enable debugging\n\
- -G, --debugfile=DEBUGFILENAME   output debugging activity to file\n\
- -k, --link                      link instead of copying files to new cluster\n\
- -l, --logfile=LOGFILENAME       log session activity to file\n\
- -p, --old-port=portnum          old cluster port number (default %d)\n\
- -P, --new-port=portnum          new cluster port number (default %d)\n\
- -u, --user=username             clusters superuser (default \"%s\")\n\
- -v, --verbose                   enable verbose output\n\
- -V, --version                   display version information, then exit\n\
- -h, --help                      show this help, then exit\n\
+ -b, --old-bindir=old_bindir      old cluster executable directory\n\
+ -B, --new-bindir=new_bindir      new cluster executable directory\n\
+ -c, --check                      check clusters only, don't change any data\n\
+ -d, --old-datadir=old_datadir    old cluster data directory\n\
+ -D, --new-datadir=new_datadir    new cluster data directory\n\
+ -g, --debug                      enable debugging\n\
+ -G, --debugfile=debug_filename   output debugging activity to file\n\
+ -k, --link                       link instead of copying files to new cluster\n\
+ -l, --logfile=log_filename       log session activity to file\n\
+ -p, --old-port=old_portnum       old cluster port number (default %d)\n\
+ -P, --new-port=new_portnum       new cluster port number (default %d)\n\
+ -u, --user=username              clusters superuser (default \"%s\")\n\
+ -v, --verbose                    enable verbose output\n\
+ -V, --version                    display version information, then exit\n\
+ -h, --help                       show this help, then exit\n\
 \n\
 Before running pg_upgrade you must:\n\
   create a new database cluster (using the new version of initdb)\n\
