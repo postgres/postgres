8ce3f26fab743be87993ce9f688ac150a34d554c
Robert Haas <rhaas@postgresql.org>
2010-06-22 11:36:36 +0000

    Deprecate the use of => as an operator name.
    In HEAD, emit a warning when an operator named => is defined.
    In both HEAD and the backbranches (except in 8.2, where contrib
    modules do not have documentation), document that hstore's text =>
    text operator may be removed in a future release, and encourage the
    use of the hstore(text, text) function instead.  This function only
    exists in HEAD (previously, it was called tconvert), so backpatch
    it back to 8.2, when hstore was added.  Per discussion.

--- contrib/hstore/uninstall_hstore.sql
+++ contrib/hstore/uninstall_hstore.sql
@@ -1,4 +1,4 @@
-/* $PostgreSQL: pgsql/contrib/hstore/uninstall_hstore.sql,v 1.6 2007/11/13 04:24:28 momjian Exp $ */
+/* $PostgreSQL: pgsql/contrib/hstore/uninstall_hstore.sql,v 1.6.2.1 2010/06/22 11:36:36 rhaas Exp $ */
 
 -- Adjust this setting to control where the objects get dropped.
 SET search_path = public;
@@ -26,6 +26,7 @@ DROP FUNCTION hs_concat(hstore,hstore);
 DROP FUNCTION hs_contains(hstore,hstore);
 DROP FUNCTION hs_contained(hstore,hstore);
 DROP FUNCTION tconvert(text,text);
+DROP FUNCTION hstore(text,text);
 DROP FUNCTION akeys(hstore);
 DROP FUNCTION avals(hstore);
 DROP FUNCTION skeys(hstore);
