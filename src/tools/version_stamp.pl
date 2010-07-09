b40466c33731eed31e08f116487259d800eb1891
Tom Lane <tgl@sss.pgh.pa.us>
2010-07-09 04:10:58 +0000

    Stamp HEAD as 9.1devel. (And there was much rejoicing.)

--- src/tools/version_stamp.pl
+++ src/tools/version_stamp.pl
@@ -5,7 +5,7 @@
 #
 # Copyright (c) 2008-2010, PostgreSQL Global Development Group
 #
-# $PostgreSQL: pgsql/src/tools/version_stamp.pl,v 1.6 2010/02/17 04:19:41 tgl Exp $
+# $PostgreSQL: pgsql/src/tools/version_stamp.pl,v 1.7 2010/07/09 04:10:58 tgl Exp $
 #################################################################
 
 #
@@ -23,7 +23,7 @@
 # Major version is hard-wired into the script.  We update it when we branch
 # a new development version.
 $major1 = 9;
-$major2 = 0;
+$major2 = 1;
 
 # Validate argument and compute derived variables
 $minor = shift;
