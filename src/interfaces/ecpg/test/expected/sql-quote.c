b25749cc64995ae668f6744765169b354a7f43be
Robert Haas <rhaas@postgresql.org>
2010-07-20 01:57:19 +0000

    Make ECPG regression tests independent of standard_conforming_strings.
    Per buildfarm, again.

--- src/interfaces/ecpg/test/expected/sql-quote.c
+++ src/interfaces/ecpg/test/expected/sql-quote.c
@@ -61,9 +61,7 @@ if (sqlca.sqlcode < 0) sqlprint();}
 #line 20 "quote.pgc"
 
 
-  { ECPGdo(__LINE__, 0, 1, NULL, 0, ECPGst_normal, "show standard_conforming_strings", ECPGt_EOIT, 
-	ECPGt_char,(var),(long)25,(long)1,(25)*sizeof(char), 
-	ECPGt_NO_INDICATOR, NULL , 0L, 0L, 0L, ECPGt_EORT);
+  { ECPGdo(__LINE__, 0, 1, NULL, 0, ECPGst_normal, "set standard_conforming_strings to off", ECPGt_EOIT, ECPGt_EORT);
 #line 22 "quote.pgc"
 
 if (sqlca.sqlwarn[0] == 'W') sqlprint();
@@ -71,86 +69,111 @@ if (sqlca.sqlwarn[0] == 'W') sqlprint();
 
 if (sqlca.sqlcode < 0) sqlprint();}
 #line 22 "quote.pgc"
+
+
+  { ECPGdo(__LINE__, 0, 1, NULL, 0, ECPGst_normal, "show standard_conforming_strings", ECPGt_EOIT, 
+	ECPGt_char,(var),(long)25,(long)1,(25)*sizeof(char), 
+	ECPGt_NO_INDICATOR, NULL , 0L, 0L, 0L, ECPGt_EORT);
+#line 24 "quote.pgc"
+
+if (sqlca.sqlwarn[0] == 'W') sqlprint();
+#line 24 "quote.pgc"
+
+if (sqlca.sqlcode < 0) sqlprint();}
+#line 24 "quote.pgc"
 
   printf("Standard conforming strings: %s\n", var);
 
   /* this is a\\b actually */
   { ECPGdo(__LINE__, 0, 1, NULL, 0, ECPGst_normal, "insert into \"My_Table\" values ( 1 , 'a\\\\\\\\b' )", ECPGt_EOIT, ECPGt_EORT);
-#line 26 "quote.pgc"
+#line 28 "quote.pgc"
 
 if (sqlca.sqlwarn[0] == 'W') sqlprint();
-#line 26 "quote.pgc"
+#line 28 "quote.pgc"
 
 if (sqlca.sqlcode < 0) sqlprint();}
-#line 26 "quote.pgc"
+#line 28 "quote.pgc"
 
   /* this is a\\b */
   { ECPGdo(__LINE__, 0, 1, NULL, 0, ECPGst_normal, "insert into \"My_Table\" values ( 1 , E'a\\\\\\\\b' )", ECPGt_EOIT, ECPGt_EORT);
-#line 28 "quote.pgc"
+#line 30 "quote.pgc"
 
 if (sqlca.sqlwarn[0] == 'W') sqlprint();
-#line 28 "quote.pgc"
+#line 30 "quote.pgc"
 
 if (sqlca.sqlcode < 0) sqlprint();}
-#line 28 "quote.pgc"
+#line 30 "quote.pgc"
 
 
   { ECPGdo(__LINE__, 0, 1, NULL, 0, ECPGst_normal, "set standard_conforming_strings to on", ECPGt_EOIT, ECPGt_EORT);
-#line 30 "quote.pgc"
+#line 32 "quote.pgc"
 
 if (sqlca.sqlwarn[0] == 'W') sqlprint();
-#line 30 "quote.pgc"
+#line 32 "quote.pgc"
 
 if (sqlca.sqlcode < 0) sqlprint();}
-#line 30 "quote.pgc"
+#line 32 "quote.pgc"
 
+
+  { ECPGdo(__LINE__, 0, 1, NULL, 0, ECPGst_normal, "show standard_conforming_strings", ECPGt_EOIT, 
+	ECPGt_char,(var),(long)25,(long)1,(25)*sizeof(char), 
+	ECPGt_NO_INDICATOR, NULL , 0L, 0L, 0L, ECPGt_EORT);
+#line 34 "quote.pgc"
+
+if (sqlca.sqlwarn[0] == 'W') sqlprint();
+#line 34 "quote.pgc"
+
+if (sqlca.sqlcode < 0) sqlprint();}
+#line 34 "quote.pgc"
+
+  printf("Standard conforming strings: %s\n", var);
 
   /* this is a\\\\b actually */
   { ECPGdo(__LINE__, 0, 1, NULL, 0, ECPGst_normal, "insert into \"My_Table\" values ( 2 , 'a\\\\\\\\b' )", ECPGt_EOIT, ECPGt_EORT);
-#line 33 "quote.pgc"
+#line 38 "quote.pgc"
 
 if (sqlca.sqlwarn[0] == 'W') sqlprint();
-#line 33 "quote.pgc"
+#line 38 "quote.pgc"
 
 if (sqlca.sqlcode < 0) sqlprint();}
-#line 33 "quote.pgc"
+#line 38 "quote.pgc"
 
   /* this is a\\b */
   { ECPGdo(__LINE__, 0, 1, NULL, 0, ECPGst_normal, "insert into \"My_Table\" values ( 2 , E'a\\\\\\\\b' )", ECPGt_EOIT, ECPGt_EORT);
-#line 35 "quote.pgc"
+#line 40 "quote.pgc"
 
 if (sqlca.sqlwarn[0] == 'W') sqlprint();
-#line 35 "quote.pgc"
+#line 40 "quote.pgc"
 
 if (sqlca.sqlcode < 0) sqlprint();}
-#line 35 "quote.pgc"
+#line 40 "quote.pgc"
 
 
   { ECPGtrans(__LINE__, NULL, "begin");
-#line 37 "quote.pgc"
+#line 42 "quote.pgc"
 
 if (sqlca.sqlwarn[0] == 'W') sqlprint();
-#line 37 "quote.pgc"
+#line 42 "quote.pgc"
 
 if (sqlca.sqlcode < 0) sqlprint();}
-#line 37 "quote.pgc"
+#line 42 "quote.pgc"
 
   /* declare C cursor for select * from \"My_Table\" */
-#line 38 "quote.pgc"
+#line 43 "quote.pgc"
 
 
   { ECPGdo(__LINE__, 0, 1, NULL, 0, ECPGst_normal, "declare C cursor for select * from \"My_Table\"", ECPGt_EOIT, ECPGt_EORT);
-#line 40 "quote.pgc"
+#line 45 "quote.pgc"
 
 if (sqlca.sqlwarn[0] == 'W') sqlprint();
-#line 40 "quote.pgc"
+#line 45 "quote.pgc"
 
 if (sqlca.sqlcode < 0) sqlprint();}
-#line 40 "quote.pgc"
+#line 45 "quote.pgc"
 
 
   /* exec sql whenever not found  break ; */
-#line 42 "quote.pgc"
+#line 47 "quote.pgc"
 
 
   while (true)
@@ -160,47 +183,47 @@ if (sqlca.sqlcode < 0) sqlprint();}
 	ECPGt_NO_INDICATOR, NULL , 0L, 0L, 0L, 
 	ECPGt_char,(var),(long)25,(long)1,(25)*sizeof(char), 
 	ECPGt_NO_INDICATOR, NULL , 0L, 0L, 0L, ECPGt_EORT);
-#line 46 "quote.pgc"
+#line 51 "quote.pgc"
 
 if (sqlca.sqlcode == ECPG_NOT_FOUND) break;
-#line 46 "quote.pgc"
+#line 51 "quote.pgc"
 
 if (sqlca.sqlwarn[0] == 'W') sqlprint();
-#line 46 "quote.pgc"
+#line 51 "quote.pgc"
 
 if (sqlca.sqlcode < 0) sqlprint();}
-#line 46 "quote.pgc"
+#line 51 "quote.pgc"
 
 	printf("value: %d %s\n", i, var);
   }
 
   { ECPGtrans(__LINE__, NULL, "rollback");
-#line 50 "quote.pgc"
+#line 55 "quote.pgc"
 
 if (sqlca.sqlwarn[0] == 'W') sqlprint();
-#line 50 "quote.pgc"
+#line 55 "quote.pgc"
 
 if (sqlca.sqlcode < 0) sqlprint();}
-#line 50 "quote.pgc"
+#line 55 "quote.pgc"
 
   { ECPGdo(__LINE__, 0, 1, NULL, 0, ECPGst_normal, "drop table \"My_Table\"", ECPGt_EOIT, ECPGt_EORT);
-#line 51 "quote.pgc"
+#line 56 "quote.pgc"
 
 if (sqlca.sqlwarn[0] == 'W') sqlprint();
-#line 51 "quote.pgc"
+#line 56 "quote.pgc"
 
 if (sqlca.sqlcode < 0) sqlprint();}
-#line 51 "quote.pgc"
+#line 56 "quote.pgc"
 
 
   { ECPGdisconnect(__LINE__, "ALL");
-#line 53 "quote.pgc"
+#line 58 "quote.pgc"
 
 if (sqlca.sqlwarn[0] == 'W') sqlprint();
-#line 53 "quote.pgc"
+#line 58 "quote.pgc"
 
 if (sqlca.sqlcode < 0) sqlprint();}
-#line 53 "quote.pgc"
+#line 58 "quote.pgc"
 
 
   return 0;
