733959b9614f7a274c4f4a51857c7120ddd5cea6
Robert Haas <rhaas@postgresql.org>
2010-07-23 00:43:17 +0000

    Avoid deep recursion when assigning XIDs to multiple levels of subxacts.
    Backpatch to 8.0.
    
    Andres Freund, with cleanup and adjustment for older branches by me.

--- src/backend/access/transam/xact.c
+++ src/backend/access/transam/xact.c
@@ -10,7 +10,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/access/transam/xact.c,v 1.274.2.3 2010/01/24 21:49:31 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/access/transam/xact.c,v 1.274.2.4 2010/07/23 00:43:17 rhaas Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -399,10 +399,32 @@ AssignTransactionId(TransactionState s)
 
 	/*
 	 * Ensure parent(s) have XIDs, so that a child always has an XID later
-	 * than its parent.
+	 * than its parent.  Musn't recurse here, or we might get a stack overflow
+	 * if we're at the bottom of a huge stack of subtransactions none of which
+	 * have XIDs yet.
 	 */
 	if (isSubXact && !TransactionIdIsValid(s->parent->transactionId))
-		AssignTransactionId(s->parent);
+	{
+		TransactionState	p = s->parent;
+		TransactionState   *parents;
+		size_t	parentOffset = 0;
+
+		parents = palloc(sizeof(TransactionState) *  s->nestingLevel);
+		while (p != NULL && !TransactionIdIsValid(p->transactionId))
+		{
+			parents[parentOffset++] = p;
+			p = p->parent;
+		}
+
+		/*
+		 * This is technically a recursive call, but the recursion will
+		 * never be more than one layer deep.
+		 */
+		while (parentOffset != 0)
+			AssignTransactionId(parents[--parentOffset]);
+
+		pfree(parents);
+	}
 
 	/*
 	 * Generate a new Xid and record it in PG_PROC and pg_subtrans.
