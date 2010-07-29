a6c243ed9c3b9686e802107d8365ac69a1d62949
Peter Eisentraut <peter_e@gmx.net>
2010-07-29 19:39:47 +0000

    Translation updates for 9.0beta4

--- src/pl/plpgsql/src/nls.mk
+++ src/pl/plpgsql/src/nls.mk
@@ -1,6 +1,6 @@
-# $PostgreSQL: pgsql/src/pl/plpgsql/src/nls.mk,v 1.12 2009/11/12 00:13:00 tgl Exp $
+# $PostgreSQL: pgsql/src/pl/plpgsql/src/nls.mk,v 1.12.8.1 2010/07/29 19:39:47 petere Exp $
 CATALOG_NAME	:= plpgsql
-AVAIL_LANGUAGES	:= de es fr it ja ro
+AVAIL_LANGUAGES	:= de es fr it ja pt_BR ro
 GETTEXT_FILES	:= pl_comp.c pl_exec.c pl_gram.c pl_funcs.c pl_handler.c pl_scanner.c
 GETTEXT_TRIGGERS:= _ errmsg errmsg_plural:1,2 errdetail errdetail_log errdetail_plural:1,2 errhint errcontext yyerror plpgsql_yyerror
 
