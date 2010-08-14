946b07804478d379d29b2b2038a5729b3cad2f55
Tom Lane <tgl@sss.pgh.pa.us>
2010-08-14 13:37:21 +0000

    MyBackendId now needs to be PGDLLIMPORT, so that contrib modules can access it on Windows.  Per buildfarm.

--- src/include/storage/backendid.h
+++ src/include/storage/backendid.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2010, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/storage/backendid.h,v 1.23 2010/01/02 16:58:08 momjian Exp $
+ * $PostgreSQL: pgsql/src/include/storage/backendid.h,v 1.24 2010/08/14 13:37:21 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -22,6 +22,6 @@ typedef int BackendId;			/* unique currently active backend identifier */
 
 #define InvalidBackendId		(-1)
 
-extern BackendId MyBackendId;	/* backend id of this backend */
+extern PGDLLIMPORT BackendId MyBackendId;	/* backend id of this backend */
 
 #endif   /* BACKENDID_H */
