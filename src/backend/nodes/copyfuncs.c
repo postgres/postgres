0321d031b51f35567947e4a478b157293c4a38c6
Tom Lane <tgl@sss.pgh.pa.us>
2010-08-18 15:22:00 +0000

    Add missing handling of PlannedStmt.transientPlan in copyfuncs/outfuncs.
    _outPlannedStmt is only debug support, so the omission there was not very
    serious, but the omission in _copyPlannedStmt is a real bug.  The consequence
    would be that a copied plan tree would never be marked as a transient plan,
    so that we would forget we ought to replan it after some not-yet-ready index
    becomes ready for use.  This might explain some past complaints about indexes
    created with CREATE INDEX CONCURRENTLY not being used right away.  Problem
    spotted by Yeb Havinga.
    
    Back-patch to 8.3, where the field was added.

--- src/backend/nodes/copyfuncs.c
+++ src/backend/nodes/copyfuncs.c
@@ -15,7 +15,7 @@
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/nodes/copyfuncs.c,v 1.464 2010/02/26 02:00:43 momjian Exp $
+ *	  $PostgreSQL: pgsql/src/backend/nodes/copyfuncs.c,v 1.464.4.1 2010/08/18 15:22:00 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -79,6 +79,7 @@ _copyPlannedStmt(PlannedStmt *from)
 	COPY_SCALAR_FIELD(commandType);
 	COPY_SCALAR_FIELD(hasReturning);
 	COPY_SCALAR_FIELD(canSetTag);
+	COPY_SCALAR_FIELD(transientPlan);
 	COPY_NODE_FIELD(planTree);
 	COPY_NODE_FIELD(rtable);
 	COPY_NODE_FIELD(resultRelations);
