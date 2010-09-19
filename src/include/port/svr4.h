9eef3318a2a36d462551dd526f8940e15f33b446
Tom Lane <tgl@sss.pgh.pa.us>
2010-09-19 16:17:45 +0000

    Fix several broken $PostgreSQL$ keywords.  Noted while experimenting with Magnus's script to remove these.

--- src/include/port/svr4.h
+++ src/include/port/svr4.h
@@ -1,3 +1,3 @@
-/* $PostgreSQL */
+/* $PostgreSQL: pgsql/src/include/port/svr4.h,v 1.15 2010/09/19 16:17:45 tgl Exp $ */
 
 /* nothing needed */
--- src/interfaces/ecpg/preproc/nls.mk
+++ src/interfaces/ecpg/preproc/nls.mk
@@ -1,4 +1,4 @@
-# $PostgreSQL $
+# $PostgreSQL: pgsql/src/interfaces/ecpg/preproc/nls.mk,v 1.7 2010/09/19 16:17:45 tgl Exp $
 CATALOG_NAME	 = ecpg
 AVAIL_LANGUAGES	 = de es fr it ja pt_BR tr zh_CN
 GETTEXT_FILES	 = descriptor.c ecpg.c pgc.c preproc.c type.c variable.c
