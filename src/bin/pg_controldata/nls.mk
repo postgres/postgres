1100d1eaffb26948d1080968ccaab57f698fc9d9
Peter Eisentraut <peter_e@gmx.net>
2010-09-30 23:31:19 +0300

    Translation updates for 8.4.5

--- src/bin/pg_controldata/nls.mk
+++ src/bin/pg_controldata/nls.mk
@@ -1,5 +1,5 @@
 # $PostgreSQL: pgsql/src/bin/pg_controldata/nls.mk,v 1.19.2.2 2010/05/13 10:50:00 petere Exp $
 CATALOG_NAME	:= pg_controldata
-AVAIL_LANGUAGES	:= de es fr it ja ko pt_BR sv ta tr zh_CN
+AVAIL_LANGUAGES	:= de es fr it ja ko pt_BR sv ta tr zh_CN zh_TW
 GETTEXT_FILES	:= pg_controldata.c
 GETTEXT_TRIGGERS:= _
--- src/bin/pg_ctl/nls.mk
+++ src/bin/pg_ctl/nls.mk
@@ -1,5 +1,5 @@
 # $PostgreSQL: pgsql/src/bin/pg_ctl/nls.mk,v 1.18.2.2 2010/05/13 10:50:02 petere Exp $
 CATALOG_NAME	:= pg_ctl
-AVAIL_LANGUAGES	:= de es fr it ja ko pt_BR ru sv ta tr zh_CN
+AVAIL_LANGUAGES	:= de es fr it ja ko pt_BR ru sv ta tr zh_CN zh_TW
 GETTEXT_FILES	:= pg_ctl.c ../../port/exec.c
 GETTEXT_TRIGGERS:= _ simple_prompt
