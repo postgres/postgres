9103b311a4f126e1a8b674aa90c6f2890ff15124
Peter Eisentraut <peter_e@gmx.net>
2010-09-30 23:46:16 +0300

    Translation updates for 9.0.1

--- src/pl/plpython/nls.mk
+++ src/pl/plpython/nls.mk
@@ -1,5 +1,5 @@
 # $PostgreSQL: pgsql/src/pl/plpython/nls.mk,v 1.7 2009/10/20 18:23:27 petere Exp $
 CATALOG_NAME	:= plpython
-AVAIL_LANGUAGES	:= de es fr it ja pt_BR tr
+AVAIL_LANGUAGES	:= de es fr it ja pt_BR ro tr
 GETTEXT_FILES	:= plpython.c
 GETTEXT_TRIGGERS:= errmsg errmsg_plural:1,2 errdetail errdetail_log errdetail_plural:1,2 errhint errcontext PLy_elog:2 PLy_exception_set:2 PLy_exception_set_plural:2,3
--- src/pl/tcl/nls.mk
+++ src/pl/tcl/nls.mk
@@ -1,5 +1,5 @@
 # $PostgreSQL: pgsql/src/pl/tcl/nls.mk,v 1.7 2009/10/20 18:23:27 petere Exp $
 CATALOG_NAME	:= pltcl
-AVAIL_LANGUAGES	:= de es fr it ja pt_BR tr
+AVAIL_LANGUAGES	:= de es fr it ja pt_BR ro tr
 GETTEXT_FILES	:= pltcl.c
 GETTEXT_TRIGGERS:= errmsg errmsg_plural:1,2 errdetail errdetail_log errdetail_plural:1,2 errhint errcontext
