032191b4aa44a65a7116413c765b64d25c6e700e
Tom Lane <tgl@sss.pgh.pa.us>
2010-11-13 00:35:03 -0500

    Add missing outfuncs.c support for struct InhRelation.
    This is needed to support debug_print_parse, per report from Jon Nelson.
    Cursory testing via the regression tests suggests we aren't missing
    anything else.

--- src/backend/nodes/outfuncs.c
+++ src/backend/nodes/outfuncs.c
@@ -1630,6 +1630,15 @@ _outDefElem(StringInfo str, DefElem *node)
 }
 
 static void
+_outInhRelation(StringInfo str, InhRelation *node)
+{
+	WRITE_NODE_TYPE("INHRELATION");
+
+	WRITE_NODE_FIELD(relation);
+	WRITE_NODE_FIELD(options);
+}
+
+static void
 _outLockingClause(StringInfo str, LockingClause *node)
 {
 	WRITE_NODE_TYPE("LOCKINGCLAUSE");
@@ -2434,6 +2443,9 @@ _outNode(StringInfo str, void *obj)
 				break;
 			case T_DefElem:
 				_outDefElem(str, obj);
+				break;
+			case T_InhRelation:
+				_outInhRelation(str, obj);
 				break;
 			case T_LockingClause:
 				_outLockingClause(str, obj);
