317a568c88ee92bb50238f9fe9508803bc81501c
Itagaki Takahiro <itagaki.takahiro@gmail.com>
2010-11-25 19:40:58 +0900

    Don't raise "identifier will be truncated" messages in dblink except creating new connections.

--- contrib/dblink/dblink.c
+++ contrib/dblink/dblink.c
@@ -2109,7 +2109,7 @@ getConnectionByName(const char *name)
 		remoteConnHash = createConnHash();
 
 	key = pstrdup(name);
-	truncate_identifier(key, strlen(key), true);
+	truncate_identifier(key, strlen(key), false);
 	hentry = (remoteConnHashEnt *) hash_search(remoteConnHash,
 											   key, HASH_FIND, NULL);
 
@@ -2170,7 +2170,7 @@ deleteConnection(const char *name)
 		remoteConnHash = createConnHash();
 
 	key = pstrdup(name);
-	truncate_identifier(key, strlen(key), true);
+	truncate_identifier(key, strlen(key), false);
 	hentry = (remoteConnHashEnt *) hash_search(remoteConnHash,
 											   key, HASH_REMOVE, &found);
 
