770bddc1b4b59338395185966100056d90e6dcf9
Tom Lane <tgl@sss.pgh.pa.us>
2010-12-15 23:50:49 -0500

    Fix up getopt() reset management so it works on recent mingw.
    The mingw people don't appear to care about compatibility with non-GNU
    versions of getopt, so force use of our own copy of getopt on Windows.
    Also, ensure that we make use of optreset when using our own copy.
    
    Per report from Andrew Dunstan.  Back-patch to all versions supported
    on Windows.

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -312,7 +312,8 @@ extern char *optarg;
 extern int	optind,
 			opterr;
 
-#ifdef HAVE_INT_OPTRESET
+/* If not HAVE_GETOPT, we are using src/port/getopt.c, which has optreset */
+#if defined(HAVE_INT_OPTRESET) || !defined(HAVE_GETOPT)
 extern int	optreset;			/* might not be declared by system headers */
 #endif
 
@@ -750,7 +751,7 @@ PostmasterMain(int argc, char *argv[])
 	 * getopt(3) library so that it will work correctly in subprocesses.
 	 */
 	optind = 1;
-#ifdef HAVE_INT_OPTRESET
+#if defined(HAVE_INT_OPTRESET) || !defined(HAVE_GETOPT)
 	optreset = 1;				/* some systems need this too */
 #endif
 
