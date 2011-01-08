1b2b96c4231f11313f14ded55e25640fb239de31
Michael Meskes <meskes@postgresql.org>
2011-01-08 18:35:37 +0100

    In ecpg's parser removed a fixed length limit for constants defining an array dimension.

--- src/interfaces/ecpg/test/expected/preproc-array_of_struct.c
+++ src/interfaces/ecpg/test/expected/preproc-array_of_struct.c
@@ -114,7 +114,7 @@ int main()
  int r ;
  
 #line 45 "array_of_struct.pgc"
-  struct varchar_onlyname_5  { int len; char arr[ 50 ]; }  onlyname [2] ;
+  struct varchar_onlyname_5  { int len; char arr[ 50 ]; }  onlyname [ 2 ] ;
 /* exec sql end declare section */
 #line 46 "array_of_struct.pgc"
 
