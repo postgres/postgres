db4d22d0eff091e96d92c82566c9e9d297040b93
Magnus Hagander <magnus@hagander.net>
2011-01-09 15:06:55 +0100

    Add pgreadlink() on Windows to read junction points
    Add support for reading back information about the symbolic
    links we've created with pgsymlink(), which are actually
    Junction Points. Just like pgsymlink() can only create directory
    symlinks, pgreadlink() can only read directory symlinks.

--- src/include/port.h
+++ src/include/port.h
@@ -284,8 +284,11 @@ extern int	pgunlink(const char *path);
  */
 #if defined(WIN32) && !defined(__CYGWIN__)
 extern int	pgsymlink(const char *oldpath, const char *newpath);
+extern int	pgreadlink(const char *path, char *buf, size_t size);
+extern bool pgwin32_is_junction(char *path);
 
 #define symlink(oldpath, newpath)	pgsymlink(oldpath, newpath)
+#define readlink(path, buf, size)	pgreadlink(path, buf, size)
 #endif
 
 extern bool rmtree(const char *path, bool rmtopdir);
