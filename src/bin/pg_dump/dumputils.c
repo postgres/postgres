1a1167a172ebc390437ffc5547a9f755fa56b731
Tom Lane <tgl@sss.pgh.pa.us>
2011-01-21 16:22:27 -0500

    Fix pg_restore to do the right thing when escaping large objects.
    Specifically, this makes the workflow pg_dump -Fc -> pg_restore -> file
    produce correct output for BLOBs when the source database has
    standard_conforming_strings turned on.  It was already okay when that was
    off, or if pg_restore was told to restore directly into a database.
    
    This is a back-port of commit b1732111f233bbb72788e92a627242ec28a85631 of
    2009-08-04, with additional changes to emit old-style escaped bytea data
    instead of hex-style.  At the time, we had not heard of anyone encountering
    the problem in the field, so I judged it not worth the risk of changing
    back branches.  Now we do have a report, from Bosco Rama, so back-patch
    into 8.2 through 8.4.  9.0 and up are okay already.

--- src/bin/pg_dump/dumputils.c
+++ src/bin/pg_dump/dumputils.c
@@ -275,6 +275,84 @@ appendStringLiteralDQ(PQExpBuffer buf, const char *str, const char *dqprefix)
 
 
 /*
+ * Convert a bytea value (presented as raw bytes) to an SQL string literal
+ * and append it to the given buffer.  We assume the specified
+ * standard_conforming_strings setting.
+ *
+ * This is needed in situations where we do not have a PGconn available.
+ * Where we do, PQescapeByteaConn is a better choice.
+ */
+void
+appendByteaLiteral(PQExpBuffer buf, const unsigned char *str, size_t length,
+				   bool std_strings)
+{
+	const unsigned char *vp;
+	unsigned char *rp;
+	size_t		i;
+	size_t		len;
+	size_t		bslash_len = (std_strings ? 1 : 2);
+
+	len = 2;					/* for the quote marks */
+	vp = str;
+	for (i = length; i > 0; i--, vp++)
+	{
+		if (*vp < 0x20 || *vp > 0x7e)
+			len += bslash_len + 3;
+		else if (*vp == '\'')
+			len += 2;
+		else if (*vp == '\\')
+			len += bslash_len + bslash_len;
+		else
+			len++;
+	}
+
+	if (!enlargePQExpBuffer(buf, len))
+		return;
+
+	rp = (unsigned char *) (buf->data + buf->len);
+	*rp++ = '\'';
+
+	vp = str;
+	for (i = length; i > 0; i--, vp++)
+	{
+		if (*vp < 0x20 || *vp > 0x7e)
+		{
+			int			val = *vp;
+
+			if (!std_strings)
+				*rp++ = '\\';
+			*rp++ = '\\';
+			*rp++ = (val >> 6) + '0';
+			*rp++ = ((val >> 3) & 07) + '0';
+			*rp++ = (val & 07) + '0';
+		}
+		else if (*vp == '\'')
+		{
+			*rp++ = '\'';
+			*rp++ = '\'';
+		}
+		else if (*vp == '\\')
+		{
+			if (!std_strings)
+			{
+				*rp++ = '\\';
+				*rp++ = '\\';
+			}
+			*rp++ = '\\';
+			*rp++ = '\\';
+		}
+		else
+			*rp++ = *vp;
+	}
+
+	*rp++ = '\'';
+	*rp = '\0';
+
+	buf->len = ((char *) rp) - buf->data;
+}
+
+
+/*
  * Convert backend's version string into a number.
  */
 int
