356f2cbbb42811596820c94aaec2e32ad1ec58d8
Robert Haas <rhaas@postgresql.org>
2011-02-04 09:28:06 -0500

    Make handling of errcodes.h more consistent with other generated headers.
    This fixes make distprep, and seems more robust in other ways as well.
    Some special handling is required because errcodes.txt is needed by
    some stuff in src/port, but just by src/backend as is the case for the
    other generated headers.
    
    While I'm at it, fix a few other things that were overlooked in the
    original patch.

--- src/tools/msvc/clean.bat
+++ src/tools/msvc/clean.bat
@@ -20,6 +20,7 @@ REM Delete files created with GenerateFiles() in Solution.pm
 if exist src\include\pg_config.h del /q src\include\pg_config.h
 if exist src\include\pg_config_os.h del /q src\include\pg_config_os.h
 if %DIST%==1 if exist src\backend\parser\gram.h del /q src\backend\parser\gram.h
+if exist src\include\utils\errcodes.h del /q src\include\utils\errcodes.h
 if exist src\include\utils\fmgroids.h del /q src\include\utils\fmgroids.h
 if exist src\include\utils\probes.h del /q src\include\utils\probes.h
 
