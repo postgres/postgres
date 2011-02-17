de623f33353c96657651f9c3a6c8756616c610e4
Tom Lane <tgl@sss.pgh.pa.us>
2011-02-17 18:11:28 -0500

    Fix upgrade of contrib/xml2 from 9.0.
    Update script was being sloppy about two functions that have been changed
    since 9.0.

--- contrib/xml2/xml2--unpackaged--1.0.sql
+++ contrib/xml2/xml2--unpackaged--1.0.sql
@@ -13,3 +13,14 @@ ALTER EXTENSION xml2 ADD function xpath_nodeset(text,text,text,text);
 ALTER EXTENSION xml2 ADD function xpath_string(text,text);
 ALTER EXTENSION xml2 ADD function xml_encode_special_chars(text);
 ALTER EXTENSION xml2 ADD function xml_valid(text);
+
+-- xml_valid is now an alias for core xml_is_well_formed()
+
+CREATE OR REPLACE FUNCTION xml_valid(text) RETURNS bool
+AS 'xml_is_well_formed'
+LANGUAGE INTERNAL STRICT STABLE;
+
+-- xml_is_well_formed is now in core, not needed in extension.
+-- be careful to drop extension's copy not core's.
+
+DROP FUNCTION @extschema@.xml_is_well_formed(text);
