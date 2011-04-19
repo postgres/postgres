908eb1f98bd9f81613cf4c14d6ab5877815c7b50
Peter Eisentraut <peter_e@gmx.net>
2011-04-19 22:00:35 +0300

    Fix typo

--- src/tools/msvc/config_default.pl
+++ src/tools/msvc/config_default.pl
@@ -8,7 +8,7 @@ our $config = {
     # float4byval=>1,         # --disable-float4-byval, on by default
     # float8byval=>0,         # --disable-float8-byval, off by default
     # blocksize => 8,         # --with-blocksize, 8kB by default
-    # wal_blocksize => 8,     # --with-wal-blocksize, 8kb by default
+    # wal_blocksize => 8,     # --with-wal-blocksize, 8kB by default
     # wal_segsize => 16,      # --with-wal-segsize, 16MB by default
     ldap=>1,				# --with-ldap
     nls=>undef,				# --enable-nls=<path>
