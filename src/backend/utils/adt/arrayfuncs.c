18c0b4eccdc86ffb7eccc2c6facfe382537ab877
Tom Lane <tgl@sss.pgh.pa.us>
2011-04-27 13:58:36 -0400

    Fix array- and path-creating functions to ensure padding bytes are zeroes.
    Per recent discussion, it's important for all computed datums (not only the
    results of input functions) to not contain any ill-defined (uninitialized)
    bits.  Failing to ensure that can result in equal() reporting that
    semantically indistinguishable Consts are not equal, which in turn leads to
    bizarre and undesirable planner behavior, such as in a recent example from
    David Johnston.  We might eventually try to fix this in a general manner by
    allowing datatypes to define identity-testing functions, but for now the
    path of least resistance is to expect datatypes to force all unused bits
    into consistent states.
    
    Per some testing by Noah Misch, array and path functions seem to be the
    only ones presenting risks at the moment, so I looked through all the
    functions in adt/array*.c and geo_ops.c and fixed them as necessary.  In
    the array functions, the easiest/safest fix is to allocate result arrays
    with palloc0 instead of palloc.  Possibly in future someone will want to
    look into whether we can just zero the padding bytes, but that looks too
    complex for a back-patchable fix.  In the path functions, we already had a
    precedent in path_in for just zeroing the one known pad field, so duplicate
    that code as needed.
    
    Back-patch to all supported branches.

--- src/backend/utils/adt/arrayfuncs.c
+++ src/backend/utils/adt/arrayfuncs.c
@@ -1339,7 +1339,7 @@ array_recv(PG_FUNCTION_ARGS)
 		dataoffset = 0;			/* marker for no null bitmap */
 		nbytes += ARR_OVERHEAD_NONULLS(ndim);
 	}
-	retval = (ArrayType *) palloc(nbytes);
+	retval = (ArrayType *) palloc0(nbytes);
 	SET_VARSIZE(retval, nbytes);
 	retval->ndim = ndim;
 	retval->dataoffset = dataoffset;
@@ -1977,7 +1977,7 @@ array_get_slice(ArrayType *array,
 		bytes += ARR_OVERHEAD_NONULLS(ndim);
 	}
 
-	newarray = (ArrayType *) palloc(bytes);
+	newarray = (ArrayType *) palloc0(bytes);
 	SET_VARSIZE(newarray, bytes);
 	newarray->ndim = ndim;
 	newarray->dataoffset = dataoffset;
@@ -2230,7 +2230,7 @@ array_set(ArrayType *array,
 	/*
 	 * OK, create the new array and fill in header/dimensions
 	 */
-	newarray = (ArrayType *) palloc(newsize);
+	newarray = (ArrayType *) palloc0(newsize);
 	SET_VARSIZE(newarray, newsize);
 	newarray->ndim = ndim;
 	newarray->dataoffset = newhasnulls ? overheadlen : 0;
@@ -2560,7 +2560,7 @@ array_set_slice(ArrayType *array,
 
 	newsize = overheadlen + olddatasize - olditemsize + newitemsize;
 
-	newarray = (ArrayType *) palloc(newsize);
+	newarray = (ArrayType *) palloc0(newsize);
 	SET_VARSIZE(newarray, newsize);
 	newarray->ndim = ndim;
 	newarray->dataoffset = newhasnulls ? overheadlen : 0;
@@ -2819,7 +2819,7 @@ array_map(FunctionCallInfo fcinfo, Oid inpType, Oid retType,
 		dataoffset = 0;			/* marker for no null bitmap */
 		nbytes += ARR_OVERHEAD_NONULLS(ndim);
 	}
-	result = (ArrayType *) palloc(nbytes);
+	result = (ArrayType *) palloc0(nbytes);
 	SET_VARSIZE(result, nbytes);
 	result->ndim = ndim;
 	result->dataoffset = dataoffset;
@@ -2955,7 +2955,7 @@ construct_md_array(Datum *elems,
 		dataoffset = 0;			/* marker for no null bitmap */
 		nbytes += ARR_OVERHEAD_NONULLS(ndims);
 	}
-	result = (ArrayType *) palloc(nbytes);
+	result = (ArrayType *) palloc0(nbytes);
 	SET_VARSIZE(result, nbytes);
 	result->ndim = ndims;
 	result->dataoffset = dataoffset;
@@ -2979,7 +2979,7 @@ construct_empty_array(Oid elmtype)
 {
 	ArrayType  *result;
 
-	result = (ArrayType *) palloc(sizeof(ArrayType));
+	result = (ArrayType *) palloc0(sizeof(ArrayType));
 	SET_VARSIZE(result, sizeof(ArrayType));
 	result->ndim = 0;
 	result->dataoffset = 0;
