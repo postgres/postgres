376f93e0a1959c8f84980c051195d8ee262b2059
Magnus Hagander <magnus@hagander.net>
2011-06-09 18:21:38 +0200

    Use the correct eventlog severity for error

--- src/bin/pg_ctl/pg_ctl.c
+++ src/bin/pg_ctl/pg_ctl.c
@@ -1233,7 +1233,7 @@ pgwin32_ServiceMain(DWORD argc, LPTSTR * argv)
 		write_eventlog(EVENTLOG_INFORMATION_TYPE, _("Waiting for server startup...\n"));
 		if (test_postmaster_connection(true) == false)
 		{
-			write_eventlog(EVENTLOG_INFORMATION_TYPE, _("Timed out waiting for server startup\n"));
+			write_eventlog(EVENTLOG_ERROR_TYPE, _("Timed out waiting for server startup\n"));
 			pgwin32_SetServiceStatus(SERVICE_STOPPED);
 			return;
 		}
