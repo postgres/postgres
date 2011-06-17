7975f2baa955397fbeb64b5301235ed1dd0f9be6
Tom Lane <tgl@sss.pgh.pa.us>
2011-06-17 18:19:14 -0400

    Obtain table locks as soon as practical during pg_dump.
    For some reason, when we (I) added table lock acquisition to pg_dump,
    we didn't think about making it happen as soon as possible after the
    start of the transaction.  What with subsequent additions, there was
    actually quite a lot going on before we got around to that; which sort
    of defeats the purpose.  Rearrange the order of calls in dumpSchema()
    to close the risk window as much as we easily can.  Back-patch to all
    supported branches.

--- src/bin/pg_dump/common.c
+++ src/bin/pg_dump/common.c
@@ -115,6 +115,17 @@ getSchemaData(int *numTablesPtr)
 		write_msg(NULL, "reading schemas\n");
 	nsinfo = getNamespaces(&numNamespaces);
 
+	/*
+	 * getTables should be done as soon as possible, so as to minimize the
+	 * window between starting our transaction and acquiring per-table locks.
+	 * However, we have to do getNamespaces first because the tables get
+	 * linked to their containing namespaces during getTables.
+	 */
+	if (g_verbose)
+		write_msg(NULL, "reading user-defined tables\n");
+	tblinfo = getTables(&numTables);
+	tblinfoindex = buildIndexArray(tblinfo, numTables, sizeof(TableInfo));
+
 	if (g_verbose)
 		write_msg(NULL, "reading user-defined functions\n");
 	funinfo = getFuncs(&numFuncs);
@@ -179,11 +190,6 @@ getSchemaData(int *numTablesPtr)
 	if (g_verbose)
 		write_msg(NULL, "reading user-defined conversions\n");
 	convinfo = getConversions(&numConversions);
-
-	if (g_verbose)
-		write_msg(NULL, "reading user-defined tables\n");
-	tblinfo = getTables(&numTables);
-	tblinfoindex = buildIndexArray(tblinfo, numTables, sizeof(TableInfo));
 
 	if (g_verbose)
 		write_msg(NULL, "reading table inheritance information\n");
