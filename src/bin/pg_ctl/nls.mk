314837ec3b00bb387f1bd601faa9d535fa2dc80b
Peter Eisentraut <peter_e@gmx.net>
2011-06-26 15:36:15 +0300

    Remove some cruft from nls.mk files

--- src/bin/pg_ctl/nls.mk
+++ src/bin/pg_ctl/nls.mk
@@ -2,4 +2,4 @@
 CATALOG_NAME	:= pg_ctl
 AVAIL_LANGUAGES	:= de es fr it ja ko pt_BR ru sv ta tr zh_CN zh_TW
 GETTEXT_FILES	:= pg_ctl.c ../../port/exec.c
-GETTEXT_TRIGGERS:= _ simple_prompt
+GETTEXT_TRIGGERS:= _
--- src/pl/plpgsql/src/nls.mk
+++ src/pl/plpgsql/src/nls.mk
@@ -3,6 +3,3 @@ CATALOG_NAME	:= plpgsql
 AVAIL_LANGUAGES	:= de es fr it ja ko pt_BR ro zh_CN zh_TW
 GETTEXT_FILES	:= pl_comp.c pl_exec.c pl_gram.c pl_funcs.c pl_handler.c pl_scanner.c
 GETTEXT_TRIGGERS:= _ errmsg errmsg_plural:1,2 errdetail errdetail_log errdetail_plural:1,2 errhint errcontext yyerror plpgsql_yyerror
-
-.PHONY: gettext-files
-gettext-files: distprep
