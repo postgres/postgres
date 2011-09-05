56a9ed92b664af191040d848ca923fb3234671ee
Alvaro Herrera <alvherre@alvh.no-ip.org>
2011-09-05 18:52:49 -0300

    Adjust translator comment format to xgettext expectations

--- src/backend/catalog/dependency.c
+++ src/backend/catalog/dependency.c
@@ -2447,12 +2447,11 @@ getObjectDescription(const ObjectAddress *object)
 				initStringInfo(&opfam);
 				getOpFamilyDescription(&opfam, amopForm->amopfamily);
 
-				/*
-				 * translator: %d is the operator strategy (a number), the
-				 * first two %s's are data type names, the third %s is the
-				 * description of the operator family, and the last %s is the
-				 * textual form of the operator with arguments.
-				 */
+				/*------
+				   translator: %d is the operator strategy (a number), the
+				   first two %s's are data type names, the third %s is the
+				   description of the operator family, and the last %s is the
+				   textual form of the operator with arguments.  */
 				appendStringInfo(&buffer, _("operator %d (%s, %s) of %s: %s"),
 								 amopForm->amopstrategy,
 								 format_type_be(amopForm->amoplefttype),
@@ -2498,12 +2497,11 @@ getObjectDescription(const ObjectAddress *object)
 				initStringInfo(&opfam);
 				getOpFamilyDescription(&opfam, amprocForm->amprocfamily);
 
-				/*
-				 * translator: %d is the function number, the first two %s's
-				 * are data type names, the third %s is the description of the
-				 * operator family, and the last %s is the textual form of the
-				 * function with arguments.
-				 */
+				/*------
+				   translator: %d is the function number, the first two %s's
+				   are data type names, the third %s is the description of the
+				   operator family, and the last %s is the textual form of the
+				   function with arguments.  */
 				appendStringInfo(&buffer, _("function %d (%s, %s) of %s: %s"),
 								 amprocForm->amprocnum,
 								 format_type_be(amprocForm->amproclefttype),
