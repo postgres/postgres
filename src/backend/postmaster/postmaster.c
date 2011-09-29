96a263f885729aeaaedbe3f1ed8e3cc6f7cd1211
Bruce Momjian <bruce@momjian.us>
2011-09-29 17:20:56 -0400

    Fix pg_upgrade for EXEC_BACKEND builds (e.g. Windows) by properly passing the -b/binary-upgrade flag.
    Backpatch to 9.1.X.

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -437,6 +437,7 @@ typedef struct
 	TimestampTz PgStartTime;
 	TimestampTz PgReloadTime;
 	bool		redirection_done;
+	bool		IsBinaryUpgrade;
 #ifdef WIN32
 	HANDLE		PostmasterHandle;
 	HANDLE		initial_signal_pipe;
@@ -4742,6 +4743,7 @@ save_backend_variables(BackendParameters *param, Port *port,
 	param->PgReloadTime = PgReloadTime;
 
 	param->redirection_done = redirection_done;
+	param->IsBinaryUpgrade = IsBinaryUpgrade;
 
 #ifdef WIN32
 	param->PostmasterHandle = PostmasterHandle;
@@ -4960,6 +4962,7 @@ restore_backend_variables(BackendParameters *param, Port *port)
 	PgReloadTime = param->PgReloadTime;
 
 	redirection_done = param->redirection_done;
+	IsBinaryUpgrade = param->IsBinaryUpgrade;
 
 #ifdef WIN32
 	PostmasterHandle = param->PostmasterHandle;
