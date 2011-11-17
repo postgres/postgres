663e27c405a0e6bfe7c4fd7e23dce67a46fb07e9
Bruce Momjian <bruce@momjian.us>
2011-11-17 13:40:45 -0500

    Fix pg_upgrade's pg_scandir_internal() the right way.  Backpatch to 9.1.

--- contrib/pg_upgrade/file.c
+++ contrib/pg_upgrade/file.c
@@ -294,7 +294,7 @@ pg_scandir_internal(const char *dirname,
 	while ((direntry = readdir(dirdesc)) != NULL)
 	{
 		/* Invoke the selector function to see if the direntry matches */
-		if (selector && (*selector) (direntry))
+		if (!selector || (*selector) (direntry))
 		{
 			count++;
 
