658ee0108637683216fb093b9f2c7e23bfd150c5
Tom Lane <tgl@sss.pgh.pa.us>
2012-01-04 18:31:01 -0500

    Make executor's SELECT INTO code save and restore original tuple receiver.
    As previously coded, the QueryDesc's dest pointer was left dangling
    (pointing at an already-freed receiver object) after ExecutorEnd.  It's a
    bit astonishing that it took us this long to notice, and I'm not sure that
    the known problem case with SQL functions is the only one.  Fix it by
    saving and restoring the original receiver pointer, which seems the most
    bulletproof way of ensuring any related bugs are also covered.
    
    Per bug #6379 from Paul Ramsey.  Back-patch to 8.4 where the current
    handling of SELECT INTO was introduced.

--- src/test/regress/sql/select_into.sql
+++ src/test/regress/sql/select_into.sql
@@ -15,3 +15,18 @@ SELECT *
    WHERE onek2.unique1 < 2;
 
 DROP TABLE tmp1;
+
+--
+-- CREATE TABLE AS/SELECT INTO as last command in a SQL function
+-- have been known to cause problems
+--
+CREATE FUNCTION make_table() RETURNS VOID
+AS $$
+  CREATE TABLE created_table AS SELECT * FROM int8_tbl;
+$$ LANGUAGE SQL;
+
+SELECT make_table();
+
+SELECT * FROM created_table;
+
+DROP TABLE created_table;
