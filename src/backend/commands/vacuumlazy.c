d0dcb315db0043f10073a9a244cea138e9e60edd
Robert Haas <rhaas@postgresql.org>
2012-01-13 08:22:31 -0500

    Fix broken logic in lazy_vacuum_heap.
    As noted by Tom Lane, the previous coding in this area, which I
    introduced in commit bbb6e559c4ea0fb4c346beda76736451dc24eb4e, was
    poorly tested and caused the vacuum's second heap to go into what would
    have been an infinite loop but for the fact that it eventually caused a
    memory allocation failure.  This version seems to work better.

--- src/backend/commands/vacuumlazy.c
+++ src/backend/commands/vacuumlazy.c
@@ -991,7 +991,11 @@ lazy_vacuum_heap(Relation onerel, LVRelStats *vacrelstats)
 		buf = ReadBufferExtended(onerel, MAIN_FORKNUM, tblk, RBM_NORMAL,
 								 vac_strategy);
 		if (!ConditionalLockBufferForCleanup(buf))
+		{
+			ReleaseBuffer(buf);
+			++tupindex;
 			continue;
+		}
 		tupindex = lazy_vacuum_page(onerel, tblk, buf, tupindex, vacrelstats);
 
 		/* Now that we've compacted the page, record its available space */
