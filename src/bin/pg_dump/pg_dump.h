182228bd747caa362664bff525fc8346a37da16e
Tom Lane <tgl@sss.pgh.pa.us>
2012-02-10 13:28:10 -0500

    Fix pg_dump for better handling of inherited columns.
    Revise pg_dump's handling of inherited columns, which was last looked at
    seriously in 2001, to eliminate several misbehaviors associated with
    inherited default expressions and NOT NULL flags.  In particular make sure
    that a column is printed in a child table's CREATE TABLE command if and
    only if it has attislocal = true; the former behavior would sometimes cause
    a column to become marked attislocal when it was not so marked in the
    source database.  Also, stop relying on textual comparison of default
    expressions to decide if they're inherited; instead, don't use
    default-expression inheritance at all, but just install the default
    explicitly at each level of the hierarchy.  This fixes the
    search-path-related misbehavior recently exhibited by Chester Young, and
    also removes some dubious assumptions about the order in which ALTER TABLE
    SET DEFAULT commands would be executed.
    
    Back-patch to all supported branches.

--- src/bin/pg_dump/pg_dump.h
+++ src/bin/pg_dump/pg_dump.h
@@ -275,17 +275,9 @@ typedef struct _tableInfo
 	bool	   *attislocal;		/* true if attr has local definition */
 	char	  **attoptions;		/* per-attribute options */
 	Oid		   *attcollation;	/* per-attribute collation selection */
-
-	/*
-	 * Note: we need to store per-attribute notnull, default, and constraint
-	 * stuff for all interesting tables so that we can tell which constraints
-	 * were inherited.
-	 */
-	bool	   *notnull;		/* Not null constraints on attributes */
-	struct _attrDefInfo **attrdefs;		/* DEFAULT expressions */
-	bool	   *inhAttrs;		/* true if each attribute is inherited */
-	bool	   *inhAttrDef;		/* true if attr's default is inherited */
+	bool	   *notnull;		/* NOT NULL constraints on attributes */
 	bool	   *inhNotNull;		/* true if NOT NULL is inherited */
+	struct _attrDefInfo **attrdefs;		/* DEFAULT expressions */
 	struct _constraintInfo *checkexprs; /* CHECK constraints */
 
 	/*
@@ -298,7 +290,7 @@ typedef struct _tableInfo
 
 typedef struct _attrDefInfo
 {
-	DumpableObject dobj;
+	DumpableObject dobj;		/* note: dobj.name is name of table */
 	TableInfo  *adtable;		/* link to table of attribute */
 	int			adnum;
 	char	   *adef_expr;		/* decompiled DEFAULT expression */
@@ -556,6 +548,7 @@ extern void getTriggers(TableInfo tblinfo[], int numTables);
 extern ProcLangInfo *getProcLangs(int *numProcLangs);
 extern CastInfo *getCasts(int *numCasts);
 extern void getTableAttrs(TableInfo *tbinfo, int numTables);
+extern bool shouldPrintColumn(TableInfo *tbinfo, int colno);
 extern TSParserInfo *getTSParsers(int *numTSParsers);
 extern TSDictInfo *getTSDictionaries(int *numTSDicts);
 extern TSTemplateInfo *getTSTemplates(int *numTSTemplates);
