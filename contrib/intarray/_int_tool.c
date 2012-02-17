81b4e5ceb6904f3d689a694eb72a90b7ea6cd961
Tom Lane <tgl@sss.pgh.pa.us>
2012-02-16 20:00:29 -0500

    Fix longstanding error in contrib/intarray's int[] & int[] operator.
    The array intersection code would give wrong results if the first entry of
    the correct output array would be "1".  (I think only this value could be
    at risk, since the previous word would always be a lower-bound entry with
    that fixed value.)
    
    Problem spotted by Julien Rouhaud, initial patch by Guillaume Lelarge,
    cosmetic improvements by me.

--- contrib/intarray/_int_tool.c
+++ contrib/intarray/_int_tool.c
@@ -146,7 +146,8 @@ inner_int_inter(ArrayType *a, ArrayType *b)
 			   *db,
 			   *dr;
 	int			i,
-				j;
+				j,
+				k;
 
 	CHECKARRVALID(a);
 	CHECKARRVALID(b);
@@ -161,27 +162,29 @@ inner_int_inter(ArrayType *a, ArrayType *b)
 	r = new_intArrayType(Min(na, nb));
 	dr = ARRPTR(r);
 
-	i = j = 0;
+	i = j = k = 0;
 	while (i < na && j < nb)
+	{
 		if (da[i] < db[j])
 			i++;
 		else if (da[i] == db[j])
 		{
-			if (i + j == 0 || (i + j > 0 && *(dr - 1) != db[j]))
-				*dr++ = db[j];
+			if (k == 0 || dr[k - 1] != db[j])
+				dr[k++] = db[j];
 			i++;
 			j++;
 		}
 		else
 			j++;
+	}
 
-	if ((dr - ARRPTR(r)) == 0)
+	if (k == 0)
 	{
 		pfree(r);
 		return new_intArrayType(0);
 	}
 	else
-		return resize_intArrayType(r, dr - ARRPTR(r));
+		return resize_intArrayType(r, k);
 }
 
 void
