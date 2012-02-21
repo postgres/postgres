140766dff659cb9429fe9a5b8341b757d6c4c169
Alvaro Herrera <alvherre@alvh.no-ip.org>
2012-02-21 17:58:02 -0300

    REASSIGN OWNED: Support foreign data wrappers and servers
    This was overlooked when implementing those kinds of objects, in commit
    cae565e503c42a0942ca1771665243b4453c5770.
    
    Per report from Pawel Casperek.

--- src/backend/catalog/pg_shdepend.c
+++ src/backend/catalog/pg_shdepend.c
@@ -24,6 +24,8 @@
 #include "catalog/pg_conversion.h"
 #include "catalog/pg_database.h"
 #include "catalog/pg_default_acl.h"
+#include "catalog/pg_foreign_data_wrapper.h"
+#include "catalog/pg_foreign_server.h"
 #include "catalog/pg_language.h"
 #include "catalog/pg_largeobject.h"
 #include "catalog/pg_namespace.h"
@@ -1374,6 +1376,14 @@ shdepReassignOwned(List *roleids, Oid newrole)
 
 				case OperatorFamilyRelationId:
 					AlterOpFamilyOwner_oid(sdepForm->objid, newrole);
+					break;
+
+				case ForeignServerRelationId:
+					AlterForeignServerOwner_oid(sdepForm->objid, newrole);
+					break;
+
+				case ForeignDataWrapperRelationId:
+					AlterForeignDataWrapperOwner_oid(sdepForm->objid, newrole);
 					break;
 
 				default:
