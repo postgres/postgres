31f26140b3be4b9c59d6095012602dbf42b3a505
Peter Eisentraut <peter_e@gmx.net>
2012-02-23 12:51:33 +0200

    Remove inappropriate quotes
    And adjust wording for consistency.

--- src/backend/commands/foreigncmds.c
+++ src/backend/commands/foreigncmds.c
@@ -275,7 +275,7 @@ AlterForeignDataWrapperOwner_oid(Oid fwdId, Oid newOwnerId)
 	if (!HeapTupleIsValid(tup))
 		ereport(ERROR,
 				(errcode(ERRCODE_UNDEFINED_OBJECT),
-				 errmsg("foreign-data wrapper with OID \"%u\" does not exist", fwdId)));
+				 errmsg("foreign-data wrapper with OID %u does not exist", fwdId)));
 
 	AlterForeignDataWrapperOwner_internal(rel, tup, newOwnerId);
 
@@ -373,7 +373,7 @@ AlterForeignServerOwner_oid(Oid srvId, Oid newOwnerId)
 	if (!HeapTupleIsValid(tup))
 		ereport(ERROR,
 				(errcode(ERRCODE_UNDEFINED_OBJECT),
-				 errmsg("server with OID \"%u\" does not exist", srvId)));
+				 errmsg("foreign server with OID %u does not exist", srvId)));
 
 	AlterForeignServerOwner_internal(rel, tup, newOwnerId);
 
