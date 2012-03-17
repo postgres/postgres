e3fc4a97bc8ee82a78605b5ffe79bd4cf3c6213b
Andrew Dunstan <andrew@dunslane.net>
2012-03-17 17:24:15 -0400

    Honor inputdir and outputdir when converting regression files.
    When converting source files, pg_regress' inputdir and outputdir options were
    ignored when computing the locations of the destination files. In consequence,
    these options were effectively unusable when the regression inputs need to
    be adjusted by pg_regress. This patch makes pg_regress put the converted files
    in the same place that these options specify non-converted input or results
    files are to be found. Backpatched to all live branches.

--- src/test/regress/pg_regress.c
+++ src/test/regress/pg_regress.c
@@ -407,7 +407,7 @@ replace_string(char *string, char *replace, char *replacement)
  * the given suffix.
  */
 static void
-convert_sourcefiles_in(char *source_subdir, char *dest_subdir, char *suffix)
+convert_sourcefiles_in(char *source_subdir, char *dest_dir, char *dest_subdir, char *suffix)
 {
 	char		testtablespace[MAXPGPATH];
 	char		indir[MAXPGPATH];
@@ -475,7 +475,8 @@ convert_sourcefiles_in(char *source_subdir, char *dest_subdir, char *suffix)
 		/* build the full actual paths to open */
 		snprintf(prefix, strlen(*name) - 6, "%s", *name);
 		snprintf(srcfile, MAXPGPATH, "%s/%s", indir, *name);
-		snprintf(destfile, MAXPGPATH, "%s/%s.%s", dest_subdir, prefix, suffix);
+		snprintf(destfile, MAXPGPATH, "%s/%s/%s.%s", dest_dir, dest_subdir, 
+				 prefix, suffix);
 
 		infile = fopen(srcfile, "r");
 		if (!infile)
@@ -522,8 +523,8 @@ convert_sourcefiles_in(char *source_subdir, char *dest_subdir, char *suffix)
 static void
 convert_sourcefiles(void)
 {
-	convert_sourcefiles_in("input", "sql", "sql");
-	convert_sourcefiles_in("output", "expected", "out");
+	convert_sourcefiles_in("input", inputdir, "sql", "sql");
+	convert_sourcefiles_in("output", outputdir, "expected", "out");
 }
 
 /*
