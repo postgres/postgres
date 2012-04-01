5633df2582bedc93c2fb37555b7376eb51518bd5
Peter Eisentraut <peter_e@gmx.net>
2012-04-02 02:39:34 +0300

    Fix recently introduced typo in NLS file lists

--- src/bin/psql/nls.mk
+++ src/bin/psql/nls.mk
@@ -3,7 +3,7 @@ CATALOG_NAME     = psql
 AVAIL_LANGUAGES  = cs de es fr ja pt_BR sv tr zh_CN zh_TW
 GETTEXT_FILES    = command.c common.c copy.c help.c input.c large_obj.c \
                    mainloop.c print.c psqlscan.c startup.c describe.c sql_help.h sql_help.c \
-                   tab-complete.c variable.c \
+                   tab-complete.c variables.c \
                    ../../port/exec.c
 GETTEXT_TRIGGERS = N_ psql_error simple_prompt
 GETTEXT_FLAGS    = psql_error:1:c-format
