1540d3bf4d405278533377ba81dbbf1c857eaec0
Peter Eisentraut <peter_e@gmx.net>
2012-05-10 20:07:56 +0300

    PL/Python: Update incorrect comment
    Jan Urbański

--- src/pl/plpython/plpy_resultobject.h
+++ src/pl/plpython/plpy_resultobject.h
@@ -13,7 +13,7 @@ typedef struct PLyResultObject
 	PyObject_HEAD
 	/* HeapTuple *tuples; */
 	PyObject   *nrows;			/* number of rows returned by query */
-	PyObject   *rows;			/* data rows, or None if no data returned */
+	PyObject   *rows;			/* data rows, or empty list if no data returned */
 	PyObject   *status;			/* query status, SPI_OK_*, or SPI_ERR_* */
 	TupleDesc	tupdesc;
 } PLyResultObject;
