0bf8eb2a15875bc3f4d5638da73cb34ee9ca868e
Tom Lane <tgl@sss.pgh.pa.us>
2012-07-12 14:29:58 -0400

    Fix walsender processes to establish a SIGALRM handler.
    Walsenders must have working SIGALRM handling during InitPostgres,
    but they set the handler to SIG_IGN so that nothing would happen
    if a timeout was reached.  This could result in two failure modes:
    
    * If a walsender participated in a deadlock during its authentication
    transaction, and was the last to wait in the deadly embrace, the deadlock
    would not get cleared automatically.  This would require somebody to be
    trying to take out AccessExclusiveLock on multiple system catalogs, so
    it's not very probable.
    
    * If a client failed to respond to a walsender's authentication challenge,
    the intended disconnect after AuthenticationTimeout wouldn't happen, and
    the walsender would wait indefinitely for the client.
    
    For the moment, fix in back branches only, since this is fixed in a
    different way in the timeout-infrastructure patch that's awaiting
    application to HEAD.  If we choose not to apply that, then we'll need
    to do this in HEAD as well.

--- src/backend/replication/walsender.c
+++ src/backend/replication/walsender.c
@@ -1370,7 +1370,7 @@ WalSndSignals(void)
 	pqsignal(SIGINT, SIG_IGN);	/* not used */
 	pqsignal(SIGTERM, WalSndShutdownHandler);	/* request shutdown */
 	pqsignal(SIGQUIT, WalSndQuickDieHandler);	/* hard crash time */
-	pqsignal(SIGALRM, SIG_IGN);
+	pqsignal(SIGALRM, handle_sig_alarm);
 	pqsignal(SIGPIPE, SIG_IGN);
 	pqsignal(SIGUSR1, WalSndXLogSendHandler);	/* request WAL sending */
 	pqsignal(SIGUSR2, WalSndLastCycleHandler);	/* request a last cycle and
