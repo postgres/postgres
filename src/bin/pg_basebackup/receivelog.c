postgres/postgres

commit 16282ae688de2b320cf176e9be8a89e4dfc60698

Make pg_recievexlog by default loop on connection failures
Avoids the need for an external script in the most common
scenario. Behavior can be overridden using the -n/--noloop
commandline parameter.

src/bin/pg_basebackup/streamutil.c
@@ -65,6 +65,11 @@ xmalloc0(int size)
 }
 
 
+/*
+ * Connect to the server. Returns a valid PGconn pointer if connected,
+ * or NULL on non-permanent error. On permanent error, the function will
+ * call exit(1) directly.
+ */
 PGconn *
 GetConnection(void)
 {
@@ -151,7 +156,7 @@ GetConnection(void)
 		{
 			fprintf(stderr, _("%s: could not connect to server: %s\n"),
 					progname, PQerrorMessage(tmpconn));
-			exit(1);
+			return NULL;
 		}
 
 		/* Connection ok! */

doc/src/sgml/ref/pg_receivexlog.sgml
@@ -58,6 +58,14 @@ PostgreSQL documentation
    configured with <xref linkend="guc-max-wal-senders"> set high enough to
    leave at least one session available for the stream.
   </para>
+
+  <para>
+   If the connection is lost, or if it cannot be initially established,
+   with a non-fatal error, <application>pg_receivexlog</application> will
+   retry the connection indefinitely, and reestablish streaming as soon
+   as possible. To avoid this behavior, use the <literal>-n</literal>
+   parameter.
+  </para>
  </refsect1>
 
  <refsect1>
@@ -86,6 +94,17 @@ PostgreSQL documentation
     The following command-line options control the running of the program.
 
     <variablelist>
+     <varlistentry>
+      <term><option>-n</option></term>
+      <term><option>--noloop</option></term>
+      <listitem>
+       <para>
+        Don't loop on connection errors. Instead, exit right away with
+        an error.
+       </para>
+      </listitem>
+     </varlistentry>
+
      <varlistentry>
       <term><option>-v</option></term>
       <term><option>--verbose</option></term>

src/bin/pg_basebackup/pg_basebackup.c
@@ -279,6 +279,9 @@ StartLogStreamer(char *startpos, uint32 timeline, char *sysidentifier)
 
 	/* Get a second connection */
 	param->bgconn = GetConnection();
+	if (!param->bgconn)
+		/* Error message already written in GetConnection() */
+		exit(1);
 
 	/*
 	 * Always in plain format, so we can write to basedir/pg_xlog. But the
@@ -915,6 +918,9 @@ BaseBackup(void)
 	 * Connect in replication mode to the server
 	 */
 	conn = GetConnection();
+	if (!conn)
+		/* Error message already written in GetConnection() */
+		exit(1);
 
 	/*
 	 * Run IDENTIFY_SYSTEM so we can get the timeline

src/bin/pg_basebackup/pg_receivexlog.c
@@ -33,9 +33,13 @@
 
 #include "getopt_long.h"
 
+/* Time to sleep between reconnection attempts */
+#define RECONNECT_SLEEP_TIME 5
+
 /* Global options */
 char	   *basedir = NULL;
 int			verbose = 0;
+int			noloop = 0;
 int			standby_message_timeout = 10;		/* 10 sec = default */
 volatile bool time_to_abort = false;
 
@@ -55,6 +59,7 @@ usage(void)
 	printf(_("\nOptions controlling the output:\n"));
 	printf(_("  -D, --dir=directory       receive xlog files into this directory\n"));
 	printf(_("\nGeneral options:\n"));
+	printf(_("  -n, --noloop              do not loop on connection lost\n"));
 	printf(_("  -v, --verbose             output verbose messages\n"));
 	printf(_("  -?, --help                show this help, then exit\n"));
 	printf(_("  -V, --version             output version information, then exit\n"));
@@ -214,6 +219,9 @@ StreamLog(void)
 	 * Connect in replication mode to the server
 	 */
 	conn = GetConnection();
+	if (!conn)
+		/* Error message already written in GetConnection() */
+		return;
 
 	/*
 	 * Run IDENTIFY_SYSTEM so we can get the timeline and current xlog
@@ -289,6 +297,7 @@ main(int argc, char **argv)
 		{"host", required_argument, NULL, 'h'},
 		{"port", required_argument, NULL, 'p'},
 		{"username", required_argument, NULL, 'U'},
+		{"noloop", no_argument, NULL, 'n'},
 		{"no-password", no_argument, NULL, 'w'},
 		{"password", no_argument, NULL, 'W'},
 		{"statusint", required_argument, NULL, 's'},
@@ -317,7 +326,7 @@ main(int argc, char **argv)
 		}
 	}
 
-	while ((c = getopt_long(argc, argv, "D:h:p:U:s:wWv",
+	while ((c = getopt_long(argc, argv, "D:h:p:U:s:nwWv",
 							long_options, &option_index)) != -1)
 	{
 		switch (c)
@@ -354,6 +363,9 @@ main(int argc, char **argv)
 							progname, optarg);
 					exit(1);
 				}
+				break;
+			case 'n':
+				noloop = 1;
 				break;
 			case 'v':
 				verbose++;
@@ -397,7 +409,28 @@ main(int argc, char **argv)
 	pqsignal(SIGINT, sigint_handler);
 #endif
 
-	StreamLog();
+	while (true)
+	{
+		StreamLog();
+		if (time_to_abort)
+			/*
+			 * We've been Ctrl-C'ed. That's not an error, so exit without
+			 * an errorcode.
+			 */
+			exit(0);
+		else if (noloop)
+		{
+			fprintf(stderr, _("%s: disconnected.\n"), progname);
+			exit(1);
+		}
+		else
+		{
+			fprintf(stderr, _("%s: disconnected. Waiting %d seconds to try again\n"),
+					progname, RECONNECT_SLEEP_TIME);
+			pg_usleep(RECONNECT_SLEEP_TIME * 1000000);
+		}
+	}
 
-	exit(0);
+	/* Never get here */
+	exit(2);
 }

commit 776bdc4c5c019a9556a6622a01a406c6c0fec4c9

Fix memory and file descriptor leaks in pg_receivexlog/pg_basebackup
When the internal loop mode was added, freeing memory and closing
filedescriptors before returning became important, and a few cases
in the code missed that.

This is a backpatch of commit 058a050e to the 9.2 branch, which seems to
have been neglected (in error, because the bugs it fixes were introduced
in commit 16282ae6 which is present in both master and 9.2).

Fujii Masao

src/bin/pg_basebackup/receivelog.c
@@ -40,6 +40,10 @@
 
 const XLogRecPtr InvalidXLogRecPtr = {0, 0};
 
+/* fd for currently open WAL file */
+static int		walfile = -1;
+
+
 /*
  * Open a new WAL file in the specified directory. Store the name
  * (not including the full directory) in namebuf. Assumes there is
@@ -97,6 +101,7 @@ open_walfile(XLogRecPtr startpoint, uint32 timeline, char *basedir, char *namebu
 		{
 			fprintf(stderr, _("%s: could not pad WAL segment %s: %s\n"),
 					progname, fn, strerror(errno));
+			free(zerobuf);
 			close(f);
 			unlink(fn);
 			return -1;
@@ -121,7 +126,7 @@ open_walfile(XLogRecPtr startpoint, uint32 timeline, char *basedir, char *namebu
  * completed writing the whole segment.
  */
 static bool
-close_walfile(int walfile, char *basedir, char *walname, bool segment_complete)
+close_walfile(char *basedir, char *walname, bool segment_complete)
 {
 	off_t		currpos = lseek(walfile, 0, SEEK_CUR);
 
@@ -143,8 +148,10 @@ close_walfile(int walfile, char *basedir, char *walname, bool segment_complete)
 	{
 		fprintf(stderr, _("%s: could not close file %s: %s\n"),
 				progname, walname, strerror(errno));
+		walfile = -1;
 		return false;
 	}
+	walfile = -1;
 
 	/*
 	 * Rename the .partial file only if we've completed writing the whole
@@ -271,7 +278,6 @@ ReceiveXlogStream(PGconn *conn, XLogRecPtr startpos, uint32 timeline, char *sysi
 	char		current_walfile_name[MAXPGPATH];
 	PGresult   *res;
 	char	   *copybuf = NULL;
-	int			walfile = -1;
 	int64		last_status = -1;
 	XLogRecPtr	blockpos = InvalidXLogRecPtr;
 
@@ -315,6 +321,7 @@ ReceiveXlogStream(PGconn *conn, XLogRecPtr startpos, uint32 timeline, char *sysi
 	{
 		fprintf(stderr, _("%s: could not start replication: %s\n"),
 				progname, PQresultErrorMessage(res));
+		PQclear(res);
 		return false;
 	}
 	PQclear(res);
@@ -341,9 +348,9 @@ ReceiveXlogStream(PGconn *conn, XLogRecPtr startpos, uint32 timeline, char *sysi
 		 */
 		if (stream_stop && stream_stop(blockpos, timeline, false))
 		{
-			if (walfile != -1)
+			if (walfile != -1 && !close_walfile(basedir, current_walfile_name, rename_partial))
 				/* Potential error message is written by close_walfile */
-				return close_walfile(walfile, basedir, current_walfile_name, rename_partial);
+				goto error;
 			return true;
 		}
 
@@ -370,7 +377,7 @@ ReceiveXlogStream(PGconn *conn, XLogRecPtr startpos, uint32 timeline, char *sysi
 			{
 				fprintf(stderr, _("%s: could not send feedback packet: %s"),
 						progname, PQerrorMessage(conn));
-				return false;
+				goto error;
 			}
 
 			last_status = now;
@@ -421,14 +428,14 @@ ReceiveXlogStream(PGconn *conn, XLogRecPtr startpos, uint32 timeline, char *sysi
 			{
 				fprintf(stderr, _("%s: select() failed: %s\n"),
 						progname, strerror(errno));
-				return false;
+				goto error;
 			}
 			/* Else there is actually data on the socket */
 			if (PQconsumeInput(conn) == 0)
 			{
 				fprintf(stderr, _("%s: could not receive data from WAL stream: %s\n"),
 						progname, PQerrorMessage(conn));
-				return false;
+				goto error;
 			}
 			continue;
 		}
@@ -439,7 +446,7 @@ ReceiveXlogStream(PGconn *conn, XLogRecPtr startpos, uint32 timeline, char *sysi
 		{
 			fprintf(stderr, _("%s: could not read copy data: %s\n"),
 					progname, PQerrorMessage(conn));
-			return false;
+			goto error;
 		}
 		if (copybuf[0] == 'k')
 		{
@@ -451,7 +458,7 @@ ReceiveXlogStream(PGconn *conn, XLogRecPtr startpos, uint32 timeline, char *sysi
 			{
 				fprintf(stderr, _("%s: keepalive message is incorrect size: %d\n"),
 						progname, r);
-				return false;
+				goto error;
 			}
 			continue;
 		}
@@ -459,13 +466,13 @@ ReceiveXlogStream(PGconn *conn, XLogRecPtr startpos, uint32 timeline, char *sysi
 		{
 			fprintf(stderr, _("%s: unrecognized streaming header: \"%c\"\n"),
 					progname, copybuf[0]);
-			return false;
+			goto error;
 		}
 		if (r < STREAMING_HEADER_SIZE + 1)
 		{
 			fprintf(stderr, _("%s: streaming header too small: %d\n"),
 					progname, r);
-			return false;
+			goto error;
 		}
 
 		/* Extract WAL location for this block */
@@ -483,7 +490,7 @@ ReceiveXlogStream(PGconn *conn, XLogRecPtr startpos, uint32 timeline, char *sysi
 			{
 				fprintf(stderr, _("%s: received xlog record for offset %u with no file open\n"),
 						progname, xlogoff);
-				return false;
+				goto error;
 			}
 		}
 		else
@@ -494,7 +501,7 @@ ReceiveXlogStream(PGconn *conn, XLogRecPtr startpos, uint32 timeline, char *sysi
 			{
 				fprintf(stderr, _("%s: got WAL data offset %08x, expected %08x\n"),
 						progname, xlogoff, (int) lseek(walfile, 0, SEEK_CUR));
-				return false;
+				goto error;
 			}
 		}
 
@@ -520,7 +527,7 @@ ReceiveXlogStream(PGconn *conn, XLogRecPtr startpos, uint32 timeline, char *sysi
 									   basedir, current_walfile_name);
 				if (walfile == -1)
 					/* Error logged by open_walfile */
-					return false;
+					goto error;
 			}
 
 			if (write(walfile,
@@ -532,7 +539,7 @@ ReceiveXlogStream(PGconn *conn, XLogRecPtr startpos, uint32 timeline, char *sysi
 						bytes_to_write,
 						current_walfile_name,
 						strerror(errno));
-				return false;
+				goto error;
 			}
 
 			/* Write was successful, advance our position */
@@ -544,11 +551,10 @@ ReceiveXlogStream(PGconn *conn, XLogRecPtr startpos, uint32 timeline, char *sysi
 			/* Did we reach the end of a WAL segment? */
 			if (blockpos.xrecoff % XLOG_SEG_SIZE == 0)
 			{
-				if (!close_walfile(walfile, basedir, current_walfile_name, false))
+				if (!close_walfile(basedir, current_walfile_name, false))
 					/* Error message written in close_walfile() */
-					return false;
+					goto error;
 
-				walfile = -1;
 				xlogoff = 0;
 
 				if (stream_stop != NULL)
@@ -577,8 +583,22 @@ ReceiveXlogStream(PGconn *conn, XLogRecPtr startpos, uint32 timeline, char *sysi
 	{
 		fprintf(stderr, _("%s: unexpected termination of replication stream: %s\n"),
 				progname, PQresultErrorMessage(res));
-		return false;
+		goto error;
 	}
 	PQclear(res);
+
+	if (copybuf != NULL)
+		PQfreemem(copybuf);
+	if (walfile != -1 && close(walfile) != 0)
+		fprintf(stderr, _("%s: could not close file %s: %s\n"),
+				progname, current_walfile_name, strerror(errno));
 	return true;
+
+error:
+	if (copybuf != NULL)
+		PQfreemem(copybuf);
+	if (walfile != -1 && close(walfile) != 0)
+		fprintf(stderr, _("%s: could not close file %s: %s\n"),
+				progname, current_walfile_name, strerror(errno));
+	return false;
 }

src/bin/pg_basebackup/streamutil.c
@@ -143,6 +143,17 @@ GetConnection(void)
 
 		tmpconn = PQconnectdbParams(keywords, values, true);
 
+		/*
+		 * If there is too little memory even to allocate the PGconn object
+		 * and PQconnectdbParams returns NULL, we call exit(1) directly.
+		 */
+		if (!tmpconn)
+		{
+			fprintf(stderr, _("%s: could not connect to server\n"),
+					progname);
+			exit(1);
+		}
+
 		if (PQstatus(tmpconn) == CONNECTION_BAD &&
 			PQconnectionNeedsPassword(tmpconn) &&
 			dbgetpassword != -1)
@@ -154,8 +165,11 @@ GetConnection(void)
 
 		if (PQstatus(tmpconn) != CONNECTION_OK)
 		{
-			fprintf(stderr, _("%s: could not connect to server: %s"),
+			fprintf(stderr, _("%s: could not connect to server: %s\n"),
 					progname, PQerrorMessage(tmpconn));
+			PQfinish(tmpconn);
+			free(values);
+			free(keywords);
 			return NULL;
 		}
 