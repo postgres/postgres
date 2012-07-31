63aba79c7f1f06422b22e2b44fdcb563bbc3f7a5
Tom Lane <tgl@sss.pgh.pa.us>
2012-07-31 14:36:58 -0400

    Fix syslogger so that log_truncate_on_rotation works in the first rotation.
    In the original coding of the log rotation stuff, we did not bother to make
    the truncation logic work for the very first rotation after postmaster
    start (or after a syslogger crash and restart).  It just always appended
    in that case.  It did not seem terribly important at the time, but we've
    recently had two separate complaints from people who expected it to work
    unsurprisingly.  (Both users tend to restart the postmaster about as often
    as a log rotation is configured to happen, which is maybe not typical use,
    but still...)  Since the initial log file is opened in the postmaster,
    fixing this requires passing down some more state to the syslogger child
    process.
    
    It's always been like this, so back-patch to all supported branches.

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -437,6 +437,7 @@ typedef struct
 	pid_t		PostmasterPid;
 	TimestampTz PgStartTime;
 	TimestampTz PgReloadTime;
+	pg_time_t	first_syslogger_file_time;
 	bool		redirection_done;
 	bool		IsBinaryUpgrade;
 	int			max_safe_fds;
@@ -4704,7 +4705,7 @@ MaxLivePostmasterChildren(void)
 
 /*
  * The following need to be available to the save/restore_backend_variables
- * functions
+ * functions.  They are marked NON_EXEC_STATIC in their home modules.
  */
 extern slock_t *ShmemLock;
 extern LWLock *LWLockArray;
@@ -4712,6 +4713,7 @@ extern slock_t *ProcStructLock;
 extern PGPROC *AuxiliaryProcs;
 extern PMSignalData *PMSignalState;
 extern pgsocket pgStatSock;
+extern pg_time_t first_syslogger_file_time;
 
 #ifndef WIN32
 #define write_inheritable_socket(dest, src, childpid) ((*(dest) = (src)), true)
@@ -4764,6 +4766,7 @@ save_backend_variables(BackendParameters *param, Port *port,
 	param->PostmasterPid = PostmasterPid;
 	param->PgStartTime = PgStartTime;
 	param->PgReloadTime = PgReloadTime;
+	param->first_syslogger_file_time = first_syslogger_file_time;
 
 	param->redirection_done = redirection_done;
 	param->IsBinaryUpgrade = IsBinaryUpgrade;
@@ -4988,6 +4991,7 @@ restore_backend_variables(BackendParameters *param, Port *port)
 	PostmasterPid = param->PostmasterPid;
 	PgStartTime = param->PgStartTime;
 	PgReloadTime = param->PgReloadTime;
+	first_syslogger_file_time = param->first_syslogger_file_time;
 
 	redirection_done = param->redirection_done;
 	IsBinaryUpgrade = param->IsBinaryUpgrade;
