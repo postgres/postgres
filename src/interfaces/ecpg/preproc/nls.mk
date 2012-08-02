095bcf93a8cdf71b7809877a14f94c9cbf7c47fd
Peter Eisentraut <peter_e@gmx.net>
2012-08-02 23:01:01 +0300

    Translation updates

--- src/interfaces/ecpg/preproc/nls.mk
+++ src/interfaces/ecpg/preproc/nls.mk
@@ -1,6 +1,6 @@
 # src/interfaces/ecpg/preproc/nls.mk
 CATALOG_NAME     = ecpg
-AVAIL_LANGUAGES  = de es fr it ja ko pt_BR tr zh_CN zh_TW
+AVAIL_LANGUAGES  = cs de es fr it ja ko pl pt_BR ru tr zh_CN zh_TW
 GETTEXT_FILES    = descriptor.c ecpg.c pgc.c preproc.c type.c variable.c
 GETTEXT_TRIGGERS = mmerror:3
 GETTEXT_FLAGS    = mmerror:3:c-format
