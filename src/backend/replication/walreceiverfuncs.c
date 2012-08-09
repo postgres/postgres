6a2cbe12358d3e3c356d863c8fc40c5f8daadb2a
Simon Riggs <simon@2ndQuadrant.com>
2012-08-09 17:03:10 +0100

    Ensure all replication message info is available and correct via WalRcv

--- src/backend/replication/walreceiverfuncs.c
+++ src/backend/replication/walreceiverfuncs.c
@@ -261,7 +261,7 @@ GetReplicationApplyDelay(void)
 
 	replayPtr = GetXLogReplayRecPtr(NULL);
 
-	if (XLByteLE(receivePtr, replayPtr))
+	if (XLByteEQ(receivePtr, replayPtr))
 		return 0;
 
 	TimestampDifference(GetCurrentChunkReplayStartTime(),
