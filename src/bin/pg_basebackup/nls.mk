2152001773ecddb838b4c7566e332dd184e7d769
Peter Eisentraut <peter_e@gmx.net>
2012-08-14 16:37:40 -0400

    Translation updates

--- src/bin/pg_basebackup/nls.mk
+++ src/bin/pg_basebackup/nls.mk
@@ -1,4 +1,4 @@
 # src/bin/pg_basebackup/nls.mk
 CATALOG_NAME     = pg_basebackup
-AVAIL_LANGUAGES  = de es fr ja
+AVAIL_LANGUAGES  = de es fr ja ru
 GETTEXT_FILES    = pg_basebackup.c pg_receivexlog.c receivelog.c streamutil.c
