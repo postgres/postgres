7665e82b6a6008d2df5fa67ab80d981de8b9c738
Tom Lane <tgl@sss.pgh.pa.us>
2012-08-15 19:01:29 -0400

    Fix rescan logic in nodeCtescan.
    The previous coding essentially assumed that nodes would be rescanned in
    the same order they were initialized in; or at least that the "leader" of
    a group of CTEscans would be rescanned before any others were required to
    execute.  Unfortunately, that isn't even a little bit true.  It's possible
    to devise queries in which the leader isn't rescanned until other CTEscans
    on the same CTE have run to completion, or even in which the leader never
    gets a rescan call at all.
    
    The fix makes the leader specially responsible only for initial creation
    and final destruction of the tuplestore; rescan resets are now a
    symmetrically shared responsibility.  This means that we might reset the
    tuplestore multiple times when restarting a plan subtree containing
    multiple CTEscans; but resetting an already-empty tuplestore is cheap
    enough that that doesn't seem like a problem.
    
    Per report from Adam Mackler; the new regression test cases are based on
    his example query.
    
    Back-patch to 8.4 where CTE scans were introduced.

--- src/test/regress/sql/with.sql
+++ src/test/regress/sql/with.sql
@@ -575,6 +575,62 @@ WITH RECURSIVE outermost(x) AS (
 SELECT * FROM outermost;
 
 --
+-- Test CTEs read in non-initialization orders
+--
+
+WITH RECURSIVE
+  tab(id_key,link) AS (VALUES (1,17), (2,17), (3,17), (4,17), (6,17), (5,17)),
+  iter (id_key, row_type, link) AS (
+      SELECT 0, 'base', 17
+    UNION ALL (
+      WITH remaining(id_key, row_type, link, min) AS (
+        SELECT tab.id_key, 'true'::text, iter.link, MIN(tab.id_key) OVER ()
+        FROM tab INNER JOIN iter USING (link)
+        WHERE tab.id_key > iter.id_key
+      ),
+      first_remaining AS (
+        SELECT id_key, row_type, link
+        FROM remaining
+        WHERE id_key=min
+      ),
+      effect AS (
+        SELECT tab.id_key, 'new'::text, tab.link
+        FROM first_remaining e INNER JOIN tab ON e.id_key=tab.id_key
+        WHERE e.row_type = 'false'
+      )
+      SELECT * FROM first_remaining
+      UNION ALL SELECT * FROM effect
+    )
+  )
+SELECT * FROM iter;
+
+WITH RECURSIVE
+  tab(id_key,link) AS (VALUES (1,17), (2,17), (3,17), (4,17), (6,17), (5,17)),
+  iter (id_key, row_type, link) AS (
+      SELECT 0, 'base', 17
+    UNION (
+      WITH remaining(id_key, row_type, link, min) AS (
+        SELECT tab.id_key, 'true'::text, iter.link, MIN(tab.id_key) OVER ()
+        FROM tab INNER JOIN iter USING (link)
+        WHERE tab.id_key > iter.id_key
+      ),
+      first_remaining AS (
+        SELECT id_key, row_type, link
+        FROM remaining
+        WHERE id_key=min
+      ),
+      effect AS (
+        SELECT tab.id_key, 'new'::text, tab.link
+        FROM first_remaining e INNER JOIN tab ON e.id_key=tab.id_key
+        WHERE e.row_type = 'false'
+      )
+      SELECT * FROM first_remaining
+      UNION ALL SELECT * FROM effect
+    )
+  )
+SELECT * FROM iter;
+
+--
 -- Data-modifying statements in WITH
 --
 
