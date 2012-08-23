d6a3863506599a4d22dd62e0601da9ab2ed933da
Tom Lane <tgl@sss.pgh.pa.us>
2012-08-23 17:25:33 -0400

    Fix cascading privilege revoke to notice when privileges are still held.
    If we revoke a grant option from some role X, but X still holds the option
    via another grant, we should not recursively revoke the privilege from
    role(s) Y that X had granted it to.  This was supposedly fixed as one
    aspect of commit 4b2dafcc0b1a579ef5daaa2728223006d1ff98e9, but I must not
    have tested it, because in fact that code never worked: it forgot to shift
    the grant-option bits back over when masking the bits being revoked.
    
    Per bug #6728 from Daniel German.  Back-patch to all active branches,
    since this has been wrong since 8.0.

--- src/test/regress/sql/privileges.sql
+++ src/test/regress/sql/privileges.sql
@@ -469,6 +469,30 @@ SELECT has_table_privilege('regressuser3', 'atest4', 'SELECT'); -- false
 SELECT has_table_privilege('regressuser1', 'atest4', 'SELECT WITH GRANT OPTION'); -- true
 
 
+-- test that dependent privileges are revoked (or not) properly
+\c -
+
+set session role regressuser1;
+create table dep_priv_test (a int);
+grant select on dep_priv_test to regressuser2 with grant option;
+grant select on dep_priv_test to regressuser3 with grant option;
+set session role regressuser2;
+grant select on dep_priv_test to regressuser4 with grant option;
+set session role regressuser3;
+grant select on dep_priv_test to regressuser4 with grant option;
+set session role regressuser4;
+grant select on dep_priv_test to regressuser5;
+\dp dep_priv_test
+set session role regressuser2;
+revoke select on dep_priv_test from regressuser4 cascade;
+\dp dep_priv_test
+set session role regressuser3;
+revoke select on dep_priv_test from regressuser4 cascade;
+\dp dep_priv_test
+set session role regressuser1;
+drop table dep_priv_test;
+
+
 -- clean up
 
 \c
