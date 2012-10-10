864db11683df44dae51f8f9fc8a4c124e100d2f3
Tom Lane <tgl@sss.pgh.pa.us>
2012-10-10 17:04:37 -0400

    Update obsolete comment.
    We no longer use GetNewOidWithIndex on pg_largeobject; rather,
    pg_largeobject_metadata's regular OID column is considered the repository
    of OIDs for large objects.  The special functionality is still needed for
    TOAST tables however.

--- src/backend/catalog/catalog.c
+++ src/backend/catalog/catalog.c
@@ -472,10 +472,10 @@ GetNewOid(Relation relation)
  *
  * This is exported separately because there are cases where we want to use
  * an index that will not be recognized by RelationGetOidIndex: TOAST tables
- * and pg_largeobject have indexes that are usable, but have multiple columns
- * and are on ordinary columns rather than a true OID column.  This code
- * will work anyway, so long as the OID is the index's first column.  The
- * caller must pass in the actual heap attnum of the OID column, however.
+ * have indexes that are usable, but have multiple columns and are on
+ * ordinary columns rather than a true OID column.  This code will work
+ * anyway, so long as the OID is the index's first column.  The caller must
+ * pass in the actual heap attnum of the OID column, however.
  *
  * Caller must have a suitable lock on the relation.
  */
