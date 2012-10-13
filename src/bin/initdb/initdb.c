288367427472f3d300c51ff9941eb486cfc01c6a
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2012-10-13 12:48:14 +0300

    Fix race condition in pg_ctl reading postmaster.pid.
    If postmaster changed postmaster.pid while pg_ctl was reading it, pg_ctl
    could overrun the buffer it allocated for the file. Fix by reading the
    whole file to memory with one read() call.
    
    initdb contains an identical copy of the readfile() function, but the files
    that initdb reads are static, not modified concurrently. Nevertheless, add
    a simple bounds-check there, if only to silence static analysis tools.
    
    Per report from Dave Vitek. Backpatch to all supported branches.

--- src/bin/initdb/initdb.c
+++ src/bin/initdb/initdb.c
@@ -373,6 +373,7 @@ readfile(const char *path)
 	int			maxlength = 1,
 				linelen = 0;
 	int			nlines = 0;
+	int			n;
 	char	  **result;
 	char	   *buffer;
 	int			c;
@@ -410,13 +411,13 @@ readfile(const char *path)
 
 	/* now reprocess the file and store the lines */
 	rewind(infile);
-	nlines = 0;
-	while (fgets(buffer, maxlength + 1, infile) != NULL)
-		result[nlines++] = xstrdup(buffer);
+	n = 0;
+	while (fgets(buffer, maxlength + 1, infile) != NULL && n < nlines)
+		result[n++] = xstrdup(buffer);
 
 	fclose(infile);
 	free(buffer);
-	result[nlines] = NULL;
+	result[n] = NULL;
 
 	return result;
 }
