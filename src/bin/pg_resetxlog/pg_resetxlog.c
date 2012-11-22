postgres/postgres

commit eb5949d190e80360386113fde0f05854f0c9824d

Arrange for the postmaster (and standalone backends, initdb, etc) to chdir into PGDATA and subsequently use relative paths instead of absolute paths to access all files under PGDATA.  This seems to give a small performance improvement, and it should make the system more robust against naive DBAs doing things like moving a database directory that has a live postmaster in it.  Per recent discussion.

src/bin/pg_resetxlog/pg_resetxlog.c
@@ -23,7 +23,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/bin/pg_resetxlog/pg_resetxlog.c,v 1.34 2005/06/08 15:50:27 tgl Exp $
+ * $PostgreSQL: pgsql/src/bin/pg_resetxlog/pg_resetxlog.c,v 1.35 2005/07/04 04:51:51 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -49,9 +49,6 @@
 extern int	optind;
 extern char *optarg;
 
-
-char		XLogDir[MAXPGPATH]; /* not static, see xlog_internal.h */
-static char ControlFilePath[MAXPGPATH];
 
 static ControlFileData ControlFile;		/* pg_control values */
 static uint32 newXlogId,
@@ -236,8 +233,13 @@ main(int argc, char *argv[])
 #endif
 
 	DataDir = argv[optind];
-	snprintf(XLogDir, MAXPGPATH, "%s/pg_xlog", DataDir);
-	snprintf(ControlFilePath, MAXPGPATH, "%s/global/pg_control", DataDir);
+
+	if (chdir(DataDir) < 0)
+	{
+		fprintf(stderr, _("%s: could not change directory to \"%s\": %s\n"),
+				progname, DataDir, strerror(errno));
+		exit(1);
+	}
 
 	/*
 	 * Check for a postmaster lock file --- if there is one, refuse to
@@ -348,7 +350,7 @@ ReadControlFile(void)
 	char	   *buffer;
 	pg_crc32	crc;
 
-	if ((fd = open(ControlFilePath, O_RDONLY)) < 0)
+	if ((fd = open(XLOG_CONTROL_FILE, O_RDONLY)) < 0)
 	{
 		/*
 		 * If pg_control is not there at all, or we can't read it, the
@@ -356,12 +358,12 @@ ReadControlFile(void)
 		 * can do "touch pg_control" to force us to proceed.
 		 */
 		fprintf(stderr, _("%s: could not open file \"%s\" for reading: %s\n"),
-				progname, ControlFilePath, strerror(errno));
+				progname, XLOG_CONTROL_FILE, strerror(errno));
 		if (errno == ENOENT)
 			fprintf(stderr, _("If you are sure the data directory path is correct, execute\n"
 							  "  touch %s\n"
 							  "and try again.\n"),
-					ControlFilePath);
+					XLOG_CONTROL_FILE);
 		exit(1);
 	}
 
@@ -372,7 +374,7 @@ ReadControlFile(void)
 	if (len < 0)
 	{
 		fprintf(stderr, _("%s: could not read file \"%s\": %s\n"),
-				progname, ControlFilePath, strerror(errno));
+				progname, XLOG_CONTROL_FILE, strerror(errno));
 		exit(1);
 	}
 	close(fd);
@@ -598,9 +600,11 @@ RewriteControlFile(void)
 	memset(buffer, 0, BLCKSZ);
 	memcpy(buffer, &ControlFile, sizeof(ControlFileData));
 
-	unlink(ControlFilePath);
+	unlink(XLOG_CONTROL_FILE);
 
-	fd = open(ControlFilePath, O_RDWR | O_CREAT | O_EXCL | PG_BINARY, S_IRUSR | S_IWUSR);
+	fd = open(XLOG_CONTROL_FILE,
+			  O_RDWR | O_CREAT | O_EXCL | PG_BINARY,
+			  S_IRUSR | S_IWUSR);
 	if (fd < 0)
 	{
 		fprintf(stderr, _("%s: could not create pg_control file: %s\n"),
@@ -639,11 +643,11 @@ KillExistingXLOG(void)
 	struct dirent *xlde;
 	char		path[MAXPGPATH];
 
-	xldir = opendir(XLogDir);
+	xldir = opendir(XLOGDIR);
 	if (xldir == NULL)
 	{
 		fprintf(stderr, _("%s: could not open directory \"%s\": %s\n"),
-				progname, XLogDir, strerror(errno));
+				progname, XLOGDIR, strerror(errno));
 		exit(1);
 	}
 
@@ -653,7 +657,7 @@ KillExistingXLOG(void)
 		if (strlen(xlde->d_name) == 24 &&
 			strspn(xlde->d_name, "0123456789ABCDEF") == 24)
 		{
-			snprintf(path, MAXPGPATH, "%s/%s", XLogDir, xlde->d_name);
+			snprintf(path, MAXPGPATH, "%s/%s", XLOGDIR, xlde->d_name);
 			if (unlink(path) < 0)
 			{
 				fprintf(stderr, _("%s: could not delete file \"%s\": %s\n"),
@@ -676,7 +680,7 @@ KillExistingXLOG(void)
 	if (errno)
 	{
 		fprintf(stderr, _("%s: could not read from directory \"%s\": %s\n"),
-				progname, XLogDir, strerror(errno));
+				progname, XLOGDIR, strerror(errno));
 		exit(1);
 	}
 	closedir(xldir);

contrib/dbsize/dbsize.c
@@ -5,7 +5,7 @@
  * Copyright (c) 2002-2005, PostgreSQL Global Development Group
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/contrib/dbsize/dbsize.c,v 1.18 2005/06/19 21:34:00 tgl Exp $
+ *	  $PostgreSQL: pgsql/contrib/dbsize/dbsize.c,v 1.19 2005/07/04 04:51:43 tgl Exp $
  *
  */
 
@@ -23,9 +23,6 @@
 #include "utils/builtins.h"
 #include "utils/syscache.h"
 
-
-/* hack to make it compile under Win32 */
-extern DLLIMPORT char *DataDir;
 
 Datum pg_tablespace_size(PG_FUNCTION_ARGS);
 Datum pg_database_size(PG_FUNCTION_ARGS);
@@ -91,11 +88,11 @@ calculate_database_size(Oid dbOid)
 	/* Shared storage in pg_global is not counted */
 
 	/* Include pg_default storage */
-	snprintf(pathname, MAXPGPATH, "%s/base/%u", DataDir, dbOid);
+	snprintf(pathname, MAXPGPATH, "base/%u", dbOid);
 	totalsize += db_dir_size(pathname);
 
 	/* Scan the non-default tablespaces */
-	snprintf(pathname, MAXPGPATH, "%s/pg_tblspc", DataDir);
+	snprintf(pathname, MAXPGPATH, "pg_tblspc");
 	dirdesc = AllocateDir(pathname);
 
 	while ((direntry = ReadDir(dirdesc, pathname)) != NULL)
@@ -104,8 +101,8 @@ calculate_database_size(Oid dbOid)
 			strcmp(direntry->d_name, "..") == 0)
 		    continue;
 
-		snprintf(pathname, MAXPGPATH, "%s/pg_tblspc/%s/%u",
-				 DataDir, direntry->d_name, dbOid);
+		snprintf(pathname, MAXPGPATH, "pg_tblspc/%s/%u",
+				 direntry->d_name, dbOid);
 		totalsize += db_dir_size(pathname);
 	}
 
@@ -134,11 +131,11 @@ pg_tablespace_size(PG_FUNCTION_ARGS)
     struct dirent *direntry;
 
 	if (tblspcOid == DEFAULTTABLESPACE_OID)
-	    snprintf(tblspcPath, MAXPGPATH, "%s/base", DataDir);
+	    snprintf(tblspcPath, MAXPGPATH, "base");
 	else if (tblspcOid == GLOBALTABLESPACE_OID)
-	    snprintf(tblspcPath, MAXPGPATH, "%s/global", DataDir);
+	    snprintf(tblspcPath, MAXPGPATH, "global");
 	else
-		snprintf(tblspcPath, MAXPGPATH, "%s/pg_tblspc/%u", DataDir, tblspcOid);
+		snprintf(tblspcPath, MAXPGPATH, "pg_tblspc/%u", tblspcOid);
 
 	dirdesc = AllocateDir(tblspcPath);
 
@@ -208,12 +205,12 @@ calculate_relation_size(Oid tblspcOid, Oid relnodeOid)
 		tblspcOid = MyDatabaseTableSpace;
 
 	if (tblspcOid == DEFAULTTABLESPACE_OID)
-	    snprintf(dirpath, MAXPGPATH, "%s/base/%u", DataDir, MyDatabaseId);
+	    snprintf(dirpath, MAXPGPATH, "base/%u", MyDatabaseId);
 	else if (tblspcOid == GLOBALTABLESPACE_OID)
-	    snprintf(dirpath, MAXPGPATH, "%s/global", DataDir);
+	    snprintf(dirpath, MAXPGPATH, "global");
 	else
-	    snprintf(dirpath, MAXPGPATH, "%s/pg_tblspc/%u/%u",
-				 DataDir, tblspcOid, MyDatabaseId);
+	    snprintf(dirpath, MAXPGPATH, "pg_tblspc/%u/%u",
+				 tblspcOid, MyDatabaseId);
 
 	for (segcount = 0 ;; segcount++)
 	{

src/backend/access/transam/slru.c
@@ -48,7 +48,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/backend/access/transam/slru.c,v 1.25 2005/06/19 21:34:01 tgl Exp $
+ * $PostgreSQL: pgsql/src/backend/access/transam/slru.c,v 1.26 2005/07/04 04:51:44 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -190,7 +190,7 @@ SimpleLruInit(SlruCtl ctl, const char *name,
 	 */
 	ctl->shared = shared;
 	ctl->do_fsync = true;		/* default behavior */
-	snprintf(ctl->Dir, MAXPGPATH, "%s/%s", DataDir, subdir);
+	StrNCpy(ctl->Dir, subdir, sizeof(ctl->Dir));
 }
 
 /*

src/backend/access/transam/twophase.c
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  * IDENTIFICATION
- *		$PostgreSQL: pgsql/src/backend/access/transam/twophase.c,v 1.7 2005/06/28 05:08:51 tgl Exp $
+ *		$PostgreSQL: pgsql/src/backend/access/transam/twophase.c,v 1.8 2005/07/04 04:51:44 tgl Exp $
  *
  * NOTES
  *		Each global transaction is associated with a global transaction
@@ -661,7 +661,7 @@ TwoPhaseGetDummyProc(TransactionId xid)
 /************************************************************************/
 
 #define TwoPhaseFilePath(path, xid) \
-	snprintf(path, MAXPGPATH, "%s/%s/%08X", DataDir, TWOPHASE_DIR, xid)
+	snprintf(path, MAXPGPATH, TWOPHASE_DIR "/%08X", xid)
 
 /*
  * 2PC state file format:
@@ -1434,14 +1434,11 @@ PrescanPreparedTransactions(void)
 {
 	TransactionId origNextXid = ShmemVariableCache->nextXid;
 	TransactionId result = origNextXid;
-	char	dir[MAXPGPATH];
 	DIR		*cldir;
 	struct dirent *clde;
 
-	snprintf(dir, MAXPGPATH, "%s/%s", DataDir, TWOPHASE_DIR);
-
-	cldir = AllocateDir(dir);
-	while ((clde = ReadDir(cldir, dir)) != NULL)
+	cldir = AllocateDir(TWOPHASE_DIR);
+	while ((clde = ReadDir(cldir, TWOPHASE_DIR)) != NULL)
 	{
 		if (strlen(clde->d_name) == 8 &&
 			strspn(clde->d_name, "0123456789ABCDEF") == 8)
@@ -1540,7 +1537,7 @@ RecoverPreparedTransactions(void)
 	DIR		*cldir;
 	struct dirent *clde;
 
-	snprintf(dir, MAXPGPATH, "%s/%s", DataDir, TWOPHASE_DIR);
+	snprintf(dir, MAXPGPATH, "%s", TWOPHASE_DIR);
 
 	cldir = AllocateDir(dir);
 	while ((clde = ReadDir(cldir, dir)) != NULL)

src/backend/access/transam/xlog.c
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/backend/access/transam/xlog.c,v 1.205 2005/06/30 00:00:50 tgl Exp $
+ * $PostgreSQL: pgsql/src/backend/access/transam/xlog.c,v 1.206 2005/07/04 04:51:44 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -89,6 +89,12 @@
 #define DEFAULT_SYNC_METHOD		  SYNC_METHOD_FSYNC_WRITETHROUGH
 #define DEFAULT_SYNC_FLAGBIT	  0
 #endif
+
+
+/* File path names (all relative to $PGDATA) */
+#define BACKUP_LABEL_FILE		"backup_label"
+#define RECOVERY_COMMAND_FILE	"recovery.conf"
+#define RECOVERY_COMMAND_DONE	"recovery.done"
 
 
 /* User-settable parameters */
@@ -377,11 +383,6 @@ static ControlFileData *ControlFile = NULL;
 
 #define NextBufIdx(idx)		\
 		(((idx) == XLogCtl->XLogCacheBlck) ? 0 : ((idx) + 1))
-
-
-/* File path names */
-char		XLogDir[MAXPGPATH];
-static char ControlFilePath[MAXPGPATH];
 
 /*
  * Private, possibly out-of-date copy of shared LogwrtResult.
@@ -991,7 +992,7 @@ XLogCheckBuffer(XLogRecData *rdata,
  *
  * The name of the notification file is the message that will be picked up
  * by the archiver, e.g. we write 0000000100000001000000C6.ready
- * and the archiver then knows to archive XLogDir/0000000100000001000000C6,
+ * and the archiver then knows to archive XLOGDIR/0000000100000001000000C6,
  * then when complete, rename it to 0000000100000001000000C6.done
  */
 static void
@@ -1645,7 +1646,7 @@ XLogFileInit(uint32 log, uint32 seg,
 	 * up pre-creating an extra log segment.  That seems OK, and better
 	 * than holding the lock throughout this lengthy process.
 	 */
-	snprintf(tmppath, MAXPGPATH, "%s/xlogtemp.%d", XLogDir, (int)getpid());
+	snprintf(tmppath, MAXPGPATH, XLOGDIR "/xlogtemp.%d", (int) getpid());
 
 	unlink(tmppath);
 
@@ -1768,7 +1769,7 @@ XLogFileCopy(uint32 log, uint32 seg,
 	/*
 	 * Copy into a temp file name.
 	 */
-	snprintf(tmppath, MAXPGPATH, "%s/xlogtemp.%d", XLogDir, (int)getpid());
+	snprintf(tmppath, MAXPGPATH, XLOGDIR "/xlogtemp.%d", (int) getpid());
 
 	unlink(tmppath);
 
@@ -2039,8 +2040,8 @@ RestoreArchivedFile(char *path, const char *xlogfname,
 
 	/*
 	 * When doing archive recovery, we always prefer an archived log file
-	 * even if a file of the same name exists in XLogDir.  The reason is
-	 * that the file in XLogDir could be an old, un-filled or
+	 * even if a file of the same name exists in XLOGDIR.  The reason is
+	 * that the file in XLOGDIR could be an old, un-filled or
 	 * partly-filled version that was copied and restored as part of
 	 * backing up $PGDATA.
 	 *
@@ -2051,18 +2052,18 @@ RestoreArchivedFile(char *path, const char *xlogfname,
 	 * robustness, so we elect not to do this.
 	 *
 	 * If we cannot obtain the log file from the archive, however, we will
-	 * try to use the XLogDir file if it exists.  This is so that we can
+	 * try to use the XLOGDIR file if it exists.  This is so that we can
 	 * make use of log segments that weren't yet transferred to the
 	 * archive.
 	 *
 	 * Notice that we don't actually overwrite any files when we copy back
 	 * from archive because the recoveryRestoreCommand may inadvertently
 	 * restore inappropriate xlogs, or they may be corrupt, so we may wish
-	 * to fallback to the segments remaining in current XLogDir later. The
+	 * to fallback to the segments remaining in current XLOGDIR later. The
 	 * copy-from-archive filename is always the same, ensuring that we
 	 * don't run out of disk space on long recoveries.
 	 */
-	snprintf(xlogpath, MAXPGPATH, "%s/%s", XLogDir, recovername);
+	snprintf(xlogpath, MAXPGPATH, XLOGDIR "/%s", recovername);
 
 	/*
 	 * Make sure there is no existing file named recovername.
@@ -2136,7 +2137,7 @@ RestoreArchivedFile(char *path, const char *xlogfname,
 							 xlogRestoreCmd)));
 
 	/*
-	 * Copy xlog from archival storage to XLogDir
+	 * Copy xlog from archival storage to XLOGDIR
 	 */
 	rc = system(xlogRestoreCmd);
 	if (rc == 0)
@@ -2192,13 +2193,13 @@ RestoreArchivedFile(char *path, const char *xlogfname,
 
 	/*
 	 * if an archived file is not available, there might still be a
-	 * version of this file in XLogDir, so return that as the filename to
+	 * version of this file in XLOGDIR, so return that as the filename to
 	 * open.
 	 *
 	 * In many recovery scenarios we expect this to fail also, but if so that
 	 * just means we've reached the end of WAL.
 	 */
-	snprintf(path, MAXPGPATH, "%s/%s", XLogDir, xlogfname);
+	snprintf(path, MAXPGPATH, XLOGDIR "/%s", xlogfname);
 	return false;
 }
 
@@ -2257,16 +2258,16 @@ MoveOfflineLogs(uint32 log, uint32 seg, XLogRecPtr endptr,
 	XLByteToPrevSeg(endptr, endlogId, endlogSeg);
 	max_advance = XLOGfileslop;
 
-	xldir = AllocateDir(XLogDir);
+	xldir = AllocateDir(XLOGDIR);
 	if (xldir == NULL)
 		ereport(ERROR,
 				(errcode_for_file_access(),
 			errmsg("could not open transaction log directory \"%s\": %m",
-				   XLogDir)));
+				   XLOGDIR)));
 
 	XLogFileName(lastoff, ThisTimeLineID, log, seg);
 
-	while ((xlde = ReadDir(xldir, XLogDir)) != NULL)
+	while ((xlde = ReadDir(xldir, XLOGDIR)) != NULL)
 	{
 		/*
 		 * We ignore the timeline part of the XLOG segment identifiers in
@@ -2292,7 +2293,7 @@ MoveOfflineLogs(uint32 log, uint32 seg, XLogRecPtr endptr,
 
 			if (recycle)
 			{
-				snprintf(path, MAXPGPATH, "%s/%s", XLogDir, xlde->d_name);
+				snprintf(path, MAXPGPATH, XLOGDIR "/%s", xlde->d_name);
 
 				/*
 				 * Before deleting the file, see if it can be recycled as
@@ -2341,14 +2342,14 @@ RemoveOldBackupHistory(void)
 	struct dirent *xlde;
 	char		path[MAXPGPATH];
 
-	xldir = AllocateDir(XLogDir);
+	xldir = AllocateDir(XLOGDIR);
 	if (xldir == NULL)
 		ereport(ERROR,
 				(errcode_for_file_access(),
 			errmsg("could not open transaction log directory \"%s\": %m",
-				   XLogDir)));
+				   XLOGDIR)));
 
-	while ((xlde = ReadDir(xldir, XLogDir)) != NULL)
+	while ((xlde = ReadDir(xldir, XLOGDIR)) != NULL)
 	{
 		if (strlen(xlde->d_name) > 24 &&
 			strspn(xlde->d_name, "0123456789ABCDEF") == 24 &&
@@ -2361,7 +2362,7 @@ RemoveOldBackupHistory(void)
 				ereport(DEBUG2,
 					  (errmsg("removing transaction log backup history file \"%s\"",
 							  xlde->d_name)));
-				snprintf(path, MAXPGPATH, "%s/%s", XLogDir, xlde->d_name);
+				snprintf(path, MAXPGPATH, XLOGDIR "/%s", xlde->d_name);
 				unlink(path);
 				XLogArchiveCleanup(xlde->d_name);
 			}
@@ -3132,7 +3133,7 @@ writeTimeLineHistory(TimeLineID newTLI, TimeLineID parentTLI,
 	/*
 	 * Write into a temp file name.
 	 */
-	snprintf(tmppath, MAXPGPATH, "%s/xlogtemp.%d", XLogDir, (int)getpid());
+	snprintf(tmppath, MAXPGPATH, XLOGDIR "/xlogtemp.%d", (int) getpid());
 
 	unlink(tmppath);
 
@@ -3291,15 +3292,6 @@ writeTimeLineHistory(TimeLineID newTLI, TimeLineID parentTLI,
  * ReadControlFile() verifies they are correct.  We could split out the
  * I/O and compatibility-check functions, but there seems no need currently.
  */
-
-void
-XLOGPathInit(void)
-{
-	/* Init XLOG file paths */
-	snprintf(XLogDir, MAXPGPATH, "%s/pg_xlog", DataDir);
-	snprintf(ControlFilePath, MAXPGPATH, "%s/global/pg_control", DataDir);
-}
-
 static void
 WriteControlFile(void)
 {
@@ -3358,13 +3350,14 @@ WriteControlFile(void)
 	memset(buffer, 0, BLCKSZ);
 	memcpy(buffer, ControlFile, sizeof(ControlFileData));
 
-	fd = BasicOpenFile(ControlFilePath, O_RDWR | O_CREAT | O_EXCL | PG_BINARY,
+	fd = BasicOpenFile(XLOG_CONTROL_FILE,
+					   O_RDWR | O_CREAT | O_EXCL | PG_BINARY,
 					   S_IRUSR | S_IWUSR);
 	if (fd < 0)
 		ereport(PANIC,
 				(errcode_for_file_access(),
 				 errmsg("could not create control file \"%s\": %m",
-						ControlFilePath)));
+						XLOG_CONTROL_FILE)));
 
 	errno = 0;
 	if (write(fd, buffer, BLCKSZ) != BLCKSZ)
@@ -3397,12 +3390,14 @@ ReadControlFile(void)
 	/*
 	 * Read data...
 	 */
-	fd = BasicOpenFile(ControlFilePath, O_RDWR | PG_BINARY, S_IRUSR | S_IWUSR);
+	fd = BasicOpenFile(XLOG_CONTROL_FILE,
+					   O_RDWR | PG_BINARY,
+					   S_IRUSR | S_IWUSR);
 	if (fd < 0)
 		ereport(PANIC,
 				(errcode_for_file_access(),
 				 errmsg("could not open control file \"%s\": %m",
-						ControlFilePath)));
+						XLOG_CONTROL_FILE)));
 
 	if (read(fd, ControlFile, sizeof(ControlFileData)) != sizeof(ControlFileData))
 		ereport(PANIC,
@@ -3546,12 +3541,14 @@ UpdateControlFile(void)
 			   offsetof(ControlFileData, crc));
 	FIN_CRC32(ControlFile->crc);
 
-	fd = BasicOpenFile(ControlFilePath, O_RDWR | PG_BINARY, S_IRUSR | S_IWUSR);
+	fd = BasicOpenFile(XLOG_CONTROL_FILE,
+					   O_RDWR | PG_BINARY,
+					   S_IRUSR | S_IWUSR);
 	if (fd < 0)
 		ereport(PANIC,
 				(errcode_for_file_access(),
 				 errmsg("could not open control file \"%s\": %m",
-						ControlFilePath)));
+						XLOG_CONTROL_FILE)));
 
 	errno = 0;
 	if (write(fd, ControlFile, sizeof(ControlFileData)) != sizeof(ControlFileData))
@@ -3814,15 +3811,13 @@ str_time(time_t tnow)
 static void
 readRecoveryCommandFile(void)
 {
-	char		recoveryCommandFile[MAXPGPATH];
 	FILE	   *fd;
 	char		cmdline[MAXPGPATH];
 	TimeLineID	rtli = 0;
 	bool		rtliGiven = false;
 	bool		syntaxError = false;
 
-	snprintf(recoveryCommandFile, MAXPGPATH, "%s/recovery.conf", DataDir);
-	fd = AllocateFile(recoveryCommandFile, "r");
+	fd = AllocateFile(RECOVERY_COMMAND_FILE, "r");
 	if (fd == NULL)
 	{
 		if (errno == ENOENT)
@@ -3830,7 +3825,7 @@ readRecoveryCommandFile(void)
 		ereport(FATAL,
 				(errcode_for_file_access(),
 				 errmsg("could not open recovery command file \"%s\": %m",
-						recoveryCommandFile)));
+						RECOVERY_COMMAND_FILE)));
 	}
 
 	ereport(LOG,
@@ -3974,7 +3969,7 @@ readRecoveryCommandFile(void)
 	if (recoveryRestoreCommand == NULL)
 		ereport(FATAL,
 				(errmsg("recovery command file \"%s\" did not specify restore_command",
-						recoveryCommandFile)));
+						RECOVERY_COMMAND_FILE)));
 
 	/* Enable fetching from archive recovery area */
 	InArchiveRecovery = true;
@@ -4012,8 +4007,6 @@ exitArchiveRecovery(TimeLineID endTLI, uint32 endLogId, uint32 endLogSeg)
 {
 	char		recoveryPath[MAXPGPATH];
 	char		xlogpath[MAXPGPATH];
-	char		recoveryCommandFile[MAXPGPATH];
-	char		recoveryCommandDone[MAXPGPATH];
 
 	/*
 	 * We are no longer in archive recovery state.
@@ -4035,7 +4028,7 @@ exitArchiveRecovery(TimeLineID endTLI, uint32 endLogId, uint32 endLogSeg)
 	/*
 	 * If the segment was fetched from archival storage, we want to
 	 * replace the existing xlog segment (if any) with the archival
-	 * version.  This is because whatever is in XLogDir is very possibly
+	 * version.  This is because whatever is in XLOGDIR is very possibly
 	 * older than what we have from the archives, since it could have come
 	 * from restoring a PGDATA backup.	In any case, the archival version
 	 * certainly is more descriptive of what our current database state
@@ -4045,7 +4038,7 @@ exitArchiveRecovery(TimeLineID endTLI, uint32 endLogId, uint32 endLogSeg)
 	 * already set to the new value, and so we will create a new file
 	 * instead of overwriting any existing file.
 	 */
-	snprintf(recoveryPath, MAXPGPATH, "%s/RECOVERYXLOG", XLogDir);
+	snprintf(recoveryPath, MAXPGPATH, XLOGDIR "/RECOVERYXLOG");
 	XLogFilePath(xlogpath, ThisTimeLineID, endLogId, endLogSeg);
 
 	if (restoredFromArchive)
@@ -4087,21 +4080,19 @@ exitArchiveRecovery(TimeLineID endTLI, uint32 endLogId, uint32 endLogSeg)
 	XLogArchiveCleanup(xlogpath);
 
 	/* Get rid of any remaining recovered timeline-history file, too */
-	snprintf(recoveryPath, MAXPGPATH, "%s/RECOVERYHISTORY", XLogDir);
+	snprintf(recoveryPath, MAXPGPATH, XLOGDIR "/RECOVERYHISTORY");
 	unlink(recoveryPath);		/* ignore any error */
 
 	/*
 	 * Rename the config file out of the way, so that we don't
 	 * accidentally re-enter archive recovery mode in a subsequent crash.
 	 */
-	snprintf(recoveryCommandFile, MAXPGPATH, "%s/recovery.conf", DataDir);
-	snprintf(recoveryCommandDone, MAXPGPATH, "%s/recovery.done", DataDir);
-	unlink(recoveryCommandDone);
-	if (rename(recoveryCommandFile, recoveryCommandDone) != 0)
+	unlink(RECOVERY_COMMAND_DONE);
+	if (rename(RECOVERY_COMMAND_FILE, RECOVERY_COMMAND_DONE) != 0)
 		ereport(FATAL,
 				(errcode_for_file_access(),
 				 errmsg("could not rename file \"%s\" to \"%s\": %m",
-						recoveryCommandFile, recoveryCommandDone)));
+						RECOVERY_COMMAND_FILE, RECOVERY_COMMAND_DONE)));
 
 	ereport(LOG,
 			(errmsg("archive recovery complete")));
@@ -5467,7 +5458,6 @@ pg_start_backup(PG_FUNCTION_ARGS)
 	XLogRecPtr	startpoint;
 	time_t		stamp_time;
 	char		strfbuf[128];
-	char		labelfilepath[MAXPGPATH];
 	char		xlogfilename[MAXFNAMELEN];
 	uint32		_logId;
 	uint32		_logSeg;
@@ -5526,31 +5516,30 @@ pg_start_backup(PG_FUNCTION_ARGS)
 	 * Check for existing backup label --- implies a backup is already
 	 * running
 	 */
-	snprintf(labelfilepath, MAXPGPATH, "%s/backup_label", DataDir);
-	if (stat(labelfilepath, &stat_buf) != 0)
+	if (stat(BACKUP_LABEL_FILE, &stat_buf) != 0)
 	{
 		if (errno != ENOENT)
 			ereport(ERROR,
 					(errcode_for_file_access(),
 					 errmsg("could not stat file \"%s\": %m",
-							labelfilepath)));
+							BACKUP_LABEL_FILE)));
 	}
 	else
 		ereport(ERROR,
 				(errcode(ERRCODE_OBJECT_NOT_IN_PREREQUISITE_STATE),
 				 errmsg("a backup is already in progress"),
 				 errhint("If you're sure there is no backup in progress, remove file \"%s\" and try again.",
-						 labelfilepath)));
+						 BACKUP_LABEL_FILE)));
 
 	/*
 	 * Okay, write the file
 	 */
-	fp = AllocateFile(labelfilepath, "w");
+	fp = AllocateFile(BACKUP_LABEL_FILE, "w");
 	if (!fp)
 		ereport(ERROR,
 				(errcode_for_file_access(),
 				 errmsg("could not create file \"%s\": %m",
-						labelfilepath)));
+						BACKUP_LABEL_FILE)));
 	fprintf(fp, "START WAL LOCATION: %X/%X (file %s)\n",
 			startpoint.xlogid, startpoint.xrecoff, xlogfilename);
 	fprintf(fp, "CHECKPOINT LOCATION: %X/%X\n",
@@ -5561,7 +5550,7 @@ pg_start_backup(PG_FUNCTION_ARGS)
 		ereport(ERROR,
 				(errcode_for_file_access(),
 				 errmsg("could not write file \"%s\": %m",
-						labelfilepath)));
+						BACKUP_LABEL_FILE)));
 
 	/*
 	 * We're done.  As a convenience, return the starting WAL offset.
@@ -5590,7 +5579,6 @@ pg_stop_backup(PG_FUNCTION_ARGS)
 	XLogRecPtr	stoppoint;
 	time_t		stamp_time;
 	char		strfbuf[128];
-	char		labelfilepath[MAXPGPATH];
 	char		histfilepath[MAXPGPATH];
 	char		startxlogfilename[MAXFNAMELEN];
 	char		stopxlogfilename[MAXFNAMELEN];
@@ -5631,15 +5619,14 @@ pg_stop_backup(PG_FUNCTION_ARGS)
 	/*
 	 * Open the existing label file
 	 */
-	snprintf(labelfilepath, MAXPGPATH, "%s/backup_label", DataDir);
-	lfp = AllocateFile(labelfilepath, "r");
+	lfp = AllocateFile(BACKUP_LABEL_FILE, "r");
 	if (!lfp)
 	{
 		if (errno != ENOENT)
 			ereport(ERROR,
 					(errcode_for_file_access(),
 					 errmsg("could not read file \"%s\": %m",
-							labelfilepath)));
+							BACKUP_LABEL_FILE)));
 		ereport(ERROR,
 				(errcode(ERRCODE_OBJECT_NOT_IN_PREREQUISITE_STATE),
 				 errmsg("a backup is not in progress")));
@@ -5655,7 +5642,7 @@ pg_stop_backup(PG_FUNCTION_ARGS)
 			   &ch) != 4 || ch != '\n')
 		ereport(ERROR,
 				(errcode(ERRCODE_OBJECT_NOT_IN_PREREQUISITE_STATE),
-				 errmsg("invalid data in file \"%s\"", labelfilepath)));
+				 errmsg("invalid data in file \"%s\"", BACKUP_LABEL_FILE)));
 
 	/*
 	 * Write the backup history file
@@ -5690,12 +5677,12 @@ pg_stop_backup(PG_FUNCTION_ARGS)
 		ereport(ERROR,
 				(errcode_for_file_access(),
 				 errmsg("could not read file \"%s\": %m",
-						labelfilepath)));
-	if (unlink(labelfilepath) != 0)
+						BACKUP_LABEL_FILE)));
+	if (unlink(BACKUP_LABEL_FILE) != 0)
 		ereport(ERROR,
 				(errcode_for_file_access(),
 				 errmsg("could not remove file \"%s\": %m",
-						labelfilepath)));
+						BACKUP_LABEL_FILE)));
 
 	RemoveOldBackupHistory();
 	
@@ -5741,7 +5728,6 @@ read_backup_label(XLogRecPtr *checkPointLoc)
 {
 	XLogRecPtr	startpoint;
 	XLogRecPtr	stoppoint;
-	char		labelfilepath[MAXPGPATH];
 	char		histfilename[MAXFNAMELEN];
 	char		histfilepath[MAXPGPATH];
 	char		startxlogfilename[MAXFNAMELEN];
@@ -5756,15 +5742,14 @@ read_backup_label(XLogRecPtr *checkPointLoc)
 	/*
 	 * See if label file is present
 	 */
-	snprintf(labelfilepath, MAXPGPATH, "%s/backup_label", DataDir);
-	lfp = AllocateFile(labelfilepath, "r");
+	lfp = AllocateFile(BACKUP_LABEL_FILE, "r");
 	if (!lfp)
 	{
 		if (errno != ENOENT)
 			ereport(FATAL,
 					(errcode_for_file_access(),
 					 errmsg("could not read file \"%s\": %m",
-							labelfilepath)));
+							BACKUP_LABEL_FILE)));
 		return false;			/* it's not there, all is fine */
 	}
 
@@ -5778,18 +5763,18 @@ read_backup_label(XLogRecPtr *checkPointLoc)
 			   startxlogfilename, &ch) != 5 || ch != '\n')
 		ereport(FATAL,
 				(errcode(ERRCODE_OBJECT_NOT_IN_PREREQUISITE_STATE),
-				 errmsg("invalid data in file \"%s\"", labelfilepath)));
+				 errmsg("invalid data in file \"%s\"", BACKUP_LABEL_FILE)));
 	if (fscanf(lfp, "CHECKPOINT LOCATION: %X/%X%c",
 			   &checkPointLoc->xlogid, &checkPointLoc->xrecoff,
 			   &ch) != 3 || ch != '\n')
 		ereport(FATAL,
 				(errcode(ERRCODE_OBJECT_NOT_IN_PREREQUISITE_STATE),
-				 errmsg("invalid data in file \"%s\"", labelfilepath)));
+				 errmsg("invalid data in file \"%s\"", BACKUP_LABEL_FILE)));
 	if (ferror(lfp) || FreeFile(lfp))
 		ereport(FATAL,
 				(errcode_for_file_access(),
 				 errmsg("could not read file \"%s\": %m",
-						labelfilepath)));
+						BACKUP_LABEL_FILE)));
 
 	/*
 	 * Try to retrieve the backup history file (no error if we can't)
@@ -5843,13 +5828,10 @@ read_backup_label(XLogRecPtr *checkPointLoc)
 static void
 remove_backup_label(void)
 {
-	char		labelfilepath[MAXPGPATH];
-
-	snprintf(labelfilepath, MAXPGPATH, "%s/backup_label", DataDir);
-	if (unlink(labelfilepath) != 0)
+	if (unlink(BACKUP_LABEL_FILE) != 0)
 		if (errno != ENOENT)
 			ereport(FATAL,
 					(errcode_for_file_access(),
 					 errmsg("could not remove file \"%s\": %m",
-							labelfilepath)));
+							BACKUP_LABEL_FILE)));
 }

src/backend/bootstrap/bootstrap.c
@@ -8,7 +8,7 @@
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/bootstrap/bootstrap.c,v 1.204 2005/05/06 17:24:52 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/bootstrap/bootstrap.c,v 1.205 2005/07/04 04:51:45 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -365,14 +365,16 @@ BootstrapMain(int argc, char *argv[])
 	Assert(DataDir);
 	ValidatePgVersion(DataDir);
 
+	/* Change into DataDir (if under postmaster, should be done already) */
+	if (!IsUnderPostmaster)
+		ChangeToDataDir();
+
 	/* If standalone, create lockfile for data directory */
 	if (!IsUnderPostmaster)
-		CreateDataDirLockFile(DataDir, false);
+		CreateDataDirLockFile(false);
 
 	SetProcessingMode(BootstrapProcessing);
 	IgnoreSystemIndexes(true);
-
-	XLOGPathInit();
 
 	BaseInit();
 

commit 60511606fbb6d7882e4e048f85569e1bd915332d

Fix pg_resetxlog to use correct path to postmaster.pid.
Since we've already chdir'd into the data directory, the file should
be referenced as just "postmaster.pid", without prefixing the directory
path.  This is harmless in the normal case where an absolute PGDATA path
is used, but quite dangerous if a relative path is specified, since the
program might then fail to notice an active postmaster.

Reported by Hari Babu.  This got broken in my commit
eb5949d190e80360386113fde0f05854f0c9824d, so patch all active versions.

src/bin/pg_resetxlog/pg_resetxlog.c
@@ -95,7 +95,6 @@ main(int argc, char *argv[])
 	char	   *endptr3;
 	char	   *DataDir;
 	int			fd;
-	char		path[MAXPGPATH];
 
 	set_pglocale_pgservice(argv[0], PG_TEXTDOMAIN("pg_resetxlog"));
 
@@ -270,13 +269,12 @@ main(int argc, char *argv[])
 	 * Check for a postmaster lock file --- if there is one, refuse to
 	 * proceed, on grounds we might be interfering with a live installation.
 	 */
-	snprintf(path, MAXPGPATH, "%s/postmaster.pid", DataDir);
-
-	if ((fd = open(path, O_RDONLY, 0)) < 0)
+	if ((fd = open("postmaster.pid", O_RDONLY, 0)) < 0)
 	{
 		if (errno != ENOENT)
 		{
-			fprintf(stderr, _("%s: could not open file \"%s\" for reading: %s\n"), progname, path, strerror(errno));
+			fprintf(stderr, _("%s: could not open file \"%s\" for reading: %s\n"),
+					progname, "postmaster.pid", strerror(errno));
 			exit(1);
 		}
 	}
@@ -284,7 +282,7 @@ main(int argc, char *argv[])
 	{
 		fprintf(stderr, _("%s: lock file \"%s\" exists\n"
 						  "Is a server running?  If not, delete the lock file and try again.\n"),
-				progname, path);
+				progname, "postmaster.pid");
 		exit(1);
 	}
 