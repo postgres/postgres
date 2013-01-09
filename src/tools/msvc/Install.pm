ecf5c1e90e36abcbdb56734535705028c5765a3e
Magnus Hagander <magnus@hagander.net>
2013-01-09 17:34:18 +0100

    Properly install ecpg_compat and pgtypes libraries on msvc
    JiangGuiqing

--- src/tools/msvc/Install.pm
+++ src/tools/msvc/Install.pm
@@ -76,7 +76,7 @@ sub Install
         'Import libraries',
         $target .'/lib/',
         "$conf\\", "postgres\\postgres.lib","libpq\\libpq.lib", "libecpg\\libecpg.lib",
-        "libpgport\\libpgport.lib"
+        "libpgport\\libpgport.lib", "libpgtypes\\libpgtypes.lib","libecpg_compat\\libecpg_compat.lib"
     );
     CopySetOfFiles(
         'timezone names',
