b04ce529fd38b1d00492d03acf896f9293b6fb28
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2013-01-15 18:54:03 +0200

    Don't pass NULL to fprintf, if not currently connected to a database.
    Backpatch all the way to 8.3. Fixes bug #7811, per report and diagnosis by
    Meng Qingzhong.

--- src/bin/psql/help.c
+++ src/bin/psql/help.c
@@ -161,6 +161,11 @@ void
 slashUsage(unsigned short int pager)
 {
 	FILE	   *output;
+	char	   *currdb;
+
+	currdb = PQdb(pset.db);
+	if (currdb == NULL)
+		currdb = _("none");
 
 	output = PageOutput(94, pager);
 
@@ -254,7 +259,7 @@ slashUsage(unsigned short int pager)
 	fprintf(output, _("Connection\n"));
 	fprintf(output, _("  \\c[onnect] [DBNAME|- USER|- HOST|- PORT|-]\n"
 	"                         connect to new database (currently \"%s\")\n"),
-			PQdb(pset.db));
+			currdb);
 	fprintf(output, _("  \\encoding [ENCODING]   show or set client encoding\n"));
 	fprintf(output, _("  \\password [USERNAME]   securely change the password for a user\n"));
 	fprintf(output, _("  \\conninfo              display information about current connection\n"));
