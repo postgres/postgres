390523596deb3ee772555efc47953cad4b641197
Peter Eisentraut <peter_e@gmx.net>
2013-02-03 23:58:38 -0500

    Translation updates

--- src/bin/pg_ctl/nls.mk
+++ src/bin/pg_ctl/nls.mk
@@ -1,5 +1,5 @@
 # src/bin/pg_ctl/nls.mk
 CATALOG_NAME	:= pg_ctl
-AVAIL_LANGUAGES	:= cs de es fr ja pl pt_BR ru tr zh_TW
+AVAIL_LANGUAGES	:= cs de es fr it ja pl pt_BR ru tr zh_CN zh_TW
 GETTEXT_FILES	:= pg_ctl.c ../../port/exec.c
 GETTEXT_TRIGGERS:= _ simple_prompt
