0e1bee9ff494e870b7b12d3dc797b896c91938a8
Peter Eisentraut <peter_e@gmx.net>
2013-02-04 00:01:19 -0500

    Translation updates

--- src/bin/pg_controldata/nls.mk
+++ src/bin/pg_controldata/nls.mk
@@ -1,4 +1,4 @@
 # src/bin/pg_controldata/nls.mk
 CATALOG_NAME     = pg_controldata
-AVAIL_LANGUAGES  = cs de es fr ja pl pt_BR ro ru tr zh_CN zh_TW
+AVAIL_LANGUAGES  = cs de es fr it ja pl pt_BR ro ru tr zh_CN zh_TW
 GETTEXT_FILES    = pg_controldata.c
