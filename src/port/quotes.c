5ddf38f21d3f34c794de2d1c9b61ba92a7d6f39c
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2013-02-27 18:42:40 +0200

    Add standard file header comment to quotes.c.

--- src/port/quotes.c
+++ src/port/quotes.c
@@ -1,3 +1,18 @@
+/*-------------------------------------------------------------------------
+ *
+ * quotes.c
+ *	  string quoting and escaping functions
+ *
+ * Portions Copyright (c) 1996-2013, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1994, Regents of the University of California
+ *
+ *
+ * IDENTIFICATION
+ *	  src/port/quotes.c
+ *
+ *-------------------------------------------------------------------------
+ */
+
 #include "c.h"
 
 /*
