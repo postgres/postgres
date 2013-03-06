e11cb8ba2c9134c9f16253213f2f0cf089c5838e
Tom Lane <tgl@sss.pgh.pa.us>
2013-03-06 18:20:51 -0500

    Fix missing #include in commands/matview.h.
    It needs parsenodes.h to be compilable regardless of previous headers.

--- src/include/commands/matview.h
+++ src/include/commands/matview.h
@@ -4,7 +4,7 @@
  *	  prototypes for matview.c.
  *
  *
- * Portions Copyright (c) 1996-2012, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1996-2013, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  * src/include/commands/matview.h
@@ -15,8 +15,10 @@
 #define MATVIEW_H
 
 #include "nodes/params.h"
+#include "nodes/parsenodes.h"
 #include "tcop/dest.h"
 #include "utils/relcache.h"
+
 
 extern void SetRelationIsScannable(Relation relation);
 
