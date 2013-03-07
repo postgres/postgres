f11af2bcab36b51a7e41df7986d6f030459d7d09
Peter Eisentraut <peter_e@gmx.net>
2013-03-06 20:25:25 -0500

    Adjust nls.mk for split out of wait_error.c

--- src/bin/psql/nls.mk
+++ src/bin/psql/nls.mk
@@ -4,6 +4,6 @@ AVAIL_LANGUAGES  = cs de es fr ja pt_BR sv tr zh_CN zh_TW
 GETTEXT_FILES    = command.c common.c copy.c help.c input.c large_obj.c \
                    mainloop.c print.c psqlscan.c startup.c describe.c sql_help.h sql_help.c \
                    tab-complete.c variables.c \
-                   ../../port/exec.c
+                   ../../port/exec.c ../../port/wait_error.c
 GETTEXT_TRIGGERS = N_ psql_error simple_prompt
 GETTEXT_FLAGS    = psql_error:1:c-format
