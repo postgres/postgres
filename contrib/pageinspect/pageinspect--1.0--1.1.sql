e016b72411caa7027f9c980fb2c0560cdab2a129
Simon Riggs <simon@2ndQuadrant.com>
2013-03-18 14:39:17 +0000

    Add pageinspect--1.0--1.sql for checksum changes

---
+++ contrib/pageinspect/pageinspect--1.0--1.1.sql
@@ -0,0 +1,18 @@
+/* contrib/pageinspect/pageinspect--1.0--1.1.sql */
+
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION UPDATE
+\echo Use "ALTER EXTENSION pageinspect UPDATE TO 1.1" to load this file. \quit
+
+DROP FUNCTION page_header(bytea);
+CREATE FUNCTION page_header(IN page bytea,
+    OUT lsn text,
+    OUT checksum smallint,
+    OUT flags smallint,
+    OUT lower smallint,
+    OUT upper smallint,
+    OUT special smallint,
+    OUT pagesize smallint,
+    OUT version smallint,
+    OUT prune_xid xid)
+AS 'MODULE_PATHNAME', 'page_header'
+LANGUAGE C STRICT;
