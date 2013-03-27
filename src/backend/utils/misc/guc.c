7a5a59d378e052618d6feae64d1d2b4f2ad6f9bc
Simon Riggs <simon@2ndQuadrant.com>
2013-03-27 16:35:38 +0000

    Set recovery_config_directory for EXEC_BACKEND. Remove comment questioning whether this is necessary for DataDir. From buildfarm failures on Windows.

--- src/backend/utils/misc/guc.c
+++ src/backend/utils/misc/guc.c
@@ -4187,9 +4187,7 @@ SelectConfigFiles(const char *userDoption, const char *progname)
 	 * Reflect the final DataDir value back into the data_directory GUC var.
 	 * (If you are wondering why we don't just make them a single variable,
 	 * it's because the EXEC_BACKEND case needs DataDir to be transmitted to
-	 * child backends specially.  XXX is that still true?  Given that we now
-	 * chdir to DataDir, EXEC_BACKEND can read the config file without knowing
-	 * DataDir in advance.)
+	 * child backends specially.
 	 */
 	SetConfigOption("data_directory", DataDir, PGC_POSTMASTER, PGC_S_OVERRIDE);
 
@@ -4204,6 +4202,11 @@ SelectConfigFiles(const char *userDoption, const char *progname)
 		SetRecoveryConfDir(recovery_config_directory);
 	else
 		SetRecoveryConfDir(DataDir);
+
+	/*
+	 * Reflect the final RecoveryConfDir value back into the GUC var, as above.
+	 */
+	SetConfigOption("recovery_config_directory", RecoveryConfDir, PGC_POSTMASTER, PGC_S_OVERRIDE);
 
 	/*
 	 * If timezone_abbreviations wasn't set in the configuration file, install
