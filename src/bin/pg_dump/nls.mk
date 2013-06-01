01497e738e58b0a5d87706353f28eccc5bea9591
Peter Eisentraut <peter_e@gmx.net>
2013-05-31 20:03:39 -0400

    Add new source files to nls.mk

--- src/bin/pg_dump/nls.mk
+++ src/bin/pg_dump/nls.mk
@@ -6,6 +6,7 @@ GETTEXT_FILES    = pg_backup_archiver.c pg_backup_db.c pg_backup_custom.c \
                    pg_backup_directory.c dumputils.c compress_io.c \
                    pg_dump.c common.c pg_dump_sort.c \
                    pg_restore.c pg_dumpall.c \
+                   parallel.c parallel.h pg_backup_utils.c pg_backup_utils.h \
                    ../../common/fe_memutils.c ../../port/exec.c
 GETTEXT_TRIGGERS = write_msg:2 exit_horribly:2 simple_prompt \
                    ExecuteSqlCommand:3 ahlog:3 warn_or_exit_horribly:3
--- src/bin/scripts/nls.mk
+++ src/bin/scripts/nls.mk
@@ -4,6 +4,7 @@ AVAIL_LANGUAGES  = cs de es fr it ja ko pl pt_BR ro ru sv ta tr zh_CN zh_TW
 GETTEXT_FILES    = createdb.c createlang.c createuser.c \
                    dropdb.c droplang.c dropuser.c \
                    clusterdb.c vacuumdb.c reindexdb.c \
+                   pg_isready.c \
                    common.c \
                    ../../common/fe_memutils.c
 GETTEXT_TRIGGERS = simple_prompt yesno_prompt
