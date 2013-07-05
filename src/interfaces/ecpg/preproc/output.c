d0d71252395fe6f80d9871cf88e1d192d7db7ab4
Michael Meskes <meskes@postgresql.org>
2013-07-05 11:07:16 +0200

    Applied patch by MauMau <maumau307@gmail.com> to escape filenames in #line statements.

--- src/interfaces/ecpg/preproc/output.c
+++ src/interfaces/ecpg/preproc/output.c
@@ -95,9 +95,22 @@ hashline_number(void)
 #endif
 		)
 	{
-		char	   *line = mm_alloc(strlen("\n#line %d \"%s\"\n") + sizeof(int) * CHAR_BIT * 10 / 3 + strlen(input_filename));
+		/* "* 2" here is for escaping \s below */
+		char	   *line = mm_alloc(strlen("\n#line %d \"%s\"\n") + sizeof(int) * CHAR_BIT * 10 / 3 + strlen(input_filename) * 2);
+		char	   *src,
+				   *dest;
 
-		sprintf(line, "\n#line %d \"%s\"\n", yylineno, input_filename);
+		sprintf(line, "\n#line %d \"", yylineno);
+		src = input_filename;
+		dest = line + strlen(line);
+		while (*src)
+		{
+			if (*src == '\\')
+				*dest++ = '\\';
+			*dest++ = *src++;
+		}
+		*dest = '\0';
+		strcat(dest, "\"\n");
 
 		return line;
 	}
