6f9e39bc9993c18686f0950f9b9657c7c97c7450
Fujii Masao <fujii@postgresql.org>
2013-07-19 04:13:01 +0900

    Fix typo in update scripts for some contrib modules.

--- contrib/pg_stat_statements/pg_stat_statements--1.0--1.1.sql
+++ contrib/pg_stat_statements/pg_stat_statements--1.0--1.1.sql
@@ -1,7 +1,7 @@
 /* contrib/pg_stat_statements/pg_stat_statements--1.0--1.1.sql */
 
 -- complain if script is sourced in psql, rather than via ALTER EXTENSION
-\echo Use "ALTER EXTENSION pg_stat_statements UPDATE" to load this file. \quit
+\echo Use "ALTER EXTENSION pg_stat_statements UPDATE TO '1.1'" to load this file. \quit
 
 /* First we have to remove them from the extension */
 ALTER EXTENSION pg_stat_statements DROP VIEW pg_stat_statements;
