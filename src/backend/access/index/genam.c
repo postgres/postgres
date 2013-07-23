765ad89be36f699e2d65238c1f0458a1ab7e4d8b
Robert Haas <rhaas@postgresql.org>
2013-07-23 10:58:32 -0400

    Use InvalidSnapshot, now SnapshotNow, as the default snapshot.
    As far as I can determine, there's no code in the core distribution
    that fails to explicitly set the snapshot of a scan or executor
    state.  If there is any such code, this will probably cause it to
    seg fault; friendlier suggestions were discussed on pgsql-hackers,
    but there was no consensus that anything more than this was
    needed.
    
    This is another step towards the hoped-for complete removal of
    SnapshotNow.

--- src/backend/access/index/genam.c
+++ src/backend/access/index/genam.c
@@ -79,7 +79,7 @@ RelationGetIndexScan(Relation indexRelation, int nkeys, int norderbys)
 
 	scan->heapRelation = NULL;	/* may be set later */
 	scan->indexRelation = indexRelation;
-	scan->xs_snapshot = SnapshotNow;	/* may be set later */
+	scan->xs_snapshot = InvalidSnapshot;	/* caller must initialize this */
 	scan->numberOfKeys = nkeys;
 	scan->numberOfOrderBys = norderbys;
 
