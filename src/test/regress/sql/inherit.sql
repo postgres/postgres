3c2a425da71d56edade2d7e2734305e8af778c9a
Tom Lane <tgl@sss.pgh.pa.us>
2013-08-30 21:40:21 -0400

    Improve regression test for #8410.
    The previous version of the query disregarded the result of the MergeAppend
    instead of checking its results.
    
    Andres Freund

--- src/test/regress/sql/inherit.sql
+++ src/test/regress/sql/inherit.sql
@@ -442,21 +442,21 @@ ORDER BY x, y;
 -- exercise rescan code path via a repeatedly-evaluated subquery
 explain (costs off)
 SELECT
-    (SELECT g.i FROM (
-        (SELECT * FROM generate_series(1, 2) ORDER BY 1)
+    ARRAY(SELECT f.i FROM (
+        (SELECT d + g.i FROM generate_series(4, 30, 3) d ORDER BY 1)
         UNION ALL
-        (SELECT * FROM generate_series(1, 2) ORDER BY 1)
+        (SELECT d + g.i FROM generate_series(0, 30, 5) d ORDER BY 1)
     ) f(i)
-    ORDER BY f.i LIMIT 1)
+    ORDER BY f.i LIMIT 10)
 FROM generate_series(1, 3) g(i);
 
 SELECT
-    (SELECT g.i FROM (
-        (SELECT * FROM generate_series(1, 2) ORDER BY 1)
+    ARRAY(SELECT f.i FROM (
+        (SELECT d + g.i FROM generate_series(4, 30, 3) d ORDER BY 1)
         UNION ALL
-        (SELECT * FROM generate_series(1, 2) ORDER BY 1)
+        (SELECT d + g.i FROM generate_series(0, 30, 5) d ORDER BY 1)
     ) f(i)
-    ORDER BY f.i LIMIT 1)
+    ORDER BY f.i LIMIT 10)
 FROM generate_series(1, 3) g(i);
 
 reset enable_seqscan;
