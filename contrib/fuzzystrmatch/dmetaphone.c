f0e86930dd56f2da00101d5384cbb51ada3730da
Bruce Momjian <bruce@momjian.us>
2013-09-10 21:34:01 -0400

    fuzzystrmatch: replace broken link in C comment
    Albe Laurenz

--- contrib/fuzzystrmatch/dmetaphone.c
+++ contrib/fuzzystrmatch/dmetaphone.c
@@ -10,7 +10,7 @@
  * Information on using Double Metaphone can be found at
  *	 http://www.codeproject.com/string/dmetaphone1.asp
  * and the original article describing it can be found at
- *	 http://www.cuj.com/documents/s=8038/cuj0006philips/
+ *	 http://drdobbs.com/184401251
  *
  * For PostgreSQL we provide 2 functions - one for the primary and one for
  * the alternate. That way the functions are pure text->text mappings that
