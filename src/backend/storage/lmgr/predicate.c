c01262a82484106111a81dca8c922fd5670b644d
Kevin Grittner <kgrittn@postgresql.org>
2013-10-07 14:16:54 -0500

    Eliminate xmin from hash tag for predicate locks on heap tuples.
    If a tuple was frozen while its predicate locks mattered,
    read-write dependencies could be missed, resulting in failure to
    detect conflicts which could lead to anomalies in committed
    serializable transactions.
    
    This field was added to the tag when we still thought that it was
    necessary to carry locks forward to a new version of an updated
    row.  That was later proven to be unnecessary, which allowed
    simplification of the code, but elimination of xmin from the tag
    was missed at the time.
    
    Per report and analysis by Heikki Linnakangas.
    Backpatch to 9.1.

--- src/backend/storage/lmgr/predicate.c
+++ src/backend/storage/lmgr/predicate.c
@@ -156,9 +156,9 @@
  *		PredicateLockTuple(Relation relation, HeapTuple tuple,
  *						Snapshot snapshot)
  *		PredicateLockPageSplit(Relation relation, BlockNumber oldblkno,
- *							   BlockNumber newblkno);
+ *							   BlockNumber newblkno)
  *		PredicateLockPageCombine(Relation relation, BlockNumber oldblkno,
- *								 BlockNumber newblkno);
+ *								 BlockNumber newblkno)
  *		TransferPredicateLocksToHeapRelation(Relation relation)
  *		ReleasePredicateLocks(bool isCommit)
  *
@@ -381,7 +381,7 @@ static SHM_QUEUE *FinishedSerializableTransactions;
  * this entry, you can ensure that there's enough scratch space available for
  * inserting one entry in the hash table. This is an otherwise-invalid tag.
  */
-static const PREDICATELOCKTARGETTAG ScratchTargetTag = {0, 0, 0, 0, 0};
+static const PREDICATELOCKTARGETTAG ScratchTargetTag = {0, 0, 0, 0};
 static uint32 ScratchTargetTagHash;
 static int	ScratchPartitionLock;
 
@@ -2492,8 +2492,6 @@ PredicateLockTuple(Relation relation, HeapTuple tuple, Snapshot snapshot)
 			}
 		}
 	}
-	else
-		targetxmin = InvalidTransactionId;
 
 	/*
 	 * Do quick-but-not-definitive test for a relation lock first.	This will
@@ -2512,8 +2510,7 @@ PredicateLockTuple(Relation relation, HeapTuple tuple, Snapshot snapshot)
 									 relation->rd_node.dbNode,
 									 relation->rd_id,
 									 ItemPointerGetBlockNumber(tid),
-									 ItemPointerGetOffsetNumber(tid),
-									 targetxmin);
+									 ItemPointerGetOffsetNumber(tid));
 	PredicateLockAcquire(&tag);
 }
 
@@ -4283,8 +4280,7 @@ CheckForSerializableConflictIn(Relation relation, HeapTuple tuple,
 										 relation->rd_node.dbNode,
 										 relation->rd_id,
 						 ItemPointerGetBlockNumber(&(tuple->t_data->t_ctid)),
-						ItemPointerGetOffsetNumber(&(tuple->t_data->t_ctid)),
-									  HeapTupleHeaderGetXmin(tuple->t_data));
+						ItemPointerGetOffsetNumber(&(tuple->t_data->t_ctid)));
 		CheckTargetForConflictsIn(&targettag);
 	}
 
