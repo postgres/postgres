f90d7426ed3605ab925910a387783163763833fa
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2013-10-23 14:03:54 +0300

    Fix two bugs in setting the vm bit of empty pages.
    Use a critical section when setting the all-visible flag on an empty page,
    and WAL-logging it. log_newpage_buffer() contains an assertion that it
    must be called inside a critical section, and it's the right thing to do
    when modifying a buffer anyway.
    
    Also, the page should be marked dirty before calling log_newpage_buffer(),
    per the comment in log_newpage_buffer() and src/backend/access/transam/README.
    
    Patch by Andres Freund, in response to my report. Backpatch to 9.2, like
    the patch that introduced these bugs (a6370fd9).

--- src/backend/commands/vacuumlazy.c
+++ src/backend/commands/vacuumlazy.c
@@ -663,6 +663,11 @@ lazy_scan_heap(Relation onerel, LVRelStats *vacrelstats,
 			/* empty pages are always all-visible */
 			if (!PageIsAllVisible(page))
 			{
+				START_CRIT_SECTION();
+
+				/* mark buffer dirty before writing a WAL record */
+				MarkBufferDirty(buf);
+
 				/*
 				 * It's possible that another backend has extended the heap,
 				 * initialized the page, and then failed to WAL-log the page
@@ -682,9 +687,9 @@ lazy_scan_heap(Relation onerel, LVRelStats *vacrelstats,
 					log_newpage_buffer(buf);
 
 				PageSetAllVisible(page);
-				MarkBufferDirty(buf);
 				visibilitymap_set(onerel, blkno, buf, InvalidXLogRecPtr,
 								  vmbuffer, InvalidTransactionId);
+				END_CRIT_SECTION();
 			}
 
 			UnlockReleaseBuffer(buf);
