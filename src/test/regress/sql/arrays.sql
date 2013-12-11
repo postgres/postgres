9c9a1f2ebb00f96f1164f01006ab9861b36f1ab1
Tom Lane <tgl@sss.pgh.pa.us>
2013-12-11 15:08:38 -0500

    Tweak placement of explicit ANALYZE commands in the regression tests.
    Make the COPY test, which loads most of the large static tables used in
    the tests, also explicitly ANALYZE those tables.  This allows us to get
    rid of various ad-hoc, and rather redundant, ANALYZE commands that had
    gotten stuck into various test scripts over time to ensure we got
    consistent plan choices.  (We could have done a database-wide ANALYZE,
    but that would cause stats to get attached to the small static tables
    too, which results in plan changes compared to the historical behavior.
    I'm not sure that's a good idea, so not going that far for now.)
    
    Back-patch to 9.0, since 9.0 and 9.1 are currently sometimes failing
    regression tests for lack of an "ANALYZE tenk1" in the subselect test.
    There's no need for this in 8.4 since we didn't print any plans back
    then.

--- src/test/regress/sql/arrays.sql
+++ src/test/regress/sql/arrays.sql
@@ -196,8 +196,6 @@ SELECT ARRAY[[1,2],[3,4]] || ARRAY[5,6] AS "{{1,2},{3,4},{5,6}}";
 SELECT ARRAY[0,0] || ARRAY[1,1] || ARRAY[2,2] AS "{0,0,1,1,2,2}";
 SELECT 0 || ARRAY[1,2] || 3 AS "{0,1,2,3}";
 
-ANALYZE array_op_test;
-
 SELECT * FROM array_op_test WHERE i @> '{32}' ORDER BY seqno;
 SELECT * FROM array_op_test WHERE i && '{32}' ORDER BY seqno;
 SELECT * FROM array_op_test WHERE i @> '{17}' ORDER BY seqno;
