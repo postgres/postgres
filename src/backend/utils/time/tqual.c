11ac4c73cb89551d7e0d0180b58d82186f072f8d
Alvaro Herrera <alvherre@alvh.no-ip.org>
2013-12-18 13:31:27 -0300

    Don't ignore tuple locks propagated by our updates
    If a tuple was locked by transaction A, and transaction B updated it,
    the new version of the tuple created by B would be locked by A, yet
    visible only to B; due to an oversight in HeapTupleSatisfiesUpdate, the
    lock held by A wouldn't get checked if transaction B later deleted (or
    key-updated) the new version of the tuple.  This might cause referential
    integrity checks to give false positives (that is, allow deletes that
    should have been rejected).
    
    This is an easy oversight to have made, because prior to improved tuple
    locks in commit 0ac5ad5134f it wasn't possible to have tuples created by
    our own transaction that were also locked by remote transactions, and so
    locks weren't even considered in that code path.
    
    It is recommended that foreign keys be rechecked manually in bulk after
    installing this update, in case some referenced rows are missing with
    some referencing row remaining.
    
    Per bug reported by Daniel Wood in
    CAPweHKe5QQ1747X2c0tA=5zf4YnS2xcvGf13Opd-1Mq24rF1cQ@mail.gmail.com

--- src/backend/utils/time/tqual.c
+++ src/backend/utils/time/tqual.c
@@ -493,8 +493,36 @@ HeapTupleSatisfiesUpdate(HeapTuple htup, CommandId curcid,
 			if (tuple->t_infomask & HEAP_XMAX_INVALID)	/* xid invalid */
 				return HeapTupleMayBeUpdated;
 
-			if (HEAP_XMAX_IS_LOCKED_ONLY(tuple->t_infomask))	/* not deleter */
-				return HeapTupleMayBeUpdated;
+			if (HEAP_XMAX_IS_LOCKED_ONLY(tuple->t_infomask))
+			{
+				TransactionId	xmax;
+
+				xmax = HeapTupleHeaderGetRawXmax(tuple);
+
+				/*
+				 * Careful here: even though this tuple was created by our own
+				 * transaction, it might be locked by other transactions, if
+				 * the original version was key-share locked when we updated
+				 * it.
+				 */
+
+				if (tuple->t_infomask & HEAP_XMAX_IS_MULTI)
+				{
+					if (MultiXactHasRunningRemoteMembers(xmax))
+						return HeapTupleBeingUpdated;
+					else
+						return HeapTupleMayBeUpdated;
+				}
+
+				/* if locker is gone, all's well */
+				if (!TransactionIdIsInProgress(xmax))
+					return HeapTupleMayBeUpdated;
+
+				if (!TransactionIdIsCurrentTransactionId(xmax))
+					return HeapTupleBeingUpdated;
+				else
+					return HeapTupleMayBeUpdated;
+			}
 
 			if (tuple->t_infomask & HEAP_XMAX_IS_MULTI)
 			{
@@ -507,7 +535,11 @@ HeapTupleSatisfiesUpdate(HeapTuple htup, CommandId curcid,
 
 				/* updating subtransaction must have aborted */
 				if (!TransactionIdIsCurrentTransactionId(xmax))
+				{
+					if (MultiXactHasRunningRemoteMembers(HeapTupleHeaderGetRawXmax(tuple)))
+						return HeapTupleBeingUpdated;
 					return HeapTupleMayBeUpdated;
+				}
 				else
 				{
 					if (HeapTupleHeaderGetCmax(tuple) >= curcid)
