28fff0ef8b1fadc105aaf28e224e8a5e87866875
Michael Meskes <meskes@postgresql.org>
2014-01-09 15:41:51 +0100

    Fix descriptor output in ECPG.
    While working on most platforms the old way sometimes created alignment
    problems. This should fix it. Also the regresion tests were updated to test for
    the reported case.
    
    Report and fix by MauMau <maumau307@gmail.com>

--- src/interfaces/ecpg/test/expected/sql-dynalloc2.c
+++ src/interfaces/ecpg/test/expected/sql-dynalloc2.c
@@ -188,7 +188,7 @@ if (sqlca.sqlcode < 0) sqlprint ( );
 #line 32 "dynalloc2.pgc"
 
    { ECPGdo(__LINE__, 0, 1, NULL, 0, ECPGst_normal, "select * from test", ECPGt_EOIT, 
-	ECPGt_descriptor, "mydesc", 0L, 0L, 0L, 
+	ECPGt_descriptor, "mydesc", 1L, 1L, 1L, 
 	ECPGt_NO_INDICATOR, NULL , 0L, 0L, 0L, ECPGt_EORT);
 #line 33 "dynalloc2.pgc"
 
