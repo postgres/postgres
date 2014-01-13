5143dfd5713299048c556d69f93bf17108693db9
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2014-01-13 15:42:35 +0200

    Fix calculation of ISMN check digit.
    This has always been broken, so back-patch to all supported versions.
    
    Fabien COELHO

--- contrib/isn/isn.c
+++ contrib/isn/isn.c
@@ -827,7 +827,7 @@ string2ean(const char *str, bool errorOK, ean13 *result,
 		case ISMN:
 			strncpy(buf, "9790", 4);	/* this isn't for sure yet, for now
 										 * ISMN it's only 9790 */
-			valid = (valid && ((rcheck = checkdig(buf + 3, 10)) == check || magic));
+			valid = (valid && ((rcheck = checkdig(buf, 13)) == check || magic));
 			break;
 		case ISBN:
 			strncpy(buf, "978", 3);
