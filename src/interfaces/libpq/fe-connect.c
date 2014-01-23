77035fa8a92d8c39f4c689e54f46813f203f09a8
Fujii Masao <fujii@postgresql.org>
2014-01-23 22:48:12 +0900

    Fix bugs in PQhost().
    In the platform that doesn't support Unix-domain socket, when
    neither host nor hostaddr are specified, the default host
    'localhost' is used to connect to the server and PQhost() must
    return that, but it didn't. This patch fixes PQhost() so that
    it returns the default host in that case.
    
    Also this patch fixes PQhost() so that it doesn't return
    Unix-domain socket directory path in the platform that doesn't
    support Unix-domain socket.
    
    Back-patch to all supported versions.

--- src/interfaces/libpq/fe-connect.c
+++ src/interfaces/libpq/fe-connect.c
@@ -5188,7 +5188,16 @@ PQhost(const PGconn *conn)
 {
 	if (!conn)
 		return NULL;
-	return conn->pghost ? conn->pghost : conn->pgunixsocket;
+	if (conn->pghost != NULL && conn->pghost[0] != '\0')
+		return conn->pghost;
+	else
+	{
+#ifdef HAVE_UNIX_SOCKETS
+		return conn->pgunixsocket;
+#else
+		return DefaultHost;
+#endif
+	}
 }
 
 char *
