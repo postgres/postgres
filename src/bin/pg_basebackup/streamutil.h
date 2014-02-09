63ab2befe0d2332a5571f46c1a2c0af9447c6a4b
Magnus Hagander <magnus@hagander.net>
2014-02-09 13:10:14 +0100

    Kill pg_basebackup background process when exiting
    If an error occurs in the foreground (backup) process of pg_basebackup,
    and we exit in a controlled way, the background process (streaming
    xlog process) would stay around and keep streaming.

--- src/bin/pg_basebackup/streamutil.h
+++ src/bin/pg_basebackup/streamutil.h
@@ -11,10 +11,4 @@ extern char *replication_slot;
 /* Connection kept global so we can disconnect easily */
 extern PGconn *conn;
 
-#define disconnect_and_exit(code)				\
-	{											\
-	if (conn != NULL) PQfinish(conn);			\
-	exit(code);									\
-	}
-
 extern PGconn *GetConnection(void);
