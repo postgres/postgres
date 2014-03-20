d1bdab2fa39f9a29fc806e1f2b6b5428b88d7cc5
Robert Haas <rhaas@postgresql.org>
2014-03-20 08:42:53 -0400

    test_shm_mq: Improve regression tests.
    Per discussion with Tom Lane.

--- contrib/test_shm_mq/sql/test_shm_mq.sql
+++ contrib/test_shm_mq/sql/test_shm_mq.sql
@@ -5,5 +5,8 @@ CREATE EXTENSION test_shm_mq;
 -- the operations complete without crashing or hanging and that none of their
 -- internal sanity tests fail.
 --
-SELECT test_shm_mq(32768, (select string_agg(chr(32+(random()*96)::int), '') from generate_series(1,400)), 10000, 1);
-SELECT test_shm_mq_pipelined(16384, (select string_agg(chr(32+(random()*96)::int), '') from generate_series(1,270000)), 200, 3);
+SELECT test_shm_mq(1024, '', 2000, 1);
+SELECT test_shm_mq(1024, 'a', 2001, 1);
+SELECT test_shm_mq(32768, (select string_agg(chr(32+(random()*95)::int), '') from generate_series(1,(100+900*random())::int)), 10000, 1);
+SELECT test_shm_mq(100, (select string_agg(chr(32+(random()*95)::int), '') from generate_series(1,(100+200*random())::int)), 10000, 1);
+SELECT test_shm_mq_pipelined(16384, (select string_agg(chr(32+(random()*95)::int), '') from generate_series(1,270000)), 200, 3);
