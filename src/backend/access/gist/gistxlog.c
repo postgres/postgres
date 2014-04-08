ac0078c1de6614b1db40fd1c5d03e4989e7be060
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2014-04-08 14:47:24 +0300

    Fix hot standby bug with GiST scans.
    Don't reset the rightlink of a page when replaying a page update record.
    This was a leftover from pre-hot standby days, when it was not possible to
    have scans concurrent with WAL replay. Resetting the right-link was not
    necessary back then either, but it was done for the sake of tidiness. But
    with hot standby, it's wrong, because a concurrent scan might still need it.
    
    Backpatch all versions with hot standby, 9.0 and above.

--- src/backend/access/gist/gistxlog.c
+++ src/backend/access/gist/gistxlog.c
@@ -186,7 +186,6 @@ gistRedoPageUpdateRecord(XLogRecPtr lsn, XLogRecord *record)
 		GistPageSetLeaf(page);
 	}
 
-	GistPageGetOpaque(page)->rightlink = InvalidBlockNumber;
 	PageSetLSN(page, lsn);
 	PageSetTLI(page, ThisTimeLineID);
 	MarkBufferDirty(buffer);
