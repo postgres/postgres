2e6602dfc25d1f8e8a2e1df48c96440b02aa19c1
Bruce Momjian <bruce@momjian.us>
2014-05-14 21:13:59 -0400

    pg_update:  slight major version adjustment

--- contrib/pg_upgrade/check.c
+++ contrib/pg_upgrade/check.c
@@ -123,7 +123,7 @@ check_and_dump_old_cluster(bool live_check, char **sequence_script_file_name)
 	}
 
 	/* Pre-PG 9.4 had a different 'line' data type internal format */
-	if (GET_MAJOR_VERSION(old_cluster.major_version) < 904)
+	if (GET_MAJOR_VERSION(old_cluster.major_version) <= 903)
 		old_9_3_check_for_line_data_type_usage(&old_cluster);
 
 	/* Pre-PG 9.0 had no large object permissions */
