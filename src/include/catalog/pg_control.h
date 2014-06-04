eeca4cd35e284c72b2ea1b4494e64e7738896e81
Tom Lane <tgl@sss.pgh.pa.us>
2014-06-04 18:16:17 -0400

    Bump PG_CONTROL_VERSION for previous 9.4 changes.
    This should have been done in 6bc8ef0b7f1f1df3998745a66e1790e27424aa0c
    and/or 50e547096c4858a68abf09894667a542cc418315, but better late than
    never.  If we don't change this then we risk 9.3 pg_controldata or
    pg_resetxlog being inappropriately used against a 9.4 pg_control file,
    or vice versa.

--- src/include/catalog/pg_control.h
+++ src/include/catalog/pg_control.h
@@ -21,7 +21,7 @@
 
 
 /* Version identifier for this pg_control format */
-#define PG_CONTROL_VERSION	937
+#define PG_CONTROL_VERSION	941
 
 /*
  * Body of CheckPoint XLOG records.  This is declared here because we keep
