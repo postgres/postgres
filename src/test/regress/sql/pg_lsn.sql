d4d48a5edd9eb28a7f2ee2e4cbe20d984274982e
Tom Lane <tgl@sss.pgh.pa.us>
2014-06-04 21:31:41 -0400

    Tweak new regression test case for better portability.
    Buildfarm says we get different plans on 32-bit and 64-bit platforms,
    probably because of MAXALIGN-related differences in memory-consumption
    calculations.  Add some dummy WHERE clauses so that the planner estimates
    different sizes for the three generate_series() relations; that should
    stabilize the choice of join order.

--- src/test/regress/sql/pg_lsn.sql
+++ src/test/regress/sql/pg_lsn.sql
@@ -30,10 +30,12 @@ SELECT DISTINCT (i || '/' || j)::pg_lsn f
   FROM generate_series(1, 10) i,
        generate_series(1, 10) j,
        generate_series(1, 5) k
+  WHERE i <= 10 AND j > 0 AND j <= 10
   ORDER BY f;
 
 SELECT DISTINCT (i || '/' || j)::pg_lsn f
   FROM generate_series(1, 10) i,
        generate_series(1, 10) j,
        generate_series(1, 5) k
+  WHERE i <= 10 AND j > 0 AND j <= 10
   ORDER BY f;
