841baf28f8e86b67c0b036fb921446a310e5c60b
Noah Misch <noah@leadboat.com>
2014-06-14 09:41:13 -0400

    Add mkdtemp() to libpgport.
    This function is pervasive on free software operating systems; import
    NetBSD's implementation.  Back-patch to 8.4, like the commit that will
    harness it.

--- src/tools/msvc/Mkvcbuild.pm
+++ src/tools/msvc/Mkvcbuild.pm
@@ -69,7 +69,7 @@ sub mkvcbuild
 	  srandom.c getaddrinfo.c gettimeofday.c inet_net_ntop.c kill.c open.c
 	  erand48.c snprintf.c strlcat.c strlcpy.c dirmod.c exec.c noblock.c path.c
 	  pgcheckdir.c pg_crc.c pgmkdirp.c pgsleep.c pgstrcasecmp.c pqsignal.c
-	  qsort.c qsort_arg.c quotes.c
+	  mkdtemp.c qsort.c qsort_arg.c quotes.c
 	  sprompt.c tar.c thread.c wait_error.c getopt.c getopt_long.c dirent.c
 	  win32env.c win32error.c win32setlocale.c);
 
