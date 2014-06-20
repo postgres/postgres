1044e79a0b7b21ddedd9e093394a34e8cc2d42bc
Tom Lane <tgl@sss.pgh.pa.us>
2014-06-19 22:13:44 -0400

    Avoid leaking memory while evaluating arguments for a table function.
    ExecMakeTableFunctionResult evaluated the arguments for a function-in-FROM
    in the query-lifespan memory context.  This is insignificant in simple
    cases where the function relation is scanned only once; but if the function
    is in a sub-SELECT or is on the inside of a nested loop, any memory
    consumed during argument evaluation can add up quickly.  (The potential for
    trouble here had been foreseen long ago, per existing comments; but we'd
    not previously seen a complaint from the field about it.)  To fix, create
    an additional temporary context just for this purpose.
    
    Per an example from MauMau.  Back-patch to all active branches.

--- src/include/executor/executor.h
+++ src/include/executor/executor.h
@@ -231,6 +231,7 @@ extern Datum GetAttributeByName(HeapTupleHeader tuple, const char *attname,
 				   bool *isNull);
 extern Tuplestorestate *ExecMakeTableFunctionResult(ExprState *funcexpr,
 							ExprContext *econtext,
+							MemoryContext argContext,
 							TupleDesc expectedDesc,
 							bool randomAccess);
 extern Datum ExecEvalExprSwitchContext(ExprState *expression, ExprContext *econtext,
