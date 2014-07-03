f545d233ebce6971b6f9847680e48b679e707d22
Peter Eisentraut <peter_e@gmx.net>
2014-07-02 21:44:02 -0400

    Use a separate temporary directory for the Unix-domain socket
    Creating the Unix-domain socket in the build directory can run into
    name-length limitations.  Therefore, create the socket file in the
    default temporary directory of the operating system.  Keep the temporary
    data directory etc. in the build tree.

--- src/bin/pg_ctl/t/002_status.pl
+++ src/bin/pg_ctl/t/002_status.pl
@@ -4,11 +4,12 @@ use TestLib;
 use Test::More tests => 2;
 
 my $tempdir = TestLib::tempdir;
+my $tempdir_short = TestLib::tempdir_short;
 
 system_or_bail "initdb -D $tempdir/data -A trust >/dev/null";
 open CONF, ">>$tempdir/data/postgresql.conf";
 print CONF "listen_addresses = ''\n";
-print CONF "unix_socket_directories = '$tempdir'\n";
+print CONF "unix_socket_directories = '$tempdir_short'\n";
 close CONF;
 
 command_exit_is([ 'pg_ctl', 'status', '-D', "$tempdir/data" ],
