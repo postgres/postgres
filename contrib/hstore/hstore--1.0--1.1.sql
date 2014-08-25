f13feb088aaf5a1f725289c01f636c5f78fa85e9
Fujii Masao <fujii@postgresql.org>
2014-08-25 18:30:48 +0200

    Backpatch: Fix typo in update scripts for some contrib modules.
    Backpatch as discussed in 20140702192641.GD22738@awork2.anarazel.de
    ff. as the error messages are user facing and possibly confusing.
    
    Original commit: 6f9e39bc9993c18686f0950f9b9657c7c97c7450

--- contrib/hstore/hstore--1.0--1.1.sql
+++ contrib/hstore/hstore--1.0--1.1.sql
@@ -1,6 +1,6 @@
 /* contrib/hstore/hstore--1.0--1.1.sql */
 
--- complain if script is sourced in psql, rather than via CREATE EXTENSION
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
 \echo Use "ALTER EXTENSION hstore UPDATE TO '1.1'" to load this file. \quit
 
 ALTER EXTENSION hstore DROP OPERATOR => (text, text);
