886b58b476fe4799702aecd47880aa7b7d336c17
Tom Lane <tgl@sss.pgh.pa.us>
2014-09-08 16:09:56 -0400

    Fix psql \s to work with recent libedit, and add pager support.
    psql's \s (print command history) doesn't work at all with recent libedit
    versions when printing to the terminal, because libedit tries to do an
    fchmod() on the target file which will fail if the target is /dev/tty.
    (We'd already noted this in the context of the target being /dev/null.)
    Even before that, it didn't work pleasantly, because libedit likes to
    encode the command history file (to ensure successful reloading), which
    renders it nigh unreadable, not to mention significantly different-looking
    depending on exactly which libedit version you have.  So let's forget using
    write_history() for this purpose, and instead print the data ourselves,
    using logic similar to that used to iterate over the history for newline
    encoding/decoding purposes.
    
    While we're at it, insert the ability to use the pager when \s is printing
    to the terminal.  This has been an acknowledged shortcoming of \s for many
    years, so while you could argue it's not exactly a back-patchable bug fix
    it still seems like a good improvement.  Anyone who's seriously annoyed
    at this can use "\s /dev/tty" or local equivalent to get the old behavior.
    
    Experimentation with this showed that the history iteration logic was
    actually rather broken when used with libedit.  It turns out that with
    libedit you have to use previous_history() not next_history() to advance
    to more recent history entries.  The easiest and most robust fix for this
    seems to be to make a run-time test to verify which function to call.
    We had not noticed this because libedit doesn't really need the newline
    encoding logic: its own encoding ensures that command entries containing
    newlines are reloaded correctly (unlike libreadline).  So the effective
    behavior with recent libedits was that only the oldest history entry got
    newline-encoded or newline-decoded.  However, because of yet other bugs in
    history_set_pos(), some old versions of libedit allowed the existing loop
    logic to reach entries besides the oldest, which means there may be libedit
    ~/.psql_history files out there containing encoded newlines in more than
    just the oldest entry.  To ensure we can reload such files, it seems
    appropriate to back-patch this fix, even though that will result in some
    incompatibility with older psql versions (ie, multiline history entries
    written by a psql with this fix will look corrupted to a psql without it,
    if its libedit is reasonably up to date).
    
    Stepan Rutz and Tom Lane

--- src/bin/psql/input.h
+++ src/bin/psql/input.h
@@ -42,7 +42,8 @@ char	   *gets_interactive(const char *prompt);
 char	   *gets_fromFile(FILE *source);
 
 void		initializeInput(int flags);
-bool		saveHistory(char *fname, int max_lines, bool appendFlag, bool encodeFlag);
+
+bool		printHistory(const char *fname, unsigned short int pager);
 
 void		pg_append_history(const char *s, PQExpBuffer history_buf);
 void		pg_send_history(PQExpBuffer history_buf);
