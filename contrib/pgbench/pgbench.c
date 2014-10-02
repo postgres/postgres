98a68b47858b43a37bb9752c311412a31c091fe7
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2014-10-02 15:51:31 +0300

    Fix typo in error message.

--- contrib/pgbench/pgbench.c
+++ contrib/pgbench/pgbench.c
@@ -1907,7 +1907,7 @@ main(int argc, char **argv)
 			case 'M':
 				if (num_files > 0)
 				{
-					fprintf(stderr, "query mode (-M) should be specifiled before transaction scripts (-f)\n");
+					fprintf(stderr, "query mode (-M) should be specified before transaction scripts (-f)\n");
 					exit(1);
 				}
 				for (querymode = 0; querymode < NUM_QUERYMODE; querymode++)
