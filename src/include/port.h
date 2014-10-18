3609c0d1f8a96b646060acda45b4459fe7905200
Tom Lane <tgl@sss.pgh.pa.us>
2014-10-17 22:55:30 -0400

    Declare mkdtemp() only if we're providing it.
    Follow our usual style of providing an "extern" for a standard library
    function only when we're also providing the implementation.  This avoids
    issues when the system headers declare the function slightly differently
    than we do, as noted by Caleb Welton.
    
    We might have to go to the extent of probing to see if the system headers
    declare the function, but let's not do that until it's demonstrated to be
    necessary.
    
    Oversight in commit 9e6b1bf258170e62dac555fc82ff0536dfe01d29.  Back-patch
    to all supported branches, as that was.

--- src/include/port.h
+++ src/include/port.h
@@ -408,6 +408,10 @@ extern int	getpeereid(int sock, uid_t *uid, gid_t *gid);
 extern int	isinf(double x);
 #endif
 
+#ifndef HAVE_MKDTEMP
+extern char *mkdtemp(char *path);
+#endif
+
 #ifndef HAVE_RINT
 extern double rint(double x);
 #endif
@@ -474,8 +478,5 @@ extern int	pg_check_dir(const char *dir);
 
 /* port/pgmkdirp.c */
 extern int	pg_mkdir_p(char *path, int omode);
-
-/* port/mkdtemp.c */
-extern char *mkdtemp(char *path);
 
 #endif   /* PG_PORT_H */
