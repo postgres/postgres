1325b239b9e862cc31de2af363f1b73f0d15d920
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2014-10-29 14:32:01 +0200

    Reset error message at PQreset()
    If you call PQreset() repeatedly, and the connection cannot be
    re-established, the error messages from the failed connection attempts
    kept accumulating in the error string.
    
    Fixes bug #11455 reported by Caleb Epstein. Backpatch to all supported
    versions.

--- src/interfaces/libpq/fe-connect.c
+++ src/interfaces/libpq/fe-connect.c
@@ -2909,6 +2909,7 @@ closePGconn(PGconn *conn)
 										 * absent */
 	conn->asyncStatus = PGASYNC_IDLE;
 	pqClearAsyncResult(conn);	/* deallocate result */
+	resetPQExpBuffer(&conn->errorMessage);
 	pg_freeaddrinfo_all(conn->addrlist_family, conn->addrlist);
 	conn->addrlist = NULL;
 	conn->addr_cur = NULL;
