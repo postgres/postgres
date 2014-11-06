171c377a0abe12d1418540dc154feccd9355995e
Bruce Momjian <bruce@momjian.us>
2014-11-06 02:33:05 -0500

    C comment:  mention why the Gregorian calendar is used pre-1582

--- src/include/utils/datetime.h
+++ src/include/utils/datetime.h
@@ -271,6 +271,11 @@ extern const char *const months[];		/* months (3-char abbreviations) */
 extern const char *const days[];	/* days (full names) */
 extern const int day_tab[2][13];
 
+/*
+ * These are the rules for the Gregorian calendar, which was adopted in 1582.
+ * However, we use this calculation for all prior years as well because the
+ * SQL standard specifies use of the Gregorian calendar.
+ */
 #define isleap(y) (((y) % 4) == 0 && (((y) % 100) != 0 || ((y) % 400) == 0))
 
 
