326b6f009f9f76afbbcd39d751e07822fe453730
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2014-12-05 12:11:07 +0200

    Print new track_commit_timestamp in rm_desc of a parameter-change record.
    Michael Paquier

--- src/backend/access/rmgrdesc/xlogdesc.c
+++ src/backend/access/rmgrdesc/xlogdesc.c
@@ -110,13 +110,15 @@ xlog_desc(StringInfo buf, XLogReaderState *record)
 
 		appendStringInfo(buf, "max_connections=%d max_worker_processes=%d "
 						 "max_prepared_xacts=%d max_locks_per_xact=%d "
-						 "wal_level=%s wal_log_hints=%s",
+						 "wal_level=%s wal_log_hints=%s "
+						 "track_commit_timestamps=%s",
 						 xlrec.MaxConnections,
 						 xlrec.max_worker_processes,
 						 xlrec.max_prepared_xacts,
 						 xlrec.max_locks_per_xact,
 						 wal_level_str,
-						 xlrec.wal_log_hints ? "on" : "off");
+						 xlrec.wal_log_hints ? "on" : "off",
+						 xlrec.track_commit_timestamp ? "on" : "off");
 	}
 	else if (info == XLOG_FPW_CHANGE)
 	{
