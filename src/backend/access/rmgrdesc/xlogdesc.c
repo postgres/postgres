463dde8def8befec1e8f1f3c106c1ce1108ad54f
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2014-12-05 11:58:24 +0200

    Print wal_log_hints in the rm_desc routing of a parameter-change record.
    It was an oversight in the original commit.
    
    Also note in the sample config file that changing wal_log_hints requires a
    restart.
    
    Michael Paquier. Backpatch to 9.4, where wal_log_hints was added.

--- src/backend/access/rmgrdesc/xlogdesc.c
+++ src/backend/access/rmgrdesc/xlogdesc.c
@@ -117,12 +117,15 @@ xlog_desc(StringInfo buf, uint8 xl_info, char *rec)
 			}
 		}
 
-		appendStringInfo(buf, "parameter change: max_connections=%d max_worker_processes=%d max_prepared_xacts=%d max_locks_per_xact=%d wal_level=%s",
+		appendStringInfo(buf, "parameter change: max_connections=%d max_worker_processes=%d "
+						 "max_prepared_xacts=%d max_locks_per_xact=%d "
+						 "wal_level=%s wal_log_hints=%s",
 						 xlrec.MaxConnections,
 						 xlrec.max_worker_processes,
 						 xlrec.max_prepared_xacts,
 						 xlrec.max_locks_per_xact,
-						 wal_level_str);
+						 wal_level_str,
+						 xlrec.wal_log_hints ? "on" : "off");
 	}
 	else if (info == XLOG_FPW_CHANGE)
 	{
