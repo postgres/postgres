0916eba131530b296f86137759a32f66ffedff7b
Noah Misch <noah@leadboat.com>
2014-12-15 20:56:09 -0500

    Fix commit_ts test suite for systems with coarse timestamp granularity.
    Noticed on a couple of Windows configurations.
    
    Petr Jelinek, reviewed by Michael Paquier.

--- src/test/modules/commit_ts/sql/commit_timestamp.sql
+++ src/test/modules/commit_ts/sql/commit_timestamp.sql
@@ -10,7 +10,7 @@ INSERT INTO committs_test DEFAULT VALUES;
 
 SELECT id,
        pg_xact_commit_timestamp(xmin) >= ts,
-       pg_xact_commit_timestamp(xmin) < now(),
+       pg_xact_commit_timestamp(xmin) <= now(),
        pg_xact_commit_timestamp(xmin) - ts < '60s' -- 60s should give a lot of reserve
 FROM committs_test
 ORDER BY id;
@@ -21,4 +21,4 @@ SELECT pg_xact_commit_timestamp('0'::xid);
 SELECT pg_xact_commit_timestamp('1'::xid);
 SELECT pg_xact_commit_timestamp('2'::xid);
 
-SELECT x.xid::text::bigint > 0, x.timestamp > '-infinity'::timestamptz, x.timestamp < now() FROM pg_last_committed_xact() x;
+SELECT x.xid::text::bigint > 0, x.timestamp > '-infinity'::timestamptz, x.timestamp <= now() FROM pg_last_committed_xact() x;
