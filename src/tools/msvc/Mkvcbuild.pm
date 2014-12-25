7bb78b2acacb0acf0858430b267d41613f8a8dd3
Noah Misch <noah@leadboat.com>
2014-12-25 13:52:03 -0500

    Have config_sspi_auth() permit IPv6 localhost connections.
    Windows versions later than Windows Server 2003 map "localhost" to ::1.
    Account for that in the generated pg_hba.conf, fixing another oversight
    in commit f6dc6dd5ba54d52c0733aaafc50da2fbaeabb8b0.  Back-patch to 9.0,
    like that commit.
    
    David Rowley and Noah Misch

--- src/tools/msvc/Mkvcbuild.pm
+++ src/tools/msvc/Mkvcbuild.pm
@@ -307,6 +307,7 @@ sub mkvcbuild
 	$pgregress_ecpg->AddIncludeDir('src\test\regress');
 	$pgregress_ecpg->AddDefine('HOST_TUPLE="i686-pc-win32vc"');
 	$pgregress_ecpg->AddDefine('FRONTEND');
+	$pgregress_ecpg->AddLibrary('ws2_32.lib');
 	$pgregress_ecpg->AddReference($libpgport);
 
 	my $isolation_tester =
@@ -332,6 +333,7 @@ sub mkvcbuild
 	$pgregress_isolation->AddIncludeDir('src\test\regress');
 	$pgregress_isolation->AddDefine('HOST_TUPLE="i686-pc-win32vc"');
 	$pgregress_isolation->AddDefine('FRONTEND');
+	$pgregress_isolation->AddLibrary('ws2_32.lib');
 	$pgregress_isolation->AddReference($libpgport);
 
 	# src/bin
@@ -561,6 +563,8 @@ sub mkvcbuild
 	$pgregress->AddFile('src\test\regress\pg_regress_main.c');
 	$pgregress->AddIncludeDir('src\port');
 	$pgregress->AddDefine('HOST_TUPLE="i686-pc-win32vc"');
+	$pgregress->AddDefine('FRONTEND');
+	$pgregress->AddLibrary('ws2_32.lib');
 	$pgregress->AddReference($libpgport);
 
 	$solution->Save();
