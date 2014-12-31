1773e07025e72b36fa5a1deeab55cd80f5383bcf
Tom Lane <tgl@sss.pgh.pa.us>
2014-12-31 12:17:08 -0500

    Improve consistency of parsing of psql's magic variables.
    For simple boolean variables such as ON_ERROR_STOP, psql has for a long
    time recognized variant spellings of "on" and "off" (such as "1"/"0"),
    and it also made a point of warning you if you'd misspelled the setting.
    But these conveniences did not exist for other keyword-valued variables.
    In particular, though ECHO_HIDDEN and ON_ERROR_ROLLBACK include "on" and
    "off" as possible values, none of the alternative spellings for those were
    recognized; and to make matters worse the code would just silently assume
    "on" was meant for any unrecognized spelling.  Several people have reported
    getting bitten by this, so let's fix it.  In detail, this patch:
    
    * Allows all spellings recognized by ParseVariableBool() for ECHO_HIDDEN
    and ON_ERROR_ROLLBACK.
    
    * Reports a warning for unrecognized values for COMP_KEYWORD_CASE, ECHO,
    ECHO_HIDDEN, HISTCONTROL, ON_ERROR_ROLLBACK, and VERBOSITY.
    
    * Recognizes all values for all these variables case-insensitively;
    previously there was a mishmash of case-sensitive and case-insensitive
    behaviors.
    
    Back-patch to all supported branches.  There is a small risk of breaking
    existing scripts that were accidentally failing to malfunction; but the
    consensus is that the chance of detecting real problems and preventing
    future mistakes outweighs this.

--- src/bin/psql/command.c
+++ src/bin/psql/command.c
@@ -1252,7 +1252,7 @@ exec_command(const char *cmd,
 												 OT_NORMAL, NULL, false);
 
 		if (opt)
-			pset.timing = ParseVariableBool(opt);
+			pset.timing = ParseVariableBool(opt, "\\timing");
 		else
 			pset.timing = !pset.timing;
 		if (!pset.quiet)
@@ -2159,10 +2159,12 @@ do_pset(const char *param, const char *value, printQueryOpt *popt, bool quiet)
 	}
 
 	/* set expanded/vertical mode */
-	else if (strcmp(param, "x") == 0 || strcmp(param, "expanded") == 0 || strcmp(param, "vertical") == 0)
+	else if (strcmp(param, "x") == 0 ||
+			 strcmp(param, "expanded") == 0 ||
+			 strcmp(param, "vertical") == 0)
 	{
 		if (value)
-			popt->topt.expanded = ParseVariableBool(value);
+			popt->topt.expanded = ParseVariableBool(value, param);
 		else
 			popt->topt.expanded = !popt->topt.expanded;
 		if (!quiet)
@@ -2175,7 +2177,7 @@ do_pset(const char *param, const char *value, printQueryOpt *popt, bool quiet)
 	else if (strcmp(param, "numericlocale") == 0)
 	{
 		if (value)
-			popt->topt.numericLocale = ParseVariableBool(value);
+			popt->topt.numericLocale = ParseVariableBool(value, param);
 		else
 			popt->topt.numericLocale = !popt->topt.numericLocale;
 		if (!quiet)
@@ -2232,7 +2234,7 @@ do_pset(const char *param, const char *value, printQueryOpt *popt, bool quiet)
 	else if (strcmp(param, "t") == 0 || strcmp(param, "tuples_only") == 0)
 	{
 		if (value)
-			popt->topt.tuples_only = ParseVariableBool(value);
+			popt->topt.tuples_only = ParseVariableBool(value, param);
 		else
 			popt->topt.tuples_only = !popt->topt.tuples_only;
 		if (!quiet)
@@ -2286,10 +2288,12 @@ do_pset(const char *param, const char *value, printQueryOpt *popt, bool quiet)
 		if (value && pg_strcasecmp(value, "always") == 0)
 			popt->topt.pager = 2;
 		else if (value)
-			if (ParseVariableBool(value))
+		{
+			if (ParseVariableBool(value, param))
 				popt->topt.pager = 1;
 			else
 				popt->topt.pager = 0;
+		}
 		else if (popt->topt.pager == 1)
 			popt->topt.pager = 0;
 		else
@@ -2309,7 +2313,7 @@ do_pset(const char *param, const char *value, printQueryOpt *popt, bool quiet)
 	else if (strcmp(param, "footer") == 0)
 	{
 		if (value)
-			popt->default_footer = ParseVariableBool(value);
+			popt->default_footer = ParseVariableBool(value, param);
 		else
 			popt->default_footer = !popt->default_footer;
 		if (!quiet)
