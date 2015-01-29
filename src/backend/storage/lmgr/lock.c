9be655632bf23fa08bf48ffe672a6be125af8780
Andres Freund <andres@anarazel.de>
2015-01-29 17:49:03 +0100

    Properly terminate the array returned by GetLockConflicts().
    GetLockConflicts() has for a long time not properly terminated the
    returned array. During normal processing the returned array is zero
    initialized which, while not pretty, is sufficient to be recognized as
    a invalid virtual transaction id. But the HotStandby case is more than
    aesthetically broken: The allocated (and reused) array is neither
    zeroed upon allocation, nor reinitialized, nor terminated.
    
    Not having a terminating element means that the end of the array will
    not be recognized and that recovery conflict handling will thus read
    ahead into adjacent memory. Only terminating when hitting memory
    content that looks like a invalid virtual transaction id.  Luckily
    this seems so far not have caused significant problems, besides making
    recovery conflict more expensive.
    
    Discussion: 20150127142713.GD29457@awork2.anarazel.de
    
    Backpatch into all supported branches.

--- src/backend/storage/lmgr/lock.c
+++ src/backend/storage/lmgr/lock.c
@@ -2808,6 +2808,8 @@ GetLockConflicts(const LOCKTAG *locktag, LOCKMODE lockmode)
 		 * on this lockable object.
 		 */
 		LWLockRelease(partitionLock);
+		vxids[count].backendId = InvalidBackendId;
+		vxids[count].localTransactionId = InvalidLocalTransactionId;
 		return vxids;
 	}
 
@@ -2861,6 +2863,8 @@ GetLockConflicts(const LOCKTAG *locktag, LOCKMODE lockmode)
 	if (count > MaxBackends)	/* should never happen */
 		elog(PANIC, "too many conflicting locks found");
 
+	vxids[count].backendId = InvalidBackendId;
+	vxids[count].localTransactionId = InvalidLocalTransactionId;
 	return vxids;
 }
 
