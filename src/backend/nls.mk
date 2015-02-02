1de0702d92ed4288ae69c2b3587dddc023eb966f
Peter Eisentraut <peter_e@gmx.net>
2015-02-01 23:18:42 -0500

    Translation updates
    Source-Git-URL: git://git.postgresql.org/git/pgtranslation/messages.git
    Source-Git-Hash: 19c72ea8d856d7b1d4f5d759a766c8206bf9ce53

--- src/backend/nls.mk
+++ src/backend/nls.mk
@@ -1,6 +1,6 @@
 # src/backend/nls.mk
 CATALOG_NAME     = postgres
-AVAIL_LANGUAGES  = de es fr it ja pl pt_BR ru zh_CN
+AVAIL_LANGUAGES  = de es fr id it ja pl pt_BR ru zh_CN
 GETTEXT_FILES    = + gettext-files
 GETTEXT_TRIGGERS = $(BACKEND_COMMON_GETTEXT_TRIGGERS) \
     GUC_check_errmsg GUC_check_errdetail GUC_check_errhint \
