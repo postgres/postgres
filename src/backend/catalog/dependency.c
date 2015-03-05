8167ef8e2cd4e3c4c875ed5d29f40cf9cc4c88e5
Alvaro Herrera <alvherre@alvh.no-ip.org>
2015-03-05 18:03:16 -0300

    Fix user mapping object description
    We were using "user mapping for user XYZ" as description for user mappings, but
    that's ambiguous because users can have mappings on multiple foreign
    servers; therefore change it to "for user XYZ on server UVW" instead.
    Object identities for user mappings are also updated in the same way, in
    branches 9.3 and above.
    
    The incomplete description string was introduced together with the whole
    SQL/MED infrastructure by commit cae565e503 of 8.4 era, so backpatch all
    the way back.

--- src/backend/catalog/dependency.c
+++ src/backend/catalog/dependency.c
@@ -2773,6 +2773,8 @@ getObjectDescription(const ObjectAddress *object)
 				HeapTuple	tup;
 				Oid			useid;
 				char	   *usename;
+				Form_pg_user_mapping umform;
+				ForeignServer *srv;
 
 				tup = SearchSysCache1(USERMAPPINGOID,
 									  ObjectIdGetDatum(object->objectId));
@@ -2780,7 +2782,9 @@ getObjectDescription(const ObjectAddress *object)
 					elog(ERROR, "cache lookup failed for user mapping %u",
 						 object->objectId);
 
-				useid = ((Form_pg_user_mapping) GETSTRUCT(tup))->umuser;
+				umform = (Form_pg_user_mapping) GETSTRUCT(tup);
+				useid = umform->umuser;
+				srv = GetForeignServer(umform->umserver);
 
 				ReleaseSysCache(tup);
 
@@ -2789,7 +2793,9 @@ getObjectDescription(const ObjectAddress *object)
 				else
 					usename = "public";
 
-				appendStringInfo(&buffer, _("user mapping for %s"), usename);
+				appendStringInfo(&buffer, _("user mapping for %s on server %s"), usename,
+								 srv->servername);
+
 				break;
 			}
 
