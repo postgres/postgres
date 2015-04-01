f4540cae10d8642b59deea50869888b78f16d722
Alvaro Herrera <alvherre@alvh.no-ip.org>
2015-04-01 20:00:07 -0300

    psql: fix \connect with URIs and conninfo strings
    psql was already accepting conninfo strings as the first parameter in
    \connect, but the way it worked wasn't sane; some of the other
    parameters would get the previous connection's values, causing it to
    connect to a completely unexpected server or, more likely, not finding
    any server at all because of completely wrong combinations of
    parameters.
    
    Fix by explicitely checking for a conninfo-looking parameter in the
    dbname position; if one is found, use its complete specification rather
    than mix with the other arguments.  Also, change tab-completion to not
    try to complete conninfo/URI-looking "dbnames" and document that
    conninfos are accepted as first argument.
    
    There was a weak consensus to backpatch this, because while the behavior
    of using the dbname as a conninfo is nowhere documented for \connect, it
    is reasonable to expect that it works because it does work in many other
    contexts.  Therefore this is backpatched all the way back to 9.0.
    
    To implement this, routines previously private to libpq have been
    duplicated so that psql can decide what looks like a conninfo/URI
    string.  In back branches, just duplicate the same code all the way back
    to 9.2, where URIs where introduced; 9.0 and 9.1 have a simpler version.
    In master, the routines are moved to src/common and renamed.
    
    Author: David Fetter, Andrew Dunstan.  Some editorialization by me
    (probably earning a Gierth's "Sloppy" badge in the process.)
    Reviewers: Andrew Gierth, Erik Rijkers, Pavel Stěhule, Stephen Frost,
    Robert Haas, Andrew Dunstan.

--- src/bin/psql/common.c
+++ src/bin/psql/common.c
@@ -1714,3 +1714,44 @@ expand_tilde(char **filename)
 
 	return;
 }
+
+/*
+ * Checks if connection string starts with either of the valid URI prefix
+ * designators.
+ *
+ * Returns the URI prefix length, 0 if the string doesn't contain a URI prefix.
+ *
+ * XXX This is a duplicate of the eponymous libpq function.
+ */
+static int
+uri_prefix_length(const char *connstr)
+{
+	/* The connection URI must start with either of the following designators: */
+	static const char uri_designator[] = "postgresql://";
+	static const char short_uri_designator[] = "postgres://";
+
+	if (strncmp(connstr, uri_designator,
+				sizeof(uri_designator) - 1) == 0)
+		return sizeof(uri_designator) - 1;
+
+	if (strncmp(connstr, short_uri_designator,
+				sizeof(short_uri_designator) - 1) == 0)
+		return sizeof(short_uri_designator) - 1;
+
+	return 0;
+}
+
+/*
+ * Recognized connection string either starts with a valid URI prefix or
+ * contains a "=" in it.
+ *
+ * Must be consistent with parse_connection_string: anything for which this
+ * returns true should at least look like it's parseable by that routine.
+ *
+ * XXX This is a duplicate of the eponymous libpq function.
+ */
+bool
+recognized_connection_string(const char *connstr)
+{
+	return uri_prefix_length(connstr) != 0 || strchr(connstr, '=') != NULL;
+}
