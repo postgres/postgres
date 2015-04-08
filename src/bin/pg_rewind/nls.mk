b216ad7bf1a9308c97d2032d4793010e8c8aa7ec
Fujii Masao <fujii@postgresql.org>
2015-04-08 11:06:25 +0900

    Mark the second argument of pg_log as the translatable string in nls.mk.

--- src/bin/pg_rewind/nls.mk
+++ src/bin/pg_rewind/nls.mk
@@ -3,7 +3,7 @@ CATALOG_NAME     = pg_rewind
 AVAIL_LANGUAGES  =
 GETTEXT_FILES    = copy_fetch.c datapagemap.c fetch.c filemap.c libpq_fetch.c logging.c parsexlog.c pg_rewind.c timeline.c ../../common/fe_memutils.c ../../common/restricted_token.c ../../../src/backend/access/transam/xlogreader.c
 
-GETTEXT_TRIGGERS = pg_log pg_fatal report_invalid_record:2
+GETTEXT_TRIGGERS = pg_log:2 pg_fatal report_invalid_record:2
 GETTEXT_FLAGS    = pg_log:2:c-format \
     pg_fatal:1:c-format \
     report_invalid_record:2:c-format
