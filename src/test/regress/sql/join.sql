2e38198f605f39e96544c81a43f1984147e0890c
Tom Lane <tgl@sss.pgh.pa.us>
2015-04-25 16:44:27 -0400

    Prevent improper reordering of antijoins vs. outer joins.
    An outer join appearing within the RHS of an antijoin can't commute with
    the antijoin, but somehow I missed teaching make_outerjoininfo() about
    that.  In Teodor Sigaev's recent trouble report, this manifests as a
    "could not find RelOptInfo for given relids" error within eqjoinsel();
    but I think silently wrong query results are possible too, if the planner
    misorders the joins and doesn't happen to trigger any internal consistency
    checks.  It's broken as far back as we had antijoins, so back-patch to all
    supported branches.

--- src/test/regress/sql/join.sql
+++ src/test/regress/sql/join.sql
@@ -444,6 +444,23 @@ LEFT JOIN (
 WHERE d.f1 IS NULL;
 
 --
+-- regression test for proper handling of outer joins within antijoins
+--
+
+create temp table tt4x(c1 int, c2 int, c3 int);
+
+explain (costs off)
+select * from tt4x t1
+where not exists (
+  select 1 from tt4x t2
+    left join tt4x t3 on t2.c3 = t3.c1
+    left join ( select t5.c1 as c1
+                from tt4x t4 left join tt4x t5 on t4.c2 = t5.c1
+              ) a1 on t3.c2 = a1.c1
+  where t1.c1 = t2.c2
+);
+
+--
 -- regression test for problems of the sort depicted in bug #3494
 --
 
