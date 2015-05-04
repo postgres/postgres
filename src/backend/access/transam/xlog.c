14de825dee324cb6b1e8298ee845e1134edcc33e
Robert Haas <rhaas@postgresql.org>
2015-05-04 12:06:53 -0400

    Recursively fsync() the data directory after a crash.
    Otherwise, if there's another crash, some writes from after the first
    crash might make it to disk while writes from before the crash fail
    to make it to disk.  This could lead to data corruption.
    
    Back-patch to all supported versions.
    
    Abhijit Menon-Sen, reviewed by Andres Freund and slightly revised
    by me.

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -696,6 +696,8 @@ static void rm_redo_error_callback(void *arg);
 static int	get_sync_bit(int method);
 
 
+static void fsync_pgdata(char *datadir);
+
 /*
  * Insert an XLOG record having the specified RMID and info bytes,
  * with the body of the record being the data chunk(s) described by
@@ -5012,6 +5014,18 @@ StartupXLOG(void)
 		ereport(LOG,
 			  (errmsg("database system was interrupted; last known up at %s",
 					  str_time(ControlFile->time))));
+
+	/*
+	 * If we previously crashed, there might be data which we had written,
+	 * intending to fsync it, but which we had not actually fsync'd yet.
+	 * Therefore, a power failure in the near future might cause earlier
+	 * unflushed writes to be lost, even though more recent data written to
+	 * disk from here on would be persisted.  To avoid that, fsync the entire
+	 * data directory.
+	 */
+	if (ControlFile->state != DB_SHUTDOWNED &&
+		ControlFile->state != DB_SHUTDOWNED_IN_RECOVERY)
+		fsync_pgdata(data_directory);
 
 	/* This is just to allow attaching to startup process with a debugger */
 #ifdef XLOG_REPLAY_DELAY
@@ -10178,4 +10192,32 @@ SetWalWriterSleeping(bool sleeping)
 	SpinLockAcquire(&xlogctl->info_lck);
 	xlogctl->WalWriterSleeping = sleeping;
 	SpinLockRelease(&xlogctl->info_lck);
+}
+
+/*
+ * Issue fsync recursively on PGDATA and all its contents.
+ */
+static void
+fsync_pgdata(char *datadir)
+{
+	if (!enableFsync)
+		return;
+
+	/*
+	 * If possible, hint to the kernel that we're soon going to fsync
+	 * the data directory and its contents.
+	 */
+#if defined(HAVE_SYNC_FILE_RANGE) || \
+	(defined(USE_POSIX_FADVISE) && defined(POSIX_FADV_DONTNEED))
+	walkdir(datadir, pre_sync_fname);
+#endif
+
+	/*
+	 * Now we do the fsync()s in the same order.
+	 *
+	 * It's important to fsync the destination directory itself as individual
+	 * file fsyncs don't guarantee that the directory entry for the file is
+	 * synced.
+	 */
+	walkdir(datadir, fsync_fname);
 }
