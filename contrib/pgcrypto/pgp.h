7b758b7d605aca10b36aa1c26bbf16c33f8ac726
Noah Misch <noah@leadboat.com>
2015-05-18 10:02:31 -0400

    pgcrypto: Report errant decryption as "Wrong key or corrupt data".
    This has been the predominant outcome.  When the output of decrypting
    with a wrong key coincidentally resembled an OpenPGP packet header,
    pgcrypto could instead report "Corrupt data", "Not text data" or
    "Unsupported compression algorithm".  The distinct "Corrupt data"
    message added no value.  The latter two error messages misled when the
    decrypted payload also exhibited fundamental integrity problems.  Worse,
    error message variance in other systems has enabled cryptologic attacks;
    see RFC 4880 section "14. Security Considerations".  Whether these
    pgcrypto behaviors are likewise exploitable is unknown.
    
    In passing, document that pgcrypto does not resist side-channel attacks.
    Back-patch to 9.0 (all supported versions).
    
    Security: CVE-2015-3167

--- contrib/pgcrypto/pgp.h
+++ contrib/pgcrypto/pgp.h
@@ -148,7 +148,9 @@ struct PGP_Context
 	 * internal variables
 	 */
 	int			mdc_checked;
-	int			corrupt_prefix;
+	int			corrupt_prefix; /* prefix failed RFC 4880 "quick check" */
+	int			unsupported_compr;		/* has bzip2 compression */
+	int			unexpected_binary;		/* binary data seen in text_mode */
 	int			in_mdc_pkt;
 	int			use_mdcbuf_filter;
 	PX_MD	   *mdc_ctx;
