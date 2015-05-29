35dd1b51f2ea7a41621b7ab04be605ecfd7cfaf7
Tom Lane <tgl@sss.pgh.pa.us>
2015-05-29 15:11:36 -0400

    Remove special cases for ETXTBSY from new fsync'ing logic.
    The argument that this is a sufficiently-expected case to be silently
    ignored seems pretty thin.  Andres had brought it up back when we were
    still considering that most fsync failures should be hard errors, and it
    probably would be legit not to fail hard for ETXTBSY --- but the same is
    true for EROFS and other cases, which is why we gave up on hard failures.
    ETXTBSY is surely not a normal case, so logging the failure seems fine
    from here.

--- src/bin/initdb/initdb.c
+++ src/bin/initdb/initdb.c
@@ -625,12 +625,6 @@ pre_sync_fname(const char *fname, bool isdir)
 	{
 		if (errno == EACCES || (isdir && errno == EISDIR))
 			return;
-
-#ifdef ETXTBSY
-		if (errno == ETXTBSY)
-			return;
-#endif
-
 		fprintf(stderr, _("%s: could not open file \"%s\": %s\n"),
 				progname, fname, strerror(errno));
 		return;
@@ -690,12 +684,6 @@ fsync_fname_ext(const char *fname, bool isdir)
 	{
 		if (errno == EACCES || (isdir && errno == EISDIR))
 			return;
-
-#ifdef ETXTBSY
-		if (errno == ETXTBSY)
-			return;
-#endif
-
 		fprintf(stderr, _("%s: could not open file \"%s\": %s\n"),
 				progname, fname, strerror(errno));
 		return;
