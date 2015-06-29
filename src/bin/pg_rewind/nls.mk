c5e5d444de85a7caff462443c5915544d4406a62
Peter Eisentraut <peter_e@gmx.net>
2015-06-28 23:56:55 -0400

    Translation updates
    Source-Git-URL: git://git.postgresql.org/git/pgtranslation/messages.git
    Source-Git-Hash: fb7e72f46cfafa1b5bfe4564d9686d63a1e6383f

--- src/bin/pg_rewind/nls.mk
+++ src/bin/pg_rewind/nls.mk
@@ -1,6 +1,6 @@
 # src/bin/pg_rewind/nls.mk
 CATALOG_NAME     = pg_rewind
-AVAIL_LANGUAGES  =
+AVAIL_LANGUAGES  =de
 GETTEXT_FILES    = copy_fetch.c datapagemap.c fetch.c file_ops.c filemap.c libpq_fetch.c logging.c parsexlog.c pg_rewind.c timeline.c ../../common/fe_memutils.c ../../common/restricted_token.c ../../../src/backend/access/transam/xlogreader.c
 
 GETTEXT_TRIGGERS = pg_log:2 pg_fatal report_invalid_record:2
