6c534fd68568452adcc9ccecb557eff74f6f0f4d
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2015-07-06 22:18:26 +0300

    Turn install.bat into a pure one line wrapper fort he perl script.
    Build.bat and vcregress.bat got similar treatment years ago. I'm not sure
    why install.bat wasn't treated at the same time, but it seems like a good
    idea anyway.
    
    The immediate problem with the old install.bat was that it had quoting
    issues, and wouldn't work if the target directory's name contained spaces.
    This fixes that problem.

--- src/tools/msvc/install.bat
+++ src/tools/msvc/install.bat
@@ -1,27 +1,6 @@
 @echo off
 REM src/tools/msvc/install.bat
-
-if NOT "%1"=="" GOTO RUN_INSTALL
-
-echo Invalid command line options.
-echo Usage: "install.bat <path>"
-echo.
-REM exit fix for pre-2003 shell especially if used on buildfarm
-if "%XP_EXIT_FIX%" == "yes" exit 1
-exit /b 1
-
-:RUN_INSTALL
-
-SETLOCAL
-
-IF NOT EXIST buildenv.pl goto nobuildenv
-perl -e "require 'buildenv.pl'; while(($k,$v) = each %%ENV) { print qq[\@SET $k=$v\n]; }" > bldenv.bat
-CALL bldenv.bat
-del bldenv.bat
-:nobuildenv
-
-perl install.pl "%1" %2
-
-REM exit fix for pre-2003 shell especially if used on buildfarm
-if "%XP_EXIT_FIX%" == "yes" exit %ERRORLEVEL%
-exit /b %ERRORLEVEL%
+REM all the logic for this now belongs in install.pl. This file really
+REM only exists so you don't have to type "perl install.pl"
+REM Resist any temptation to add any logic here.
+@perl install.pl %*
