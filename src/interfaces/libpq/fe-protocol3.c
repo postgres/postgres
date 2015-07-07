a788cbfdc406952ec41e34b1df64e65128021312
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2015-07-07 18:54:08 +0300

    Oops, PQExpBufferDataBroken doesn't exist before 9.2.
    My previous back-patching went wrong.

--- src/interfaces/libpq/fe-protocol3.c
+++ src/interfaces/libpq/fe-protocol3.c
@@ -887,7 +887,7 @@ pqGetErrorNotice3(PGconn *conn, bool isError)
 			res->errMsg = pqResultStrdup(res, workBuf.data);
 		pqClearAsyncResult(conn);
 		conn->result = res;
-		if (PQExpBufferDataBroken(workBuf))
+		if (PQExpBufferBroken(&workBuf))
 			printfPQExpBuffer(&conn->errorMessage,
 							  libpq_gettext("out of memory"));
 		else
