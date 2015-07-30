54833c5e772ee0e8461269e1b40ab6b9fae4b1cd
Tom Lane <tgl@sss.pgh.pa.us>
2015-07-30 12:11:23 -0400

    Avoid some zero-divide hazards in the planner.
    Although I think on all modern machines floating division by zero
    results in Infinity not SIGFPE, we still don't want infinities
    running around in the planner's costing estimates; too much risk
    of that leading to insane behavior.
    
    grouping_planner() failed to consider the possibility that final_rel
    might be known dummy and hence have zero rowcount.  (I wonder if it
    would be better to set a rows estimate of 1 for dummy relations?
    But at least in the back branches, changing this convention seems
    like a bad idea, so I'll leave that for another day.)
    
    Make certain that get_variable_numdistinct() produces a nonzero result.
    The case that can be shown to be broken is with stadistinct < 0.0 and
    small ntuples; we did not prevent the result from rounding to zero.
    For good luck I applied clamp_row_est() to all the nonconstant return
    values.
    
    In ExecChooseHashTableSize(), Assert that we compute positive nbuckets
    and nbatch.  I know of no reason to think this isn't the case, but it
    seems like a good safety check.
    
    Per reports from Piotr Stefaniak.  Back-patch to all active branches.

--- src/backend/utils/adt/selfuncs.c
+++ src/backend/utils/adt/selfuncs.c
@@ -4337,8 +4337,8 @@ examine_variable(PlannerInfo *root, Node *node, int varRelid,
  *
  * vardata: results of examine_variable
  *
- * NB: be careful to produce an integral result, since callers may compare
- * the result to exact integer counts.
+ * NB: be careful to produce a positive integral result, since callers may
+ * compare the result to exact integer counts, or might divide by it.
  */
 double
 get_variable_numdistinct(VariableStatData *vardata)
@@ -4412,7 +4412,7 @@ get_variable_numdistinct(VariableStatData *vardata)
 	 * If we had an absolute estimate, use that.
 	 */
 	if (stadistinct > 0.0)
-		return stadistinct;
+		return clamp_row_est(stadistinct);
 
 	/*
 	 * Otherwise we need to get the relation size; punt if not available.
@@ -4427,14 +4427,14 @@ get_variable_numdistinct(VariableStatData *vardata)
 	 * If we had a relative estimate, use that.
 	 */
 	if (stadistinct < 0.0)
-		return floor((-stadistinct * ntuples) + 0.5);
+		return clamp_row_est(-stadistinct * ntuples);
 
 	/*
 	 * With no data, estimate ndistinct = ntuples if the table is small, else
 	 * use default.
 	 */
 	if (ntuples < DEFAULT_NUM_DISTINCT)
-		return ntuples;
+		return clamp_row_est(ntuples);
 
 	return DEFAULT_NUM_DISTINCT;
 }
