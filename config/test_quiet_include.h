a664d4790e5f93726f264c77c044a7ce4c1a675c
Noah Misch <noah@leadboat.com>
2015-07-29 22:49:48 -0400

    Blacklist xlc 32-bit inlining.
    Per a suggestion from Tom Lane.  Back-patch to 9.0 (all supported
    versions).  While only 9.4 and up have code known to elicit this
    compiler bug, we were disabling inlining by accident until commit
    43d89a23d59c487bc9258fad7a6187864cb8c0c0.

--- config/test_quiet_include.h
+++ config/test_quiet_include.h
@@ -7,3 +7,12 @@ fun()
 {
 	return 0;
 }
+
+/*
+ * "IBM XL C/C++ for AIX, V12.1" miscompiles, for 32-bit, some inline
+ * expansions of ginCompareItemPointers() "long long" arithmetic.  To take
+ * advantage of inlining, build a 64-bit PostgreSQL.
+ */
+#if defined(__ILP32__) && defined(__IBMC__)
+#error "known inlining bug"
+#endif
