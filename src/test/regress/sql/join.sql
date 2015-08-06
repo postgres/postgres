8703059c6b55c427100e00a09f66534b6ccbfaa1
Tom Lane <tgl@sss.pgh.pa.us>
2015-08-06 15:35:27 -0400

    Further fixes for degenerate outer join clauses.
    Further testing revealed that commit f69b4b9495269cc4 was still a few
    bricks shy of a load: minor tweaking of the previous test cases resulted
    in the same wrong-outer-join-order problem coming back.  After study
    I concluded that my previous changes in make_outerjoininfo() were just
    accidentally masking the problem, and should be reverted in favor of
    forcing syntactic join order whenever an upper outer join's predicate
    doesn't mention a lower outer join's LHS.  This still allows the
    chained-outer-joins style that is the normally optimizable case.
    
    I also tightened things up some more in join_is_legal().  It seems to me
    on review that what's really happening in the exception case where we
    ignore a mismatched special join is that we're allowing the proposed join
    to associate into the RHS of the outer join we're comparing it to.  As
    such, we should *always* insist that the proposed join be a left join,
    which eliminates a bunch of rather dubious argumentation.  The case where
    we weren't enforcing that was the one that was already known buggy anyway
    (it had a violatable Assert before the aforesaid commit) so it hardly
    deserves a lot of deference.
    
    Back-patch to all active branches, like the previous patch.  The added
    regression test case failed in all branches back to 9.1, and I think it's
    only an unrelated change in costing calculations that kept 9.0 from
    choosing a broken plan.

--- src/test/regress/sql/join.sql
+++ src/test/regress/sql/join.sql
@@ -1065,6 +1065,31 @@ select t1.* from
   left join int4_tbl i4
   on (i8.q2 = i4.f1);
 
+explain (verbose, costs off)
+select t1.* from
+  text_tbl t1
+  left join (select *, '***'::text as d1 from int8_tbl i8b1) b1
+    left join int8_tbl i8
+      left join (select *, null::int as d2 from int8_tbl i8b2, int4_tbl i4b2
+                 where q1 = f1) b2
+      on (i8.q1 = b2.q1)
+    on (b2.d2 = b1.q2)
+  on (t1.f1 = b1.d1)
+  left join int4_tbl i4
+  on (i8.q2 = i4.f1);
+
+select t1.* from
+  text_tbl t1
+  left join (select *, '***'::text as d1 from int8_tbl i8b1) b1
+    left join int8_tbl i8
+      left join (select *, null::int as d2 from int8_tbl i8b2, int4_tbl i4b2
+                 where q1 = f1) b2
+      on (i8.q1 = b2.q1)
+    on (b2.d2 = b1.q2)
+  on (t1.f1 = b1.d1)
+  left join int4_tbl i4
+  on (i8.q2 = i4.f1);
+
 --
 -- test ability to push constants through outer join clauses
 --
