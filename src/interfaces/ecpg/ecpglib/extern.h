edebddbb845575206d07145af2d718609b01f6ad
Michael Meskes <meskes@postgresql.org>
2015-08-13 13:22:29 +0200

    Fix declaration of isarray variable.
    Found and fixed by Andres Freund.

--- src/interfaces/ecpg/ecpglib/extern.h
+++ src/interfaces/ecpg/ecpglib/extern.h
@@ -44,7 +44,7 @@ struct ECPGtype_information_cache
 {
 	struct ECPGtype_information_cache *next;
 	int			oid;
-	bool		isarray;
+	enum ARRAY_TYPE	isarray;
 };
 
 /* structure to store one statement */
