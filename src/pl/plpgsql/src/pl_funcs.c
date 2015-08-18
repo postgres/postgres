4c3754ffe0f5f85cdecd01d7c1ab55df94559302
Tom Lane <tgl@sss.pgh.pa.us>
2015-08-18 19:22:37 -0400

    Fix a few bogus statement type names in plpgsql error messages.
    plpgsql's error location context messages ("PL/pgSQL function fn-name line
    line-no at stmt-type") would misreport a CONTINUE statement as being an
    EXIT, and misreport a MOVE statement as being a FETCH.  These are clear
    bugs that have been there a long time, so back-patch to all supported
    branches.
    
    In addition, in 9.5 and HEAD, change the description of EXECUTE from
    "EXECUTE statement" to just plain EXECUTE; there seems no good reason why
    this statement type should be described differently from others that have
    a well-defined head keyword.  And distinguish GET STACKED DIAGNOSTICS from
    plain GET DIAGNOSTICS.  These are a bit more of a judgment call, and also
    affect existing regression-test outputs, so I did not back-patch into
    stable branches.
    
    Pavel Stehule and Tom Lane

--- src/pl/plpgsql/src/pl_funcs.c
+++ src/pl/plpgsql/src/pl_funcs.c
@@ -235,7 +235,7 @@ plpgsql_stmt_typename(PLpgSQL_stmt *stmt)
 		case PLPGSQL_STMT_FOREACH_A:
 			return _("FOREACH over array");
 		case PLPGSQL_STMT_EXIT:
-			return "EXIT";
+			return ((PLpgSQL_stmt_exit *) stmt)->is_exit ? "EXIT" : "CONTINUE";
 		case PLPGSQL_STMT_RETURN:
 			return "RETURN";
 		case PLPGSQL_STMT_RETURN_NEXT:
@@ -249,15 +249,16 @@ plpgsql_stmt_typename(PLpgSQL_stmt *stmt)
 		case PLPGSQL_STMT_EXECSQL:
 			return _("SQL statement");
 		case PLPGSQL_STMT_DYNEXECUTE:
-			return _("EXECUTE statement");
+			return "EXECUTE";
 		case PLPGSQL_STMT_DYNFORS:
 			return _("FOR over EXECUTE statement");
 		case PLPGSQL_STMT_GETDIAG:
-			return "GET DIAGNOSTICS";
+			return ((PLpgSQL_stmt_getdiag *) stmt)->is_stacked ?
+				"GET STACKED DIAGNOSTICS" : "GET DIAGNOSTICS";
 		case PLPGSQL_STMT_OPEN:
 			return "OPEN";
 		case PLPGSQL_STMT_FETCH:
-			return "FETCH";
+			return ((PLpgSQL_stmt_fetch *) stmt)->is_move ? "MOVE" : "FETCH";
 		case PLPGSQL_STMT_CLOSE:
 			return "CLOSE";
 		case PLPGSQL_STMT_PERFORM:
