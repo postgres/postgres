747ca66977d5539a5ab9e5f33d3ca074fc3fd19b
Tom Lane <tgl@sss.pgh.pa.us>
2015-08-29 16:34:30 -0400

    Use "mb" not the nonexistent "rmb" for pg_read_barrier() on Alpha.
    It's only necessary to fix this in 9.4; later versions don't have this
    code (because we ripped out Alpha support entirely), while earlier ones
    aren't actually using pg_read_barrier() anywhere.
    
    Per rather belated report from Christoph Berg.

--- src/include/storage/barrier.h
+++ src/include/storage/barrier.h
@@ -117,7 +117,7 @@ extern slock_t dummy_spinlock;
  * read barrier to cover that case.  We might need to add that later.
  */
 #define pg_memory_barrier()		__asm__ __volatile__ ("mb" : : : "memory")
-#define pg_read_barrier()		__asm__ __volatile__ ("rmb" : : : "memory")
+#define pg_read_barrier()		__asm__ __volatile__ ("mb" : : : "memory")
 #define pg_write_barrier()		__asm__ __volatile__ ("wmb" : : : "memory")
 #elif defined(__hppa) || defined(__hppa__)		/* HP PA-RISC */
 
