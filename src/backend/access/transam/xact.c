dae1c9480e770a2c6f1716f3f0e1ad186cd7e7b3
Tom Lane <tgl@sss.pgh.pa.us>
2015-09-04 13:36:50 -0400

    Fix subtransaction cleanup after an outer-subtransaction portal fails.
    Formerly, we treated only portals created in the current subtransaction as
    having failed during subtransaction abort.  However, if the error occurred
    while running a portal created in an outer subtransaction (ie, a cursor
    declared before the last savepoint), that has to be considered broken too.
    
    To allow reliable detection of which ones those are, add a bookkeeping
    field to struct Portal that tracks the innermost subtransaction in which
    each portal has actually been executed.  (Without this, we'd end up
    failing portals containing functions that had called the subtransaction,
    thereby breaking plpgsql exception blocks completely.)
    
    In addition, when we fail an outer-subtransaction Portal, transfer its
    resources into the subtransaction's resource owner, so that they're
    released early in cleanup of the subxact.  This fixes a problem reported by
    Jim Nasby in which a function executed in an outer-subtransaction cursor
    could cause an Assert failure or crash by referencing a relation created
    within the inner subtransaction.
    
    The proximate cause of the Assert failure is that AtEOSubXact_RelationCache
    assumed it could blow away a relcache entry without first checking that the
    entry had zero refcount.  That was a bad idea on its own terms, so add such
    a check there, and to the similar coding in AtEOXact_RelationCache.  This
    provides an independent safety measure in case there are still ways to
    provoke the situation despite the Portal-level changes.
    
    This has been broken since subtransactions were invented, so back-patch
    to all supported branches.
    
    Tom Lane and Michael Paquier

--- src/backend/access/transam/xact.c
+++ src/backend/access/transam/xact.c
@@ -4146,6 +4146,7 @@ AbortSubTransaction(void)
 		AfterTriggerEndSubXact(false);
 		AtSubAbort_Portals(s->subTransactionId,
 						   s->parent->subTransactionId,
+						   s->curTransactionOwner,
 						   s->parent->curTransactionOwner);
 		AtEOSubXact_LargeObject(false, s->subTransactionId,
 								s->parent->subTransactionId);
--- src/backend/tcop/pquery.c
+++ src/backend/tcop/pquery.c
@@ -733,11 +733,7 @@ PortalRun(Portal portal, long count, bool isTopLevel,
 	/*
 	 * Check for improper portal use, and mark portal active.
 	 */
-	if (portal->status != PORTAL_READY)
-		ereport(ERROR,
-				(errcode(ERRCODE_OBJECT_NOT_IN_PREREQUISITE_STATE),
-				 errmsg("portal \"%s\" cannot be run", portal->name)));
-	portal->status = PORTAL_ACTIVE;
+	MarkPortalActive(portal);
 
 	/*
 	 * Set up global portal context pointers.
@@ -1397,11 +1393,7 @@ PortalRunFetch(Portal portal,
 	/*
 	 * Check for improper portal use, and mark portal active.
 	 */
-	if (portal->status != PORTAL_READY)
-		ereport(ERROR,
-				(errcode(ERRCODE_OBJECT_NOT_IN_PREREQUISITE_STATE),
-				 errmsg("portal \"%s\" cannot be run", portal->name)));
-	portal->status = PORTAL_ACTIVE;
+	MarkPortalActive(portal);
 
 	/*
 	 * Set up global portal context pointers.
