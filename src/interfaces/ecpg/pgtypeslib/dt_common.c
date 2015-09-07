0198a8d82a16ccffefbc692278eccba85f41c381
Greg Stark <stark@mit.edu>
2015-09-07 13:35:09 +0100

    Change type of DOW/DOY to UNITS

--- src/interfaces/ecpg/pgtypeslib/dt_common.c
+++ src/interfaces/ecpg/pgtypeslib/dt_common.c
@@ -123,8 +123,8 @@ static datetkn datetktbl[] = {
 	{"dec", MONTH, 12},
 	{"december", MONTH, 12},
 	{"dnt", TZ, 3600},			/* Dansk Normal Tid */
-	{"dow", RESERV, DTK_DOW},	/* day of week */
-	{"doy", RESERV, DTK_DOY},	/* day of year */
+	{"dow", UNITS, DTK_DOW},	/* day of week */
+	{"doy", UNITS, DTK_DOY},	/* day of year */
 	{"dst", DTZMOD, SECS_PER_HOUR},
 #if 0
 	{"dusst", DTZ, 21600},		/* Dushanbe Summer Time */
@@ -206,7 +206,7 @@ static datetkn datetktbl[] = {
 	{"irkst", DTZ, 32400},		/* Irkutsk Summer Time */
 	{"irkt", TZ, 28800},		/* Irkutsk Time */
 	{"irt", TZ, 12600},			/* Iran Time */
-	{"isodow", RESERV, DTK_ISODOW},		/* ISO day of week, Sunday == 7 */
+	{"isodow", UNITS, DTK_ISODOW},		/* ISO day of week, Sunday == 7 */
 #if 0
 	isst
 #endif
