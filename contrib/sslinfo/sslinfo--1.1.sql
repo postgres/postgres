49124613f134b04594b1a5c46368eb0a5db16d4b
Alvaro Herrera <alvherre@alvh.no-ip.org>
2015-09-07 21:24:17 -0300

    contrib/sslinfo: add ssl_extension_info SRF
    This new function provides information about SSL extensions present in
    the X509 certificate used for the current connection.
    
    Extension version updated to version 1.1.
    
    Author: Дмитрий Воронин (Dmitry Voronin)
    Reviewed by: Michael Paquier, Heikki Linnakangas, Álvaro Herrera

--- contrib/sslinfo/sslinfo--1.1.sql
+++ contrib/sslinfo/sslinfo--1.1.sql
@@ -1,4 +1,4 @@
-/* contrib/sslinfo/sslinfo--1.0.sql */
+/* contrib/sslinfo/sslinfo--1.1.sql */
 
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION sslinfo" to load this file. \quit
@@ -37,4 +37,12 @@ LANGUAGE C STRICT;
 
 CREATE FUNCTION ssl_issuer_dn() RETURNS text
 AS 'MODULE_PATHNAME', 'ssl_issuer_dn'
+LANGUAGE C STRICT;
+
+CREATE FUNCTION
+ssl_extension_info(OUT name text,
+    OUT value text,
+    OUT critical boolean
+) RETURNS SETOF record
+AS 'MODULE_PATHNAME', 'ssl_extension_info'
 LANGUAGE C STRICT;
