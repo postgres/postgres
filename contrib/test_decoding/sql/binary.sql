eef34e523693c15eb4ad7067c96269ab96c87ef5
Andres Freund <andres@anarazel.de>
2015-09-22 15:33:30 +0200

    test_decoding: Protect against rare spurious test failures.
    A bunch of tests missed specifying that empty transactions shouldn't be
    displayed. That causes problems when e.g. autovacuum runs in an
    unfortunate moment. The tests in question only run for a very short
    time, making this quite unlikely.
    
    Reported-By: Buildfarm member axolotl
    Backpatch: 9.4, where logical decoding was introduced

--- contrib/test_decoding/sql/binary.sql
+++ contrib/test_decoding/sql/binary.sql
@@ -3,12 +3,12 @@ SET synchronous_commit = on;
 
 SELECT 'init' FROM pg_create_logical_replication_slot('regression_slot', 'test_decoding');
 -- succeeds, textual plugin, textual consumer
-SELECT data FROM pg_logical_slot_get_changes('regression_slot', NULL, NULL, 'force-binary', '0');
+SELECT data FROM pg_logical_slot_get_changes('regression_slot', NULL, NULL, 'force-binary', '0', 'skip-empty-xacts', '1');
 -- fails, binary plugin, textual consumer
-SELECT data FROM pg_logical_slot_get_changes('regression_slot', NULL, NULL, 'force-binary', '1');
+SELECT data FROM pg_logical_slot_get_changes('regression_slot', NULL, NULL, 'force-binary', '1', 'skip-empty-xacts', '1');
 -- succeeds, textual plugin, binary consumer
-SELECT data FROM pg_logical_slot_get_binary_changes('regression_slot', NULL, NULL, 'force-binary', '0');
+SELECT data FROM pg_logical_slot_get_binary_changes('regression_slot', NULL, NULL, 'force-binary', '0', 'skip-empty-xacts', '1');
 -- succeeds, binary plugin, binary consumer
-SELECT data FROM pg_logical_slot_get_binary_changes('regression_slot', NULL, NULL, 'force-binary', '1');
+SELECT data FROM pg_logical_slot_get_binary_changes('regression_slot', NULL, NULL, 'force-binary', '1', 'skip-empty-xacts', '1');
 
 SELECT 'init' FROM pg_drop_replication_slot('regression_slot');
