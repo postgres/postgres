729d0056b42a512648e49daa727e5fe77d0cfeeb
Andrew Dunstan <andrew@dunslane.net>
2015-09-28 18:29:20 -0400

    Fix compiler warning about unused function in non-readline case.
    Backpatch to all live branches to keep the code in sync.

--- src/bin/psql/input.c
+++ src/bin/psql/input.c
@@ -382,10 +382,10 @@ initializeInput(int flags)
  *
  * max_lines: if >= 0, limit history file to that many entries.
  */
+#ifdef USE_READLINE
 static bool
 saveHistory(char *fname, int max_lines)
 {
-#ifdef USE_READLINE
 	int			errnum;
 
 	/*
@@ -450,10 +450,10 @@ saveHistory(char *fname, int max_lines)
 		psql_error("could not save history to file \"%s\": %s\n",
 				   fname, strerror(errnum));
 	}
-#endif
-
 	return false;
 }
+#endif
+
 
 
 /*
