0577821b57253d0ae43ffdddc8f94f07866830ae
Andres Freund <andres@anarazel.de>
2015-10-05 11:53:43 +0200

    Re-Align *_freeze_max_age reloption limits with corresponding GUC limits.
    In 020235a5754 I lowered the autovacuum_*freeze_max_age minimums to
    allow for easier testing of wraparounds. I did not touch the
    corresponding per-table limits. While those don't matter for the purpose
    of wraparound, it seems more consistent to lower them as well.
    
    It's noteworthy that the previous reloption lower limit for
    autovacuum_multixact_freeze_max_age was too high by one magnitude, even
    before 020235a5754.
    
    Discussion: 26377.1443105453@sss.pgh.pa.us
    Backpatch: back to 9.0 (in parts), like the prior patch

--- src/backend/access/common/reloptions.c
+++ src/backend/access/common/reloptions.c
@@ -185,7 +185,7 @@ static relopt_int intRelOpts[] =
 			"Age at which to autovacuum a table to prevent transaction ID wraparound",
 			RELOPT_KIND_HEAP | RELOPT_KIND_TOAST
 		},
-		-1, 100000000, 2000000000
+		-1, 100000, 2000000000
 	},
 	{
 		{
@@ -193,7 +193,7 @@ static relopt_int intRelOpts[] =
 			"Multixact age at which to autovacuum a table to prevent multixact wraparound",
 			RELOPT_KIND_HEAP | RELOPT_KIND_TOAST
 		},
-		-1, 100000000, 2000000000
+		-1, 10000, 2000000000
 	},
 	{
 		{
