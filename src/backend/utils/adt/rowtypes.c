a0c02ed5b4ef24d91a6fb22d447fc5cba8aeb146
Noah Misch <noah@leadboat.com>
2015-10-05 10:06:29 -0400

    Prevent stack overflow in container-type functions.
    A range type can name another range type as its subtype, and a record
    type can bear a column of another record type.  Consequently, functions
    like range_cmp() and record_recv() are recursive.  Functions at risk
    include operator family members and referents of pg_type regproc
    columns.  Treat as recursive any such function that looks up and calls
    the same-purpose function for a record column type or the range subtype.
    Back-patch to 9.0 (all supported versions).
    
    An array type's element type is never itself an array type, so array
    functions are unaffected.  Recursion depth proportional to array
    dimensionality, found in array_dim_to_jsonb(), is fine thanks to MAXDIM.

--- src/backend/utils/adt/rowtypes.c
+++ src/backend/utils/adt/rowtypes.c
@@ -21,6 +21,7 @@
 #include "catalog/pg_type.h"
 #include "funcapi.h"
 #include "libpq/pqformat.h"
+#include "miscadmin.h"
 #include "utils/builtins.h"
 #include "utils/lsyscache.h"
 #include "utils/typcache.h"
@@ -85,6 +86,8 @@ record_in(PG_FUNCTION_ARGS)
 	Datum	   *values;
 	bool	   *nulls;
 	StringInfoData buf;
+
+	check_stack_depth();		/* recurses for record-type columns */
 
 	/*
 	 * Give a friendly error message if we did not get enough info to identify
@@ -309,6 +312,8 @@ record_out(PG_FUNCTION_ARGS)
 	bool	   *nulls;
 	StringInfoData buf;
 
+	check_stack_depth();		/* recurses for record-type columns */
+
 	/* Extract type info from the tuple itself */
 	tupType = HeapTupleHeaderGetTypeId(rec);
 	tupTypmod = HeapTupleHeaderGetTypMod(rec);
@@ -457,6 +462,8 @@ record_recv(PG_FUNCTION_ARGS)
 	int			i;
 	Datum	   *values;
 	bool	   *nulls;
+
+	check_stack_depth();		/* recurses for record-type columns */
 
 	/*
 	 * Give a friendly error message if we did not get enough info to identify
@@ -650,6 +657,8 @@ record_send(PG_FUNCTION_ARGS)
 	bool	   *nulls;
 	StringInfoData buf;
 
+	check_stack_depth();		/* recurses for record-type columns */
+
 	/* Extract type info from the tuple itself */
 	tupType = HeapTupleHeaderGetTypeId(rec);
 	tupTypmod = HeapTupleHeaderGetTypMod(rec);
@@ -792,6 +801,8 @@ record_cmp(FunctionCallInfo fcinfo)
 	int			i1;
 	int			i2;
 	int			j;
+
+	check_stack_depth();		/* recurses for record-type columns */
 
 	/* Extract type info from the tuples */
 	tupType1 = HeapTupleHeaderGetTypeId(record1);
@@ -1028,6 +1039,8 @@ record_eq(PG_FUNCTION_ARGS)
 	int			i1;
 	int			i2;
 	int			j;
+
+	check_stack_depth();		/* recurses for record-type columns */
 
 	/* Extract type info from the tuples */
 	tupType1 = HeapTupleHeaderGetTypeId(record1);
