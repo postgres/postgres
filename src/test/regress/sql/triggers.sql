34263e8e4a679ee785e601566bf144bf1f50a2b6
Andres Freund <andres@anarazel.de>
2015-12-10 16:26:45 +0100

    Fix ON CONFLICT UPDATE bug breaking AFTER UPDATE triggers.
    ExecOnConflictUpdate() passed t_ctid of the to-be-updated tuple to
    ExecUpdate(). That's problematic primarily because of two reason: First
    and foremost t_ctid could point to a different tuple. Secondly, and
    that's what triggered the complaint by Stanislav, t_ctid is changed by
    heap_update() to point to the new tuple version.  The behavior of AFTER
    UPDATE triggers was therefore broken, with NEW.* and OLD.* tuples
    spuriously identical within AFTER UPDATE triggers.
    
    To fix both issues, pass a pointer to t_self of a on-stack HeapTuple
    instead.
    
    Fixing this bug lead to one change in regression tests, which previously
    failed due to the first issue mentioned above. There's a reasonable
    expectation that test fails, as it updates one row repeatedly within one
    INSERT ... ON CONFLICT statement. That is only possible if the second
    update is triggered via ON CONFLICT ... SET, ON CONFLICT ... WHERE, or
    by a WITH CHECK expression, as those are executed after
    ExecOnConflictUpdate() does a visibility check. That could easily be
    prohibited, but given it's allowed for plain UPDATEs and a rare corner
    case, it doesn't seem worthwhile.
    
    Reported-By: Stanislav Grozev
    Author: Andres Freund and Peter Geoghegan
    Discussion: CAA78GVqy1+LisN-8DygekD_Ldfy=BJLarSpjGhytOsgkpMavfQ@mail.gmail.com
    Backpatch: 9.5, where ON CONFLICT was introduced

--- src/test/regress/sql/triggers.sql
+++ src/test/regress/sql/triggers.sql
@@ -1215,7 +1215,7 @@ create function upsert_after_func()
 $$
 begin
   if (TG_OP = 'UPDATE') then
-    raise warning 'after update (old): %', new.*::text;
+    raise warning 'after update (old): %', old.*::text;
     raise warning 'after update (new): %', new.*::text;
   elsif (TG_OP = 'INSERT') then
     raise warning 'after insert (new): %', new.*::text;
