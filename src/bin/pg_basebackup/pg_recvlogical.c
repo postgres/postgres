c3e068b26185e6c667dbb45f05bb2466ed11cfea
Alvaro Herrera <alvherre@alvh.no-ip.org>
2015-12-28 10:50:35 -0300

    Fix translation domain in pg_basebackup
    For some reason, we've been overlooking the fact that pg_receivexlog
    and pg_recvlogical are using wrong translation domains all along,
    so their output hasn't ever been translated.  The right domain is
    pg_basebackup, not their own executable names.
    
    Noticed by Ioseph Kim, who's been working on the Korean translation.
    
    Backpatch pg_receivexlog to 9.2 and pg_recvlogical to 9.4.

--- src/bin/pg_basebackup/pg_recvlogical.c
+++ src/bin/pg_basebackup/pg_recvlogical.c
@@ -645,7 +645,7 @@ main(int argc, char **argv)
 	char	   *db_name;
 
 	progname = get_progname(argv[0]);
-	set_pglocale_pgservice(argv[0], PG_TEXTDOMAIN("pg_recvlogical"));
+	set_pglocale_pgservice(argv[0], PG_TEXTDOMAIN("pg_basebackup"));
 
 	if (argc > 1)
 	{
