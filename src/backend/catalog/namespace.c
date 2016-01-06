4bf87169cc1890442aa694f3057e0a0ad60c51f4
Tom Lane <tgl@sss.pgh.pa.us>
2016-01-06 11:06:42 -0500

    Comment typo fix.
    Per Amit Langote.

--- src/backend/catalog/namespace.c
+++ src/backend/catalog/namespace.c
@@ -513,9 +513,9 @@ RangeVarGetCreationNamespace(const RangeVar *newRelation)
  * As a side effect, this function acquires AccessShareLock on the target
  * namespace.  Without this, the namespace could be dropped before our
  * transaction commits, leaving behind relations with relnamespace pointing
- * to a no-longer-exstant namespace.
+ * to a no-longer-existent namespace.
  *
- * As a further side-effect, if the select namespace is a temporary namespace,
+ * As a further side-effect, if the selected namespace is a temporary namespace,
  * we mark the RangeVar as RELPERSISTENCE_TEMP.
  */
 Oid
