3ef16c46fb3a64c150a3b42c3cc4a8538a12ff3f
Tom Lane <tgl@sss.pgh.pa.us>
2016-01-09 17:20:58 -0500

    Add some checks on "char"-type columns to type_sanity and opr_sanity.
    I noticed that the sanity checks in the regression tests omitted to
    check a couple of "poor man's enum" columns that you'd reasonably
    expect them to check.
    
    There are other "char"-type columns in system catalogs that are not
    covered by either type_sanity or opr_sanity, e.g. pg_rewrite.ev_type.
    However, those catalogs are not populated with any manually-created
    data during bootstrap, so it seems less necessary to check them
    this way.

--- src/test/regress/sql/opr_sanity.sql
+++ src/test/regress/sql/opr_sanity.sql
@@ -64,7 +64,9 @@ WHERE p1.prolang = 0 OR p1.prorettype = 0 OR
        array_upper(p1.proargtypes, 1) != p1.pronargs-1 OR
        0::oid = ANY (p1.proargtypes) OR
        procost <= 0 OR
-       CASE WHEN proretset THEN prorows <= 0 ELSE prorows != 0 END;
+       CASE WHEN proretset THEN prorows <= 0 ELSE prorows != 0 END OR
+       provolatile NOT IN ('i', 's', 'v') OR
+       proparallel NOT IN ('s', 'r', 'u');
 
 -- prosrc should never be null or empty
 SELECT p1.oid, p1.proname
