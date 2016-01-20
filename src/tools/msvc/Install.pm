49d65e857cad7ddc4d82f8276cc63ddce0be4ee1
Bruce Momjian <bruce@momjian.us>
2016-01-19 23:30:28 -0500

    Properly install dynloader.h on MSVC builds
    This will enable PL/Java to be cleanly compiled, as dynloader.h is a
    requirement.
    
    Report by Chapman Flack
    
    Patch by Michael Paquier
    
    Backpatch through 9.1

--- src/tools/msvc/Install.pm
+++ src/tools/msvc/Install.pm
@@ -492,7 +492,7 @@ sub CopyIncludeFiles
 		'Public headers',
 		$target . '/include/',
 		'src/include/', 'postgres_ext.h', 'pg_config.h', 'pg_config_os.h',
-		'pg_config_manual.h');
+		'dynloader.h', 'pg_config_manual.h');
 	lcopy('src/include/libpq/libpq-fs.h', $target . '/include/libpq/')
 	  || croak 'Could not copy libpq-fs.h';
 
@@ -515,7 +515,7 @@ sub CopyIncludeFiles
 	CopyFiles(
 		'Server headers',
 		$target . '/include/server/',
-		'src/include/', 'pg_config.h', 'pg_config_os.h');
+		'src/include/', 'pg_config.h', 'pg_config_os.h', 'dynloader.h');
 	CopyFiles(
 		'Grammar header',
 		$target . '/include/server/parser/',
