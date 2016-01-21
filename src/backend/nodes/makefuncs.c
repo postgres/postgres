e80c85e4e8d9b7bd02ff5737f7a740487cee71d4
Tom Lane <tgl@sss.pgh.pa.us>
2016-01-21 12:55:59 -0500

    Add defenses against putting expanded objects into Const nodes.
    Putting a reference to an expanded-format value into a Const node would be
    a bad idea for a couple of reasons.  It'd be possible for the supposedly
    immutable Const to change value, if something modified the referenced
    variable ... in fact, if the Const's reference were R/W, any function that
    has the Const as argument might itself change it at runtime.  Also, because
    datumIsEqual() is pretty simplistic, the Const might fail to compare equal
    to other Consts that it should compare equal to, notably including copies
    of itself.  This could lead to unexpected planner behavior, such as "could
    not find pathkey item to sort" errors or inferior plans.
    
    I have not been able to find any way to get an expanded value into a Const
    within the existing core code; but Paul Ramsey was able to trigger the
    problem by writing a datatype input function that returns an expanded
    value.
    
    The best fix seems to be to establish a rule that varlena values being
    placed into Const nodes should be passed through pg_detoast_datum().
    That will do nothing (and cost little) in normal cases, but it will flatten
    expanded values and thereby avoid the above problems.  Also, it will
    convert short-header or compressed values into canonical format, which will
    avoid possible unexpected lack-of-equality issues for those cases too.
    And it provides a last-ditch defense against putting a toasted value into
    a Const, which we already knew was dangerous, cf commit 2b0c86b66563cf2f.
    (In the light of this discussion, I'm no longer sure that that commit
    provided 100% protection against such cases, but this fix should do it.)
    
    The test added in commit 65c3d05e18e7c530 to catch datatype input functions
    with unstable results would fail for functions that returned expanded
    values; but it seems a bit uncharitable to deem a result unstable just
    because it's expressed in expanded form, so revise the coding so that we
    check for bitwise equality only after applying pg_detoast_datum().  That's
    a sufficient condition anyway given the new rule about detoasting when
    forming a Const.
    
    Back-patch to 9.5 where the expanded-object facility was added.  It's
    possible that this should go back further; but in the absence of clear
    evidence that there's any live bug in older branches, I'll refrain for now.

--- src/backend/nodes/makefuncs.c
+++ src/backend/nodes/makefuncs.c
@@ -17,6 +17,7 @@
 
 #include "catalog/pg_class.h"
 #include "catalog/pg_type.h"
+#include "fmgr.h"
 #include "nodes/makefuncs.h"
 #include "nodes/nodeFuncs.h"
 #include "utils/lsyscache.h"
@@ -301,6 +302,14 @@ makeConst(Oid consttype,
 		  bool constbyval)
 {
 	Const	   *cnst = makeNode(Const);
+
+	/*
+	 * If it's a varlena value, force it to be in non-expanded (non-toasted)
+	 * format; this avoids any possible dependency on external values and
+	 * improves consistency of representation, which is important for equal().
+	 */
+	if (!constisnull && constlen == -1)
+		constvalue = PointerGetDatum(PG_DETOAST_DATUM(constvalue));
 
 	cnst->consttype = consttype;
 	cnst->consttypmod = consttypmod;
