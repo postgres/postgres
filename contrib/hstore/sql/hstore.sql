1c291624b29dae2049ff1c7008563fbca85df658
Tom Lane <tgl@sss.pgh.pa.us>
2016-02-03 01:39:08 -0500

    Fix IsValidJsonNumber() to notice trailing non-alphanumeric garbage.
    Commit e09996ff8dee3f70 was one brick shy of a load: it didn't insist
    that the detected JSON number be the whole of the supplied string.
    This allowed inputs such as "2016-01-01" to be misdetected as valid JSON
    numbers.  Per bug #13906 from Dmitry Ryabov.
    
    In passing, be more wary of zero-length input (I'm not sure this can
    happen given current callers, but better safe than sorry), and do some
    minor cosmetic cleanup.

--- contrib/hstore/sql/hstore.sql
+++ contrib/hstore/sql/hstore.sql
@@ -334,11 +334,11 @@ select count(*) from testhstore where h = 'pos=>98, line=>371, node=>CBA, indexe
 -- json and jsonb
 select hstore_to_json('"a key" =>1, b => t, c => null, d=> 12345, e => 012345, f=> 1.234, g=> 2.345e+4');
 select cast( hstore  '"a key" =>1, b => t, c => null, d=> 12345, e => 012345, f=> 1.234, g=> 2.345e+4' as json);
-select hstore_to_json_loose('"a key" =>1, b => t, c => null, d=> 12345, e => 012345, f=> 1.234, g=> 2.345e+4');
+select hstore_to_json_loose('"a key" =>1, b => t, c => null, d=> 12345, e => 012345, f=> 1.234, g=> 2.345e+4, h=> "2016-01-01"');
 
 select hstore_to_jsonb('"a key" =>1, b => t, c => null, d=> 12345, e => 012345, f=> 1.234, g=> 2.345e+4');
 select cast( hstore  '"a key" =>1, b => t, c => null, d=> 12345, e => 012345, f=> 1.234, g=> 2.345e+4' as jsonb);
-select hstore_to_jsonb_loose('"a key" =>1, b => t, c => null, d=> 12345, e => 012345, f=> 1.234, g=> 2.345e+4');
+select hstore_to_jsonb_loose('"a key" =>1, b => t, c => null, d=> 12345, e => 012345, f=> 1.234, g=> 2.345e+4, h=> "2016-01-01"');
 
 create table test_json_agg (f1 text, f2 hstore);
 insert into test_json_agg values ('rec1','"a key" =>1, b => t, c => null, d=> 12345, e => 012345, f=> 1.234, g=> 2.345e+4'),
