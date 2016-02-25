f6a605702aa15f7a49da1ec0ab60a8abd956c139
Alvaro Herrera <alvherre@alvh.no-ip.org>
2016-02-25 20:50:20 -0300

    Fix typos
    Backpatch to: 9.4

--- src/include/replication/reorderbuffer.h
+++ src/include/replication/reorderbuffer.h
@@ -65,8 +65,8 @@ typedef struct ReorderBufferChange
 	enum ReorderBufferChangeType action;
 
 	/*
-	 * Context data for the change, which part of the union is valid depends
-	 * on action/action_internal.
+	 * Context data for the change. Which part of the union is valid depends
+	 * on action.
 	 */
 	union
 	{
@@ -228,7 +228,7 @@ typedef struct ReorderBufferTXN
 	/* ---
 	 * Position in one of three lists:
 	 * * list of subtransactions if we are *known* to be subxact
-	 * * list of toplevel xacts (can be a as-yet unknown subxact)
+	 * * list of toplevel xacts (can be an as-yet unknown subxact)
 	 * * list of preallocated ReorderBufferTXNs
 	 * ---
 	 */
@@ -266,7 +266,7 @@ struct ReorderBuffer
 
 	/*
 	 * Transactions that could be a toplevel xact, ordered by LSN of the first
-	 * record bearing that xid..
+	 * record bearing that xid.
 	 */
 	dlist_head	toplevel_by_lsn;
 
@@ -278,7 +278,7 @@ struct ReorderBuffer
 	ReorderBufferTXN *by_txn_last_txn;
 
 	/*
-	 * Callacks to be called when a transactions commits.
+	 * Callbacks to be called when a transactions commits.
 	 */
 	ReorderBufferBeginCB begin;
 	ReorderBufferApplyChangeCB apply_change;
@@ -301,7 +301,7 @@ struct ReorderBuffer
 	 * overhead we cache some unused ones here.
 	 *
 	 * The maximum number of cached entries is controlled by const variables
-	 * ontop of reorderbuffer.c
+	 * on top of reorderbuffer.c
 	 */
 
 	/* cached ReorderBufferTXNs */
