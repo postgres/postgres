87cc6b57a911bf439ff9e53f1eddba58e9749ffe
Andrew Dunstan <andrew@dunslane.net>
2016-02-27 12:28:21 -0500

    Respect TEMP_CONFIG when pg_regress_check and friends are called
    This reverts commit 9117985b6ba9beda4f280f596035649fc23b6233 in favor of
    a more general solution.

--- contrib/contrib-global.mk
+++ contrib/contrib-global.mk
@@ -1,9 +1,4 @@
 # contrib/contrib-global.mk
 
-# file with extra config for temp build
-ifdef TEMP_CONFIG
-REGRESS_OPTS += --temp-config=$(TEMP_CONFIG)
-endif
-
 NO_PGXS = 1
 include $(top_srcdir)/src/makefiles/pgxs.mk
