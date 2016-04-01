a067b50470cf7fda77dd28b03519f2483c2322bf
Tom Lane <tgl@sss.pgh.pa.us>
2016-04-01 12:25:17 -0400

    Get rid of minus zero in box regression test.
    Commit acdf2a8b added a test case involving minus zero as a box endpoint.
    This is not very portable, as evidenced by the several older buildfarm
    members that are failing on the test because they print minus zero as
    just "0".  If there were any significant reason to test this behavior,
    we could consider carrying a separate expected-file; but it doesn't look
    to me like there's adequate justification to accept such a maintenance
    burden.  Just change the test to use plain zero, instead.

--- src/test/regress/sql/box.sql
+++ src/test/regress/sql/box.sql
@@ -132,7 +132,7 @@ CREATE INDEX box_spgist ON box_temp USING spgist (f1);
 
 INSERT INTO box_temp
 	VALUES (NULL),
-		   ('(-0,0)(0,100)'),
+		   ('(0,0)(0,100)'),
 		   ('(-3,4.3333333333)(40,1)'),
 		   ('(0,100)(0,infinity)'),
 		   ('(-infinity,0)(0,infinity)'),
