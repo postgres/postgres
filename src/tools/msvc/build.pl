01a07e6c11562127ad5e6f58d3d6128416b8ab65
Andrew Dunstan <andrew@dunslane.net>
2016-04-08 12:25:10 -0400

    Turn down MSVC compiler verbosity
    Most of what is produced by the detailed verbosity level is of no
    interest at all, so switch to the normal level for more usable output.
    
    Christian Ullrich
    
    Backpatch to all live branches

--- src/tools/msvc/build.pl
+++ src/tools/msvc/build.pl
@@ -53,7 +53,7 @@ elsif (uc($ARGV[0]) ne "RELEASE")
 if ($buildwhat and $vcver >= 10.00)
 {
 	system(
-"msbuild $buildwhat.vcxproj /verbosity:detailed /p:Configuration=$bconf");
+"msbuild $buildwhat.vcxproj /verbosity:normal /p:Configuration=$bconf");
 }
 elsif ($buildwhat)
 {
@@ -61,7 +61,7 @@ elsif ($buildwhat)
 }
 else
 {
-	system("msbuild pgsql.sln /verbosity:detailed /p:Configuration=$bconf");
+	system("msbuild pgsql.sln /verbosity:normal /p:Configuration=$bconf");
 }
 
 # report status
