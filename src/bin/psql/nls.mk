c3136876734b31ce50018f39c87b00145a8c7c33
Peter Eisentraut <peter_e@gmx.net>
2016-04-15 20:23:41 -0400

    psql: Add new gettext trigger

--- src/bin/psql/nls.mk
+++ src/bin/psql/nls.mk
@@ -8,5 +8,5 @@ GETTEXT_FILES    = command.c common.c copy.c help.c input.c large_obj.c \
                    ../../fe_utils/print.c ../../fe_utils/psqlscan.c \
                    ../../common/exec.c ../../common/fe_memutils.c ../../common/username.c \
                    ../../common/wait_error.c
-GETTEXT_TRIGGERS = N_ psql_error simple_prompt
-GETTEXT_FLAGS    = psql_error:1:c-format
+GETTEXT_TRIGGERS = N_ psql_error simple_prompt write_error
+GETTEXT_FLAGS    = psql_error:1:c-format write_error:1:c-format
