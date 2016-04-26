3019f432d6fffe6d8e04f5ccc592eb385af96492
Peter Eisentraut <peter_e@gmx.net>
2016-04-26 12:04:43 -0400

    pg_dump: Message style improvements
    forgotten in b6dacc173b6830c515d970698cead9a85663c553

--- src/bin/pg_dump/pg_dump.c
+++ src/bin/pg_dump/pg_dump.c
@@ -1243,7 +1243,7 @@ expand_table_name_patterns(Archive *fout,
 
 		res = ExecuteSqlQuery(fout, query->data, PGRES_TUPLES_OK);
 		if (strict_names && PQntuples(res) == 0)
-			exit_horribly(NULL, "No matching table(s) were found for pattern \"%s\"\n", cell->val);
+			exit_horribly(NULL, "no matching tables were found for pattern \"%s\"\n", cell->val);
 
 		for (i = 0; i < PQntuples(res); i++)
 		{
