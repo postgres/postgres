32ceb8dfbf3c2ae785a595db9821d16e92f8431b
Tom Lane <tgl@sss.pgh.pa.us>
2016-06-06 17:44:18 -0400

    Don't reset changes_since_analyze after a selective-columns ANALYZE.
    If we ANALYZE only selected columns of a table, we should not postpone
    auto-analyze because of that; other columns may well still need stats
    updates.  As committed, the counter is left alone if a column list is
    given, whether or not it includes all analyzable columns of the table.
    Per complaint from Tomasz Ostrowski.
    
    It's been like this a long time, so back-patch to all supported branches.
    
    Report: <ef99c1bd-ff60-5f32-2733-c7b504eb960c@ato.waw.pl>

--- src/backend/commands/analyze.c
+++ src/backend/commands/analyze.c
@@ -567,10 +567,13 @@ do_analyze_rel(Relation onerel, VacuumStmt *vacstmt,
 	/*
 	 * Report ANALYZE to the stats collector, too.  However, if doing
 	 * inherited stats we shouldn't report, because the stats collector only
-	 * tracks per-table stats.
+	 * tracks per-table stats.  Reset the changes_since_analyze counter only
+	 * if we analyzed all columns; otherwise, there is still work for
+	 * auto-analyze to do.
 	 */
 	if (!inh)
-		pgstat_report_analyze(onerel, totalrows, totaldeadrows);
+		pgstat_report_analyze(onerel, totalrows, totaldeadrows,
+							  (vacstmt->va_cols == NIL));
 
 	/* If this isn't part of VACUUM ANALYZE, let index AMs do cleanup */
 	if (!(vacstmt->options & VACOPT_VACUUM))
--- src/include/pgstat.h
+++ src/include/pgstat.h
@@ -339,6 +339,7 @@ typedef struct PgStat_MsgAnalyze
 	Oid			m_databaseid;
 	Oid			m_tableoid;
 	bool		m_autovacuum;
+	bool		m_resetcounter;
 	TimestampTz m_analyzetime;
 	PgStat_Counter m_live_tuples;
 	PgStat_Counter m_dead_tuples;
@@ -707,7 +708,8 @@ extern void pgstat_report_autovac(Oid dboid);
 extern void pgstat_report_vacuum(Oid tableoid, bool shared,
 					 PgStat_Counter tuples);
 extern void pgstat_report_analyze(Relation rel,
-					  PgStat_Counter livetuples, PgStat_Counter deadtuples);
+					  PgStat_Counter livetuples, PgStat_Counter deadtuples,
+					  bool resetcounter);
 
 extern void pgstat_report_recovery_conflict(int reason);
 
