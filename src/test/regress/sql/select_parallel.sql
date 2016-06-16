8c1d9d56e9a00680a035b8b333a98ea16b121eb7
Robert Haas <rhaas@postgresql.org>
2016-06-16 12:00:55 -0400

    Add regression test for 04ae11f62e643e07c411c4935ea6af46cb112aa9.
    The code in this area needs further revision, and it would be best
    not to re-break the things we've already fixed.
    
    Per a gripe from Tom Lane.

--- src/test/regress/sql/select_parallel.sql
+++ src/test/regress/sql/select_parallel.sql
@@ -2,6 +2,15 @@
 -- PARALLEL
 --
 
+create or replace function parallel_restricted(int) returns int as $$
+begin
+  perform * from pg_stat_activity where client_port is null;
+  if (found) then
+    raise 'parallel restricted function run in worker';
+  end if;
+  return $1;
+end$$ language plpgsql parallel restricted;
+
 -- Serializable isolation would disable parallel query, so explicitly use an
 -- arbitrary other level.
 begin isolation level repeatable read;
@@ -9,10 +18,19 @@ begin isolation level repeatable read;
 -- setup parallel test
 set parallel_setup_cost=0;
 set parallel_tuple_cost=0;
+set max_parallel_workers_per_gather=4;
 
 explain (costs off)
   select count(*) from a_star;
 select count(*) from a_star;
+
+-- test that parallel_restricted function doesn't run in worker
+alter table tenk1 set (parallel_workers = 4);
+explain (verbose, costs off)
+select parallel_restricted(unique1) from tenk1
+  where stringu1 = 'GRAAAA' order by 1;
+select parallel_restricted(unique1) from tenk1
+  where stringu1 = 'GRAAAA' order by 1;
 
 set force_parallel_mode=1;
 
