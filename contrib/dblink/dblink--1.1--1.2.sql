20eb2731b7775f3381939d2667b6aa8ba62ab2c5
Robert Haas <rhaas@postgresql.org>
2016-06-17 15:09:57 -0400

    Update dblink extension for parallel query.
    Almost all functions provided by this extension are PARALLEL
    RESTRICTED.  Mostly, that's because the leader's TCP connections won't
    be shared with the workers, but in some cases like dblink_get_pkey
    it's because they obtain locks which might be released early if taken
    within a parallel worker.  dblink_fdw_validator probably can't be used
    in a query anyway, but there would be no problem from the point of
    view of parallel query if it were, so it's PARALLEL SAFE.
    
    Andreas Karlsson

---
+++ contrib/dblink/dblink--1.1--1.2.sql
@@ -0,0 +1,46 @@
+/* contrib/dblink/dblink--1.1--1.2.sql */
+
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION dblink UPDATE TO '1.2'" to load this file. \quit
+
+ALTER FUNCTION dblink_connect(text) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_connect(text, text) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_connect_u(text) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_connect_u(text, text) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_disconnect() PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_disconnect(text) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_open(text, text) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_open(text, text, boolean) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_open(text, text, text) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_open(text, text, text, boolean) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_fetch(text, int) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_fetch(text, int, boolean) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_fetch(text, text, int) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_fetch(text, text, int, boolean) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_close(text) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_close(text, boolean) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_close(text, text) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_close(text, text, boolean) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink(text, text) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink(text, text, boolean) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink(text) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink(text, boolean) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_exec(text, text) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_exec(text, text, boolean) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_exec(text) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_exec(text, boolean) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_get_pkey(text) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_build_sql_insert(text, int2vector, int, _text, _text) PARALLEL SAFE;
+ALTER FUNCTION dblink_build_sql_delete(text, int2vector, int, _text) PARALLEL SAFE;
+ALTER FUNCTION dblink_build_sql_update(text, int2vector, int, _text, _text) PARALLEL SAFE;
+ALTER FUNCTION dblink_current_query() PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_send_query(text, text) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_is_busy(text) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_get_result(text) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_get_result(text, bool) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_get_connections() PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_cancel_query(text) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_error_message(text) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_get_notify() PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_get_notify(text) PARALLEL RESTRICTED;
+ALTER FUNCTION dblink_fdw_validator(text[], oid) PARALLEL SAFE;
