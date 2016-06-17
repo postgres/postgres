1ca4a1b5d2c633d33161792af1c70e397f879ed9
Alvaro Herrera <alvherre@alvh.no-ip.org>
2016-06-17 18:05:55 -0400

    Finish up XLOG_HINT renaming
    Commit b8fd1a09f3 renamed XLOG_HINT to XLOG_FPI, but neglected two
    places.
    
    Backpatch to 9.3, like that commit.

--- src/backend/storage/buffer/bufmgr.c
+++ src/backend/storage/buffer/bufmgr.c
@@ -3352,7 +3352,7 @@ IncrBufferRefCount(Buffer buffer)
  * This is essentially the same as MarkBufferDirty, except:
  *
  * 1. The caller does not write WAL; so if checksums are enabled, we may need
- *	  to write an XLOG_HINT WAL record to protect against torn pages.
+ *	  to write an XLOG_FPI WAL record to protect against torn pages.
  * 2. The caller might have only share-lock instead of exclusive-lock on the
  *	  buffer's content lock.
  * 3. This function does not guarantee that the buffer is always marked dirty
