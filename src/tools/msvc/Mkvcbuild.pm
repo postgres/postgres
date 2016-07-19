bdbcfba35c70503ca7db7718d4c93bd06779bde8
Tom Lane <tgl@sss.pgh.pa.us>
2016-07-19 17:53:31 -0400

    Fix MSVC build for changes in zic.
    Ooops, I missed back-patching commit f5f15ea6a along with the other stuff.

--- src/tools/msvc/Mkvcbuild.pm
+++ src/tools/msvc/Mkvcbuild.pm
@@ -440,8 +440,7 @@ sub mkvcbuild
 	$pgrestore->AddLibrary('ws2_32.lib');
 
 	my $zic = $solution->AddProject('zic', 'exe', 'utils');
-	$zic->AddFiles('src\timezone', 'zic.c', 'ialloc.c', 'scheck.c',
-		'localtime.c');
+	$zic->AddFiles('src\timezone', 'zic.c');
 	$zic->AddReference($libpgcommon, $libpgport);
 
 	if ($solution->{options}->{xml})
