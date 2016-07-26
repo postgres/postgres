0733188ccfcef37d715a9b29d5dc987a369a0c86
Tom Lane <tgl@sss.pgh.pa.us>
2016-07-26 15:25:02 -0400

    Fix constant-folding of ROW(...) IS [NOT] NULL with composite fields.
    The SQL standard appears to specify that IS [NOT] NULL's tests of field
    nullness are non-recursive, ie, we shouldn't consider that a composite
    field with value ROW(NULL,NULL) is null for this purpose.
    ExecEvalNullTest got this right, but eval_const_expressions did not,
    leading to weird inconsistencies depending on whether the expression
    was such that the planner could apply constant folding.
    
    Also, adjust the docs to mention that IS [NOT] DISTINCT FROM NULL can be
    used as a substitute test if a simple null check is wanted for a rowtype
    argument.  That motivated reordering things so that IS [NOT] DISTINCT FROM
    is described before IS [NOT] NULL.  In HEAD, I went a bit further and added
    a table showing all the comparison-related predicates.
    
    Per bug #14235.  Back-patch to all supported branches, since it's certainly
    undesirable that constant-folding should change the semantics.
    
    Report and patch by Andrew Gierth; assorted wordsmithing and revised
    regression test cases by me.
    
    Report: <20160708024746.1410.57282@wrigleys.postgresql.org>

--- src/backend/optimizer/util/clauses.c
+++ src/backend/optimizer/util/clauses.c
@@ -3269,7 +3269,7 @@ eval_const_expressions_mutator(Node *node,
 
 				arg = eval_const_expressions_mutator((Node *) ntest->arg,
 													 context);
-				if (arg && IsA(arg, RowExpr))
+				if (ntest->argisrow && arg && IsA(arg, RowExpr))
 				{
 					/*
 					 * We break ROW(...) IS [NOT] NULL into separate tests on
@@ -3280,8 +3280,6 @@ eval_const_expressions_mutator(Node *node,
 					RowExpr    *rarg = (RowExpr *) arg;
 					List	   *newargs = NIL;
 					ListCell   *l;
-
-					Assert(ntest->argisrow);
 
 					foreach(l, rarg->args)
 					{
@@ -3301,10 +3299,17 @@ eval_const_expressions_mutator(Node *node,
 								return makeBoolConst(false, false);
 							continue;
 						}
+
+						/*
+						 * Else, make a scalar (argisrow == false) NullTest
+						 * for this field.  Scalar semantics are required
+						 * because IS [NOT] NULL doesn't recurse; see comments
+						 * in ExecEvalNullTest().
+						 */
 						newntest = makeNode(NullTest);
 						newntest->arg = (Expr *) relem;
 						newntest->nulltesttype = ntest->nulltesttype;
-						newntest->argisrow = type_is_rowtype(exprType(relem));
+						newntest->argisrow = false;
 						newargs = lappend(newargs, newntest);
 					}
 					/* If all the inputs were constants, result is TRUE */
