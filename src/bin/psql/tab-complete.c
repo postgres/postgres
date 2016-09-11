52803098ab26051c0c9802f3c19edffc90de8843
Kevin Grittner <kgrittn@postgresql.org>
2016-09-11 15:37:27 -0500

    psql tab completion for CREATE DATABASE ... TEMPLATE ...
    Sehrope Sarkuni, reviewed by Merlin Moncure & Vitaly Burovoy
    with some editing by me

--- src/bin/psql/tab-complete.c
+++ src/bin/psql/tab-complete.c
@@ -599,8 +599,11 @@ static const SchemaQuery Query_for_list_of_matviews = {
 "        OR '\"' || nspname || '\"' ='%s') "
 
 #define Query_for_list_of_template_databases \
-"SELECT pg_catalog.quote_ident(datname) FROM pg_catalog.pg_database "\
-" WHERE substring(pg_catalog.quote_ident(datname),1,%d)='%s' AND datistemplate"
+"SELECT pg_catalog.quote_ident(d.datname) "\
+"  FROM pg_catalog.pg_database d "\
+"  JOIN pg_catalog.pg_roles r ON r.rolname = CURRENT_USER "\
+" WHERE substring(pg_catalog.quote_ident(d.datname),1,%d)='%s' "\
+"   AND (d.datistemplate OR r.rolsuper OR d.datdba = r.oid)"
 
 #define Query_for_list_of_databases \
 "SELECT pg_catalog.quote_ident(datname) FROM pg_catalog.pg_database "\
