92668cd4d3df5a7a48408fc722e876a3e1c167d9
Robert Haas <rhaas@postgresql.org>
2016-09-20 12:24:44 -0400

    Use PostmasterRandom(), not random(), for DSM control segment ID.
    Otherwise, every startup gets the same "random" value, which is
    definitely not what was intended.

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -403,7 +403,6 @@ static void processCancelRequest(Port *port, void *pkt);
 static int	initMasks(fd_set *rmask);
 static void report_fork_failure_to_client(Port *port, int errnum);
 static CAC_state canAcceptConnections(void);
-static long PostmasterRandom(void);
 static void RandomSalt(char *md5Salt);
 static void signal_child(pid_t pid, int signal);
 static bool SignalSomeChildren(int signal, int targets);
@@ -5103,7 +5102,7 @@ RandomSalt(char *md5Salt)
 /*
  * PostmasterRandom
  */
-static long
+long
 PostmasterRandom(void)
 {
 	/*
