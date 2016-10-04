976a1ce91008944121d0b9d2b84c9644c36f7fbb
Robert Haas <rhaas@postgresql.org>
2016-10-04 11:18:43 -0400

    Adjust worker_spi for 6f3bd98ebfc008cbd676da777bb0b2376c4c4bfa.

--- src/test/modules/worker_spi/worker_spi.c
+++ src/test/modules/worker_spi/worker_spi.c
@@ -227,7 +227,8 @@ worker_spi_main(Datum main_arg)
 		 */
 		rc = WaitLatch(MyLatch,
 					   WL_LATCH_SET | WL_TIMEOUT | WL_POSTMASTER_DEATH,
-					   worker_spi_naptime * 1000L);
+					   worker_spi_naptime * 1000L,
+					   WAIT_EXTENSION);
 		ResetLatch(MyLatch);
 
 		/* emergency bailout if postmaster has died */
