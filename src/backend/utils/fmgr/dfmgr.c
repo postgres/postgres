52f0142eb4c755d1f4c390464769d35f21e998c3
Peter Eisentraut <peter_e@gmx.net>
2016-10-09 12:00:00 -0400

    Add a noreturn attribute to help static analyzers

--- src/backend/utils/fmgr/dfmgr.c
+++ src/backend/utils/fmgr/dfmgr.c
@@ -65,7 +65,7 @@ char	   *Dynamic_library_path;
 
 static void *internal_load_library(const char *libname);
 static void incompatible_module_error(const char *libname,
-						  const Pg_magic_struct *module_magic_data);
+									  const Pg_magic_struct *module_magic_data) pg_attribute_noreturn();
 static void internal_unload_library(const char *libname);
 static bool file_exists(const char *name);
 static char *expand_dynamic_library_name(const char *name);
