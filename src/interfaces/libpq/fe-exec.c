886f6c5ccdb500eeeec7e0abdf1500e20a304c45
Tom Lane <tgl@sss.pgh.pa.us>
2016-10-10 10:35:58 -0400

    In PQsendQueryStart(), avoid leaking any left-over async result.
    Ordinarily there would not be an async result sitting around at this
    point, but it appears that in corner cases there can be.  Considering
    all the work we're about to launch, it's hardly going to cost anything
    noticeable to check.
    
    It's been like this forever, so back-patch to all supported branches.
    
    Report: <CAD-Qf1eLUtBOTPXyFQGW-4eEsop31tVVdZPu4kL9pbQ6tJPO8g@mail.gmail.com>

--- src/interfaces/libpq/fe-exec.c
+++ src/interfaces/libpq/fe-exec.c
@@ -1386,8 +1386,7 @@ PQsendQueryStart(PGconn *conn)
 	}
 
 	/* initialize async result-accumulation state */
-	conn->result = NULL;
-	conn->next_result = NULL;
+	pqClearAsyncResult(conn);
 
 	/* reset single-row processing mode */
 	conn->singleRowMode = false;
