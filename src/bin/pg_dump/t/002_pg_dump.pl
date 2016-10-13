b4fc645787cc7c614c0c97fc9fffacf2bdc6a388
Andres Freund <andres@anarazel.de>
2016-10-12 18:29:57 -0700

    Make pg_dumpall's database ACL query independent of hash table order.
    Previously GRANT order on databases was not well defined, due to the use
    of EXCEPT without an ORDER BY.  Add an ORDER BY, adapt test output.
    
    I don't, at the moment, see reason to backpatch this.

--- src/bin/pg_dump/t/002_pg_dump.pl
+++ src/bin/pg_dump/t/002_pg_dump.pl
@@ -2484,6 +2484,7 @@ qr/^GRANT SELECT ON TABLE test_third_table TO regress_dump_test_role;/m,
 		create_sql   => 'REVOKE CONNECT ON DATABASE dump_test FROM public;',
 		regexp       => qr/^
 			\QREVOKE CONNECT,TEMPORARY ON DATABASE dump_test FROM PUBLIC;\E\n
+			\QGRANT CREATE ON DATABASE dump_test TO regress_dump_test_role;\E\n
 			\QGRANT TEMPORARY ON DATABASE dump_test TO PUBLIC;\E
 			/xm,
 		like   => { pg_dumpall_dbprivs => 1, },
