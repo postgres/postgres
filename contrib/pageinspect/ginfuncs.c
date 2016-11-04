postgres/postgres

commit 84ad68d645222f534e4fe51fbba43a5c9cc382a5

pageinspect: Fix unaligned struct access in GIN functions
The raw page data that is passed into the functions will not be aligned
at 8-byte boundaries.  Casting that to a struct and accessing int64
fields will result in unaligned access.  On most platforms, you get away
with it, but it will result on a crash on pickier platforms such as ia64
and sparc64.

contrib/pageinspect/ginfuncs.c
@@ -28,11 +28,31 @@ PG_FUNCTION_INFO_V1(gin_metapage_info);
 PG_FUNCTION_INFO_V1(gin_page_opaque_info);
 PG_FUNCTION_INFO_V1(gin_leafpage_items);
 
+
+static Page
+get_page_from_raw(bytea *raw_page)
+{
+	int			raw_page_size;
+	Page		page;
+
+	raw_page_size = VARSIZE(raw_page) - VARHDRSZ;
+	if (raw_page_size < BLCKSZ)
+		ereport(ERROR,
+				(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+				 errmsg("input page too small (%d bytes)", raw_page_size)));
+
+	/* make a copy so that the page is properly aligned for struct access */
+	page = palloc(raw_page_size);
+	memcpy(page, VARDATA(raw_page), raw_page_size);
+
+	return page;
+}
+
+
 Datum
 gin_metapage_info(PG_FUNCTION_ARGS)
 {
 	bytea	   *raw_page = PG_GETARG_BYTEA_P(0);
-	int			raw_page_size;
 	TupleDesc	tupdesc;
 	Page		page;
 	GinPageOpaque opaq;
@@ -46,12 +66,7 @@ gin_metapage_info(PG_FUNCTION_ARGS)
 				(errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
 				 (errmsg("must be superuser to use raw page functions"))));
 
-	raw_page_size = VARSIZE(raw_page) - VARHDRSZ;
-	if (raw_page_size < BLCKSZ)
-		ereport(ERROR,
-				(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
-				 errmsg("input page too small (%d bytes)", raw_page_size)));
-	page = VARDATA(raw_page);
+	page = get_page_from_raw(raw_page);
 
 	opaq = (GinPageOpaque) PageGetSpecialPointer(page);
 	if (opaq->flags != GIN_META)
@@ -94,7 +109,6 @@ Datum
 gin_page_opaque_info(PG_FUNCTION_ARGS)
 {
 	bytea	   *raw_page = PG_GETARG_BYTEA_P(0);
-	int			raw_page_size;
 	TupleDesc	tupdesc;
 	Page		page;
 	GinPageOpaque opaq;
@@ -110,12 +124,7 @@ gin_page_opaque_info(PG_FUNCTION_ARGS)
 				(errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
 				 (errmsg("must be superuser to use raw page functions"))));
 
-	raw_page_size = VARSIZE(raw_page) - VARHDRSZ;
-	if (raw_page_size < BLCKSZ)
-		ereport(ERROR,
-				(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
-				 errmsg("input page too small (%d bytes)", raw_page_size)));
-	page = VARDATA(raw_page);
+	page = get_page_from_raw(raw_page);
 
 	opaq = (GinPageOpaque) PageGetSpecialPointer(page);
 
@@ -173,7 +182,6 @@ Datum
 gin_leafpage_items(PG_FUNCTION_ARGS)
 {
 	bytea	   *raw_page = PG_GETARG_BYTEA_P(0);
-	int			raw_page_size;
 	FuncCallContext *fctx;
 	gin_leafpage_items_state *inter_call_data;
 
@@ -182,8 +190,6 @@ gin_leafpage_items(PG_FUNCTION_ARGS)
 				(errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
 				 (errmsg("must be superuser to use raw page functions"))));
 
-	raw_page_size = VARSIZE(raw_page) - VARHDRSZ;
-
 	if (SRF_IS_FIRSTCALL())
 	{
 		TupleDesc	tupdesc;
@@ -191,11 +197,7 @@ gin_leafpage_items(PG_FUNCTION_ARGS)
 		Page		page;
 		GinPageOpaque opaq;
 
-		if (raw_page_size < BLCKSZ)
-			ereport(ERROR,
-					(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
-				  errmsg("input page too small (%d bytes)", raw_page_size)));
-		page = VARDATA(raw_page);
+		page = get_page_from_raw(raw_page);
 
 		if (PageGetSpecialSize(page) != MAXALIGN(sizeof(GinPageOpaqueData)))
 			ereport(ERROR,

commit 367b99bbb13a14bd96bba6f73c231b2056ba9592

Fix gin_leafpage_items().
On closer inspection, commit 84ad68d64 broke gin_leafpage_items(),
because the aligned copy of the page got palloc'd in a short-lived
context whereas it needs to be in the SRF's multi_call_memory_ctx.
This was not exposed by the regression test, because the regression
test doesn't actually exercise the function in a meaningful way.
Fix the code bug, and extend the test in what I hope is a portable
fashion.

contrib/pageinspect/expected/gin.out
@@ -1,6 +1,6 @@
 CREATE TABLE test1 (x int, y int[]);
 INSERT INTO test1 VALUES (1, ARRAY[11, 111]);
-CREATE INDEX test1_y_idx ON test1 USING gin (y);
+CREATE INDEX test1_y_idx ON test1 USING gin (y) WITH (fastupdate = off);
 \x
 SELECT * FROM gin_metapage_info(get_raw_page('test1_y_idx', 0));
 -[ RECORD 1 ]----+-----------
@@ -27,4 +27,11 @@ flags     | {leaf}
 SELECT * FROM gin_leafpage_items(get_raw_page('test1_y_idx', 1));
 ERROR:  input page is not a compressed GIN data leaf page
 DETAIL:  Flags 0002, expected 0083
-DROP TABLE test1;
+INSERT INTO test1 SELECT x, ARRAY[1,10] FROM generate_series(2,10000) x;
+SELECT COUNT(*) > 0
+FROM gin_leafpage_items(get_raw_page('test1_y_idx',
+                        (pg_relation_size('test1_y_idx') /
+                         current_setting('block_size')::bigint)::int - 1));
+-[ RECORD 1 ]
+?column? | t
+

contrib/pageinspect/ginfuncs.c
@@ -197,6 +197,9 @@ gin_leafpage_items(PG_FUNCTION_ARGS)
 		Page		page;
 		GinPageOpaque opaq;
 
+		fctx = SRF_FIRSTCALL_INIT();
+		mctx = MemoryContextSwitchTo(fctx->multi_call_memory_ctx);
+
 		page = get_page_from_raw(raw_page);
 
 		if (PageGetSpecialSize(page) != MAXALIGN(sizeof(GinPageOpaqueData)))
@@ -215,9 +218,6 @@ gin_leafpage_items(PG_FUNCTION_ARGS)
 					 errdetail("Flags %04X, expected %04X",
 							   opaq->flags,
 							   (GIN_DATA | GIN_LEAF | GIN_COMPRESSED))));
-
-		fctx = SRF_FIRSTCALL_INIT();
-		mctx = MemoryContextSwitchTo(fctx->multi_call_memory_ctx);
 
 		inter_call_data = palloc(sizeof(gin_leafpage_items_state));
 

contrib/pageinspect/sql/gin.sql
@@ -1,6 +1,6 @@
 CREATE TABLE test1 (x int, y int[]);
 INSERT INTO test1 VALUES (1, ARRAY[11, 111]);
-CREATE INDEX test1_y_idx ON test1 USING gin (y);
+CREATE INDEX test1_y_idx ON test1 USING gin (y) WITH (fastupdate = off);
 
 \x
 
@@ -11,4 +11,9 @@ SELECT * FROM gin_page_opaque_info(get_raw_page('test1_y_idx', 1));
 
 SELECT * FROM gin_leafpage_items(get_raw_page('test1_y_idx', 1));
 
-DROP TABLE test1;
+INSERT INTO test1 SELECT x, ARRAY[1,10] FROM generate_series(2,10000) x;
+
+SELECT COUNT(*) > 0
+FROM gin_leafpage_items(get_raw_page('test1_y_idx',
+                        (pg_relation_size('test1_y_idx') /
+                         current_setting('block_size')::bigint)::int - 1));