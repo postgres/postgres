e2f5cd9cf5588cc37f665fb2ca061898b5c73f2a
Tom Lane <tgl@sss.pgh.pa.us>
2016-11-07 12:08:19 -0500

    Band-aid fix for incorrect use of view options as StdRdOptions.
    We really ought to make StdRdOptions and the other decoded forms of
    reloptions self-identifying, but for the moment, assume that only plain
    relations could possibly be user_catalog_tables.  Fixes problem with bogus
    "ON CONFLICT is not supported on table ... used as a catalog table" error
    when target is a view with cascade option.
    
    Discussion: <26681.1477940227@sss.pgh.pa.us>

--- src/test/regress/sql/insert_conflict.sql
+++ src/test/regress/sql/insert_conflict.sql
@@ -283,6 +283,24 @@ on conflict (b) where coalesce(a, 1) > 1 do nothing;
 
 drop table insertconflict;
 
+--
+-- test insertion through view
+--
+
+create table insertconflict (f1 int primary key, f2 text);
+create view insertconflictv as
+  select * from insertconflict with cascaded check option;
+
+insert into insertconflictv values (1,'foo')
+  on conflict (f1) do update set f2 = excluded.f2;
+select * from insertconflict;
+insert into insertconflictv values (1,'bar')
+  on conflict (f1) do update set f2 = excluded.f2;
+select * from insertconflict;
+
+drop view insertconflictv;
+drop table insertconflict;
+
 
 -- ******************************************************************
 -- *                                                                *
