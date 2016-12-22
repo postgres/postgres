f0f2e56ac28c05f930ca76c432d61909c9a12cdb
Stephen Frost <sfrost@snowman.net>
2016-12-22 17:08:47 -0500

    Use TSConfigRelationId in AlterTSConfiguration()
    When we are altering a text search configuration, we are getting the
    tuple from pg_ts_config and using its OID, so use TSConfigRelationId
    when invoking any post-alter hooks and setting the object address.
    
    Further, in the functions called from AlterTSConfiguration(), we're
    saving information about the command via
    EventTriggerCollectAlterTSConfig(), so we should be setting
    commandCollected to true.  Also add a regression test to
    test_ddl_deparse for ALTER TEXT SEARCH CONFIGURATION.
    
    Author: Artur Zakirov, a few additional comments by me
    Discussion: https://www.postgresql.org/message-id/57a71eba-f2c7-e7fd-6fc0-2126ec0b39bd%40postgrespro.ru
    
    Back-patch the fix for the InvokeObjectPostAlterHook() call to 9.3 where
    it was introduced, and the fix for the ObjectAddressSet() call and
    setting commandCollected to true to 9.5 where those changes to
    ProcessUtilitySlow() were introduced.

--- src/backend/tcop/utility.c
+++ src/backend/tcop/utility.c
@@ -1467,7 +1467,13 @@ ProcessUtilitySlow(Node *parsetree,
 				break;
 
 			case T_AlterTSConfigurationStmt:
-				address = AlterTSConfiguration((AlterTSConfigurationStmt *) parsetree);
+				AlterTSConfiguration((AlterTSConfigurationStmt *) parsetree);
+				/*
+				 * Commands are stashed in MakeConfigurationMapping and
+				 * DropConfigurationMapping, which are called from
+				 * AlterTSConfiguration
+				 */
+				commandCollected = true;
 				break;
 
 			case T_AlterTableMoveAllStmt:
