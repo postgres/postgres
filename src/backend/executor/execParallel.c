ff33d1456ea098e160cbbc74b332656c06abc2ab
Tom Lane <tgl@sss.pgh.pa.us>
2016-12-23 11:53:35 -0500

    Spellcheck: s/descendent/descendant/g
    I got a little annoyed by reading documentation paragraphs containing
    both spellings within a few lines of each other.  My dictionary says
    "descendant" is the preferred spelling, and it's certainly the majority
    usage in our tree, so standardize on that.
    
    For one usage in parallel.sgml, I thought it better to rewrite to avoid
    the term altogether.

--- src/backend/executor/execParallel.c
+++ src/backend/executor/execParallel.c
@@ -518,7 +518,7 @@ ExecInitParallelPlan(PlanState *planstate, EState *estate, int nworkers)
 }
 
 /*
- * Copy instrumentation information about this node and its descendents from
+ * Copy instrumentation information about this node and its descendants from
  * dynamic shared memory.
  */
 static bool
@@ -666,7 +666,7 @@ ExecParallelGetQueryDesc(shm_toc *toc, DestReceiver *receiver,
 }
 
 /*
- * Copy instrumentation information from this node and its descendents into
+ * Copy instrumentation information from this node and its descendants into
  * dynamic shared memory, so that the parallel leader can retrieve it.
  */
 static bool
@@ -706,7 +706,7 @@ ExecParallelReportInstrumentation(PlanState *planstate,
 }
 
 /*
- * Initialize the PlanState and its descendents with the information
+ * Initialize the PlanState and its descendants with the information
  * retrieved from shared memory.  This has to be done once the PlanState
  * is allocated and initialized by executor; that is, after ExecutorStart().
  */
