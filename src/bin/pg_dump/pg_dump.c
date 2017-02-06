863e70aa7f4dbfab6d94f6c708986610d62ddce3
Peter Eisentraut <peter_e@gmx.net>
2017-02-06 09:47:39 -0500

    Add missing newline to error messages
    Also improve the message style a bit while we're here.

--- src/bin/pg_dump/pg_dump.c
+++ src/bin/pg_dump/pg_dump.c
@@ -9657,7 +9657,7 @@ dumpCast(Archive *fout, CastInfo *cast)
 	{
 		funcInfo = findFuncByOid(cast->castfunc);
 		if (funcInfo == NULL)
-			exit_horribly(NULL, "unable to find function definition for OID %u",
+			exit_horribly(NULL, "could not find function definition for function with OID %u\n",
 						  cast->castfunc);
 	}
 
