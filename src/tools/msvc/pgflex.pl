65d508fd4decee4d5a54c1e7b93acd25d5e80556
Tom Lane <tgl@sss.pgh.pa.us>
2017-02-19 13:04:30 -0500

    Suppress "unused variable" warnings with older versions of flex.
    Versions of flex before 2.5.36 might generate code that results in an
    "unused variable" warning, when using %option reentrant.  Historically
    we've worked around that by specifying -Wno-error, but that's an
    unsatisfying solution.  The official "fix" for this was just to insert a
    dummy reference to the variable, so write a small perl script that edits
    the generated C code similarly.
    
    The MSVC side of this is untested, but the buildfarm should soon reveal
    if I broke that.
    
    Discussion: https://postgr.es/m/25456.1487437842@sss.pgh.pa.us

--- src/tools/msvc/pgflex.pl
+++ src/tools/msvc/pgflex.pl
@@ -51,23 +51,29 @@ my $flexflags = ($make =~ /^$basetarg:\s*FLEXFLAGS\s*=\s*(\S.*)/m ? $1 : '');
 system("flex $flexflags -o$output $input");
 if ($? == 0)
 {
-
-	# For non-reentrant scanners we need to fix up the yywrap macro definition
-	# to keep the MS compiler happy.
-	# For reentrant scanners (like the core scanner) we do not
-	# need to (and must not) change the yywrap definition.
+	# Check for "%option reentrant" in .l file.
 	my $lfile;
 	open($lfile, $input) || die "opening $input for reading: $!";
 	my $lcode = <$lfile>;
 	close($lfile);
-	if ($lcode !~ /\%option\sreentrant/)
+	if ($lcode =~ /\%option\sreentrant/)
 	{
+		# Reentrant scanners usually need a fix to prevent
+		# "unused variable" warnings with older flex versions.
+		system("perl src\\tools\\fix-flex-warning.pl $output");
+	}
+	else
+	{
+		# For non-reentrant scanners we need to fix up the yywrap
+		# macro definition to keep the MS compiler happy.
+		# For reentrant scanners (like the core scanner) we do not
+		# need to (and must not) change the yywrap definition.
 		my $cfile;
 		open($cfile, $output) || die "opening $output for reading: $!";
 		my $ccode = <$cfile>;
 		close($cfile);
 		$ccode =~ s/yywrap\(n\)/yywrap()/;
-		open($cfile, ">$output") || die "opening $output for reading: $!";
+		open($cfile, ">$output") || die "opening $output for writing: $!";
 		print $cfile $ccode;
 		close($cfile);
 	}
