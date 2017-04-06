255efa241f460ee4f4c4c98c8cdd7457807f3af9
Kevin Grittner <kgrittn@postgresql.org>
2017-04-06 17:32:53 -0500

    Fix the RTE_NAMEDTUPLESTORE case in get_rte_attribute_is_dropped().
    Problems pointed out by Andres Freund and Thomas Munro.

--- src/backend/parser/parse_relation.c
+++ src/backend/parser/parse_relation.c
@@ -2891,12 +2891,12 @@ get_rte_attribute_is_dropped(RangeTblEntry *rte, AttrNumber attnum)
 				Assert(rte->enrname);
 
 				/*
-				 * We checked when we loaded ctecoltypes for the tuplestore
-				 * that InvalidOid was only used for dropped columns, so it is
-				 * safe to count on that here.
+				 * We checked when we loaded coltypes for the tuplestore that
+				 * InvalidOid was only used for dropped columns, so it is safe
+				 * to count on that here.
 				 */
 				result =
-					(list_nth(rte->coltypes, attnum - 1) != InvalidOid);
+					((list_nth_oid(rte->coltypes, attnum - 1) == InvalidOid));
 			}
 			break;
 		case RTE_JOIN:
