6c9bd27aece5efd0ccef668828a44f59bd2c7a44
Peter Eisentraut <peter_e@gmx.net>
2017-04-26 21:12:35 -0400

    Fix typo in comment
    Author: Masahiko Sawada <sawada.mshk@gmail.com>

--- src/backend/replication/logical/worker.c
+++ src/backend/replication/logical/worker.c
@@ -351,7 +351,7 @@ slot_store_cstrings(TupleTableSlot *slot, LogicalRepRelMapEntry *rel,
 }
 
 /*
- * Modify slot with user data provided as C strigs.
+ * Modify slot with user data provided as C strings.
  * This is somewhat similar to heap_modify_tuple but also calls the type
  * input function on the user data as the input is the text representation
  * of the types.
