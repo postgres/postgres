d56b8b41b32b3f5c576d6fc2b2c9af3ae4b55511
Tom Lane <tgl@sss.pgh.pa.us>
2017-05-02 18:05:54 -0400

    Ensure commands in extension scripts see the results of preceding DDL.
    Due to a missing CommandCounterIncrement() call, parsing of a non-utility
    command in an extension script would not see the effects of the immediately
    preceding DDL command, unless that command's execution ends with
    CommandCounterIncrement() internally ... which some do but many don't.
    Report by Philippe Beaudoin, diagnosis by Julien Rouhaud.
    
    Rather remarkably, this bug has evaded detection since extensions were
    invented, so back-patch to all supported branches.
    
    Discussion: https://postgr.es/m/2cf7941e-4e41-7714-3de8-37b1a8f74dff@free.fr

--- src/test/modules/test_extensions/test_ext3--1.0.sql
+++ src/test/modules/test_extensions/test_ext3--1.0.sql
@@ -1,3 +1,9 @@
 /* src/test/modules/test_extensions/test_ext3--1.0.sql */
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION test_ext3" to load this file. \quit
+
+CREATE TABLE test_ext3_table (col_old INT);
+
+ALTER TABLE test_ext3_table RENAME col_old TO col_new;
+
+UPDATE test_ext3_table SET col_new = 0;
