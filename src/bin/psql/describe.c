b83f4e4a25394b964a7fadc6429c7de82d62b58a
Peter Eisentraut <peter_e@gmx.net>
2017-05-10 10:14:49 -0400

    psql: Add missing translation markers

--- src/bin/psql/describe.c
+++ src/bin/psql/describe.c
@@ -2752,7 +2752,7 @@ describeOneTableDetails(const char *schemaname,
 			}
 
 			/* Print server name */
-			printfPQExpBuffer(&buf, "Server: %s",
+			printfPQExpBuffer(&buf, _("Server: %s"),
 							  PQgetvalue(result, 0, 0));
 			printTableAddFooter(&cont, buf.data);
 
@@ -2760,7 +2760,7 @@ describeOneTableDetails(const char *schemaname,
 			ftoptions = PQgetvalue(result, 0, 1);
 			if (ftoptions && ftoptions[0] != '\0')
 			{
-				printfPQExpBuffer(&buf, "FDW Options: (%s)", ftoptions);
+				printfPQExpBuffer(&buf, _("FDW Options: (%s)"), ftoptions);
 				printTableAddFooter(&cont, buf.data);
 			}
 			PQclear(result);
