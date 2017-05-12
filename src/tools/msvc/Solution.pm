2c7d2114bb883b07816c2171830edd654696c856
Andrew Dunstan <andrew@dunslane.net>
2017-05-12 10:17:54 -0400

    Add libxml2 include path for MSVC builds
    On Unix this path is detected via the use of xml2-config, but that's not
    available on Windows. This means that users building with libxml2 will
    no longer need to move things around from the standard libxml2
    installation for MSVC builds.
    
    Backpatch to all live branches.

--- src/tools/msvc/Solution.pm
+++ src/tools/msvc/Solution.pm
@@ -530,6 +530,7 @@ sub AddProject
 	if ($self->{options}->{xml})
 	{
 		$proj->AddIncludeDir($self->{options}->{xml} . '\include');
+		$proj->AddIncludeDir($self->{options}->{xml} . '\include\libxml2');
 		$proj->AddLibrary($self->{options}->{xml} . '\lib\libxml2.lib');
 	}
 	if ($self->{options}->{xslt})
