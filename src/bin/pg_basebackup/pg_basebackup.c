05b5feb60e7cf36db08eb0d34dbd21854cb9e8ce
Tom Lane <tgl@sss.pgh.pa.us>
2017-05-17 13:04:03 -0400

    Revert changes to pg_basebackup and pg_waldump usage() code.
    Partially revert commit c079673dcb7f210617c9fc1470e6bf166d8a2971.
    There were complaints that splitting switch descriptions would
    complicate translation efforts.  There are probably ways to resolve
    the formatting problem without doing that, but undo it while we're
    discussing.

--- src/bin/pg_basebackup/pg_basebackup.c
+++ src/bin/pg_basebackup/pg_basebackup.c
@@ -331,22 +331,22 @@ usage(void)
 	printf(_("\nOptions controlling the output:\n"));
 	printf(_("  -D, --pgdata=DIRECTORY receive base backup into directory\n"));
 	printf(_("  -F, --format=p|t       output format (plain (default), tar)\n"));
-	printf(_("  -r, --max-rate=RATE    maximum transfer rate to transfer data directory\n"));
-	printf(_("                         (in kB/s, or use suffix \"k\" or \"M\")\n"));
-	printf(_("  -R, --write-recovery-conf\n"));
-	printf(_("                         write recovery.conf for replication\n"));
+	printf(_("  -r, --max-rate=RATE    maximum transfer rate to transfer data directory\n"
+	  "                         (in kB/s, or use suffix \"k\" or \"M\")\n"));
+	printf(_("  -R, --write-recovery-conf\n"
+			 "                         write recovery.conf for replication\n"));
 	printf(_("  -S, --slot=SLOTNAME    replication slot to use\n"));
 	printf(_("      --no-slot          prevent creation of temporary replication slot\n"));
-	printf(_("  -T, --tablespace-mapping=OLDDIR=NEWDIR\n"));
-	printf(_("                         relocate tablespace in OLDDIR to NEWDIR\n"));
-	printf(_("  -X, --wal-method=none|fetch|stream\n"));
-	printf(_("                         include required WAL files with specified method\n"));
+	printf(_("  -T, --tablespace-mapping=OLDDIR=NEWDIR\n"
+	  "                         relocate tablespace in OLDDIR to NEWDIR\n"));
+	printf(_("  -X, --wal-method=none|fetch|stream\n"
+			 "                         include required WAL files with specified method\n"));
 	printf(_("      --waldir=WALDIR    location for the write-ahead log directory\n"));
 	printf(_("  -z, --gzip             compress tar output\n"));
 	printf(_("  -Z, --compress=0-9     compress tar output with given compression level\n"));
 	printf(_("\nGeneral options:\n"));
-	printf(_("  -c, --checkpoint=fast|spread\n"));
-	printf(_("                         set fast or spread checkpointing\n"));
+	printf(_("  -c, --checkpoint=fast|spread\n"
+			 "                         set fast or spread checkpointing\n"));
 	printf(_("  -l, --label=LABEL      set backup label\n"));
 	printf(_("  -n, --no-clean         do not clean up after errors\n"));
 	printf(_("  -N, --no-sync          do not wait for changes to be written safely to disk\n"));
@@ -358,8 +358,8 @@ usage(void)
 	printf(_("  -d, --dbname=CONNSTR   connection string\n"));
 	printf(_("  -h, --host=HOSTNAME    database server host or socket directory\n"));
 	printf(_("  -p, --port=PORT        database server port number\n"));
-	printf(_("  -s, --status-interval=INTERVAL\n"));
-	printf(_("                         time between status packets sent to server (in seconds)\n"));
+	printf(_("  -s, --status-interval=INTERVAL\n"
+			 "                         time between status packets sent to server (in seconds)\n"));
 	printf(_("  -U, --username=NAME    connect as specified database user\n"));
 	printf(_("  -w, --no-password      never prompt for password\n"));
 	printf(_("  -W, --password         force password prompt (should happen automatically)\n"));
