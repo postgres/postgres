b3c536bce953e7e890b0148d3cb6e90a1fd15846
Tom Lane <tgl@sss.pgh.pa.us>
2017-05-21 13:05:17 -0400

    Fix precision and rounding issues in money multiplication and division.
    The cash_div_intX functions applied rint() to the result of the division.
    That's not merely useless (because the result is already an integer) but
    it causes precision loss for values larger than 2^52 or so, because of
    the forced conversion to float8.
    
    On the other hand, the cash_mul_fltX functions neglected to apply rint() to
    their multiplication results, thus possibly causing off-by-one outputs.
    
    Per C standard, arithmetic between any integral value and a float value is
    performed in float format.  Thus, cash_mul_flt4 and cash_div_flt4 produced
    answers good to only about six digits, even when the float value is exact.
    We can improve matters noticeably by widening the float inputs to double.
    (It's tempting to consider using "long double" arithmetic if available,
    but that's probably too much of a stretch for a back-patched fix.)
    
    Also, document that cash_div_intX operators truncate rather than round.
    
    Per bug #14663 from Richard Pistole.  Back-patch to all supported branches.
    
    Discussion: https://postgr.es/m/22403.1495223615@sss.pgh.pa.us

--- src/backend/utils/adt/cash.c
+++ src/backend/utils/adt/cash.c
@@ -623,7 +623,7 @@ cash_mul_flt8(PG_FUNCTION_ARGS)
 	float8		f = PG_GETARG_FLOAT8(1);
 	Cash		result;
 
-	result = c * f;
+	result = rint(c * f);
 	PG_RETURN_CASH(result);
 }
 
@@ -638,7 +638,7 @@ flt8_mul_cash(PG_FUNCTION_ARGS)
 	Cash		c = PG_GETARG_CASH(1);
 	Cash		result;
 
-	result = f * c;
+	result = rint(f * c);
 	PG_RETURN_CASH(result);
 }
 
@@ -673,7 +673,7 @@ cash_mul_flt4(PG_FUNCTION_ARGS)
 	float4		f = PG_GETARG_FLOAT4(1);
 	Cash		result;
 
-	result = c * f;
+	result = rint(c * (float8) f);
 	PG_RETURN_CASH(result);
 }
 
@@ -688,7 +688,7 @@ flt4_mul_cash(PG_FUNCTION_ARGS)
 	Cash		c = PG_GETARG_CASH(1);
 	Cash		result;
 
-	result = f * c;
+	result = rint((float8) f * c);
 	PG_RETURN_CASH(result);
 }
 
@@ -709,7 +709,7 @@ cash_div_flt4(PG_FUNCTION_ARGS)
 				(errcode(ERRCODE_DIVISION_BY_ZERO),
 				 errmsg("division by zero")));
 
-	result = rint(c / f);
+	result = rint(c / (float8) f);
 	PG_RETURN_CASH(result);
 }
 
@@ -758,7 +758,7 @@ cash_div_int8(PG_FUNCTION_ARGS)
 				(errcode(ERRCODE_DIVISION_BY_ZERO),
 				 errmsg("division by zero")));
 
-	result = rint(c / i);
+	result = c / i;
 
 	PG_RETURN_CASH(result);
 }
@@ -810,7 +810,7 @@ cash_div_int4(PG_FUNCTION_ARGS)
 				(errcode(ERRCODE_DIVISION_BY_ZERO),
 				 errmsg("division by zero")));
 
-	result = rint(c / i);
+	result = c / i;
 
 	PG_RETURN_CASH(result);
 }
@@ -860,7 +860,7 @@ cash_div_int2(PG_FUNCTION_ARGS)
 				(errcode(ERRCODE_DIVISION_BY_ZERO),
 				 errmsg("division by zero")));
 
-	result = rint(c / s);
+	result = c / s;
 	PG_RETURN_CASH(result);
 }
 
