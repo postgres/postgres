eac0a6c7d35dee6d4329b3c7d8bf82fac4c1eff1
Tom Lane <tgl@sss.pgh.pa.us>
2017-05-28 14:52:18 -0400

    Avoid locale-dependent output in select_views regression test.
    Use 'COLLATE "C"' to force locale-independent sorting of the iexit
    view results in select_views.sql.  We aren't particularly interested
    in the exact sorting behavior here, and this doesn't change the shape
    of the generated plan, so it seems like a wash as far as the goals
    of this test go.
    
    This is in response to bug #14637 from Tomasz Kontusz.  It doesn't
    fully resolve his problem, because he also saw some diffs in the
    create_index test.  But other people have had issues with select_views
    too, and this fix lets us drop the select_views_1.out variant expected
    file altogether, which is a nice win from a maintenance standpoint.
    
    Emre Hasegeli
    
    Discussion: https://postgr.es/m/20170501000609.24360.24248@wrigleys.postgresql.org

--- src/test/regress/sql/select_views.sql
+++ src/test/regress/sql/select_views.sql
@@ -5,7 +5,7 @@
 
 SELECT * FROM street;
 
-SELECT name, #thepath FROM iexit ORDER BY 1, 2;
+SELECT name, #thepath FROM iexit ORDER BY name COLLATE "C", 2;
 
 SELECT * FROM toyemp WHERE name = 'sharon';
 
