4e88fe8f8f148a45feacb50c2eaed9ca9ddea8bb
Peter Eisentraut <peter_e@gmx.net>
2017-06-14 14:43:54 -0400

    Add missing serial comma

--- src/bin/pg_basebackup/pg_basebackup.c
+++ src/bin/pg_basebackup/pg_basebackup.c
@@ -2183,7 +2183,7 @@ main(int argc, char **argv)
 				else
 				{
 					fprintf(stderr,
-							_("%s: invalid wal-method option \"%s\", must be \"fetch\", \"stream\" or \"none\"\n"),
+							_("%s: invalid wal-method option \"%s\", must be \"fetch\", \"stream\", or \"none\"\n"),
 							progname, optarg);
 					exit(1);
 				}
