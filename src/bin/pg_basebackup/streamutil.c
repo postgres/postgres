4808d69955f5115686633cd3cc78b9957122e1ad
Magnus Hagander <magnus@hagander.net>
2017-07-07 15:08:55 +0300

    Fix out of date comment
    Author: Masahiko Sawada <sawada.mshk@gmail.com>

--- src/bin/pg_basebackup/streamutil.c
+++ src/bin/pg_basebackup/streamutil.c
@@ -1,6 +1,7 @@
 /*-------------------------------------------------------------------------
  *
- * streamutil.c - utility functions for pg_basebackup and pg_receivelog
+ * streamutil.c - utility functions for pg_basebackup, pg_receivewal and
+ * 					pg_recvlogical
  *
  * Author: Magnus Hagander <magnus@hagander.net>
  *
