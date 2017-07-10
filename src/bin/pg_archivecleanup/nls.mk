6c774caf0ea6977f00af4225192915f0a602ea3d
Alvaro Herrera <alvherre@alvh.no-ip.org>
2017-07-10 11:51:32 -0400

    Translation updates
    Source-Git-URL: git://git.postgresql.org/git/pgtranslation/messages.git
    Source-Git-Hash: c5a8de3653bb1af6b0eb41cc6bf090c5522df52b

--- src/bin/pg_archivecleanup/nls.mk
+++ src/bin/pg_archivecleanup/nls.mk
@@ -1,4 +1,4 @@
 # src/bin/pg_archivecleanup/nls.mk
 CATALOG_NAME     = pg_archivecleanup
-AVAIL_LANGUAGES  =pl ru
+AVAIL_LANGUAGES  =pl ru fr es
 GETTEXT_FILES    = pg_archivecleanup.c
--- src/bin/pg_test_timing/nls.mk
+++ src/bin/pg_test_timing/nls.mk
@@ -1,4 +1,4 @@
 # src/bin/pg_test_timing/nls.mk
 CATALOG_NAME     = pg_test_timing
-AVAIL_LANGUAGES  =de pl ru
+AVAIL_LANGUAGES  =de pl ru fr es
 GETTEXT_FILES    = pg_test_timing.c
