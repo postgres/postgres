ca793c59a51e94cedf8cbea5c29668bf8fa298f3
Alvaro Herrera <alvherre@alvh.no-ip.org>
2017-07-12 14:39:44 -0400

    commit_ts test: Set node name in test
    Otherwise, the script output has a lot of pointless warnings.
    
    This was forgotten in 9def031bd2821f35b5f506260d922482648a8bb0

--- src/test/modules/commit_ts/t/001_base.pl
+++ src/test/modules/commit_ts/t/001_base.pl
@@ -7,7 +7,7 @@ use TestLib;
 use Test::More tests => 2;
 use PostgresNode;
 
-my $node = get_new_node();
+my $node = get_new_node('foxtrot');
 $node->init;
 $node->append_conf('postgresql.conf', 'track_commit_timestamp = on');
 $node->start;
