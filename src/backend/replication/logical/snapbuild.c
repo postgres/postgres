d68742562ca48decbad57adf0014aaed788ce372
Andres Freund <andres@anarazel.de>
2017-08-05 20:52:53 -0700

    Fix thinko introduced in 2bef06d516460 et al.
    The callers for GetOldestSafeDecodingTransactionId() all inverted the
    argument for the argument introduced in 2bef06d516460. Luckily this
    appears to be inconsequential for the moment, as we wait for
    concurrent in-progress transaction when assembling a
    snapshot. Additionally this could only make a difference when adding a
    second logical slot, because only a pre-existing slot could cause an
    issue by lowering the returned xid dangerously much.
    
    Reported-By: Antonin Houska
    Discussion: https://postgr.es/m/32704.1496993134@localhost
    Backport: 9.4-, where 2bef06d516460 was backpatched to.

--- src/backend/replication/logical/snapbuild.c
+++ src/backend/replication/logical/snapbuild.c
@@ -595,7 +595,7 @@ SnapBuildExportSnapshot(SnapBuild *builder)
 		TransactionId safeXid;
 
 		LWLockAcquire(ProcArrayLock, LW_SHARED);
-		safeXid = GetOldestSafeDecodingTransactionId(true);
+		safeXid = GetOldestSafeDecodingTransactionId(false);
 		LWLockRelease(ProcArrayLock);
 
 		Assert(TransactionIdPrecedesOrEquals(safeXid, snap->xmin));
