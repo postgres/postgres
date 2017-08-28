89f6d587f06b39884f755e871aeeb368e2a75712
Peter Eisentraut <peter_e@gmx.net>
2017-08-28 10:34:14 -0400

    Translation updates
    Source-Git-URL: git://git.postgresql.org/git/pgtranslation/messages.git
    Source-Git-Hash: 31ad7831c3018858b662ed1d26a6c3bfe92b4e1f

--- src/bin/pg_archivecleanup/nls.mk
+++ src/bin/pg_archivecleanup/nls.mk
@@ -1,4 +1,4 @@
 # src/bin/pg_archivecleanup/nls.mk
 CATALOG_NAME     = pg_archivecleanup
-AVAIL_LANGUAGES  =de es fr pl ru sv
+AVAIL_LANGUAGES  = de es fr ko pl ru sv
 GETTEXT_FILES    = pg_archivecleanup.c
--- src/bin/pg_waldump/nls.mk
+++ src/bin/pg_waldump/nls.mk
@@ -1,6 +1,6 @@
 # src/bin/pg_waldump/nls.mk
 CATALOG_NAME     = pg_waldump
-AVAIL_LANGUAGES  = es fr sv
+AVAIL_LANGUAGES  = de es fr sv
 GETTEXT_FILES    = pg_waldump.c
 GETTEXT_TRIGGERS = fatal_error
 GETTEXT_FLAGS    = fatal_error:1:c-format
