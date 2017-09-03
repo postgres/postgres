d5c65d2f11ac58c517ab7fda01be62e4763504c9
Tom Lane <tgl@sss.pgh.pa.us>
2017-09-03 11:01:08 -0400

    Fix macro-redefinition warning on MSVC.
    In commit 9d6b160d7, I tweaked pg_config.h.win32 to use
    "#define HAVE_LONG_LONG_INT_64 1" rather than defining it as empty,
    for consistency with what happens in an autoconf'd build.
    But Solution.pm injects another definition of that macro into
    ecpg_config.h, leading to justifiable (though harmless) compiler whining.
    Make that one consistent too.  Back-patch, like the previous patch.
    
    Discussion: https://postgr.es/m/CAEepm=1dWsXROuSbRg8PbKLh0S=8Ou-V8sr05DxmJOF5chBxqQ@mail.gmail.com

--- src/tools/msvc/Solution.pm
+++ src/tools/msvc/Solution.pm
@@ -425,7 +425,7 @@ s{PG_VERSION_STR "[^"]+"}{PG_VERSION_STR "PostgreSQL $self->{strver}$extraver, c
 		  || confess "Could not open ecpg_config.h";
 		print $o <<EOF;
 #if (_MSC_VER > 1200)
-#define HAVE_LONG_LONG_INT_64
+#define HAVE_LONG_LONG_INT_64 1
 #define ENABLE_THREAD_SAFETY 1
 EOF
 		print $o "#endif\n";
