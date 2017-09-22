58ffe141eb37c3f027acd25c1fc6b36513bf9380
Peter Eisentraut <peter_e@gmx.net>
2017-09-22 16:34:46 -0400

    Revert "Add basic TAP test setup for pg_upgrade"
    This reverts commit f41e56c76e39f02bef7ba002c9de03d62b76de4d.
    
    The build farm client would run the pg_upgrade tests twice, once as part
    of the existing pg_upgrade check run and once as part of picking up all
    TAP tests by looking for "t" directories.  Since the pg_upgrade tests
    are pretty slow, we will need a better solution or possibly a build farm
    client change before we can proceed with this.

--- src/bin/pg_upgrade/t/001_basic.pl
+++
@@ -1,9 +0,0 @@
-use strict;
-use warnings;
-
-use TestLib;
-use Test::More tests => 8;
-
-program_help_ok('pg_upgrade');
-program_version_ok('pg_upgrade');
-program_options_handling_ok('pg_upgrade');
