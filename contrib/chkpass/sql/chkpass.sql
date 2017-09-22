5d3cad564729f64d972c5c803ff34f0eb40bfd0c
Peter Eisentraut <peter_e@gmx.net>
2017-09-22 10:59:46 -0400

    Remove contrib/chkpass
    The recent addition of a test suite for this module revealed a few
    problems.  It uses a crypt() method that is no longer considered secure
    and doesn't work anymore on some platforms.  Using a volatile input
    function violates internal sanity check assumptions and leads to
    failures on the build farm.
    
    So this module is neither a usable security tool nor a good example for
    an extension.  No one wanted to argue for keeping or improving it, so
    remove it.
    
    Discussion: https://www.postgresql.org/message-id/5645b0d7-cc40-6ab5-c553-292a91091ee7%402ndquadrant.com

--- contrib/chkpass/sql/chkpass.sql
+++
@@ -1,7 +0,0 @@
-CREATE EXTENSION chkpass;
-
-CREATE TABLE test (i int, p chkpass);
-INSERT INTO test VALUES (1, 'hello'), (2, 'goodbye');
-
-SELECT i, p = 'hello' AS "hello?" FROM test;
-SELECT i, p <> 'hello' AS "!hello?" FROM test;
