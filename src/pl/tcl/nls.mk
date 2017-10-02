1f19550a874d02c6e9f6192ed1a97995f9598f53
Peter Eisentraut <peter_e@gmx.net>
2017-10-02 12:03:01 -0400

    Translation updates
    Source-Git-URL: git://git.postgresql.org/git/pgtranslation/messages.git
    Source-Git-Hash: 4eb5acee0bc0ba7b40220367dfc44bb4af188c88

--- src/pl/tcl/nls.mk
+++ src/pl/tcl/nls.mk
@@ -1,6 +1,6 @@
 # src/pl/tcl/nls.mk
 CATALOG_NAME     = pltcl
-AVAIL_LANGUAGES  = de es fr it pl ru sv
+AVAIL_LANGUAGES  = de es fr it ko pl ru sv
 GETTEXT_FILES    = pltcl.c
 GETTEXT_TRIGGERS = $(BACKEND_COMMON_GETTEXT_TRIGGERS)
 GETTEXT_FLAGS    = $(BACKEND_COMMON_GETTEXT_FLAGS)
