69bc245d92961d5db83aabefb9a8b5646732b797
Tom Lane <tgl@sss.pgh.pa.us>
2017-10-11 14:28:33 -0400

    Fix low-probability loss of NOTIFY messages due to XID wraparound.
    Up to now async.c has used TransactionIdIsInProgress() to detect whether
    a notify message's source transaction is still running.  However, that
    function has a quick-exit path that reports that XIDs before RecentXmin
    are no longer running.  If a listening backend is doing nothing but
    listening, and not running any queries, there is nothing that will advance
    its value of RecentXmin.  Once 2 billion transactions elapse, the
    RecentXmin check causes active transactions to be reported as not running.
    If they aren't committed yet according to CLOG, async.c decides they
    aborted and discards their messages.  The timing for that is a bit tight
    but it can happen when multiple backends are sending notifies concurrently.
    The net symptom therefore is that a sufficiently-long-surviving
    listen-only backend starts to miss some fraction of NOTIFY traffic,
    but only under heavy load.
    
    The only function that updates RecentXmin is GetSnapshotData().
    A brute-force fix would therefore be to take a snapshot before
    processing incoming notify messages.  But that would add cycles,
    as well as contention for the ProcArrayLock.  We can be smarter:
    having taken the snapshot, let's use that to check for running
    XIDs, and not call TransactionIdIsInProgress() at all.  In this
    way we reduce the number of ProcArrayLock acquisitions from one
    per message to one per notify interrupt; that's the same under
    light load but should be a benefit under heavy load.  Light testing
    says that this change is a wash performance-wise for normal loads.
    
    I looked around for other callers of TransactionIdIsInProgress()
    that might be at similar risk, and didn't find any; all of them
    are inside transactions that presumably have already taken a
    snapshot.
    
    Problem report and diagnosis by Marko Tiikkaja, patch by me.
    Back-patch to all supported branches, since it's been like this
    since 9.0.
    
    Discussion: https://postgr.es/m/20170926182935.14128.65278@wrigleys.postgresql.org

--- src/backend/utils/time/tqual.c
+++ src/backend/utils/time/tqual.c
@@ -74,8 +74,6 @@ SnapshotData SnapshotSelfData = {HeapTupleSatisfiesSelf};
 SnapshotData SnapshotAnyData = {HeapTupleSatisfiesAny};
 SnapshotData SnapshotToastData = {HeapTupleSatisfiesToast};
 
-/* local functions */
-static bool XidInMVCCSnapshot(TransactionId xid, Snapshot snapshot);
 
 /*
  * SetHintBits()
@@ -1435,10 +1433,11 @@ HeapTupleIsSurelyDead(HeapTuple htup, TransactionId OldestXmin)
  *
  * Note: GetSnapshotData never stores either top xid or subxids of our own
  * backend into a snapshot, so these xids will not be reported as "running"
- * by this function.  This is OK for current uses, because we actually only
- * apply this for known-committed XIDs.
+ * by this function.  This is OK for current uses, because we always check
+ * TransactionIdIsCurrentTransactionId first, except when it's known the
+ * XID could not be ours anyway.
  */
-static bool
+bool
 XidInMVCCSnapshot(TransactionId xid, Snapshot snapshot)
 {
 	uint32		i;
