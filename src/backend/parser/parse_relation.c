d01d97c25cf425acc097520bc1c6af57dff51c44
Tom Lane <tgl@sss.pgh.pa.us>
2017-10-27 18:16:25 -0400

    Dept of second thoughts: keep aliasp_item in sync with tlistitem.
    Commit d5b760ecb wasn't quite right, on second thought: if the
    caller didn't ask for column names then it would happily emit
    more Vars than if the caller did ask for column names.  This
    is surely not a good idea.  Advance the aliasp_item whether or
    not we're preparing a colnames list.

--- src/backend/parser/parse_relation.c
+++ src/backend/parser/parse_relation.c
@@ -1844,7 +1844,6 @@ expandRTE(RangeTblEntry *rte, int rtindex, int sublevels_up,
 						char	   *label = strVal(lfirst(aliasp_item));
 
 						*colnames = lappend(*colnames, makeString(pstrdup(label)));
-						aliasp_item = lnext(aliasp_item);
 					}
 
 					if (colvars)
@@ -1860,6 +1859,8 @@ expandRTE(RangeTblEntry *rte, int rtindex, int sublevels_up,
 
 						*colvars = lappend(*colvars, varnode);
 					}
+
+					aliasp_item = lnext(aliasp_item);
 				}
 			}
 			break;
