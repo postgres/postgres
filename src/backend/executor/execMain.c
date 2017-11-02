08ba67d596a1443c32d0b5300aaad8042729cd41
Alvaro Herrera <alvherre@alvh.no-ip.org>
2017-11-02 15:51:05 +0100

    Revert bogus fixes of HOT-freezing bug
    It turns out we misdiagnosed what the real problem was.  Revert the
    previous changes, because they may have worse consequences going
    forward.  A better fix is forthcoming.
    
    The simplistic test case is kept, though disabled.
    
    Discussion: https://postgr.es/m/20171102112019.33wb7g5wp4zpjelu@alap3.anarazel.de

--- src/backend/executor/execMain.c
+++ src/backend/executor/execMain.c
@@ -2275,7 +2275,8 @@ EvalPlanQualFetch(EState *estate, Relation relation, int lockmode,
 			 * atomic, and Xmin never changes in an existing tuple, except to
 			 * invalid or frozen, and neither of those can match priorXmax.)
 			 */
-			if (!HeapTupleUpdateXmaxMatchesXmin(priorXmax, tuple.t_data))
+			if (!TransactionIdEquals(HeapTupleHeaderGetXmin(tuple.t_data),
+									 priorXmax))
 			{
 				ReleaseBuffer(buffer);
 				return NULL;
@@ -2422,7 +2423,8 @@ EvalPlanQualFetch(EState *estate, Relation relation, int lockmode,
 		/*
 		 * As above, if xmin isn't what we're expecting, do nothing.
 		 */
-		if (!HeapTupleUpdateXmaxMatchesXmin(priorXmax, tuple.t_data))
+		if (!TransactionIdEquals(HeapTupleHeaderGetXmin(tuple.t_data),
+								 priorXmax))
 		{
 			ReleaseBuffer(buffer);
 			return NULL;
