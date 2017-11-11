34baf8a00b018caf7269134cf9b461266e66d9a7
Noah Misch <noah@leadboat.com>
2017-11-11 14:33:02 -0800

    Make connect/test1 independent of localhost IPv6.
    Since commit 868898739a8da9ab74c105b8349b7b5c711f265a, it has assumed
    "localhost" resolves to both ::1 and 127.0.0.1.  We gain nothing from
    that assumption, and it does not hold in a default installation of Red
    Hat Enterprise Linux 5.  Back-patch to 9.3 (all supported versions).

--- src/interfaces/ecpg/test/expected/connect-test1.c
+++ src/interfaces/ecpg/test/expected/connect-test1.c
@@ -109,7 +109,7 @@ main(void)
 
 
 	/* wrong port */
-	{ ECPGconnect(__LINE__, 0, "tcp:postgresql://localhost:20/ecpg2_regression" , "regress_ecpg_user1" , "connectpw" , NULL, 0); }
+	{ ECPGconnect(__LINE__, 0, "tcp:postgresql://127.0.0.1:20/ecpg2_regression" , "regress_ecpg_user1" , "connectpw" , NULL, 0); }
 #line 57 "test1.pgc"
 
 	/* no disconnect necessary */
