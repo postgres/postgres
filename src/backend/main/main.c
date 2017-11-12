8c92e66f1a168fdb0862b8d96db4fe0353effc9d
Noah Misch <noah@leadboat.com>
2017-11-12 14:31:00 -0800

    Install Windows crash dump handler before all else.
    Apart from calling write_stderr() on failure, the handler depends on no
    PostgreSQL facilities.  We have experienced crashes before reaching the
    former call site.  Given such an early crash, this change cannot hurt
    and may produce a helpful dump.  Absent an early crash, this change has
    no effect.  Back-patch to 9.3 (all supported versions).
    
    Takayuki Tsunakawa
    
    Discussion: https://postgr.es/m/0A3221C70F24FB45833433255569204D1F80CD13@G01JPEXMBYT05

--- src/backend/main/main.c
+++ src/backend/main/main.c
@@ -61,6 +61,14 @@ main(int argc, char *argv[])
 {
 	bool		do_check_root = true;
 
+	/*
+	 * If supported on the current platform, set up a handler to be called if
+	 * the backend/postmaster crashes with a fatal signal or exception.
+	 */
+#if defined(WIN32) && defined(HAVE_MINIDUMP_TYPE)
+	pgwin32_install_crashdump_handler();
+#endif
+
 	progname = get_progname(argv[0]);
 
 	/*
@@ -80,14 +88,6 @@ main(int argc, char *argv[])
 	 * result pointer.
 	 */
 	argv = save_ps_display_args(argc, argv);
-
-	/*
-	 * If supported on the current platform, set up a handler to be called if
-	 * the backend/postmaster crashes with a fatal signal or exception.
-	 */
-#if defined(WIN32) && defined(HAVE_MINIDUMP_TYPE)
-	pgwin32_install_crashdump_handler();
-#endif
 
 	/*
 	 * Fire up essential subsystems: error and memory management
