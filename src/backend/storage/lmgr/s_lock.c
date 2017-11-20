ae772bbf92742b7ca57f7a8872fedcbbc077f526
Tom Lane <tgl@sss.pgh.pa.us>
2017-11-20 18:05:02 -0500

    Use out-of-line M68K spinlock code for OpenBSD as well as NetBSD.
    David Carlier (from a patch being carried by OpenBSD packagers)
    
    Discussion: https://postgr.es/m/CA+XhMqzwFSGVU7MEnfhCecc8YdP98tigXzzpd0AAdwaGwaVXEA@mail.gmail.com

--- src/backend/storage/lmgr/s_lock.c
+++ src/backend/storage/lmgr/s_lock.c
@@ -251,7 +251,7 @@ static void
 tas_dummy()
 {
 	__asm__ __volatile__(
-#if defined(__NetBSD__) && defined(__ELF__)
+#if (defined(__NetBSD__) || defined(__OpenBSD__)) && defined(__ELF__)
 /* no underscore for label and % for registers */
 						 "\
 .global		tas 				\n\
@@ -276,7 +276,7 @@ _tas:							\n\
 _success:						\n\
 			moveq 	#0,d0		\n\
 			rts					\n"
-#endif							/* __NetBSD__ && __ELF__ */
+#endif							/* (__NetBSD__ || __OpenBSD__) && __ELF__ */
 		);
 }
 #endif							/* __m68k__ && !__linux__ */
