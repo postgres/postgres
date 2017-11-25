1601a9413fbfb98370c3299e3c9da514a0331c6d
Tom Lane <tgl@sss.pgh.pa.us>
2017-11-25 18:15:23 -0500

    Update MSVC build process for new timezone data.
    Missed this dependency in commits 7cce222c9 et al.

--- src/tools/msvc/Install.pm
+++ src/tools/msvc/Install.pm
@@ -343,8 +343,8 @@ sub GenerateTimezoneFiles
 	my $mf     = read_file("src/timezone/Makefile");
 	$mf =~ s{\\\s*[\r\n]+}{}mg;
 
-	$mf =~ /^TZDATA\s*:?=\s*(.*)$/m
-	  || die "Could not find TZDATA line in timezone makefile\n";
+	$mf =~ /^TZDATAFILES\s*:?=\s*(.*)$/m
+	  || die "Could not find TZDATAFILES line in timezone makefile\n";
 	my @tzfiles = split /\s+/, $1;
 
 	$mf =~ /^POSIXRULES\s*:?=\s*(.*)$/m
@@ -359,7 +359,8 @@ sub GenerateTimezoneFiles
 	foreach (@tzfiles)
 	{
 		my $tzfile = $_;
-		push(@args, "src/timezone/data/$tzfile")
+		$tzfile =~ s|\$\(srcdir\)|src/timezone|;
+		push(@args, $tzfile);
 	}
 
 	system(@args);
