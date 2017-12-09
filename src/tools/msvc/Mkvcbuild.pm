85a83a3cc69f72fa779eceea400b44efd7c26fa7
Noah Misch <noah@leadboat.com>
2017-12-08 18:04:45 -0800

    MSVC: Remove cosmetic, cross-branch differences pertaining to Perl.
    This simplifies back-patch of the next change to v9.5 and v9.6.

--- src/tools/msvc/Mkvcbuild.pm
+++ src/tools/msvc/Mkvcbuild.pm
@@ -511,10 +511,10 @@ sub mkvcbuild
 
 		# Add defines from Perl's ccflags; see PGAC_CHECK_PERL_EMBED_CCFLAGS
 		my @perl_embed_ccflags;
-		foreach my $f (split(" ",$Config{ccflags}))
+		foreach my $f (split(" ", $Config{ccflags}))
 		{
-			if ($f =~ /^-D[^_]/ ||
-			    $f =~ /^-D_USE_32BIT_TIME_T/)
+			if (   $f =~ /^-D[^_]/
+				|| $f =~ /^-D_USE_32BIT_TIME_T/)
 			{
 				$f =~ s/\-D//;
 				push(@perl_embed_ccflags, $f);
@@ -616,7 +616,8 @@ sub mkvcbuild
 		}
 		else
 		{
-			die "could not identify perl library version";
+			die
+"could not identify perl library version matching pattern $perl_path\n";
 		}
 
 		# Add transform module dependent on plperl
