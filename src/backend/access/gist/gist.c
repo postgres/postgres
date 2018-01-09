38a23790e13c84446b55cb0f7028c9da67c361e3
Alvaro Herrera <alvherre@alvh.no-ip.org>
2018-01-09 15:54:38 -0300

    Change some bogus PageGetLSN calls to BufferGetLSNAtomic
    As src/backend/access/transam/README says, PageGetLSN may only be called
    by processes holding either exclusive lock on buffer, or a shared lock
    on buffer plus buffer header lock.  Therefore any place that only holds
    a shared buffer lock must use BufferGetLSNAtomic instead of PageGetLSN,
    which internally obtains buffer header lock prior to reading the LSN.
    
    A few callsites failed to comply with this rule.  This was detected by
    running all tests under a new (not committed) assertion that verifies
    PageGetLSN locking contract.  All but one of the callsites that failed
    the assertion are fixed by this patch.  Remaining callsites were
    inspected manually and determined not to need any change.
    
    The exception (unfixed callsite) is in TestForOldSnapshot, which only
    has a Page argument, making it impossible to access the corresponding
    Buffer from it.  Fixing that seems a much larger patch that will have to
    be done separately; and that's just as well, since it was only
    introduced in 9.6 and other bugs are much older.
    
    Some of these bugs are ancient; backpatch all the way back to 9.3.
    
    Authors: Jacob Champion, Asim Praveen, Ashwin Agrawal
    Reviewed-by: Michaël Paquier
    Discussion: https://postgr.es/m/CABAq_6GXgQDVu3u12mK9O5Xt5abBZWQ0V40LZCE+oUf95XyNFg@mail.gmail.com

--- src/backend/access/gist/gist.c
+++ src/backend/access/gist/gist.c
@@ -566,7 +566,8 @@ gistdoinsert(Relation r, IndexTuple itup, Size freespace, GISTSTATE *giststate)
 		}
 
 		stack->page = (Page) BufferGetPage(stack->buffer);
-		stack->lsn = PageGetLSN(stack->page);
+		stack->lsn = xlocked ?
+			PageGetLSN(stack->page) : BufferGetLSNAtomic(stack->buffer);
 		Assert(!RelationNeedsWAL(state.r) || !XLogRecPtrIsInvalid(stack->lsn));
 
 		/*
@@ -816,7 +817,7 @@ gistFindPath(Relation r, BlockNumber child, OffsetNumber *downlinkoffnum)
 			break;
 		}
 
-		top->lsn = PageGetLSN(page);
+		top->lsn = BufferGetLSNAtomic(buffer);
 
 		/*
 		 * If F_FOLLOW_RIGHT is set, the page to the right doesn't have a
