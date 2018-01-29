d18d4bca81f80686238551d1cc460510c3c30544
Andres Freund <andres@anarazel.de>
2018-01-29 11:02:09 -0800

    Improve bit perturbation in TupleHashTableHash.
    The changes in b81b5a96f424531b97cdd1dba97d9d1b9c9d372e did not fully
    address the issue, because the bit-mixing of the IV into the final
    hash-key didn't prevent clustering in the input-data survive in the
    output data.
    
    This didn't cause a lot of problems because of the additional growth
    conditions added d4c62a6b623d6eef88218158e9fa3cf974c6c7e5. But as we
    want to rein those in due to explosive growth in some edges, this
    needs to be fixed.
    
    Author: Andres Freund
    Discussion: https://postgr.es/m/20171127185700.1470.20362@wrigleys.postgresql.org
    Backpatch: 10, where simplehash was introduced

--- src/test/regress/sql/groupingsets.sql
+++ src/test/regress/sql/groupingsets.sql
@@ -342,12 +342,13 @@ explain (costs off)
 
 select a, b, sum(v.x)
   from (values (1),(2)) v(x), gstest_data(v.x)
- group by grouping sets (a,b);
+ group by grouping sets (a,b)
+ order by 1, 2, 3;
 explain (costs off)
   select a, b, sum(v.x)
     from (values (1),(2)) v(x), gstest_data(v.x)
-   group by grouping sets (a,b);
-
+   group by grouping sets (a,b)
+   order by 3, 1, 2;
 select *
   from (values (1),(2)) v(x),
        lateral (select a, b, sum(v.x) from gstest_data(v.x) group by grouping sets (a,b)) s;
