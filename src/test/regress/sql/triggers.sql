cef60043dd27c47a1a4a220158836ccff20be07a
Alvaro Herrera <alvherre@alvh.no-ip.org>
2018-02-13 19:47:16 -0300

    Mention trigger name in trigger test
    This makes it more explicit exactly what is going on, for further
    proposed behavior changes.
    
    Discussion: https://postgr.es/m/20180214212624.hm7of76flesodamf@alvherre.pgsql

--- src/test/regress/sql/triggers.sql
+++ src/test/regress/sql/triggers.sql
@@ -1317,7 +1317,7 @@ create table parted2_stmt_trig2 partition of parted2_stmt_trig for values in (2)
 
 create or replace function trigger_notice() returns trigger as $$
   begin
-    raise notice 'trigger on % % % for %', TG_TABLE_NAME, TG_WHEN, TG_OP, TG_LEVEL;
+    raise notice 'trigger % on % % % for %', TG_NAME, TG_TABLE_NAME, TG_WHEN, TG_OP, TG_LEVEL;
     if TG_LEVEL = 'ROW' then
        return NEW;
     end if;
