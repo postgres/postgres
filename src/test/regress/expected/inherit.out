postgres/postgres

commit d007a95055b9b649b74b5d25aa4d2b46f3eca21c

Simple constraint exclusion.  For now, only child tables of inheritance scans are candidates for exclusion; this should be fixed eventually. Simon Riggs, with some help from Tom Lane.

src/backend/optimizer/util/plancat.c
@@ -9,7 +9,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/optimizer/util/plancat.c,v 1.112 2005/06/13 23:14:48 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/optimizer/util/plancat.c,v 1.113 2005/07/23 21:05:47 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -25,6 +25,7 @@
 #include "nodes/makefuncs.h"
 #include "optimizer/clauses.h"
 #include "optimizer/plancat.h"
+#include "optimizer/prep.h"
 #include "optimizer/tlist.h"
 #include "parser/parsetree.h"
 #include "parser/parse_expr.h"
@@ -358,6 +359,85 @@ estimate_rel_size(Relation rel, int32 *attr_widths,
 			break;
 	}
 }
+
+
+/*
+ * get_relation_constraints
+ *
+ * Retrieve the CHECK constraint expressions of the given relation.
+ *
+ * Returns a List (possibly empty) of constraint expressions.  Each one
+ * has been canonicalized, and its Vars are changed to have the varno
+ * indicated by rel->relid.  This allows the expressions to be easily
+ * compared to expressions taken from WHERE.
+ *
+ * Note: at present this is invoked at most once per relation per planner
+ * run, and in many cases it won't be invoked at all, so there seems no
+ * point in caching the data in RelOptInfo.
+ */
+List *
+get_relation_constraints(Oid relationObjectId, RelOptInfo *rel)
+{
+	List	   *result = NIL;
+	Index		varno = rel->relid;
+	Relation	relation;
+	TupleConstr *constr;
+
+	/*
+	 * We assume the relation has already been safely locked.
+	 */
+	relation = heap_open(relationObjectId, NoLock);
+
+	constr = relation->rd_att->constr;
+	if (constr != NULL)
+	{
+		int		num_check = constr->num_check;
+		int		i;
+
+		for (i = 0; i < num_check; i++)
+		{
+			Node	*cexpr;
+
+			cexpr = stringToNode(constr->check[i].ccbin);
+
+			/*
+			 * Run each expression through const-simplification and
+			 * canonicalization.  This is not just an optimization, but is
+			 * necessary, because we will be comparing it to
+			 * similarly-processed qual clauses, and may fail to detect valid
+			 * matches without this.  This must match the processing done to
+			 * qual clauses in preprocess_expression()!  (We can skip the
+			 * stuff involving subqueries, however, since we don't allow any
+			 * in check constraints.)
+			 */
+			cexpr = eval_const_expressions(cexpr);
+
+			cexpr = (Node *) canonicalize_qual((Expr *) cexpr);
+
+			/*
+			 * Also mark any coercion format fields as "don't care", so that
+			 * we can match to both explicit and implicit coercions.
+			 */
+			set_coercionform_dontcare(cexpr);
+
+			/* Fix Vars to have the desired varno */
+			if (varno != 1)
+				ChangeVarNodes(cexpr, 1, varno, 0);
+
+			/*
+			 * Finally, convert to implicit-AND format (that is, a List)
+			 * and append the resulting item(s) to our output list.
+			 */
+			result = list_concat(result,
+								 make_ands_implicit((Expr *) cexpr));
+		}
+	}
+
+	heap_close(relation, NoLock);
+
+	return result;
+}
+
 
 /*
  * build_physical_tlist

doc/src/sgml/runtime.sgml
@@ -1,5 +1,5 @@
 <!--
-$PostgreSQL: pgsql/doc/src/sgml/runtime.sgml,v 1.338 2005/07/14 05:13:38 tgl Exp $
+$PostgreSQL: pgsql/doc/src/sgml/runtime.sgml,v 1.339 2005/07/23 21:05:45 tgl Exp $
 -->
 
 <chapter Id="runtime">
@@ -2274,6 +2274,56 @@ archive_command = 'copy "%p" /mnt/server/archivedir/"%f"'  # Windows
         planner's estimates. The default is 10. For more information
         on the use of statistics by the <productname>PostgreSQL</>
         query planner, refer to <xref linkend="planner-stats">.
+       </para>
+      </listitem>
+     </varlistentry>
+
+     <varlistentry id="guc-enable-constraint-exclusion" xreflabel="enable_constraint_exclusion">
+      <term><varname>enable_constraint_exclusion</varname> (<type>boolean</type>)</term>
+      <indexterm>
+       <primary>constraint exclusion</primary>
+      </indexterm>
+      <indexterm>
+       <primary><varname>enable_constraint_exclusion</> configuration parameter</primary>
+      </indexterm>
+      <listitem>
+       <para>
+        Enables or disables the query planner's use of table constraints.
+        The default is <literal>off</>.
+       </para>
+
+       <para>
+        When this parameter is <literal>on</>, the planner compares query
+        conditions to table CHECK constraints, and omits scanning tables
+        for which the conditions contradict the constraints.  (Presently
+        this is done only for child tables of inheritance scans.)  For
+        example:
+
+<programlisting>
+CREATE TABLE parent(key integer, ...);
+CREATE TABLE child1000(check (key between 1000 and 1999)) INHERITS(parent);
+CREATE TABLE child2000(check (key between 2000 and 2999)) INHERITS(parent);
+...
+SELECT * FROM parent WHERE key = 2400;
+</programlisting>
+
+        With constraint exclusion enabled, this SELECT will not scan
+        <structname>child1000</> at all.  This can improve performance when
+        inheritance is used to build partitioned tables.
+       </para>
+
+       <para>
+        Currently, <varname>enable_constraint_exclusion</> defaults to
+        <literal>off</>, because it creates a risk of wrong answers when
+        query plans are cached: if a table constraint is changed or dropped,
+        the previously generated plan may now be wrong, and there is no
+        built-in mechanism to force re-planning.  (This deficiency will
+        probably be addressed in a future
+        <productname>PostgreSQL</productname> release.)  Another reason
+        for keeping it off is that the constraint checks are relatively
+        expensive to make, and in many circumstances will yield no savings.
+        It is recommended to turn this on only if you are actually using
+        partitioned tables designed to take advantage of the feature.
        </para>
       </listitem>
      </varlistentry>

src/backend/optimizer/path/allpaths.c
@@ -8,13 +8,14 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/optimizer/path/allpaths.c,v 1.134 2005/06/10 03:32:21 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/optimizer/path/allpaths.c,v 1.135 2005/07/23 21:05:46 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
 
 #include "postgres.h"
 
+#include "nodes/makefuncs.h"
 #ifdef OPTIMIZER_DEBUG
 #include "nodes/print.h"
 #endif
@@ -25,6 +26,7 @@
 #include "optimizer/paths.h"
 #include "optimizer/plancat.h"
 #include "optimizer/planner.h"
+#include "optimizer/predtest.h"
 #include "optimizer/prep.h"
 #include "optimizer/var.h"
 #include "parser/parsetree.h"
@@ -34,6 +36,7 @@
 
 
 /* These parameters are set by GUC */
+bool		enable_constraint_exclusion = false;
 bool		enable_geqo = false;	/* just in case GUC doesn't set it */
 int			geqo_threshold;
 
@@ -311,7 +314,37 @@ set_inherited_rel_pathlist(PlannerInfo *root, RelOptInfo *rel,
 								   childOID);
 
 		/*
-		 * Now compute child access paths, and save the cheapest.
+		 * If we can prove we don't need to scan this child via constraint
+		 * exclusion, just ignore it.  (We have to have converted the
+		 * baserestrictinfo Vars before we can make the test.)
+		 */
+		if (enable_constraint_exclusion)
+		{
+			List   *constraint_pred;
+
+			constraint_pred = get_relation_constraints(childOID, childrel);
+			/*
+			 * We do not currently enforce that CHECK constraints contain
+			 * only immutable functions, so it's necessary to check here.
+			 * We daren't draw conclusions from plan-time evaluation of
+			 * non-immutable functions.
+			 */
+			if (!contain_mutable_functions((Node *) constraint_pred))
+			{
+				/*
+				 * The constraints are effectively ANDed together, so we can
+				 * just try to refute the entire collection at once.  This may
+				 * allow us to make proofs that would fail if we took them
+				 * individually.
+				 */
+				if (predicate_refuted_by(constraint_pred,
+										 childrel->baserestrictinfo))
+					continue;
+			}
+		}
+
+		/*
+		 * Compute the child's access paths, and save the cheapest.
 		 */
 		set_plain_rel_pathlist(root, childrel, childrte);
 
@@ -345,7 +378,8 @@ set_inherited_rel_pathlist(PlannerInfo *root, RelOptInfo *rel,
 
 	/*
 	 * Finally, build Append path and install it as the only access path
-	 * for the parent rel.
+	 * for the parent rel.  (Note: this is correct even if we have zero
+	 * or one live subpath due to constraint exclusion.)
 	 */
 	add_path(rel, (Path *) create_append_path(rel, subpaths));
 

src/backend/optimizer/plan/createplan.c
@@ -10,7 +10,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/optimizer/plan/createplan.c,v 1.194 2005/07/15 22:02:51 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/optimizer/plan/createplan.c,v 1.195 2005/07/23 21:05:46 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -40,7 +40,7 @@ static List *build_relation_tlist(RelOptInfo *rel);
 static bool use_physical_tlist(RelOptInfo *rel);
 static void disuse_physical_tlist(Plan *plan, Path *path);
 static Join *create_join_plan(PlannerInfo *root, JoinPath *best_path);
-static Append *create_append_plan(PlannerInfo *root, AppendPath *best_path);
+static Plan *create_append_plan(PlannerInfo *root, AppendPath *best_path);
 static Result *create_result_plan(PlannerInfo *root, ResultPath *best_path);
 static Material *create_material_plan(PlannerInfo *root, MaterialPath *best_path);
 static Plan *create_unique_plan(PlannerInfo *root, UniquePath *best_path);
@@ -435,7 +435,7 @@ create_join_plan(PlannerInfo *root, JoinPath *best_path)
  *
  *	  Returns a Plan node.
  */
-static Append *
+static Plan *
 create_append_plan(PlannerInfo *root, AppendPath *best_path)
 {
 	Append	   *plan;
@@ -443,6 +443,25 @@ create_append_plan(PlannerInfo *root, AppendPath *best_path)
 	List	   *subplans = NIL;
 	ListCell   *subpaths;
 
+	/*
+	 * It is possible for the subplans list to contain only one entry,
+	 * or even no entries.  Handle these cases specially.
+	 *
+	 * XXX ideally, if there's just one entry, we'd not bother to generate
+	 * an Append node but just return the single child.  At the moment this
+	 * does not work because the varno of the child scan plan won't match
+	 * the parent-rel Vars it'll be asked to emit.
+	 */
+	if (best_path->subpaths == NIL)
+	{
+		/* Generate a Result plan with constant-FALSE gating qual */
+		return (Plan *) make_result(tlist,
+									(Node *) list_make1(makeBoolConst(false,
+																	  false)),
+									NULL);
+	}
+
+	/* Normal case with multiple subpaths */
 	foreach(subpaths, best_path->subpaths)
 	{
 		Path	   *subpath = (Path *) lfirst(subpaths);
@@ -452,7 +471,7 @@ create_append_plan(PlannerInfo *root, AppendPath *best_path)
 
 	plan = make_append(subplans, false, tlist);
 
-	return plan;
+	return (Plan *) plan;
 }
 
 /*

src/backend/optimizer/plan/planagg.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/optimizer/plan/planagg.c,v 1.5 2005/06/05 22:32:56 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/optimizer/plan/planagg.c,v 1.6 2005/07/23 21:05:46 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -184,7 +184,6 @@ optimize_minmax_aggregates(PlannerInfo *root, List *tlist, Path *best_path)
 	 */
 	if (IsA(best_path, ResultPath))
 	{
-		Assert(((ResultPath *) best_path)->subpath != NULL);
 		constant_quals = ((ResultPath *) best_path)->constantqual;
 		/* no need to do this more than once: */
 		constant_quals = order_qual_clauses(root, constant_quals);

src/backend/utils/misc/guc.c
@@ -10,7 +10,7 @@
  * Written by Peter Eisentraut <peter_e@gmx.net>.
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/utils/misc/guc.c,v 1.276 2005/07/21 18:06:12 momjian Exp $
+ *	  $PostgreSQL: pgsql/src/backend/utils/misc/guc.c,v 1.277 2005/07/23 21:05:47 tgl Exp $
  *
  *--------------------------------------------------------------------
  */
@@ -434,6 +434,16 @@ static struct config_bool ConfigureNamesBool[] =
 		},
 		&enable_hashjoin,
 		true, NULL, NULL
+	},
+	{
+		{"enable_constraint_exclusion", PGC_USERSET, QUERY_TUNING_OTHER,
+			gettext_noop("Enables the planner's use of constraints in queries."),
+			gettext_noop("Constraints will be examined to exclude tables "
+						 "that can be proven not to be required to produce "
+						 "a correct result for the query.")
+		},
+		&enable_constraint_exclusion,
+		false, NULL, NULL
 	},
 	{
 		{"geqo", PGC_USERSET, QUERY_TUNING_GEQO,

src/backend/utils/misc/postgresql.conf.sample
@@ -173,6 +173,7 @@
 # - Other Planner Options -
 
 #default_statistics_target = 10	# range 1-1000
+#enable_constraint_exclusion = off
 #from_collapse_limit = 8
 #join_collapse_limit = 8	# 1 disables collapsing of explicit JOINs
 

commit 106d588055a2773ac005125360c65dd0d26cd35e

Fix improper uses of canonicalize_qual().
One of the things canonicalize_qual() does is to remove constant-NULL
subexpressions of top-level AND/OR clauses.  It does that on the assumption
that what it's given is a top-level WHERE clause, so that NULL can be
treated like FALSE.  Although this is documented down inside a subroutine
of canonicalize_qual(), it wasn't mentioned in the documentation of that
function itself, and some callers hadn't gotten that memo.

Notably, commit d007a9505 caused get_relation_constraints() to apply
canonicalize_qual() to CHECK constraints.  That allowed constraint
exclusion to misoptimize situations in which a CHECK constraint had a
provably-NULL subclause, as seen in the regression test case added here,
in which a child table that should be scanned is not.  (Although this
thinko is ancient, the test case doesn't fail before 9.2, for reasons
I've not bothered to track down in detail.  There may be related cases
that do fail before that.)

More recently, commit f0e44751d added an independent bug by applying
canonicalize_qual() to index expressions, which is even sillier since
those might not even be boolean.  If they are, though, I think this
could lead to making incorrect index entries for affected index
expressions in v10.  I haven't attempted to prove that though.

To fix, add an "is_check" parameter to canonicalize_qual() to specify
whether it should assume WHERE or CHECK semantics, and make it perform
NULL-elimination accordingly.  Adjust the callers to apply the right
semantics, or remove the call entirely in cases where it's not known
that the expression has one or the other semantics.  I also removed
the call in some cases involving partition expressions, where it should
be a no-op because such expressions should be canonical already ...
and was a no-op, independently of whether it could in principle have
done something, because it was being handed the qual in implicit-AND

src/backend/optimizer/plan/planner.c
@@ -742,7 +742,7 @@ preprocess_expression(PlannerInfo *root, Node *expr, int kind)
 	 */
 	if (kind == EXPRKIND_QUAL)
 	{
-		expr = (Node *) canonicalize_qual((Expr *) expr);
+		expr = (Node *) canonicalize_qual_ext((Expr *) expr, false);
 
 #ifdef OPTIMIZER_DEBUG
 		printf("After canonicalize_qual()\n");

src/backend/optimizer/plan/subselect.c
@@ -1696,7 +1696,7 @@ convert_EXISTS_to_ANY(PlannerInfo *root, Query *subselect,
 	 * subroot.
 	 */
 	whereClause = eval_const_expressions(root, whereClause);
-	whereClause = (Node *) canonicalize_qual((Expr *) whereClause);
+	whereClause = (Node *) canonicalize_qual_ext((Expr *) whereClause, false);
 	whereClause = (Node *) make_ands_implicit((Expr *) whereClause);
 
 	/*

src/backend/optimizer/prep/prepqual.c
@@ -39,7 +39,7 @@
 
 static List *pull_ands(List *andlist);
 static List *pull_ors(List *orlist);
-static Expr *find_duplicate_ors(Expr *qual);
+static Expr *find_duplicate_ors(Expr *qual, bool is_check);
 static Expr *process_duplicate_ors(List *orlist);
 
 
@@ -269,6 +269,24 @@ negate_clause(Node *node)
  * canonicalize_qual
  *	  Convert a qualification expression to the most useful form.
  *
+ * Backwards-compatibility wrapper for use by external code that hasn't
+ * been updated.
+ */
+Expr *
+canonicalize_qual(Expr *qual)
+{
+	return canonicalize_qual_ext(qual, false);
+}
+
+/*
+ * canonicalize_qual_ext
+ *	  Convert a qualification expression to the most useful form.
+ *
+ * This is primarily intended to be used on top-level WHERE (or JOIN/ON)
+ * clauses.  It can also be used on top-level CHECK constraints, for which
+ * pass is_check = true.  DO NOT call it on any expression that is not known
+ * to be one or the other, as it might apply inappropriate simplifications.
+ *
  * The name of this routine is a holdover from a time when it would try to
  * force the expression into canonical AND-of-ORs or OR-of-ANDs form.
  * Eventually, we recognized that that had more theoretical purity than
@@ -283,7 +301,7 @@ negate_clause(Node *node)
  * Returns the modified qualification.
  */
 Expr *
-canonicalize_qual(Expr *qual)
+canonicalize_qual_ext(Expr *qual, bool is_check)
 {
 	Expr	   *newqual;
 
@@ -296,7 +314,7 @@ canonicalize_qual(Expr *qual)
 	 * within the top-level AND/OR structure; there's no point in looking
 	 * deeper.  Also remove any NULL constants in the top-level structure.
 	 */
-	newqual = find_duplicate_ors(qual);
+	newqual = find_duplicate_ors(qual, is_check);
 
 	return newqual;
 }
@@ -395,16 +413,17 @@ pull_ors(List *orlist)
  *	  Only the top-level AND/OR structure is searched.
  *
  * While at it, we remove any NULL constants within the top-level AND/OR
- * structure, eg "x OR NULL::boolean" is reduced to "x".  In general that
- * would change the result, so eval_const_expressions can't do it; but at
- * top level of WHERE, we don't need to distinguish between FALSE and NULL
- * results, so it's valid to treat NULL::boolean the same as FALSE and then
- * simplify AND/OR accordingly.
+ * structure, eg in a WHERE clause, "x OR NULL::boolean" is reduced to "x".
+ * In general that would change the result, so eval_const_expressions can't
+ * do it; but at top level of WHERE, we don't need to distinguish between
+ * FALSE and NULL results, so it's valid to treat NULL::boolean the same
+ * as FALSE and then simplify AND/OR accordingly.  Conversely, in a top-level
+ * CHECK constraint, we may treat a NULL the same as TRUE.
  *
  * Returns the modified qualification.  AND/OR flatness is preserved.
  */
 static Expr *
-find_duplicate_ors(Expr *qual)
+find_duplicate_ors(Expr *qual, bool is_check)
 {
 	if (or_clause((Node *) qual))
 	{
@@ -416,18 +435,29 @@ find_duplicate_ors(Expr *qual)
 		{
 			Expr	   *arg = (Expr *) lfirst(temp);
 
-			arg = find_duplicate_ors(arg);
+			arg = find_duplicate_ors(arg, is_check);
 
 			/* Get rid of any constant inputs */
 			if (arg && IsA(arg, Const))
 			{
 				Const	   *carg = (Const *) arg;
 
-				/* Drop constant FALSE or NULL */
-				if (carg->constisnull || !DatumGetBool(carg->constvalue))
-					continue;
-				/* constant TRUE, so OR reduces to TRUE */
-				return arg;
+				if (is_check)
+				{
+					/* Within OR in CHECK, drop constant FALSE */
+					if (!carg->constisnull && !DatumGetBool(carg->constvalue))
+						continue;
+					/* Constant TRUE or NULL, so OR reduces to TRUE */
+					return (Expr *) makeBoolConst(true, false);
+				}
+				else
+				{
+					/* Within OR in WHERE, drop constant FALSE or NULL */
+					if (carg->constisnull || !DatumGetBool(carg->constvalue))
+						continue;
+					/* Constant TRUE, so OR reduces to TRUE */
+					return arg;
+				}
 			}
 
 			orlist = lappend(orlist, arg);
@@ -449,18 +479,29 @@ find_duplicate_ors(Expr *qual)
 		{
 			Expr	   *arg = (Expr *) lfirst(temp);
 
-			arg = find_duplicate_ors(arg);
+			arg = find_duplicate_ors(arg, is_check);
 
 			/* Get rid of any constant inputs */
 			if (arg && IsA(arg, Const))
 			{
 				Const	   *carg = (Const *) arg;
 
-				/* Drop constant TRUE */
-				if (!carg->constisnull && DatumGetBool(carg->constvalue))
-					continue;
-				/* constant FALSE or NULL, so AND reduces to FALSE */
-				return (Expr *) makeBoolConst(false, false);
+				if (is_check)
+				{
+					/* Within AND in CHECK, drop constant TRUE or NULL */
+					if (carg->constisnull || DatumGetBool(carg->constvalue))
+						continue;
+					/* Constant FALSE, so AND reduces to FALSE */
+					return arg;
+				}
+				else
+				{
+					/* Within AND in WHERE, drop constant TRUE */
+					if (!carg->constisnull && DatumGetBool(carg->constvalue))
+						continue;
+					/* Constant FALSE or NULL, so AND reduces to FALSE */
+					return (Expr *) makeBoolConst(false, false);
+				}
 			}
 
 			andlist = lappend(andlist, arg);

src/backend/optimizer/util/plancat.c
@@ -1056,7 +1056,7 @@ get_relation_constraints(PlannerInfo *root,
 			 */
 			cexpr = eval_const_expressions(root, cexpr);
 
-			cexpr = (Node *) canonicalize_qual((Expr *) cexpr);
+			cexpr = (Node *) canonicalize_qual_ext((Expr *) cexpr, true);
 
 			/* Fix Vars to have the desired varno */
 			if (varno != 1)

src/backend/utils/cache/relcache.c
@@ -4091,7 +4091,8 @@ RelationGetIndexExpressions(Relation relation)
 	 * Run the expressions through eval_const_expressions. This is not just an
 	 * optimization, but is necessary, because the planner will be comparing
 	 * them to similarly-processed qual clauses, and may fail to detect valid
-	 * matches without this.  We don't bother with canonicalize_qual, however.
+	 * matches without this.  We must not use canonicalize_qual, however,
+	 * since these aren't qual expressions.
 	 */
 	result = (List *) eval_const_expressions(NULL, (Node *) result);
 
@@ -4159,7 +4160,7 @@ RelationGetIndexPredicate(Relation relation)
 	 */
 	result = (List *) eval_const_expressions(NULL, (Node *) result);
 
-	result = (List *) canonicalize_qual((Expr *) result);
+	result = (List *) canonicalize_qual_ext((Expr *) result, false);
 
 	/* Also convert to implicit-AND format */
 	result = make_ands_implicit((Expr *) result);

src/include/optimizer/prep.h
@@ -34,6 +34,7 @@ extern Relids get_relids_for_join(PlannerInfo *root, int joinrelid);
  */
 extern Node *negate_clause(Node *node);
 extern Expr *canonicalize_qual(Expr *qual);
+extern Expr *canonicalize_qual_ext(Expr *qual, bool is_check);
 
 /*
  * prototypes for prepsecurity.c

src/test/regress/expected/inherit.out
@@ -1591,3 +1591,27 @@ FROM generate_series(1, 3) g(i);
 reset enable_seqscan;
 reset enable_indexscan;
 reset enable_bitmapscan;
+--
+-- Check handling of a constant-null CHECK constraint
+--
+create table cnullparent (f1 int);
+create table cnullchild (check (f1 = 1 or f1 = null)) inherits(cnullparent);
+insert into cnullchild values(1);
+insert into cnullchild values(2);
+insert into cnullchild values(null);
+select * from cnullparent;
+ f1 
+----
+  1
+  2
+   
+(3 rows)
+
+select * from cnullparent where f1 = 2;
+ f1 
+----
+  2
+(1 row)
+
+drop table cnullparent cascade;
+NOTICE:  drop cascades to table cnullchild

src/test/regress/sql/inherit.sql
@@ -562,3 +562,15 @@ FROM generate_series(1, 3) g(i);
 reset enable_seqscan;
 reset enable_indexscan;
 reset enable_bitmapscan;
+
+--
+-- Check handling of a constant-null CHECK constraint
+--
+create table cnullparent (f1 int);
+create table cnullchild (check (f1 = 1 or f1 = null)) inherits(cnullparent);
+insert into cnullchild values(1);
+insert into cnullchild values(2);
+insert into cnullchild values(null);
+select * from cnullparent;
+select * from cnullparent where f1 = 2;
+drop table cnullparent cascade;