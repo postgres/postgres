a2102e1a92bf0b485487b0582f2efaa9ff025556
Alvaro Herrera <alvherre@alvh.no-ip.org>
2018-03-15 09:51:12 -0300

    test_ddl_deparse: Don't use pg_class as source for a matview
    Doing so causes a pg_upgrade of a database containing these objects to
    fail whenever pg_class changes.  And it's pointless anyway: we have more
    interesting tables anyhow.
    
    Discussion: https://postgr.es/m/CAD5tBc+s8pW9WvH2+_z=B4x95FD4QuzZKcaMpff_9H4rS0VU1A@mail.gmail.com

--- src/test/modules/test_ddl_deparse/sql/matviews.sql
+++ src/test/modules/test_ddl_deparse/sql/matviews.sql
@@ -3,6 +3,6 @@
 --
 
 CREATE MATERIALIZED VIEW pg_class_mv AS
-  SELECT * FROM pg_class LIMIT 1 WITH NO DATA;
+  SELECT * FROM datatype_table LIMIT 1 WITH NO DATA;
 
 REFRESH MATERIALIZED VIEW pg_class_mv;
