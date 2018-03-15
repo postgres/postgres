eb1c481ac3975574c1709042efe2c6c32a937a23
Alvaro Herrera <alvherre@alvh.no-ip.org>
2018-03-15 15:14:15 -0300

    test_ddl_deparse: rename matview
    Should have done this in e69f5e0efca ...
    Per note from Tom Lane.

--- src/test/modules/test_ddl_deparse/sql/matviews.sql
+++ src/test/modules/test_ddl_deparse/sql/matviews.sql
@@ -2,7 +2,7 @@
 -- Materialized views
 --
 
-CREATE MATERIALIZED VIEW pg_class_mv AS
+CREATE MATERIALIZED VIEW ddl_deparse_mv AS
   SELECT * FROM datatype_table LIMIT 1 WITH NO DATA;
 
-REFRESH MATERIALIZED VIEW pg_class_mv;
+REFRESH MATERIALIZED VIEW ddl_deparse_mv;
