1944cdc98273dbb8439ad9b387ca2858531afcf0
Peter Eisentraut <peter_e@gmx.net>
2018-03-27 12:31:34 -0400

    libpq: PQhost to return active connected host or hostaddr
    Previously, PQhost didn't return the connected host details when the
    connection type was CHT_HOST_ADDRESS (i.e., via hostaddr).  Instead, it
    returned the complete host connection parameter (which could contain
    multiple hosts) or the default host details, which was confusing and
    arguably incorrect.
    
    Change this to return the actually connected host or hostaddr
    irrespective of the connection type.  When hostaddr but no host was
    specified, hostaddr is now returned.  Never return the original host
    connection parameter, and document that PQhost cannot be relied on
    before the connection is established.
    
    PQport is similarly changed to always return the active connection port
    and never the original connection parameter.
    
    Author: Hari Babu <kommi.haribabu@gmail.com>
    Reviewed-by: Michael Paquier <michael@paquier.xyz>
    Reviewed-by: Kyotaro HORIGUCHI <horiguchi.kyotaro@lab.ntt.co.jp>
    Reviewed-by: David G. Johnston <david.g.johnston@gmail.com>

--- src/interfaces/libpq/fe-connect.c
+++ src/interfaces/libpq/fe-connect.c
@@ -6017,19 +6017,18 @@ PQhost(const PGconn *conn)
 {
 	if (!conn)
 		return NULL;
-	if (conn->connhost != NULL &&
-		conn->connhost[conn->whichhost].type != CHT_HOST_ADDRESS)
-		return conn->connhost[conn->whichhost].host;
-	else if (conn->pghost != NULL && conn->pghost[0] != '\0')
-		return conn->pghost;
-	else
+
+	if (conn->connhost != NULL)
 	{
-#ifdef HAVE_UNIX_SOCKETS
-		return DEFAULT_PGSOCKET_DIR;
-#else
-		return DefaultHost;
-#endif
+		if (conn->connhost[conn->whichhost].host != NULL &&
+			conn->connhost[conn->whichhost].host[0] != '\0')
+			return conn->connhost[conn->whichhost].host;
+		else if (conn->connhost[conn->whichhost].hostaddr != NULL &&
+				 conn->connhost[conn->whichhost].hostaddr[0] != '\0')
+			return conn->connhost[conn->whichhost].hostaddr;
 	}
+
+	return "";
 }
 
 char *
@@ -6037,9 +6036,11 @@ PQport(const PGconn *conn)
 {
 	if (!conn)
 		return NULL;
+
 	if (conn->connhost != NULL)
 		return conn->connhost[conn->whichhost].port;
-	return conn->pgport;
+
+	return "";
 }
 
 char *
