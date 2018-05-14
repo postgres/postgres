fc2a41e23e10f2338be4907f71c277ea09526680
Magnus Hagander <magnus@hagander.net>
2018-05-14 18:59:43 +0200

    Fix file paths in comments
    Author: Daniel Gustafsson <daniel@yesql.se>

--- src/port/inet_net_ntop.c
+++ src/port/inet_net_ntop.c
@@ -14,7 +14,7 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
  * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *
- *	  src/backend/utils/adt/inet_net_ntop.c
+ *	  src/port/inet_net_ntop.c
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
