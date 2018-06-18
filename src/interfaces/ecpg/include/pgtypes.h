db05d0b906be6dbf194ce594e0a0777ebdaf7e93
Thomas Munro <tmunro@postgresql.org>
2018-06-18 18:33:53 +1200

    Add PGTYPESchar_free() to avoid cross-module problems on Windows.
    On Windows, it is sometimes important for corresponding malloc() and
    free() calls to be made from the same DLL, since some build options can
    result in multiple allocators being active at the same time.  For that
    reason we already provided PQfreemem().  This commit adds a similar
    function for freeing string results allocated by the pgtypes library.
    
    Author: Takayuki Tsunakawa
    Reviewed-by: Kyotaro Horiguchi
    Discussion: https://postgr.es/m/0A3221C70F24FB45833433255569204D1F8AD5D6%40G01JPEXMBYT05

---
+++ src/interfaces/ecpg/include/pgtypes.h
@@ -0,0 +1,17 @@
+/* src/interfaces/ecpg/include/pgtypes.h */
+
+#ifndef PGTYPES_H
+#define PGTYPES_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+extern void PGTYPESchar_free(char *ptr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif							/* PGTYPES_H */
