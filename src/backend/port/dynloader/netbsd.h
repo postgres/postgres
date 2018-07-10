b68ff3ea672c066b7eee6ca777618025b40abfd4
Peter Eisentraut <peter_e@gmx.net>
2018-07-10 16:07:10 +0200

    Remove obsolete netbsd dynloader code
    dlopen() has been documented since NetBSD 1.1 (1995).

--- src/backend/port/dynloader/netbsd.h
+++ src/backend/port/dynloader/netbsd.h
@@ -3,7 +3,6 @@
  * netbsd.h
  *	  port-specific prototypes for NetBSD
  *
- *
  * Portions Copyright (c) 1996-2018, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
@@ -14,24 +13,9 @@
 #ifndef PORT_PROTOS_H
 #define PORT_PROTOS_H
 
-#include <nlist.h>
-#include <link.h>
 #include <dlfcn.h>
 
 #include "utils/dynamic_loader.h"	/* pgrminclude ignore */
-
-/*
- * Dynamic Loader on NetBSD 1.0.
- *
- * this dynamic loader uses the system dynamic loading interface for shared
- * libraries (ie. dlopen/dlsym/dlclose). The user must specify a shared
- * library as the file to be dynamically loaded.
- *
- * agc - I know this is all a bit crufty, but it does work, is fairly
- * portable, and works (the stipulation that the d.l. function must
- * begin with an underscore is fairly tricky, and some versions of
- * NetBSD (like 1.0, and 1.0A pre June 1995) have no dlerror.)
- */
 
 /*
  * In some older systems, the RTLD_NOW flag isn't defined and the mode
@@ -46,14 +30,9 @@
 #define RTLD_GLOBAL 0
 #endif
 
-#define		   pg_dlopen(f)    BSD44_derived_dlopen((f), RTLD_NOW | RTLD_GLOBAL)
-#define		   pg_dlsym		   BSD44_derived_dlsym
-#define		   pg_dlclose	   BSD44_derived_dlclose
-#define		   pg_dlerror	   BSD44_derived_dlerror
-
-char	   *BSD44_derived_dlerror(void);
-void	   *BSD44_derived_dlopen(const char *filename, int num);
-void	   *BSD44_derived_dlsym(void *handle, const char *name);
-void		BSD44_derived_dlclose(void *handle);
+#define pg_dlopen(f)	dlopen((f), RTLD_NOW | RTLD_GLOBAL)
+#define pg_dlsym		dlsym
+#define pg_dlclose		dlclose
+#define pg_dlerror		dlerror
 
 #endif							/* PORT_PROTOS_H */
