a01d0fa1d889cc2003e1941e8b98707c4d701ba9
Alexander Korotkov <akorotkov@postgresql.org>
2018-07-11 13:16:46 +0300

    Fix wrong file path in header comment
    Header comment of shm_mq.c was mistakenly specifying path to shm_mq.h.
    It was introduced in ec9037df.  So, theoretically it could be
    backpatched to 9.4, but it doesn't seem to worth it.

--- src/backend/storage/ipc/shm_mq.c
+++ src/backend/storage/ipc/shm_mq.c
@@ -11,7 +11,7 @@
  * Portions Copyright (c) 1996-2018, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * src/include/storage/shm_mq.h
+ * src/backend/storage/ipc/shm_mq.c
  *
  *-------------------------------------------------------------------------
  */
