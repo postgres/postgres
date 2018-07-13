1f4ec8945967a75f711d721860752985624a0957
Peter Eisentraut <peter_e@gmx.net>
2018-07-13 10:01:04 +0200

    Remove obsolete documentation build tools for Windows
    The scripts and instructions have been nonfunctional at least since
    PostgreSQL 10 (commit 510074f9f0131a04322d6a3d2a51c87e6db243f9) and
    nobody has stepped up to fix them.  So right now just remove them until
    someone wants to resurrect them.
    
    Discussion: https://www.postgresql.org/message-id/flat/B74C0219-6BA9-46E1-A524-5B9E8CD3BDB3%40yesql.se

--- src/tools/msvc/builddoc.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-REM src/tools/msvc/builddoc.bat
-REM all the logic for this now belongs in builddoc.pl. This file really
-REM only exists so you don't have to type "perl builddoc.pl"
-REM Resist any temptation to add any logic here.
-@perl builddoc.pl %*
