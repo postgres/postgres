fa7d5b704a3786be0f3e3334dd506fe8a46b02bc
Michael Paquier <michael@paquier.xyz>
2018-07-29 07:53:11 +0900

    Add verbosity to pg_basebackup for sync
    This is useful to know when the data copy has been finished.  The
    current situation can be confusing for users as the last message is
    "waiting for background process to finish streaming", so it looks like
    this is taking time but the final sync is instead.
    
    Author: Jeff Janes
    Discussion: https://postgr.es/m/CAMkU=1ypeoMJ=tFBG8vP13sxEtXd4Pm_x1SqsJdW_RvzpcvN=A@mail.gmail.com

--- src/bin/pg_basebackup/pg_basebackup.c
+++ src/bin/pg_basebackup/pg_basebackup.c
@@ -2112,6 +2112,9 @@ BaseBackup(void)
 	 */
 	if (do_sync)
 	{
+		if (verbose)
+			fprintf(stderr,
+					_("%s: syncing data to disk ...\n"), progname);
 		if (format == 't')
 		{
 			if (strcmp(basedir, "-") != 0)
