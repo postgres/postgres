5ad143cda092f19760d7fb1be861ed24e724ef4d
Tom Lane <tgl@sss.pgh.pa.us>
2018-08-04 19:38:59 -0400

    Fix INSERT ON CONFLICT UPDATE through a view that isn't just SELECT *.
    When expanding an updatable view that is an INSERT's target, the rewriter
    failed to rewrite Vars in the ON CONFLICT UPDATE clause.  This accidentally
    worked if the view was just "SELECT * FROM ...", as the transformation
    would be a no-op in that case.  With more complicated view targetlists,
    this omission would often lead to "attribute ... has the wrong type" errors
    or even crashes, as reported by Mario De Frutos Dieguez.
    
    Fix by adding code to rewriteTargetView to fix up the data structure
    correctly.  The easiest way to update the exclRelTlist list is to rebuild
    it from scratch looking at the new target relation, so factor the code
    for that out of transformOnConflictClause to make it sharable.
    
    In passing, avoid duplicate permissions checks against the EXCLUDED
    pseudo-relation, and prevent useless view expansion of that relation's
    dummy RTE.  The latter is only known to happen (after this patch) in cases
    where the query would fail later due to not having any INSTEAD OF triggers
    for the view.  But by exactly that token, it would create an unintended
    and very poorly tested state of the query data structure, so it seems like
    a good idea to prevent it from happening at all.
    
    This has been broken since ON CONFLICT was introduced, so back-patch
    to 9.5.
    
    Dean Rasheed, based on an earlier patch by Amit Langote;
    comment-kibitzing and back-patching by me
    
    Discussion: https://postgr.es/m/CAFYwGJ0xfzy8jaK80hVN2eUWr6huce0RU8AgU04MGD00igqkTg@mail.gmail.com

--- src/include/parser/analyze.h
+++ src/include/parser/analyze.h
@@ -42,4 +42,7 @@ extern void applyLockingClause(Query *qry, Index rtindex,
 				   LockClauseStrength strength,
 				   LockWaitPolicy waitPolicy, bool pushedDown);
 
+extern List *BuildOnConflictExcludedTargetlist(Relation targetrel,
+								  Index exclRelIndex);
+
 #endif   /* ANALYZE_H */
