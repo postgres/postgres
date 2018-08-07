41db97399d6b5492cb4538680531f81f4711b0f4
Tom Lane <tgl@sss.pgh.pa.us>
2018-08-07 16:00:44 -0400

    Fix incorrect initialization of BackendActivityBuffer.
    Since commit c8e8b5a6e, this has been zeroed out using the wrong length.
    In practice the length would always be too small, leading to not zeroing
    the whole buffer rather than clobbering additional memory; and that's
    pretty harmless, both because shmem would likely start out as zeroes
    and because we'd reinitialize any given entry before use.  Still,
    it's bogus, so fix it.
    
    Reported by Petru-Florin Mihancea (bug #15312)
    
    Discussion: https://postgr.es/m/153363913073.1303.6518849192351268091@wrigleys.postgresql.org

--- src/backend/postmaster/pgstat.c
+++ src/backend/postmaster/pgstat.c
@@ -2695,7 +2695,7 @@ CreateSharedBackendStatus(void)
 
 	if (!found)
 	{
-		MemSet(BackendActivityBuffer, 0, size);
+		MemSet(BackendActivityBuffer, 0, BackendActivityBufferSize);
 
 		/* Initialize st_activity pointers. */
 		buffer = BackendActivityBuffer;
