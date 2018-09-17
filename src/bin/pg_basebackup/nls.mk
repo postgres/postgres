bcbd1590279c1dfc3e6756a09166943437071bd2
Peter Eisentraut <peter_e@gmx.net>
2018-09-17 08:40:36 +0200

    Translation updates
    Source-Git-URL: https://git.postgresql.org/git/pgtranslation/messages.git
    Source-Git-Hash: be9925199917aac824dd4b472bdce3b97dbc90ca

--- src/bin/pg_basebackup/nls.mk
+++ src/bin/pg_basebackup/nls.mk
@@ -1,5 +1,5 @@
 # src/bin/pg_basebackup/nls.mk
 CATALOG_NAME     = pg_basebackup
-AVAIL_LANGUAGES  = cs de es fr he it ja ko pl pt_BR ru sv tr vi zh_CN
+AVAIL_LANGUAGES  = cs de es fr he it ja ko pl ru sv tr vi
 GETTEXT_FILES    = pg_basebackup.c pg_receivewal.c pg_recvlogical.c receivelog.c streamutil.c walmethods.c ../../common/fe_memutils.c ../../common/file_utils.c
 GETTEXT_TRIGGERS = simple_prompt tar_set_error
--- src/bin/pg_verify_checksums/nls.mk
+++ src/bin/pg_verify_checksums/nls.mk
@@ -1,4 +1,4 @@
 # src/bin/pg_verify_checksums/nls.mk
 CATALOG_NAME     = pg_verify_checksums
-AVAIL_LANGUAGES  =
+AVAIL_LANGUAGES  =de ja ko sv tr
 GETTEXT_FILES    = pg_verify_checksums.c
