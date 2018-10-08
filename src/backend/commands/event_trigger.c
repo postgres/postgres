6b6b59b38e4407c7cc2ed860bfafc721fe24ec7e
Alvaro Herrera <alvherre@alvh.no-ip.org>
2018-10-08 10:37:21 -0300

    Silence compiler warning in Assert()
    gcc 6.3 does not whine about this mistake I made in 39808e8868c8 but
    evidently lots of other compilers do, according to Michael Paquier,
    Peter Eisentraut, Arthur Zakirov, Tomas Vondra.
    
    Discussion: too many to list

--- src/backend/commands/event_trigger.c
+++ src/backend/commands/event_trigger.c
@@ -1790,7 +1790,7 @@ EventTriggerCollectAlterTableSubcmd(Node *subcmd, ObjectAddress address)
 		return;
 
 	Assert(IsA(subcmd, AlterTableCmd));
-	Assert(OidIsValid(currentEventTriggerState->currentCommand));
+	Assert(currentEventTriggerState->currentCommand != NULL);
 	Assert(OidIsValid(currentEventTriggerState->currentCommand->d.alterTable.objectId));
 
 	oldcxt = MemoryContextSwitchTo(currentEventTriggerState->cxt);
