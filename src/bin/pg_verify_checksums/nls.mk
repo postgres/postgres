69ff26b96f6f1ea947bd52d18f21e99fd61c25ec
Peter Eisentraut <peter_e@gmx.net>
2018-10-08 12:03:54 +0200

    Translation updates
    Source-Git-URL: https://git.postgresql.org/git/pgtranslation/messages.git
    Source-Git-Hash: 64b916c6c8a34d9e6aad88e78cc2356a941f1335

--- src/bin/pg_verify_checksums/nls.mk
+++ src/bin/pg_verify_checksums/nls.mk
@@ -1,4 +1,4 @@
 # src/bin/pg_verify_checksums/nls.mk
 CATALOG_NAME     = pg_verify_checksums
-AVAIL_LANGUAGES  =de ja ko sv tr
+AVAIL_LANGUAGES  =de fr ja ko ru sv tr
 GETTEXT_FILES    = pg_verify_checksums.c
