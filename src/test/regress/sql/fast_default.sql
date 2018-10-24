ef4583238b9ceaca728a27aea711a5f467dc49d5
Andrew Dunstan <andrew@dunslane.net>
2018-10-24 19:39:50 -0400

    Fix typo in regression test comment
    per Michael Banck

--- src/test/regress/sql/fast_default.sql
+++ src/test/regress/sql/fast_default.sql
@@ -472,7 +472,7 @@ SELECT * FROM t;
 DROP TABLE t;
 
 -- make sure expanded tuple has correct self pointer
--- it will be required by the RI tigger doing the cascading delete
+-- it will be required by the RI trigger doing the cascading delete
 
 CREATE TABLE leader (a int PRIMARY KEY, b int);
 CREATE TABLE follower (a int REFERENCES leader ON DELETE CASCADE, b int);
