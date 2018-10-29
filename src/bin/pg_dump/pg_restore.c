2fe42baf7c1ad96b5f9eb898161e258315298351
Peter Eisentraut <peter_e@gmx.net>
2018-10-29 11:31:43 +0100

    pg_restore: Augment documentation for -N option
    This was forgotten when the option was added.
    
    Author: Michael Banck <michael.banck@credativ.de>

--- src/bin/pg_dump/pg_restore.c
+++ src/bin/pg_dump/pg_restore.c
@@ -506,7 +506,7 @@ usage(const char *progname)
 	printf(_("  --role=ROLENAME          do SET ROLE before restore\n"));
 
 	printf(_("\n"
-			 "The options -I, -n, -P, -t, -T, and --section can be combined and specified\n"
+			 "The options -I, -n, -N, -P, -t, -T, and --section can be combined and specified\n"
 			 "multiple times to select multiple objects.\n"));
 	printf(_("\nIf no input file name is supplied, then standard input is used.\n\n"));
 	printf(_("Report bugs to <pgsql-bugs@postgresql.org>.\n"));
