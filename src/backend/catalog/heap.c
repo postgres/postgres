277602dfee51489994d29d889592fcff9f73bf73
Tom Lane <tgl@sss.pgh.pa.us>
2018-11-09 20:42:03 -0500

    Fix missing role dependencies for some schema and type ACLs.
    This patch fixes several related cases in which pg_shdepend entries were
    never made, or were lost, for references to roles appearing in the ACLs of
    schemas and/or types.  While that did no immediate harm, if a referenced
    role were later dropped, the drop would be allowed and would leave a
    dangling reference in the object's ACL.  That still wasn't a big problem
    for normal database usage, but it would cause obscure failures in
    subsequent dump/reload or pg_upgrade attempts, taking the form of
    attempts to grant privileges to all-numeric role names.  (I think I've
    seen field reports matching that symptom, but can't find any right now.)
    
    Several cases are fixed here:
    
    1. ALTER DOMAIN SET/DROP DEFAULT would lose the dependencies for any
    existing ACL entries for the domain.  This case is ancient, dating
    back as far as we've had pg_shdepend tracking at all.
    
    2. If a default type privilege applies, CREATE TYPE recorded the
    ACL properly but forgot to install dependency entries for it.
    This dates to the addition of default privileges for types in 9.2.
    
    3. If a default schema privilege applies, CREATE SCHEMA recorded the
    ACL properly but forgot to install dependency entries for it.
    This dates to the addition of default privileges for schemas in v10
    (commit ab89e465c).
    
    Another somewhat-related problem is that when creating a relation
    rowtype or implicit array type, TypeCreate would apply any available
    default type privileges to that type, which we don't really want
    since such an object isn't supposed to have privileges of its own.
    (You can't, for example, drop such privileges once they've been added
    to an array type.)
    
    ab89e465c is also to blame for a race condition in the regression tests:
    privileges.sql transiently installed globally-applicable default
    privileges on schemas, which sometimes got absorbed into the ACLs of
    schemas created by concurrent test scripts.  This should have resulted
    in failures when privileges.sql tried to drop the role holding such
    privileges; but thanks to the bug fixed here, it instead led to dangling
    ACLs in the final state of the regression database.  We'd managed not to
    notice that, but it became obvious in the wake of commit da906766c, which
    allowed the race condition to occur in pg_upgrade tests.
    
    To fix, add a function recordDependencyOnNewAcl to encapsulate what
    callers of get_user_default_acl need to do; while the original call
    sites got that right via ad-hoc code, none of the later-added ones
    have.  Also change GenerateTypeDependencies to generate these
    dependencies, which requires adding the typacl to its parameter list.
    (That might be annoying if there are any extensions calling that
    function directly; but if there are, they're most likely buggy in the
    same way as the core callers were, so they need work anyway.)  While
    I was at it, I changed GenerateTypeDependencies to accept most of its
    parameters in the form of a Form_pg_type pointer, making its parameter
    list a bit less unwieldy and mistake-prone.
    
    The test race condition is fixed just by wrapping the addition and
    removal of default privileges into a single transaction, so that that
    state is never visible externally.  We might eventually prefer to
    separate out tests of default privileges into a script that runs by
    itself, but that would be a bigger change and would make the tests
    run slower overall.
    
    Back-patch relevant parts to all supported branches.
    
    Discussion: https://postgr.es/m/15719.1541725287@sss.pgh.pa.us

--- src/backend/catalog/heap.c
+++ src/backend/catalog/heap.c
@@ -1287,12 +1287,15 @@ heap_create_with_catalog(const char *relname,
 		myself.classId = RelationRelationId;
 		myself.objectId = relid;
 		myself.objectSubId = 0;
+
 		referenced.classId = NamespaceRelationId;
 		referenced.objectId = relnamespace;
 		referenced.objectSubId = 0;
 		recordDependencyOn(&myself, &referenced, DEPENDENCY_NORMAL);
 
 		recordDependencyOnOwner(RelationRelationId, relid, ownerid);
+
+		recordDependencyOnNewAcl(RelationRelationId, relid, 0, ownerid, relacl);
 
 		if (relpersistence != RELPERSISTENCE_TEMP)
 			recordDependencyOnCurrentExtension(&myself, false);
@@ -1303,18 +1306,6 @@ heap_create_with_catalog(const char *relname,
 			referenced.objectId = reloftypeid;
 			referenced.objectSubId = 0;
 			recordDependencyOn(&myself, &referenced, DEPENDENCY_NORMAL);
-		}
-
-		if (relacl != NULL)
-		{
-			int			nnewmembers;
-			Oid		   *newmembers;
-
-			nnewmembers = aclmembers(relacl, &newmembers);
-			updateAclDependencies(RelationRelationId, relid, 0,
-								  ownerid,
-								  0, NULL,
-								  nnewmembers, newmembers);
 		}
 	}
 
