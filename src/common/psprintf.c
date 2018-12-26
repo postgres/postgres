4f7ab73106a0782fde22f7f405ef36954cb40eb5
Tom Lane <tgl@sss.pgh.pa.us>
2018-12-26 15:30:10 -0500

    Fix portability failure introduced in commits d2b0b60e7 et al.
    I made a frontend fprintf() format use %m, forgetting that that's only
    safe in HEAD not the back branches; prior to 96bf88d52 and d6c55de1f,
    it would work on glibc platforms but not elsewhere.  Revert to using
    %s ... strerror(errno) as the code did before.
    
    We could have left HEAD as-is, but for code consistency across branches,
    I chose to apply this patch there too.
    
    Per Coverity and a few buildfarm members.

--- src/common/psprintf.c
+++ src/common/psprintf.c
@@ -131,7 +131,8 @@ pvsnprintf(char *buf, size_t len, const char *fmt, va_list args)
 #ifndef FRONTEND
 		elog(ERROR, "vsnprintf failed: %m with format string \"%s\"", fmt);
 #else
-		fprintf(stderr, "vsnprintf failed: %m with format string \"%s\"\n", fmt);
+		fprintf(stderr, "vsnprintf failed: %s with format string \"%s\"\n",
+				strerror(errno), fmt);
 		exit(EXIT_FAILURE);
 #endif
 	}
