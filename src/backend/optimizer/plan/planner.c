4f80974990dce96771ef0e44c45999d8cc029c22
Tom Lane <tgl@sss.pgh.pa.us>
2019-01-11 15:53:34 -0500

    Avoid sharing PARAM_EXEC slots between different levels of NestLoop.
    Up to now, createplan.c attempted to share PARAM_EXEC slots for
    NestLoopParams across different plan levels, if the same underlying Var
    was being fed down to different righthand-side subplan trees by different
    NestLoops.  This was, I think, more of an artifact of using subselect.c's
    PlannerParamItem infrastructure than an explicit design goal, but anyway
    that was the end result.
    
    This works well enough as long as the plan tree is executing synchronously,
    but the feature whereby Gather can execute the parallelized subplan locally
    breaks it.  An upper NestLoop node might execute for a row retrieved from
    a parallel worker, and assign a value for a PARAM_EXEC slot from that row,
    while the leader's copy of the parallelized subplan is suspended with a
    different active value of the row the Var comes from.  When control
    eventually returns to the leader's subplan, it gets the wrong answers if
    the same PARAM_EXEC slot is being used within the subplan, as reported
    in bug #15577 from Bartosz Polnik.
    
    This is pretty reminiscent of the problem fixed in commit 46c508fbc, and
    the proper fix seems to be the same: don't try to share PARAM_EXEC slots
    across different levels of controlling NestLoop nodes.
    
    This requires decoupling NestLoopParam handling from PlannerParamItem
    handling, although the logic remains somewhat similar.  To avoid bizarre
    division of labor between subselect.c and createplan.c, I decided to move
    all the param-slot-assignment logic for both cases out of those files
    and put it into a new file paramassign.c.  Hopefully it's a bit better
    documented now, too.
    
    A regression test case for this might be nice, but we don't know a
    test case that triggers the problem with a suitably small amount
    of data.
    
    Back-patch to 9.6 where we added Gather nodes.  It's conceivable that
    related problems exist in older branches; but without some evidence
    for that, I'll leave the older branches alone.
    
    Discussion: https://postgr.es/m/15577-ca61ab18904af852@postgresql.org

--- src/backend/optimizer/plan/planner.c
+++ src/backend/optimizer/plan/planner.c
@@ -36,6 +36,7 @@
 #endif
 #include "optimizer/clauses.h"
 #include "optimizer/cost.h"
+#include "optimizer/paramassign.h"
 #include "optimizer/pathnode.h"
 #include "optimizer/paths.h"
 #include "optimizer/plancat.h"
@@ -466,7 +467,7 @@ subquery_planner(PlannerGlobal *glob, Query *parse,
 	root->hasInheritedTarget = false;
 	root->hasRecursion = hasRecursion;
 	if (hasRecursion)
-		root->wt_param_id = SS_assign_special_param(root);
+		root->wt_param_id = assign_special_exec_param(root);
 	else
 		root->wt_param_id = -1;
 	root->non_recursive_path = NULL;
@@ -1362,7 +1363,7 @@ inheritance_planner(PlannerInfo *root)
 									 returningLists,
 									 rowMarks,
 									 NULL,
-									 SS_assign_special_param(root)));
+									 assign_special_exec_param(root)));
 }
 
 /*--------------------
@@ -1955,7 +1956,7 @@ grouping_planner(PlannerInfo *root, bool inheritance_update,
 		{
 			path = (Path *) create_lockrows_path(root, final_rel, path,
 												 root->rowMarks,
-											  SS_assign_special_param(root));
+												 assign_special_exec_param(root));
 		}
 
 		/*
@@ -2015,7 +2016,7 @@ grouping_planner(PlannerInfo *root, bool inheritance_update,
 										returningLists,
 										rowMarks,
 										parse->onConflict,
-										SS_assign_special_param(root));
+										assign_special_exec_param(root));
 		}
 
 		/* And shove it into final_rel */
