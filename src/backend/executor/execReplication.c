5279c8d097a1346e3dddbed7c0002c735aa23f73
Peter Eisentraut <peter@eisentraut.org>
2019-01-19 09:34:24 +0100

    Fix outdated comment
    The issue the comment is referring to was fixed by
    08859bb5c2cebc132629ca838113d27bb31b990c.

--- src/backend/executor/execReplication.c
+++ src/backend/executor/execReplication.c
@@ -219,11 +219,6 @@ retry:
 
 /*
  * Compare the tuple and slot and check if they have equal values.
- *
- * We use binary datum comparison which might return false negatives but
- * that's the best we can do here as there may be multiple notions of
- * equality for the data types and table columns don't specify which one
- * to use.
  */
 static bool
 tuple_equals_slot(TupleDesc desc, HeapTuple tup, TupleTableSlot *slot)
