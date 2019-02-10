73668c590cf5c5719ac0d0481201fd4fd79454bb
Tom Lane <tgl@sss.pgh.pa.us>
2019-02-09 19:45:38 -0500

    Repair unsafe/unportable snprintf usage in pg_restore.
    warn_or_exit_horribly() was blithely passing a potentially-NULL
    string pointer to a %s format specifier.  That works (at least
    to the extent of not crashing) on some platforms, but not all,
    and since we switched to our own snprintf.c it doesn't work
    for us anywhere.
    
    Of the three string fields being handled this way here, I think
    that only "owner" is supposed to be nullable ... but considering
    that this is error-reporting code, it has very little business
    assuming anything, so put in defenses for all three.
    
    Per a crash observed on buildfarm member crake and then
    reproduced here.  Because of the portability aspect,
    back-patch to all supported versions.

--- src/bin/pg_dump/pg_backup_archiver.c
+++ src/bin/pg_dump/pg_backup_archiver.c
@@ -1796,8 +1796,11 @@ warn_or_exit_horribly(ArchiveHandle *AH,
 	{
 		write_msg(modulename, "Error from TOC entry %d; %u %u %s %s %s\n",
 				  AH->currentTE->dumpId,
-				  AH->currentTE->catalogId.tableoid, AH->currentTE->catalogId.oid,
-				  AH->currentTE->desc, AH->currentTE->tag, AH->currentTE->owner);
+				  AH->currentTE->catalogId.tableoid,
+				  AH->currentTE->catalogId.oid,
+				  AH->currentTE->desc ? AH->currentTE->desc : "(no desc)",
+				  AH->currentTE->tag ? AH->currentTE->tag : "(no tag)",
+				  AH->currentTE->owner ? AH->currentTE->owner : "(no owner)");
 	}
 	AH->lastErrorStage = AH->stage;
 	AH->lastErrorTE = AH->currentTE;
