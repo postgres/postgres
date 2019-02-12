postgres/postgres

commit fa2cf164aaf91e074be653c28e035f65d80eb666

Rename nodes/relation.h to nodes/pathnodes.h.
The old name of this file was never a very good indication of what it
was for.  Now that there's also access/relation.h, we have a potential
confusion hazard as well, so let's rename it to something more apropos.
Per discussion, "pathnodes.h" is reasonable, since a good fraction of
the file is Path node definitions.

While at it, tweak a couple of other headers that were gratuitously
importing relation.h into modules that don't need it.

Discussion: https://postgr.es/m/7719.1548688728@sss.pgh.pa.us

src/include/partitioning/partprune.h
@@ -15,7 +15,8 @@
 #define PARTPRUNE_H
 
 #include "nodes/execnodes.h"
-#include "nodes/relation.h"
+struct PlannerInfo;				/* avoid including pathnodes.h here */
+struct RelOptInfo;
 
 
 /*
@@ -71,12 +72,12 @@ typedef struct PartitionPruneContext
 #define PruneCxtStateIdx(partnatts, step_id, keyno) \
 	((partnatts) * (step_id) + (keyno))
 
-extern PartitionPruneInfo *make_partition_pruneinfo(PlannerInfo *root,
-						 RelOptInfo *parentrel,
+extern PartitionPruneInfo *make_partition_pruneinfo(struct PlannerInfo *root,
+						 struct RelOptInfo *parentrel,
 						 List *subpaths,
 						 List *partitioned_rels,
 						 List *prunequal);
-extern Relids prune_append_rel_partitions(RelOptInfo *rel);
+extern Bitmapset *prune_append_rel_partitions(struct RelOptInfo *rel);
 extern Bitmapset *get_matching_partitions(PartitionPruneContext *context,
 						List *pruning_steps);
 

contrib/bloom/bloom.h
@@ -17,7 +17,7 @@
 #include "access/generic_xlog.h"
 #include "access/itup.h"
 #include "access/xlog.h"
-#include "nodes/relation.h"
+#include "nodes/pathnodes.h"
 #include "fmgr.h"
 
 /* Support procedures numbers */

contrib/postgres_fdw/postgres_fdw.h
@@ -15,7 +15,7 @@
 
 #include "foreign/foreign.h"
 #include "lib/stringinfo.h"
-#include "nodes/relation.h"
+#include "nodes/pathnodes.h"
 #include "utils/relcache.h"
 
 #include "libpq-fe.h"

src/backend/executor/execAmi.c
@@ -56,8 +56,9 @@
 #include "executor/nodeValuesscan.h"
 #include "executor/nodeWindowAgg.h"
 #include "executor/nodeWorktablescan.h"
+#include "nodes/extensible.h"
 #include "nodes/nodeFuncs.h"
-#include "nodes/relation.h"
+#include "nodes/pathnodes.h"
 #include "utils/rel.h"
 #include "utils/syscache.h"
 

src/backend/executor/nodeCustom.c
@@ -14,6 +14,7 @@
 #include "executor/executor.h"
 #include "executor/nodeCustom.h"
 #include "nodes/execnodes.h"
+#include "nodes/extensible.h"
 #include "nodes/plannodes.h"
 #include "miscadmin.h"
 #include "parser/parsetree.h"

src/backend/nodes/README
@@ -36,8 +36,8 @@ FILES IN src/include/nodes/
 	nodes.h		- define node tags (NodeTag)
 	primnodes.h	- primitive nodes
 	parsenodes.h	- parse tree nodes
+	pathnodes.h	- path tree nodes and planner internal structures
 	plannodes.h	- plan tree nodes
-	relation.h	- planner internal nodes
 	execnodes.h	- executor nodes
 	memnodes.h	- memory nodes
 	pg_list.h	- generic list

src/backend/nodes/copyfuncs.c
@@ -24,8 +24,8 @@
 
 #include "miscadmin.h"
 #include "nodes/extensible.h"
+#include "nodes/pathnodes.h"
 #include "nodes/plannodes.h"
-#include "nodes/relation.h"
 #include "utils/datum.h"
 #include "utils/rel.h"
 
@@ -2196,7 +2196,7 @@ _copyOnConflictExpr(const OnConflictExpr *from)
 }
 
 /* ****************************************************************
- *						relation.h copy functions
+ *						pathnodes.h copy functions
  *
  * We don't support copying RelOptInfo, IndexOptInfo, or Path nodes.
  * There are some subsidiary structs that are useful to copy, though.

src/backend/nodes/equalfuncs.c
@@ -31,7 +31,7 @@
 
 #include "miscadmin.h"
 #include "nodes/extensible.h"
-#include "nodes/relation.h"
+#include "nodes/pathnodes.h"
 #include "utils/datum.h"
 
 
@@ -814,7 +814,7 @@ _equalOnConflictExpr(const OnConflictExpr *a, const OnConflictExpr *b)
 }
 
 /*
- * Stuff from relation.h
+ * Stuff from pathnodes.h
  */
 
 static bool

commit b07c695d9c34cccfa0138ca7f4c76547a24c74e1

Fix header inclusion issue.
partprune.h failed to compile by itself; needs to include partdefs.h.

I think I must've broken this in fa2cf164a, though I'd swear I ran
the appropriate tests when removing #includes.  Anyway, it's very
sensible for this file to include partdefs.h, so let's just do that.

Per cpluspluscheck.

src/include/partitioning/partprune.h
@@ -15,6 +15,8 @@
 #define PARTPRUNE_H
 
 #include "nodes/execnodes.h"
+#include "partitioning/partdefs.h"
+
 struct PlannerInfo;				/* avoid including pathnodes.h here */
 struct RelOptInfo;
 