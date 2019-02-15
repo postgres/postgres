d08fae3773ea3b9930d3efc0429a38614c2dd706
Michael Paquier <michael@paquier.xyz>
2019-02-15 17:12:46 +0900

    Fix support for CREATE TABLE IF NOT EXISTS AS EXECUTE
    The grammar IF NOT EXISTS for CTAS is supported since 9.5 and documented
    as such, however the case of using EXECUTE as query has never been
    covered as EXECUTE CTAS statements and normal CTAS statements are parsed
    separately.
    
    Author: Andreas Karlsson
    Discussion: https://postgr.es/m/2ddcc188-e37c-a0be-32bf-a56b07c3559e@proxel.se
    Backpatch-through: 9.5

--- src/test/regress/sql/create_table.sql
+++ src/test/regress/sql/create_table.sql
@@ -266,6 +266,14 @@ CREATE TABLE as_select1 AS SELECT * FROM pg_class WHERE relkind = 'r';
 CREATE TABLE IF NOT EXISTS as_select1 AS SELECT * FROM pg_class WHERE relkind = 'r';
 DROP TABLE as_select1;
 
+PREPARE select1 AS SELECT 1 as a;
+CREATE TABLE as_select1 AS EXECUTE select1;
+CREATE TABLE as_select1 AS EXECUTE select1;
+SELECT * FROM as_select1;
+CREATE TABLE IF NOT EXISTS as_select1 AS EXECUTE select1;
+DROP TABLE as_select1;
+DEALLOCATE select1;
+
 -- check that the oid column is added before the primary key is checked
 CREATE TABLE oid_pk (f1 INT, PRIMARY KEY(oid)) WITH OIDS;
 DROP TABLE oid_pk;
