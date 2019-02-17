4eca1905d34b34a48b80553262edc81048fd7dfe
Tom Lane <tgl@sss.pgh.pa.us>
2019-02-17 12:37:31 -0500

    Fix CREATE VIEW to allow zero-column views.
    We should logically have allowed this case when we allowed zero-column
    tables, but it was overlooked.
    
    Although this might be thought a feature addition, it's really a bug
    fix, because it was possible to create a zero-column view via
    the convert-table-to-view code path, and then you'd have a situation
    where dump/reload would fail.  Hence, back-patch to all supported
    branches.
    
    Arrange the added test cases to provide coverage of the related
    pg_dump code paths (since these views will be dumped and reloaded
    during the pg_upgrade regression test).  I also made them test
    the case where pg_dump has to postpone the view rule into post-data,
    which disturbingly had no regression coverage before.
    
    Report and patch by Ashutosh Sharma (test case by me)
    
    Discussion: https://postgr.es/m/CAE9k0PkmHdeSaeZt2ujnb_cKucmK3sDDceDzw7+d5UZoNJPYOg@mail.gmail.com

--- src/test/regress/sql/create_view.sql
+++ src/test/regress/sql/create_view.sql
@@ -24,6 +24,18 @@ COMMENT ON VIEW noview IS 'no view';
 COMMENT ON VIEW toyemp IS 'is a view';
 COMMENT ON VIEW toyemp IS NULL;
 
+-- These views are left around mainly to exercise special cases in pg_dump.
+
+CREATE TABLE view_base_table (key int PRIMARY KEY, data varchar(20));
+
+CREATE VIEW key_dependent_view AS
+   SELECT * FROM view_base_table GROUP BY key;
+
+ALTER TABLE view_base_table DROP CONSTRAINT view_base_table_pkey;  -- fails
+
+CREATE VIEW key_dependent_view_no_cols AS
+   SELECT FROM view_base_table GROUP BY key HAVING length(data) > 0;
+
 --
 -- CREATE OR REPLACE VIEW
 --
