7c414cdc39a3f183ff398be4cfcef0646f419281
Noah Misch <noah@leadboat.com>
2019-04-03 17:03:46 -0700

    Consistently test for in-use shared memory.
    postmaster startup scrutinizes any shared memory segment recorded in
    postmaster.pid, exiting if that segment matches the current data
    directory and has an attached process.  When the postmaster.pid file was
    missing, a starting postmaster used weaker checks.  Change to use the
    same checks in both scenarios.  This increases the chance of a startup
    failure, in lieu of data corruption, if the DBA does "kill -9 `head -n1
    postmaster.pid` && rm postmaster.pid && pg_ctl -w start".  A postmaster
    will no longer recycle segments pertaining to other data directories.
    That's good for production, but it's bad for integration tests that
    crash a postmaster and immediately delete its data directory.  Such a
    test now leaks a segment indefinitely.  No "make check-world" test does
    that.  win32_shmem.c already avoided all these problems.  In 9.6 and
    later, enhance PostgresNode to facilitate testing.  Back-patch to 9.4
    (all supported versions).
    
    Reviewed by Daniel Gustafsson and Kyotaro HORIGUCHI.
    
    Discussion: https://postgr.es/m/20130911033341.GD225735@tornado.leadboat.com

--- src/backend/storage/ipc/ipci.c
+++ src/backend/storage/ipc/ipci.c
@@ -88,12 +88,9 @@ RequestAddinShmemSpace(Size size)
  * through the same code as before.  (Note that the called routines mostly
  * check IsUnderPostmaster, rather than EXEC_BACKEND, to detect this case.
  * This is a bit code-wasteful and could be cleaned up.)
- *
- * If "makePrivate" is true then we only need private memory, not shared
- * memory.  This is true for a standalone backend, false for a postmaster.
  */
 void
-CreateSharedMemoryAndSemaphores(bool makePrivate, int port)
+CreateSharedMemoryAndSemaphores(int port)
 {
 	PGShmemHeader *shim = NULL;
 
@@ -166,7 +163,7 @@ CreateSharedMemoryAndSemaphores(bool makePrivate, int port)
 		/*
 		 * Create the shmem segment
 		 */
-		seghdr = PGSharedMemoryCreate(size, makePrivate, port, &shim);
+		seghdr = PGSharedMemoryCreate(size, port, &shim);
 
 		InitShmemAccess(seghdr);
 
@@ -187,12 +184,9 @@ CreateSharedMemoryAndSemaphores(bool makePrivate, int port)
 	{
 		/*
 		 * We are reattaching to an existing shared memory segment. This
-		 * should only be reached in the EXEC_BACKEND case, and even then only
-		 * with makePrivate == false.
+		 * should only be reached in the EXEC_BACKEND case.
 		 */
-#ifdef EXEC_BACKEND
-		Assert(!makePrivate);
-#else
+#ifndef EXEC_BACKEND
 		elog(PANIC, "should be attached to shared memory already");
 #endif
 	}
