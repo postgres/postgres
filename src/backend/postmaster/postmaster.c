0777696fe346dbd3b20814acce08dd93b512afcb
Noah Misch <noah@leadboat.com>
2019-04-05 00:00:52 -0700

    Revert "Consistently test for in-use shared memory."
    This reverts commits 2f932f71d9f2963bbd201129d7b971c8f5f077fd,
    16ee6eaf80a40007a138b60bb5661660058d0422 and
    6f0e190056fe441f7cf788ff19b62b13c94f68f3.  The buildfarm has revealed
    several bugs.  Back-patch like the original commits.
    
    Discussion: https://postgr.es/m/20190404145319.GA1720877@rfd.leadboat.com

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -2510,7 +2510,7 @@ reset_shared(int port)
 	 * determine IPC keys.  This helps ensure that we will clean up dead IPC
 	 * objects if the postmaster crashes and is restarted.
 	 */
-	CreateSharedMemoryAndSemaphores(port);
+	CreateSharedMemoryAndSemaphores(false, port);
 }
 
 
@@ -4867,7 +4867,7 @@ SubPostmasterMain(int argc, char *argv[])
 		InitProcess();
 
 		/* Attach process to shared data structures */
-		CreateSharedMemoryAndSemaphores(0);
+		CreateSharedMemoryAndSemaphores(false, 0);
 
 		/* And run the backend */
 		BackendRun(&port);		/* does not return */
@@ -4881,7 +4881,7 @@ SubPostmasterMain(int argc, char *argv[])
 		InitAuxiliaryProcess();
 
 		/* Attach process to shared data structures */
-		CreateSharedMemoryAndSemaphores(0);
+		CreateSharedMemoryAndSemaphores(false, 0);
 
 		AuxiliaryProcessMain(argc - 2, argv + 2);		/* does not return */
 	}
@@ -4894,7 +4894,7 @@ SubPostmasterMain(int argc, char *argv[])
 		InitProcess();
 
 		/* Attach process to shared data structures */
-		CreateSharedMemoryAndSemaphores(0);
+		CreateSharedMemoryAndSemaphores(false, 0);
 
 		AutoVacLauncherMain(argc - 2, argv + 2);		/* does not return */
 	}
@@ -4907,7 +4907,7 @@ SubPostmasterMain(int argc, char *argv[])
 		InitProcess();
 
 		/* Attach process to shared data structures */
-		CreateSharedMemoryAndSemaphores(0);
+		CreateSharedMemoryAndSemaphores(false, 0);
 
 		AutoVacWorkerMain(argc - 2, argv + 2);	/* does not return */
 	}
@@ -4925,7 +4925,7 @@ SubPostmasterMain(int argc, char *argv[])
 		InitProcess();
 
 		/* Attach process to shared data structures */
-		CreateSharedMemoryAndSemaphores(0);
+		CreateSharedMemoryAndSemaphores(false, 0);
 
 		shmem_slot = atoi(argv[1] + 15);
 		MyBgworkerEntry = BackgroundWorkerEntry(shmem_slot);
