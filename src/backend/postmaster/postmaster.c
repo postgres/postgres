3ef5e16c618177160bc5f95216dccab3588defdc
Noah Misch <noah@leadboat.com>
2019-04-12 22:36:38 -0700

    Consistently test for in-use shared memory.
    postmaster startup scrutinizes any shared memory segment recorded in
    postmaster.pid, exiting if that segment matches the current data
    directory and has an attached process.  When the postmaster.pid file was
    missing, a starting postmaster used weaker checks.  Change to use the
    same checks in both scenarios.  This increases the chance of a startup
    failure, in lieu of data corruption, if the DBA does "kill -9 `head -n1
    postmaster.pid` && rm postmaster.pid && pg_ctl -w start".  A postmaster
    will no longer stop if shmat() of an old segment fails with EACCES.  A
    postmaster will no longer recycle segments pertaining to other data
    directories.  That's good for production, but it's bad for integration
    tests that crash a postmaster and immediately delete its data directory.
    Such a test now leaks a segment indefinitely.  No "make check-world"
    test does that.  win32_shmem.c already avoided all these problems.  In
    9.6 and later, enhance PostgresNode to facilitate testing.  Back-patch
    to 9.4 (all supported versions).
    
    Reviewed (in earlier versions) by Daniel Gustafsson and Kyotaro HORIGUCHI.
    
    Discussion: https://postgr.es/m/20190408064141.GA2016666@rfd.leadboat.com

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -2511,7 +2511,7 @@ reset_shared(int port)
 	 * determine IPC keys.  This helps ensure that we will clean up dead IPC
 	 * objects if the postmaster crashes and is restarted.
 	 */
-	CreateSharedMemoryAndSemaphores(false, port);
+	CreateSharedMemoryAndSemaphores(port);
 }
 
 
@@ -4868,7 +4868,7 @@ SubPostmasterMain(int argc, char *argv[])
 		InitProcess();
 
 		/* Attach process to shared data structures */
-		CreateSharedMemoryAndSemaphores(false, 0);
+		CreateSharedMemoryAndSemaphores(0);
 
 		/* And run the backend */
 		BackendRun(&port);		/* does not return */
@@ -4882,7 +4882,7 @@ SubPostmasterMain(int argc, char *argv[])
 		InitAuxiliaryProcess();
 
 		/* Attach process to shared data structures */
-		CreateSharedMemoryAndSemaphores(false, 0);
+		CreateSharedMemoryAndSemaphores(0);
 
 		AuxiliaryProcessMain(argc - 2, argv + 2);		/* does not return */
 	}
@@ -4895,7 +4895,7 @@ SubPostmasterMain(int argc, char *argv[])
 		InitProcess();
 
 		/* Attach process to shared data structures */
-		CreateSharedMemoryAndSemaphores(false, 0);
+		CreateSharedMemoryAndSemaphores(0);
 
 		AutoVacLauncherMain(argc - 2, argv + 2);		/* does not return */
 	}
@@ -4908,7 +4908,7 @@ SubPostmasterMain(int argc, char *argv[])
 		InitProcess();
 
 		/* Attach process to shared data structures */
-		CreateSharedMemoryAndSemaphores(false, 0);
+		CreateSharedMemoryAndSemaphores(0);
 
 		AutoVacWorkerMain(argc - 2, argv + 2);	/* does not return */
 	}
@@ -4926,7 +4926,7 @@ SubPostmasterMain(int argc, char *argv[])
 		InitProcess();
 
 		/* Attach process to shared data structures */
-		CreateSharedMemoryAndSemaphores(false, 0);
+		CreateSharedMemoryAndSemaphores(0);
 
 		shmem_slot = atoi(argv[1] + 15);
 		MyBgworkerEntry = BackgroundWorkerEntry(shmem_slot);
