809c9b48f4bdd11dfc088f6d0b9a6c57936c32ca
Andres Freund <andres@anarazel.de>
2019-04-30 17:45:32 -0700

    Run catalog reindexing test from 3dbb317d32 serially, to avoid deadlocks.
    The tests turn out to cause deadlocks in some circumstances. Fairly
    reproducibly so with -DRELCACHE_FORCE_RELEASE
    -DCATCACHE_FORCE_RELEASE.  Some of the deadlocks may be hard to fix
    without disproportionate measures, but others probably should be fixed
    - but not in 12.
    
    We discussed removing the new tests until we can fix the issues
    underlying the deadlocks, but results from buildfarm animal
    markhor (which runs with CLOBBER_CACHE_ALWAYS) indicates that there
    might be a more severe, as of yet undiagnosed, issue (including on
    stable branches) with reindexing catalogs. The failure is:
    ERROR: could not read block 0 in file "base/16384/28025": read only 0 of 8192 bytes
    Therefore it seems advisable to keep the tests.
    
    It's not certain that running the tests in isolation removes the risk
    of deadlocks. It's possible that additional locks are needed to
    protect against a concurrent auto-analyze or such.
    
    Per discussion with Tom Lane.
    
    Discussion: https://postgr.es/m/28926.1556664156@sss.pgh.pa.us
    Backpatch: 9.4-, like 3dbb317d3

---
+++ src/test/regress/sql/reindex_catalog.sql
@@ -0,0 +1,36 @@
+--
+-- Check that system tables can be reindexed.
+--
+-- Note that this test currently has to run without parallel tests
+-- being scheduled, as currently reindex catalog tables can cause
+-- deadlocks:
+--
+-- * The lock upgrade between the ShareLock acquired for the reindex
+--   and RowExclusiveLock needed for pg_class/pg_index locks can
+--   trigger deadlocks.
+--
+-- * The uniqueness checks performed when reindexing a unique/primary
+--   key index possibly need to wait for the transaction of a
+--   about-to-deleted row in pg_class to commit. That can cause
+--   deadlocks because, in contrast to user tables, locks on catalog
+--   tables are routinely released before commit - therefore the lock
+--   held for reindexing doesn't guarantee that no running transaction
+--   performed modifications in the table underlying the index.
+
+
+-- Check reindexing of whole tables
+REINDEX TABLE pg_class; -- mapped, non-shared, critical
+REINDEX TABLE pg_index; -- non-mapped, non-shared, critical
+REINDEX TABLE pg_operator; -- non-mapped, non-shared, critical
+REINDEX TABLE pg_database; -- mapped, shared, critical
+REINDEX TABLE pg_shdescription; -- mapped, shared non-critical
+
+-- Check that individual system indexes can be reindexed. That's a bit
+-- different from the entire-table case because reindex_relation
+-- treats e.g. pg_class special.
+REINDEX INDEX pg_class_oid_index; -- mapped, non-shared, critical
+REINDEX INDEX pg_class_relname_nsp_index; -- mapped, non-shared, non-critical
+REINDEX INDEX pg_index_indexrelid_index; -- non-mapped, non-shared, critical
+REINDEX INDEX pg_index_indrelid_index; -- non-mapped, non-shared, non-critical
+REINDEX INDEX pg_database_oid_index; -- mapped, shared, critical
+REINDEX INDEX pg_shdescription_o_c_index; -- mapped, shared, non-critical
