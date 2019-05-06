dcbdd1a8d55a4fd8a2b318fd5a2647ba4dbb4275
Peter Eisentraut <peter@eisentraut.org>
2019-05-06 14:39:59 +0200

    Translation updates
    Source-Git-URL: https://git.postgresql.org/git/pgtranslation/messages.git
    Source-Git-Hash: 96d81aab04631d76c9ca90a3b12885100c061775

--- src/bin/pg_resetwal/nls.mk
+++ src/bin/pg_resetwal/nls.mk
@@ -1,4 +1,4 @@
 # src/bin/pg_resetwal/nls.mk
 CATALOG_NAME     = pg_resetwal
-AVAIL_LANGUAGES  = cs de es fr ja ko ru sv tr
+AVAIL_LANGUAGES  = cs de es fr ja ko ru sv tr zh_CN
 GETTEXT_FILES    = pg_resetwal.c ../../common/restricted_token.c
