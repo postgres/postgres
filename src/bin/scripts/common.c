af82f95abb23a56d18fd009ef9eca68ef803a041
Michael Paquier <michael@paquier.xyz>
2019-05-07 09:39:39 +0900

    Remove some code related to 7.3 and older servers from tools of src/bin/
    This code was broken as of 582edc3, and is most likely not used anymore.
    Note that pg_dump supports servers down to 8.0, and psql has code to
    support servers down to 7.4.
    
    Author: Julien Rouhaud
    Reviewed-by: Tom Lane
    Discussion: https://postgr.es/m/CAOBaU_Y5y=zo3+2gf+2NJC1pvMYPcbRXoQaPXx=U7+C8Qh4CzQ@mail.gmail.com

--- src/bin/scripts/common.c
+++ src/bin/scripts/common.c
@@ -146,9 +146,8 @@ connectDatabase(const char *dbname, const char *pghost,
 		exit(1);
 	}
 
-	if (PQserverVersion(conn) >= 70300)
-		PQclear(executeQuery(conn, ALWAYS_SECURE_SEARCH_PATH_SQL,
-							 progname, echo));
+	PQclear(executeQuery(conn, ALWAYS_SECURE_SEARCH_PATH_SQL,
+						 progname, echo));
 
 	return conn;
 }
@@ -307,13 +306,6 @@ appendQualifiedRelation(PQExpBuffer buf, const char *spec,
 	PQExpBufferData sql;
 	PGresult   *res;
 	int			ntups;
-
-	/* Before 7.3, the concept of qualifying a name did not exist. */
-	if (PQserverVersion(conn) < 70300)
-	{
-		appendPQExpBufferStr(&sql, spec);
-		return;
-	}
 
 	splitTableColumnsSpec(spec, PQclientEncoding(conn), &table, &columns);
 
