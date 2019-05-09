cd805f46d857291b26ba6eb491ce11b6e0fc9ad3
Peter Eisentraut <peter@eisentraut.org>
2019-05-09 09:15:59 +0200

    pg_controldata: Add common gettext flags
    So it picks up strings in pg_log_* calls.  This was forgotten when it
    was added to all other relevant subdirectories.

--- src/bin/pg_controldata/nls.mk
+++ src/bin/pg_controldata/nls.mk
@@ -2,3 +2,5 @@
 CATALOG_NAME     = pg_controldata
 AVAIL_LANGUAGES  = cs de es fr it ja ko pl pt_BR ru sv tr vi zh_CN
 GETTEXT_FILES    = pg_controldata.c ../../common/controldata_utils.c
+GETTEXT_TRIGGERS = $(FRONTEND_COMMON_GETTEXT_TRIGGERS)
+GETTEXT_FLAGS    = $(FRONTEND_COMMON_GETTEXT_FLAGS)
