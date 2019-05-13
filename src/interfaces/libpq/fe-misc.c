postgres/postgres

commit 4e86f1b16da85ba0a99c592dc7b977b3be80a483

Put SSL_pending() call behind the new internal SSL API.
It seems likely that any SSL implementation will need a similar call, not
just OpenSSL.

src/interfaces/libpq/fe-misc.c
@@ -1054,9 +1054,9 @@ pqSocketCheck(PGconn *conn, int forRead, int forWrite, time_t end_time)
 		return -1;
 	}
 
-#ifdef USE_OPENSSL
+#ifdef USE_SSL
 	/* Check for SSL library buffering read bytes */
-	if (forRead && conn->ssl && SSL_pending(conn->ssl) > 0)
+	if (forRead && conn->ssl_in_use && pgtls_read_pending(conn) > 0)
 	{
 		/* short-circuit the select */
 		return 1;

src/interfaces/libpq/fe-secure-openssl.c
@@ -187,6 +187,15 @@ pgtls_open_client(PGconn *conn)
 }
 
 /*
+ *  Is there unread data waiting in the SSL read buffer?
+ */
+bool
+pgtls_read_pending(PGconn *conn)
+{
+	return SSL_pending(conn->ssl);
+}
+
+/*
  *	Read data from a secure connection.
  *
  * On failure, this function is responsible for putting a suitable message

src/interfaces/libpq/libpq-int.h
@@ -641,6 +641,7 @@ extern int pgtls_init(PGconn *conn);
 extern PostgresPollingStatusType pgtls_open_client(PGconn *conn);
 extern void pgtls_close(PGconn *conn);
 extern ssize_t pgtls_read(PGconn *conn, void *ptr, size_t len);
+extern bool pgtls_read_pending(PGconn *conn);
 extern ssize_t pgtls_write(PGconn *conn, const void *ptr, size_t len);
 
 /*

commit 0ec3d2ab250ef696f5e32d4ff7779849bfb0770d

Fix misuse of an integer as a bool.
pgtls_read_pending is declared to return bool, but what the underlying
SSL_pending function returns is a count of available bytes.

This is actually somewhat harmless if we're using C99 bools, but in
the back branches it's a live bug: if the available-bytes count happened
to be a multiple of 256, it would get converted to a zero char value.
On machines where char is signed, counts of 128 and up could misbehave
as well.  The net effect is that when using SSL, libpq might block
waiting for data even though some has already been received.

Broken by careless refactoring in commit 4e86f1b16, so back-patch
to 9.5 where that came in.

Per bug #15802 from David Binderman.

Discussion: https://postgr.es/m/15802-f0911a97f0346526@postgresql.org

src/interfaces/libpq/fe-misc.c
@@ -1064,7 +1064,7 @@ pqSocketCheck(PGconn *conn, int forRead, int forWrite, time_t end_time)
 
 #ifdef USE_SSL
 	/* Check for SSL library buffering read bytes */
-	if (forRead && conn->ssl_in_use && pgtls_read_pending(conn) > 0)
+	if (forRead && conn->ssl_in_use && pgtls_read_pending(conn))
 	{
 		/* short-circuit the select */
 		return 1;

src/interfaces/libpq/fe-secure-openssl.c
@@ -152,7 +152,7 @@ pgtls_open_client(PGconn *conn)
 bool
 pgtls_read_pending(PGconn *conn)
 {
-	return SSL_pending(conn->ssl);
+	return SSL_pending(conn->ssl) > 0;
 }
 
 /*