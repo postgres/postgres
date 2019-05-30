39c9efc15646b3eb36d4095c3657405894dc3ea4
Tomas Vondra <tomas.vondra@postgresql.org>
2019-05-30 16:16:12 +0200

    Make error logging in extended statistics more consistent
    Most errors reported in extended statistics are internal issues, and so
    should use elog(). The MCV list code was already following this rule, but
    the functional dependencies and ndistinct coefficients were using a mix
    of elog() and ereport(). Fix this by changing most places to elog(), with
    the exception of input functions.
    
    This is a mostly cosmetic change, it makes the life a little bit easier
    for translators, as elog() messages are not translated. So backpatch to
    PostgreSQL 10, where extended statistics were introduced.
    
    Author: Tomas Vondra
    Backpatch-through: 10 where extended statistics were added
    Discussion: https://postgr.es/m/20190503154404.GA7478@alvherre.pgsql

--- src/backend/statistics/dependencies.c
+++ src/backend/statistics/dependencies.c
@@ -529,9 +529,7 @@ statext_dependencies_deserialize(bytea *data)
 			 dependencies->type, STATS_DEPS_TYPE_BASIC);
 
 	if (dependencies->ndeps == 0)
-		ereport(ERROR,
-				(errcode(ERRCODE_DATA_CORRUPTED),
-				 errmsg("invalid zero-length item array in MVDependencies")));
+		elog(ERROR, "invalid zero-length item array in MVDependencies");
 
 	/* what minimum bytea size do we expect for those parameters */
 	min_expected_size = SizeOfDependencies +
