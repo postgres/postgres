744639739c7c6a53bcbf4cd6fe765d7ebd89716a
Alvaro Herrera <alvherre@alvh.no-ip.org>
2019-06-14 11:33:40 -0400

    Silence compiler warning
    Introduced in de87a084c0a5.

--- src/backend/access/heap/heapam.c
+++ src/backend/access/heap/heapam.c
@@ -4610,7 +4610,7 @@ l3:
 		uint16		infomask;
 		uint16		infomask2;
 		bool		require_sleep;
-		bool		skip_tuple_lock;
+		bool		skip_tuple_lock = false;
 		ItemPointerData t_ctid;
 
 		/* must copy state data before unlocking buffer */
@@ -4636,7 +4636,6 @@ l3:
 		if (first_time)
 		{
 			first_time = false;
-			skip_tuple_lock = false;
 
 			if (infomask & HEAP_XMAX_IS_MULTI)
 			{
