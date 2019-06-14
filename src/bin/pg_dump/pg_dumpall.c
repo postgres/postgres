a193cbec1199e4e529c46645b7aecbf38956048b
Alvaro Herrera <alvherre@alvh.no-ip.org>
2019-06-14 18:21:52 -0400

    Add pg_dumpall --rows-per-insert
    Commit 7e413a0f82c8 added that option to pg_dump, but neglected to teach
    pg_dumpall how to pass it along.  Repair.
    
    Author: Fabien Coelho
    Reported-by: Peter Eisentraut
    Reviewed-by: David Rowley
    Discussion: https://postgr.es/m/45f50c59-ddbb-8cf2-eedb-81003f603528@2ndquadrant.com

--- src/bin/pg_dump/pg_dumpall.c
+++ src/bin/pg_dump/pg_dumpall.c
@@ -146,6 +146,7 @@ main(int argc, char *argv[])
 		{"no-sync", no_argument, NULL, 4},
 		{"no-unlogged-table-data", no_argument, &no_unlogged_table_data, 1},
 		{"on-conflict-do-nothing", no_argument, &on_conflict_do_nothing, 1},
+		{"rows-per-insert", required_argument, NULL, 7},
 
 		{NULL, 0, NULL, 0}
 	};
@@ -327,6 +328,11 @@ main(int argc, char *argv[])
 
 			case 6:
 				simple_string_list_append(&database_exclude_patterns, optarg);
+				break;
+
+			case 7:
+				appendPQExpBufferStr(pgdumpopts, " --rows-per-insert ");
+				appendShellString(pgdumpopts, optarg);
 				break;
 
 			default:
@@ -648,6 +654,7 @@ help(void)
 	printf(_("  --no-unlogged-table-data     do not dump unlogged table data\n"));
 	printf(_("  --on-conflict-do-nothing     add ON CONFLICT DO NOTHING to INSERT commands\n"));
 	printf(_("  --quote-all-identifiers      quote all identifiers, even if not key words\n"));
+	printf(_("  --rows-per-insert=NROWS      number of rows per INSERT; implies --inserts\n"));
 	printf(_("  --use-set-session-authorization\n"
 			 "                               use SET SESSION AUTHORIZATION commands instead of\n"
 			 "                               ALTER OWNER commands to set ownership\n"));
