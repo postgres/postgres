66013fe73095f851c151a17ccc7553abe6246891
Magnus Hagander <magnus@hagander.net>
2019-06-19 14:59:26 +0200

    Fix typo
    Author: Daniel Gustafsson

--- src/backend/access/table/tableamapi.c
+++ src/backend/access/table/tableamapi.c
@@ -92,7 +92,7 @@ GetTableAmRoutine(Oid amhandler)
 
 	Assert(routine->relation_estimate_size != NULL);
 
-	/* optional, but one callback implies presence of hte other */
+	/* optional, but one callback implies presence of the other */
 	Assert((routine->scan_bitmap_next_block == NULL) ==
 		   (routine->scan_bitmap_next_tuple == NULL));
 	Assert(routine->scan_sample_next_block != NULL);
