992fe54e77f66ed67b6480fcf7dd208b2cd28aac
Magnus Hagander <magnus@hagander.net>
2019-06-19 14:38:23 +0200

    Replace an occurrence of slave with standby
    Commit a1ef920e27ba6ab3602aaf6d6751d8628fac1af8 replaced the use of
    slave with standby, but overlooked this comment.
    
    Author: Daniel Gustafsson

--- contrib/pg_stat_statements/pg_stat_statements.c
+++ contrib/pg_stat_statements/pg_stat_statements.c
@@ -625,7 +625,7 @@ pgss_shmem_startup(void)
 
 	/*
 	 * Remove the persisted stats file so it's not included in
-	 * backups/replication slaves, etc.  A new file will be written on next
+	 * backups/replication standbys, etc.  A new file will be written on next
 	 * shutdown.
 	 *
 	 * Note: it's okay if the PGSS_TEXT_FILE is included in a basebackup,
