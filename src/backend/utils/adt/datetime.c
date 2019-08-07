1de3e0589a47681f081a79055f9e133eb94115ad
Michael Paquier <michael@paquier.xyz>
2019-08-07 18:17:57 +0900

    Fix some incorrect parsing of time with time zone strings
    When parsing a timetz string with a dynamic timezone abbreviation or a
    timezone not specified, it was possible to generate incorrect timestamps
    based on a date which uses some non-initialized variables if the input
    string did not specify fully a date to parse.  This is already checked
    when a full timezone spec is included in the input string, but the two
    other cases mentioned above missed the same checks.
    
    This gets fixed by generating an error as this input is invalid, or in
    short when a date is not fully specified.
    
    Valgrind was complaining about this problem.
    
    Bug: #15910
    Author: Alexander Lakhin
    Discussion: https://postgr.es/m/15910-2eba5106b9aa0c61@postgresql.org
    Backpatch-through: 9.4

--- src/backend/utils/adt/datetime.c
+++ src/backend/utils/adt/datetime.c
@@ -2327,6 +2327,9 @@ DecodeTimeOnly(char **field, int *ftype, int nf,
 			GetCurrentDateTime(tmp);
 		else
 		{
+			/* a date has to be specified */
+			if ((fmask & DTK_DATE_M) != DTK_DATE_M)
+				return DTERR_BAD_FORMAT;
 			tmp->tm_year = tm->tm_year;
 			tmp->tm_mon = tm->tm_mon;
 			tmp->tm_mday = tm->tm_mday;
@@ -2354,6 +2357,9 @@ DecodeTimeOnly(char **field, int *ftype, int nf,
 			GetCurrentDateTime(tmp);
 		else
 		{
+			/* a date has to be specified */
+			if ((fmask & DTK_DATE_M) != DTK_DATE_M)
+				return DTERR_BAD_FORMAT;
 			tmp->tm_year = tm->tm_year;
 			tmp->tm_mon = tm->tm_mon;
 			tmp->tm_mday = tm->tm_mday;
