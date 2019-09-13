82d0ae919ff8e7a9cb9283420a8834c9b9344128
Alvaro Herrera <alvherre@alvh.no-ip.org>
2019-09-13 16:26:55 -0300

    Fix under-parenthesized macro definitions
    Lack of parens in the definitions could cause a statement using these
    macros to have unexpected semantics.  In current code no bug is
    apparent, but best to fix the definitions to avoid problems down the
    line.
    
    Reported-by: Tom Lane
    Discussion: https://postgr.es/m/19795.1568400476@sss.pgh.pa.us

--- src/include/nodes/parsenodes.h
+++ src/include/nodes/parsenodes.h
@@ -3271,7 +3271,7 @@ typedef struct ConstraintsSetStmt
  */
 
 /* Reindex options */
-#define REINDEXOPT_VERBOSE 1 << 0	/* print progress info */
+#define REINDEXOPT_VERBOSE (1 << 0)	/* print progress info */
 
 typedef enum ReindexObjectType
 {
