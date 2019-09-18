59354ccef5d7671bb11982628d6ddd6fffbad2c4
Alvaro Herrera <alvherre@alvh.no-ip.org>
2019-09-18 16:50:20 -0300

    Fix example program in docs too (??)
    Fixup for previous commit: actually, the complete source for
    testlibpq3.c appears in SGML docs, so we need to patch that also.
    Go figure.

--- src/test/examples/testlibpq3.c
+++ src/test/examples/testlibpq3.c
@@ -10,6 +10,7 @@
  *
  * CREATE SCHEMA testlibpq3;
  * SET search_path = testlibpq3;
+ * SET standard_conforming_strings = ON;
  * CREATE TABLE test1 (i int4, t text, b bytea);
  * INSERT INTO test1 values (1, 'joe''s place', '\000\001\002\003\004');
  * INSERT INTO test1 values (2, 'ho there', '\004\003\002\001\000');
