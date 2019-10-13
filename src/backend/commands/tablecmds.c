postgres/postgres

commit 1d92a0c9f7dd547ad14cd8a3974289c5ec7f04ce

Redesign the partition dependency mechanism.
The original setup for dependencies of partitioned objects had
serious problems:

1. It did not verify that a drop cascading to a partition-child object
also cascaded to at least one of the object's partition parents.  Now,
normally a child object would share all its dependencies with one or
another parent (e.g. a child index's opclass dependencies would be shared
with the parent index), so that this oversight is usually harmless.
But if some dependency failed to fit this pattern, the child could be
dropped while all its parents remain, creating a logically broken
situation.  (It's easy to construct artificial cases that break it,
such as attaching an unrelated extension dependency to the child object
and then dropping the extension.  I'm not sure if any less-artificial
cases exist.)

2. Management of partition dependencies during ATTACH/DETACH PARTITION
was complicated and buggy; for example, after detaching a partition
table it was possible to create cases where a formerly-child index
should be dropped and was not, because the correct set of dependencies
had not been reconstructed.

Less seriously, because multiple partition relationships were
represented identically in pg_depend, there was an order-of-traversal
dependency on which partition parent was cited in error messages.
We also had some pre-existing order-of-traversal hazards for error
messages related to internal and extension dependencies.  This is
cosmetic to users but causes testing problems.

To fix #1, add a check at the end of the partition tree traversal
to ensure that at least one partition parent got deleted.  To fix #2,
establish a new policy that partition dependencies are in addition to,
not instead of, a child object's usual dependencies; in this way
ATTACH/DETACH PARTITION need not cope with adding or removing the
usual dependencies.

To fix the cosmetic problem, distinguish between primary and secondary

src/backend/commands/tablecmds.c
@@ -7825,7 +7825,8 @@ CloneFkReferencing(Relation pg_constraint, Relation parentRel,
 		bool		attach_it;
 		Oid			constrOid;
 		ObjectAddress parentAddr,
-					childAddr;
+					childAddr,
+					childTableAddr;
 		ListCell   *cell;
 		int			i;
 
@@ -7966,7 +7967,8 @@ CloneFkReferencing(Relation pg_constraint, Relation parentRel,
 			systable_endscan(scan);
 			table_close(trigrel, RowExclusiveLock);
 
-			ConstraintSetParentConstraint(fk->conoid, parentConstrOid);
+			ConstraintSetParentConstraint(fk->conoid, parentConstrOid,
+										  RelationGetRelid(partRel));
 			CommandCounterIncrement();
 			attach_it = true;
 			break;
@@ -8013,8 +8015,14 @@ CloneFkReferencing(Relation pg_constraint, Relation parentRel,
 								  1, false, true);
 		subclone = lappend_oid(subclone, constrOid);
 
+		/* Set up partition dependencies for the new constraint */
 		ObjectAddressSet(childAddr, ConstraintRelationId, constrOid);
-		recordDependencyOn(&childAddr, &parentAddr, DEPENDENCY_INTERNAL_AUTO);
+		recordDependencyOn(&childAddr, &parentAddr,
+						   DEPENDENCY_PARTITION_PRI);
+		ObjectAddressSet(childTableAddr, RelationRelationId,
+						 RelationGetRelid(partRel));
+		recordDependencyOn(&childAddr, &childTableAddr,
+						   DEPENDENCY_PARTITION_SEC);
 
 		fkconstraint = makeNode(Constraint);
 		/* for now this is all we need */
@@ -14893,7 +14901,8 @@ AttachPartitionEnsureIndexes(Relation rel, Relation attachrel)
 				/* bingo. */
 				IndexSetParentIndex(attachrelIdxRels[i], idx);
 				if (OidIsValid(constraintOid))
-					ConstraintSetParentConstraint(cldConstrOid, constraintOid);
+					ConstraintSetParentConstraint(cldConstrOid, constraintOid,
+												  RelationGetRelid(attachrel));
 				update_relispartition(NULL, cldIdxId, true);
 				found = true;
 				break;
@@ -15151,7 +15160,7 @@ ATExecDetachPartition(Relation rel, RangeVar *name)
 		constrOid = get_relation_idx_constraint_oid(RelationGetRelid(partRel),
 													idxid);
 		if (OidIsValid(constrOid))
-			ConstraintSetParentConstraint(constrOid, InvalidOid);
+			ConstraintSetParentConstraint(constrOid, InvalidOid, InvalidOid);
 
 		index_close(idx, NoLock);
 	}
@@ -15183,7 +15192,7 @@ ATExecDetachPartition(Relation rel, RangeVar *name)
 		}
 
 		/* unset conparentid and adjust conislocal, coninhcount, etc. */
-		ConstraintSetParentConstraint(fk->conoid, InvalidOid);
+		ConstraintSetParentConstraint(fk->conoid, InvalidOid, InvalidOid);
 
 		/*
 		 * Make the action triggers on the referenced relation.  When this was
@@ -15419,7 +15428,8 @@ ATExecAttachPartitionIdx(List **wqueue, Relation parentIdx, RangeVar *name)
 		/* All good -- do it */
 		IndexSetParentIndex(partIdx, RelationGetRelid(parentIdx));
 		if (OidIsValid(constraintOid))
-			ConstraintSetParentConstraint(cldConstrId, constraintOid);
+			ConstraintSetParentConstraint(cldConstrId, constraintOid,
+										  RelationGetRelid(partTbl));
 		update_relispartition(NULL, partIdxId, true);
 
 		pfree(attmap);

src/test/regress/expected/indexing.out
@@ -528,6 +528,30 @@ select relname, relkind from pg_class where relname like 'idxpart%' order by rel
 ---------+---------
 (0 rows)
 
+create table idxpart (a int, b int, c int) partition by range(a);
+create index on idxpart(c);
+create table idxpart1 partition of idxpart for values from (0) to (250);
+create table idxpart2 partition of idxpart for values from (250) to (500);
+alter table idxpart detach partition idxpart2;
+\d idxpart2
+              Table "public.idxpart2"
+ Column |  Type   | Collation | Nullable | Default 
+--------+---------+-----------+----------+---------
+ a      | integer |           |          | 
+ b      | integer |           |          | 
+ c      | integer |           |          | 
+Indexes:
+    "idxpart2_c_idx" btree (c)
+
+alter table idxpart2 drop column c;
+\d idxpart2
+              Table "public.idxpart2"
+ Column |  Type   | Collation | Nullable | Default 
+--------+---------+-----------+----------+---------
+ a      | integer |           |          | 
+ b      | integer |           |          | 
+
+drop table idxpart, idxpart2;
 -- Verify that expression indexes inherit correctly
 create table idxpart (a int, b int) partition by range (a);
 create table idxpart1 (like idxpart);

src/test/regress/sql/indexing.sql
@@ -249,6 +249,16 @@ select relname, relkind from pg_class where relname like 'idxpart%' order by rel
 drop table idxpart, idxpart1, idxpart2, idxpart3;
 select relname, relkind from pg_class where relname like 'idxpart%' order by relname;
 
+create table idxpart (a int, b int, c int) partition by range(a);
+create index on idxpart(c);
+create table idxpart1 partition of idxpart for values from (0) to (250);
+create table idxpart2 partition of idxpart for values from (250) to (500);
+alter table idxpart detach partition idxpart2;
+\d idxpart2
+alter table idxpart2 drop column c;
+\d idxpart2
+drop table idxpart, idxpart2;
+
 -- Verify that expression indexes inherit correctly
 create table idxpart (a int, b int) partition by range (a);
 create table idxpart1 (like idxpart);

doc/src/sgml/catalogs.sgml
@@ -2970,7 +2970,7 @@ SCRAM-SHA-256$<replaceable>&lt;iteration count&gt;</replaceable>:<replaceable>&l
        referenced object, and should be automatically dropped
        (regardless of <literal>RESTRICT</literal> or <literal>CASCADE</literal>
        mode) if the referenced object is dropped.  Example: a named
-       constraint on a table is made autodependent on the table, so
+       constraint on a table is made auto-dependent on the table, so
        that it will go away if the table is dropped.
       </para>
      </listitem>
@@ -2982,38 +2982,61 @@ SCRAM-SHA-256$<replaceable>&lt;iteration count&gt;</replaceable>:<replaceable>&l
       <para>
        The dependent object was created as part of creation of the
        referenced object, and is really just a part of its internal
-       implementation.  A <command>DROP</command> of the dependent object
-       will be disallowed outright (we'll tell the user to issue a
-       <command>DROP</command> against the referenced object, instead).  A
-       <command>DROP</command> of the referenced object will be propagated
-       through to drop the dependent object whether
-       <command>CASCADE</command> is specified or not.  Example: a trigger
-       that's created to enforce a foreign-key constraint is made
-       internally dependent on the constraint's
-       <structname>pg_constraint</structname> entry.
+       implementation.  A direct <command>DROP</command> of the dependent
+       object will be disallowed outright (we'll tell the user to issue
+       a <command>DROP</command> against the referenced object, instead).
+       A <command>DROP</command> of the referenced object will result in
+       automatically dropping the dependent object
+       whether <literal>CASCADE</literal> is specified or not.  If the
+       dependent object is reached due to a dependency on some other object,
+       the drop is converted to a drop of the referenced object, so
+       that <literal>NORMAL</literal> and <literal>AUTO</literal>
+       dependencies of the dependent object behave much like they were
+       dependencies of the referenced object.
+       Example: a view's <literal>ON SELECT</literal> rule is made
+       internally dependent on the view, preventing it from being dropped
+       while the view remains.  Dependencies of the rule (such as tables it
+       refers to) act as if they were dependencies of the view.
       </para>
      </listitem>
     </varlistentry>
 
     <varlistentry>
-     <term><symbol>DEPENDENCY_INTERNAL_AUTO</symbol> (<literal>I</literal>)</term>
+     <term><symbol>DEPENDENCY_PARTITION_PRI</symbol> (<literal>P</literal>)</term>
+     <term><symbol>DEPENDENCY_PARTITION_SEC</symbol> (<literal>S</literal>)</term>
      <listitem>
       <para>
        The dependent object was created as part of creation of the
        referenced object, and is really just a part of its internal
-       implementation.  A <command>DROP</command> of the dependent object
-       will be disallowed outright (we'll tell the user to issue a
-       <command>DROP</command> against the referenced object, instead).
-       While a regular internal dependency will prevent
-       the dependent object from being dropped while any such dependencies
-       remain, <literal>DEPENDENCY_INTERNAL_AUTO</literal> will allow such
-       a drop as long as the object can be found by following any of such
+       implementation; however, unlike <literal>INTERNAL</literal>,
+       there is more than one such referenced object.  The dependent object
+       must not be dropped unless at least one of these referenced objects
+       is dropped; if any one is, the dependent object should be dropped
+       whether or not <literal>CASCADE</literal> is specified.  Also
+       unlike <literal>INTERNAL</literal>, a drop of some other object
+       that the dependent object depends on does not result in automatic
+       deletion of any partition-referenced object.  Hence, if the drop
+       does not cascade to at least one of these objects via some other
+       path, it will be refused.  (In most cases, the dependent object
+       shares all its non-partition dependencies with at least one
+       partition-referenced object, so that this restriction does not
+       result in blocking any cascaded delete.)
+       Primary and secondary partition dependencies behave identically
+       except that the primary dependency is preferred for use in error
+       messages; hence, a partition-dependent object should have one
+       primary partition dependency and one or more secondary partition
        dependencies.
-       Example: an index on a partition is made internal-auto-dependent on
-       both the partition itself as well as on the index on the parent
-       partitioned table; so the partition index is dropped together with
-       either the partition it indexes, or with the parent index it is
-       attached to.
+       Note that partition dependencies are made in addition to, not
+       instead of, any dependencies the object would normally have.  This
+       simplifies <command>ATTACH/DETACH PARTITION</command> operations:
+       the partition dependencies need only be added or removed.
+       Example: a child partitioned index is made partition-dependent
+       on both the partition table it is on and the parent partitioned
+       index, so that it goes away if either of those is dropped, but
+       not otherwise.  The dependency on the parent index is primary,
+       so that if the user tries to drop the child partitioned index,
+       the error message will suggest dropping the parent index instead
+       (not the table).
       </para>
      </listitem>
     </varlistentry>
@@ -3026,9 +3049,10 @@ SCRAM-SHA-256$<replaceable>&lt;iteration count&gt;</replaceable>:<replaceable>&l
        the referenced object (see
        <link linkend="catalog-pg-extension"><structname>pg_extension</structname></link>).
        The dependent object can be dropped only via
-       <command>DROP EXTENSION</command> on the referenced object.  Functionally
-       this dependency type acts the same as an internal dependency, but
-       it's kept separate for clarity and to simplify <application>pg_dump</application>.
+       <command>DROP EXTENSION</command> on the referenced object.
+       Functionally this dependency type acts the same as
+       an <literal>INTERNAL</literal> dependency, but it's kept separate for
+       clarity and to simplify <application>pg_dump</application>.
       </para>
      </listitem>
     </varlistentry>
@@ -3038,10 +3062,13 @@ SCRAM-SHA-256$<replaceable>&lt;iteration count&gt;</replaceable>:<replaceable>&l
      <listitem>
       <para>
        The dependent object is not a member of the extension that is the
-       referenced object (and so should not be ignored by pg_dump), but
-       cannot function without it and should be dropped when the
-       extension itself is. The dependent object may be dropped on its
-       own as well.
+       referenced object (and so it should not be ignored
+       by <application>pg_dump</application>), but it cannot function
+       without the extension and should be auto-dropped if the extension is.
+       The dependent object may be dropped on its own as well.
+       Functionally this dependency type acts the same as
+       an <literal>AUTO</literal> dependency, but it's kept separate for
+       clarity and to simplify <application>pg_dump</application>.
       </para>
      </listitem>
     </varlistentry>
@@ -3061,6 +3088,19 @@ SCRAM-SHA-256$<replaceable>&lt;iteration count&gt;</replaceable>:<replaceable>&l
    </variablelist>
 
    Other dependency flavors might be needed in future.
+  </para>
+
+  <para>
+   Note that it's quite possible for two objects to be linked by more than
+   one <structname>pg_depend</structname> entry.  For example, a child
+   partitioned index would have both a partition-type dependency on its
+   associated partition table, and an auto dependency on each column of
+   that table that it indexes.  This sort of situation expresses the union
+   of multiple dependency semantics.  A dependent object can be dropped
+   without <literal>CASCADE</literal> if any of its dependencies satisfies
+   its condition for automatic dropping.  Conversely, all the
+   dependencies' restrictions about which objects must be dropped together
+   must be satisfied.
   </para>
 
  </sect1>

commit 1df5875d39383b3981b804666ee1f4b0ff65943f

Fix dependency handling of column drop with partitioned tables
When dropping a column on a partitioned table which has one or more
partitioned indexes, the operation was failing as dependencies with
partitioned indexes using the column dropped were not getting removed in
a way consistent with the columns involved across all the relations part
of an inheritance tree.

This commit refactors the code executing column drop so as all the
columns from an inheritance tree to remove are gathered first, and
dropped all at the end.  This way, we let the dependency machinery sort
out by itself the deletion of all the columns with the partitioned
indexes across a partition tree.

This issue has been introduced by 1d92a0c, so backpatch down to
REL_12_STABLE.

Author: Amit Langote, Michael Paquier
Reviewed-by: Álvaro Herrera, Ashutosh Sharma
Discussion: https://postgr.es/m/CA+HiwqE9kuBsZ3b5pob2-cvE8ofzPWs-og+g8bKKGnu6b4-yTQ@mail.gmail.com
Backpatch-through: 12

src/backend/commands/tablecmds.c
@@ -401,7 +401,8 @@ static void ATPrepDropColumn(List **wqueue, Relation rel, bool recurse, bool rec
 static ObjectAddress ATExecDropColumn(List **wqueue, Relation rel, const char *colName,
 									  DropBehavior behavior,
 									  bool recurse, bool recursing,
-									  bool missing_ok, LOCKMODE lockmode);
+									  bool missing_ok, LOCKMODE lockmode,
+									  ObjectAddresses *addrs);
 static ObjectAddress ATExecAddIndex(AlteredTableInfo *tab, Relation rel,
 									IndexStmt *stmt, bool is_rebuild, LOCKMODE lockmode);
 static ObjectAddress ATExecAddConstraint(List **wqueue,
@@ -4273,12 +4274,14 @@ ATExecCmd(List **wqueue, AlteredTableInfo *tab, Relation rel,
 		case AT_DropColumn:		/* DROP COLUMN */
 			address = ATExecDropColumn(wqueue, rel, cmd->name,
 									   cmd->behavior, false, false,
-									   cmd->missing_ok, lockmode);
+									   cmd->missing_ok, lockmode,
+									   NULL);
 			break;
 		case AT_DropColumnRecurse:	/* DROP COLUMN with recursion */
 			address = ATExecDropColumn(wqueue, rel, cmd->name,
 									   cmd->behavior, true, false,
-									   cmd->missing_ok, lockmode);
+									   cmd->missing_ok, lockmode,
+									   NULL);
 			break;
 		case AT_AddIndex:		/* ADD INDEX */
 			address = ATExecAddIndex(tab, rel, (IndexStmt *) cmd->def, false,
@@ -7013,13 +7016,22 @@ ATPrepDropColumn(List **wqueue, Relation rel, bool recurse, bool recursing,
 }
 
 /*
- * Return value is the address of the dropped column.
+ * Drops column 'colName' from relation 'rel' and returns the address of the
+ * dropped column.  The column is also dropped (or marked as no longer
+ * inherited from relation) from the relation's inheritance children, if any.
+ *
+ * In the recursive invocations for inheritance child relations, instead of
+ * dropping the column directly (if to be dropped at all), its object address
+ * is added to 'addrs', which must be non-NULL in such invocations.  All
+ * columns are dropped at the same time after all the children have been
+ * checked recursively.
  */
 static ObjectAddress
 ATExecDropColumn(List **wqueue, Relation rel, const char *colName,
 				 DropBehavior behavior,
 				 bool recurse, bool recursing,
-				 bool missing_ok, LOCKMODE lockmode)
+				 bool missing_ok, LOCKMODE lockmode,
+				 ObjectAddresses *addrs)
 {
 	HeapTuple	tuple;
 	Form_pg_attribute targetatt;
@@ -7031,6 +7043,11 @@ ATExecDropColumn(List **wqueue, Relation rel, const char *colName,
 	/* At top level, permission check was done in ATPrepCmd, else do it */
 	if (recursing)
 		ATSimplePermissions(rel, ATT_TABLE | ATT_FOREIGN_TABLE);
+
+	/* Initialize addrs on the first invocation */
+	Assert(!recursing || addrs != NULL);
+	if (!recursing)
+		addrs = new_object_addresses();
 
 	/*
 	 * get the number of the attribute
@@ -7144,7 +7161,7 @@ ATExecDropColumn(List **wqueue, Relation rel, const char *colName,
 					/* Time to delete this child column, too */
 					ATExecDropColumn(wqueue, childrel, colName,
 									 behavior, true, true,
-									 false, lockmode);
+									 false, lockmode, addrs);
 				}
 				else
 				{
@@ -7180,14 +7197,18 @@ ATExecDropColumn(List **wqueue, Relation rel, const char *colName,
 		table_close(attr_rel, RowExclusiveLock);
 	}
 
-	/*
-	 * Perform the actual column deletion
-	 */
+	/* Add object to delete */
 	object.classId = RelationRelationId;
 	object.objectId = RelationGetRelid(rel);
 	object.objectSubId = attnum;
+	add_exact_object_address(&object, addrs);
 
-	performDeletion(&object, behavior, 0);
+	if (!recursing)
+	{
+		/* Recursion has ended, drop everything that was collected */
+		performMultipleDeletions(addrs, behavior, 0);
+		free_object_addresses(addrs);
+	}
 
 	return object;
 }

src/test/regress/expected/indexing.out
@@ -1258,3 +1258,64 @@ ERROR:  cannot drop inherited constraint "parted_uniq_detach_test1_a_key" of rel
 alter table parted_uniq_detach_test detach partition parted_uniq_detach_test1;
 alter table parted_uniq_detach_test1 drop constraint parted_uniq_detach_test1_a_key;
 drop table parted_uniq_detach_test, parted_uniq_detach_test1;
+-- check that dropping a column takes with it any partitioned indexes
+-- depending on it.
+create table parted_index_col_drop(a int, b int, c int)
+  partition by list (a);
+create table parted_index_col_drop1 partition of parted_index_col_drop
+  for values in (1) partition by list (a);
+-- leave this partition without children.
+create table parted_index_col_drop2 partition of parted_index_col_drop
+  for values in (2) partition by list (a);
+create table parted_index_col_drop11 partition of parted_index_col_drop1
+  for values in (1);
+create index on parted_index_col_drop (b);
+create index on parted_index_col_drop (c);
+create index on parted_index_col_drop (b, c);
+alter table parted_index_col_drop drop column c;
+\d parted_index_col_drop
+ Partitioned table "public.parted_index_col_drop"
+ Column |  Type   | Collation | Nullable | Default 
+--------+---------+-----------+----------+---------
+ a      | integer |           |          | 
+ b      | integer |           |          | 
+Partition key: LIST (a)
+Indexes:
+    "parted_index_col_drop_b_idx" btree (b)
+Number of partitions: 2 (Use \d+ to list them.)
+
+\d parted_index_col_drop1
+ Partitioned table "public.parted_index_col_drop1"
+ Column |  Type   | Collation | Nullable | Default 
+--------+---------+-----------+----------+---------
+ a      | integer |           |          | 
+ b      | integer |           |          | 
+Partition of: parted_index_col_drop FOR VALUES IN (1)
+Partition key: LIST (a)
+Indexes:
+    "parted_index_col_drop1_b_idx" btree (b)
+Number of partitions: 1 (Use \d+ to list them.)
+
+\d parted_index_col_drop2
+ Partitioned table "public.parted_index_col_drop2"
+ Column |  Type   | Collation | Nullable | Default 
+--------+---------+-----------+----------+---------
+ a      | integer |           |          | 
+ b      | integer |           |          | 
+Partition of: parted_index_col_drop FOR VALUES IN (2)
+Partition key: LIST (a)
+Indexes:
+    "parted_index_col_drop2_b_idx" btree (b)
+Number of partitions: 0
+
+\d parted_index_col_drop11
+      Table "public.parted_index_col_drop11"
+ Column |  Type   | Collation | Nullable | Default 
+--------+---------+-----------+----------+---------
+ a      | integer |           |          | 
+ b      | integer |           |          | 
+Partition of: parted_index_col_drop1 FOR VALUES IN (1)
+Indexes:
+    "parted_index_col_drop11_b_idx" btree (b)
+
+drop table parted_index_col_drop;

src/test/regress/sql/indexing.sql
@@ -703,3 +703,24 @@ alter table parted_uniq_detach_test1 drop constraint parted_uniq_detach_test1_a_
 alter table parted_uniq_detach_test detach partition parted_uniq_detach_test1;
 alter table parted_uniq_detach_test1 drop constraint parted_uniq_detach_test1_a_key;
 drop table parted_uniq_detach_test, parted_uniq_detach_test1;
+
+-- check that dropping a column takes with it any partitioned indexes
+-- depending on it.
+create table parted_index_col_drop(a int, b int, c int)
+  partition by list (a);
+create table parted_index_col_drop1 partition of parted_index_col_drop
+  for values in (1) partition by list (a);
+-- leave this partition without children.
+create table parted_index_col_drop2 partition of parted_index_col_drop
+  for values in (2) partition by list (a);
+create table parted_index_col_drop11 partition of parted_index_col_drop1
+  for values in (1);
+create index on parted_index_col_drop (b);
+create index on parted_index_col_drop (c);
+create index on parted_index_col_drop (b, c);
+alter table parted_index_col_drop drop column c;
+\d parted_index_col_drop
+\d parted_index_col_drop1
+\d parted_index_col_drop2
+\d parted_index_col_drop11
+drop table parted_index_col_drop;