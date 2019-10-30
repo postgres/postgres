82200115e05501c633595c2a7b61143f0e610a00
Andres Freund <andres@anarazel.de>
2019-10-29 22:46:40 -0700

    pg_waldump: Fix --bkp-details to not issue spurious newlines for FPWs.
    The additional newline seems to have accidentally been introduced in
    2c03216d831, in 9.5. The newline is only issued when an FPW is
    present for the block reference.
    
    While there could be an argument that removing the newlines in the
    back branches could cause a problem for somebody parsing the
    pg_waldump output, the likelihood of that seems small enough. It seems
    at least equally likely that the randomness of when newlines are
    issued causes problems.
    
    Author: Andres Freund
    Discussion: https://postgr.es/m/20191029233341.4gnyau7e5v2lh5sc@alap3.anarazel.de
    Backpatch: 9.5, like 2c03216d831.

--- src/bin/pg_waldump/pg_waldump.c
+++ src/bin/pg_waldump/pg_waldump.c
@@ -515,7 +515,7 @@ XLogDumpDisplayRecord(XLogDumpConfig *config, XLogReaderState *record)
 					BKPIMAGE_IS_COMPRESSED)
 				{
 					printf(" (FPW%s); hole: offset: %u, length: %u, "
-						   "compression saved: %u\n",
+						   "compression saved: %u",
 						   XLogRecBlockImageApply(record, block_id) ?
 						   "" : " for WAL verification",
 						   record->blocks[block_id].hole_offset,
@@ -526,7 +526,7 @@ XLogDumpDisplayRecord(XLogDumpConfig *config, XLogReaderState *record)
 				}
 				else
 				{
-					printf(" (FPW%s); hole: offset: %u, length: %u\n",
+					printf(" (FPW%s); hole: offset: %u, length: %u",
 						   XLogRecBlockImageApply(record, block_id) ?
 						   "" : " for WAL verification",
 						   record->blocks[block_id].hole_offset,
