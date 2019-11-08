64b62b7b9b8566a0b92986ce26de84101820b556
Peter Eisentraut <peter@eisentraut.org>
2019-11-08 18:12:51 +0100

    Fix gratuitous error message variation

--- src/backend/replication/logical/origin.c
+++ src/backend/replication/logical/origin.c
@@ -1055,7 +1055,7 @@ replorigin_session_setup(RepOriginId node)
 		{
 			ereport(ERROR,
 					(errcode(ERRCODE_OBJECT_IN_USE),
-			 errmsg("replication origin %d is already active for PID %d",
+			 errmsg("replication origin with OID %d is already active for PID %d",
 					curstate->roident, curstate->acquired_by)));
 		}
 
