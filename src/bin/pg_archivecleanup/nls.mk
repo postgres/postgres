c1646c81ef155ec1c687239610a5e09375e8c75a
Peter Eisentraut <peter@eisentraut.org>
2019-11-11 10:53:15 +0100

    Translation updates
    Source-Git-URL: https://git.postgresql.org/git/pgtranslation/messages.git
    Source-Git-Hash: 99bbc57cce0a1024898ac8d38b35fc6df7294e9e

--- src/bin/pg_archivecleanup/nls.mk
+++ src/bin/pg_archivecleanup/nls.mk
@@ -1,6 +1,6 @@
 # src/bin/pg_archivecleanup/nls.mk
 CATALOG_NAME     = pg_archivecleanup
-AVAIL_LANGUAGES  =cs de es fr ja ru sv tr zh_CN
+AVAIL_LANGUAGES  = cs de es fr ja ru sv tr zh_CN
 GETTEXT_FILES    = $(FRONTEND_COMMON_GETTEXT_FILES) pg_archivecleanup.c
 GETTEXT_TRIGGERS = $(FRONTEND_COMMON_GETTEXT_TRIGGERS)
 GETTEXT_FLAGS    = $(FRONTEND_COMMON_GETTEXT_FLAGS)
--- src/bin/pg_checksums/nls.mk
+++ src/bin/pg_checksums/nls.mk
@@ -1,6 +1,6 @@
 # src/bin/pg_checksums/nls.mk
 CATALOG_NAME     = pg_checksums
-AVAIL_LANGUAGES  =cs de es ja ru sv tr
+AVAIL_LANGUAGES  = cs de es ja ru sv tr
 GETTEXT_FILES    = $(FRONTEND_COMMON_GETTEXT_FILES) pg_checksums.c
 GETTEXT_TRIGGERS = $(FRONTEND_COMMON_GETTEXT_TRIGGERS)
 GETTEXT_FLAGS    = $(FRONTEND_COMMON_GETTEXT_FLAGS)
