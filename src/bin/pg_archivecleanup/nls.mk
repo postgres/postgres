5b41fc1e0f57e655281097a633c0917472c3fe4d
Peter Eisentraut <peter@eisentraut.org>
2019-11-11 10:50:22 +0100

    Translation updates
    Source-Git-URL: https://git.postgresql.org/git/pgtranslation/messages.git
    Source-Git-Hash: 36cb12a154ee719a594401e7f8763e472f41614a

--- src/bin/pg_archivecleanup/nls.mk
+++ src/bin/pg_archivecleanup/nls.mk
@@ -1,4 +1,4 @@
 # src/bin/pg_archivecleanup/nls.mk
 CATALOG_NAME     = pg_archivecleanup
-AVAIL_LANGUAGES  =cs de es fr ja ko pl ru sv tr vi zh_CN
+AVAIL_LANGUAGES  = cs de es fr ja ko pl ru sv tr vi zh_CN
 GETTEXT_FILES    = pg_archivecleanup.c
--- src/bin/pg_verify_checksums/nls.mk
+++ src/bin/pg_verify_checksums/nls.mk
@@ -1,4 +1,4 @@
 # src/bin/pg_verify_checksums/nls.mk
 CATALOG_NAME     = pg_verify_checksums
-AVAIL_LANGUAGES  =de es fr ja ko ru sv tr zh_CN
+AVAIL_LANGUAGES  = cs de es fr ja ko ru sv tr zh_CN
 GETTEXT_FILES    = pg_verify_checksums.c
