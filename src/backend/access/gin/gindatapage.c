ab64b474d9bec8486b42e550526a121f0d81b681
Alexander Korotkov <akorotkov@postgresql.org>
2019-11-19 23:08:14 +0300

    Fix traversing to the deleted GIN page via downlink
    Current GIN code appears to don't handle traversing to the deleted page via
    downlink.  This commit fixes that by stepping right from the delete page like
    we do in nbtree.
    
    This commit also fixes setting 'deleted' flag to the GIN pages.  Now other page
    flags are not erased once page is deleted.  That helps to keep our assertions
    true if we arrive deleted page via downlink.
    
    Discussion: https://postgr.es/m/CAPpHfdvMvsw-NcE5bRS7R1BbvA4BxoDnVVjkXC5W0Czvy9LVrg%40mail.gmail.com
    Author: Alexander Korotkov
    Reviewed-by: Peter Geoghegan
    Backpatch-through: 9.4

--- src/backend/access/gin/gindatapage.c
+++ src/backend/access/gin/gindatapage.c
@@ -237,6 +237,9 @@ dataIsMoveRight(GinBtree btree, Page page)
 	if (GinPageRightMost(page))
 		return FALSE;
 
+	if (GinPageIsDeleted(page))
+		return TRUE;
+
 	return (ginCompareItemPointers(&btree->itemptr, iptr) > 0) ? TRUE : FALSE;
 }
 
