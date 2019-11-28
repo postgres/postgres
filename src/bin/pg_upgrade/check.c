60b35b7f1ea1d5cd17805e30299fd21616855b7d
Bruce Momjian <bruce@momjian.us>
2019-11-27 20:36:33 -0500

    pg_upgrade:  adjust error paragraph width to be consistent
    Previously these error paragraphs were too wide.
    
    Backpatch-through: 13

--- src/bin/pg_upgrade/check.c
+++ src/bin/pg_upgrade/check.c
@@ -963,8 +963,8 @@ check_for_tables_with_oids(ClusterInfo *cluster)
 	if (found)
 	{
 		pg_log(PG_REPORT, "fatal\n");
-		pg_fatal("Your installation contains tables declared WITH OIDS, which is not supported\n"
-				 "anymore. Consider removing the oid column using\n"
+		pg_fatal("Your installation contains tables declared WITH OIDS, which is not\n"
+				 "supported anymore.  Consider removing the oid column using\n"
 				 "    ALTER TABLE ... SET WITHOUT OIDS;\n"
 				 "A list of tables with the problem is in the file:\n"
 				 "    %s\n\n", output_path);
@@ -1076,8 +1076,8 @@ check_for_reg_data_type_usage(ClusterInfo *cluster)
 		pg_fatal("Your installation contains one of the reg* data types in user tables.\n"
 				 "These data types reference system OIDs that are not preserved by\n"
 				 "pg_upgrade, so this cluster cannot currently be upgraded.  You can\n"
-				 "remove the problem tables and restart the upgrade.  A list of the problem\n"
-				 "columns is in the file:\n"
+				 "remove the problem tables and restart the upgrade.  A list of the\n"
+				 "problem columns is in the file:\n"
 				 "    %s\n\n", output_path);
 	}
 	else
@@ -1165,9 +1165,10 @@ check_for_jsonb_9_4_usage(ClusterInfo *cluster)
 	{
 		pg_log(PG_REPORT, "fatal\n");
 		pg_fatal("Your installation contains the \"jsonb\" data type in user tables.\n"
-				 "The internal format of \"jsonb\" changed during 9.4 beta so this cluster cannot currently\n"
-				 "be upgraded.  You can remove the problem tables and restart the upgrade.  A list\n"
-				 "of the problem columns is in the file:\n"
+				 "The internal format of \"jsonb\" changed during 9.4 beta so this\n"
+				 "cluster cannot currently be upgraded.  You can remove the problem\n"
+				 "tables and restart the upgrade.  A list of the problem columns is\n"
+				 "in the file:\n"
 				 "    %s\n\n", output_path);
 	}
 	else
