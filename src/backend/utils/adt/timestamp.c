332584da9cfdbe91a651427a9fc3ed76dffaa529
Tom Lane <tgl@sss.pgh.pa.us>
2019-12-12 12:30:44 -0500

    Fix EXTRACT(ISOYEAR FROM timestamp) for years BC.
    The test cases added by commit 26ae3aa80 exposed an old oversight in
    timestamp[tz]_part: they didn't correct the result of date2isoyear()
    for BC years, so that we produced an off-by-one answer for such years.
    Fix that, and back-patch to all supported branches.
    
    Discussion: https://postgr.es/m/SG2PR06MB37762CAE45DB0F6CA7001EA9B6550@SG2PR06MB3776.apcprd06.prod.outlook.com

--- src/backend/utils/adt/timestamp.c
+++ src/backend/utils/adt/timestamp.c
@@ -4265,6 +4265,7 @@ date2isoweek(int year, int mon, int mday)
 /* date2isoyear()
  *
  *	Returns ISO 8601 year number.
+ *	Note: zero or negative results follow the year-zero-exists convention.
  */
 int
 date2isoyear(int year, int mon, int mday)
@@ -4539,6 +4540,9 @@ timestamp_part(PG_FUNCTION_ARGS)
 
 			case DTK_ISOYEAR:
 				result = date2isoyear(tm->tm_year, tm->tm_mon, tm->tm_mday);
+				/* Adjust BC years */
+				if (result <= 0)
+					result -= 1;
 				break;
 
 			case DTK_DOW:
@@ -4735,6 +4739,9 @@ timestamptz_part(PG_FUNCTION_ARGS)
 
 			case DTK_ISOYEAR:
 				result = date2isoyear(tm->tm_year, tm->tm_mon, tm->tm_mday);
+				/* Adjust BC years */
+				if (result <= 0)
+					result -= 1;
 				break;
 
 			case DTK_DOW:
