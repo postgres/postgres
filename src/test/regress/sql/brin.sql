611ce856f5a862a291a05d148828208e1f4ed46b
Alvaro Herrera <alvherre@alvh.no-ip.org>
2020-01-22 18:35:05 -0300

    Add BRIN test case
    This test case was sketched in commit message 4c87010981f3 to explain an
    ancient bug; it translates to a coverage increase, so add it to the BRIN
    regression tests.

--- src/test/regress/sql/brin.sql
+++ src/test/regress/sql/brin.sql
@@ -448,6 +448,16 @@ SELECT brin_summarize_range('brin_summarize_idx', 4294967295);
 SELECT brin_summarize_range('brin_summarize_idx', -1);
 SELECT brin_summarize_range('brin_summarize_idx', 4294967296);
 
+-- test value merging in add_value
+CREATE TABLE brintest_2 (n numrange);
+CREATE INDEX brinidx_2 ON brintest_2 USING brin (n);
+INSERT INTO brintest_2 VALUES ('empty');
+INSERT INTO brintest_2 VALUES (numrange(0, 2^1000::numeric));
+INSERT INTO brintest_2 VALUES ('(-1, 0)');
+
+SELECT brin_desummarize_range('brinidx', 0);
+SELECT brin_summarize_range('brinidx', 0);
+DROP TABLE brintest_2;
 
 -- test brin cost estimates behave sanely based on correlation of values
 CREATE TABLE brin_test (a INT, b INT);
