006b9dcad41956f180f9363648206fede8417592
Robert Haas <rhaas@postgresql.org>
2020-01-29 10:50:17 -0500

    Add jsonapi.c to Mkvcbuild.pm's @pgcommonallfiles.
    My recent commit beb4699091e9fab1c5f465056bef35c9ddf7f9fc caused
    some buildfarm breakage, as reported by Tom Lane. Try to repair.
    
    This fix is extracted from a larger patch by Andrew Dunstan.
    
    Discussion: http://postgr.es/m/8440ddc9-8347-ca64-1405-845d10e054cd@2ndQuadrant.com
    Discussion: http://postgr.es/m/14178.1580312751@sss.pgh.pa.us

--- src/tools/msvc/Mkvcbuild.pm
+++ src/tools/msvc/Mkvcbuild.pm
@@ -121,7 +121,7 @@ sub mkvcbuild
 
 	our @pgcommonallfiles = qw(
 	  base64.c config_info.c controldata_utils.c d2s.c encnames.c exec.c
-	  f2s.c file_perm.c ip.c
+	  f2s.c file_perm.c ip.c jsonapi.c
 	  keywords.c kwlookup.c link-canary.c md5.c
 	  pg_lzcompress.c pgfnames.c psprintf.c relpath.c rmtree.c
 	  saslprep.c scram-common.c string.c stringinfo.c unicode_norm.c username.c
