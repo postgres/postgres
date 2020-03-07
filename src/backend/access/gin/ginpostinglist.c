ea7dace2aa21e79a7a8eb23c493bcd2057d9bd7e
Tom Lane <tgl@sss.pgh.pa.us>
2020-03-07 13:31:17 -0500

    Simplify/speed up assertion cross-check in ginCompressPostingList().
    I noticed while testing some other stuff that the CHECK_ENCODING_ROUNDTRIP
    logic in ginCompressPostingList could account for over 50% of the runtime
    of an INSERT with a GIN index.  While that's not relevant to production
    performance, it's still kind of annoying in a debug build.  Replacing
    the loop around short memcmp's with one long memcmp works just as well
    and is significantly faster, at least on my machine.

--- src/backend/access/gin/ginpostinglist.c
+++ src/backend/access/gin/ginpostinglist.c
@@ -266,11 +266,9 @@ ginCompressPostingList(const ItemPointer ipd, int nipd, int maxsize,
 	{
 		int			ndecoded;
 		ItemPointer tmp = ginPostingListDecode(result, &ndecoded);
-		int			i;
 
 		Assert(ndecoded == totalpacked);
-		for (i = 0; i < ndecoded; i++)
-			Assert(memcmp(&tmp[i], &ipd[i], sizeof(ItemPointerData)) == 0);
+		Assert(memcmp(tmp, ipd, ndecoded * sizeof(ItemPointerData)) == 0);
 		pfree(tmp);
 	}
 #endif
