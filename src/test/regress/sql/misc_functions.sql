630590d6ff632b95418eff9e98482d9a93b60ee3
Tom Lane <tgl@sss.pgh.pa.us>
2020-03-11 18:23:57 -0400

    Fix test case instability introduced in 085b6b667.
    I forgot that the WAL directory might hold other files besides WAL
    segments, notably including new segments still being filled.
    That means a blind test for the first file's size being 16MB can
    fail.  Restrict based on file name length to make it more robust.
    
    Per buildfarm.

--- src/test/regress/sql/misc_functions.sql
+++ src/test/regress/sql/misc_functions.sql
@@ -46,7 +46,8 @@ select count(*) > 0 as ok from (select pg_ls_waldir()) ss;
 -- Test not-run-to-completion cases.
 select * from pg_ls_waldir() limit 0;
 select count(*) > 0 as ok from (select * from pg_ls_waldir() limit 1) ss;
-select (pg_ls_waldir()).size = :segsize as ok limit 1;
+select (w).size = :segsize as ok
+from (select pg_ls_waldir() w) ss where length((w).name) = 24 limit 1;
 
 select count(*) >= 0 as ok from pg_ls_archive_statusdir();
 
