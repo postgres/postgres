5e4a0b7194b9b53eeedc718fde1430eb649b8b62
Tom Lane <tgl@sss.pgh.pa.us>
2020-03-16 21:05:29 -0400

    Avoid holding a directory FD open across assorted SRF calls.
    This extends the fixes made in commit 085b6b667 to other SRFs with the
    same bug, namely pg_logdir_ls(), pgrowlocks(), pg_timezone_names(),
    pg_ls_dir(), and pg_tablespace_databases().
    
    Also adjust various comments and documentation to warn against
    expecting to clean up resources during a ValuePerCall SRF's final
    call.
    
    Back-patch to all supported branches, since these functions were
    all born broken.
    
    Justin Pryzby, with cosmetic tweaks by me
    
    Discussion: https://postgr.es/m/20200308173103.GC1357@telsasoft.com

--- src/test/regress/sql/misc_functions.sql
+++ src/test/regress/sql/misc_functions.sql
@@ -51,3 +51,12 @@ select * from pg_ls_waldir() limit 0;
 select count(*) > 0 as ok from (select * from pg_ls_waldir() limit 1) ss;
 select (w).size = :segbsize * :bsize as ok
 from (select pg_ls_waldir() w) ss where length((w).name) = 24 limit 1;
+
+select * from (select pg_ls_dir('.') a) a where a = 'base' limit 1;
+
+select * from (select (pg_timezone_names()).name) ptn where name='UTC' limit 1;
+
+select count(*) > 0 from
+  (select pg_tablespace_databases(oid) as pts from pg_tablespace
+   where spcname = 'pg_default') pts
+  join pg_database db on pts.pts = db.oid;
