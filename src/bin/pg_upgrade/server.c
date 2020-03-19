7df528312d10907aa79fb7c230e54cfcba9f4329
Bruce Momjian <bruce@momjian.us>
2020-03-19 15:20:55 -0400

    pg_upgrade:  make get_major_server_version() err msg consistent
    This patch fixes the error message in get_major_server_version() to be
    "could not parse version file", and uses the full file path name, rather
    than just the data directory path.
    
    Also, commit 4109bb5de4 added the cause of the failure to the  "could
    not open" error message, and improved quoting.  This patch backpatches
    the "could not open" cause to PG 12, where it was first widely used, and
    backpatches the quoting fix in that patch to all supported releases.
    
    Reported-by: Tom Lane
    
    Discussion: https://postgr.es/m/87pne2w98h.fsf@wibble.ilmari.org
    
    Author: Dagfinn Ilmari Mannsåker
    
    Backpatch-through: 9.5

--- src/bin/pg_upgrade/server.c
+++ src/bin/pg_upgrade/server.c
@@ -165,12 +165,12 @@ get_major_server_version(ClusterInfo *cluster)
 	snprintf(ver_filename, sizeof(ver_filename), "%s/PG_VERSION",
 			 cluster->pgdata);
 	if ((version_fd = fopen(ver_filename, "r")) == NULL)
-		pg_fatal("could not open version file: %s\n", ver_filename);
+		pg_fatal("could not open version file \"%s\"\n", ver_filename);
 
 	if (fscanf(version_fd, "%63s", cluster->major_version_str) == 0 ||
 		sscanf(cluster->major_version_str, "%d.%d", &integer_version,
 			   &fractional_version) != 2)
-		pg_fatal("could not get version from %s\n", cluster->pgdata);
+		pg_fatal("could not parse version file \"%s\"\n", ver_filename);
 
 	fclose(version_fd);
 
