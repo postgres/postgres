909fbf10528c13b273ce0c5116223a3da082dcef
Noah Misch <noah@leadboat.com>
2020-03-21 09:38:26 -0700

    Fix cosmetic blemishes involving rd_createSubid.
    Remove an obsolete comment from AtEOXact_cleanup().  Restore formatting
    of a comment in struct RelationData, mangled by the pgindent run in
    commit 9af4159fce6654aa0e081b00d02bca40b978745c.  Back-patch to 9.5 (all
    supported versions), because another fix stacks on this.

--- src/include/utils/rel.h
+++ src/include/utils/rel.h
@@ -63,7 +63,7 @@ typedef struct RelationData
 	char		rd_indexvalid;	/* state of rd_indexlist: 0 = not valid, 1 =
 								 * valid, 2 = temporarily forced */
 
-	/*
+	/*----------
 	 * rd_createSubid is the ID of the highest subtransaction the rel has
 	 * survived into; or zero if the rel was not created in the current top
 	 * transaction.  This can be now be relied on, whereas previously it could
@@ -73,8 +73,13 @@ typedef struct RelationData
 	 * have forgotten changing it). rd_newRelfilenodeSubid can be forgotten
 	 * when a relation has multiple new relfilenodes within a single
 	 * transaction, with one of them occurring in a subsequently aborted
-	 * subtransaction, e.g. BEGIN; TRUNCATE t; SAVEPOINT save; TRUNCATE t;
-	 * ROLLBACK TO save; -- rd_newRelfilenode is now forgotten
+	 * subtransaction, e.g.
+	 *		BEGIN;
+	 *		TRUNCATE t;
+	 *		SAVEPOINT save;
+	 *		TRUNCATE t;
+	 *		ROLLBACK TO save;
+	 *		-- rd_newRelfilenodeSubid is now forgotten
 	 */
 	SubTransactionId rd_createSubid;	/* rel was created in current xact */
 	SubTransactionId rd_newRelfilenodeSubid;	/* new relfilenode assigned in
