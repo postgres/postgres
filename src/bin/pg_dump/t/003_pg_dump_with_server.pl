ffd398021c296e55b59ab848ac71a1769ddff93f
Alvaro Herrera <alvherre@alvh.no-ip.org>
2020-03-25 15:14:14 -0300

    pg_dump new test: Change order of arguments
    Some getopt_long implementations don't like to have a non-option
    argument before option arguments, so put the database name as the
    last switch.
    
    Per buildfarm member hoverfly.

--- src/bin/pg_dump/t/003_pg_dump_with_server.pl
+++ src/bin/pg_dump/t/003_pg_dump_with_server.pl
@@ -27,10 +27,10 @@ $node->safe_psql( 'postgres', "CREATE FOREIGN TABLE t1 (a int) SERVER s1");
 my ($cmd, $stdout, $stderr, $result);
 
 command_fails_like(
-	[ "pg_dump",  '-p', $port, 'postgres', '--include-foreign-data=s0' ],
+	[ "pg_dump",  '-p', $port, '--include-foreign-data=s0', 'postgres' ],
 	qr/foreign-data wrapper \"dummy\" has no handler\r?\npg_dump: error: query was:.*t0/,
 	"correctly fails to dump a foreign table from a dummy FDW");
 
 command_ok(
-	[ "pg_dump", '-p', $port, 'postgres', '-a', '--include-foreign-data=s2' ] ,
+	[ "pg_dump", '-p', $port, '-a', '--include-foreign-data=s2', 'postgres' ] ,
 	"dump foreign server with no tables");
