2d8597ba66a1646e353c0ff755b4b0b85cd1747f
Peter Eisentraut <peter@eisentraut.org>
2020-03-26 11:51:39 +0100

    Drop slot's LWLock before returning from SaveSlotToPath()
    When SaveSlotToPath() is called with elevel=LOG, the early exits didn't
    release the slot's io_in_progress_lock.
    
    This could result in a walsender being stuck on the lock forever.  A
    possible way to get into this situation is if the offending code paths
    are triggered in a low disk space situation.
    
    Author: Pavan Deolasee <pavan.deolasee@2ndquadrant.com>
    Reported-by: Craig Ringer <craig@2ndquadrant.com>
    Discussion: https://www.postgresql.org/message-id/flat/56a138c5-de61-f553-7e8f-6789296de785%402ndquadrant.com

--- src/backend/replication/slot.c
+++ src/backend/replication/slot.c
@@ -1062,6 +1062,12 @@ SaveSlotToPath(ReplicationSlot *slot, const char *dir, int elevel)
 						   S_IRUSR | S_IWUSR);
 	if (fd < 0)
 	{
+		/*
+		 * If not an ERROR, then release the lock before returning.  In case
+		 * of an ERROR, the error recovery path automatically releases the
+		 * lock, but no harm in explicitly releasing even in that case.
+		 */
+		LWLockRelease(&slot->io_in_progress_lock);
 		ereport(elevel,
 				(errcode_for_file_access(),
 				 errmsg("could not create file \"%s\": %m",
@@ -1091,6 +1097,7 @@ SaveSlotToPath(ReplicationSlot *slot, const char *dir, int elevel)
 		int			save_errno = errno;
 
 		CloseTransientFile(fd);
+		LWLockRelease(&slot->io_in_progress_lock);
 
 		/* if write didn't set errno, assume problem is no disk space */
 		errno = save_errno ? save_errno : ENOSPC;
@@ -1107,6 +1114,7 @@ SaveSlotToPath(ReplicationSlot *slot, const char *dir, int elevel)
 		int			save_errno = errno;
 
 		CloseTransientFile(fd);
+		LWLockRelease(&slot->io_in_progress_lock);
 		errno = save_errno;
 		ereport(elevel,
 				(errcode_for_file_access(),
@@ -1120,6 +1128,7 @@ SaveSlotToPath(ReplicationSlot *slot, const char *dir, int elevel)
 	/* rename to permanent file, fsync file and directory */
 	if (rename(tmppath, path) != 0)
 	{
+		LWLockRelease(&slot->io_in_progress_lock);
 		ereport(elevel,
 				(errcode_for_file_access(),
 				 errmsg("could not rename file \"%s\" to \"%s\": %m",
