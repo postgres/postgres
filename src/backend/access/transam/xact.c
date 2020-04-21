5c1c27eef94a3d22f2fa4feab922c4af72a3a669
Tom Lane <tgl@sss.pgh.pa.us>
2020-04-21 15:58:43 -0400

    Fix possible crash during FATAL exit from reindexing.
    index.c supposed that it could just use a PG_TRY block to clean up the
    state associated with an active REINDEX operation.  However, that code
    doesn't run if we do a FATAL exit --- for example, due to a SIGTERM
    shutdown signal --- while the REINDEX is happening.  And that state does
    get consulted during catalog accesses, which makes it problematic if we
    do any catalog accesses during shutdown --- for example, to clean up any
    temp tables created in the session.
    
    If this combination of circumstances occurred, we could find ourselves
    trying to access already-freed memory.  In debug builds that'd fairly
    reliably cause an assertion failure.  In production we might often
    get away with it, but with some bad luck it could cause a core dump.
    
    Another possible bad outcome is an erroneous conclusion that an
    index-to-be-accessed is being reindexed; but it looks like that would
    be unlikely to have any consequences worse than failing to drop temp
    tables right away.  (They'd still get dropped by the next session that
    uses that temp schema.)
    
    To fix, get rid of the use of PG_TRY here, and instead hook into
    the transaction abort mechanisms to clean up reindex state.
    
    Per bug #16378 from Alexander Lakhin.  This has been wrong for a
    very long time, so back-patch to all supported branches.
    
    Discussion: https://postgr.es/m/16378-7a70ca41b3ec2009@postgresql.org

--- src/backend/access/transam/xact.c
+++ src/backend/access/transam/xact.c
@@ -31,6 +31,7 @@
 #include "access/xloginsert.h"
 #include "access/xlogutils.h"
 #include "catalog/catalog.h"
+#include "catalog/index.h"
 #include "catalog/namespace.h"
 #include "catalog/storage.h"
 #include "commands/async.h"
@@ -2548,6 +2549,9 @@ AbortTransaction(void)
 	 */
 	SetUserIdAndSecContext(s->prevUser, s->prevSecContext);
 
+	/* Forget about any active REINDEX. */
+	ResetReindexState(s->nestingLevel);
+
 	/* If in parallel mode, clean up workers and exit parallel mode. */
 	if (IsInParallelMode())
 	{
@@ -4649,6 +4653,9 @@ AbortSubTransaction(void)
 	 * AbortTransaction.)
 	 */
 	SetUserIdAndSecContext(s->prevUser, s->prevSecContext);
+
+	/* Forget about any active REINDEX. */
+	ResetReindexState(s->nestingLevel);
 
 	/* Exit from parallel mode, if necessary. */
 	if (IsInParallelMode())
