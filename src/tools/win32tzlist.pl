b31f9fd63341441717ddd364cfef9a89afb75677
Tom Lane <tgl@sss.pgh.pa.us>
2020-04-24 17:53:23 -0400

    Update Windows timezone name list to include currently-known zones.
    Thanks to Juan José Santamaría Flecha.
    
    Discussion: https://postgr.es/m/5752.1587740484@sss.pgh.pa.us

--- src/tools/win32tzlist.pl
+++ src/tools/win32tzlist.pl
@@ -123,7 +123,7 @@ if (@add)
 	for my $z (@add)
 	{
 		print
-		  "\t{\n\t\t\"$z->{std}\", \"$z->{dlt}\",\n\t\t\"FIXME\"\n\t},\t\t\t\t\t\t\t/* $z->{display} */\n";
+		  "\t{\n\t\t/* $z->{display} */\n\t\t\"$z->{std}\", \"$z->{dlt}\",\n\t\t\"FIXME\"\n\t},\n";
 	}
 }
 
