d9c501da70b079a7138f8b339339169d5bd24143
Peter Geoghegan <pg@bowt.ie>
2020-04-30 14:33:13 -0700

    Add nbtree ScalarArrayOpExpr tests.
    Add test coverage for the nbtutils.c routines concerned with IndexScans
    that have native ScalarArrayOpExpr quals.  The ScalarArrayOpExpr
    specialized mark and restore routines, and the "find extreme element"
    routine now have some test coverage.
    
    These functions are probably infrequently exercised by real world
    queries, so having some coverage seems like a good idea.  The mark and
    restore routines were originally added by a bugfix that came several
    weeks after the first stable release of Postgres 9.2 (see commit
    70bc5833195).

--- src/test/regress/sql/join.sql
+++ src/test/regress/sql/join.sql
@@ -2124,6 +2124,24 @@ select * from j1
 inner join j2 on j1.id1 = j2.id1 and j1.id2 = j2.id2
 where j1.id1 % 1000 = 1 and j2.id1 % 1000 = 1;
 
+-- Exercise array keys mark/restore B-Tree code
+explain (costs off) select * from j1
+inner join j2 on j1.id1 = j2.id1 and j1.id2 = j2.id2
+where j1.id1 % 1000 = 1 and j2.id1 % 1000 = 1 and j2.id1 = any (array[1]);
+
+select * from j1
+inner join j2 on j1.id1 = j2.id1 and j1.id2 = j2.id2
+where j1.id1 % 1000 = 1 and j2.id1 % 1000 = 1 and j2.id1 = any (array[1]);
+
+-- Exercise array keys "find extreme element" B-Tree code
+explain (costs off) select * from j1
+inner join j2 on j1.id1 = j2.id1 and j1.id2 = j2.id2
+where j1.id1 % 1000 = 1 and j2.id1 % 1000 = 1 and j2.id1 >= any (array[1,5]);
+
+select * from j1
+inner join j2 on j1.id1 = j2.id1 and j1.id2 = j2.id2
+where j1.id1 % 1000 = 1 and j2.id1 % 1000 = 1 and j2.id1 >= any (array[1,5]);
+
 reset enable_nestloop;
 reset enable_hashjoin;
 reset enable_sort;
