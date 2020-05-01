2e08d314ed07363636a5da65f2a3abf7135f8ba8
Tomas Vondra <tomas.vondra@postgresql.org>
2020-05-02 00:36:25 +0200

    Remove pg_xact entry from SLRU stats
    The "pg_xact" entry was duplicate with "clog" and was added by mistake.
    
    Reported-by: Fujii Masao
    Discussion: https://postgr.es/m/20200119143707.gyinppnigokesjok@development

--- src/backend/postmaster/pgstat.c
+++ src/backend/postmaster/pgstat.c
@@ -152,7 +152,7 @@ PgStat_MsgBgWriter BgWriterStats;
  */
 static char *slru_names[] = {"async", "clog", "commit_timestamp",
 							  "multixact_offset", "multixact_member",
-							  "oldserxid", "pg_xact", "subtrans",
+							  "oldserxid", "subtrans",
 							  "other" /* has to be last */};
 
 /* number of elemenents of slru_name array */
