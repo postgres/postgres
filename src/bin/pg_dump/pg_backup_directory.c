f8635747ee9c07844819075675b2c5c07a7e016e
Alvaro Herrera <alvherre@alvh.no-ip.org>
2020-05-08 19:38:46 -0400

    pg_restore: Provide file name with one failure message
    Almost all error messages already include file name where relevant, but
    this one had been overlooked.  Repair.
    
    Backpatch to 9.5.
    
    Author: Euler Taveira <euler.taveira@2ndquadrant.com>
    Discussion: https://postgr.es/m/CAH503wA_VOrcKL_43p9atRejCDYmOZ8MzfK9S6TJrQqBqNeAXA@mail.gmail.com
    Reviewed-by: Álvaro Herrera <alvherre@alvh.no-ip.org>

--- src/bin/pg_dump/pg_backup_directory.c
+++ src/bin/pg_dump/pg_backup_directory.c
@@ -406,8 +406,8 @@ _PrintFileData(ArchiveHandle *AH, char *filename)
 
 	free(buf);
 	if (cfclose(cfp) !=0)
-		exit_horribly(modulename, "could not close data file: %s\n",
-					  strerror(errno));
+		exit_horribly(modulename, "could not close data file \"%s\": %s\n",
+					  filename, strerror(errno));
 }
 
 /*
