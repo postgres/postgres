d10b19e224c2ead10b59382f695f0c2bd65039fa
Tom Lane <tgl@sss.pgh.pa.us>
2020-06-07 17:16:30 -0400

    Stamp HEAD as 14devel.
    Let the hacking begin ...

--- src/tools/version_stamp.pl
+++ src/tools/version_stamp.pl
@@ -25,7 +25,7 @@ use warnings;
 
 # Major version is hard-wired into the script.  We update it when we branch
 # a new development version.
-my $majorversion = 13;
+my $majorversion = 14;
 
 # Validate argument and compute derived variables
 my $minor = shift;
