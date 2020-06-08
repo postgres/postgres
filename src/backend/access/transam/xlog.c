72766ad6397a1795f2739bda95370b02d5738a3f
Thomas Munro <tmunro@postgresql.org>
2020-06-08 13:57:24 +1200

    Fix locking bugs that could corrupt pg_control.
    The redo routines for XLOG_CHECKPOINT_{ONLINE,SHUTDOWN} must acquire
    ControlFileLock before modifying ControlFile->checkPointCopy, or the
    checkpointer could write out a control file with a bad checksum.
    
    Likewise, XLogReportParameters() must acquire ControlFileLock before
    modifying ControlFile and calling UpdateControlFile().
    
    Back-patch to all supported releases.
    
    Author: Nathan Bossart <bossartn@amazon.com>
    Author: Fujii Masao <masao.fujii@oss.nttdata.com>
    Reviewed-by: Fujii Masao <masao.fujii@oss.nttdata.com>
    Reviewed-by: Michael Paquier <michael@paquier.xyz>
    Reviewed-by: Thomas Munro <thomas.munro@gmail.com>
    Discussion: https://postgr.es/m/70BF24D6-DC51-443F-B55A-95735803842A%40amazon.com

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -9539,6 +9539,8 @@ XLogReportParameters(void)
 			XLogFlush(recptr);
 		}
 
+		LWLockAcquire(ControlFileLock, LW_EXCLUSIVE);
+
 		ControlFile->MaxConnections = MaxConnections;
 		ControlFile->max_worker_processes = max_worker_processes;
 		ControlFile->max_wal_senders = max_wal_senders;
@@ -9548,6 +9550,8 @@ XLogReportParameters(void)
 		ControlFile->wal_log_hints = wal_log_hints;
 		ControlFile->track_commit_timestamp = track_commit_timestamp;
 		UpdateControlFile();
+
+		LWLockRelease(ControlFileLock);
 	}
 }
 
@@ -9772,7 +9776,9 @@ xlog_redo(XLogReaderState *record)
 		}
 
 		/* ControlFile->checkPointCopy always tracks the latest ckpt XID */
+		LWLockAcquire(ControlFileLock, LW_EXCLUSIVE);
 		ControlFile->checkPointCopy.nextFullXid = checkPoint.nextFullXid;
+		LWLockRelease(ControlFileLock);
 
 		/* Update shared-memory copy of checkpoint XID/epoch */
 		SpinLockAcquire(&XLogCtl->info_lck);
@@ -9829,7 +9835,9 @@ xlog_redo(XLogReaderState *record)
 			SetTransactionIdLimit(checkPoint.oldestXid,
 								  checkPoint.oldestXidDB);
 		/* ControlFile->checkPointCopy always tracks the latest ckpt XID */
+		LWLockAcquire(ControlFileLock, LW_EXCLUSIVE);
 		ControlFile->checkPointCopy.nextFullXid = checkPoint.nextFullXid;
+		LWLockRelease(ControlFileLock);
 
 		/* Update shared-memory copy of checkpoint XID/epoch */
 		SpinLockAcquire(&XLogCtl->info_lck);
