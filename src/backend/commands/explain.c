6d9f7a094ca7374554013759072e21cd90e19fb2
Tom Lane <tgl@sss.pgh.pa.us>
2020-06-22 11:46:41 -0400

    Undo double-quoting of index names in non-text EXPLAIN output formats.
    explain_get_index_name() applied quote_identifier() to the index name.
    This is fine for text output, but the non-text output formats all have
    their own quoting conventions and would much rather start from the
    actual index name.  For example in JSON you'd get something like
    
           "Index Name": "\"My Index\"",
    
    which is surely not desirable, especially when the same does not
    happen for table names.  Hence, move the responsibility for applying
    quoting out to the callers, where it can go into already-existing
    special code paths for text format.
    
    This changes the API spec for users of explain_get_index_name_hook:
    before, they were supposed to apply quote_identifier() if necessary,
    now they should not.  Research suggests that the only publicly
    available user of the hook is hypopg, and it actually forgot to
    apply quoting anyway, so it's fine.  (In any case, there's no
    behavioral change for the output of a hook as seen in non-text
    EXPLAIN formats, so this won't break any case that programs should
    be relying on.)
    
    Digging in the commit logs, it appears that quoting was included in
    explain_get_index_name's duties when commit 604ffd280 invented it;
    and that was fine at the time because we only had text output format.
    This should have been rethought when non-text formats were invented,
    but it wasn't.
    
    This is a fairly clear bug for users of non-text EXPLAIN formats,
    so back-patch to all supported branches.
    
    Per bug #16502 from Maciek Sakrejda.  Patch by me (based on
    investigation by Euler Taveira); thanks to Julien Rouhaud for review.
    
    Discussion: https://postgr.es/m/16502-57bd1c9f913ed1d1@postgresql.org

--- src/backend/commands/explain.c
+++ src/backend/commands/explain.c
@@ -1187,7 +1187,8 @@ ExplainNode(PlanState *planstate, List *ancestors,
 				explain_get_index_name(bitmapindexscan->indexid);
 
 				if (es->format == EXPLAIN_FORMAT_TEXT)
-					appendStringInfo(es->str, " on %s", indexname);
+					appendStringInfo(es->str, " on %s",
+									 quote_identifier(indexname));
 				else
 					ExplainPropertyText("Index Name", indexname, es);
 			}
@@ -2447,6 +2448,10 @@ show_foreignscan_info(ForeignScanState *fsstate, ExplainState *es)
  *
  * We allow plugins to get control here so that plans involving hypothetical
  * indexes can be explained.
+ *
+ * Note: names returned by this function should be "raw"; the caller will
+ * apply quoting if needed.  Formerly the convention was to do quoting here,
+ * but we don't want that in non-text output formats.
  */
 static const char *
 explain_get_index_name(Oid indexId)
@@ -2459,11 +2464,10 @@ explain_get_index_name(Oid indexId)
 		result = NULL;
 	if (result == NULL)
 	{
-		/* default behavior: look in the catalogs and quote it */
+		/* default behavior: look it up in the catalogs */
 		result = get_rel_name(indexId);
 		if (result == NULL)
 			elog(ERROR, "cache lookup failed for index %u", indexId);
-		result = quote_identifier(result);
 	}
 	return result;
 }
@@ -2591,7 +2595,7 @@ ExplainIndexScanDetails(Oid indexid, ScanDirection indexorderdir,
 	{
 		if (ScanDirectionIsBackward(indexorderdir))
 			appendStringInfoString(es->str, " Backward");
-		appendStringInfo(es->str, " using %s", indexname);
+		appendStringInfo(es->str, " using %s", quote_identifier(indexname));
 	}
 	else
 	{
