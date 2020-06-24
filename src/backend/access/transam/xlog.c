411493d701e2f97e778dc1ff14fb7169eea2e94c
Alvaro Herrera <alvherre@alvh.no-ip.org>
2020-06-24 14:00:37 -0400

    Add parens to ConvertToXSegs macro
    The current definition is dangerous.  No bugs exist in our code at
    present, but backpatch to 11 nonetheless where it was introduced.
    
    Author: Álvaro Herrera <alvherre@alvh.no-ip.org>

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -760,9 +760,12 @@ static ControlFileData *ControlFile = NULL;
  */
 #define UsableBytesInPage (XLOG_BLCKSZ - SizeOfXLogShortPHD)
 
-/* Convert values of GUCs measured in megabytes to equiv. segment count */
+/*
+ * Convert values of GUCs measured in megabytes to equiv. segment count.
+ * Rounds down.
+ */
 #define ConvertToXSegs(x, segsize)	\
-	(x / ((segsize) / (1024 * 1024)))
+	((x) / ((segsize) / (1024 * 1024)))
 
 /* The number of bytes in a WAL segment usable for WAL data. */
 static int	UsableBytesInSegment;
