794e8e32bb5ae1b38a90cbae2a88895633797599
Alvaro Herrera <alvherre@alvh.no-ip.org>
2020-07-13 13:49:50 -0400

    Fix uninitialized value in segno calculation
    Remove previous hack in KeepLogSeg that added a case to deal with a
    (badly represented) invalid segment number.  This was added for the sake
    of GetWALAvailability.  But it's not needed if in that function we
    initialize the segment number to be retreated to the currently being
    written segment, so do that instead.
    
    Per valgrind-running buildfarm member skink, and some sparc64 animals.
    
    Discussion: https://postgr.es/m/1724648.1594230917@sss.pgh.pa.us

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -9521,13 +9521,13 @@ GetWALAvailability(XLogRecPtr targetLSN)
 	if (XLogRecPtrIsInvalid(targetLSN))
 		return WALAVAIL_INVALID_LSN;
 
-	currpos = GetXLogWriteRecPtr();
-
 	/*
-	 * calculate the oldest segment currently reserved by all slots,
-	 * considering wal_keep_segments and max_slot_wal_keep_size
+	 * Calculate the oldest segment currently reserved by all slots,
+	 * considering wal_keep_segments and max_slot_wal_keep_size.  Initialize
+	 * oldestSlotSeg to the current segment.
 	 */
-	XLByteToSeg(targetLSN, targetSeg, wal_segment_size);
+	currpos = GetXLogWriteRecPtr();
+	XLByteToSeg(currpos, oldestSlotSeg, wal_segment_size);
 	KeepLogSeg(currpos, &oldestSlotSeg);
 
 	/*
@@ -9545,6 +9545,9 @@ GetWALAvailability(XLogRecPtr targetLSN)
 		oldestSegMaxWalSize = currSeg - keepSegs;
 	else
 		oldestSegMaxWalSize = 1;
+
+	/* the segment we care about */
+	XLByteToSeg(targetLSN, targetSeg, wal_segment_size);
 
 	/*
 	 * No point in returning reserved or extended status values if the
@@ -9622,7 +9625,7 @@ KeepLogSeg(XLogRecPtr recptr, XLogSegNo *logSegNo)
 	}
 
 	/* don't delete WAL segments newer than the calculated segment */
-	if (XLogRecPtrIsInvalid(*logSegNo) || segno < *logSegNo)
+	if (segno < *logSegNo)
 		*logSegNo = segno;
 }
 
