7b1110d2fd3da3d7536530d14952d4f4d9c25438
Michael Paquier <michael@paquier.xyz>
2020-07-31 14:17:29 +0900

    Fix comment in instrument.h
    local_blks_dirtied tracks the number of local blocks dirtied, not shared
    ones.
    
    Author: Kirk Jamison
    Discussion: https://postgr.es/m/OSBPR01MB2341760686DC056DE89D2AB9EF710@OSBPR01MB2341.jpnprd01.prod.outlook.com

--- src/include/executor/instrument.h
+++ src/include/executor/instrument.h
@@ -24,7 +24,7 @@ typedef struct BufferUsage
 	long		shared_blks_written;	/* # of shared disk blocks written */
 	long		local_blks_hit; /* # of local buffer hits */
 	long		local_blks_read;	/* # of local disk blocks read */
-	long		local_blks_dirtied; /* # of shared blocks dirtied */
+	long		local_blks_dirtied; /* # of local blocks dirtied */
 	long		local_blks_written; /* # of local disk blocks written */
 	long		temp_blks_read; /* # of temp blocks read */
 	long		temp_blks_written;	/* # of temp blocks written */
