a4ad7ac2d8d422f6aad3dec46f13e6b0bd6bd7ed
Peter Geoghegan <pg@bowt.ie>
2020-07-31 15:34:23 -0700

    Restore lost amcheck TOAST test coverage.
    Commit eba77534 fixed an amcheck false positive bug involving
    inconsistencies in TOAST input state between table and index.  A test
    case was added that verified that such an inconsistency didn't result in
    a spurious corruption related error.
    
    Test coverage from the test was accidentally lost by commit 501e41dd,
    which propagated ALTER TABLE ...  SET STORAGE attstorage state to
    indexes.  This broke the test because the test specifically relied on
    attstorage not being propagated.  This artificially forced there to be
    index tuples whose datums were equivalent to the datums in the heap
    without the datums actually being bitwise equal.
    
    Fix this by updating pg_attribute directly instead.  Commit 501e41dd
    made similar changes to a test_decoding TOAST-related test case which
    made the same assumption, but overlooked the amcheck test case.
    
    Backpatch: 11-, just like commit eba77534 (and commit 501e41dd).

--- contrib/amcheck/sql/check_btree.sql
+++ contrib/amcheck/sql/check_btree.sql
@@ -93,11 +93,17 @@ SELECT bt_index_parent_check('delete_test_table_pkey', true);
 -- tuple.  Bloom filter must fingerprint normalized index tuple representation.
 --
 CREATE TABLE toast_bug(buggy text);
-ALTER TABLE toast_bug ALTER COLUMN buggy SET STORAGE plain;
--- pg_attribute entry for toasty.buggy will have plain storage:
-CREATE INDEX toasty ON toast_bug(buggy);
--- Whereas pg_attribute entry for toast_bug.buggy now has extended storage:
 ALTER TABLE toast_bug ALTER COLUMN buggy SET STORAGE extended;
+CREATE INDEX toasty ON toast_bug(buggy);
+
+-- pg_attribute entry for toasty.buggy (the index) will have plain storage:
+UPDATE pg_attribute SET attstorage = 'p'
+WHERE attrelid = 'toasty'::regclass AND attname = 'buggy';
+
+-- Whereas pg_attribute entry for toast_bug.buggy (the table) still has extended storage:
+SELECT attstorage FROM pg_attribute
+WHERE attrelid = 'toast_bug'::regclass AND attname = 'buggy';
+
 -- Insert compressible heap tuple (comfortably exceeds TOAST_TUPLE_THRESHOLD):
 INSERT INTO toast_bug SELECT repeat('a', 2200);
 -- Should not get false positive report of corruption:
