82a0ba7707e010a29f5fe1a0020d963c82b8f1cb
Amit Kapila <akapila@postgresql.org>
2020-08-08 12:13:18 +0530

    Fix the logical streaming test.
    Commit 7259736a6e added the capability to stream changes in ReorderBuffer
    which has some tests to test the streaming mode. It is quite possible that
    while this test is running a parallel transaction could be logged by
    autovacuum. Such a transaction won't perform any insert/update/delete to
    non-catalog tables so will be shown as an empty transaction. Fix it by
    skipping the empty transactions during this test.
    
    Per report by buildfarm.

--- contrib/test_decoding/sql/stream.sql
+++ contrib/test_decoding/sql/stream.sql
@@ -16,7 +16,7 @@ rollback to s1;
 INSERT INTO stream_test SELECT repeat('a', 10) || g.i FROM generate_series(1, 20) g(i);
 COMMIT;
 
-SELECT data FROM pg_logical_slot_get_changes('regression_slot', NULL,NULL, 'include-xids', '0', 'stream-changes', '1');
+SELECT data FROM pg_logical_slot_get_changes('regression_slot', NULL,NULL, 'include-xids', '0', 'skip-empty-xacts', '1', 'stream-changes', '1');
 
 -- streaming test for toast changes
 ALTER TABLE stream_test ALTER COLUMN data set storage external;
@@ -24,7 +24,7 @@ ALTER TABLE stream_test ALTER COLUMN data set storage external;
 SELECT data FROM pg_logical_slot_get_changes('regression_slot', NULL, NULL, 'include-xids', '0', 'skip-empty-xacts', '1');
 
 INSERT INTO stream_test SELECT repeat('a', 6000) || g.i FROM generate_series(1, 10) g(i);
-SELECT data FROM pg_logical_slot_get_changes('regression_slot', NULL,NULL, 'include-xids', '0', 'stream-changes', '1');
+SELECT data FROM pg_logical_slot_get_changes('regression_slot', NULL,NULL, 'include-xids', '0', 'skip-empty-xacts', '1', 'stream-changes', '1');
 
 DROP TABLE stream_test;
 SELECT pg_drop_replication_slot('regression_slot');
