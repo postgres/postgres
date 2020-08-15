53095b5fe650270118bc2ab77416d08e19472cd3
Peter Eisentraut <peter@eisentraut.org>
2020-08-15 11:23:18 +0200

    Remove obsolete cygwin.h hack
    The version being checked for is 20 years old.
    
    Reviewed-by: Marco Atzeri <marco.atzeri@gmail.com>
    Discussion: https://www.postgresql.org/message-id/flat/aa266ede-baaa-f4e6-06cf-5b1737610e9a%402ndquadrant.com

--- src/include/port/cygwin.h
+++ src/include/port/cygwin.h
@@ -1,14 +1,5 @@
 /* src/include/port/cygwin.h */
 
-#include <cygwin/version.h>
-
-/*
- * Check for b20.1 and disable AF_UNIX family socket support.
- */
-#if CYGWIN_VERSION_DLL_MAJOR < 1001
-#undef HAVE_UNIX_SOCKETS
-#endif
-
 #ifdef BUILDING_DLL
 #define PGDLLIMPORT __declspec (dllexport)
 #else
