d4d443b3bbbb3eb9cdc511564ef3c57fde7dd3ac
Tom Lane <tgl@sss.pgh.pa.us>
2020-08-15 12:04:19 -0400

    Remove no-longer-usable hstore--1.0--1.1.sql update script.
    Since commit 865f14a2d made "=>" unusable as an operator name,
    it's been impossible either to install hstore 1.0 or to execute
    this update script.  There's not much point in continuing
    to ship it.
    
    Discussion: https://postgr.es/m/653936.1597431032@sss.pgh.pa.us

--- contrib/hstore/hstore--1.0--1.1.sql
+++
@@ -1,7 +0,0 @@
-/* contrib/hstore/hstore--1.0--1.1.sql */
-
--- complain if script is sourced in psql, rather than via ALTER EXTENSION
-\echo Use "ALTER EXTENSION hstore UPDATE TO '1.1'" to load this file. \quit
-
-ALTER EXTENSION hstore DROP OPERATOR => (text, text);
-DROP OPERATOR => (text, text);
