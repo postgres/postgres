cb8885ac49697eb2568c4764ae3565cea52be92b
Tom Lane <tgl@sss.pgh.pa.us>
2020-09-24 18:19:38 -0400

    Fix handling of -d "connection string" in pg_dump/pg_restore.
    Parallel pg_dump failed if its -d parameter was a connection string
    containing any essential information other than host, port, or username.
    The same was true for pg_restore with --create.
    
    The reason is that these scenarios failed to preserve the connection
    string from the command line; the code felt free to replace that with
    just the database name when reconnecting from a pg_dump parallel worker
    or after creating the target database.  By chance, parallel pg_restore
    did not suffer this defect, as long as you didn't say --create.
    
    In practice it seems that the error would be obvious only if the
    connstring included essential, non-default SSL or GSS parameters.
    This may explain why it took us so long to notice.  (It also makes
    it very difficult to craft a regression test case illustrating the
    problem, since the test would fail in builds without those options.)
    
    Fix by refactoring so that ConnectDatabase always receives all the
    relevant options directly from the command line, rather than
    reconstructed values.  Inject a different database name, when necessary,
    by relying on libpq's rules for handling multiple "dbname" parameters.
    
    While here, let's get rid of the essentially duplicate _connectDB
    function, as well as some obsolete nearby cruft.
    
    Per bug #16604 from Zsolt Ero.  Back-patch to all supported branches.
    
    Discussion: https://postgr.es/m/16604-933f4b8791227b15@postgresql.org

--- src/bin/pg_dump/pg_restore.c
+++ src/bin/pg_dump/pg_restore.c
@@ -163,7 +163,7 @@ main(int argc, char **argv)
 				opts->createDB = 1;
 				break;
 			case 'd':
-				opts->dbname = pg_strdup(optarg);
+				opts->cparams.dbname = pg_strdup(optarg);
 				break;
 			case 'e':
 				opts->exit_on_error = true;
@@ -177,7 +177,7 @@ main(int argc, char **argv)
 				break;
 			case 'h':
 				if (strlen(optarg) != 0)
-					opts->pghost = pg_strdup(optarg);
+					opts->cparams.pghost = pg_strdup(optarg);
 				break;
 
 			case 'j':			/* number of restore jobs */
@@ -206,7 +206,7 @@ main(int argc, char **argv)
 
 			case 'p':
 				if (strlen(optarg) != 0)
-					opts->pgport = pg_strdup(optarg);
+					opts->cparams.pgport = pg_strdup(optarg);
 				break;
 			case 'R':
 				/* no-op, still accepted for backwards compatibility */
@@ -240,7 +240,7 @@ main(int argc, char **argv)
 				break;
 
 			case 'U':
-				opts->username = pg_strdup(optarg);
+				opts->cparams.username = pg_strdup(optarg);
 				break;
 
 			case 'v':			/* verbose */
@@ -249,11 +249,11 @@ main(int argc, char **argv)
 				break;
 
 			case 'w':
-				opts->promptPassword = TRI_NO;
+				opts->cparams.promptPassword = TRI_NO;
 				break;
 
 			case 'W':
-				opts->promptPassword = TRI_YES;
+				opts->cparams.promptPassword = TRI_YES;
 				break;
 
 			case 'x':			/* skip ACL dump */
@@ -303,14 +303,14 @@ main(int argc, char **argv)
 	}
 
 	/* Complain if neither -f nor -d was specified (except if dumping TOC) */
-	if (!opts->dbname && !opts->filename && !opts->tocSummary)
+	if (!opts->cparams.dbname && !opts->filename && !opts->tocSummary)
 	{
 		pg_log_error("one of -d/--dbname and -f/--file must be specified");
 		exit_nicely(1);
 	}
 
 	/* Should get at most one of -d and -f, else user is confused */
-	if (opts->dbname)
+	if (opts->cparams.dbname)
 	{
 		if (opts->filename)
 		{
