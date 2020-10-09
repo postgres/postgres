a9beed67670e680edeadd2a3cf7557a3c9808adf
Tom Lane <tgl@sss.pgh.pa.us>
2020-10-09 16:20:12 -0400

    Fix our Windows stat() emulation to handle file sizes > 4GB.
    Hack things so that our idea of "struct stat" is equivalent to Windows'
    struct __stat64, allowing it to have a wide enough st_size field.
    
    Instead of relying on native stat(), use GetFileInformationByHandle().
    This avoids a number of issues with Microsoft's multiple and rather
    slipshod emulations of stat().  We still need to jump through hoops
    to deal with ERROR_DELETE_PENDING, though :-(
    
    Pull the relevant support code out of dirmod.c and put it into
    its own file, win32stat.c.
    
    Still TODO: do we need to do something different with lstat(),
    rather than treating it identically to stat()?
    
    Juan José Santamaría Flecha, reviewed by Emil Iggland;
    based on prior work by Michael Paquier, Sergey Zubkovsky, and others
    
    Discussion: https://postgr.es/m/1803D792815FC24D871C00D17AE95905CF5099@g01jpexmbkw24
    Discussion: https://postgr.es/m/15858-9572469fd3b73263@postgresql.org
    (cherry picked from commit bed90759fcbcd72d4d06969eebab81e47326f9a2)
    
    Author: Alexandra Wang <alexandra.wang.oss@gmail.com>

--- src/tools/msvc/Mkvcbuild.pm
+++ src/tools/msvc/Mkvcbuild.pm
@@ -103,7 +103,7 @@ sub mkvcbuild
 	  pg_strong_random.c pgcheckdir.c pgmkdirp.c pgsleep.c pgstrcasecmp.c
 	  pqsignal.c mkdtemp.c qsort.c qsort_arg.c quotes.c setenv.c system.c
 	  sprompt.c strerror.c tar.c thread.c
-	  win32env.c win32error.c win32security.c win32setlocale.c);
+	  win32env.c win32error.c win32security.c win32setlocale.c win32stat.c);
 
 	push(@pgportfiles, 'strtof.c') if ($vsVersion < '14.00');
 
