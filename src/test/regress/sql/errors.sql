ae0f7b11f143d9748e2201c3647330893c4c1c5a
Tom Lane <tgl@sss.pgh.pa.us>
2020-10-13 17:44:56 -0400

    Paper over regression failures in infinite_recurse() on PPC64 Linux.
    Our infinite_recurse() test to verify sane stack-overrun behavior
    is affected by a bug of the Linux kernel on PPC64: it will get SIGSEGV
    if it receives a signal when the stack depth is (a) over 1MB and
    (b) within a few kB of filling the current physical stack allocation.
    See https://bugzilla.kernel.org/show_bug.cgi?id=205183.
    
    Since this test is a bit time-consuming and we run it in parallel with
    test scripts that do a lot of DDL, it can be expected to get an sinval
    catchup interrupt at some point, leading to failure if the timing is
    wrong.  This has caused more than 100 buildfarm failures over the
    past year or so.
    
    While a fix exists for the kernel bug, it might be years before that
    propagates into all production kernels, particularly in some of the
    older distros we have in the buildfarm.  For now, let's just back off
    and not run this test on Linux PPC64; that loses nothing in test
    coverage so far as our own code is concerned.
    
    To do that, split this test into a new script infinite_recurse.sql
    and skip the test when the platform name is powerpc64...-linux-gnu.
    
    Back-patch to v12.  Branches before that have not been seen to get
    this failure.  No doubt that's because the "errors" test was not
    run in parallel with other tests before commit 798070ec0, greatly
    reducing the odds of an sinval catchup being necessary.
    
    I also back-patched 3c8553547 into v12, just so the new regression
    script would look the same in all branches having it.
    
    Discussion: https://postgr.es/m/3479046.1602607848@sss.pgh.pa.us
    Discussion: https://postgr.es/m/20190723162703.GM22387%40telsasoft.com

--- src/test/regress/sql/errors.sql
+++ src/test/regress/sql/errors.sql
@@ -364,12 +364,3 @@ INT4
 UNIQUE
 NOT
 NULL);
-
--- Check that stack depth detection mechanism works and
--- max_stack_depth is not set too high.  The full error report is not
--- very stable, so show only SQLSTATE and primary error message.
-create function infinite_recurse() returns int as
-'select infinite_recurse()' language sql;
-\set VERBOSITY sqlstate
-select infinite_recurse();
-\echo :LAST_ERROR_MESSAGE
