6a7b55f3716fad9c40ecb960cb7b7d616d5b02fd
Tomas Vondra <tomas.vondra@postgresql.org>
2020-11-07 00:40:06 +0100

    Properly detoast data in brin_form_tuple
    brin_form_tuple failed to consider the values may be toasted, inserting
    the toast pointer into the index. This may easily result in index
    corruption, as the toast data may be deleted and cleaned up by vacuum.
    The cleanup however does not care about indexes, leaving invalid toast
    pointers behind, which triggers errors like this:
    
      ERROR:  missing chunk number 0 for toast value 16433 in pg_toast_16426
    
    A less severe consequence are inconsistent failures due to the index row
    being too large, depending on whether brin_form_tuple operated on plain
    or toasted version of the row. For example
    
        CREATE TABLE t (val TEXT);
        INSERT INTO t VALUES ('... long value ...')
        CREATE INDEX idx ON t USING brin (val);
    
    would likely succeed, as the row would likely include toast pointer.
    Switching the order of INSERT and CREATE INDEX would likely fail:
    
        ERROR:  index row size 8712 exceeds maximum 8152 for index "idx"
    
    because this happens before the row values are toasted.
    
    The bug exists since PostgreSQL 9.5 where BRIN indexes were introduced.
    So backpatch all the way back.
    
    Author: Tomas Vondra
    Reviewed-by: Alvaro Herrera
    Backpatch-through: 9.5
    Discussion: https://postgr.es/m/20201001184133.oq5uq75sb45pu3aw@development
    Discussion: https://postgr.es/m/20201104010544.zexj52mlldagzowv%40development

--- src/test/regress/sql/brin.sql
+++ src/test/regress/sql/brin.sql
@@ -470,3 +470,42 @@ VACUUM ANALYZE brin_test;
 EXPLAIN (COSTS OFF) SELECT * FROM brin_test WHERE a = 1;
 -- Ensure brin index is not used when values are not correlated
 EXPLAIN (COSTS OFF) SELECT * FROM brin_test WHERE b = 1;
+
+-- make sure data are properly de-toasted in BRIN index
+CREATE TABLE brintest_3 (a text, b text, c text, d text);
+
+-- long random strings (~2000 chars each, so ~6kB for min/max on two
+-- columns) to trigger toasting
+WITH rand_value AS (SELECT string_agg(md5(i::text),'') AS val FROM generate_series(1,60) s(i))
+INSERT INTO brintest_3
+SELECT val, val, val, val FROM rand_value;
+
+CREATE INDEX brin_test_toast_idx ON brintest_3 USING brin (b, c);
+DELETE FROM brintest_3;
+
+-- We need to wait a bit for all transactions to complete, so that the
+-- vacuum actually removes the TOAST rows. Creating an index concurrently
+-- is a one way to achieve that, because it does exactly such wait.
+CREATE INDEX CONCURRENTLY brin_test_temp_idx ON brintest_3(a);
+DROP INDEX brin_test_temp_idx;
+
+-- vacuum the table, to discard TOAST data
+VACUUM brintest_3;
+
+-- retry insert with a different random-looking (but deterministic) value
+-- the value is different, and so should replace either min or max in the
+-- brin summary
+WITH rand_value AS (SELECT string_agg(md5((-i)::text),'') AS val FROM generate_series(1,60) s(i))
+INSERT INTO brintest_3
+SELECT val, val, val, val FROM rand_value;
+
+-- now try some queries, accessing the brin index
+SET enable_seqscan = off;
+
+EXPLAIN (COSTS OFF)
+SELECT * FROM brintest_3 WHERE b < '0';
+
+SELECT * FROM brintest_3 WHERE b < '0';
+
+DROP TABLE brintest_3;
+RESET enable_seqscan;
