951dfa34f41bb851602216d3ada5f80135370e4f
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2020-11-12 19:18:34 +0200

    Remove tests that don't work on Windows.
    On Windows, a filename cannot contain backslashes, because a backslash
    is used directory separator. Remove tests I added in commit 9c4f5192f
    that tried to do that. We could perhaps use a SKIP block to only skip
    them on Windows, but I'm not sure how exactly to formulate that, so just
    remove the tests to make the buildfarm green again.
    
    Per buildfarm.

--- src/bin/pg_rewind/t/003_extrafiles.pl
+++ src/bin/pg_rewind/t/003_extrafiles.pl
@@ -46,16 +46,10 @@ sub run_test
 	append_to_file
 	  "$test_standby_datadir/tst_standby_dir/standby_file4 with double\"quote",
 	  "in standby4";
-	append_to_file
-	  "$test_standby_datadir/tst_standby_dir/standby_file5 with back\\slash",
-	  "in standby5";
-	append_to_file
-	  "$test_standby_datadir/tst_standby_dir/standby_file6_with_backslash\\\"and_double-quote",
-	  "in standby6";
 	mkdir "$test_standby_datadir/tst_standby_dir/standby_subdir/";
 	append_to_file
-	  "$test_standby_datadir/tst_standby_dir/standby_subdir/standby_file7",
-	  "in standby7";
+	  "$test_standby_datadir/tst_standby_dir/standby_subdir/standby_file5",
+	  "in standby5";
 
 	mkdir "$test_primary_datadir/tst_primary_dir";
 	append_to_file "$test_primary_datadir/tst_primary_dir/primary_file1",
@@ -94,10 +88,8 @@ sub run_test
 			"$test_primary_datadir/tst_standby_dir/standby_file2",
 			"$test_primary_datadir/tst_standby_dir/standby_file3 with 'quotes'",
 			"$test_primary_datadir/tst_standby_dir/standby_file4 with double\"quote",
-			"$test_primary_datadir/tst_standby_dir/standby_file5 with back\\slash",
-			"$test_primary_datadir/tst_standby_dir/standby_file6_with_backslash\\\"and_double-quote",
 			"$test_primary_datadir/tst_standby_dir/standby_subdir",
-			"$test_primary_datadir/tst_standby_dir/standby_subdir/standby_file7"
+			"$test_primary_datadir/tst_standby_dir/standby_subdir/standby_file5"
 		],
 		"file lists match");
 
