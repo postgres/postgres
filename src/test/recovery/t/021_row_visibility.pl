08dde1b3dc78e90e01f3af8e89ab27ed37cd8963
Tom Lane <tgl@sss.pgh.pa.us>
2020-12-22 11:10:12 -0500

    Increase timeout in 021_row_visibility.pl.
    Commit 7b28913bc figured 30 seconds is long enough for anybody,
    but in contexts like valgrind runs, it isn't necessarily.

--- src/test/recovery/t/021_row_visibility.pl
+++ src/test/recovery/t/021_row_visibility.pl
@@ -33,7 +33,7 @@ $node_standby->start;
 # instance being driven by us, add a timeout high enough that it
 # should never trigger even on very slow machines, unless something
 # is really wrong.
-my $psql_timeout = IPC::Run::timer(30);
+my $psql_timeout = IPC::Run::timer(300);
 
 # One psql to primary and standby each, for all queries. That allows
 # to check uncommitted changes being replicated and such.
