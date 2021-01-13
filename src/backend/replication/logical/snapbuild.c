0685c5c1b9225352bbaf4fe81c550f09508379ce
Amit Kapila <akapila@postgresql.org>
2021-01-13 08:31:45 +0530

    Fix memory leak in SnapBuildSerialize.
    The memory for the snapshot was leaked while serializing it to disk during
    logical decoding. This memory will be freed only once walsender stops
    streaming the changes. This can lead to a huge memory increase when master
    logs Standby Snapshot too frequently say when the user is trying to create
    many replication slots.
    
    Reported-by: funnyxj.fxj@alibaba-inc.com
    Diagnosed-by: funnyxj.fxj@alibaba-inc.com
    Author: Amit Kapila
    Backpatch-through: 9.5
    Discussion: https://postgr.es/m/033ab54c-6393-42ee-8ec9-2b399b5d8cde.funnyxj.fxj@alibaba-inc.com

--- src/backend/replication/logical/snapbuild.c
+++ src/backend/replication/logical/snapbuild.c
@@ -1480,7 +1480,7 @@ static void
 SnapBuildSerialize(SnapBuild *builder, XLogRecPtr lsn)
 {
 	Size		needed_length;
-	SnapBuildOnDisk *ondisk;
+	SnapBuildOnDisk *ondisk = NULL;
 	char	   *ondisk_c;
 	int			fd;
 	char		tmppath[MAXPGPATH];
@@ -1679,6 +1679,9 @@ SnapBuildSerialize(SnapBuild *builder, XLogRecPtr lsn)
 out:
 	ReorderBufferSetRestartPoint(builder->reorder,
 								 builder->last_serialized_snapshot);
+	/* be tidy */
+	if (ondisk)
+		pfree(ondisk);
 }
 
 /*
