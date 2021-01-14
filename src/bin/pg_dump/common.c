bb12a7f429ca1b10aebc469d9379837fa3d09220
Tom Lane <tgl@sss.pgh.pa.us>
2021-01-14 16:19:38 -0500

    pg_dump: label PUBLICATION TABLE ArchiveEntries with an owner.
    This is the same fix as commit 9eabfe300 applied to INDEX ATTACH
    entries, but for table-to-publication attachments.  As in that
    case, even though the backend doesn't record "ownership" of the
    attachment, we still ought to label it in the dump archive with
    the role name that should run the ALTER PUBLICATION command.
    The existing behavior causes the ALTER to be done by the original
    role that started the restore; that will usually work fine, but
    there may be corner cases where it fails.
    
    The bulk of the patch is concerned with changing struct
    PublicationRelInfo to include a pointer to the associated
    PublicationInfo object, so that we can get the owner's name
    out of that when the time comes.  While at it, I rewrote
    getPublicationTables() to do just one query of pg_publication_rel,
    not one per table.
    
    Back-patch to v10 where this code was introduced.
    
    Discussion: https://postgr.es/m/1165710.1610473242@sss.pgh.pa.us

--- src/bin/pg_dump/common.c
+++ src/bin/pg_dump/common.c
@@ -54,6 +54,7 @@ static DumpableObject **oprinfoindex;
 static DumpableObject **collinfoindex;
 static DumpableObject **nspinfoindex;
 static DumpableObject **extinfoindex;
+static DumpableObject **pubinfoindex;
 static int	numTables;
 static int	numTypes;
 static int	numFuncs;
@@ -61,6 +62,7 @@ static int	numOperators;
 static int	numCollations;
 static int	numNamespaces;
 static int	numExtensions;
+static int	numPublications;
 
 /* This is an array of object identities, not actual DumpableObjects */
 static ExtensionMemberId *extmembers;
@@ -95,6 +97,7 @@ getSchemaData(Archive *fout, int *numTablesPtr)
 	CollInfo   *collinfo;
 	NamespaceInfo *nspinfo;
 	ExtensionInfo *extinfo;
+	PublicationInfo *pubinfo;
 	InhInfo    *inhinfo;
 	int			numAggregates;
 	int			numInherits;
@@ -286,7 +289,9 @@ getSchemaData(Archive *fout, int *numTablesPtr)
 
 	if (g_verbose)
 		write_msg(NULL, "reading publications\n");
-	getPublications(fout);
+	pubinfo = getPublications(fout, &numPublications);
+	pubinfoindex = buildIndexArray(pubinfo, numPublications,
+								   sizeof(PublicationInfo));
 
 	if (g_verbose)
 		write_msg(NULL, "reading publication membership\n");
@@ -933,6 +938,17 @@ ExtensionInfo *
 findExtensionByOid(Oid oid)
 {
 	return (ExtensionInfo *) findObjectByOid(oid, extinfoindex, numExtensions);
+}
+
+/*
+ * findPublicationByOid
+ *	  finds the entry (in pubinfo) of the publication with the given oid
+ *	  returns NULL if not found
+ */
+PublicationInfo *
+findPublicationByOid(Oid oid)
+{
+	return (PublicationInfo *) findObjectByOid(oid, pubinfoindex, numPublications);
 }
 
 /*
