1c3a4d44d84437b40e8311a0810d2a189e54e291
Michael Paquier <michael@paquier.xyz>
2021-01-20 11:39:21 +0900

    Fix ALTER DEFAULT PRIVILEGES with duplicated objects
    Specifying duplicated objects in this command would lead to unique
    constraint violations in pg_default_acl or "tuple already updated by
    self" errors.  Similarly to GRANT/REVOKE, increment the command ID after
    each subcommand processing to allow this case to work transparently.
    
    A regression test is added by tweaking one of the existing queries of
    privileges.sql to stress this case.
    
    Reported-by: Andrus
    Author: Michael Paquier
    Reviewed-by: Álvaro Herrera
    Discussion: https://postgr.es/m/ae2a7dc1-9d71-8cba-3bb9-e4cb7eb1f44e@hot.ee
    Backpatch-through: 9.5

--- src/backend/catalog/aclchk.c
+++ src/backend/catalog/aclchk.c
@@ -1383,6 +1383,9 @@ SetDefaultACL(InternalDefaultACL *iacls)
 		ReleaseSysCache(tuple);
 
 	heap_close(rel, RowExclusiveLock);
+
+	/* prevent error when processing duplicate objects */
+	CommandCounterIncrement();
 }
 
 
