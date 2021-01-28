1b242f42ba1e732b202f35265ab1a1614ce7d859
Tom Lane <tgl@sss.pgh.pa.us>
2021-01-28 11:17:13 -0500

    Make ecpg's rjulmdy() and rmdyjul() agree with their declarations.
    We had "short *mdy" in the extern declarations, but "short mdy[3]"
    in the actual function definitions.  Per C99 these are equivalent,
    but recent versions of gcc have started to issue warnings about
    the inconsistency.  Clean it up before the warnings get any more
    widespread.
    
    Back-patch, in case anyone wants to build older PG versions with
    bleeding-edge compilers.
    
    Discussion: https://postgr.es/m/2401575.1611764534@sss.pgh.pa.us

--- src/interfaces/ecpg/compatlib/informix.c
+++ src/interfaces/ecpg/compatlib/informix.c
@@ -513,7 +513,7 @@ rtoday(date * d)
 }
 
 int
-rjulmdy(date d, short mdy[3])
+rjulmdy(date d, short *mdy)
 {
 	int			mdy_int[3];
 
@@ -564,7 +564,7 @@ rfmtdate(date d, const char *fmt, char *str)
 }
 
 int
-rmdyjul(short mdy[3], date * d)
+rmdyjul(short *mdy, date * d)
 {
 	int			mdy_int[3];
 
