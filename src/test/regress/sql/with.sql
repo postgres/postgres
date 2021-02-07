46f2971135984a2666a279faab8b61aa1f1904bd
Tom Lane <tgl@sss.pgh.pa.us>
2021-02-07 12:54:08 -0500

    Revert "Propagate CTE property flags when copying a CTE list into a rule."
    This reverts commit ed290896335414c6c069b9ccae1f3dcdd2fac6ba and
    equivalent back-branch commits.  The issue is subtler than I thought,
    and it's far from new, so just before a release deadline is no time
    to be fooling with it.  We'll consider what to do at a bit more
    leisure.
    
    Discussion: https://postgr.es/m/CAJcOf-fAdj=nDKMsRhQzndm-O13NY4dL6xGcEvdX5Xvbbi0V7g@mail.gmail.com

--- src/test/regress/sql/with.sql
+++ src/test/regress/sql/with.sql
@@ -773,22 +773,6 @@ INSERT INTO bug6051 SELECT * FROM t1;
 SELECT * FROM bug6051;
 SELECT * FROM bug6051_2;
 
--- silly example to verify that hasModifyingCTE flag is propagated
-CREATE TEMP TABLE bug6051_3 AS
-  select a from generate_series(11,13) as a;
-
-CREATE RULE bug6051_3_ins AS ON INSERT TO bug6051_3 DO INSTEAD
-  SELECT i FROM bug6051_2;
-
-BEGIN; SET LOCAL force_parallel_mode = on;
-
-WITH t1 AS ( DELETE FROM bug6051_3 RETURNING * )
-  INSERT INTO bug6051_3 SELECT * FROM t1;
-
-COMMIT;
-
-SELECT * FROM bug6051_3;
-
 -- a truly recursive CTE in the same list
 WITH RECURSIVE t(a) AS (
 	SELECT 0
