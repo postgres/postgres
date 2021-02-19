be7485a1e31122f92b6dd9ad02427c1daa870b63
Fujii Masao <fujii@postgresql.org>
2021-02-19 22:01:25 +0900

    Fix psql's ON_ERROR_ROLLBACK so that it handles COMMIT AND CHAIN.
    When ON_ERROR_ROLLBACK is enabled, psql releases a temporary savepoint
    if it's idle in a valid transaction block after executing a query. But psql
    doesn't do that after RELEASE or ROLLBACK is executed because a temporary
    savepoint has already been destroyed in that case.
    
    This commit changes psql's ON_ERROR_ROLLBACK so that it doesn't release
    a temporary savepoint also when COMMIT AND CHAIN is executed. A temporary
    savepoint doesn't need to be released in that case because
    COMMIT AND CHAIN also destroys any savepoints defined within the transaction
    to commit. Otherwise psql tries to release the savepoint that
    COMMIT AND CHAIN has already destroyed and cause an error
    "ERROR:  savepoint "pg_psql_temporary_savepoint" does not exist".
    
    Back-patch to v12 where transaction chaining was added.
    
    Reported-by: Arthur Nascimento
    Author: Arthur Nascimento
    Reviewed-by: Fujii Masao, Vik Fearing
    Discussion: https://postgr.es/m/16867-3475744069228158@postgresql.org

--- src/bin/psql/common.c
+++ src/bin/psql/common.c
@@ -1346,12 +1346,13 @@ SendQuery(const char *query)
 
 				/*
 				 * Do nothing if they are messing with savepoints themselves:
-				 * If the user did RELEASE or ROLLBACK, our savepoint is gone.
-				 * If they issued a SAVEPOINT, releasing ours would remove
-				 * theirs.
+				 * If the user did COMMIT AND CHAIN, RELEASE or ROLLBACK, our
+				 * savepoint is gone. If they issued a SAVEPOINT, releasing
+				 * ours would remove theirs.
 				 */
 				if (results &&
-					(strcmp(PQcmdStatus(results), "SAVEPOINT") == 0 ||
+					(strcmp(PQcmdStatus(results), "COMMIT") == 0 ||
+					 strcmp(PQcmdStatus(results), "SAVEPOINT") == 0 ||
 					 strcmp(PQcmdStatus(results), "RELEASE") == 0 ||
 					 strcmp(PQcmdStatus(results), "ROLLBACK") == 0))
 					svptcmd = NULL;
