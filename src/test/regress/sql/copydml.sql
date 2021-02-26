b3a9e9897ec702d56602b26a8cdc0950f23b29dc
Peter Eisentraut <peter@eisentraut.org>
2021-02-26 09:11:15 +0100

    Extend a test case a little
    This will possibly help a subsequent patch by making sure the notice
    messages are distinct so that it's clear that they come out in the
    right order.
    
    Author: Fabien COELHO <coelho@cri.ensmp.fr>
    Discussion: https://www.postgresql.org/message-id/alpine.DEB.2.21.1904240654120.3407%40lancre

--- src/test/regress/sql/copydml.sql
+++ src/test/regress/sql/copydml.sql
@@ -70,10 +70,10 @@ drop rule qqq on copydml_test;
 create function qqq_trig() returns trigger as $$
 begin
 if tg_op in ('INSERT', 'UPDATE') then
-    raise notice '% %', tg_op, new.id;
+    raise notice '% % %', tg_when, tg_op, new.id;
     return new;
 else
-    raise notice '% %', tg_op, old.id;
+    raise notice '% % %', tg_when, tg_op, old.id;
     return old;
 end if;
 end
