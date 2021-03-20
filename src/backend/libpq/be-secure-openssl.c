7d9629ed2f69cd123514e0338cb20afca9abcd59
Tom Lane <tgl@sss.pgh.pa.us>
2021-03-20 12:38:22 -0400

    Fix memory leak when initializing DH parameters in backend
    When loading DH parameters used for the generation of ephemeral DH keys
    in the backend, the code has never bothered releasing the memory used
    for the DH information loaded from a file or from libpq's default.  This
    commit makes sure that the information is properly free()'d.
    
    Back-patch of e0e569e1d.  We originally thought the leak was minor and
    not worth back-patching, but Jelte Fennema pointed out that repeated
    SIGHUP's can result in very serious bloat of the postmaster, which is
    then multiplied by being duplicated into eadh forked child.
    
    Back-patch to v10; the code looked different before c0a15e07c,
    and didn't have a leak in the actually-live code paths.
    
    Michael Paquier
    
    Discussion: https://postgr.es/m/16160-18367e56e9a28264@postgresql.org

--- src/backend/libpq/be-secure-openssl.c
+++ src/backend/libpq/be-secure-openssl.c
@@ -1100,8 +1100,11 @@ initialize_dh(SSL_CTX *context, bool isServerStart)
 				(errcode(ERRCODE_CONFIG_FILE_ERROR),
 				 (errmsg("DH: could not set DH parameters: %s",
 						 SSLerrmessage(ERR_get_error())))));
+		DH_free(dh);
 		return false;
 	}
+
+	DH_free(dh);
 	return true;
 }
 
