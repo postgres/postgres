1ec7162a8d3494bce97bf52250943143cf6638ad
Michael Paquier <michael@paquier.xyz>
2021-03-22 08:31:14 +0900

    Fix timeline assignment in checkpoints with 2PC transactions
    Any transactions found as still prepared by a checkpoint have their
    state data read from the WAL records generated by PREPARE TRANSACTION
    before being moved into their new location within pg_twophase/.  While
    reading such records, the WAL reader uses the callback
    read_local_xlog_page() to read a page, that is shared across various
    parts of the system.  This callback, since 1148e22a, has introduced an
    update of ThisTimeLineID when reading a record while in recovery, which
    is potentially helpful in the context of cascading WAL senders.
    
    This update of ThisTimeLineID interacts badly with the checkpointer if a
    promotion happens while some 2PC data is read from its record, as, by
    changing ThisTimeLineID, any follow-up WAL records would be written to
    an timeline older than the promoted one.  This results in consistency
    issues.  For instance, a subsequent server restart would cause a failure
    in finding a valid checkpoint record, resulting in a PANIC, for
    instance.
    
    This commit changes the code reading the 2PC data to reset the timeline
    once the 2PC record has been read, to prevent messing up with the static
    state of the checkpointer.  It would be tempting to do the same thing
    directly in read_local_xlog_page().  However, based on the discussion
    that has led to 1148e22a, users may rely on the updates of
    ThisTimeLineID when a WAL record page is read in recovery, so changing
    this callback could break some cases that are working currently.
    
    A TAP test reproducing the issue is added, relying on a PITR to
    precisely trigger a promotion with a prepared transaction still
    tracked.
    
    Per discussion with Heikki Linnakangas, Kyotaro Horiguchi, Fujii Masao
    and myself.
    
    Author: Soumyadeep Chakraborty, Jimmy Yih, Kevin Yeap
    Discussion: https://postgr.es/m/CAE-ML+_EjH_fzfq1F3RJ1=XaaNG=-Jz-i3JqkNhXiLAsM3z-Ew@mail.gmail.com
    Backpatch-through: 10

--- src/backend/access/transam/twophase.c
+++ src/backend/access/transam/twophase.c
@@ -1299,8 +1299,11 @@ ReadTwoPhaseFile(TransactionId xid, bool give_warnings)
  * twophase files and ReadTwoPhaseFile should be used instead.
  *
  * Note clearly that this function can access WAL during normal operation,
- * similarly to the way WALSender or Logical Decoding would do.
- *
+ * similarly to the way WALSender or Logical Decoding would do.  While
+ * accessing WAL, read_local_xlog_page() may change ThisTimeLineID,
+ * particularly if this routine is called for the end-of-recovery checkpoint
+ * in the checkpointer itself, so save the current timeline number value
+ * and restore it once done.
  */
 static void
 XlogReadTwoPhaseData(XLogRecPtr lsn, char **buf, int *len)
@@ -1308,6 +1311,7 @@ XlogReadTwoPhaseData(XLogRecPtr lsn, char **buf, int *len)
 	XLogRecord *record;
 	XLogReaderState *xlogreader;
 	char	   *errormsg;
+	TimeLineID	save_currtli = ThisTimeLineID;
 
 	xlogreader = XLogReaderAllocate(&read_local_xlog_page, NULL);
 	if (!xlogreader)
@@ -1317,6 +1321,14 @@ XlogReadTwoPhaseData(XLogRecPtr lsn, char **buf, int *len)
 				 errdetail("Failed while allocating a WAL reading processor.")));
 
 	record = XLogReadRecord(xlogreader, lsn, &errormsg);
+
+	/*
+	 * Restore immediately the timeline where it was previously, as
+	 * read_local_xlog_page() could have changed it if the record was read
+	 * while recovery was finishing or if the timeline has jumped in-between.
+	 */
+	ThisTimeLineID = save_currtli;
+
 	if (record == NULL)
 		ereport(ERROR,
 				(errcode_for_file_access(),
