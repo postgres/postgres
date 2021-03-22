992d353a190c551db39bcab2dec0ecf14fbc7a40
Michael Paquier <michael@paquier.xyz>
2021-03-22 09:51:05 +0900

    Fix new TAP test for 2PC transactions and PITRs on Windows
    The test added by 595b9cb forgot that on Windows it is necessary to set
    up pg_hba.conf (see PostgresNode::set_replication_conf) with a specific
    entry or base backups fail.  Any node that requires to support
    replication just needs to pass down allows_streaming at initialization.
    This updates the test to do so.  Simplify things a bit while on it.
    
    Per buildfarm member fairywren.  Any Windows hosts running this test
    would have failed, and I have reproduced the problem as well.
    
    Backpatch-through: 10

--- src/test/recovery/t/023_pitr_prepared_xact.pl
+++ src/test/recovery/t/023_pitr_prepared_xact.pl
@@ -8,11 +8,9 @@ use File::Compare;
 
 # Initialize and start primary node with WAL archiving
 my $node_primary = get_new_node('primary');
-$node_primary->init(has_archiving => 1);
+$node_primary->init(has_archiving => 1, allows_streaming => 1);
 $node_primary->append_conf(
 	'postgresql.conf', qq{
-max_wal_senders = 10
-wal_level = 'replica'
 max_prepared_transactions = 10});
 $node_primary->start;
 
@@ -30,7 +28,6 @@ $node_pitr->init_from_backup(
 	has_restoring => 1);
 $node_pitr->append_conf(
 	'postgresql.conf', qq{
-max_prepared_transactions = 10
 recovery_target_name = 'rp'
 recovery_target_action = 'promote'});
 
