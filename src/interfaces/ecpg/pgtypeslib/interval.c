45aea47ef430bc942b831932adaca7730c6ae775
Bruce Momjian <bruce@momjian.us>
2021-04-02 16:42:29 -0400

    Use macro MONTHS_PER_YEAR instead of '12' in /ecpg/pgtypeslib
    All other places already use MONTHS_PER_YEAR appropriately.
    
    Backpatch-through: 9.6

--- src/interfaces/ecpg/pgtypeslib/interval.c
+++ src/interfaces/ecpg/pgtypeslib/interval.c
@@ -155,7 +155,7 @@ DecodeISO8601Interval(char *str,
 			{
 				case 'Y':
 					tm->tm_year += val;
-					tm->tm_mon += (fval * 12);
+					tm->tm_mon += (fval * MONTHS_PER_YEAR);
 					break;
 				case 'M':
 					tm->tm_mon += val;
@@ -191,7 +191,7 @@ DecodeISO8601Interval(char *str,
 						return DTERR_BAD_FORMAT;
 
 					tm->tm_year += val;
-					tm->tm_mon += (fval * 12);
+					tm->tm_mon += (fval * MONTHS_PER_YEAR);
 					if (unit == '\0')
 						return 0;
 					if (unit == 'T')
