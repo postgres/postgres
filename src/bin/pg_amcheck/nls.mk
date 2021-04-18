4ed7f0599a8984d9ed967780a157d9b23d03fbb5
Peter Eisentraut <peter@eisentraut.org>
2021-04-18 16:11:58 +0200

    Add missing source files to nls.mk

--- src/bin/pg_amcheck/nls.mk
+++ src/bin/pg_amcheck/nls.mk
@@ -2,6 +2,9 @@
 CATALOG_NAME     = pg_amcheck
 AVAIL_LANGUAGES  =
 GETTEXT_FILES    = $(FRONTEND_COMMON_GETTEXT_FILES) \
-                   pg_amcheck.c
+                   pg_amcheck.c \
+                   ../../fe_utils/cancel.c \
+                   ../../fe_utils/connect_utils.c \
+                   ../../fe_utils/query_utils.c
 GETTEXT_TRIGGERS = $(FRONTEND_COMMON_GETTEXT_TRIGGERS)
 GETTEXT_FLAGS    = $(FRONTEND_COMMON_GETTEXT_FLAGS)
--- src/bin/scripts/nls.mk
+++ src/bin/scripts/nls.mk
@@ -9,6 +9,7 @@ GETTEXT_FILES    = $(FRONTEND_COMMON_GETTEXT_FILES) \
                    common.c \
                    ../../fe_utils/parallel_slot.c \
                    ../../fe_utils/cancel.c ../../fe_utils/print.c \
+                   ../../fe_utils/connect_utils.c ../../fe_utils/query_utils.c \
                    ../../common/fe_memutils.c ../../common/username.c
 GETTEXT_TRIGGERS = $(FRONTEND_COMMON_GETTEXT_TRIGGERS) simple_prompt yesno_prompt
 GETTEXT_FLAGS    = $(FRONTEND_COMMON_GETTEXT_FLAGS)
