4c7ba553bad0254aaaf525668cc1cdb2eb8fcc92
Peter Eisentraut <peter@eisentraut.org>
2021-05-10 14:32:18 +0200

    Translation updates
    Source-Git-URL: git://git.postgresql.org/git/pgtranslation/messages.git
    Source-Git-Hash: 832086c7a50768dd7a8c548ab063037741530ddf

--- src/bin/pg_ctl/nls.mk
+++ src/bin/pg_ctl/nls.mk
@@ -1,4 +1,4 @@
 # src/bin/pg_ctl/nls.mk
 CATALOG_NAME     = pg_ctl
-AVAIL_LANGUAGES  = cs de es fr it ja ko ru sv tr uk zh_CN
+AVAIL_LANGUAGES  = cs de es fr it ja ko pt_BR ru sv tr uk zh_CN
 GETTEXT_FILES    = pg_ctl.c ../../common/exec.c ../../common/fe_memutils.c ../../common/wait_error.c ../../port/path.c
