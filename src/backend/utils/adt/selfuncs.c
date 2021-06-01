e5b0fffa17f610f03f03952c8c4a247c39e61292
Tom Lane <tgl@sss.pgh.pa.us>
2021-06-01 11:12:56 -0400

    Reject SELECT ... GROUP BY GROUPING SETS (()) FOR UPDATE.
    This case should be disallowed, just as FOR UPDATE with a plain
    GROUP BY is disallowed; FOR UPDATE only makes sense when each row
    of the query result can be identified with a single table row.
    However, we missed teaching CheckSelectLocking() to check
    groupingSets as well as groupClause, so that it would allow
    degenerate grouping sets.  That resulted in a bad plan and
    a null-pointer dereference in the executor.
    
    Looking around for other instances of the same bug, the only one
    I found was in examine_simple_variable().  That'd just lead to
    silly estimates, but it should be fixed too.
    
    Per private report from Yaoguang Chen.
    Back-patch to all supported branches.

--- src/backend/utils/adt/selfuncs.c
+++ src/backend/utils/adt/selfuncs.c
@@ -5150,7 +5150,8 @@ examine_simple_variable(PlannerInfo *root, Var *var,
 		 * of learning something even with it.
 		 */
 		if (subquery->setOperations ||
-			subquery->groupClause)
+			subquery->groupClause ||
+			subquery->groupingSets)
 			return;
 
 		/*
