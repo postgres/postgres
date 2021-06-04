7fc26d11e370afe237631265714221364d7e7910
David Rowley <drowley@postgresql.org>
2021-06-04 12:19:50 +1200

    Adjust locations which have an incorrect copyright year
    A few patches committed after ca3b37487 mistakenly forgot to make the
    copyright year 2021.  Fix these.
    
    Discussion: https://postgr.es/m/CAApHDvqyLmd9P2oBQYJ=DbrV8QwyPRdmXtCTFYPE08h+ip0UJw@mail.gmail.com

--- src/common/hmac.c
+++ src/common/hmac.c
@@ -5,7 +5,7 @@
  *
  * Fallback implementation of HMAC, as specified in RFC 2104.
  *
- * Portions Copyright (c) 1996-2020, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1996-2021, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  * IDENTIFICATION
--- src/common/hmac_openssl.c
+++ src/common/hmac_openssl.c
@@ -5,7 +5,7 @@
  *
  * This should only be used if code is compiled with OpenSSL support.
  *
- * Portions Copyright (c) 1996-2020, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1996-2021, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  * IDENTIFICATION
