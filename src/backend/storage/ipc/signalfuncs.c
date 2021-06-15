0aac73e6a2602696d23aa7a9686204965f9093dc
Noah Misch <noah@leadboat.com>
2021-06-14 17:29:37 -0700

    Copy-edit text for the pg_terminate_backend() "timeout" parameter.
    Revert the pg_description entry to its v13 form, since those messages
    usually remain shorter and don't discuss individual parameters.  No
    catversion bump, since pg_description content does not impair backend
    compatibility or application compatibility.
    
    Justin Pryzby
    
    Discussion: https://postgr.es/m/20210612182743.GY16435@telsasoft.com

--- src/backend/storage/ipc/signalfuncs.c
+++ src/backend/storage/ipc/signalfuncs.c
@@ -187,12 +187,12 @@ pg_wait_until_termination(int pid, int64 timeout)
 }
 
 /*
- * Signal to terminate a backend process. This is allowed if you are a member
- * of the role whose process is being terminated. If timeout input argument is
- * 0 (which is default), then this function just signals the backend and
- * doesn't wait. Otherwise it waits until given the timeout milliseconds or no
- * process has the given PID and returns true. On timeout, a warning is emitted
- * and false is returned.
+ * Send a signal to terminate a backend process. This is allowed if you are a
+ * member of the role whose process is being terminated. If the timeout input
+ * argument is 0, then this function just signals the backend and returns
+ * true.  If timeout is nonzero, then it waits until no process has the given
+ * PID; if the process ends within the timeout, true is returned, and if the
+ * timeout is exceeded, a warning is emitted and false is returned.
  *
  * Note that only superusers can signal superuser-owned processes.
  */
@@ -201,7 +201,7 @@ pg_terminate_backend(PG_FUNCTION_ARGS)
 {
 	int			pid;
 	int			r;
-	int			timeout;
+	int			timeout;		/* milliseconds */
 
 	pid = PG_GETARG_INT32(0);
 	timeout = PG_GETARG_INT64(1);
