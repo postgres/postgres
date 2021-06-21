a7bb0ce58f56ee8907c3f49c52d99f502536c796
Peter Eisentraut <peter@eisentraut.org>
2021-06-21 12:32:14 +0200

    Translation updates
    Source-Git-URL: git://git.postgresql.org/git/pgtranslation/messages.git
    Source-Git-Hash: 70796ae860c444c764bb591c885f22cac1c168ec

--- src/bin/pg_amcheck/nls.mk
+++ src/bin/pg_amcheck/nls.mk
@@ -1,6 +1,6 @@
 # src/bin/pg_amcheck/nls.mk
 CATALOG_NAME     = pg_amcheck
-AVAIL_LANGUAGES  = de fr
+AVAIL_LANGUAGES  = de el es fr zh_CN
 GETTEXT_FILES    = $(FRONTEND_COMMON_GETTEXT_FILES) \
                    pg_amcheck.c \
                    ../../fe_utils/cancel.c \
--- src/bin/pg_controldata/nls.mk
+++ src/bin/pg_controldata/nls.mk
@@ -1,6 +1,6 @@
 # src/bin/pg_controldata/nls.mk
 CATALOG_NAME     = pg_controldata
-AVAIL_LANGUAGES  = cs de es fr it ja ko pl pt_BR ru sv tr uk vi zh_CN
+AVAIL_LANGUAGES  = cs de el es fr it ja ko pl pt_BR ru sv tr uk vi zh_CN
 GETTEXT_FILES    = pg_controldata.c ../../common/controldata_utils.c
 GETTEXT_TRIGGERS = $(FRONTEND_COMMON_GETTEXT_TRIGGERS)
 GETTEXT_FLAGS    = $(FRONTEND_COMMON_GETTEXT_FLAGS)
