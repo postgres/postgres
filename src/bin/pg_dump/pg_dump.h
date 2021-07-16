ccfc3cbb341abf515d930097c4851d9e2152504f
Alvaro Herrera <alvherre@alvh.no-ip.org>
2021-07-16 17:29:22 -0400

    Fix pg_dump for disabled triggers on partitioned tables
    pg_dump failed to preserve the 'enabled' flag (which can be not only
    disabled, but also REPLICA or ALWAYS) for partitions which had it
    changed from their respective parents.  Attempt to handle that by
    including a definition for such triggers in the dump, but replace the
    standard CREATE TRIGGER line with an ALTER TRIGGER line.
    
    Backpatch to 11, where these triggers can exist.  In branches 11 and 12,
    pick up a few test lines from commit b9b408c48724 to verify that
    pg_upgrade is okay with these arrangements.
    
    Co-authored-by: Justin Pryzby <pryzby@telsasoft.com>
    Co-authored-by: Álvaro Herrera <alvherre@alvh.no-ip.org>
    Discussion: https://postgr.es/m/20200930223450.GA14848@telsasoft.com

--- src/bin/pg_dump/pg_dump.h
+++ src/bin/pg_dump/pg_dump.h
@@ -413,6 +413,7 @@ typedef struct _triggerInfo
 	Oid			tgconstrrelid;
 	char	   *tgconstrrelname;
 	char		tgenabled;
+	bool		tgisinternal;
 	bool		tgdeferrable;
 	bool		tginitdeferred;
 	char	   *tgdef;
