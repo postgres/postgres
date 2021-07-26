0806d08d4626f0a71a4e83cb399ee965a8aac99b
Tom Lane <tgl@sss.pgh.pa.us>
2021-07-25 23:25:15 -0400

    Harden pg_stat_statements tests against CLOBBER_CACHE_ALWAYS.
    Turns out the buildfarm hasn't been testing this, which will soon change.
    
    Julien Rouhaud, per report from me
    
    Discussion: https://postgr.es/m/42557.1627229005@sss.pgh.pa.us

--- contrib/pg_stat_statements/sql/pg_stat_statements.sql
+++ contrib/pg_stat_statements/sql/pg_stat_statements.sql
@@ -356,7 +356,12 @@ EXECUTE prep1;
 SELECT 42;
 SELECT 42;
 SELECT 42;
-SELECT query, plans, calls, rows FROM pg_stat_statements ORDER BY query COLLATE "C";
+SELECT query, plans, calls, rows FROM pg_stat_statements
+  WHERE query NOT LIKE 'PREPARE%' ORDER BY query COLLATE "C";
+-- for the prepared statement we expect at least one replan, but cache
+-- invalidations could force more
+SELECT query, plans >= 2 AND plans <= calls AS plans_ok, calls, rows FROM pg_stat_statements
+  WHERE query LIKE 'PREPARE%' ORDER BY query COLLATE "C";
 
 --
 -- access to pg_stat_statements_info view
