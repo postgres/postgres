9b0d71725eb2ca6ea0aaffdc38be599b90e7dc56
Peter Eisentraut <peter@eisentraut.org>
2021-08-07 13:36:59 +0200

    pg_amcheck: Add missing translation markers

--- src/bin/pg_amcheck/nls.mk
+++ src/bin/pg_amcheck/nls.mk
@@ -6,5 +6,7 @@ GETTEXT_FILES    = $(FRONTEND_COMMON_GETTEXT_FILES) \
                    ../../fe_utils/cancel.c \
                    ../../fe_utils/connect_utils.c \
                    ../../fe_utils/query_utils.c
-GETTEXT_TRIGGERS = $(FRONTEND_COMMON_GETTEXT_TRIGGERS)
-GETTEXT_FLAGS    = $(FRONTEND_COMMON_GETTEXT_FLAGS)
+GETTEXT_TRIGGERS = $(FRONTEND_COMMON_GETTEXT_TRIGGERS) \
+                   log_no_match
+GETTEXT_FLAGS    = $(FRONTEND_COMMON_GETTEXT_FLAGS) \
+                   log_no_match:1:c-format
