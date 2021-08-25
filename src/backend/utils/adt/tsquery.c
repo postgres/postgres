5d1c36fd204bc680745263c3f9f41978b368fa0a
Fujii Masao <fujii@postgresql.org>
2021-08-25 11:43:56 +0900

    Improve error message about valid value for distance in phrase operator.
    The distance in phrase operator must be an integer value between zero
    and MAXENTRYPOS inclusive. But previously the error message about
    its valid value included the information about its upper limit
    but not lower limit (i.e., zero). This commit improves the error message
    so that it also includes the information about its lower limit.
    
    Back-patch to v9.6 where full-text phrase search was supported.
    
    Author: Kyotaro Horiguchi
    Reviewed-by: Fujii Masao
    Discussion: https://postgr.es/m/20210819.170315.1413060634876301811.horikyota.ntt@gmail.com

--- src/backend/utils/adt/tsquery.c
+++ src/backend/utils/adt/tsquery.c
@@ -163,7 +163,7 @@ parse_phrase_operator(char *buf, int16 *distance)
 				else if (errno == ERANGE || l < 0 || l > MAXENTRYPOS)
 					ereport(ERROR,
 							(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
-							 errmsg("distance in phrase operator should not be greater than %d",
+							 errmsg("distance in phrase operator must be an integer value between zero and %d inclusive",
 									MAXENTRYPOS)));
 				else
 				{
