28cde380c1c02754bc0eebb88e9ec9c4de91a8fb
Amit Kapila <akapila@postgresql.org>
2021-09-08 11:23:01 +0530

    Invalidate relcache for publications defined for all tables.
    Updates/Deletes on a relation were allowed even without replica identity
    after we define the publication for all tables. This would later lead to
    an error on subscribers. The reason was that for such publications we were
    not invalidating the relcache and the publication information for
    relations was not getting rebuilt. Similarly, we were not invalidating the
    relcache after dropping of such publications which will prohibit
    Updates/Deletes without replica identity even without any publication.
    
    Author: Vignesh C and Hou Zhijie
    Reviewed-by: Hou Zhijie, Kyotaro Horiguchi, Amit Kapila
    Backpatch-through: 10, where it was introduced
    Discussion: https://postgr.es/m/CALDaNm0pF6zeWqCA8TCe2sDuwFAy8fCqba=nHampCKag-qLixg@mail.gmail.com

--- src/backend/commands/publicationcmds.c
+++ src/backend/commands/publicationcmds.c
@@ -215,6 +215,11 @@ CreatePublication(CreatePublicationStmt *stmt)
 		PublicationAddTables(puboid, rels, true, NULL);
 		CloseTableList(rels);
 	}
+	else if (stmt->for_all_tables)
+	{
+		/* Invalidate relcache so that publication info is rebuilt. */
+		CacheInvalidateRelcacheAll();
+	}
 
 	heap_close(rel, RowExclusiveLock);
 
@@ -424,6 +429,7 @@ RemovePublicationById(Oid pubid)
 {
 	Relation	rel;
 	HeapTuple	tup;
+	Form_pg_publication pubform;
 
 	rel = heap_open(PublicationRelationId, RowExclusiveLock);
 
@@ -431,6 +437,12 @@ RemovePublicationById(Oid pubid)
 
 	if (!HeapTupleIsValid(tup))
 		elog(ERROR, "cache lookup failed for publication %u", pubid);
+
+	pubform = (Form_pg_publication) GETSTRUCT(tup);
+
+	/* Invalidate relcache so that publication info is rebuilt. */
+	if (pubform->puballtables)
+		CacheInvalidateRelcacheAll();
 
 	CatalogTupleDelete(rel, &tup->t_self);
 
