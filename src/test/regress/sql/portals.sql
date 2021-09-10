d844cd75a6764a1dd2d8b3410144df9ebf8a0f04
Tom Lane <tgl@sss.pgh.pa.us>
2021-09-10 13:18:32 -0400

    Fix some anomalies with NO SCROLL cursors.
    We have long forbidden fetching backwards from a NO SCROLL cursor,
    but the prohibition didn't extend to cases in which we rewind the
    query altogether and then re-fetch forwards.  I think the reason is
    that this logic was mainly meant to protect plan nodes that can't
    be run in the reverse direction.  However, re-reading the query output
    is problematic if the query is volatile (which includes SELECT FOR
    UPDATE, not just queries with volatile functions): the re-read can
    produce different results, which confuses the cursor navigation logic
    completely.  Another reason for disliking this approach is that some
    code paths will either fetch backwards or rewind-and-fetch-forwards
    depending on the distance to the target row; so that seemingly
    identical use-cases may or may not draw the "cursor can only scan
    forward" error.  Hence, let's clean things up by disallowing rewind
    as well as fetch-backwards in a NO SCROLL cursor.
    
    Ordinarily we'd only make such a definitional change in HEAD, but
    there is a third reason to consider this change now.  Commit ba2c6d6ce
    created some new user-visible anomalies for non-scrollable cursors
    WITH HOLD, in that navigation in the cursor result got confused if the
    cursor had been partially read before committing.  The only good way
    to resolve those anomalies is to forbid rewinding such a cursor, which
    allows removal of the incorrect cursor state manipulations that
    ba2c6d6ce added to PersistHoldablePortal.
    
    To minimize the behavioral change in the back branches (including
    v14), refuse to rewind a NO SCROLL cursor only when it has a holdStore,
    ie has been held over from a previous transaction due to WITH HOLD.
    This should avoid breaking most applications that have been sloppy
    about whether to declare cursors as scrollable.  We'll enforce the
    prohibition across-the-board beginning in v15.
    
    Back-patch to v11, as ba2c6d6ce was.
    
    Discussion: https://postgr.es/m/3712911.1631207435@sss.pgh.pa.us

--- src/test/regress/sql/portals.sql
+++ src/test/regress/sql/portals.sql
@@ -217,6 +217,26 @@ SELECT name, statement, is_holdable, is_binary, is_scrollable FROM pg_cursors;
 
 CLOSE foo25;
 
+BEGIN;
+
+DECLARE foo25ns NO SCROLL CURSOR WITH HOLD FOR SELECT * FROM tenk2;
+
+FETCH FROM foo25ns;
+
+FETCH FROM foo25ns;
+
+COMMIT;
+
+FETCH FROM foo25ns;
+
+FETCH ABSOLUTE 4 FROM foo25ns;
+
+FETCH ABSOLUTE 4 FROM foo25ns; -- fail
+
+SELECT name, statement, is_holdable, is_binary, is_scrollable FROM pg_cursors;
+
+CLOSE foo25ns;
+
 --
 -- ROLLBACK should close holdable cursors
 --
