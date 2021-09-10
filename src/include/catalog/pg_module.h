3450fae1b04ab393a27e82a027f1a12674f73375
Swaha Miller <swmiller@amazon.com>
2021-09-10 17:17:03 +0000

    WIP for POSTGRES-8041

---
+++ src/include/catalog/pg_module.h
@@ -0,0 +1,64 @@
+/*-------------------------------------------------------------------------
+ *
+ * pg_module.h
+ *	  definition of the "module" system catalog (pg_module)
+ *
+ *
+ * Portions Copyright (c) 1996-2021, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1994, Regents of the University of California
+ *
+ * src/include/catalog/pg_module.h
+ *
+ * NOTES
+ *	  The Catalog.pm module reads this file and derives module
+ *	  information.
+ *
+ *-------------------------------------------------------------------------
+ */
+#ifndef PG_MODULE_H
+#define PG_MODULE_H
+
+#include "catalog/genbki.h"
+#include "catalog/pg_module_d.h"
+#include "utils/acl.h"
+
+/* ----------------------------------------------------------------
+ *		pg_module definition.
+ *
+ *		cpp turns this into typedef struct FormData_pg_module
+ *
+ *	modname				name of the module
+ *	modowner			owner (creator) of the module
+ *	modacl				access privilege list
+ * ----------------------------------------------------------------
+ */
+CATALOG(pg_module,2635,ModuleRelationId)
+{
+	Oid			oid;			/* oid */
+
+	NameData	modname;
+	Oid			modowner BKI_DEFAULT(POSTGRES) BKI_LOOKUP(pg_authid);
+
+#ifdef CATALOG_VARLEN			/* variable-length fields start here */
+	aclitem		modacl[1];
+#endif
+} FormData_pg_module;
+
+/* ----------------
+ *		Form_pg_module corresponds to a pointer to a tuple with
+ *		the format of pg_module relation.
+ * ----------------
+ */
+typedef FormData_pg_module *Form_pg_module;
+
+DECLARE_TOAST(pg_module, 4183, 4184);
+
+DECLARE_UNIQUE_INDEX(pg_module_modname_index, 2714, ModuleNameIndexId, on pg_module using btree(modname name_ops));
+DECLARE_UNIQUE_INDEX_PKEY(pg_module_oid_index, 2715, ModuleOidIndexId, on pg_module using btree(oid oid_ops));
+
+/*
+ * prototypes for functions in pg_module.c
+ */
+extern Oid	ModuleCreate(const char *modName, Oid ownerId);
+
+#endif							/* PG_MODULE_H */
--- src/include/catalog/pg_namespace.h
+++ src/include/catalog/pg_namespace.h
@@ -29,8 +29,6 @@
  *
  *	nspname				name of the namespace
  *	nspowner			owner (creator) of the namespace
- *	nspnamespace		parent of the namespace
- *	nspkind				the type namespace
  *	nspacl				access privilege list
  * ----------------------------------------------------------------
  */
@@ -40,9 +38,6 @@ CATALOG(pg_namespace,2615,NamespaceRelationId)
 
 	NameData	nspname;
 	Oid			nspowner BKI_DEFAULT(POSTGRES) BKI_LOOKUP(pg_authid);
-	Oid			nspnamespace BKI_DEFAULT(0);
-	char		nspkind BKI_DEFAULT(s);
-
 
 #ifdef CATALOG_VARLEN			/* variable-length fields start here */
 	aclitem		nspacl[1];
@@ -58,16 +53,12 @@ typedef FormData_pg_namespace *Form_pg_namespace;
 
 DECLARE_TOAST(pg_namespace, 4163, 4164);
 
-DECLARE_UNIQUE_INDEX(pg_namespace_nspname_index, 2684, NamespaceNameIndexId, on pg_namespace using btree(nspname name_ops, nspnamespace oid_ops));
+DECLARE_UNIQUE_INDEX(pg_namespace_nspname_index, 2684, NamespaceNameIndexId, on pg_namespace using btree(nspname name_ops));
 DECLARE_UNIQUE_INDEX_PKEY(pg_namespace_oid_index, 2685, NamespaceOidIndexId, on pg_namespace using btree(oid oid_ops));
-
-#define		NSPKIND_SCHEMA			's'		/* schema */
-#define		NSPKIND_MODULE			'm'		/* module */
 
 /*
  * prototypes for functions in pg_namespace.c
  */
-extern Oid	NamespaceCreate(const char *nspName, Oid nspnamespace, char nspkind,
-							Oid ownerId, bool isTemp);
+extern Oid	NamespaceCreate(const char *nspName, Oid ownerId, bool isTemp);
 
 #endif							/* PG_NAMESPACE_H */
