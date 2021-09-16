3f50b82639637c9908afa2087de7588450aa866b
Tom Lane <tgl@sss.pgh.pa.us>
2021-09-16 10:45:42 -0400

    Fix EXPLAIN to handle SEARCH BREADTH FIRST queries.
    The rewriter transformation for SEARCH BREADTH FIRST produces a
    FieldSelect on a Var of type RECORD, where the Var references the
    recursive union's worktable output.  EXPLAIN VERBOSE failed to handle
    this case, because it only expected such Vars to appear in CteScans
    not WorkTableScans.  Fix that, and add some test cases exercising
    EXPLAIN on SEARCH and CYCLE queries.
    
    In principle this oversight is an old bug, but it seems that the
    case is unreachable without SEARCH BREADTH FIRST, because the
    parser fails when attempting to create such a reference manually.
    So for today I'll just patch HEAD/v14.  Someday we might find that
    the code portion of this patch needs to be back-patched further.
    
    Per report from Atsushi Torikoshi.
    
    Discussion: https://postgr.es/m/5bafa66ad529e11860339565c9e7c166@oss.nttdata.com

--- src/test/regress/sql/with.sql
+++ src/test/regress/sql/with.sql
@@ -358,6 +358,16 @@ insert into graph0 values
 	(1, 4, 'arc 1 -> 4'),
 	(4, 5, 'arc 4 -> 5');
 
+explain (verbose, costs off)
+with recursive search_graph(f, t, label) as (
+	select * from graph0 g
+	union all
+	select g.*
+	from graph0 g, search_graph sg
+	where g.f = sg.t
+) search depth first by f, t set seq
+select * from search_graph order by seq;
+
 with recursive search_graph(f, t, label) as (
 	select * from graph0 g
 	union all
@@ -374,6 +384,16 @@ with recursive search_graph(f, t, label) as (
 	from graph0 g, search_graph sg
 	where g.f = sg.t
 ) search depth first by f, t set seq
+select * from search_graph order by seq;
+
+explain (verbose, costs off)
+with recursive search_graph(f, t, label) as (
+	select * from graph0 g
+	union all
+	select g.*
+	from graph0 g, search_graph sg
+	where g.f = sg.t
+) search breadth first by f, t set seq
 select * from search_graph order by seq;
 
 with recursive search_graph(f, t, label) as (
@@ -502,6 +522,16 @@ with recursive search_graph(f, t, label, is_cycle, path) as (
 select * from search_graph order by path;
 
 -- CYCLE clause
+
+explain (verbose, costs off)
+with recursive search_graph(f, t, label) as (
+	select * from graph g
+	union all
+	select g.*
+	from graph g, search_graph sg
+	where g.f = sg.t
+) cycle f, t set is_cycle using path
+select * from search_graph;
 
 with recursive search_graph(f, t, label) as (
 	select * from graph g
