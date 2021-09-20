923b7efc259818688979f38ba88cba0d0d41ba10
Tom Lane <tgl@sss.pgh.pa.us>
2021-09-20 11:48:52 -0400

    Don't elide casting to typmod -1.
    Casting a value that's already of a type with a specific typmod
    to an unspecified typmod doesn't do anything so far as run-time
    behavior is concerned.  However, it really ought to change the
    exposed type of the expression to match.  Up to now,
    coerce_type_typmod hasn't bothered with that, which creates gotchas
    in contexts such as recursive unions.  If for example one side of
    the union is numeric(18,3), but it needs to be plain numeric to
    match the other side, there's no direct way to express that.
    
    This is easy enough to fix, by inserting a RelabelType to update the
    exposed type of the expression.  However, it's a bit nervous-making
    to change this behavior, because it's stood for a really long time.
    But no complaints have emerged about 14beta3, so go ahead and
    back-patch.
    
    Back-patch of 5c056b0c2 into previous supported branches.
    
    Discussion: https://postgr.es/m/CABNQVagu3bZGqiTjb31a8D5Od3fUMs7Oh3gmZMQZVHZ=uWWWfQ@mail.gmail.com
    Discussion: https://postgr.es/m/1488389.1631984807@sss.pgh.pa.us

--- src/test/regress/sql/expressions.sql
+++ src/test/regress/sql/expressions.sql
@@ -34,3 +34,23 @@ SELECT current_schema;
 SET search_path = 'pg_catalog';
 SELECT current_schema;
 RESET search_path;
+
+
+--
+-- Test parsing of a no-op cast to a type with unspecified typmod
+--
+begin;
+
+create table numeric_tbl (f1 numeric(18,3), f2 numeric);
+
+create view numeric_view as
+  select
+    f1, f1::numeric(16,4) as f1164, f1::numeric as f1n,
+    f2, f2::numeric(16,4) as f2164, f2::numeric as f2n
+  from numeric_tbl;
+
+\d+ numeric_view
+
+explain (verbose, costs off) select * from numeric_view;
+
+rollback;
