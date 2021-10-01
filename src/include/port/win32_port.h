8162464a25e5314e753c580389f76a9b7f69445b
Andres Freund <andres@anarazel.de>
2021-10-01 09:50:45 -0700

    windows: Define WIN32_LEAN_AND_MEAN to make compilation faster.
    windows.h includes a lot of other headers, slowing down compilation
    significantly. WIN32_LEAN_AND_MEAN reduces that a bit. It'd be better to
    remove the include of windows.h (as well as indirect inclusions of it) from such
    a central place, but until then...
    
    Discussion: https://postgr.es/m/20210921193035.pqzay43vpyv7in43@alap3.anarazel.de

--- src/include/port/win32_port.h
+++ src/include/port/win32_port.h
@@ -43,6 +43,14 @@
 #define _WINSOCKAPI_
 #endif
 
+/*
+ * windows.h includes a lot of other headers, slowing down compilation
+ * significantly.  WIN32_LEAN_AND_MEAN reduces that a bit. It'd be better to
+ * remove the include of windows.h (as well as indirect inclusions of it) from
+ * such a central place, but until then...
+ */
+#define WIN32_LEAN_AND_MEAN
+
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <windows.h>
