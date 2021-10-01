a5e83ad79c282421f32224c5152d6182de34da35
Daniel Gustafsson <dgustafsson@postgresql.org>
2021-10-01 22:47:05 +0200

    Fix memory leak in pg_hmac
    The intermittent h buffer was not freed, causing it to leak. Backpatch
    through 14 where HMAC was refactored to the current API.
    
    Author: Sergey Shinderuk <s.shinderuk@postgrespro.ru>
    Discussion: https://postgr.es/m/af07e620-7e28-a742-4637-2bc44aa7c2be@postgrespro.ru
    Backpatch-through: 14

--- src/common/hmac.c
+++ src/common/hmac.c
@@ -232,7 +232,10 @@ pg_hmac_final(pg_hmac_ctx *ctx, uint8 *dest, size_t len)
 	memset(h, 0, ctx->digest_size);
 
 	if (pg_cryptohash_final(ctx->hash, h, ctx->digest_size) < 0)
+	{
+		FREE(h);
 		return -1;
+	}
 
 	/* H(K XOR opad, tmp) */
 	if (pg_cryptohash_init(ctx->hash) < 0 ||
@@ -240,9 +243,11 @@ pg_hmac_final(pg_hmac_ctx *ctx, uint8 *dest, size_t len)
 		pg_cryptohash_update(ctx->hash, h, ctx->digest_size) < 0 ||
 		pg_cryptohash_final(ctx->hash, dest, len) < 0)
 	{
+		FREE(h);
 		return -1;
 	}
 
+	FREE(h);
 	return 0;
 }
 
