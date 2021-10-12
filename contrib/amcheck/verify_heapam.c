e7712155ea08c2f86dc32dccb5df2b230733be31
Peter Geoghegan <pg@bowt.ie>
2021-10-11 17:21:46 -0700

    amcheck: Skip unlogged relations in Hot Standby.
    Have verify_heapam.c treat unlogged relations as if they were simply
    empty when in Hot Standby mode.  This brings it in line with
    verify_nbtree.c, which has handled unlogged relations in the same way
    since bugfix commit 6754fe65a4.  This was an oversight in commit
    866e24d47d, which extended contrib/amcheck to check heap relations.
    
    In passing, lower the verbosity used when reporting that a relation has
    been skipped like this, from NOTICE to DEBUG1.  This is appropriate
    because the skipping behavior is only an implementation detail, needed
    to work around the fact that unlogged tables don't have smgr-level
    storage for their main fork when in Hot Standby mode.
    
    Affected unlogged relations should be considered "trivially verified",
    not skipped over.  They are verified in the same sense that a totally
    empty relation can be verified.  This behavior seems least surprising
    overall, since unlogged relations on a replica will initially be empty
    if and when the replica is promoted and Hot Standby ends.
    
    Author: Mark Dilger <mark.dilger@enterprisedb.com>
    Reviewed-By: Peter Geoghegan <pg@bowt.ie>
    Discussion: https://postgr.es/m/CAH2-Wzk_pukOFY7JmdiFLsrz+Pd3V8OwgC1TH2Vd5BH5ZgK4bA@mail.gmail.com
    Backpatch: 14-, where heapam verification was introduced.

--- contrib/amcheck/verify_heapam.c
+++ contrib/amcheck/verify_heapam.c
@@ -302,6 +302,22 @@ verify_heapam(PG_FUNCTION_ARGS)
 	ctx.rel = relation_open(relid, AccessShareLock);
 	sanity_check_relation(ctx.rel);
 
+	/*
+	 * Early exit for unlogged relations during recovery.  These will have no
+	 * relation fork, so there won't be anything to check.  We behave as if
+	 * the relation is empty.
+	 */
+	if (ctx.rel->rd_rel->relpersistence == RELPERSISTENCE_UNLOGGED &&
+		RecoveryInProgress())
+	{
+		ereport(DEBUG1,
+				(errcode(ERRCODE_READ_ONLY_SQL_TRANSACTION),
+				 errmsg("cannot verify unlogged relation \"%s\" during recovery, skipping",
+						RelationGetRelationName(ctx.rel))));
+		relation_close(ctx.rel, AccessShareLock);
+		PG_RETURN_NULL();
+	}
+
 	/* Early exit if the relation is empty */
 	nblocks = RelationGetNumberOfBlocks(ctx.rel);
 	if (!nblocks)
