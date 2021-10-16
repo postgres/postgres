16da53845bf316a20787e24d1b11fbe05b9e2a87
Kenaniah Cerny <kenaniah@gmail.com>
2021-10-16 13:31:12 -0700

    checkpoint

--- src/test/modules/unsafe_tests/sql/role_membership.sql
+++ src/test/modules/unsafe_tests/sql/role_membership.sql
@@ -25,6 +25,12 @@ $$ LANGUAGE SQL;
 CREATE ROLE role_a;
 CREATE ROLE role_b;
 CREATE ROLE role_c;
+CREATE ROLE role_d;
+CREATE ROLE role_e NOINHERIT;
+
+\connect template1
+
+CREATE TABLE data AS SELECT generate_series(1, 3);
 
 CREATE DATABASE role_membership_test1;
 CREATE DATABASE role_membership_test2;
@@ -34,9 +40,12 @@ CREATE DATABASE role_membership_test4;
 -- Initial GRANT statements
 
 GRANT pg_read_all_data TO role_a WITH ADMIN OPTION;
-GRANT pg_read_all_data TO role_b IN DATABASE role_membership_test2;
+GRANT pg_read_all_data TO role_b IN DATABASE role_membership_test1;
+GRANT role_a TO role_d IN DATABASE role_membership_test1;
+GRANT role_a TO role_e;
+GRANT role_a TO role_e IN DATABASE role_membership_test1;
 
-\connect role_membership_test3 role_admin
+\connect role_membership_test2 role_admin
 
 GRANT pg_read_all_data TO role_b IN CURRENT DATABASE;
 GRANT pg_read_all_data TO role_c IN CURRENT DATABASE WITH ADMIN OPTION;
@@ -47,13 +56,44 @@ GRANT pg_read_all_data TO role_c IN DATABASE role_membership_test4 GRANTED BY ro
 SELECT * FROM check_memberships();
 
 -- Ensure GRANT warning messages for duplicate grants
-GRANT pg_read_all_data TO role_a;
-GRANT pg_read_all_data TO role_b IN DATABASE role_membership_test3;
+GRANT pg_read_all_data TO role_a; -- warn
+GRANT pg_read_all_data TO role_b IN DATABASE role_membership_test2; -- warn
 
 -- Ensure with admin option can still be granted without warning
-GRANT pg_read_all_data TO role_b IN DATABASE role_membership_test2 WITH ADMIN OPTION;
-GRANT pg_read_all_data TO role_b IN DATABASE role_membership_test2 WITH ADMIN OPTION;
-GRANT pg_read_all_data TO role_c IN DATABASE role_membership_test3 WITH ADMIN OPTION;
+GRANT pg_read_all_data TO role_b IN DATABASE role_membership_test3 WITH ADMIN OPTION; -- silent
+GRANT pg_read_all_data TO role_b IN DATABASE role_membership_test3 WITH ADMIN OPTION; -- warn
+GRANT pg_read_all_data TO role_d IN DATABASE role_membership_test4 WITH ADMIN OPTION; -- silent
+
+SELECT * FROM check_memberships();
+
+-- Test membership privileges
+\connect role_membership_test1
+SET ROLE role_a;
+SELECT * FROM data; -- success (read-all cluster-wide)
+SET ROLE role_b;
+SELECT * FROM data; -- success (read-all in database)
+SET ROLE role_c;
+SELECT * FROM data; -- error (not granted)
+SET ROLE role_d;
+SELECT * FROM data; -- success (inherited from role_a)
+SET ROLE role_e;
+SELECT * FROM data; -- error (no inherit)
+
+\connect role_membership_test2;
+SET ROLE role_a;
+SELECT * FROM data; -- success (read-all cluster-wide)
+SET ROLE role_b;
+SELECT * FROM data; -- success (read-all in database)
+SET ROLE role_c;
+SELECT * FROM data; -- success (read-all in database)
+SET ROLE role_d;
+SELECT * FROM data; -- error (not granted)
+SET ROLE role_e;
+SELECT * FROM data; -- error (no inherit)
+
+-- Ensure ADMIN OPTION can grant cluster-wide and within any DB if cluster-wide
+-- Ensure ADMIN OPTION can grant only within same db if database-specific
+
 
 -- test GRANT works
 -- test REVOKE works
@@ -67,8 +107,13 @@ GRANT pg_read_all_data TO role_c IN DATABASE role_membership_test3 WITH ADMIN OP
 -- test set session role
 
 -- test membership privileges
--- test drop database cleanup
 
--- should deny database-specific grants for superuser roles?
+-- Ensure that DROP DATABASE cleans up the relevant memberships
+
+\connect postgres role_admin
+
+DROP DATABASE role_membership_test3;
 
 SELECT * FROM check_memberships();
+
+-- should deny database-specific grants for superuser roles?
