1539e0ecd6a447fca14920834d4546c60ff20f10
Michael Paquier <michael@paquier.xyz>
2021-10-20 16:49:03 +0900

    Fix build of MSVC with OpenSSL 3.0.0
    The build scripts of Visual Studio would fail to detect properly a 3.0.0
    build as the check on the second digit was failing.  This is adjusted
    where needed, allowing the builds to complete.  Note that the MSIs of
    OpenSSL mentioned in the documentation have not changed any library
    names for Win32 and Win64, making this change straight-forward.
    
    Reported-by: htalaco, via github
    Reviewed-by: Daniel Gustafsson
    Discussion: https://postgr.es/m/YW5XKYkq6k7OtrFq@paquier.xyz
    Backpatch-through: 9.6

--- src/tools/msvc/Solution.pm
+++ src/tools/msvc/Solution.pm
@@ -254,7 +254,8 @@ sub GenerateFiles
 			my ($digit1, $digit2, $digit3) = $self->GetOpenSSLVersion();
 
 			# More symbols are needed with OpenSSL 1.1.0 and above.
-			if ($digit1 >= '1' && $digit2 >= '1' && $digit3 >= '0')
+			if (   ($digit1 >= '3' && $digit2 >= '0' && $digit3 >= '0')
+				|| ($digit1 >= '1' && $digit2 >= '1' && $digit3 >= '0'))
 			{
 				print $o "#define HAVE_ASN1_STRING_GET0_DATA 1\n";
 				print $o "#define HAVE_BIO_GET_DATA 1\n";
@@ -662,7 +663,8 @@ sub AddProject
 		# changed their library names from:
 		# - libeay to libcrypto
 		# - ssleay to libssl
-		if ($digit1 >= '1' && $digit2 >= '1' && $digit3 >= '0')
+		if (   ($digit1 >= '3' && $digit2 >= '0' && $digit3 >= '0')
+			|| ($digit1 >= '1' && $digit2 >= '1' && $digit3 >= '0'))
 		{
 			my $dbgsuffix;
 			my $libsslpath;
