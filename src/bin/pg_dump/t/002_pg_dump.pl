476006023538730d2291125b883bfe07d03f3dfa
Tom Lane <tgl@sss.pgh.pa.us>
2021-10-22 15:22:26 -0400

    pg_dump: fix mis-dumping of non-global default privileges.
    Non-global default privilege entries should be dumped as-is,
    not made relative to the default ACL for their object type.
    This would typically only matter if one had revoked some
    on-by-default privileges in a global entry, and then wanted
    to grant them again in a non-global entry.
    
    Per report from Boris Korzun.  This is an old bug, so back-patch
    to all supported branches.
    
    Neil Chen, test case by Masahiko Sawada
    
    Discussion: https://postgr.es/m/111621616618184@mail.yandex.ru
    Discussion: https://postgr.es/m/CAA3qoJnr2+1dVJObNtfec=qW4Z0nz=A9+r5bZKoTSy5RDjskMw@mail.gmail.com

--- src/bin/pg_dump/t/002_pg_dump.pl
+++ src/bin/pg_dump/t/002_pg_dump.pl
@@ -421,6 +421,25 @@ my %tests = (
 		},
 	},
 
+	'ALTER DEFAULT PRIVILEGES FOR ROLE regress_dump_test_role GRANT EXECUTE ON FUNCTIONS'
+	  => {
+		create_order => 15,
+		create_sql   => 'ALTER DEFAULT PRIVILEGES
+					   FOR ROLE regress_dump_test_role IN SCHEMA dump_test
+					   GRANT EXECUTE ON FUNCTIONS TO regress_dump_test_role;',
+		regexp => qr/^
+			\QALTER DEFAULT PRIVILEGES \E
+			\QFOR ROLE regress_dump_test_role IN SCHEMA dump_test \E
+			\QGRANT ALL ON FUNCTIONS  TO regress_dump_test_role;\E
+			/xm,
+		like =>
+		  { %full_runs, %dump_test_schema_runs, section_post_data => 1, },
+		unlike => {
+			exclude_dump_test_schema => 1,
+			no_privs                 => 1,
+		},
+	  },
+
 	'ALTER DEFAULT PRIVILEGES FOR ROLE regress_dump_test_role REVOKE' => {
 		create_order => 55,
 		create_sql   => 'ALTER DEFAULT PRIVILEGES
