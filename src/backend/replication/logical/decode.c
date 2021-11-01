5ef21004750588e39e853baeeeca8b0a9792b391
Alvaro Herrera <alvherre@alvh.no-ip.org>
2021-11-01 13:07:23 -0300

    Handle XLOG_OVERWRITE_CONTRECORD in DecodeXLogOp
    Failing to do so results in inability of logical decoding to process the
    WAL stream.  Handle it by doing nothing.
    
    Backpatch all the way back.
    
    Reported-by: Petr Jelínek <petr.jelinek@enterprisedb.com>

--- src/backend/replication/logical/decode.c
+++ src/backend/replication/logical/decode.c
@@ -200,6 +200,7 @@ DecodeXLogOp(LogicalDecodingContext *ctx, XLogRecordBuffer *buf)
 		case XLOG_FPI_FOR_HINT:
 		case XLOG_FPI:
 		case XLOG_FPI_MULTI:
+		case XLOG_OVERWRITE_CONTRECORD:
 			break;
 		default:
 			elog(ERROR, "unexpected RM_XLOG_ID record type: %u", info);
