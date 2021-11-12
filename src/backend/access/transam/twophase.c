2d60ce3e1a4658c37a5408e56207d34af8763827
Noah Misch <noah@leadboat.com>
2021-11-11 17:10:18 -0800

    Report any XLogReadRecord() error in XlogReadTwoPhaseData().
    Buildfarm members kittiwake and tadarida have witnessed errors at this
    site.  The site discarded key facts.  Back-patch to v10 (all supported
    versions).
    
    Reviewed by Michael Paquier and Tom Lane.
    
    Discussion: https://postgr.es/m/20211107013157.GB790288@rfd.leadboat.com

--- src/backend/access/transam/twophase.c
+++ src/backend/access/transam/twophase.c
@@ -1465,11 +1465,21 @@ XlogReadTwoPhaseData(XLogRecPtr lsn, char **buf, int *len)
 	ThisTimeLineID = save_currtli;
 
 	if (record == NULL)
-		ereport(ERROR,
-				(errcode_for_file_access(),
-				 errmsg("could not read two-phase state from WAL at %X/%X",
-						(uint32) (lsn >> 32),
-						(uint32) lsn)));
+	{
+		if (errormsg)
+			ereport(ERROR,
+					(errcode_for_file_access(),
+					 errmsg("could not read two-phase state from WAL at %X/%X: %s",
+							(uint32) (lsn >> 32),
+							(uint32) lsn,
+							errormsg)));
+		else
+			ereport(ERROR,
+					(errcode_for_file_access(),
+					 errmsg("could not read two-phase state from WAL at %X/%X",
+							(uint32) (lsn >> 32),
+							(uint32) lsn)));
+	}
 
 	if (XLogRecGetRmid(xlogreader) != RM_XACT_ID ||
 		(XLogRecGetInfo(xlogreader) & XLOG_XACT_OPMASK) != XLOG_XACT_PREPARE)
