c2242d3640eadbe42556aad2f37eae393e84a4df
Tom Lane <tgl@sss.pgh.pa.us>
2021-11-21 14:13:35 -0500

    pg_receivewal, pg_recvlogical: allow canceling initial password prompt.
    Previously it was impossible to terminate these programs via control-C
    while they were prompting for a password.  We can fix that trivially
    for their initial password prompts, by moving setup of the SIGINT
    handler from just before to just after their initial GetConnection()
    calls.
    
    This fix doesn't permit escaping out of later re-prompts, but those
    should be exceedingly rare, since the user's password or the server's
    authentication setup would have to have changed meanwhile.  We
    considered applying a fix similar to commit 46d665bc2, but that
    seemed more complicated than it'd be worth.  Moreover, this way is
    back-patchable, which that wasn't.
    
    The misbehavior exists in all supported versions, so back-patch to all.
    
    Tom Lane and Nathan Bossart
    
    Discussion: https://postgr.es/m/747443.1635536754@sss.pgh.pa.us

--- src/bin/pg_basebackup/pg_receivewal.c
+++ src/bin/pg_basebackup/pg_receivewal.c
@@ -684,10 +684,6 @@ main(int argc, char **argv)
 		close_destination_dir(dir, basedir);
 	}
 
-#ifndef WIN32
-	pqsignal(SIGINT, sigint_handler);
-#endif
-
 	/*
 	 * Obtain a connection before doing anything.
 	 */
@@ -695,6 +691,14 @@ main(int argc, char **argv)
 	if (!conn)
 		/* error message already written in GetConnection() */
 		exit(1);
+
+	/*
+	 * Trap signals.  (Don't do this until after the initial password prompt,
+	 * if one is needed, in GetConnection.)
+	 */
+#ifndef WIN32
+	pqsignal(SIGINT, sigint_handler);
+#endif
 
 	/*
 	 * Run IDENTIFY_SYSTEM to make sure we've successfully have established a
