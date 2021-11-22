ec383ca2df0a6f77a9b49dcd8853e76e6b328d37
Tom Lane <tgl@sss.pgh.pa.us>
2021-11-22 12:54:52 -0500

    Probe $PROVE not $PERL while checking for modules needed by TAP tests.
    Normally "prove" and "perl" come from the same Perl installation,
    but we support the case where they don't (mainly because the MSys
    buildfarm animals need this).  In that case, AX_PROG_PERL_MODULES
    is completely the wrong thing to use, because it's checking what
    "perl" has.  Instead, make a little TAP test script including the
    required modules, and run that under "prove".
    
    We don't need ax_prog_perl_modules.m4 at all after this change,
    so remove it.
    
    Back-patch to all supported branches, for the buildfarm's benefit.
    (In v10, this also back-patches the effects of commit 264eb03aa.)
    
    Andrew Dunstan and Tom Lane, per an observation by Noah Misch
    
    Discussion: https://postgr.es/m/E1moZHS-0002Cu-Ei@gemulon.postgresql.org

--- aclocal.m4
+++ aclocal.m4
@@ -1,6 +1,5 @@
 dnl aclocal.m4
 m4_include([config/ac_func_accept_argtypes.m4])
-m4_include([config/ax_prog_perl_modules.m4])
 m4_include([config/ax_pthread.m4])
 m4_include([config/c-compiler.m4])
 m4_include([config/c-library.m4])
---
+++ config/check_modules.pl
@@ -0,0 +1,20 @@
+#
+# Verify that required Perl modules are available,
+# in at least the required minimum versions.
+# (The required minimum versions are all quite ancient now,
+# but specify them anyway for documentation's sake.)
+#
+use IPC::Run 0.79;
+
+# Test::More and Time::HiRes are supposed to be part of core Perl,
+# but some distros omit them in a minimal installation.
+use Test::More 0.87;
+use Time::HiRes 1.52;
+
+# While here, we might as well report exactly what versions we found.
+diag("IPC::Run::VERSION: $IPC::Run::VERSION");
+diag("Test::More::VERSION: $Test::More::VERSION");
+diag("Time::HiRes::VERSION: $Time::HiRes::VERSION");
+
+ok(1);
+done_testing();
