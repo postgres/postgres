a22d6a2cb62c4fc6d7c4b077d8014fd4ffaec426
Peter Eisentraut <peter@eisentraut.org>
2021-12-02 16:46:28 +0100

    pg_dump: Add missing relkind case
    Checking for RELKIND_MATVIEW was forgotten in
    guessConstraintInheritance().  This isn't a live problem, since it is
    checked in flagInhTables() which relkinds can have parents, and those
    entries will have numParents==0 after that.  But after discussion it
    was felt that this place should be kept consistent with
    flagInhTables() and flagInhAttrs().
    
    Reviewed-by: Michael Paquier <michael@paquier.xyz>
    Discussion: https://www.postgresql.org/message-id/flat/a574c8f1-9c84-93ad-a9e5-65233d6fc00f@enterprisedb.com

--- src/bin/pg_dump/pg_dump.c
+++ src/bin/pg_dump/pg_dump.c
@@ -2762,9 +2762,10 @@ guessConstraintInheritance(TableInfo *tblinfo, int numTables)
 		TableInfo **parents;
 		TableInfo  *parent;
 
-		/* Sequences and views never have parents */
+		/* Some kinds never have parents */
 		if (tbinfo->relkind == RELKIND_SEQUENCE ||
-			tbinfo->relkind == RELKIND_VIEW)
+			tbinfo->relkind == RELKIND_VIEW ||
+			tbinfo->relkind == RELKIND_MATVIEW)
 			continue;
 
 		/* Don't bother computing anything for non-target tables, either */
