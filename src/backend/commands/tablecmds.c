2ce113a4f085e078286a921d4cbe63b0dc34794e
Tom Lane <tgl@sss.pgh.pa.us>
2022-01-06 16:46:46 -0500

    Prevent altering partitioned table's rowtype, if it's used elsewhere.
    We disallow altering a column datatype within a regular table,
    if the table's rowtype is used as a column type elsewhere,
    because we lack code to go around and rewrite the other tables.
    This restriction should apply to partitioned tables as well, but it
    was not checked because ATRewriteTables and ATPrepAlterColumnType
    were not on the same page about who should do it for which relkinds.
    
    Per bug #17351 from Alexander Lakhin.  Back-patch to all supported
    branches.
    
    Discussion: https://postgr.es/m/17351-6db1870f3f4f612a@postgresql.org

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -9878,11 +9878,13 @@ ATPrepAlterColumnType(List **wqueue,
 						RelationGetRelationName(rel))));
 
 	if (tab->relkind == RELKIND_COMPOSITE_TYPE ||
-		tab->relkind == RELKIND_FOREIGN_TABLE)
+		tab->relkind == RELKIND_FOREIGN_TABLE ||
+		tab->relkind == RELKIND_PARTITIONED_TABLE ||
+		tab->relkind == RELKIND_PARTITIONED_INDEX)
 	{
 		/*
-		 * For composite types, do this check now.  Tables will check it later
-		 * when the table is being rewritten.
+		 * For relations without storage, do this check now.  Regular tables
+		 * will check it later when the table is being rewritten.
 		 */
 		find_composite_type_dependencies(rel->rd_rel->reltype, rel, NULL);
 	}
