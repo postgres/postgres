c7fa0f55deb17960de97285595e4a5342f0136c1
Andrew Dunstan <andrew@dunslane.net>
2022-01-07 16:07:45 -0500

    Allow MSVC .bat wrappers to be called from anywhere
    Instead of using a hardcoded or default path to the perl file the .bat
    file is a wrapper for, we use a path that means the file is found in
    the same directory as the .bat file.
    
    Patch by Anton Voloshin, slightly tweaked by me.
    
    Backpatch to all live branches
    
    Discussion: https://postgr.es/m/2b7a674b-5fb0-d264-75ef-ecc7a31e54f8@postgrespro.ru

--- src/tools/msvc/pgbison.bat
+++ src/tools/msvc/pgbison.bat
@@ -4,4 +4,4 @@ REM src/tools/msvc/pgbison.bat
 REM all the logic for this now belongs in pgbison.pl. This file really
 REM only exists so you don't have to type "perl src/tools/msvc/pgbison.pl"
 REM Resist any temptation to add any logic here.
-@perl src/tools/msvc/pgbison.pl %*
+@perl %~dp0/pgbison.pl %*
