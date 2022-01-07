d897361c7567ada5d11e3ee40e29a947264e3ecf
Andrew Dunstan <andrew@dunslane.net>
2022-01-07 16:07:45 -0500

    Allow MSVC .bat wrappers to be called from anywhere
    Instead of using a hardcoded or default path to the perl file the .bat
    file is a wrapper for, we use a path that means the file is found in
    the same directory as the .bat file.
    
    Patch by Anton Voloshin, slightly tweaked by me.
    
    Backpatch to all live branches
    
    Discussion: https://postgr.es/m/2b7a674b-5fb0-d264-75ef-ecc7a31e54f8@postgrespro.ru

--- src/tools/msvc/build.bat
+++ src/tools/msvc/build.bat
@@ -3,4 +3,4 @@ REM src/tools/msvc/build.bat
 REM all the logic for this now belongs in build.pl. This file really
 REM only exists so you don't have to type "perl build.pl"
 REM Resist any temptation to add any logic here.
-@perl build.pl %*
+@perl %~dp0/build.pl %*
--- src/tools/msvc/install.bat
+++ src/tools/msvc/install.bat
@@ -3,4 +3,4 @@ REM src/tools/msvc/install.bat
 REM all the logic for this now belongs in install.pl. This file really
 REM only exists so you don't have to type "perl install.pl"
 REM Resist any temptation to add any logic here.
-@perl install.pl %*
+@perl %~dp0/install.pl %*
