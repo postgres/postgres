81596645ca2ba382918fca9e82aa3089db737e14
Magnus Hagander <magnus@hagander.net>
2022-01-26 09:52:41 +0100

    Fix pg_hba_file_rules for authentication method cert
    For authentication method cert, clientcert=verify-full is implied. But
    the pg_hba_file_rules entry would incorrectly show clientcert=verify-ca.
    
    Per bug #17354
    
    Reported-By: Feike Steenbergen
    Reviewed-By: Jonathan Katz
    Backpatch-through: 12

--- src/backend/libpq/hba.c
+++ src/backend/libpq/hba.c
@@ -1632,7 +1632,11 @@ parse_hba_line(TokenizedLine *tok_line, int elevel)
 	 */
 	if (parsedline->auth_method == uaCert)
 	{
-		parsedline->clientcert = clientCertCA;
+		/*
+		 * For auth method cert, client certificate validation is mandatory, and it implies
+		 * the level of verify-full.
+		 */
+		parsedline->clientcert = clientCertFull;
 	}
 
 	return parsedline;
