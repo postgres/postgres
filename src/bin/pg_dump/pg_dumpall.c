138c51b72168e7b57c9edb4e9935274d3abf6bed
Robert Haas <rhaas@postgresql.org>
2022-02-17 10:53:51 -0500

    Add missing binary-upgrade guard.
    Commit 9a974cbcba005256a19991203583a94b4f9a21a9 arranged for
    pg_dumpall to preserve tablespace OIDs, but it should only do that
    in binary upgrade mode, not all the time.
    
    Reported by Christoph Berg.
    
    Discussion: http://postgr.es/m/YgjwrkEvNEqoz4Vm@msg.df7cb.de

--- src/bin/pg_dump/pg_dumpall.c
+++ src/bin/pg_dump/pg_dumpall.c
@@ -1066,8 +1066,11 @@ dumpTablespaces(PGconn *conn)
 		/* needed for buildACLCommands() */
 		fspcname = pg_strdup(fmtId(spcname));
 
-		appendPQExpBufferStr(buf, "\n-- For binary upgrade, must preserve pg_tablespace oid\n");
-		appendPQExpBuffer(buf, "SELECT pg_catalog.binary_upgrade_set_next_pg_tablespace_oid('%u'::pg_catalog.oid);\n", spcoid);
+		if (binary_upgrade)
+		{
+			appendPQExpBufferStr(buf, "\n-- For binary upgrade, must preserve pg_tablespace oid\n");
+			appendPQExpBuffer(buf, "SELECT pg_catalog.binary_upgrade_set_next_pg_tablespace_oid('%u'::pg_catalog.oid);\n", spcoid);
+		}
 
 		appendPQExpBuffer(buf, "CREATE TABLESPACE %s", fspcname);
 		appendPQExpBuffer(buf, " OWNER %s", fmtId(spcowner));
