f7ea240aa7491b6ed2985bb50888bd432f3341df
Tom Lane <tgl@sss.pgh.pa.us>
2022-03-03 20:03:47 -0500

    Tighten overflow checks in tidin().
    This code seems to have been written on the assumption that
    "unsigned long" is 32 bits; or at any rate it ignored the
    possibility of conversion overflow.  Rewrite, borrowing some
    logic from oidin().
    
    Discussion: https://postgr.es/m/3441768.1646343914@sss.pgh.pa.us

--- src/test/regress/sql/tid.sql
+++ src/test/regress/sql/tid.sql
@@ -1,3 +1,15 @@
+-- basic tests for the TID data type
+
+SELECT
+  '(0,0)'::tid as tid00,
+  '(0,1)'::tid as tid01,
+  '(-1,0)'::tid as tidm10,
+  '(4294967295,65535)'::tid as tidmax;
+
+SELECT '(4294967296,1)'::tid;  -- error
+SELECT '(1,65536)'::tid;  -- error
+
+
 -- tests for functions related to TID handling
 
 CREATE TABLE tid_tab (a int);
