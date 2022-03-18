84f3ecdaae8333d4e4815f34726fd07e46c3e3d4
Tom Lane <tgl@sss.pgh.pa.us>
2022-03-18 16:01:42 -0400

    Fix incorrect xmlschema output for types timetz and timestamptz.
    The output of table_to_xmlschema() and allied functions includes
    a regex describing valid values for these types ... but the regex
    was itself invalid, as it failed to escape a literal "+" sign.
    
    Report and fix by Renan Soares Lopes.  Back-patch to all
    supported branches.
    
    Discussion: https://postgr.es/m/7f6fabaa-3f8f-49ab-89ca-59fbfe633105@me.com

--- src/backend/utils/adt/xml.c
+++ src/backend/utils/adt/xml.c
@@ -3642,7 +3642,7 @@ map_sql_type_to_xmlschema_type(Oid typeoid, int typmod)
 			case TIMEOID:
 			case TIMETZOID:
 				{
-					const char *tz = (typeoid == TIMETZOID ? "(+|-)\\p{Nd}{2}:\\p{Nd}{2}" : "");
+					const char *tz = (typeoid == TIMETZOID ? "(\\+|-)\\p{Nd}{2}:\\p{Nd}{2}" : "");
 
 					if (typmod == -1)
 						appendStringInfo(&result,
@@ -3665,7 +3665,7 @@ map_sql_type_to_xmlschema_type(Oid typeoid, int typmod)
 			case TIMESTAMPOID:
 			case TIMESTAMPTZOID:
 				{
-					const char *tz = (typeoid == TIMESTAMPTZOID ? "(+|-)\\p{Nd}{2}:\\p{Nd}{2}" : "");
+					const char *tz = (typeoid == TIMESTAMPTZOID ? "(\\+|-)\\p{Nd}{2}:\\p{Nd}{2}" : "");
 
 					if (typmod == -1)
 						appendStringInfo(&result,
