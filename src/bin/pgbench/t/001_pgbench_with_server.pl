ac9c5dc5d4cc15e396b59ca66df25969e51a2a00
Tatsuo Ishii <ishii@postgresql.org>
2022-03-24 13:32:59 +0900

    Fix pgbench TAP test.
    Bildfarm member prairiedog reported a pgbench TAP test failure after
    commit: 4a39f87acd6e681e5ded1239391d8a92645b43d6. This commit attempts
    to fix some copy&paste errors introduced in the previous commit.
    
    Author: Yugo Nagata
    Reported-by: Tom Lane
    Discussion: https://postgr.es/m/2775989.1648060014%40sss.pgh.pa.us

--- src/bin/pgbench/t/001_pgbench_with_server.pl
+++ src/bin/pgbench/t/001_pgbench_with_server.pl
@@ -1222,7 +1222,8 @@ local $ENV{PGOPTIONS} = "-c default_transaction_isolation=repeatable\\ read";
 # Check that we have a serialization error and the same random value of the
 # delta variable in the next try
 my $err_pattern =
-    "client (0|1) got an error in command 3 \\(SQL\\) of script 0; "
+	"(client (0|1) sending UPDATE xy SET y = y \\+ -?\\d+\\b).*"
+  . "client \\g2 got an error in command 3 \\(SQL\\) of script 0; "
   . "ERROR:  could not serialize access due to concurrent update\\b.*"
   . "\\g1";
 
