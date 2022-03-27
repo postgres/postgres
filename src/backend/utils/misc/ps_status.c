postgres/postgres

commit 8c6d30f211390df911072d33f0114a31f066a4cd

Fix compiler warnings on MSYS2
The PS_USE_NONE case in ps_status.c left a couple of unused variables
exposed.

Discussion: https://www.postgresql.org/message-id/flat/6b467edc-4018-521f-ab18-171f098557ca%402ndquadrant.com

src/backend/utils/misc/ps_status.c
@@ -89,6 +89,8 @@ bool		update_process_title = true;
 #endif
 
 
+#ifndef PS_USE_NONE
+
 #ifndef PS_USE_CLOBBER_ARGV
 /* all but one option need a buffer to write their ps line in */
 #define PS_BUFFER_SIZE 256
@@ -103,6 +105,8 @@ static size_t last_status_len;	/* use to minimize length of clobber */
 static size_t ps_buffer_cur_len;	/* nominal strlen(ps_buffer) */
 
 static size_t ps_buffer_fixed_size; /* size of the constant prefix */
+
+#endif							/* not PS_USE_NONE */
 
 /* save the original argv[] location here */
 static int	save_argc;
@@ -420,7 +424,11 @@ get_ps_display(int *displen)
 	}
 #endif
 
+#ifndef PS_USE_NONE
 	*displen = (int) (ps_buffer_cur_len - ps_buffer_fixed_size);
 
 	return ps_buffer + ps_buffer_fixed_size;
+#else
+	return "";
+#endif
 }

commit 0fb6954aa5012fc0c41af364fb328f90e648f6b7

Fix breakage of get_ps_display() in the PS_USE_NONE case.
Commit 8c6d30f21 caused this function to fail to set *displen
in the PS_USE_NONE code path.  If the variable's previous value
had been negative, that'd lead to a memory clobber at some call
sites.  We'd managed not to notice due to very thin test coverage
of such configurations, but this appears to explain buildfarm member
lorikeet's recent struggles.

Credit to Andrew Dunstan for spotting the problem.  Back-patch
to v13 where the bug was introduced.

Discussion: https://postgr.es/m/136102.1648320427@sss.pgh.pa.us

src/backend/utils/misc/ps_status.c
@@ -443,6 +443,7 @@ get_ps_display(int *displen)
 
 	return ps_buffer + ps_buffer_fixed_size;
 #else
+	*displen = 0;
 	return "";
 #endif
 }