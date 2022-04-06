dbafe127bb215f512164669b49f99fcb7ed9d266
Tom Lane <tgl@sss.pgh.pa.us>
2022-04-06 17:03:35 -0400

    Suppress "variable 'pagesaving' set but not used" warning.
    With asserts disabled, late-model clang notices that this variable
    is incremented but never otherwise read.
    
    Discussion: https://postgr.es/m/3171401.1649275153@sss.pgh.pa.us

--- src/backend/access/nbtree/nbtdedup.c
+++ src/backend/access/nbtree/nbtdedup.c
@@ -65,7 +65,7 @@ _bt_dedup_pass(Relation rel, Buffer buf, Relation heapRel, IndexTuple newitem,
 	BTPageOpaque opaque = BTPageGetOpaque(page);
 	Page		newpage;
 	BTDedupState state;
-	Size		pagesaving = 0;
+	Size		pagesaving PG_USED_FOR_ASSERTS_ONLY = 0;
 	bool		singlevalstrat = false;
 	int			nkeyatts = IndexRelationGetNumberOfKeyAttributes(rel);
 
