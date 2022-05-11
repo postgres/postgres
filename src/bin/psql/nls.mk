3aa7a3d2a3d1177d32680b94541dc2db7f3d7b0b
Peter Eisentraut <peter@eisentraut.org>
2022-05-11 06:16:21 +0200

    Add missing source files to nls.mk

--- src/bin/psql/nls.mk
+++ src/bin/psql/nls.mk
@@ -8,7 +8,7 @@ GETTEXT_FILES    = $(FRONTEND_COMMON_GETTEXT_FILES) \
                    tab-complete.c variables.c \
                    ../../fe_utils/cancel.c ../../fe_utils/print.c ../../fe_utils/psqlscan.c \
                    ../../common/exec.c ../../common/fe_memutils.c ../../common/username.c \
-                   ../../common/wait_error.c
+                   ../../common/wait_error.c ../../port/thread.c
 GETTEXT_TRIGGERS = $(FRONTEND_COMMON_GETTEXT_TRIGGERS) \
                    N_ simple_prompt simple_prompt_extended
 GETTEXT_FLAGS    = $(FRONTEND_COMMON_GETTEXT_FLAGS)
