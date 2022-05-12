c2f436151edc818601c2499e400e1ad53d0da66f
Tom Lane <tgl@sss.pgh.pa.us>
2022-05-12 15:35:15 -0400

    Do pre-release housekeeping on catalog data.
    Run renumber_oids.pl to move high-numbered OIDs down, as per pre-beta
    tasks specified by RELEASE_CHANGES.  For reference, the command was
    
    ./renumber_oids.pl --first-mapped-oid 8000 --target-oid 6205

--- src/include/catalog/catversion.h
+++ src/include/catalog/catversion.h
@@ -53,6 +53,6 @@
  */
 
 /*							yyyymmddN */
-#define CATALOG_VERSION_NO	202204076
+#define CATALOG_VERSION_NO	202205121
 
 #endif
--- src/include/catalog/pg_parameter_acl.h
+++ src/include/catalog/pg_parameter_acl.h
@@ -27,7 +27,7 @@
  *		typedef struct FormData_pg_parameter_acl
  * ----------------
  */
-CATALOG(pg_parameter_acl,8924,ParameterAclRelationId) BKI_SHARED_RELATION
+CATALOG(pg_parameter_acl,6243,ParameterAclRelationId) BKI_SHARED_RELATION
 {
 	Oid			oid;			/* oid */
 
@@ -48,10 +48,10 @@ CATALOG(pg_parameter_acl,8924,ParameterAclRelationId) BKI_SHARED_RELATION
  */
 typedef FormData_pg_parameter_acl * Form_pg_parameter_acl;
 
-DECLARE_TOAST_WITH_MACRO(pg_parameter_acl, 8925, 8926, PgParameterAclToastTable, PgParameterAclToastIndex);
+DECLARE_TOAST_WITH_MACRO(pg_parameter_acl, 6244, 6245, PgParameterAclToastTable, PgParameterAclToastIndex);
 
-DECLARE_UNIQUE_INDEX(pg_parameter_acl_parname_index, 8927, ParameterAclParnameIndexId, on pg_parameter_acl using btree(parname text_ops));
-DECLARE_UNIQUE_INDEX_PKEY(pg_parameter_acl_oid_index, 8928, ParameterAclOidIndexId, on pg_parameter_acl using btree(oid oid_ops));
+DECLARE_UNIQUE_INDEX(pg_parameter_acl_parname_index, 6246, ParameterAclParnameIndexId, on pg_parameter_acl using btree(parname text_ops));
+DECLARE_UNIQUE_INDEX_PKEY(pg_parameter_acl_oid_index, 6247, ParameterAclOidIndexId, on pg_parameter_acl using btree(oid oid_ops));
 
 
 extern Oid	ParameterAclLookup(const char *parameter, bool missing_ok);
