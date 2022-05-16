cd690e07fd47f245df02ab461754755956bf3e00
Peter Eisentraut <peter@eisentraut.org>
2022-05-16 09:41:02 +0200

    pg_upgrade: Add missing gettext triggers
    Forgot to add it in one place in the previous commit.

--- src/bin/pg_upgrade/nls.mk
+++ src/bin/pg_upgrade/nls.mk
@@ -4,7 +4,7 @@ AVAIL_LANGUAGES  = cs de es fr ja ko ru sv tr uk zh_CN
 GETTEXT_FILES    = check.c controldata.c dump.c exec.c file.c function.c \
                    info.c option.c parallel.c pg_upgrade.c relfilenode.c \
                    server.c tablespace.c util.c version.c
-GETTEXT_TRIGGERS = pg_fatal pg_log:2 prep_status report_status:2
+GETTEXT_TRIGGERS = pg_fatal pg_log:2 prep_status prep_status_progress report_status:2
 GETTEXT_FLAGS    = \
     pg_fatal:1:c-format \
     pg_log:2:c-format \
