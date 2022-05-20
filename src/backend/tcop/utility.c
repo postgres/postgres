3753a169e110bfedd1c7eef5159d3a5c05cd881e
Alvaro Herrera <alvherre@alvh.no-ip.org>
2022-05-20 18:52:55 +0200

    Fix DDL deparse of CREATE OPERATOR CLASS
    When an implicit operator family is created, it wasn't getting reported.
    Make it do so.
    
    This has always been missing.  Backpatch to 10.
    
    Author: Masahiko Sawada <sawada.mshk@gmail.com>
    Reported-by: Leslie LEMAIRE <leslie.lemaire@developpement-durable.gouv.fr>
    Reviewed-by: Amit Kapila <amit.kapila16@gmail.com>
    Reviewed-by: Michael Paquiër <michael@paquier.xyz>
    Discussion: https://postgr.es/m/f74d69e151b22171e8829551b1159e77@developpement-durable.gouv.fr

--- src/backend/tcop/utility.c
+++ src/backend/tcop/utility.c
@@ -1713,6 +1713,12 @@ ProcessUtilitySlow(ParseState *pstate,
 
 			case T_CreateOpFamilyStmt:
 				address = DefineOpFamily((CreateOpFamilyStmt *) parsetree);
+
+				/*
+				 * DefineOpFamily calls EventTriggerCollectSimpleCommand
+				 * directly.
+				 */
+				commandCollected = true;
 				break;
 
 			case T_CreateTransformStmt:
