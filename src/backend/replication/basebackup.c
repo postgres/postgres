33bd4698c1174d24f6e53f965f6d64aeab3611f6
Michael Paquier <michael@paquier.xyz>
2022-07-01 09:37:17 +0900

    Fix code comments still referring to pg_start/stop_backup()
    pg_start_backup() and pg_stop_backup() have been respectively renamed to
    pg_backup_start() and pg_backup_stop() as of 39969e2, but a few comments
    did not get the call.
    
    Reviewed-by: Kyotaro Horiguchi, David Steele
    Discussion: https://postgr.es/m/YrqGlj1+4DF3dbZ/@paquier.xyz

--- src/backend/replication/basebackup.c
+++ src/backend/replication/basebackup.c
@@ -16,7 +16,7 @@
 #include <unistd.h>
 #include <time.h>
 
-#include "access/xlog_internal.h"	/* for pg_start/stop_backup */
+#include "access/xlog_internal.h"	/* for pg_backup_start/stop */
 #include "common/compression.h"
 #include "common/file_perm.h"
 #include "commands/defrem.h"
