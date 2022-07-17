94bcb48ab53b2d3b322acc541efe02d9702bb105
Tom Lane <tgl@sss.pgh.pa.us>
2022-07-17 17:27:50 -0400

    postgres_fdw: set search_path to 'pg_catalog' while deparsing constants.
    The motivation for this is to ensure successful transmission of the
    values of constants of regconfig and other reg* types.  The remote
    will be reading them with search_path = 'pg_catalog', so schema
    qualification is necessary when referencing objects in other schemas.
    
    Per bug #17483 from Emmanuel Quincerot.  Back-patch to all supported
    versions.  (There's some other stuff to do here, but it's less
    back-patchable.)
    
    Discussion: https://postgr.es/m/1423433.1652722406@sss.pgh.pa.us

--- contrib/postgres_fdw/sql/postgres_fdw.sql
+++ contrib/postgres_fdw/sql/postgres_fdw.sql
@@ -370,6 +370,15 @@ EXPLAIN (VERBOSE, COSTS OFF)
   SELECT count(c3) FROM ft1 t1 WHERE t1.c1 === t1.c2;
 SELECT count(c3) FROM ft1 t1 WHERE t1.c1 === t1.c2;
 
+-- check schema-qualification of regconfig constant
+CREATE TEXT SEARCH CONFIGURATION public.custom_search
+  (COPY = pg_catalog.english);
+EXPLAIN (VERBOSE, COSTS OFF)
+SELECT c1, to_tsvector('custom_search'::regconfig, c3) FROM ft1
+WHERE c1 = 642 AND length(to_tsvector('custom_search'::regconfig, c3)) > 0;
+SELECT c1, to_tsvector('custom_search'::regconfig, c3) FROM ft1
+WHERE c1 = 642 AND length(to_tsvector('custom_search'::regconfig, c3)) > 0;
+
 -- ===================================================================
 -- JOIN queries
 -- ===================================================================
