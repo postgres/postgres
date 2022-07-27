258c896418bf972728d60e6b7d197303942d31aa
Alvaro Herrera <alvherre@alvh.no-ip.org>
2022-07-27 07:55:12 +0200

    Allow "in place" tablespaces.
    This is a backpatch to branches 10-14 of the following commits:
    
    7170f2159fb2 Allow "in place" tablespaces.
    c6f2f01611d4 Fix pg_basebackup with in-place tablespaces.
    f6f0db4d6240 Fix pg_tablespace_location() with in-place tablespaces
    7a7cd84893e0 doc: Remove mention to in-place tablespaces for pg_tablespace_location()
    5344723755bd Remove unnecessary Windows-specific basebackup code.
    
    In-place tablespaces were introduced as a testing helper mechanism, but
    they are going to be used for a bugfix in WAL replay to be backpatched
    to all stable branches.
    
    I (Álvaro) had to adjust some code to account for lack of
    get_dirent_type() in branches prior to 14.
    
    Author: Thomas Munro <thomas.munro@gmail.com>
    Author: Michaël Paquier <michael@paquier.xyz>
    Author: Álvaro Herrera <alvherre@alvh.no-ip.org>
    Discussion: https://postgr.es/m/20220722081858.omhn2in5zt3g4nek@alvherre.pgsql

--- src/backend/utils/misc/guc.c
+++ src/backend/utils/misc/guc.c
@@ -37,6 +37,7 @@
 #include "catalog/pg_authid.h"
 #include "commands/async.h"
 #include "commands/prepare.h"
+#include "commands/tablespace.h"
 #include "commands/user.h"
 #include "commands/vacuum.h"
 #include "commands/variable.h"
@@ -1671,6 +1672,17 @@ static struct config_bool ConfigureNamesBool[] =
 			GUC_NOT_IN_SAMPLE
 		},
 		&IgnoreSystemIndexes,
+		false,
+		NULL, NULL, NULL
+	},
+
+	{
+		{"allow_in_place_tablespaces", PGC_SUSET, DEVELOPER_OPTIONS,
+			gettext_noop("Allows tablespaces directly inside pg_tblspc, for testing."),
+			NULL,
+			GUC_NOT_IN_SAMPLE
+		},
+		&allow_in_place_tablespaces,
 		false,
 		NULL, NULL, NULL
 	},
