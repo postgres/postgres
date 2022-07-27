961cab0a5a90d449a64a71912b324d7e7548619b
Alvaro Herrera <alvherre@alvh.no-ip.org>
2022-07-27 07:55:13 +0200

    Allow "in place" tablespaces.
    This is a backpatch to branches 10-14 of the following commits:
    
    7170f2159fb2 Allow "in place" tablespaces.
    c6f2f01611d4 Fix pg_basebackup with in-place tablespaces.
    f6f0db4d6240 Fix pg_tablespace_location() with in-place tablespaces
    7a7cd84893e0 doc: Remove mention to in-place tablespaces for pg_tablespace_location()
    5344723755bd Remove unnecessary Windows-specific basebackup code.
    
    In-place tablespaces were introduced as a testing helper mechanism, but
    they are going to be used for a bugfix in WAL replay to be backpatched
    to all stable branches.
    
    I (Álvaro) had to adjust some code to account for lack of
    get_dirent_type() in branches prior to 14.
    
    Author: Thomas Munro <thomas.munro@gmail.com>
    Author: Michaël Paquier <michael@paquier.xyz>
    Author: Álvaro Herrera <alvherre@alvh.no-ip.org>
    Discussion: https://postgr.es/m/20220722081858.omhn2in5zt3g4nek@alvherre.pgsql

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -43,6 +43,7 @@
 #include "commands/progress.h"
 #include "commands/tablespace.h"
 #include "common/controldata_utils.h"
+#include "common/file_utils.h"
 #include "executor/instrument.h"
 #include "miscadmin.h"
 #include "pg_trace.h"
@@ -11167,6 +11168,14 @@ do_pg_start_backup(const char *backupidstr, bool fast, TimeLineID *starttli_p,
 				continue;
 
 			snprintf(fullpath, sizeof(fullpath), "pg_tblspc/%s", de->d_name);
+
+			/*
+			 * Skip anything that isn't a symlink/junction.  For testing only,
+			 * we sometimes use allow_in_place_tablespaces to create
+			 * directories directly under pg_tblspc, which would fail below.
+			 */
+			if (get_dirent_type(fullpath, de, false, ERROR) != PGFILETYPE_LNK)
+				continue;
 
 #if defined(HAVE_READLINK) || defined(WIN32)
 			rllen = readlink(fullpath, linkpath, sizeof(linkpath));
