599a487b093acb23ae1da7fe29eb0b6a4c26cc2c
Tom Lane <tgl@sss.pgh.pa.us>
2022-08-24 13:01:40 -0400

    Defend against stack overrun in a few more places.
    SplitToVariants() in the ispell code, lseg_inside_poly() in geo_ops.c,
    and regex_selectivity_sub() in selectivity estimation could recurse
    until stack overflow; fix by adding check_stack_depth() calls.
    So could next() in the regex compiler, but that case is better fixed by
    converting its tail recursion to a loop.  (We probably get better code
    that way too, since next() can now be inlined into its sole caller.)
    
    There remains a reachable stack overrun in the Turkish stemmer, but
    we'll need some advice from the Snowball people about how to fix that.
    
    Per report from Egor Chindyaskin and Alexander Lakhin.  These mistakes
    are old, so back-patch to all supported branches.
    
    Richard Guo and Tom Lane
    
    Discussion: https://postgr.es/m/1661334672.728714027@f473.i.mail.ru

--- src/backend/utils/adt/like_support.c
+++ src/backend/utils/adt/like_support.c
@@ -43,6 +43,7 @@
 #include "catalog/pg_statistic.h"
 #include "catalog/pg_type.h"
 #include "mb/pg_wchar.h"
+#include "miscadmin.h"
 #include "nodes/makefuncs.h"
 #include "nodes/nodeFuncs.h"
 #include "nodes/supportnodes.h"
@@ -1319,6 +1320,9 @@ regex_selectivity_sub(const char *patt, int pattlen, bool case_insensitive)
 	int			paren_depth = 0;
 	int			paren_pos = 0;	/* dummy init to keep compiler quiet */
 	int			pos;
+
+	/* since this function recurses, it could be driven to stack overflow */
+	check_stack_depth();
 
 	for (pos = 0; pos < pattlen; pos++)
 	{
