69a498eb6465da053604e64654179eaa62423681
Noah Misch <noah@leadboat.com>
2022-09-15 06:45:23 -0700

    Reset InstallXLogFileSegmentActive after walreceiver self-initiated exit.
    After commit cc2c7d65fc27e877c9f407587b0b92d46cd6dd16 added this flag,
    failure to reset it caused assertion failures.  In non-assert builds, it
    made the system fail to achieve the objectives listed in that commit;
    chiefly, we might emit a spurious log message.  Back-patch to v15, where
    that commit first appeared.
    
    Bharath Rupireddy and Kyotaro Horiguchi.  Reviewed by Dilip Kumar,
    Nathan Bossart and Michael Paquier.  Reported by Dilip Kumar.
    
    This commit has been applied as of b4f584f9d2a1 in v15 and newer
    versions.  This is required on stable branches of v13 and v14 to fix a
    regression reported by Noah Misch, introduced by 1f95181b44c8, causing
    spurious failures in archive recovery (neither streaming nor archive
    recovery) with concurrent restartpoints.  The backpatched versions of
    the patches have been aligned on these branches by me.  Tests have been
    conducted by the both of us.
    
    Discussion: https://postgr.es/m/CAFiTN-sE3ry=ycMPVtC+Djw4Fd7gbUGVv_qqw6qfzp=JLvqT3g@mail.gmail.com
    Discussion: https://postgr.es/m/20250306193013.36.nmisch@google.com
    Backpatch-through: 13

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -12573,8 +12573,7 @@ WaitForWALToBecomeAvailable(XLogRecPtr RecPtr, bool randAccess,
 					 * walreceiver is not active, so that it won't overwrite
 					 * WAL that we restore from archive.
 					 */
-					if (WalRcvStreaming())
-						XLogShutdownWalRcv();
+					XLogShutdownWalRcv();
 
 					/*
 					 * Before we sleep, re-scan for possible new timelines if
