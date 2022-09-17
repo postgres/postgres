c1dd5a8aedc7184d9a013721960813f7ace156ab
Peter Geoghegan <pg@bowt.ie>
2022-09-17 16:54:10 -0700

    Make check_usermap() parameter names consistent.
    The function has a bool argument named "case_insensitive", but that was
    spelled "case_sensitive" in the declaration.  Make them consistent now
    to avoid confusion in the future.
    
    Author: Peter Geoghegan <pg@bowt.ie>
    Reviewed-By: Michael Paquiër <michael@paquier.xyz>
    Discussion: https://postgr.es/m/CAH2-WznJt9CMM9KJTMjJh_zbL5hD9oX44qdJ4aqZtjFi-zA3Tg@mail.gmail.com
    Backpatch: 10-

--- src/include/libpq/hba.h
+++ src/include/libpq/hba.h
@@ -134,7 +134,7 @@ extern bool load_ident(void);
 extern void hba_getauthmethod(hbaPort *port);
 extern int	check_usermap(const char *usermap_name,
 						  const char *pg_role, const char *auth_user,
-						  bool case_sensitive);
+						  bool case_insensitive);
 extern bool pg_isblank(const char c);
 
 #endif							/* HBA_H */
