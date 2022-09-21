bb8dbc9f25127a818c1d84ac8689029d20a8cf57
Tom Lane <tgl@sss.pgh.pa.us>
2022-09-21 13:52:38 -0400

    Suppress more variable-set-but-not-used warnings from clang 15.
    Mop up assorted set-but-not-used warnings in the back branches.
    This includes back-patching relevant fixes from commit 152c9f7b8
    the rest of the way, but there are also several cases that did not
    appear in HEAD.  Some of those we'd fixed in a retail way but not
    back-patched, and others I think just got rewritten out of existence
    during nearby refactoring.
    
    While here, also back-patch b1980f6d0 (PL/Tcl: Fix compiler warnings
    with Tcl 8.6) into 9.2, so that that branch compiles warning-free
    with modern Tcl.
    
    Per project policy, this is a candidate for back-patching into
    out-of-support branches: it suppresses annoying compiler warnings
    but changes no behavior.  Hence, back-patch all the way to 9.2.
    
    Discussion: https://postgr.es/m/514615.1663615243@sss.pgh.pa.us

--- src/backend/utils/cache/relcache.c
+++ src/backend/utils/cache/relcache.c
@@ -4096,7 +4096,6 @@ AttrDefaultFetch(Relation relation)
 	HeapTuple	htup;
 	Datum		val;
 	bool		isnull;
-	int			found;
 	int			i;
 
 	ScanKeyInit(&skey,
@@ -4107,7 +4106,6 @@ AttrDefaultFetch(Relation relation)
 	adrel = heap_open(AttrDefaultRelationId, AccessShareLock);
 	adscan = systable_beginscan(adrel, AttrDefaultIndexId, true,
 								NULL, 1, &skey);
-	found = 0;
 
 	while (HeapTupleIsValid(htup = systable_getnext(adscan)))
 	{
@@ -4122,8 +4120,6 @@ AttrDefaultFetch(Relation relation)
 				elog(WARNING, "multiple attrdef records found for attr %s of rel %s",
 					 NameStr(attr->attname),
 					 RelationGetRelationName(relation));
-			else
-				found++;
 
 			val = fastgetattr(htup,
 							  Anum_pg_attrdef_adbin,
