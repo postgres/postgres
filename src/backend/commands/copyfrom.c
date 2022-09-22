e4514aafad0b134e67d7d0cf1612fb5c76fc8fec
Etsuro Fujita <efujita@postgresql.org>
2022-09-22 15:55:03 +0900

    Fix thinko in comment.
    This comment has been wrong since its introduction in commit 0d5f05cde;
    backpatch to v12 where that came in.
    
    Discussion: https://postgr.es/m/CAPmGK14VGf-xQjGQN4o1QyAbXAaxugU5%3DqfcmTDh1iufUDnV_w%40mail.gmail.com

--- src/backend/commands/copyfrom.c
+++ src/backend/commands/copyfrom.c
@@ -729,8 +729,8 @@ CopyFrom(CopyFromState cstate)
 		 * For partitioned tables we can't support multi-inserts when there
 		 * are any statement level insert triggers. It might be possible to
 		 * allow partitioned tables with such triggers in the future, but for
-		 * now, CopyMultiInsertInfoFlush expects that any before row insert
-		 * and statement level insert triggers are on the same relation.
+		 * now, CopyMultiInsertInfoFlush expects that any after row insert and
+		 * statement level insert triggers are on the same relation.
 		 */
 		insertMethod = CIM_SINGLE;
 	}
