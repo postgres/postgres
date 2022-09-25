1d2fec990c4dd6da8ffe5ca6b3c2aed2553197fb
Tom Lane <tgl@sss.pgh.pa.us>
2022-09-25 13:10:10 -0400

    Avoid loss of code coverage with unlogged-index test cases.
    Commit 4fb5c794e intended to add coverage of some ambuildempty
    methods that were not getting reached, without removing any
    test coverage.  However, by changing a temp table to unlogged
    it managed to negate the intent of 4c51a2d1e, which means that
    we didn't have reliable test coverage of ginvacuum.c anymore.
    As things stand, much of that file might or might not get reached
    depending on timing, which seems pretty undesirable.
    
    Although this is only clearly broken for the GIN test, it seems
    best to revert 4fb5c794e altogether and instead add bespoke test
    cases covering unlogged indexes for these four AMs.  We don't
    need to do very much with them, so the extra tests are cheap.
    (Note that btree, hash, and bloom already have similar test cases,
    so they need no additional work.)
    
    We can also undo dec8ad367.  Since the testing deficiency that that
    hacked around was later fixed by 2f2e24d90, let's intentionally leave
    an unlogged table behind to improve test coverage in the modules that
    use the regression database for other test purposes.  (The case I used
    also leaves an unlogged sequence behind.)
    
    Per report from Alex Kozhemyakin.  Back-patch to v15 where the
    faulty test came in.
    
    Discussion: https://postgr.es/m/b00c8ee096ee46cd25c183125562a1a7@postgrespro.ru

--- src/test/regress/sql/gist.sql
+++ src/test/regress/sql/gist.sql
@@ -41,7 +41,7 @@ reindex index gist_pointidx;
 -- Test Index-only plans on GiST indexes
 --
 
-create unlogged table gist_tbl (b box, p point, c circle);
+create table gist_tbl (b box, p point, c circle);
 
 insert into gist_tbl
 select box(point(0.05*i, 0.05*i), point(0.05*i, 0.05*i)),
@@ -178,4 +178,11 @@ reset enable_seqscan;
 reset enable_bitmapscan;
 reset enable_indexonlyscan;
 
+drop table gist_tbl;
+
+-- test an unlogged table, mostly to get coverage of gistbuildempty
+create unlogged table gist_tbl (b box);
+create index gist_tbl_box_index on gist_tbl using gist (b);
+insert into gist_tbl
+  select box(point(0.05*i, 0.05*i)) from generate_series(0,10) as i;
 drop table gist_tbl;
