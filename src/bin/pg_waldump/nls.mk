dda101315ad93e9b39c48eeb53b131d171d1fd0b
Alvaro Herrera <alvherre@alvh.no-ip.org>
2022-09-25 17:48:03 +0200

    Add missing source files to pg_waldump/nls.mk

--- src/bin/pg_waldump/nls.mk
+++ src/bin/pg_waldump/nls.mk
@@ -1,5 +1,6 @@
 # src/bin/pg_waldump/nls.mk
 CATALOG_NAME     = pg_waldump
-GETTEXT_FILES    = $(FRONTEND_COMMON_GETTEXT_FILES) pg_waldump.c
-GETTEXT_TRIGGERS = $(FRONTEND_COMMON_GETTEXT_TRIGGERS)
-GETTEXT_FLAGS    = $(FRONTEND_COMMON_GETTEXT_FLAGS)
+GETTEXT_FILES    = $(FRONTEND_COMMON_GETTEXT_FILES) pg_waldump.c xlogreader.c xlogstats.c
+GETTEXT_TRIGGERS = $(FRONTEND_COMMON_GETTEXT_TRIGGERS) report_invalid_record:2
+GETTEXT_FLAGS    = $(FRONTEND_COMMON_GETTEXT_FLAGS) \
+    report_invalid_record:2:c-format
