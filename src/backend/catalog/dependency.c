84f8e407f5d9d9c1262931ebe6a83bc90a2f635b
Alvaro Herrera <alvherre@alvh.no-ip.org>
2022-09-28 17:14:53 +0200

    Change some errdetail() to errdetail_internal()
    This prevents marking the argument string for translation for gettext,
    and it also prevents the given string (which is already translated) from
    being translated at runtime.
    
    Also, mark the strings used as arguments to check_rolespec_name for
    translation.
    
    Backpatch all the way back as appropriate.  None of this is caught by
    any tests (necessarily so), so I verified it manually.

--- src/backend/catalog/dependency.c
+++ src/backend/catalog/dependency.c
@@ -1198,14 +1198,14 @@ reportDependentObjects(const ObjectAddresses *targetObjects,
 					(errcode(ERRCODE_DEPENDENT_OBJECTS_STILL_EXIST),
 					 errmsg("cannot drop %s because other objects depend on it",
 							getObjectDescription(origObject)),
-					 errdetail("%s", clientdetail.data),
+					 errdetail_internal("%s", clientdetail.data),
 					 errdetail_log("%s", logdetail.data),
 					 errhint("Use DROP ... CASCADE to drop the dependent objects too.")));
 		else
 			ereport(ERROR,
 					(errcode(ERRCODE_DEPENDENT_OBJECTS_STILL_EXIST),
 					 errmsg("cannot drop desired object(s) because other objects depend on them"),
-					 errdetail("%s", clientdetail.data),
+					 errdetail_internal("%s", clientdetail.data),
 					 errdetail_log("%s", logdetail.data),
 					 errhint("Use DROP ... CASCADE to drop the dependent objects too.")));
 	}
@@ -1217,7 +1217,7 @@ reportDependentObjects(const ObjectAddresses *targetObjects,
 							   "drop cascades to %d other objects",
 							   numReportedClient + numNotReportedClient,
 							   numReportedClient + numNotReportedClient),
-				 errdetail("%s", clientdetail.data),
+				 errdetail_internal("%s", clientdetail.data),
 				 errdetail_log("%s", logdetail.data)));
 	}
 	else if (numReportedClient == 1)
