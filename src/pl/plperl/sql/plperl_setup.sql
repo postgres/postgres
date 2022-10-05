aa2ec0403d0af0082a8f8a68a5a231c4e62a9c3a
Andres Freund <andres@anarazel.de>
2022-10-05 10:43:13 -0700

    tests: Rename conflicting role names
    These cause problems when running installcheck-world USE_MODULE_DB=1 with -j.
    
    Note: This patch has been originally applied to v16 and newer branches
    as of 6a20b04f0408.  Sami Imseih has reported this issue as being
    possible to reach for other branches still supported: REL_15_STABLE and
    REL_14_STABLE.
    
    Reviewed-by: Michael Paquier <michael@paquier.xyz>
    Discussion: https://postgr.es/m/20221003234111.4ob7yph6r4g4ywhu@awork3.anarazel.de
    Discussion: https://postgr.es/m/CAA5RZ0soPHUvwXLZ+mf5Q=uJRL5ggnRg1uB0qKVG1bbdA9up6w@mail.gmail.com
    Backpatch-through: 14

--- src/pl/plperl/sql/plperl_setup.sql
+++ src/pl/plperl/sql/plperl_setup.sql
@@ -6,10 +6,10 @@
 -- a non-superuser is allowed to install plperl (but not plperlu) when
 -- suitable permissions have been granted.
 
-CREATE USER regress_user1;
-CREATE USER regress_user2;
+CREATE USER regress_plperl_user1;
+CREATE USER regress_plperl_user2;
 
-SET ROLE regress_user1;
+SET ROLE regress_plperl_user1;
 
 CREATE EXTENSION plperl;  -- fail
 CREATE EXTENSION plperlu;  -- fail
@@ -18,18 +18,18 @@ RESET ROLE;
 
 DO $$
 begin
-  execute format('grant create on database %I to regress_user1',
+  execute format('grant create on database %I to regress_plperl_user1',
                  current_database());
 end;
 $$;
 
-SET ROLE regress_user1;
+SET ROLE regress_plperl_user1;
 
 CREATE EXTENSION plperl;
 CREATE EXTENSION plperlu;  -- fail
 CREATE SCHEMA plperl_setup_scratch;
 SET search_path = plperl_setup_scratch;
-GRANT ALL ON SCHEMA plperl_setup_scratch TO regress_user2;
+GRANT ALL ON SCHEMA plperl_setup_scratch TO regress_plperl_user2;
 
 CREATE FUNCTION foo1() returns int language plperl as '1;';
 SELECT foo1();
@@ -38,25 +38,25 @@ SELECT foo1();
 \c -
 SET search_path = plperl_setup_scratch;
 
-SET ROLE regress_user1;
+SET ROLE regress_plperl_user1;
 
 -- Should be able to change privileges on the language
 revoke all on language plperl from public;
 
-SET ROLE regress_user2;
+SET ROLE regress_plperl_user2;
 
 CREATE FUNCTION foo2() returns int language plperl as '2;';  -- fail
 
-SET ROLE regress_user1;
+SET ROLE regress_plperl_user1;
 
-grant usage on language plperl to regress_user2;
+grant usage on language plperl to regress_plperl_user2;
 
-SET ROLE regress_user2;
+SET ROLE regress_plperl_user2;
 
 CREATE FUNCTION foo2() returns int language plperl as '2;';
 SELECT foo2();
 
-SET ROLE regress_user1;
+SET ROLE regress_plperl_user1;
 
 -- Should be able to drop the extension, but not the language per se
 DROP LANGUAGE plperl CASCADE;
@@ -64,9 +64,9 @@ DROP EXTENSION plperl CASCADE;
 
 -- Clean up
 RESET ROLE;
-DROP OWNED BY regress_user1;
-DROP USER regress_user1;
-DROP USER regress_user2;
+DROP OWNED BY regress_plperl_user1;
+DROP USER regress_plperl_user1;
+DROP USER regress_plperl_user2;
 
 -- Now install the versions that will be used by subsequent test scripts.
 CREATE EXTENSION plperl;
