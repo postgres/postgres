5c51afe23d6580955b0c386ad0604e8d6ad6a49b
Amit Kapila <akapila@postgresql.org>
2022-10-21 12:08:14 +0530

    Add CHECK_FOR_INTERRUPTS while restoring changes during decoding.
    Previously in commit 42681dffaf, we added CFI during decoding changes but
    missed another similar case that can happen while restoring changes
    spilled to disk back into memory in a loop.
    
    Reported-by: Robert Haas
    Author: Amit Kapila
    Backpatch-through: 10
    Discussion: https://postgr.es/m/CA+TgmoaLObg0QbstbC8ykDwOdD1bDkr4AbPpB=0DPgA2JW0mFg@mail.gmail.com

--- src/backend/replication/logical/reorderbuffer.c
+++ src/backend/replication/logical/reorderbuffer.c
@@ -2586,6 +2586,8 @@ ReorderBufferRestoreChanges(ReorderBuffer *rb, ReorderBufferTXN *txn,
 		int			readBytes;
 		ReorderBufferDiskChange *ondisk;
 
+		CHECK_FOR_INTERRUPTS();
+
 		if (*fd == -1)
 		{
 			char		path[MAXPGPATH];
