38dbaaf273877355c2deeafa4b479d315a78196b
Amit Kapila <akapila@postgresql.org>
2022-10-21 09:42:24 +0530

    Fix executing invalidation messages generated by subtransactions during decoding.
    This problem has been introduced by commit 272248a0c1 where we started
    assigning the subtransactions to the top-level transaction when we mark
    both the top-level transaction and its subtransactions as containing
    catalog changes. After we assign subtransactions to the top-level
    transaction, we were not allowed to execute any invalidations associated
    with it when we decide to skip the transaction.
    
    The reason to assign the subtransactions to the top-level transaction was
    to avoid the assertion failure in AssertTXNLsnOrder() as they have the
    same LSN when we sometimes start accumulating transaction changes for
    partial transactions after the restart. Now that with commit 64ff0fe4e8,
    we skip this assertion check until we reach the LSN at which we start
    decoding the contents of the transaction, so, there is no reason for such
    an assignment anymore.
    
    The assignment change was introduced in 15 and prior versions but this bug
    doesn't exist in branches prior to 14 since we don't add invalidation
    messages to subtransactions. We decided to backpatch through 11 for
    consistency but not for 10 since its final release is near.
    
    Reported-by: Kuroda Hayato
    Author: Masahiko Sawada
    Reviewed-by: Amit Kapila
    Backpatch-through: 11
    Discussion: https://postgr.es/m/TYAPR01MB58660803BCAA7849C8584AA4F57E9%40TYAPR01MB5866.jpnprd01.prod.outlook.com
    Discussion: https://postgr.es/m/a89b46b6-0239-2fd5-71a9-b19b1f7a7145%40enterprisedb.com

--- src/backend/replication/logical/snapbuild.c
+++ src/backend/replication/logical/snapbuild.c
@@ -2146,9 +2146,6 @@ SnapBuildXidSetCatalogChanges(SnapBuild *builder, TransactionId xid, int subxcnt
 				sizeof(TransactionId), xidComparator) != NULL)
 	{
 		for (int i = 0; i < subxcnt; i++)
-		{
-			ReorderBufferAssignChild(builder->reorder, xid, subxacts[i], lsn);
 			ReorderBufferXidSetCatalogChanges(builder->reorder, subxacts[i], lsn);
-		}
 	}
 }
