e5f94d42eb38f39af81f934d70c3969913f2862e
Noah Misch <noah@leadboat.com>
2022-11-12 11:19:50 -0800

    If wait_for_catchup fails under has_wal_read_bug, skip balance of test.
    Test files should now ignore has_wal_read_bug() so long as
    wait_for_catchup() is their only known way of reaching the bug.  That's
    at least five files today, a number expected to grow over time.  This
    commit removes skip logic from three.  By doing so, systems having the
    bug regain the ability to catch other kinds of defects via those three
    tests.  The other two, 002_databases.pl and 031_recovery_conflict.pl,
    have been unprotected.  Back-patch to v15, where done_testing() first
    became our standard.
    
    Discussion: https://postgr.es/m/20221030031639.GA3082137@rfd.leadboat.com

--- src/test/recovery/t/027_stream_regress.pl
+++ src/test/recovery/t/027_stream_regress.pl
@@ -6,13 +6,6 @@ use PostgreSQL::Test::Utils;
 use Test::More;
 use File::Basename;
 
-if (PostgreSQL::Test::Utils::has_wal_read_bug)
-{
-	# We'd prefer to use Test::More->builder->todo_start, but the bug causes
-	# this test file to die(), not merely to fail.
-	plan skip_all => 'filesystem bug';
-}
-
 # Initialize primary node
 my $node_primary = PostgreSQL::Test::Cluster->new('primary');
 $node_primary->init(allows_streaming => 1);
