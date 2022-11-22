a1efcda7c33d10658dac214514ca8359a1da4e42
Amit Kapila <akapila@postgresql.org>
2022-11-22 15:22:00 +0530

    Improve comments atop pg_get_replication_slots.
    Update comments atop pg_get_replication_slots to make it clear that it
    shows all replication slots that currently exist on the database cluster.
    
    Author: sirisha chamarthi
    Discussion: https://postgr.es/m/CAKrAKeXRuFpeiWS+STGFm-RFfW19sUDxju66JkyRi13kdQf94Q@mail.gmail.com

--- src/backend/replication/slotfuncs.c
+++ src/backend/replication/slotfuncs.c
@@ -226,7 +226,8 @@ pg_drop_replication_slot(PG_FUNCTION_ARGS)
 }
 
 /*
- * pg_get_replication_slots - SQL SRF showing active replication slots.
+ * pg_get_replication_slots - SQL SRF showing all replication slots
+ * that currently exist on the database cluster.
  */
 Datum
 pg_get_replication_slots(PG_FUNCTION_ARGS)
