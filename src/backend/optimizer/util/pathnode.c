c959f84c2b770bc136ade3cfd11f6063a51d2708
Tom Lane <tgl@sss.pgh.pa.us>
2022-12-05 12:36:41 -0500

    Fix Memoize to work with partitionwise joining.
    A couple of places weren't up to speed for this.  By sheer good
    luck, we didn't fail but just selected a non-memoized join plan,
    at least in the test case we have.  Nonetheless, it's a bug,
    and I'm not quite sure that it couldn't have worse consequences
    in other examples.  So back-patch to v14 where Memoize came in.
    
    Richard Guo
    
    Discussion: https://postgr.es/m/CAMbWs48GkNom272sfp0-WeD6_0HSR19BJ4H1c9ZKSfbVnJsvRg@mail.gmail.com

--- src/backend/optimizer/util/pathnode.c
+++ src/backend/optimizer/util/pathnode.c
@@ -4193,6 +4193,7 @@ do { \
 
 				FLAT_COPY_PATH(mpath, path, MemoizePath);
 				REPARAMETERIZE_CHILD_PATH(mpath->subpath);
+				ADJUST_CHILD_ATTRS(mpath->param_exprs);
 				new_path = (Path *) mpath;
 			}
 			break;
