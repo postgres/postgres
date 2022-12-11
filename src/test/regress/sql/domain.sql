b8c0ffbd2c8038fbebe76b48616860a7efec66ae
Tom Lane <tgl@sss.pgh.pa.us>
2022-12-11 12:56:54 -0500

    Convert domain_in to report errors softly.
    This is straightforward as far as it goes.  However, it does not
    attempt to trap errors occurring during the execution of domain
    CHECK constraints.  Since those are general user-defined
    expressions, the only way to do that would involve starting up a
    subtransaction for each check.  Of course the entire point of
    the soft-errors feature is to not need subtransactions, so that
    would be self-defeating.  For now, we'll rely on the assumption
    that domain checks are written to avoid throwing errors.
    
    Discussion: https://postgr.es/m/1181028.1670635727@sss.pgh.pa.us

--- src/test/regress/sql/domain.sql
+++ src/test/regress/sql/domain.sql
@@ -69,6 +69,25 @@ drop domain domainint4 restrict;
 drop domain domaintext;
 
 
+-- Test non-error-throwing input
+
+create domain positiveint int4 check(value > 0);
+create domain weirdfloat float8 check((1 / value) < 10);
+
+select pg_input_is_valid('1', 'positiveint');
+select pg_input_is_valid('junk', 'positiveint');
+select pg_input_is_valid('-1', 'positiveint');
+select pg_input_error_message('junk', 'positiveint');
+select pg_input_error_message('-1', 'positiveint');
+select pg_input_error_message('junk', 'weirdfloat');
+select pg_input_error_message('0.01', 'weirdfloat');
+-- We currently can't trap errors raised in the CHECK expression itself
+select pg_input_error_message('0', 'weirdfloat');
+
+drop domain positiveint;
+drop domain weirdfloat;
+
+
 -- Test domains over array types
 
 create domain domainint4arr int4[1];
