942cc240f95998793e10156952c14961b5559b2a
Tom Lane <tgl@sss.pgh.pa.us>
2022-12-13 14:23:59 -0500

    Rethink handling of [Prevent|Is]InTransactionBlock in pipeline mode.
    Commits f92944137 et al. made IsInTransactionBlock() set the
    XACT_FLAGS_NEEDIMMEDIATECOMMIT flag before returning "false",
    on the grounds that that kept its API promises equivalent to those of
    PreventInTransactionBlock().  This turns out to be a bad idea though,
    because it allows an ANALYZE in a pipelined series of commands to
    cause an immediate commit, which is unexpected.
    
    Furthermore, if we return "false" then we have another issue,
    which is that ANALYZE will decide it's allowed to do internal
    commit-and-start-transaction sequences, thus possibly unexpectedly
    committing the effects of previous commands in the pipeline.
    
    To fix the latter situation, invent another transaction state flag
    XACT_FLAGS_PIPELINING, which explicitly records the fact that we
    have executed some extended-protocol command and not yet seen a
    commit for it.  Then, require that flag to not be set before allowing
    InTransactionBlock() to return "false".
    
    Having done that, we can remove its setting of NEEDIMMEDIATECOMMIT
    without fear of causing problems.  This means that the API guarantees
    of IsInTransactionBlock now diverge from PreventInTransactionBlock,
    which is mildly annoying, but it seems OK given the very limited usage
    of IsInTransactionBlock.  (In any case, a caller preferring the old
    behavior could always set NEEDIMMEDIATECOMMIT for itself.)
    
    For consistency also require XACT_FLAGS_PIPELINING to not be set
    in PreventInTransactionBlock.  This too is meant to prevent commands
    such as CREATE DATABASE from silently committing previous commands
    in a pipeline.
    
    Per report from Peter Eisentraut.  As before, back-patch to all
    supported branches (which sadly no longer includes v10).
    
    Discussion: https://postgr.es/m/65a899dd-aebc-f667-1d0a-abb89ff3abf8@enterprisedb.com

--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -2225,6 +2225,12 @@ exec_execute_message(const char *portal_name, long max_rows)
 			CommandCounterIncrement();
 
 			/*
+			 * Set XACT_FLAGS_PIPELINING whenever we complete an Execute
+			 * message without immediately committing the transaction.
+			 */
+			MyXactFlags |= XACT_FLAGS_PIPELINING;
+
+			/*
 			 * Disable statement timeout whenever we complete an Execute
 			 * message.  The next protocol message will start a fresh timeout.
 			 */
@@ -2239,6 +2245,12 @@ exec_execute_message(const char *portal_name, long max_rows)
 		/* Portal run not complete, so send PortalSuspended */
 		if (whereToSendOutput == DestRemote)
 			pq_putemptymessage('s');
+
+		/*
+		 * Set XACT_FLAGS_PIPELINING whenever we suspend an Execute message,
+		 * too.
+		 */
+		MyXactFlags |= XACT_FLAGS_PIPELINING;
 	}
 
 	/*
