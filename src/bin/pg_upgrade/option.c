746915c6866953506379e996ce5198bc9e9e5b94
Peter Eisentraut <peter@eisentraut.org>
2022-12-16 17:49:59 +0100

    pg_upgrade: Add --copy option
    This option selects the default transfer mode.  Having an explicit
    option is handy to make scripts and tests more explicit.  It also
    makes it easier to talk about a "copy" mode rather than "the default
    mode" or something like that, since until now the default mode didn't
    have an externally visible name.
    
    Reviewed-by: Daniel Gustafsson <daniel@yesql.se>
    Reviewed-by: Kyotaro Horiguchi <horikyota.ntt@gmail.com>
    Discussion: https://www.postgresql.org/message-id/flat/50a97009-8ff9-ca4d-a0f6-6086a6775a5b%40enterprisedb.com

--- src/bin/pg_upgrade/option.c
+++ src/bin/pg_upgrade/option.c
@@ -56,6 +56,7 @@ parseCommandLine(int argc, char *argv[])
 		{"socketdir", required_argument, NULL, 's'},
 		{"verbose", no_argument, NULL, 'v'},
 		{"clone", no_argument, NULL, 1},
+		{"copy", no_argument, NULL, 2},
 
 		{NULL, 0, NULL, 0}
 	};
@@ -194,6 +195,10 @@ parseCommandLine(int argc, char *argv[])
 				user_opts.transfer_mode = TRANSFER_MODE_CLONE;
 				break;
 
+			case 2:
+				user_opts.transfer_mode = TRANSFER_MODE_COPY;
+				break;
+
 			default:
 				fprintf(stderr, _("Try \"%s --help\" for more information.\n"),
 						os_info.progname);
@@ -283,6 +288,7 @@ usage(void)
 	printf(_("  -v, --verbose                 enable verbose internal logging\n"));
 	printf(_("  -V, --version                 display version information, then exit\n"));
 	printf(_("  --clone                       clone instead of copying files to new cluster\n"));
+	printf(_("  --copy                        copy files to new cluster (default)\n"));
 	printf(_("  -?, --help                    show this help, then exit\n"));
 	printf(_("\n"
 			 "Before running pg_upgrade you must:\n"
