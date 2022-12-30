883dc0214a64c0923455bf30bfcc5f953e70af39
Tomas Vondra <tomas.vondra@postgresql.org>
2022-12-30 19:44:48 +0100

    Fix assert in BRIN build_distances
    When brin_minmax_multi_union merges summaries, we may end up with just a
    single range after merge_overlapping_ranges. The summaries may contain
    just one range each, and they may overlap (or be exactly the same).
    
    With a single range there's no distance to calculate, but we happen to
    call build_distances anyway - which is fine, we don't calculate the
    distance in this case, except that with asserts this failed due to a
    check there are at least two ranges.
    
    The assert is unnecessarily strict, so relax it a bit and bail out if
    there's just a single range. The relaxed assert would be enough, but
    this way we don't allocate unnecessary memory for distance.
    
    Backpatch to 14, where minmax-multi opclasses were introduced.
    
    Reported-by: Jaime Casanova
    Backpatch-through: 14
    Discussion: https://postgr.es/m/YzVA55qS0hgz8P3r@ahch-to

--- src/backend/access/brin/brin_minmax_multi.c
+++ src/backend/access/brin/brin_minmax_multi.c
@@ -1335,7 +1335,11 @@ build_distances(FmgrInfo *distanceFn, Oid colloid,
 	int			ndistances;
 	DistanceValue *distances;
 
-	Assert(neranges >= 2);
+	Assert(neranges > 0);
+
+	/* If there's only a single range, there's no distance to calculate. */
+	if (neranges == 1)
+		return NULL;
 
 	ndistances = (neranges - 1);
 	distances = (DistanceValue *) palloc0(sizeof(DistanceValue) * ndistances);
