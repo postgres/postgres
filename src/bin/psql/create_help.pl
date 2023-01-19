efd2474ab5b40778722054659e52254840773f95
Michael Paquier <michael@paquier.xyz>
2023-01-19 10:02:10 +0900

    Fix failure with perlcritic in psql's create_help.pl
    No buildfarm members have reported that yet, but a recently-refreshed
    Debian host did.
    
    Reviewed-by: Andrew Dunstan
    Discussion: https://postgr.es/m/Y8ey5z4Nav62g4/K@paquier.xyz
    Backpatch-through: 11

--- src/bin/psql/create_help.pl
+++ src/bin/psql/create_help.pl
@@ -41,7 +41,7 @@ my $define = $hfilebasename;
 $define =~ tr/a-z/A-Z/;
 $define =~ s/\W/_/g;
 
-opendir(DIR, $docdir)
+opendir(my $dh, $docdir)
   or die "$0: could not open documentation source dir '$docdir': $!\n";
 open(my $hfile_handle, '>', $hfile)
   or die "$0: could not open output file '$hfile': $!\n";
@@ -93,7 +93,7 @@ my $maxlen = 0;
 
 my %entries;
 
-foreach my $file (sort readdir DIR)
+foreach my $file (sort readdir $dh)
 {
 	my ($cmdid, @cmdnames, $cmddesc, $cmdsynopsis);
 	$file =~ /\.sgml$/ or next;
@@ -216,4 +216,4 @@ print $hfile_handle "
 
 close $cfile_handle;
 close $hfile_handle;
-closedir DIR;
+closedir $dh;
