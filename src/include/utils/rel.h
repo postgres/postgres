6d066d56ba126b046944f42350e993e1d5bf92a1
Tom Lane <tgl@sss.pgh.pa.us>
2023-01-20 11:58:12 -0500

    Make our back branches build under -fkeep-inline-functions.
    Add "#ifndef FRONTEND" where necessary to make pg_waldump build
    on compilers that don't elide unused static-inline functions.
    
    This back-patches relevant parts of commit 3e9ca5260, fixing build
    breakage from dc7420c2c and back-patching of f10f0ae42.
    
    Per recently-resurrected buildfarm member castoroides.  We aren't
    expecting castoroides to build anything newer than v11, but we
    might as well clean up the intermediate branches while at it.

--- src/include/utils/rel.h
+++ src/include/utils/rel.h
@@ -472,6 +472,7 @@ typedef struct ViewOptions
 	(RELKIND_HAS_STORAGE((relation)->rd_rel->relkind) && \
 	 ((relation)->rd_rel->relfilenode == InvalidOid))
 
+#ifndef FRONTEND
 /*
  * RelationGetSmgr
  *		Returns smgr file handle for a relation, opening it if needed.
@@ -492,6 +493,7 @@ RelationGetSmgr(Relation rel)
 		smgrsetowner(&(rel->rd_smgr), smgropen(rel->rd_node, rel->rd_backend));
 	return rel->rd_smgr;
 }
+#endif							/* !FRONTEND */
 
 /*
  * RelationOpenSmgr
