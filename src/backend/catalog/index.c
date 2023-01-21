a5f3f2fce1e90d8ca1c3ba632613fa33196e863e
Tom Lane <tgl@sss.pgh.pa.us>
2023-01-21 13:10:30 -0500

    Allow REPLICA IDENTITY to be set on an index that's not (yet) valid.
    The motivation for this change is that when pg_dump dumps a
    partitioned index that's marked REPLICA IDENTITY, it generates a
    command sequence that applies REPLICA IDENTITY before the partitioned
    index has been marked valid, causing restore to fail.  We could
    perhaps change pg_dump to not do it like that, but that would be
    difficult and would not fix existing dump files with the problem.
    There seems to be very little reason for the backend to disallow
    this anyway --- the code ignores indisreplident when the index
    isn't valid --- so instead let's fix it by allowing the case.
    
    Commit 9511fb37a previously expressed a concern that allowing
    indisreplident to be set on invalid indexes might allow us to
    wind up in a situation where a table could have indisreplident
    set on multiple indexes.  I'm not sure I follow that concern
    exactly, but in any case the only way that could happen is because
    relation_mark_replica_identity is too trusting about the existing set
    of markings being valid.  Let's just rip out its early-exit code path
    (which sure looks like premature optimization anyway; what are we
    doing expending code to make redundant ALTER TABLE ... REPLICA
    IDENTITY commands marginally faster and not-redundant ones marginally
    slower?) and fix it to positively guarantee that no more than one
    index is marked indisreplident.
    
    The pg_dump failure can be demonstrated in all supported branches,
    so back-patch all the way.  I chose to back-patch 9511fb37a as well,
    just to keep indisreplident handling the same in all branches.
    
    Per bug #17756 from Sergey Belyashov.
    
    Discussion: https://postgr.es/m/17756-dd50e8e0c8dd4a40@postgresql.org

--- src/backend/catalog/index.c
+++ src/backend/catalog/index.c
@@ -1547,7 +1547,6 @@ index_concurrently_swap(Oid newIndexId, Oid oldIndexId, const char *oldName)
 
 	/* Preserve indisreplident in the new index */
 	newIndexForm->indisreplident = oldIndexForm->indisreplident;
-	oldIndexForm->indisreplident = false;
 
 	/* Preserve indisclustered in the new index */
 	newIndexForm->indisclustered = oldIndexForm->indisclustered;
@@ -1559,6 +1558,7 @@ index_concurrently_swap(Oid newIndexId, Oid oldIndexId, const char *oldName)
 	newIndexForm->indisvalid = true;
 	oldIndexForm->indisvalid = false;
 	oldIndexForm->indisclustered = false;
+	oldIndexForm->indisreplident = false;
 
 	CatalogTupleUpdate(pg_index, &oldIndexTuple->t_self, oldIndexTuple);
 	CatalogTupleUpdate(pg_index, &newIndexTuple->t_self, newIndexTuple);
@@ -3462,10 +3462,12 @@ index_set_state_flags(Oid indexId, IndexStateFlagsAction action)
 			 * CONCURRENTLY that failed partway through.)
 			 *
 			 * Note: the CLUSTER logic assumes that indisclustered cannot be
-			 * set on any invalid index, so clear that flag too.
+			 * set on any invalid index, so clear that flag too.  For
+			 * cleanliness, also clear indisreplident.
 			 */
 			indexForm->indisvalid = false;
 			indexForm->indisclustered = false;
+			indexForm->indisreplident = false;
 			break;
 		case INDEX_DROP_SET_DEAD:
 
@@ -3477,6 +3479,8 @@ index_set_state_flags(Oid indexId, IndexStateFlagsAction action)
 			 * the index at all.
 			 */
 			Assert(!indexForm->indisvalid);
+			Assert(!indexForm->indisclustered);
+			Assert(!indexForm->indisreplident);
 			indexForm->indisready = false;
 			indexForm->indislive = false;
 			break;
