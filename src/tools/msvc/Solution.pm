2eb8e54cc373e4e4f9ecf7360563912f9c40ad76
Michael Paquier <michael@paquier.xyz>
2023-02-15 10:12:36 +0900

    Fix handling of SCRAM-SHA-256's channel binding with RSA-PSS certificates
    OpenSSL 1.1.1 and newer versions have added support for RSA-PSS
    certificates, which requires the use of a specific routine in OpenSSL to
    determine which hash function to use when compiling it when using
    channel binding in SCRAM-SHA-256.  X509_get_signature_nid(), that is the
    original routine the channel binding code has relied on, is not able to
    determine which hash algorithm to use for such certificates.  However,
    X509_get_signature_info(), new to OpenSSL 1.1.1, is able to do it.  This
    commit switches the channel binding logic to rely on
    X509_get_signature_info() over X509_get_signature_nid(), which would be
    the choice when building with 1.1.1 or newer.
    
    The error could have been triggered on the client or the server, hence
    libpq and the backend need to have their related code paths patched.
    Note that attempting to load an RSA-PSS certificate with OpenSSL 1.1.0
    or older leads to a failure due to an unsupported algorithm.
    
    The discovery of relying on X509_get_signature_info() comes from Jacob,
    the tests have been written by Heikki (with few tweaks from me), while I
    have bundled the whole together while adding the bits needed for MSVC
    and meson.
    
    This issue exists since channel binding exists, so backpatch all the way
    down.  Some tests are added in 15~, triggered if compiling with OpenSSL
    1.1.1 or newer, where the certificate and key files can easily be
    generated for RSA-PSS.
    
    Reported-by: Gunnar "Nick" Bluth
    Author: Jacob Champion, Heikki Linnakangas
    Discussion: https://postgr.es/m/17760-b6c61e752ec07060@postgresql.org
    Backpatch-through: 11

--- src/tools/msvc/Solution.pm
+++ src/tools/msvc/Solution.pm
@@ -412,6 +412,7 @@ sub GenerateFiles
 		HAVE_WCSTOMBS_L                          => 1,
 		HAVE_WCTYPE_H                            => 1,
 		HAVE_X509_GET_SIGNATURE_NID              => 1,
+		HAVE_X509_GET_SIGNATURE_INFO             => undef,
 		HAVE_X86_64_POPCNTQ                      => undef,
 		HAVE__BOOL                               => undef,
 		HAVE__BUILTIN_BSWAP16                    => undef,
@@ -522,7 +523,14 @@ sub GenerateFiles
 
 		my ($digit1, $digit2, $digit3) = $self->GetOpenSSLVersion();
 
-		# More symbols are needed with OpenSSL 1.1.0 and above.
+		# Symbols needed with OpenSSL 1.1.1 and above.
+		if (   ($digit1 >= '3' && $digit2 >= '0' && $digit3 >= '0')
+			|| ($digit1 >= '1' && $digit2 >= '1' && $digit3 >= '1'))
+		{
+			$define{HAVE_X509_GET_SIGNATURE_INFO} = 1;
+		}
+
+		# Symbols needed with OpenSSL 1.1.0 and above.
 		if (   ($digit1 >= '3' && $digit2 >= '0' && $digit3 >= '0')
 			|| ($digit1 >= '1' && $digit2 >= '1' && $digit3 >= '0'))
 		{
