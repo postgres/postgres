721626cb57c023a957397a81564439560f0f155f
Tom Lane <tgl@sss.pgh.pa.us>
2023-03-07 18:21:37 -0500

    Fix more bugs caused by adding columns to the end of a view.
    If a view is defined atop another view, and then CREATE OR REPLACE
    VIEW is used to add columns to the lower view, then when the upper
    view's referencing RTE is expanded by ApplyRetrieveRule we will have
    a subquery RTE with fewer eref->colnames than output columns.  This
    confuses various code that assumes those lists are always in sync,
    as they are in plain parser output.
    
    We have seen such problems before (cf commit d5b760ecb), and now
    I think the time has come to do what was speculated about in that
    commit: let's make ApplyRetrieveRule synthesize some column names to
    preserve the invariant that holds in parser output.  Otherwise we'll
    be chasing this class of bugs indefinitely.  Moreover, it appears from
    testing that this actually gives us better results in the test case
    d5b760ecb added, and likely in other corner cases that we lack
    coverage for.
    
    In HEAD, I replaced d5b760ecb's hack to make expandRTE exit early with
    an elog(ERROR) call, since the case is now presumably unreachable.
    But it seems like changing that in back branches would bring more risk
    than benefit, so there I just updated the comment.
    
    Per bug #17811 from Alexander Lakhin.  Back-patch to all supported
    branches.
    
    Discussion: https://postgr.es/m/17811-d31686b78f0dffc9@postgresql.org

--- src/backend/rewrite/rewriteHandler.c
+++ src/backend/rewrite/rewriteHandler.c
@@ -24,6 +24,7 @@
 #include "catalog/dependency.h"
 #include "catalog/pg_type.h"
 #include "commands/trigger.h"
+#include "executor/executor.h"
 #include "foreign/fdwapi.h"
 #include "miscadmin.h"
 #include "nodes/makefuncs.h"
@@ -1641,6 +1642,7 @@ ApplyRetrieveRule(Query *parsetree,
 	RangeTblEntry *rte,
 			   *subrte;
 	RowMarkClause *rc;
+	int			numCols;
 
 	if (list_length(rule->actions) != 1)
 		elog(ERROR, "expected just one rule action");
@@ -1792,6 +1794,20 @@ ApplyRetrieveRule(Query *parsetree,
 	rte->selectedCols = NULL;
 	rte->insertedCols = NULL;
 	rte->updatedCols = NULL;
+
+	/*
+	 * Since we allow CREATE OR REPLACE VIEW to add columns to a view, the
+	 * rule_action might emit more columns than we expected when the current
+	 * query was parsed.  Various places expect rte->eref->colnames to be
+	 * consistent with the non-junk output columns of the subquery, so patch
+	 * things up if necessary by adding some dummy column names.
+	 */
+	numCols = ExecCleanTargetListLength(rule_action->targetList);
+	while (list_length(rte->eref->colnames) < numCols)
+	{
+		rte->eref->colnames = lappend(rte->eref->colnames,
+									  makeString(pstrdup("?column?")));
+	}
 
 	return parsetree;
 }
