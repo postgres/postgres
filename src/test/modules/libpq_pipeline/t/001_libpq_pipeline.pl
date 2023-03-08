87e4f24d82939ef532b68f37fc66e6a48cff2cd9
Alvaro Herrera <alvherre@alvh.no-ip.org>
2023-03-08 18:31:55 +0100

    001_libpq_pipeline.pl: use Test::Differences if available
    When one of these tests fails to match the trace, this better shows what
    the problem is.
    
    Discussion: https://postgr.es/m/20220617183150.ilgokxp22mzywnhh@alvherre.pgsql
    Reviewed-by: Kyotaro Horiguchi <horikyota.ntt@gmail.com>

--- src/test/modules/libpq_pipeline/t/001_libpq_pipeline.pl
+++ src/test/modules/libpq_pipeline/t/001_libpq_pipeline.pl
@@ -8,6 +8,19 @@ use PostgreSQL::Test::Cluster;
 use PostgreSQL::Test::Utils;
 use Test::More;
 
+# Use Test::Differences if installed, and select unified diff output.
+# No decent way to select a context line count with this;
+# we could use a sub ref to allow that.
+BEGIN
+{
+	#<<< protect next line from pgperltidy
+	if (!eval q{ use Test::Differences; unified_diff(); 1 })    ## no critic (ProhibitStringyEval)
+	#>>>
+	{
+		*eq_or_diff = \&is;
+	}
+}
+
 my $node = PostgreSQL::Test::Cluster->new('main');
 $node->init;
 $node->start;
@@ -55,7 +68,7 @@ for my $testname (@tests)
 		$result = slurp_file_eval($traceout);
 		next unless $result ne "";
 
-		is($result, $expected, "$testname trace match");
+		eq_or_diff($result, $expected, "$testname trace match");
 	}
 }
 
