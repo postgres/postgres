8cb94344c3c7130a0cd5e21e83705739f552187e
Daniel Gustafsson <dgustafsson@postgresql.org>
2023-05-16 10:49:48 +0200

    Fix reported runtime for single tests in pg_regress
    Commit 558fff0adf got the order of the parameters to test_status_failed
    mixed up which resulted in the runtime being reported as 0 ms.  Fix by
    changing the order to the correct one.  No backpatching is needed since
    this has not been shipped in a release yet.
    
    Discussion: https://postgr.es/m/0134C9EC-5F6B-4EAC-B2D5-BB4249BEBD4D@yesql.se

--- src/test/regress/pg_regress.c
+++ src/test/regress/pg_regress.c
@@ -1891,14 +1891,14 @@ run_single_test(const char *test, test_start_function startfunc,
 
 	if (exit_status != 0)
 	{
-		test_status_failed(test, false, INSTR_TIME_GET_MILLISEC(stoptime));
+		test_status_failed(test, INSTR_TIME_GET_MILLISEC(stoptime), false);
 		log_child_failure(exit_status);
 	}
 	else
 	{
 		if (differ)
 		{
-			test_status_failed(test, false, INSTR_TIME_GET_MILLISEC(stoptime));
+			test_status_failed(test, INSTR_TIME_GET_MILLISEC(stoptime), false);
 		}
 		else
 		{
