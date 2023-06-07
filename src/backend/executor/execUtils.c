ee87f8b63aee39673d014e4ee9a1293b0b45d113
Tomas Vondra <tomas.vondra@postgresql.org>
2023-06-07 16:48:50 +0200

    Use per-tuple context in ExecGetAllUpdatedCols
    Commit fc22b6623b (generated columns) replaced ExecGetUpdatedCols() with
    ExecGetAllUpdatedCols() in a couple places handling UPDATE (triggers and
    lock mode). However, ExecGetUpdatedCols() did exec_rt_fetch() while
    ExecGetAllUpdatedCols() also allocates memory through bms_union()
    without paying attention to the memory context and happened to use the
    long-lived ExecutorState, leaking the memory until the end of the query.
    
    The amount of leaked memory is proportional to the number of (updated)
    attributes, types of UPDATE triggers, and the number of processed rows
    (which for UPDATE ... FROM ... may be much higher than updated rows).
    
    Fixed by switching to the per-tuple context in GetAllUpdatedColumns().
    This is fine for all in-core callers, but external callers may need to
    copy the result. But we're not aware of any such callers.
    
    Note the issue was introduced by fc22b6623b, but the macros were later
    renamed by f50e888990.
    
    Backpatch to 12, where the issue was introduced.
    
    Reported-by: Tomas Vondra
    Reviewed-by: Andres Freund, Tom Lane, Jakub Wartak
    Backpatch-through: 12
    Discussion: https://postgr.es/m/222a3442-7f7d-246c-ed9b-a76209d19239@enterprisedb.com

--- src/backend/executor/execUtils.c
+++ src/backend/executor/execUtils.c
@@ -1348,10 +1348,25 @@ ExecGetExtraUpdatedCols(ResultRelInfo *relinfo, EState *estate)
 	return NULL;
 }
 
-/* Return columns being updated, including generated columns */
+/*
+ * Return columns being updated, including generated columns
+ *
+ * The bitmap is allocated in per-tuple memory context. It's up to the caller to
+ * copy it into a different context with the appropriate lifespan, if needed.
+ */
 Bitmapset *
 ExecGetAllUpdatedCols(ResultRelInfo *relinfo, EState *estate)
 {
-	return bms_union(ExecGetUpdatedCols(relinfo, estate),
-					 ExecGetExtraUpdatedCols(relinfo, estate));
+
+	Bitmapset	   *ret;
+	MemoryContext	oldcxt;
+
+	oldcxt = MemoryContextSwitchTo(GetPerTupleMemoryContext(estate));
+
+	ret = bms_union(ExecGetUpdatedCols(relinfo, estate),
+					ExecGetExtraUpdatedCols(relinfo, estate));
+
+	MemoryContextSwitchTo(oldcxt);
+
+	return ret;
 }
