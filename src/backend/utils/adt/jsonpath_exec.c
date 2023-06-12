bd590d1fea1ba9245c791d589eea94d2dbad5a2b
Tom Lane <tgl@sss.pgh.pa.us>
2023-06-12 10:54:28 -0400

    Accept fractional seconds in jsonpath's datetime() method.
    Commit 927d9abb6 purported to make datetime() accept any string
    that could be output for a datetime value by to_jsonb().  But it
    overlooked the possibility of fractional seconds being present,
    so that cases as simple as to_jsonb(now()) would defeat it.
    
    Fix by adding formats that include ".US" to the list in
    executeDateTimeMethod().  (Note that while this is nominally
    microseconds, it'll do the right thing for fractions with
    fewer than six digits.)
    
    In passing, re-order the list to restore the datatype ordering
    specified in its comment.  The violation accidentally did not
    break anything; but the next edit might be less lucky, so add
    more comments.
    
    Per report from Tim Field.  Back-patch to v13 where datetime()
    was added, like the previous patch.
    
    Discussion: https://postgr.es/m/014A028B-5CE6-4FDF-AC24-426CA6FC9CEE@mohiohio.com

--- src/backend/utils/adt/jsonpath_exec.c
+++ src/backend/utils/adt/jsonpath_exec.c
@@ -1838,20 +1838,29 @@ executeDateTimeMethod(JsonPathExecContext *cxt, JsonPathItem *jsp,
 		 * According to SQL/JSON standard enumerate ISO formats for: date,
 		 * timetz, time, timestamptz, timestamp.
 		 *
-		 * We also support ISO 8601 for timestamps, because to_json[b]()
-		 * functions use this format.
+		 * We also support ISO 8601 format (with "T") for timestamps, because
+		 * to_json[b]() functions use this format.
 		 */
 		static const char *fmt_str[] =
 		{
-			"yyyy-mm-dd",
+			"yyyy-mm-dd",		/* date */
+			"HH24:MI:SS.USTZH:TZM", /* timetz */
+			"HH24:MI:SS.USTZH",
 			"HH24:MI:SSTZH:TZM",
 			"HH24:MI:SSTZH",
+			"HH24:MI:SS.US",	/* time without tz */
 			"HH24:MI:SS",
+			"yyyy-mm-dd HH24:MI:SS.USTZH:TZM",	/* timestamptz */
+			"yyyy-mm-dd HH24:MI:SS.USTZH",
 			"yyyy-mm-dd HH24:MI:SSTZH:TZM",
 			"yyyy-mm-dd HH24:MI:SSTZH",
-			"yyyy-mm-dd HH24:MI:SS",
+			"yyyy-mm-dd\"T\"HH24:MI:SS.USTZH:TZM",
+			"yyyy-mm-dd\"T\"HH24:MI:SS.USTZH",
 			"yyyy-mm-dd\"T\"HH24:MI:SSTZH:TZM",
 			"yyyy-mm-dd\"T\"HH24:MI:SSTZH",
+			"yyyy-mm-dd HH24:MI:SS.US", /* timestamp without tz */
+			"yyyy-mm-dd HH24:MI:SS",
+			"yyyy-mm-dd\"T\"HH24:MI:SS.US",
 			"yyyy-mm-dd\"T\"HH24:MI:SS"
 		};
 
