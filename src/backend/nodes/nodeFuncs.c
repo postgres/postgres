df5dcf41cf01af158d2b833a0ae752e52f5d38e8
Peter Eisentraut <peter@eisentraut.org>
2023-06-29 10:30:55 +0200

    Remove inappropriate raw_expression_tree_walker() code
    It was walking into the ColumnDef->compression field, which is not a
    node but a string.  This code is currently not reachable (because the
    compression field is only set in situations that don't go through
    raw_expression_tree_walker()), but if it had been, this could have
    behaved erratically.

--- src/backend/nodes/nodeFuncs.c
+++ src/backend/nodes/nodeFuncs.c
@@ -4011,8 +4011,6 @@ raw_expression_tree_walker(Node *node,
 
 				if (walker(coldef->typeName, context))
 					return true;
-				if (walker(coldef->compression, context))
-					return true;
 				if (walker(coldef->raw_default, context))
 					return true;
 				if (walker(coldef->collClause, context))
