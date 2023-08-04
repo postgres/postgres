7516056c584e3f7e04269f2e80835d1433e869c4
David Rowley <drowley@postgresql.org>
2023-08-04 13:27:19 +1200

    Attempt to stabilize new window agg regression test
    This test was recently added in 3900a02c9.  It appears to be unstable in
    regards to the join order presumably due to the relations at either side
    of the join being equal in side.  Here we add a qual to make one of them
    smaller so the planner is more likely to choose to hash the smaller of the
    two.
    
    Reported-by: Nathan Bossart, Tom Lane
    Discussion: https://www.postgr.es/m/20230803235403.GC1238296@nathanxps13

--- src/test/regress/sql/window.sql
+++ src/test/regress/sql/window.sql
@@ -1734,6 +1734,7 @@ LIMIT 1;
 EXPLAIN (COSTS OFF)
 SELECT COUNT(*) OVER ()
 FROM tenk1 t1 INNER JOIN tenk1 t2 ON t1.unique1 = t2.tenthous
+WHERE t2.two = 1
 LIMIT 1;
 
 -- Ensure we get a cheap total plan.  This time use UNBOUNDED FOLLOWING, which
