27a36f79b6d15cb2b267d16084007aa9d6917aec
Daniel Gustafsson <dgustafsson@postgresql.org>
2023-08-23 10:22:55 +0200

    Fix wording in comment
    The comment for the DSM_OP_CREATE paramater read "the a new handle"
    which is confusing. Fix by rewording to indicate what the parameter
    means for DSM_OP_CREATE.
    
    Reported-by: Junwang Zhao <zhjwpku@gmail.com>
    Discussion: https://postgr.es/m/CAEG8a3J2bc197ym-M_ykOXb9ox2eNn-QNKNeoSAoHYSw2NCOnw@mail.gmail.com

--- src/backend/storage/ipc/dsm_impl.c
+++ src/backend/storage/ipc/dsm_impl.c
@@ -137,7 +137,7 @@ int			min_dynamic_shared_memory;
  * Arguments:
  *	 op: The operation to be performed.
  *	 handle: The handle of an existing object, or for DSM_OP_CREATE, the
- *	   a new handle the caller wants created.
+ *	   identifier for the new handle the caller wants created.
  *	 request_size: For DSM_OP_CREATE, the requested size.  Otherwise, 0.
  *	 impl_private: Private, implementation-specific data.  Will be a pointer
  *	   to NULL for the first operation on a shared memory segment within this
