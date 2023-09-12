7e57208ed51a9f29e7e1a5d103452bc0e54f5a96
Amit Kapila <akapila@postgresql.org>
2023-09-12 09:44:37 +0530

    Fix uninitialized access to InitialRunningXacts during decoding after ERROR.
    The transactions and subtransactions array that was allocated under
    snapshot builder memory context and recorded during decoding was not
    cleared in case of errors. This can result in an assertion failure if we
    attempt to retry logical decoding within the same session. To address this
    issue, we register a callback function under the snapshot builder memory
    context to clear the recorded transactions and subtransactions array along
    with the context.
    
    This problem doesn't exist in PG16 and HEAD as instead of using
    InitialRunningXacts, we added the list of transaction IDs and
    sub-transaction IDs, that have modified catalogs and are running during
    snapshot serialization, to the serialized snapshot (see commit 7f13ac8123).
    
    Author: Hou Zhijie
    Reviewed-by: Amit Kapila
    Backpatch-through: 11
    Discussion: http://postgr.es/m/18055-ab3beed9f4b7b7d6@postgresql.org

--- src/backend/replication/logical/snapbuild.c
+++ src/backend/replication/logical/snapbuild.c
@@ -336,6 +336,17 @@ SnapBuildStartNextPhaseAt(SnapBuild *builder, TransactionId at)
 }
 
 /*
+ * Memory context reset callback for clearing the array of running transactions
+ * and subtransactions.
+ */
+static void
+SnapBuildResetRunningXactsCallback(void *arg)
+{
+	NInitialRunningXacts = 0;
+	InitialRunningXacts = NULL;
+}
+
+/*
  * Allocate a new snapshot builder.
  *
  * xmin_horizon is the xid >= which we can be sure no catalog rows have been
@@ -350,6 +361,7 @@ AllocateSnapshotBuilder(ReorderBuffer *reorder,
 	MemoryContext context;
 	MemoryContext oldcontext;
 	SnapBuild  *builder;
+	MemoryContextCallback *mcallback;
 
 	/* allocate memory in own context, to have better accountability */
 	context = AllocSetContextCreate(CurrentMemoryContext,
@@ -373,6 +385,10 @@ AllocateSnapshotBuilder(ReorderBuffer *reorder,
 	builder->initial_xmin_horizon = xmin_horizon;
 	builder->start_decoding_at = start_lsn;
 	builder->building_full_snapshot = need_full_snapshot;
+
+	mcallback = palloc0(sizeof(MemoryContextCallback));
+	mcallback->func = SnapBuildResetRunningXactsCallback;
+	MemoryContextRegisterResetCallback(CurrentMemoryContext, mcallback);
 
 	MemoryContextSwitchTo(oldcontext);
 
@@ -399,10 +415,6 @@ FreeSnapshotBuilder(SnapBuild *builder)
 
 	/* other resources are deallocated via memory context reset */
 	MemoryContextDelete(context);
-
-	/* InitialRunningXacts is freed along with the context */
-	NInitialRunningXacts = 0;
-	InitialRunningXacts = NULL;
 }
 
 /*
