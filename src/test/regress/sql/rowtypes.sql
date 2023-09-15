53630f12d39b9c5d98abb7bae4818715306422ed
Tom Lane <tgl@sss.pgh.pa.us>
2023-09-15 17:01:26 -0400

    Track nesting depth correctly when drilling down into RECORD Vars.
    expandRecordVariable() failed to adjust the parse nesting structure
    correctly when recursing to inspect an outer-level Var.  This could
    result in assertion failures or core dumps in corner cases.
    
    Likewise, get_name_for_var_field() failed to adjust the deparse
    namespace stack correctly when recursing to inspect an outer-level
    Var.  In this case the likely result was a "bogus varno" error
    while deparsing a view.
    
    Per bug #18077 from Jingzhou Fu.  Back-patch to all supported
    branches.
    
    Richard Guo, with some adjustments by me
    
    Discussion: https://postgr.es/m/18077-b9db97c6e0ab45d8@postgresql.org

--- src/test/regress/sql/rowtypes.sql
+++ src/test/regress/sql/rowtypes.sql
@@ -494,6 +494,31 @@ with r(a,b) as materialized
           (null,row(1,2)), (null,row(null,null)), (null,null) )
 select r, r is null as isnull, r is not null as isnotnull from r;
 
+--
+-- Check parsing of indirect references to composite values (bug #18077)
+--
+explain (verbose, costs off)
+with cte(c) as materialized (select row(1, 2)),
+     cte2(c) as (select * from cte)
+select * from cte2 as t
+where (select * from (select c as c1) s
+       where (select (c1).f1 > 0)) is not null;
+
+with cte(c) as materialized (select row(1, 2)),
+     cte2(c) as (select * from cte)
+select * from cte2 as t
+where (select * from (select c as c1) s
+       where (select (c1).f1 > 0)) is not null;
+
+-- Also check deparsing of such cases
+create view composite_v as
+with cte(c) as materialized (select row(1, 2)),
+     cte2(c) as (select * from cte)
+select 1 as one from cte2 as t
+where (select * from (select c as c1) s
+       where (select (c1).f1 > 0)) is not null;
+select pg_get_viewdef('composite_v', true);
+drop view composite_v;
 
 --
 -- Tests for component access / FieldSelect
