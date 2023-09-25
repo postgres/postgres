99d334a187ae5c283aca1c31b9fad0cea140a4b2
Thomas Munro <tmunro@postgresql.org>
2023-09-26 09:07:26 +1300

    Fix edge-case for xl_tot_len broken by bae868ca.
    bae868ca removed a check that was still needed.  If you had an
    xl_tot_len at the end of a page that was too small for a record header,
    but not big enough to span onto the next page, we'd immediately perform
    the CRC check using a bogus large length.  Because of arbitrary coding
    differences between the CRC implementations on different platforms,
    nothing very bad happened on common modern systems.  On systems using
    the _sb8.c fallback we could segfault.
    
    Restore that check, add a new assertion and supply a test for that case.
    Back-patch to 12, like bae868ca.
    
    Tested-by: Tom Lane <tgl@sss.pgh.pa.us>
    Tested-by: Alexander Lakhin <exclusion@gmail.com>
    Discussion: https://postgr.es/m/CA%2BhUKGLCkTT7zYjzOxuLGahBdQ%3DMcF%3Dz5ZvrjSOnW4EDhVjT-g%40mail.gmail.com

--- src/backend/access/transam/xlogreader.c
+++ src/backend/access/transam/xlogreader.c
@@ -651,6 +651,15 @@ restart:
 	}
 	else
 	{
+		/* There may be no next page if it's too small. */
+		if (total_len < SizeOfXLogRecord)
+		{
+			report_invalid_record(state,
+								  "invalid record length at %X/%X: wanted %u, got %u",
+								  LSN_FORMAT_ARGS(RecPtr),
+								  (uint32) SizeOfXLogRecord, total_len);
+			goto err;
+		}
 		/* We'll validate the header once we have the next page. */
 		gotheader = false;
 	}
@@ -1187,6 +1196,8 @@ static bool
 ValidXLogRecord(XLogReaderState *state, XLogRecord *record, XLogRecPtr recptr)
 {
 	pg_crc32c	crc;
+
+	Assert(record->xl_tot_len >= SizeOfXLogRecord);
 
 	/* Calculate the CRC */
 	INIT_CRC32C(crc);
