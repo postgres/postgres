19fa977311b9da9c6c84f0108600e78213751a38
Tom Lane <tgl@sss.pgh.pa.us>
2023-10-17 13:10:32 -0400

    Dodge a compiler bug affecting timetz_zone/timetz_izone.
    Use a modulo operator instead of implementing the same behavior
    with a loop.  The loop solution is doubtless microscopically
    faster for the typical case of only wrapping into the very next
    day, but maybe not so much for large interval values.  In any
    case, timetz is such a backwater that it's doubtful anybody
    would notice any performance change anyway.
    
    This avoids a compiler bug occurring in AIX's xlc, even in pretty
    late-model revisions.
    
    We did not have test coverage for the case where the initial
    result->time value is negative, so add that.
    
    For the moment, install this only in HEAD.  My plan is to
    back-patch the test case, and then the code change assuming that
    buildfarm testing proves the bug occurs in the back branches.
    (That seems pretty likely, but let's find out for sure.)
    
    Per buildfarm results from commits 97957fdba and 2f0472030.
    Thanks to Michael Paquier for the idea to use a modulo operation
    to replace the faulty loop.
    
    Discussion: https://postgr.es/m/CA+hUKGK=DOC+hE-62FKfZy=Ybt5uLkrg3zCZD-jFykM-iPn8yw@mail.gmail.com

--- src/test/regress/sql/timetz.sql
+++ src/test/regress/sql/timetz.sql
@@ -100,4 +100,9 @@ CREATE VIEW timetz_local_view AS
   ORDER BY f1;
 SELECT pg_get_viewdef('timetz_local_view', true);
 TABLE timetz_local_view;
+SELECT f1 AS dat,
+       f1 AT TIME ZONE 'UTC+10' AS dat_at_tz,
+       f1 AT TIME ZONE INTERVAL '-10:00' AS dat_at_int
+  FROM TIMETZ_TBL
+  ORDER BY f1;
 ROLLBACK;
