774185056834eef7702f6afb37731c583fa51a1a
Thomas Munro <tmunro@postgresql.org>
2023-10-18 22:15:54 +1300

    jit: Changes for LLVM 17.
    Changes required by https://llvm.org/docs/NewPassManager.html.
    
    Back-patch to 12, leaving the final release of 11 unchanged, consistent
    with earlier decision not to back-patch LLVM 16 support either.
    
    Author: Dmitry Dolgov <9erthalion6@gmail.com>
    Reviewed-by: Andres Freund <andres@anarazel.de>
    Reviewed-by: Thomas Munro <thomas.munro@gmail.com>
    Discussion: https://postgr.es/m/CA%2BhUKG%2BWXznXCyTgCADd%3DHWkP9Qksa6chd7L%3DGCnZo-MBgg9Lg%40mail.gmail.com

--- src/backend/jit/llvm/llvmjit_wrap.cpp
+++ src/backend/jit/llvm/llvmjit_wrap.cpp
@@ -23,8 +23,14 @@ extern "C"
 
 #include <llvm/IR/Attributes.h>
 #include <llvm/IR/Function.h>
+#if LLVM_VERSION_MAJOR < 17
 #include <llvm/MC/SubtargetFeature.h>
+#endif
+#if LLVM_VERSION_MAJOR > 16
+#include <llvm/TargetParser/Host.h>
+#else
 #include <llvm/Support/Host.h>
+#endif
 
 #include "jit/llvmjit.h"
 
