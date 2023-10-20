1ad3a7bd396a802566239fa37e3435df20603d8b
Tom Lane <tgl@sss.pgh.pa.us>
2023-10-20 13:40:15 -0400

    Dodge a compiler bug affecting timetz_zone/timetz_izone.
    This avoids a compiler bug occurring in AIX's xlc, even in pretty
    late-model revisions.  Buildfarm testing has now confirmed that
    only 64-bit xlc is affected.  Although we are contemplating
    dropping support for xlc in v17, it's still supported in the
    back branches, so we need this fix.
    
    Back-patch of code changes from HEAD commit 19fa97731.
    (The test cases were already back-patched, in 4a427b82c et al.)
    
    Discussion: https://postgr.es/m/CA+hUKGK=DOC+hE-62FKfZy=Ybt5uLkrg3zCZD-jFykM-iPn8yw@mail.gmail.com

--- src/backend/utils/adt/date.c
+++ src/backend/utils/adt/date.c
@@ -3083,10 +3083,11 @@ timetz_zone(PG_FUNCTION_ARGS)
 	result = (TimeTzADT *) palloc(sizeof(TimeTzADT));
 
 	result->time = t->time + (t->zone - tz) * USECS_PER_SEC;
+	/* C99 modulo has the wrong sign convention for negative input */
 	while (result->time < INT64CONST(0))
 		result->time += USECS_PER_DAY;
-	while (result->time >= USECS_PER_DAY)
-		result->time -= USECS_PER_DAY;
+	if (result->time >= USECS_PER_DAY)
+		result->time %= USECS_PER_DAY;
 
 	result->zone = tz;
 
@@ -3116,10 +3117,11 @@ timetz_izone(PG_FUNCTION_ARGS)
 	result = (TimeTzADT *) palloc(sizeof(TimeTzADT));
 
 	result->time = time->time + (time->zone - tz) * USECS_PER_SEC;
+	/* C99 modulo has the wrong sign convention for negative input */
 	while (result->time < INT64CONST(0))
 		result->time += USECS_PER_DAY;
-	while (result->time >= USECS_PER_DAY)
-		result->time -= USECS_PER_DAY;
+	if (result->time >= USECS_PER_DAY)
+		result->time %= USECS_PER_DAY;
 
 	result->zone = tz;
 
