8b62b441fffd26f0bfaa5bed2c11898ed1345f62
Alexander Korotkov <akorotkov@postgresql.org>
2023-10-27 23:50:07 +0300

    Split event_trigger_login test from event_trigger test
    That allows to still run event_trigger test in parallel with oidjoins test,
    and save ~50ms of tests runtime.
    
    Discussion: https://postgr.es/m/202310271047.mnwkql6nhbwi%40alvherre.pgsql
    Author: Alvaro Herrera, Alexander Korotkov

--- src/test/regress/sql/event_trigger.sql
+++ src/test/regress/sql/event_trigger.sql
@@ -495,29 +495,3 @@ DROP POLICY pguc ON event_trigger_test;
 CREATE POLICY pguc ON event_trigger_test USING (FALSE);
 SET event_triggers = 'off';
 DROP POLICY pguc ON event_trigger_test;
-
--- Login event triggers
-CREATE TABLE user_logins(id serial, who text);
-GRANT SELECT ON user_logins TO public;
-CREATE FUNCTION on_login_proc() RETURNS event_trigger AS $$
-BEGIN
-  INSERT INTO user_logins (who) VALUES (SESSION_USER);
-  RAISE NOTICE 'You are welcome!';
-END;
-$$ LANGUAGE plpgsql;
-CREATE EVENT TRIGGER on_login_trigger ON login EXECUTE PROCEDURE on_login_proc();
-ALTER EVENT TRIGGER on_login_trigger ENABLE ALWAYS;
-\c
-SELECT COUNT(*) FROM user_logins;
-\c
-SELECT COUNT(*) FROM user_logins;
-
--- Check dathasloginevt in system catalog
-SELECT dathasloginevt FROM pg_database WHERE datname= :'DBNAME';
-
--- Cleanup
-DROP TABLE user_logins;
-DROP EVENT TRIGGER on_login_trigger;
-DROP FUNCTION on_login_proc();
-\c
-SELECT dathasloginevt FROM pg_database WHERE datname= :'DBNAME';
