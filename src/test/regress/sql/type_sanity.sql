4b9fbd6be442cf36910fb34308e0edfd43f2dbdf
Tom Lane <tgl@sss.pgh.pa.us>
2023-10-27 21:50:44 -0400

    Allow relkind 'I' in type_sanity test.
    This is cosmetic at present, since no partitioned indexes have
    been created yet at the stage of the regression tests when
    type_sanity runs.  But it's confusing that this list of allowed
    values doesn't match reality, so fix it.
    
    Noted by Jian He.
    
    Discussion: https://postgr.es/m/CACJufxHhr58q-YoBeqnY5P-u8Xe2X5sMtsr3LYE1nQDwdqjB4w@mail.gmail.com

--- src/test/regress/sql/type_sanity.sql
+++ src/test/regress/sql/type_sanity.sql
@@ -360,7 +360,7 @@ ORDER BY 1;
 
 SELECT c1.oid, c1.relname
 FROM pg_class as c1
-WHERE relkind NOT IN ('r', 'i', 'S', 't', 'v', 'm', 'c', 'f', 'p') OR
+WHERE relkind NOT IN ('r', 'i', 'S', 't', 'v', 'm', 'c', 'f', 'p', 'I') OR
     relpersistence NOT IN ('p', 'u', 't') OR
     relreplident NOT IN ('d', 'n', 'f', 'i');
 
