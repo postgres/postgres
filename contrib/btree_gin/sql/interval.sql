35e6a5c20d453921efa24342effb648cad3573ec
Dean Rasheed <dean.a.rasheed@gmail.com>
2023-10-29 11:14:34 +0000

    btree_gin: Fix calculation of leftmost interval value.
    Formerly, the value computed by leftmostvalue_interval() was a long
    way short of the minimum possible interval value.  As a result, an
    index scan on a GIN index on an interval column with < or <= operators
    would miss large negative interval values.
    
    Fix by setting all fields of the leftmost interval to their minimum
    values, ensuring that the result is less than any other possible
    interval.  Since this only affects index searches, no index rebuild is
    necessary.
    
    Back-patch to all supported branches.
    
    Dean Rasheed, reviewed by Heikki Linnakangas.
    
    Discussion: https://postgr.es/m/CAEZATCV80%2BgOfF8ehNUUfaKBZgZMDfCfL-g1HhWGb6kC3rpDfw%40mail.gmail.com

--- contrib/btree_gin/sql/interval.sql
+++ contrib/btree_gin/sql/interval.sql
@@ -5,12 +5,14 @@ CREATE TABLE test_interval (
 );
 
 INSERT INTO test_interval VALUES
+	( '-178000000 years' ),
 	( '03:55:08' ),
 	( '04:55:08' ),
 	( '05:55:08' ),
 	( '08:55:08' ),
 	( '09:55:08' ),
-	( '10:55:08' )
+	( '10:55:08' ),
+	( '178000000 years' )
 ;
 
 CREATE INDEX idx_interval ON test_interval USING gin (i);
