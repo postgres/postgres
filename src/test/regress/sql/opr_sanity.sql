2f30226624b0645913ea832c15b10b0de43bc7c4
Michael Paquier <michael@paquier.xyz>
2023-11-22 09:32:06 +0900

    Fix query checking consistency of table amhandlers in opr_sanity.sql
    As written, the query checked for an access method of type 's', which is
    not an AM type supported in the core code.
    
    Error introduced by 8586bf7ed888.  As this query is not checking what it
    should, backpatch all the way down.
    
    Reviewed-by: Aleksander Alekseev
    Discussion: https://postgr.es/m/ZVxJkAJrKbfHETiy@paquier.xyz
    Backpatch-through: 12

--- src/test/regress/sql/opr_sanity.sql
+++ src/test/regress/sql/opr_sanity.sql
@@ -1223,7 +1223,7 @@ WHERE p1.oid = a1.amhandler AND a1.amtype = 'i' AND
 
 SELECT a1.oid, a1.amname, p1.oid, p1.proname
 FROM pg_am AS a1, pg_proc AS p1
-WHERE p1.oid = a1.amhandler AND a1.amtype = 's' AND
+WHERE p1.oid = a1.amhandler AND a1.amtype = 't' AND
     (p1.prorettype != 'table_am_handler'::regtype
      OR p1.proretset
      OR p1.pronargs != 1
