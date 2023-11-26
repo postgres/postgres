bc3c8db8ae2f899cfd65edce99fe9cf65caf1ed7
Alexander Korotkov <akorotkov@postgresql.org>
2023-11-27 01:30:39 +0200

    Display length and bounds histograms in pg_stats
    Values corresponding to STATISTIC_KIND_RANGE_LENGTH_HISTOGRAM and
    STATISTIC_KIND_BOUNDS_HISTOGRAM were not exposed to pg_stats when these
    slot kinds were introduced in 918eee0c49.
    
    This commit adds the missing fields to pg_stats.
    
    Catversion is bumped.
    
    Discussion: https://postgr.es/m/flat/b67d8b57-9357-7e82-a2e7-f6ce6eaeec67@postgrespro.ru
    Author: Egor Rogov, Soumyadeep Chakraborty
    Reviewed-by: Tomas Vondra, Justin Pryzby, Jian He

--- src/backend/catalog/system_views.sql
+++ src/backend/catalog/system_views.sql
@@ -243,7 +243,28 @@ CREATE VIEW pg_stats WITH (security_barrier) AS
             WHEN stakind3 = 5 THEN stanumbers3
             WHEN stakind4 = 5 THEN stanumbers4
             WHEN stakind5 = 5 THEN stanumbers5
-        END AS elem_count_histogram
+        END AS elem_count_histogram,
+        CASE
+            WHEN stakind1 = 6 THEN stavalues1
+            WHEN stakind2 = 6 THEN stavalues2
+            WHEN stakind3 = 6 THEN stavalues3
+            WHEN stakind4 = 6 THEN stavalues4
+            WHEN stakind5 = 6 THEN stavalues5
+        END AS range_length_histogram,
+        CASE
+            WHEN stakind1 = 6 THEN stanumbers1[1]
+            WHEN stakind2 = 6 THEN stanumbers2[1]
+            WHEN stakind3 = 6 THEN stanumbers3[1]
+            WHEN stakind4 = 6 THEN stanumbers4[1]
+            WHEN stakind5 = 6 THEN stanumbers5[1]
+        END AS range_empty_frac,
+        CASE
+            WHEN stakind1 = 7 THEN stavalues1
+            WHEN stakind2 = 7 THEN stavalues2
+            WHEN stakind3 = 7 THEN stavalues3
+            WHEN stakind4 = 7 THEN stavalues4
+            WHEN stakind5 = 7 THEN stavalues5
+            END AS range_bounds_histogram
     FROM pg_statistic s JOIN pg_class c ON (c.oid = s.starelid)
          JOIN pg_attribute a ON (c.oid = attrelid AND attnum = s.staattnum)
          LEFT JOIN pg_namespace n ON (n.oid = c.relnamespace)
--- src/include/catalog/catversion.h
+++ src/include/catalog/catversion.h
@@ -57,6 +57,6 @@
  */
 
 /*							yyyymmddN */
-#define CATALOG_VERSION_NO	202311142
+#define CATALOG_VERSION_NO	202311271
 
 #endif
