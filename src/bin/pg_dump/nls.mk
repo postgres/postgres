c3f4a84481b6a8d84b1928165a7bf459e5304ed8
Daniel Gustafsson <dgustafsson@postgresql.org>
2023-11-30 10:56:51 +0100

    Add missing file to nls Makefile
    Author: Kyotaro Horiguchi <horikyota.ntt@gmail.com>
    Discussion: https://postgr.es/m/20231130.120009.1370713511317755874.horikyota.ntt@gmail.com

--- src/bin/pg_dump/nls.mk
+++ src/bin/pg_dump/nls.mk
@@ -20,6 +20,7 @@ GETTEXT_FILES    = $(FRONTEND_COMMON_GETTEXT_FILES) \
                    pg_dumpall.c \
                    parallel.c \
                    parallel.h \
+                   filter.c \
                    pg_backup_utils.c \
                    pg_backup_utils.h \
                    ../../common/compression.c \
