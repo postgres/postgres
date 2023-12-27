3727b8d0e3f1fa7568f6a6f4378ce5a79daeb0d9
Peter Eisentraut <peter@eisentraut.org>
2023-12-27 10:48:01 +0100

    pg_stat_statements: Add test coverage for pg_stat_statements_1_8()
    This requires reading pg_stat_statements at least once while the 1.8
    version of the extension is installed.
    
    Reviewed-by: Michael Paquier <michael@paquier.xyz>
    Discussion: https://www.postgresql.org/message-id/flat/40d1e4f2-835f-448f-a541-8ff5db75bf3d@eisentraut.org

--- contrib/pg_stat_statements/sql/oldextversions.sql
+++ contrib/pg_stat_statements/sql/oldextversions.sql
@@ -33,8 +33,9 @@ SELECT count(*) > 0 AS has_data FROM pg_stat_statements;
 
 -- New functions and views for pg_stat_statements in 1.8
 AlTER EXTENSION pg_stat_statements UPDATE TO '1.8';
-\d pg_stat_statements
 SELECT pg_get_functiondef('pg_stat_statements_reset'::regproc);
+\d pg_stat_statements
+SELECT count(*) > 0 AS has_data FROM pg_stat_statements;
 
 -- New function pg_stat_statement_info, and new function
 -- and view for pg_stat_statements introduced in 1.9
