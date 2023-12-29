8d4e8b746815505c3b808ab96674ec2e140afc56
Tom Lane <tgl@sss.pgh.pa.us>
2023-12-29 10:57:11 -0500

    In pg_dump, don't dump a stats object unless dumping underlying table.
    If the underlying table isn't being dumped, it's useless to dump
    an extended statistics object; it'll just cause errors at restore.
    We have always applied similar policies to, say, indexes.
    
    (When and if we get cross-table stats objects, it might be profitable
    to think a little harder about what to do with them.  But for now
    there seems no point in considering a stats object as anything but
    an appendage of its table.)
    
    Rian McGuire and Tom Lane, per report from Rian McGuire.
    Back-patch to supported branches.
    
    Discussion: https://postgr.es/m/7075d3aa-3f05-44a5-b68f-47dc6a8a0550@buildkite.com

--- src/bin/pg_dump/pg_dump.h
+++ src/bin/pg_dump/pg_dump.h
@@ -387,6 +387,7 @@ typedef struct _statsExtInfo
 {
 	DumpableObject dobj;
 	char	   *rolname;		/* name of owner, or empty string */
+	TableInfo  *stattable;		/* link to table the stats are for */
 	int			stattarget;		/* statistics target */
 } StatsExtInfo;
 
