6d94cc6ca1b775335f58527ece6c12d74ec41bde
Alexander Korotkov <akorotkov@postgresql.org>
2024-01-08 19:43:05 +0200

    Fix indentation in ExecParallelHashIncreaseNumBatches()
    Backpatch-through: 12

--- src/backend/executor/nodeHash.c
+++ src/backend/executor/nodeHash.c
@@ -1168,6 +1168,7 @@ ExecParallelHashIncreaseNumBatches(HashJoinTable hashtable)
 					 * array.
 					 */
 					dtuples = (old_batch0->ntuples * 2.0) / new_nbatch;
+
 					/*
 					 * We need to calculate the maximum number of buckets to
 					 * stay within the MaxAllocSize boundary.  Round the
