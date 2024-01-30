e894d1d829196a482511577de743088e4e0aed76
Andrew Dunstan <andrew@dunslane.net>
2024-01-30 17:09:44 -0500

    Fix 003_extrafiles.pl test for the Windows
    File::Find converts backslashes to slashes in the newer Perl versions.
    See: https://github.com/Perl/perl5/commit/414f14df98cb1c9a20f92c5c54948b67c09f072d
    
    So, do the same conversion for Windows before comparing paths. To
    support all Perl versions, always convert them on Windows regardless of
    the Perl's version.
    
    Author: Nazir Bilal Yavuz <byavuz81@gmail.com>
    
    Backpatch to all live branches

--- src/bin/pg_rewind/t/003_extrafiles.pl
+++ src/bin/pg_rewind/t/003_extrafiles.pl
@@ -67,6 +67,19 @@ sub run_test
 		},
 		$test_master_datadir);
 	@paths = sort @paths;
+
+	# File::Find converts backslashes to slashes in the newer Perl
+	# versions. To support all Perl versions, do the same conversion
+	# for Windows before comparing the paths.
+	if ($PostgreSQL::Test::Utils::windows_os)
+	{
+		for my $filename (@paths)
+		{
+			$filename =~ s{\\}{/}g;
+		}
+		$test_primary_datadir =~ s{\\}{/}g;
+	}
+
 	is_deeply(
 		\@paths,
 		[
