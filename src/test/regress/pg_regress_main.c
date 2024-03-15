196eeb6b2f7cf5f95293259efe0e5d73e8d0025d
Daniel Gustafsson <dgustafsson@postgresql.org>
2024-03-15 17:02:07 +0100

    Fix handling of expecteddir in pg_regress
    Commit c855872074b introduced a new parameter to pg_regress to set
    the directory where to look for expected files, but accidentally
    only implemented it for when compiling pg_regress for ECPG tests.
    Fix by adding support for the parameter to the main regression test
    compilation of pg_regress as well.
    
    Backpatch to v16 where --expecteddir was introduced.
    
    Author: Anthonin Bonnefoy <anthonin.bonnefoy@datadoghq.com>
    Discussion: https://postgr.es/m/CAO6_Xqq5yKJHcJsq__LPcKwSY0XHRqVERNWGxx5ttNXXj7+W=A@mail.gmail.com
    Backpatch-through: 16

--- src/test/regress/pg_regress_main.c
+++ src/test/regress/pg_regress_main.c
@@ -54,7 +54,7 @@ psql_start_test(const char *testname,
 			 outputdir, testname);
 
 	snprintf(expectfile, sizeof(expectfile), "%s/expected/%s.out",
-			 outputdir, testname);
+			 expecteddir, testname);
 	if (!file_exists(expectfile))
 		snprintf(expectfile, sizeof(expectfile), "%s/expected/%s.out",
 				 inputdir, testname);
