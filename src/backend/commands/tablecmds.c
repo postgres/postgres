e6c4e01bf4405ea779ec0b942d7eaa81e18423e1
Alvaro Herrera <alvherre@alvh.no-ip.org>
2024-03-20 15:28:14 +0100

    Review wording on tablespaces w.r.t. partitioned tables
    Remove a redundant comment, and document pg_class.reltablespace properly
    in catalogs.sgml.
    
    After commits a36c84c3e4a9, 87259588d0ab and others.
    
    Backpatch to 12.
    
    Discussion: https://postgr.es/m/202403191013.w2kr7wqlamqz@alvherre.pgsql

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -785,10 +785,6 @@ DefineRelation(CreateStmt *stmt, char relkind, Oid ownerId,
 	}
 	else if (stmt->partbound)
 	{
-		/*
-		 * For partitions, when no other tablespace is specified, we default
-		 * the tablespace to the parent partitioned table's.
-		 */
 		Assert(list_length(inheritOids) == 1);
 		tablespaceId = get_rel_tablespace(linitial_oid(inheritOids));
 	}
