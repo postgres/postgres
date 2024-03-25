bc5fcaa289a92941a1c7b191c7c9311bd742e172
Jeff Davis <jdavis@postgresql.org>
2024-03-25 11:51:44 -0700

    Clarify comment for LogicalTapeSetBlocks().
    Discussion: https://postgr.es/m/1229327.1711160246@sss.pgh.pa.us
    Backpatch-through: 13

--- src/backend/utils/sort/logtape.c
+++ src/backend/utils/sort/logtape.c
@@ -1174,10 +1174,8 @@ LogicalTapeTell(LogicalTape *lt, int64 *blocknum, int *offset)
 }
 
 /*
- * Obtain total disk space currently used by a LogicalTapeSet, in blocks.
- *
- * This should not be called while there are open write buffers; otherwise it
- * may not account for buffered data.
+ * Obtain total disk space currently used by a LogicalTapeSet, in blocks. Does
+ * not account for open write buffer, if any.
  */
 int64
 LogicalTapeSetBlocks(LogicalTapeSet *lts)
