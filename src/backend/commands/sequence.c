3cb2f13ac500983c9c6b1eef3b3c2091c26f3040
Nathan Bossart <nathan@postgresql.org>
2024-05-13 15:53:50 -0500

    Fix pg_sequence_last_value() for unlogged sequences on standbys.
    Presently, when this function is called for an unlogged sequence on
    a standby server, it will error out with a message like
    
    	ERROR:  could not open file "base/5/16388": No such file or directory
    
    Since the pg_sequences system view uses pg_sequence_last_value(),
    it can error similarly.  To fix, modify the function to return NULL
    for unlogged sequences on standby servers.  Since this bug is
    present on all versions since v15, this approach is preferable to
    making the ERROR nicer because we need to repair the pg_sequences
    view without modifying its definition on released versions.  For
    consistency, this commit also modifies the function to return NULL
    for other sessions' temporary sequences.  The pg_sequences view
    already appropriately filters out such sequences, so there's no bug
    there, but we might as well offer some defense in case someone
    invokes this function directly.
    
    Unlogged sequences were first introduced in v15, but temporary
    sequences are much older, so while the fix for unlogged sequences
    is only back-patched to v15, the temporary sequence portion is
    back-patched to all supported versions.
    
    We could also remove the privilege check in the pg_sequences view
    definition in v18 if we modify this function to return NULL for
    sequences for which the current user lacks privileges, but that is
    left as a future exercise for when v18 development begins.
    
    Reviewed-by: Tom Lane, Michael Paquier
    Discussion: https://postgr.es/m/20240501005730.GA594666%40nathanxps13
    Backpatch-through: 12

--- src/backend/commands/sequence.c
+++ src/backend/commands/sequence.c
@@ -1777,11 +1777,8 @@ pg_sequence_last_value(PG_FUNCTION_ARGS)
 	Oid			relid = PG_GETARG_OID(0);
 	SeqTable	elm;
 	Relation	seqrel;
-	Buffer		buf;
-	HeapTupleData seqtuple;
-	Form_pg_sequence_data seq;
-	bool		is_called;
-	int64		result;
+	bool		is_called = false;
+	int64		result = 0;
 
 	/* open and lock sequence */
 	init_sequence(relid, &elm, &seqrel);
@@ -1792,12 +1789,28 @@ pg_sequence_last_value(PG_FUNCTION_ARGS)
 				 errmsg("permission denied for sequence %s",
 						RelationGetRelationName(seqrel))));
 
-	seq = read_seq_tuple(seqrel, &buf, &seqtuple);
+	/*
+	 * We return NULL for other sessions' temporary sequences.  The
+	 * pg_sequences system view already filters those out, but this offers a
+	 * defense against ERRORs in case someone invokes this function directly.
+	 *
+	 * Also, for the benefit of the pg_sequences view, we return NULL for
+	 * unlogged sequences on standbys instead of throwing an error.
+	 */
+	if (!RELATION_IS_OTHER_TEMP(seqrel) &&
+		(RelationIsPermanent(seqrel) || !RecoveryInProgress()))
+	{
+		Buffer		buf;
+		HeapTupleData seqtuple;
+		Form_pg_sequence_data seq;
 
-	is_called = seq->is_called;
-	result = seq->last_value;
+		seq = read_seq_tuple(seqrel, &buf, &seqtuple);
 
-	UnlockReleaseBuffer(buf);
+		is_called = seq->is_called;
+		result = seq->last_value;
+
+		UnlockReleaseBuffer(buf);
+	}
 	sequence_close(seqrel, NoLock);
 
 	if (is_called)
