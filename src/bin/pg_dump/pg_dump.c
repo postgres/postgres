e6fc3b70df906868df36517c2c7c5a38725e1451
Daniel Gustafsson <dgustafsson@postgresql.org>
2024-05-15 22:48:51 +0200

    Fix query result leak during binary upgrade
    9a974cbcba00 moved the query in binary_upgrade_set_pg_class_oids to the
    outer level, but left the PQclear and query buffer destruction in the
    is_index conditional.  353708e1fb2d fixed the leak of the query buffer
    but left the PGresult leak. This moves clearing the result to the outer
    level ensuring that it will be called.
    
    Reviewed-by: Tom Lane <tgl@sss.pgh.pa.us>
    Discussion: https://postgr.es/m/374550C1-F4ED-4D9D-9498-0FD029CCF674@yesql.se
    Backpatch-through: v15

--- src/bin/pg_dump/pg_dump.c
+++ src/bin/pg_dump/pg_dump.c
@@ -4980,8 +4980,6 @@ binary_upgrade_set_pg_class_oids(Archive *fout,
 							  "SELECT pg_catalog.binary_upgrade_set_next_index_relfilenode('%u'::pg_catalog.oid);\n",
 							  toast_index_relfilenode);
 		}
-
-		PQclear(upgrade_res);
 	}
 	else
 	{
@@ -4993,6 +4991,8 @@ binary_upgrade_set_pg_class_oids(Archive *fout,
 						  "SELECT pg_catalog.binary_upgrade_set_next_index_relfilenode('%u'::pg_catalog.oid);\n",
 						  relfilenode);
 	}
+
+	PQclear(upgrade_res);
 
 	appendPQExpBufferChar(upgrade_buffer, '\n');
 
