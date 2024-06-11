1d0399b54068586db003691c218c3d68bee055c8
Tom Lane <tgl@sss.pgh.pa.us>
2024-06-11 17:57:46 -0400

    Fix infer_arbiter_indexes() to not assume resultRelation is 1.
    infer_arbiter_indexes failed to renumber varnos in index expressions
    or predicates that it got from the catalogs.  This escaped detection
    up to now because the stored varnos in such trees will be 1, and an
    INSERT's result relation is usually the first rangetable entry,
    so that that was fine.  However, in cases such as inserting through
    an updatable view, it's not fine, leading to failure to match the
    expressions to the query with ensuing "there is no unique or exclusion
    constraint matching the ON CONFLICT specification" errors.
    
    Fix by copy-and-paste from get_relation_info().
    
    Per bug #18502 from Michael Wang.  Back-patch to all supported
    versions.
    
    Discussion: https://postgr.es/m/18502-545b53f5b81e54e0@postgresql.org

--- src/backend/optimizer/util/plancat.c
+++ src/backend/optimizer/util/plancat.c
@@ -632,6 +632,7 @@ infer_arbiter_indexes(PlannerInfo *root)
 	OnConflictExpr *onconflict = root->parse->onConflict;
 
 	/* Iteration state */
+	Index		varno;
 	RangeTblEntry *rte;
 	Relation	relation;
 	Oid			indexOidFromConstraint = InvalidOid;
@@ -660,7 +661,8 @@ infer_arbiter_indexes(PlannerInfo *root)
 	 * the rewriter or when expand_inherited_rtentry() added it to the query's
 	 * rangetable.
 	 */
-	rte = rt_fetch(root->parse->resultRelation, root->parse->rtable);
+	varno = root->parse->resultRelation;
+	rte = rt_fetch(varno, root->parse->rtable);
 
 	relation = table_open(rte->relid, NoLock);
 
@@ -794,6 +796,9 @@ infer_arbiter_indexes(PlannerInfo *root)
 
 		/* Expression attributes (if any) must match */
 		idxExprs = RelationGetIndexExpressions(idxRel);
+		if (idxExprs && varno != 1)
+			ChangeVarNodes((Node *) idxExprs, 1, varno, 0);
+
 		foreach(el, onconflict->arbiterElems)
 		{
 			InferenceElem *elem = (InferenceElem *) lfirst(el);
@@ -845,6 +850,8 @@ infer_arbiter_indexes(PlannerInfo *root)
 		 * CONFLICT's WHERE clause.
 		 */
 		predExprs = RelationGetIndexPredicate(idxRel);
+		if (predExprs && varno != 1)
+			ChangeVarNodes((Node *) predExprs, 1, varno, 0);
 
 		if (!predicate_implied_by(predExprs, (List *) onconflict->arbiterWhere, false))
 			goto next;
