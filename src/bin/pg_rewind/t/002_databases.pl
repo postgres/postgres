c38518fa978075d08d64a34e1b4504da13a81e02
Andrew Dunstan <andrew@dunslane.net>
2024-06-13 07:38:48 -0400

    Skip some permissions checks on Cygwin
    These are checks that are already skipped on other Windows systems.
    
    Backpatch to all live branches, as appropriate.

--- src/bin/pg_rewind/t/002_databases.pl
+++ src/bin/pg_rewind/t/002_databases.pl
@@ -60,7 +60,7 @@ template1
   SKIP:
 	{
 		skip "unix-style permissions not supported on Windows", 1
-		  if ($windows_os);
+		  if ($windows_os || $Config::Config{osname} eq 'cygwin');
 
 		ok(check_mode_recursive($node_primary->data_dir(), 0750, 0640),
 			'check PGDATA permissions');
