56a8296212b68267dc2bddeb1fb40a893b1aadb3
Tom Lane <tgl@sss.pgh.pa.us>
2024-06-13 20:34:42 -0400

    Fix parsing of ignored operators in websearch_to_tsquery().
    The manual says clearly that punctuation in the input of
    websearch_to_tsquery() is ignored, except for the special cases
    of dashes and quotes.  However, this failed for cases like
    "(foo bar) or something", or in general an ISOPERATOR character
    in front of the "or".  We'd switch back to WAITOPERAND state,
    then ignore the operator character while remaining in that state,
    and then reach the "or" in WAITOPERAND state which (intentionally)
    makes us treat it as data.
    
    The fix is simple enough: if we see an ISOPERATOR character while in
    WAITOPERATOR state, we have to skip it while staying in that state.
    (We don't need to worry about other punctuation characters: those will
    be consumed as though they were words, but then rejected by lexizing.)
    
    In v14 and up (since commit eb086056f) we can simplify the code a bit
    more too, because there is no longer a reason for the WAITOPERAND
    state to distinguish between quoted and unquoted operands.
    
    Per bug #18479 from Manos Emmanouilidis.  Back-patch to all supported
    branches.
    
    Discussion: https://postgr.es/m/18479-d9b46e2fc242c33e@postgresql.org

--- src/backend/utils/adt/tsquery.c
+++ src/backend/utils/adt/tsquery.c
@@ -439,7 +439,7 @@ gettoken_query_websearch(TSQueryParserState state, int8 *operator,
 				}
 				else if (ISOPERATOR(state->buf))
 				{
-					/* or else gettoken_tsvector() will raise an error */
+					/* ignore, else gettoken_tsvector() will raise an error */
 					state->buf++;
 					state->state = WAITOPERAND;
 					continue;
@@ -476,15 +476,9 @@ gettoken_query_websearch(TSQueryParserState state, int8 *operator,
 				break;
 
 			case WAITOPERATOR:
-				if (t_iseq(state->buf, '"'))
+				if (*state->buf == '\0')
 				{
-					/*
-					 * put implicit AND after an operand and handle this quote
-					 * in WAITOPERAND
-					 */
-					state->state = WAITOPERAND;
-					*operator = OP_AND;
-					return PT_OPR;
+					return PT_END;
 				}
 				else if (parse_or_operator(state))
 				{
@@ -492,15 +486,17 @@ gettoken_query_websearch(TSQueryParserState state, int8 *operator,
 					*operator = OP_OR;
 					return PT_OPR;
 				}
-				else if (*state->buf == '\0')
+				else if (ISOPERATOR(state->buf))
 				{
-					return PT_END;
+					/* ignore other operators in this state too */
+					state->buf++;
+					continue;
 				}
 				else if (!t_isspace(state->buf))
 				{
-					/* put implicit AND after an operand */
-					*operator = OP_AND;
+					/* insert implicit AND between operands */
 					state->state = WAITOPERAND;
+					*operator = OP_AND;
 					return PT_OPR;
 				}
 				break;
