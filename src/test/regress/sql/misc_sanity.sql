01aa88f71208c2af143b044553b89df4438de33e
Tom Lane <tgl@sss.pgh.pa.us>
2024-06-14 15:29:09 -0400

    Fix misc_sanity test to accept SHARED_DEPENDENCY_INITACL entries.
    Oversight in 534287403.  We missed this up to now because the
    core regression tests create no such entries (at least up to
    this test), so the only way to see the failure is to do
    "make installcheck" in an installation where some other DB
    has such entries.  I happened to do that just now ...

--- src/test/regress/sql/misc_sanity.sql
+++ src/test/regress/sql/misc_sanity.sql
@@ -30,7 +30,7 @@ SELECT *
 FROM pg_shdepend as d1
 WHERE refclassid = 0 OR refobjid = 0 OR
       classid = 0 OR objid = 0 OR
-      deptype NOT IN ('a', 'o', 'r', 't');
+      deptype NOT IN ('a', 'i', 'o', 'r', 't');
 
 
 -- **************** pg_class ****************
