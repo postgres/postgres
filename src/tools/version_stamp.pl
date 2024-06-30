e26810d01d441a457217a6eae9c2989fba29b80f
Michael Paquier <michael@paquier.xyz>
2024-07-01 07:56:10 +0900

    Stamp HEAD as 18devel.
    Let the hacking begin ...

--- src/tools/version_stamp.pl
+++ src/tools/version_stamp.pl
@@ -25,7 +25,7 @@ use warnings FATAL => 'all';
 
 # Major version is hard-wired into the script.  We update it when we branch
 # a new development version.
-my $majorversion = 17;
+my $majorversion = 18;
 
 # Validate argument and compute derived variables
 my $minor = shift;
