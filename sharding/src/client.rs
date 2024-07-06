fecf2636e1062deb0ef9f35efc8a7fd7663da41e
NContinanza <nicolas.continanza@lambdaclass.com>
2024-07-06 18:13:39 -0300

    Restore changes in client.res

--- sharding/src/client.rs
+++ sharding/src/client.rs
@@ -7,19 +7,19 @@ use tokio::net::TcpStream;
 
 #[tokio::main]
 async fn main() -> Result<(), PgWireError> {
-    // let mut connection: TcpStream = TcpStream::connect("localhost:5433")?;
+    let mut connection: TcpStream = TcpStream::connect("localhost:5433")?;
 
-    // // Send a simple message
-    // let mut message = Query {
-    //     query: "SELECT * from employees;".to_string(),
-    // };
-    // connection.write_all(message.as_bytes()).await?;
-    // let mut empty_array: [u8; 512] = [0; 512];
-    // // Receive and decode the response
-    // let mut response = connection.read(&mut *empty_array)?;
-    // println!("{}", response.to_string());
+    // Send a simple message
+    let mut message = Query {
+        query: "SELECT * from employees;".to_string(),
+    };
+    connection.write_all(message.as_bytes()).await?;
+    let mut empty_array: [u8; 512] = [0; 512];
+    // Receive and decode the response
+    let mut response = connection.read(&mut *empty_array)?;
+    println!("{}", response.to_string());
 
-    // connection.close()?;
+    connection.close()?;
 
     Ok(())
 }
