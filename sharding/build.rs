fd11a22d79c6159c4d6a7177f84caf6b5279f070
NContinanza <nicolas.continanza@lambdaclass.com>
2024-07-06 18:10:10 -0300

    Clear build.rs

--- sharding/build.rs
+++ sharding/build.rs
@@ -1,9 +1,2 @@
-extern crate cc;
-
 fn main() {
-    cc::Build::new()
-        .file("../src/bin/psql/common.c")
-        .include("../src/include")
-        .include("../src/interfaces/libpq")
-        .compile("libsharding.a");
 }
