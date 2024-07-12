e59d2c5fc9d54b35b010f79343037dbd6d6bd5f8
francupp9-12 <fcuppari@fi.uba.ar>
2024-07-12 17:12:06 -0300

    Add multiples connections with client

--- sharding/src/client.rs
+++ sharding/src/client.rs
@@ -1,15 +1,21 @@
-extern crate pgwire;
-
-use pgwire::error::PgWireError;
-use pgwire::messages::data::DataRow;
-use pgwire::messages::simplequery::Query;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
+use std::error::Error;
+use std::thread::sleep;
+use std::time::Duration;
 
 #[tokio::main]
-async fn main() -> Result<(), PgWireError> {
-    let mut connection = TcpStream::connect("localhost:5432").await;
-    // TODO: Jobs as a Shard
-    Ok(())
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut connection = TcpStream::connect("localhost:10000").await?;
 
+    // SQL query to be sent
+    let sql_query = "SELECT * FROM employees;";
+
+    connection.write_all(sql_query.as_bytes()).await?;
+    println!("Query sent: {}", sql_query);
+
+    let mut buffer = vec![0; 1024];
+
+    tokio::time::sleep(Duration::from_secs(30)).await;
+    Ok(())
 }
--- sharding/src/server.rs
+++ sharding/src/server.rs
@@ -1,117 +1,75 @@
-use std::fmt::Debug;
-use std::sync::Arc;
-
-use async_trait::async_trait;
-use futures::{stream, Sink, SinkExt, StreamExt};
-use tokio::net::TcpListener;
-
-use pgwire::api::auth::noop::NoopStartupHandler;
-use pgwire::api::copy::NoopCopyHandler;
-use pgwire::api::query::{PlaceholderExtendedQueryHandler, SimpleQueryHandler};
-use pgwire::api::results::{DataRowEncoder, FieldFormat, FieldInfo, QueryResponse, Response, Tag};
-use pgwire::api::{ClientInfo, PgWireHandlerFactory, Type};
-use pgwire::error::ErrorInfo;
-use pgwire::error::{PgWireError, PgWireResult};
-use pgwire::messages::response::NoticeResponse;
-use pgwire::messages::PgWireBackendMessage;
-use pgwire::tokio::process_socket;
+use std::net::SocketAddr;
+use std::sync::{Arc, Mutex};
+use std::io::Read;
+use tokio::io::AsyncReadExt;
+use std::net::{TcpListener, TcpStream};
 
 // Struct representing the connection with the Client
-// Pgwire
-pub struct Client;
-pub struct Router {
-    handler: Arc<Client>,
+pub struct Client {
+    stream: TcpStream,
+    n_queries: u16,
 }
 
-#[async_trait]
-impl SimpleQueryHandler for Client {
-    async fn do_query<'a, C>(
-        &self,
-        client: &mut C,
-        query: &'a str,
-    ) -> PgWireResult<Vec<Response<'a>>>
-        where
-            C: ClientInfo + Sink<PgWireBackendMessage> + Unpin + Send + Sync,
-            C::Error: Debug,
-            PgWireError: From<<C as Sink<PgWireBackendMessage>>::Error>,
-    {
-        client
-            .send(PgWireBackendMessage::NoticeResponse(NoticeResponse::from(
-                ErrorInfo::new(
-                    "NOTICE".to_owned(),
-                    "01000".to_owned(),
-                    format!("Query received {}", query),
-                ),
-            )))
-            .await?;
+pub struct Router {
+    clients_connected: Mutex<Vec<Client>>,
+}
 
-        if query.starts_with("SELECT") {
-            let f1 = FieldInfo::new("id".into(), None, None, Type::INT4, FieldFormat::Text);
-            let f2 = FieldInfo::new("name".into(), None, None, Type::VARCHAR, FieldFormat::Text);
-            let schema = Arc::new(vec![f1, f2]);
+impl Router {
+    fn new() -> Self {
+        Router {
+            clients_connected: Mutex::new(Vec::new()),
+        }
+    }
 
-            let data = vec![
-                (Some(0), Some("Tom")),
-                (Some(1), Some("Jerry")),
-                (Some(2), None),
-            ];
-            let schema_ref = schema.clone();
-            let data_row_stream = stream::iter(data.into_iter()).map(move |r| {
-                let mut encoder = DataRowEncoder::new(schema_ref.clone());
-                encoder.encode_field(&r.0)?;
-                encoder.encode_field(&r.1)?;
+    fn add_client(&self, client: TcpStream) {
+        let client = Client {
+            stream: client,
+            n_queries: 0,
+        };
+        self.clients_connected.lock().unwrap().push(client);
+    }
 
-                encoder.finish()
-            });
-
-            Ok(vec![Response::Query(QueryResponse::new(
-                schema,
-                data_row_stream,
-            ))])
-        } else {
-            Ok(vec![Response::Execution(Tag::new("OK").with_rows(1))])
+    async fn handle_client(&self, mut client: TcpStream, addr: SocketAddr) {
+        println!("Handling client from {}", addr);
+        println!("Connections: {}", self.clients_connected.lock().unwrap().len());
+        let mut buf = [0; 1024];
+        match client.read(&mut buf) {
+            Ok(n) if n == 0 => {
+                // Connection was closed
+                println!("Client from {} disconnected", addr);
+            }
+            Ok(n) => {
+                if let Ok(data) = String::from_utf8(buf[..n].to_vec()) {
+                    println!("Received data from {}: {}", addr, data);
+                } else {
+                    println!("Received invalid UTF-8 data from {}", addr);
+                }
+            }
+            Err(e) => {
+                println!("Error reading from {}: {:?}", addr, e);
+            }
         }
     }
 }
-impl PgWireHandlerFactory for Router {
-    type StartupHandler = NoopStartupHandler;
-    type SimpleQueryHandler = Client;
-    type ExtendedQueryHandler = PlaceholderExtendedQueryHandler;
-    type CopyHandler = NoopCopyHandler;
 
-    fn simple_query_handler(&self) -> Arc<Self::SimpleQueryHandler> {
-        self.handler.clone()
-    }
-
-    fn extended_query_handler(&self) -> Arc<Self::ExtendedQueryHandler> {
-        Arc::new(PlaceholderExtendedQueryHandler)
-    }
-
-    fn startup_handler(&self) -> Arc<Self::StartupHandler> {
-        Arc::new(NoopStartupHandler)
-    }
-
-    fn copy_handler(&self) -> Arc<Self::CopyHandler> {
-        Arc::new(NoopCopyHandler)
-    }
-}
 #[tokio::main]
 pub async fn main() {
-    let leader = Arc::new(Router {
-        handler: Arc::new(Client),
-    });
+    let leader = Arc::new(Router::new());
     // The idea is: Client connect to leader
-    let server_addr = "127.0.0.1:5432";
-    let listener = TcpListener::bind(server_addr).await.unwrap();
+    let server_addr = "127.0.0.1:10000";
+    let listener = TcpListener::bind(server_addr).unwrap();
     println!("Listening to clients {}", server_addr);
+
     loop {
-        let incoming_socket = listener.accept().await.unwrap();
+        let (incoming_socket, addr) = listener.accept().unwrap();
+        // Store every connection in leader
         let leader_ref = leader.clone();
+        leader_ref.add_client(incoming_socket.try_clone().expect("Error cloning socket"));
+
         // A task per connection
-        let _ = tokio::spawn(async move {
-            process_socket(incoming_socket.0, None, leader_ref).await.expect("Error accepting client");
-            // Client - PGwire -> Leader - pgwire -> shard (executes rust-postgres inside)
-            println!("Client {:?} connected successfully", incoming_socket.1);
-        }).await;
+        tokio::spawn(async move {
+            leader_ref.handle_client(incoming_socket, addr).await;
+            println!("Client {} connected successfully", addr);
+        });
     }
 }
