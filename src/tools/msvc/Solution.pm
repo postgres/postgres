ce3045e9b0e5414638621a4941d8e7419fca9433
Daniel Gustafsson <dgustafsson@postgresql.org>
2024-07-26 19:09:27 +0200

    Fix building with MSVC for TLS session disabling
    Commit 274bbced85 omitted the required changes for the MSVC build
    system in v16 through v12. Per buildfarm animal hamerkop.
    
    Discussion: https://postgr.es/m/7919238F-723C-4113-9742-EBCE7A76A6B4@yesql.se

--- src/tools/msvc/Solution.pm
+++ src/tools/msvc/Solution.pm
@@ -363,6 +363,7 @@ sub GenerateFiles
 		HAVE_SHM_OPEN                            => undef,
 		HAVE_SOCKLEN_T                           => 1,
 		HAVE_SPINLOCKS                           => 1,
+		HAVE_SSL_CTX_SET_NUM_TICKETS             => undef,
 		HAVE_STDBOOL_H                           => 1,
 		HAVE_STDINT_H                            => 1,
 		HAVE_STDLIB_H                            => 1,
@@ -558,6 +559,7 @@ sub GenerateFiles
 			|| ($digit1 >= '1' && $digit2 >= '1' && $digit3 >= '1'))
 		{
 			$define{HAVE_X509_GET_SIGNATURE_INFO} = 1;
+			$define{HAVE_SSL_CTX_SET_NUM_TICKETS} = 1;
 		}
 
 		# Symbols needed with OpenSSL 1.1.0 and above.
