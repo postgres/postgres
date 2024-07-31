97380d4803d1f9188a1436c6fe7ecd7db285c55c
Tom Lane <tgl@sss.pgh.pa.us>
2024-07-31 18:54:10 -0400

    Allow parallel workers to cope with a newly-created session user ID.
    Parallel workers failed after a sequence like
    	BEGIN;
    	CREATE USER foo;
    	SET SESSION AUTHORIZATION foo;
    because check_session_authorization could not see the uncommitted
    pg_authid row for "foo".  This is because we ran RestoreGUCState()
    in a separate transaction using an ordinary just-created snapshot.
    The same disease afflicts any other GUC that requires catalog lookups
    and isn't forgiving about the lookups failing.
    
    To fix, postpone RestoreGUCState() into the worker's main transaction
    after we've set up a snapshot duplicating the leader's.  This affects
    check_transaction_isolation and check_transaction_deferrable, which
    think they should only run during transaction start.  Make them
    act like check_transaction_read_only, which already knows it should
    silently accept the value when InitializingParallelWorker.
    
    Per bug #18545 from Andrey Rachitskiy.  Back-patch to all
    supported branches, because this has been wrong for awhile.
    
    Discussion: https://postgr.es/m/18545-feba138862f19aaa@postgresql.org

--- src/backend/access/transam/parallel.c
+++ src/backend/access/transam/parallel.c
@@ -1402,10 +1402,6 @@ ParallelWorkerMain(Datum main_arg)
 	libraryspace = shm_toc_lookup(toc, PARALLEL_KEY_LIBRARY, false);
 	StartTransactionCommand();
 	RestoreLibraryState(libraryspace);
-
-	/* Restore GUC values from launching backend. */
-	gucspace = shm_toc_lookup(toc, PARALLEL_KEY_GUC, false);
-	RestoreGUCState(gucspace);
 	CommitTransactionCommand();
 
 	/* Crank up a transaction state appropriate to a parallel worker. */
@@ -1446,6 +1442,14 @@ ParallelWorkerMain(Datum main_arg)
 	 * system caches.
 	 */
 	InvalidateSystemCaches();
+
+	/*
+	 * Restore GUC values from launching backend.  We can't do this earlier,
+	 * because GUC check hooks that do catalog lookups need to see the same
+	 * database state as the leader.
+	 */
+	gucspace = shm_toc_lookup(toc, PARALLEL_KEY_GUC, false);
+	RestoreGUCState(gucspace);
 
 	/*
 	 * Restore current role id.  Skip verifying whether session user is
