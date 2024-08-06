a54d4ed183927f15e1853b83106acebeeeee11c8
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2024-08-06 22:15:55 +0300

    Fix datatypes in comments in instr_time.h
    The INSTR_TIME_GET_NANOSEC(t) and INSTR_TIME_GET_MICROSEC(t) macros
    return a signed int64.
    
    Discussion: https://www.postgresql.org/message-id/ZrHkv3MAQfwNSmTG@ip-10-97-1-34.eu-west-3.compute.internal

--- src/include/portability/instr_time.h
+++ src/include/portability/instr_time.h
@@ -32,9 +32,9 @@
  *
  * INSTR_TIME_GET_MILLISEC(t)		convert t to double (in milliseconds)
  *
- * INSTR_TIME_GET_MICROSEC(t)		convert t to uint64 (in microseconds)
+ * INSTR_TIME_GET_MICROSEC(t)		convert t to int64 (in microseconds)
  *
- * INSTR_TIME_GET_NANOSEC(t)		convert t to uint64 (in nanoseconds)
+ * INSTR_TIME_GET_NANOSEC(t)		convert t to int64 (in nanoseconds)
  *
  * Note that INSTR_TIME_SUBTRACT and INSTR_TIME_ACCUM_DIFF convert
  * absolute times to intervals.  The INSTR_TIME_GET_xxx operations are
