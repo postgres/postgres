b43110869fd82b549df5c6ab7ade476d6e414f37
Tom Lane <tgl@sss.pgh.pa.us>
2024-08-30 12:42:13 -0400

    Avoid inserting PlaceHolderVars in cases where pre-v16 PG did not.
    Commit 2489d76c4 removed some logic from pullup_replace_vars()
    that avoided wrapping a PlaceHolderVar around a pulled-up
    subquery output expression if the expression could be proven
    to go to NULL anyway (because it contained Vars or PHVs of the
    pulled-up relation and did not contain non-strict constructs).
    But removing that logic turns out to cause performance regressions
    in some cases, because the extra PHV blocks subexpression folding,
    and will do so even if outer-join reduction later turns it into a
    no-op with no phnullingrels bits.  This can for example prevent
    an expression from being matched to an index.
    
    The reason for always adding a PHV was to ensure we had someplace
    to put the varnullingrels marker bits of the Var being replaced.
    However, it turns out we can optimize in exactly the same cases that
    the previous code did, because we can instead attach the needed
    varnullingrels bits to the contained Var(s)/PHV(s).
    
    This is not a complete solution --- it would be even better if we
    could remove PHVs after reducing them to no-ops.  It doesn't look
    practical to back-patch such an improvement, but this change seems
    safe and at least gets rid of the performance-regression cases.
    
    Per complaint from Nikhil Raj.  Back-patch to v16 where the
    problem appeared.
    
    Discussion: https://postgr.es/m/CAG1ps1xvnTZceKK24OUfMKLPvDP2vjT-d+F2AOCWbw_v3KeEgg@mail.gmail.com

--- src/test/regress/sql/subselect.sql
+++ src/test/regress/sql/subselect.sql
@@ -891,6 +891,24 @@ fetch backward all in c1;
 commit;
 
 --
+-- Verify that we correctly flatten cases involving a subquery output
+-- expression that doesn't need to be wrapped in a PlaceHolderVar
+--
+
+explain (costs off)
+select tname, attname from (
+select relname::information_schema.sql_identifier as tname, * from
+  (select * from pg_class c) ss1) ss2
+  right join pg_attribute a on a.attrelid = ss2.oid
+where tname = 'tenk1' and attnum = 1;
+
+select tname, attname from (
+select relname::information_schema.sql_identifier as tname, * from
+  (select * from pg_class c) ss1) ss2
+  right join pg_attribute a on a.attrelid = ss2.oid
+where tname = 'tenk1' and attnum = 1;
+
+--
 -- Tests for CTE inlining behavior
 --
 
