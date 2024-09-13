7b1ddbae361b0ed92d5696d666ae85a2cc4dd883
Michael Paquier <michael@paquier.xyz>
2024-09-13 09:41:06 +0900

    pg_stat_statements: Add tests with extended query protocol
    There are currently no tests in the tree checking that queries using the
    extended query protocol are able to map with their query ID.
    
    This can be achieved for some paths of the extended query protocol with
    the psql meta-commands \bind or \bind_named, so let's add some tests
    based on both.
    
    I have found that to be a useful addition while working on a different
    issue.
    
    Discussion: https://postgr.es/m/ZuEt6MOEBSlifBfn@paquier.xyz

---
+++ contrib/pg_stat_statements/sql/extended.sql
@@ -0,0 +1,17 @@
+-- Tests with extended query protocol
+
+SET pg_stat_statements.track_utility = FALSE;
+
+SELECT pg_stat_statements_reset() IS NOT NULL AS t;
+SELECT $1 \parse stmt1
+SELECT $1, $2 \parse stmt2
+SELECT $1, $2, $3 \parse stmt3
+SELECT $1 \bind 'unnamed_val1' \g
+\bind_named stmt1 'stmt1_val1' \g
+\bind_named stmt2 'stmt2_val1' 'stmt2_val2' \g
+\bind_named stmt3 'stmt3_val1' 'stmt3_val2' 'stmt3_val3' \g
+\bind_named stmt3 'stmt3_val4' 'stmt3_val5' 'stmt3_val6' \g
+\bind_named stmt2 'stmt2_val3' 'stmt2_val4' \g
+\bind_named stmt1 'stmt1_val1' \g
+
+SELECT calls, rows, query FROM pg_stat_statements ORDER BY query COLLATE "C";
