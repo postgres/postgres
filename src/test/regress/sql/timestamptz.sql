b8ea0f675f35c3f0c2cf62175517ba0dacad4abd
Tom Lane <tgl@sss.pgh.pa.us>
2024-09-14 17:55:02 -0400

    Run regression tests with timezone America/Los_Angeles.
    Historically we've used timezone "PST8PDT", but the recent release
    2024b of tzdb changes the definition of that zone in a way that
    breaks many test cases concerned with dates before 1970.  Although
    we've not yet adopted 2024b into our own tree, this is already
    problematic for people using --with-system-tzdata if their platform
    has already adopted 2024b.  To work with both older and newer
    versions of tzdb, switch to using "America/Los_Angeles", accepting
    the ensuing changes in regression test results.
    
    Back-patch to all supported branches.
    
    Per report and patch from Wolfgang Walther.
    
    Discussion: https://postgr.es/m/0a997455-5aba-4cf2-a354-d26d8bcbfae6@technowledgy.de

--- src/test/regress/sql/timestamptz.sql
+++ src/test/regress/sql/timestamptz.sql
@@ -449,7 +449,10 @@ SELECT make_timestamptz(1910, 12, 24, 0, 0, 0, 'Nehwon/Lankhmar');
 -- abbreviations
 SELECT make_timestamptz(2008, 12, 10, 10, 10, 10, 'EST');
 SELECT make_timestamptz(2008, 12, 10, 10, 10, 10, 'EDT');
-SELECT make_timestamptz(2014, 12, 10, 10, 10, 10, 'PST8PDT');
+SELECT make_timestamptz(2014, 12, 10, 10, 10, 10, 'FOO8BAR');
+
+-- POSIX
+SELECT make_timestamptz(2014, 12, 10, 10, 10, 10, 'PST8PDT,M3.2.0,M11.1.0');
 
 RESET TimeZone;
 
