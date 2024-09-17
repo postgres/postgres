419753082c5e257477a38493303d33ecb124074b
Artem Gavrilov <charlieblackwood7@gmail.com>
2024-09-17 18:48:43 +0200

    Rename _iv_prefix_len to _start_offset to make it match underlying func parameter (#283)

--- src/include/encryption/enc_tde.h
+++ src/include/encryption/enc_tde.h
@@ -32,11 +32,11 @@ PGTdePageAddItemExtended(RelFileLocator rel, Oid oid, BlockNumber bn, Page page,
 
 /* Function Macros over crypt */
 
-#define PG_TDE_ENCRYPT_DATA(_iv_prefix, _iv_prefix_len, _data, _data_len, _out, _key) \
-	pg_tde_crypt(_iv_prefix, _iv_prefix_len, _data, _data_len, _out, _key, "ENCRYPT")
+#define PG_TDE_ENCRYPT_DATA(_iv_prefix, _start_offset, _data, _data_len, _out, _key) \
+	pg_tde_crypt(_iv_prefix, _start_offset, _data, _data_len, _out, _key, "ENCRYPT")
 
-#define PG_TDE_DECRYPT_DATA(_iv_prefix, _iv_prefix_len, _data, _data_len, _out, _key) \
-	pg_tde_crypt(_iv_prefix, _iv_prefix_len, _data, _data_len, _out, _key, "DECRYPT")
+#define PG_TDE_DECRYPT_DATA(_iv_prefix, _start_offset, _data, _data_len, _out, _key) \
+	pg_tde_crypt(_iv_prefix, _start_offset, _data, _data_len, _out, _key, "DECRYPT")
 
 #define PG_TDE_DECRYPT_TUPLE(_tuple, _out_tuple, _key) \
 	pg_tde_crypt_tuple(_tuple, _out_tuple, _key, "DECRYPT-TUPLE")
@@ -47,9 +47,9 @@ PGTdePageAddItemExtended(RelFileLocator rel, Oid oid, BlockNumber bn, Page page,
 	pg_tde_crypt_tuple(_tuple, _out_tuple, _key, _msg_context); \
 	} while(0)
 
-#define PG_TDE_ENCRYPT_PAGE_ITEM(_iv_prefix, _iv_prefix_len, _data, _data_len, _out, _key) \
+#define PG_TDE_ENCRYPT_PAGE_ITEM(_iv_prefix, _start_offset, _data, _data_len, _out, _key) \
 	do { \
-		pg_tde_crypt(_iv_prefix, _iv_prefix_len, _data, _data_len, _out, _key, "ENCRYPT-PAGE-ITEM"); \
+		pg_tde_crypt(_iv_prefix, _start_offset, _data, _data_len, _out, _key, "ENCRYPT-PAGE-ITEM"); \
 	} while(0)
 
 extern void AesEncryptKey(const TDEPrincipalKey *principal_key, const RelFileLocator *rlocator, RelKeyData *rel_key_data, RelKeyData **p_enc_rel_key_data, size_t *enc_key_bytes);
