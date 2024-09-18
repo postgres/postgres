b52c4fc3c09ec0ec9c1f9aa676f6d74304cc2f6f
Nathan Bossart <nathan@postgresql.org>
2024-09-18 14:42:57 -0500

    Add TOAST table to pg_index.
    This change allows pg_index rows to use out-of-line storage for the
    "indexprs" and "indpred" columns, which enables use-cases such as
    very large index expressions.
    
    This system catalog was previously not given a TOAST table due to a
    fear of circularity issues (see commit 96cdeae07f).  Testing has
    not revealed any such problems, and it seems unlikely that the
    entries for system indexes could ever need out-of-line storage.  In
    any case, it is still early in the v18 development cycle, so
    committing this now will hopefully increase the chances of finding
    any unexpected problems prior to release.
    
    Bumps catversion.
    
    Reported-by: Jonathan Katz
    Reviewed-by: Tom Lane
    Discussion: https://postgr.es/m/b611015f-b423-458c-aa2d-be0e655cc1b4%40postgresql.org

--- src/include/catalog/catversion.h
+++ src/include/catalog/catversion.h
@@ -57,6 +57,6 @@
  */
 
 /*							yyyymmddN */
-#define CATALOG_VERSION_NO	202409181
+#define CATALOG_VERSION_NO	202409182
 
 #endif
--- src/test/regress/sql/misc_sanity.sql
+++ src/test/regress/sql/misc_sanity.sql
@@ -38,8 +38,8 @@ WHERE refclassid = 0 OR refobjid = 0 OR
 -- Look for system tables with varlena columns but no toast table. All
 -- system tables with toastable columns should have toast tables, with
 -- the following exceptions:
--- 1. pg_class, pg_attribute, and pg_index, due to fear of recursive
--- dependencies as toast tables depend on them.
+-- 1. pg_class and pg_attribute, due to fear of recursive dependencies as
+-- toast tables depend on them.
 -- 2. pg_largeobject and pg_largeobject_metadata.  Large object catalogs
 -- and toast tables are mutually exclusive and large object data is handled
 -- as user data by pg_upgrade, which would cause failures.
