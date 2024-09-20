c00c54a9ac1e0c52ace8051a30805ca74ae53212
Peter Geoghegan <pg@bowt.ie>
2024-09-20 14:06:32 -0400

    Fix nbtree pgstats accounting with parallel scans.
    Commit 5bf748b8, which enhanced nbtree ScalarArrayOp execution, made
    parallel index scans work with the new design for arrays via explicit
    scheduling of primitive index scans.  Under this scheme a parallel index
    scan with array keys will perform the same number of index descents as
    an equivalent serial index scan (barring corner cases where an
    individual parallel worker discovers that it can advance the scan's
    array keys without anybody needing to perform another descent of the
    index to get to the relevant page on the leaf level).
    
    Despite all this, the pgstats accounting wasn't updated; it continued to
    increment the total number of index scans for the rel once per _bt_first
    call, no matter the details.  As a result, the number of (primitive)
    index scans could be over-counted during parallel scans.
    
    To fix, delay incrementing the count of index scans until after we've
    established that another descent of the index (using either _bt_search
    or _bt_endpoint) is required.  That way pg_stat_user_tables.idx_scan
    always advances in the same way, regardless of whether or not the scan
    makes use of parallelism.
    
    Oversight in commit 5bf748b8, which enhanced nbtree ScalarArrayOp
    execution.
    
    Author: Peter Geoghegan <pg@bowt.ie>
    Reviewed-By: Tomas Vondra <tomas@vondra.me>
    Discussion: https://postgr.es/m/CAH2-Wz=E7XrkvscBN0U6V81NK3Q-dQOmivvbEsjG-zwEfDdFpg@mail.gmail.com
    Discussion: https://postgr.es/m/CAH2-WzkRqvaqR2CTNqTZP0z6FuL4-3ED6eQB0yx38XBNj1v-4Q@mail.gmail.com
    Backpatch: 17-, where nbtree SAOP execution was enhanced.

--- src/backend/access/nbtree/nbtsearch.c
+++ src/backend/access/nbtree/nbtsearch.c
@@ -896,8 +896,6 @@ _bt_first(IndexScanDesc scan, ScanDirection dir)
 
 	Assert(!BTScanPosIsValid(so->currPos));
 
-	pgstat_count_index_scan(rel);
-
 	/*
 	 * Examine the scan keys and eliminate any redundant keys; also mark the
 	 * keys that must be matched to continue the scan.
@@ -959,6 +957,12 @@ _bt_first(IndexScanDesc scan, ScanDirection dir)
 		 */
 		_bt_start_array_keys(scan, dir);
 	}
+
+	/*
+	 * Count an indexscan for stats, now that we know that we'll call
+	 * _bt_search/_bt_endpoint below
+	 */
+	pgstat_count_index_scan(rel);
 
 	/*----------
 	 * Examine the scan keys to discover where we need to start the scan.
