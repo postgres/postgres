c3316a3b8dccf4d9cd17d66f1984bf7d4ce38fa9
Amit Langote <amitlan@postgresql.org>
2024-10-23 17:54:48 +0900

    Remove unnecessary word in a comment
    Relations opened by the executor are only closed once in
    ExecCloseRangeTableRelations(), so the word "again" in the comment
    for ExecGetRangeTableRelation() is misleading and unnecessary.
    
    Discussion: https://postgr.es/m/CA+HiwqHnw-zR+u060i3jp4ky5UR0CjByRFQz50oZ05de7wUg=Q@mail.gmail.com
    Backpatch-through: 12

--- src/backend/executor/execUtils.c
+++ src/backend/executor/execUtils.c
@@ -782,7 +782,7 @@ ExecInitRangeTable(EState *estate, List *rangeTable)
  * ExecGetRangeTableRelation
  *		Open the Relation for a range table entry, if not already done
  *
- * The Relations will be closed again in ExecEndPlan().
+ * The Relations will be closed in ExecEndPlan().
  */
 Relation
 ExecGetRangeTableRelation(EState *estate, Index rti)
