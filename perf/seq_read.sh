b77e9aa183145b8d4eb18e3d3c8732cd4e815531
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2024-10-29 08:01:06 +0000

    Fixing review comments

--- perf/seq_read.sh
+++ perf/seq_read.sh
@@ -2,14 +2,21 @@
 cd "$(dirname "$0")"
 
 xz -d pp-2019.csv.xz
+RECORDS=`wc -l pp-2019.csv`
+echo "CSV entries: $RECORDS"
 cp pp-2019.csv /tmp/
 createdb seq_read_test
 psql seq_read_test < seq_read_prepare.sql > /dev/null
 echo "Sequential scan read times"
 echo "=========================="
 echo -n "HEAP: "
-psql seq_read_test < seq_read_run_heap.sql | grep "Execution" | tail -n 10 | cut -d " " -f 4 | paste -sd+ | bc
+HEAP=`psql seq_read_test < seq_read_run_heap.sql | grep "Execution" | tail -n 10 | cut -d " " -f 4 | paste -sd+ | bc`
+echo $HEAP
 echo -n "TDE: "
-psql seq_read_test < seq_read_run_tde.sql | grep "Execution" | tail -n 10 | cut -d " " -f 4 | paste -sd+ | bc
+TDE=`psql seq_read_test < seq_read_run_tde.sql | grep "Execution" | tail -n 10 | cut -d " " -f 4 | paste -sd+ | bc`
+TDE_PERC=`bc <<< "$TDE*100/$HEAP"`
+echo "$TDE ($TDE_PERC%)"
 echo -n "TDE_BASIC: "
-psql seq_read_test < seq_read_run_tde_basic.sql | grep "Execution" | tail -n 10 | cut -d " " -f 4 | paste -sd+ | bc
+TDE_BASIC=`psql seq_read_test < seq_read_run_tde_basic.sql | grep "Execution" | tail -n 10 | cut -d " " -f 4 | paste -sd+ | bc`
+TDE_BASIC_PERC=`bc <<< "$TDE_BASIC*100/$HEAP"`
+echo "$TDE ($TDE_BASIC_PERC%)"
