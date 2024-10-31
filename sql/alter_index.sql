c5f2fbb322bd0f340dab5c25d00f0b142ad83837
Andrew Pogrebnoi <absourd.noise@gmail.com>
2024-10-31 11:28:03 +0200

    Fix IF eval precedence in ALTER trigger (#322)
    ... and add tests for ALTER INDEX

---
+++ sql/alter_index.sql
@@ -0,0 +1,2 @@
+\set tde_am tde_heap
+\i sql/alter_index.inc
--- src/pg_tde_event_capture.c
+++ src/pg_tde_event_capture.c
@@ -141,8 +141,8 @@ pg_tde_ddl_command_start_capture(PG_FUNCTION_ARGS)
 		{
 			AlterTableCmd *cmd = (AlterTableCmd *) lfirst(lcmd);
 			if (cmd->subtype == AT_SetAccessMethod && 
-				(cmd->name != NULL && strcmp(cmd->name, "tde_heap")==0) ||
-				(cmd->name == NULL && strcmp(default_table_access_method, "tde_heap") == 0)
+				((cmd->name != NULL && strcmp(cmd->name, "tde_heap")==0) ||
+				 (cmd->name == NULL && strcmp(default_table_access_method, "tde_heap") == 0))
 				)
 			{
 				tdeCurrentCreateEvent.encryptMode = true;
