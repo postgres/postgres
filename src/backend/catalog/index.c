766809db39029115ee3e9e742b59dae4d9476e73
Noah Misch <noah@leadboat.com>
2024-11-02 19:42:52 -0700

    Suppress new "may be used uninitialized" warning.
    Buildfarm member mamba fails to deduce that the function never uses this
    variable without initializing it.  Back-patch to v12, like commit
    b412f402d1e020c5dac94f3bf4a005db69519b99.

--- src/backend/catalog/index.c
+++ src/backend/catalog/index.c
@@ -2788,8 +2788,8 @@ index_update_stats(Relation rel,
 				   double reltuples)
 {
 	bool		update_stats;
-	BlockNumber relpages;
-	BlockNumber relallvisible;
+	BlockNumber relpages = 0;	/* keep compiler quiet */
+	BlockNumber relallvisible = 0;
 	Oid			relid = RelationGetRelid(rel);
 	Relation	pg_class;
 	ScanKeyData key[1];
@@ -2826,8 +2826,6 @@ index_update_stats(Relation rel,
 
 		if (rel->rd_rel->relkind != RELKIND_INDEX)
 			visibilitymap_count(rel, &relallvisible, NULL);
-		else					/* don't bother for indexes */
-			relallvisible = 0;
 	}
 
 	/*
