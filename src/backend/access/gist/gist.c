215f7af27d622162a1e33883f71697af374b98cf
Masahiko Sawada <msawada@postgresql.org>
2024-11-04 10:21:59 -0800

    Fix typo in comment of gistdoinsert().
    Author: Tender Wang
    Reviewed-by: Masahiko Sawada
    Discussion: https://postgr.es/m/CAHewXN%3D3sH2sNw4nC3QGCEVw1Lftmw9m5y1Xje0bXK6ApDrsPQ%40mail.gmail.com

--- src/backend/access/gist/gist.c
+++ src/backend/access/gist/gist.c
@@ -627,7 +627,7 @@ gistplacetopage(Relation rel, Size freespace, GISTSTATE *giststate,
 }
 
 /*
- * Workhouse routine for doing insertion into a GiST index. Note that
+ * Workhorse routine for doing insertion into a GiST index. Note that
  * this routine assumes it is invoked in a short-lived memory context,
  * so it does not bother releasing palloc'd allocations.
  */
