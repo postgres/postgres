1d5fb19cda88cd27c8d7b3fe935e4a645fadce5f
Alexander Korotkov <akorotkov@postgresql.org>
2024-11-12 01:44:20 +0200

    Fix arrays comparison in CompareOpclassOptions()
    The current code calls array_eq() and does not provide FmgrInfo.  This commit
    provides initialization of FmgrInfo and uses C collation as the safe option
    for text comparison because we don't know anything about the semantics of
    opclass options.
    
    Backpatch to 13, where opclass options were introduced.
    
    Reported-by: Nicolas Maus
    Discussion: https://postgr.es/m/18692-72ea398df3ec6712%40postgresql.org
    Backpatch-through: 13

--- src/backend/commands/indexcmds.c
+++ src/backend/commands/indexcmds.c
@@ -28,6 +28,7 @@
 #include "catalog/namespace.h"
 #include "catalog/pg_am.h"
 #include "catalog/pg_authid.h"
+#include "catalog/pg_collation.h"
 #include "catalog/pg_constraint.h"
 #include "catalog/pg_database.h"
 #include "catalog/pg_inherits.h"
@@ -359,10 +360,12 @@ static bool
 CompareOpclassOptions(const Datum *opts1, const Datum *opts2, int natts)
 {
 	int			i;
+	FmgrInfo	fm;
 
 	if (!opts1 && !opts2)
 		return true;
 
+	fmgr_info(F_ARRAY_EQ, &fm);
 	for (i = 0; i < natts; i++)
 	{
 		Datum		opt1 = opts1 ? opts1[i] : (Datum) 0;
@@ -378,8 +381,12 @@ CompareOpclassOptions(const Datum *opts1, const Datum *opts2, int natts)
 		else if (opt2 == (Datum) 0)
 			return false;
 
-		/* Compare non-NULL text[] datums. */
-		if (!DatumGetBool(DirectFunctionCall2(array_eq, opt1, opt2)))
+		/*
+		 * Compare non-NULL text[] datums.  Use C collation to enforce binary
+		 * equivalence of texts, because we don't know anything about the
+		 * semantics of opclass options.
+		 */
+		if (!DatumGetBool(FunctionCall2Coll(&fm, C_COLLATION_OID, opt1, opt2)))
 			return false;
 	}
 
