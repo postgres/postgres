88a16b3dbb16dc127894ad56c57c53d309ae816a
Michael Paquier <michael@paquier.xyz>
2024-11-27 09:31:40 +0900

    Handle better implicit transaction state of pipeline mode
    When using a pipeline, a transaction starts from the first command and
    is committed with a Sync message or when the pipeline ends.
    
    Functions like IsInTransactionBlock() or PreventInTransactionBlock()
    were already able to understand a pipeline as being in a transaction
    block, but it was not the case of CheckTransactionBlock().  This
    function is called for example to generate a WARNING for SET LOCAL,
    complaining that it is used outside of a transaction block.
    
    The current state of the code caused multiple problems, like:
    - SET LOCAL executed at any stage of a pipeline issued a WARNING, even
    if the command was at least second in line where the pipeline is in a
    transaction state.
    - LOCK TABLE failed when invoked at any step of a pipeline, even if it
    should be able to work within a transaction block.
    
    The pipeline protocol assumes that the first command of a pipeline is
    not part of a transaction block, and that any follow-up commands is
    considered as within a transaction block.
    
    This commit changes the backend so as an implicit transaction block is
    started each time the first Execute message of a pipeline has finished
    processing, with this implicit transaction block ended once a sync is
    processed.  The checks based on XACT_FLAGS_PIPELINING in the routines
    checking if we are in a transaction block are not necessary: it is
    enough to rely on the existing ones.
    
    Some tests are added to pgbench, that can be backpatched down to v17
    when \syncpipeline is involved and down to v14 where \startpipeline and
    \endpipeline are available.  This is unfortunately limited regarding the
    error patterns that can be checked, but it provides coverage for various
    pipeline combinations to check if these succeed or fail.  These tests
    are able to capture the case of SET LOCAL's WARNING.  The author has
    proposed a different feature to improve the coverage by adding similar
    meta-commands to psql where error messages could be checked, something
    more useful for the cases where commands cannot be used in transaction
    blocks, like REINDEX CONCURRENTLY or VACUUM.  This is considered as
    future work for v18~.
    
    Author: Anthonin Bonnefoy
    Reviewed-by: Jelte Fennema-Nio, Michael Paquier
    Discussion: https://postgr.es/m/CAO6_XqrWO8uNBQrSu5r6jh+vTGi5Oiyk4y8yXDORdE2jbzw8xw@mail.gmail.com
    Backpatch-through: 13

--- src/backend/access/transam/xact.c
+++ src/backend/access/transam/xact.c
@@ -3490,16 +3490,6 @@ PreventInTransactionBlock(bool isTopLevel, const char *stmtType)
 						stmtType)));
 
 	/*
-	 * inside a pipeline that has started an implicit transaction?
-	 */
-	if (MyXactFlags & XACT_FLAGS_PIPELINING)
-		ereport(ERROR,
-				(errcode(ERRCODE_ACTIVE_SQL_TRANSACTION),
-		/* translator: %s represents an SQL statement name */
-				 errmsg("%s cannot be executed within a pipeline",
-						stmtType)));
-
-	/*
 	 * inside a function call?
 	 */
 	if (!isTopLevel)
@@ -3608,9 +3598,6 @@ IsInTransactionBlock(bool isTopLevel)
 		return true;
 
 	if (IsSubTransaction())
-		return true;
-
-	if (MyXactFlags & XACT_FLAGS_PIPELINING)
 		return true;
 
 	if (!isTopLevel)
--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -2746,6 +2746,17 @@ start_xact_command(void)
 
 		xact_started = true;
 	}
+	else if (MyXactFlags & XACT_FLAGS_PIPELINING)
+	{
+		/*
+		 * When the first Execute message is completed, following commands
+		 * will be done in an implicit transaction block created via
+		 * pipelining. The transaction state needs to be updated to an
+		 * implicit block if we're not already in a transaction block (like
+		 * one started by an explicit BEGIN).
+		 */
+		BeginImplicitTransactionBlock();
+	}
 
 	/*
 	 * Start statement timeout if necessary.  Note that this'll intentionally
@@ -4865,6 +4876,13 @@ PostgresMain(const char *dbname, const char *username)
 
 			case 'S':			/* sync */
 				pq_getmsgend(&input_message);
+
+				/*
+				 * If pipelining was used, we may be in an implicit
+				 * transaction block. Close it before calling
+				 * finish_xact_command.
+				 */
+				EndImplicitTransactionBlock();
 				finish_xact_command();
 				send_ready_for_query = true;
 				break;
