9d8ab2c461d706ca22adf86c057d4f76ca7f6da6
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2024-12-21 23:42:39 +0200

    Update TransactionXmin when MyProc->xmin is updated
    GetSnapshotData() set TransactionXmin = MyProc->xmin, but when
    SnapshotResetXmin() advanced MyProc->xmin, it did not advance
    TransactionXmin correspondingly. That meant that TransactionXmin could
    be older than MyProc->xmin, and XIDs between than TransactionXmin and
    the real MyProc->xmin could be vacuumed away. One known consequence is
    in pg_subtrans lookups: we might try to look up the status of an XID
    that was already truncated away.
    
    Back-patch to all supported versions.
    
    Reviewed-by: Andres Freund
    Discussion: https://www.postgresql.org/message-id/d27a046d-a1e4-47d1-a95c-fbabe41debb4@iki.fi

--- src/backend/utils/time/snapmgr.c
+++ src/backend/utils/time/snapmgr.c
@@ -950,7 +950,7 @@ SnapshotResetXmin(void)
 
 	if (pairingheap_is_empty(&RegisteredSnapshots))
 	{
-		MyProc->xmin = InvalidTransactionId;
+		MyProc->xmin = TransactionXmin = InvalidTransactionId;
 		return;
 	}
 
@@ -958,7 +958,7 @@ SnapshotResetXmin(void)
 										pairingheap_first(&RegisteredSnapshots));
 
 	if (TransactionIdPrecedes(MyProc->xmin, minSnapshot->xmin))
-		MyProc->xmin = minSnapshot->xmin;
+		MyProc->xmin = TransactionXmin = minSnapshot->xmin;
 }
 
 /*
