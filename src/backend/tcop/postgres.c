4a68d5008869afd819b03075d69bf102dd2f1bda
Nathan Bossart <nathan@postgresql.org>
2025-01-07 15:34:19 -0600

    Use PqMsg_* macros in postgres.c.
    Commit f4b54e1ed9, which introduced macros for protocol characters,
    missed updating a couple of places in postgres.c.
    
    Author: Dave Cramer
    Reviewed-by: Fabrízio de Royes Mello
    Discussion: https://postgr.es/m/CADK3HHJUVBPoVOmFesPB-fN8_dYt%2BQELV2UB6jxOW2Z40qF-qw%40mail.gmail.com
    Backpatch-through: 17

--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -310,7 +310,7 @@ InteractiveBackend(StringInfo inBuf)
 		printf("statement: %s\n", inBuf->data);
 	fflush(stdout);
 
-	return 'Q';
+	return PqMsg_Query;
 }
 
 /*
@@ -4897,9 +4897,9 @@ PostgresMain(const char *dbname, const char *username)
 				break;
 
 				/*
-				 * 'X' means that the frontend is closing down the socket. EOF
-				 * means unexpected loss of frontend connection. Either way,
-				 * perform normal shutdown.
+				 * PqMsg_Terminate means that the frontend is closing down the
+				 * socket. EOF means unexpected loss of frontend connection.
+				 * Either way, perform normal shutdown.
 				 */
 			case EOF:
 
