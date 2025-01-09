69ab44651422c49a6256d1b6cca6c20b5060ad92
Álvaro Herrera <alvherre@alvh.no-ip.org>
2025-01-09 07:33:52 +0100

    Fix SLRU bank selection code
    The originally submitted code (using bit masking) was correct when the
    number of slots was restricted to be a power of two -- but that
    limitation was removed during development that led to commit
    53c2a97a9266, which made the bank selection code incorrect.  This led to
    always using a smaller number of banks than available.  Change said code
    to use integer modulo instead, which works correctly with an arbitrary
    number of banks.
    
    It's likely that we could improve on this to avoid runtime use of
    integer division.  But with this change we're, at least, not wasting
    memory on unused banks, and more banks mean less contention, which is
    likely to have a much higher performance impact than a single
    instruction's latency.
    
    Author: Yura Sokolov <y.sokolov@postgrespro.ru>
    Reviewed-by: Andrey Borodin <x4mmm@yandex-team.ru>
    Discussion: https://postgr.es/m/9444dc46-ca47-43ed-9058-89c456316306@postgrespro.ru

--- src/backend/access/transam/slru.c
+++ src/backend/access/transam/slru.c
@@ -343,7 +343,7 @@ SimpleLruInit(SlruCtl ctl, const char *name, int nslots, int nlsns,
 	ctl->shared = shared;
 	ctl->sync_handler = sync_handler;
 	ctl->long_segment_names = long_segment_names;
-	ctl->bank_mask = (nslots / SLRU_BANK_SIZE) - 1;
+	ctl->nbanks = nbanks;
 	strlcpy(ctl->Dir, subdir, sizeof(ctl->Dir));
 }
 
@@ -606,7 +606,7 @@ SimpleLruReadPage_ReadOnly(SlruCtl ctl, int64 pageno, TransactionId xid)
 {
 	SlruShared	shared = ctl->shared;
 	LWLock	   *banklock = SimpleLruGetBankLock(ctl, pageno);
-	int			bankno = pageno & ctl->bank_mask;
+	int			bankno = pageno % ctl->nbanks;
 	int			bankstart = bankno * SLRU_BANK_SIZE;
 	int			bankend = bankstart + SLRU_BANK_SIZE;
 
@@ -1180,7 +1180,7 @@ SlruSelectLRUPage(SlruCtl ctl, int64 pageno)
 		int			bestinvalidslot = 0;	/* keep compiler quiet */
 		int			best_invalid_delta = -1;
 		int64		best_invalid_page_number = 0;	/* keep compiler quiet */
-		int			bankno = pageno & ctl->bank_mask;
+		int			bankno = pageno % ctl->nbanks;
 		int			bankstart = bankno * SLRU_BANK_SIZE;
 		int			bankend = bankstart + SLRU_BANK_SIZE;
 
