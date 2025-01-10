27a1f8d108114ec463101a812f83542c257f8169
Daniel Gustafsson <dgustafsson@postgresql.org>
2025-01-10 22:02:58 +0100

    Fix missing ldapscheme option in pg_hba_file_rules()
    The ldapscheme option was missed when inspecing the HbaLine for
    assembling rows for the pg_hba_file_rules function.  Backpatch
    to all supported versions.
    
    Author: Laurenz Albe <laurenz.albe@cybertec.at>
    Reported-by: Laurenz Albe <laurenz.albe@cybertec.at>
    Reviewed-by: Daniel Gustafsson <daniel@yesql.se>
    Bug: 18769
    Discussion: https://postgr.es/m/18769-dd8610cbc0405172@postgresql.org
    Backpatch-through: v13

--- src/backend/utils/adt/hbafuncs.c
+++ src/backend/utils/adt/hbafuncs.c
@@ -89,6 +89,10 @@ get_hba_options(HbaLine *hba)
 			options[noptions++] =
 				CStringGetTextDatum(psprintf("ldapport=%d", hba->ldapport));
 
+		if (hba->ldapscheme)
+			options[noptions++] =
+				CStringGetTextDatum(psprintf("ldapscheme=%s", hba->ldapscheme));
+
 		if (hba->ldaptls)
 			options[noptions++] =
 				CStringGetTextDatum("ldaptls=true");
