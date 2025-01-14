a3b709cf73e0ae2be1259afec9994ff30416f695
Tom Lane <tgl@sss.pgh.pa.us>
2025-01-14 18:50:24 -0500

    Avoid symbol collisions between pqsignal.c and legacy-pqsignal.c.
    In the name of ABI stability (that is, to avoid a library major
    version bump for libpq), libpq still exports a version of pqsignal()
    that we no longer want to use ourselves.  However, since that has
    the same link name as the function exported by src/port/pqsignal.c,
    there is a link ordering dependency determining which version will
    actually get used by code that uses libpq as well as libpgport.a.
    
    It now emerges that the wrong version has been used by pgbench and
    psql since commit 06843df4a rearranged their link commands.  This
    can result in odd failures in pgbench with the -T switch, since its
    SIGALRM handler will now not be marked SA_RESTART.  psql may have
    some edge-case problems in \watch, too.
    
    Since we don't want to depend on link ordering effects anymore,
    let's fix this in the same spirit as b6c7cfac8: use macros to change
    the actual link names of the competing functions.  We cannot change
    legacy-pqsignal.c's exported name of course, so the victim has to be
    src/port/pqsignal.c.
    
    In master, rename its exported name to be pqsignal_fe in frontend or
    pqsignal_be in backend.  (We could perhaps have gotten away with using
    the same symbol in both cases, but since the FE and BE versions now
    work a little differently, it seems advisable to use different names.)
    
    In back branches, rename to pqsignal_fe in frontend but keep it as
    pqsignal in backend.  The frontend change could affect third-party
    code that is calling pqsignal from libpgport.a or libpgport_shlib.a,
    but only if the code is compiled against port.h from a different minor
    release than libpgport.  Since we don't support using libpgport as a
    shared library, it seems unlikely that there will be such a problem.
    I left the backend symbol unchanged to avoid an ABI break for
    extensions.  This means that the link ordering hazard still exists
    for any extension that links against libpq.  However, none of our own
    extensions use both pqsignal() and libpq, and we're not making things
    any worse for third-party extensions that do.
    
    Report from Andy Fan, diagnosis by Fujii Masao, patch by me.
    Back-patch to all supported branches, as 06843df4a was.
    
    Discussion: https://postgr.es/m/87msfz5qv2.fsf@163.com

--- src/include/port.h
+++ src/include/port.h
@@ -564,7 +564,10 @@ extern int	pg_check_dir(const char *dir);
 /* port/pgmkdirp.c */
 extern int	pg_mkdir_p(char *path, int omode);
 
-/* port/pqsignal.c */
+/* port/pqsignal.c (see also interfaces/libpq/legacy-pqsignal.c) */
+#ifdef FRONTEND
+#define pqsignal pqsignal_fe
+#endif
 typedef void (*pqsigfunc) (int signo);
 extern pqsigfunc pqsignal(int signo, pqsigfunc func);
 
