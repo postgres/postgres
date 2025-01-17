0671a71e0210f7a3be55af240e9e7bbbea3cb44d
Tom Lane <tgl@sss.pgh.pa.us>
2025-01-16 20:40:07 -0500

    Fix setrefs.c's failure to do expression processing on prune steps.
    We should run the expression subtrees of PartitionedRelPruneInfo
    structs through fix_scan_expr.  Failure to do so means that
    AlternativeSubPlans within those expressions won't be cleaned up
    properly, resulting in "unrecognized node type" errors since v14.
    
    It seems fairly likely that at least some of the other steps done
    by fix_scan_expr are important here as well, resulting in as-yet-
    undetected bugs.  Therefore, I've chosen to back-patch this to
    all supported branches including v13, even though the known
    symptom doesn't manifest in v13.
    
    Per bug #18778 from Alexander Lakhin.
    
    Discussion: https://postgr.es/m/18778-24cd399df6c806af@postgresql.org

--- src/backend/optimizer/plan/setrefs.c
+++ src/backend/optimizer/plan/setrefs.c
@@ -1794,6 +1794,12 @@ set_append_references(PlannerInfo *root,
 				PartitionedRelPruneInfo *pinfo = lfirst(l2);
 
 				pinfo->rtindex += rtoffset;
+				pinfo->initial_pruning_steps =
+					fix_scan_list(root, pinfo->initial_pruning_steps,
+								  rtoffset, 1);
+				pinfo->exec_pruning_steps =
+					fix_scan_list(root, pinfo->exec_pruning_steps,
+								  rtoffset, 1);
 			}
 		}
 	}
@@ -1870,6 +1876,12 @@ set_mergeappend_references(PlannerInfo *root,
 				PartitionedRelPruneInfo *pinfo = lfirst(l2);
 
 				pinfo->rtindex += rtoffset;
+				pinfo->initial_pruning_steps =
+					fix_scan_list(root, pinfo->initial_pruning_steps,
+								  rtoffset, 1);
+				pinfo->exec_pruning_steps =
+					fix_scan_list(root, pinfo->exec_pruning_steps,
+								  rtoffset, 1);
 			}
 		}
 	}
