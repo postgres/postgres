998c4fc7c2c89abad4171f78f9375bd31713b5ca
Tom Lane <tgl@sss.pgh.pa.us>
2025-01-29 14:24:36 -0500

    Avoid breaking SJIS encoding while de-backslashing Windows paths.
    When running on Windows, canonicalize_path() converts '\' to '/'
    to prevent confusing the Windows command processor.  It was
    doing that in a non-encoding-aware fashion; but in SJIS there
    are valid two-byte characters whose second byte matches '\'.
    So encoding corruption ensues if such a character is used in
    the path.
    
    We can fairly easily fix this if we know which encoding is
    in use, but a lot of our utilities don't have much of a clue
    about that.  After some discussion we decided we'd settle for
    fixing this only in psql, and assuming that its value of
    client_encoding matches what the user is typing.
    
    It seems hopeless to get the server to deal with the problematic
    characters in database path names, so we'll just declare that
    case to be unsupported.  That means nothing need be done in
    the server, nor in utility programs whose only contact with
    file path names is for database paths.  But psql frequently
    deals with client-side file paths, so it'd be good if it
    didn't mess those up.
    
    Bug: #18735
    Reported-by: Koichi Suzuki <koichi.suzuki@enterprisedb.com>
    Author: Tom Lane <tgl@sss.pgh.pa.us>
    Reviewed-by: Koichi Suzuki <koichi.suzuki@enterprisedb.com>
    Discussion: https://postgr.es/m/18735-4acdb3998bb9f2b1@postgresql.org
    Backpatch-through: 13

--- src/bin/psql/command.c
+++ src/bin/psql/command.c
@@ -1107,7 +1107,7 @@ exec_command_edit(PsqlScanState scan_state, bool active_branch,
 				expand_tilde(&fname);
 				if (fname)
 				{
-					canonicalize_path(fname);
+					canonicalize_path_enc(fname, pset.encoding);
 					/* Always clear buffer if the file isn't modified */
 					discard_on_quit = true;
 				}
@@ -2701,7 +2701,7 @@ exec_command_write(PsqlScanState scan_state, bool active_branch,
 				}
 				else
 				{
-					canonicalize_path(fname);
+					canonicalize_path_enc(fname, pset.encoding);
 					fd = fopen(fname, "w");
 				}
 				if (!fd)
@@ -4209,7 +4209,7 @@ process_file(char *filename, bool use_relative_path)
 	}
 	else if (strcmp(filename, "-") != 0)
 	{
-		canonicalize_path(filename);
+		canonicalize_path_enc(filename, pset.encoding);
 
 		/*
 		 * If we were asked to resolve the pathname relative to the location
@@ -4223,7 +4223,7 @@ process_file(char *filename, bool use_relative_path)
 			strlcpy(relpath, pset.inputfile, sizeof(relpath));
 			get_parent_directory(relpath);
 			join_path_components(relpath, relpath, filename);
-			canonicalize_path(relpath);
+			canonicalize_path_enc(relpath, pset.encoding);
 
 			filename = relpath;
 		}
