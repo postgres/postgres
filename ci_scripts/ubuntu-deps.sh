71bac8ded65d891dccc64eafd49d20d02324b49e
Andreas Karlsson <andreas.karlsson@percona.com>
2025-02-10 17:14:48 +0100

    Do not de-armor key used by apt
    Apt does not require keys to be de-armored if they use the correct
    extension. Additional put the key in the directory recommended by
    Debian and Ubuntu.

--- ci_scripts/ubuntu-deps.sh
+++ ci_scripts/ubuntu-deps.sh
@@ -3,7 +3,6 @@
 DEPS=(
     # Setup
     wget
-    gpg
     # Build
     bison
     docbook-xsl
@@ -46,6 +45,6 @@ sudo apt-get install -y ${DEPS[@]}
 bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
 
 # Vault
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+wget -O - https://apt.releases.hashicorp.com/gpg | sudo tee /etc/apt/keyrings/hashicorp-archive-keyring.asc
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/hashicorp-archive-keyring.asc] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt-get update && sudo apt-get install -y vault
