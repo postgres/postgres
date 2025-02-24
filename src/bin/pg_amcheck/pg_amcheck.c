e889422d98e066dcf095fa275fbcb14ffd45708c
Daniel Gustafsson <dgustafsson@postgresql.org>
2025-02-24 16:03:19 +0100

    pg_amcheck: PQclear query results
    While the potential memory leak is small, ensure to PQclear the query
    results before disconnecting.
    
    Author: Jiao Shuntian <312199339@qq.com>
    Reviewed-by: Daniel Gustafsson <daniel@yesql.se>
    Discussion: https://postgr.es/m/tencent_F34922C91C41E76C734773E767C9FBDB9906@qq.com

--- src/bin/pg_amcheck/pg_amcheck.c
+++ src/bin/pg_amcheck/pg_amcheck.c
@@ -587,6 +587,7 @@ main(int argc, char *argv[])
 			/* Querying the catalog succeeded, but amcheck is missing. */
 			pg_log_warning("skipping database \"%s\": amcheck is not installed",
 						   PQdb(conn));
+			PQclear(result);
 			disconnectDatabase(conn);
 			conn = NULL;
 			continue;
