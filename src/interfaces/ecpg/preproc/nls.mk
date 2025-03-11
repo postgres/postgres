12c5f797ea6a8e96de661e3838410b9775061796
Peter Eisentraut <peter@eisentraut.org>
2025-03-11 13:42:03 +0100

    Update nls.mk for newly added file
    Commit f18231e8175 moved some code to a new file, but the new file
    wasn't added to nls.mk.

--- src/interfaces/ecpg/preproc/nls.mk
+++ src/interfaces/ecpg/preproc/nls.mk
@@ -1,5 +1,5 @@
 # src/interfaces/ecpg/preproc/nls.mk
 CATALOG_NAME     = ecpg
-GETTEXT_FILES    = descriptor.c ecpg.c pgc.c preproc.c type.c variable.c
+GETTEXT_FILES    = descriptor.c ecpg.c pgc.c preproc.c type.c util.c variable.c
 GETTEXT_TRIGGERS = mmerror:3 mmfatal:2
 GETTEXT_FLAGS    = mmerror:3:c-format mmfatal:2:c-format
