postgres/postgres

commit 9e088f7dd8cd1f14813d0919a68564d55cbff18a

Fix compiler warning in pg_logicalinspect.
Oversight in bd65cb3cd48.

Reported-by: David Rowley <dgrowleyml@gmail.com>
Reported-by: Nathan Bossart <nathandbossart@gmail.com>
Author: David Rowley <dgrowleyml@gmail.com>
Discussion: https://postgr.es/m/CAApHDvqrhFfnetbcwgGkJ=z63T8HfQ_OyP=vX8BYiXyxFKt67w@mail.gmail.com

contrib/pg_logicalinspect/pg_logicalinspect.c
@@ -85,6 +85,8 @@ parse_snapshot_filename(const char *filename)
 parse_error:
 	ereport(ERROR,
 			errmsg("invalid snapshot file name \"%s\"", filename));
+
+	return InvalidXLogRecPtr;					/* keep compiler quiet */
 }
 
 /*

commit b955df443405e056fd9047ef819a1465654f9d79

Fix indentation issue
Introduced recently by 9e088f7dd

Per buildfarm member koel

contrib/pg_logicalinspect/pg_logicalinspect.c
@@ -86,7 +86,7 @@ parse_error:
 	ereport(ERROR,
 			errmsg("invalid snapshot file name \"%s\"", filename));
 
-	return InvalidXLogRecPtr;					/* keep compiler quiet */
+	return InvalidXLogRecPtr;	/* keep compiler quiet */
 }
 
 /*