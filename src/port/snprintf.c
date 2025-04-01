6da2ba1d8a031984eb016fed6741bb2ac945f19d
Tom Lane <tgl@sss.pgh.pa.us>
2025-04-01 16:49:51 -0400

    Fix detection and handling of strchrnul() for macOS 15.4.
    As of 15.4, macOS has strchrnul(), but access to it is blocked behind
    a check for MACOSX_DEPLOYMENT_TARGET >= 15.4.  But our does-it-link
    configure check finds it, so we try to use it, and fail with the
    present default deployment target (namely 15.0).  This accounts for
    today's buildfarm failures on indri and sifaka.
    
    This is the identical problem that we faced some years ago when Apple
    introduced preadv and pwritev in the same way.  We solved that in
    commit f014b1b9b by using AC_CHECK_DECLS instead of AC_CHECK_FUNCS
    to check the functions' availability.  So do the same now for
    strchrnul().  Interestingly, we already had a workaround for
    "the link check doesn't agree with <string.h>" cases with glibc,
    which we no longer need since only the header declaration is being
    checked.
    
    Testing this revealed that the meson version of this check has never
    worked, because it failed to use "-Werror=unguarded-availability-new".
    (Apparently nobody's tried to build with meson on macOS versions that
    lack preadv/pwritev as standard.)  Adjust that while at it.  Also,
    we had never put support for "-Werror=unguarded-availability-new"
    into v13, but we need that now.
    
    Co-authored-by: Tom Lane <tgl@sss.pgh.pa.us>
    Co-authored-by: Peter Eisentraut <peter@eisentraut.org>
    Discussion: https://postgr.es/m/385134.1743523038@sss.pgh.pa.us
    Backpatch-through: 13

--- src/port/snprintf.c
+++ src/port/snprintf.c
@@ -338,13 +338,22 @@ static void leading_pad(int zpad, int signvalue, int *padlen,
 static void trailing_pad(int padlen, PrintfTarget *target);
 
 /*
- * If strchrnul exists (it's a glibc-ism), it's a good bit faster than the
- * equivalent manual loop.  If it doesn't exist, provide a replacement.
+ * If strchrnul exists (it's a glibc-ism, but since adopted by some other
+ * platforms), it's a good bit faster than the equivalent manual loop.
+ * Use it if possible, and if it doesn't exist, use this replacement.
  *
  * Note: glibc declares this as returning "char *", but that would require
  * casting away const internally, so we don't follow that detail.
+ *
+ * Note: macOS has this too as of Sequoia 15.4, but it's hidden behind
+ * a deployment-target check that causes compile errors if the deployment
+ * target isn't high enough.  So !HAVE_DECL_STRCHRNUL may mean "yes it's
+ * declared, but it doesn't compile".  To avoid failing in that scenario,
+ * use a macro to avoid matching <string.h>'s name.
  */
-#ifndef HAVE_STRCHRNUL
+#if !HAVE_DECL_STRCHRNUL
+
+#define strchrnul pg_strchrnul
 
 static inline const char *
 strchrnul(const char *s, int c)
@@ -354,19 +363,7 @@ strchrnul(const char *s, int c)
 	return s;
 }
 
-#else
-
-/*
- * glibc's <string.h> declares strchrnul only if _GNU_SOURCE is defined.
- * While we typically use that on glibc platforms, configure will set
- * HAVE_STRCHRNUL whether it's used or not.  Fill in the missing declaration
- * so that this file will compile cleanly with or without _GNU_SOURCE.
- */
-#ifndef _GNU_SOURCE
-extern char *strchrnul(const char *s, int c);
-#endif
-
-#endif							/* HAVE_STRCHRNUL */
+#endif							/* !HAVE_DECL_STRCHRNUL */
 
 
 /*
