748e98d05b752e05c0f860f35f17bb0ba83631ed
Peter Eisentraut <peter@eisentraut.org>
2025-04-02 14:34:24 +0200

    Fix code comment
    The changes made in commit d2b4b4c2259 contained incorrect comments:
    They said that certain forward declarations were necessary to "avoid
    including pathnodes.h here", but the file is itself pathnodes.h!  So
    change the comment to just say it's a forward declaration in one case,
    and in the other case we don't need the declaration at all because it
    already appeared earlier in the file.

--- src/include/nodes/pathnodes.h
+++ src/include/nodes/pathnodes.h
@@ -1134,8 +1134,7 @@ typedef struct IndexOptInfo IndexOptInfo;
 #define HAVE_INDEXOPTINFO_TYPEDEF 1
 #endif
 
-struct IndexPath;				/* avoid including pathnodes.h here */
-struct PlannerInfo;				/* avoid including pathnodes.h here */
+struct IndexPath;				/* forward declaration */
 
 struct IndexOptInfo
 {
