2131faf17d6861d65c554c36a8d1d83d2ff0f58f
Andreas Karlsson <andreas.karlsson@percona.com>
2025-04-11 04:50:56 +0200

    Use PostgreSQL's find_typedef script instead of our own
    Since the PostgreSQL repo already contains a script extracting typedefs
    from object files let's use it.

--- ci_scripts/dump-typedefs.sh
+++ ci_scripts/dump-typedefs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd -- "$(dirname "$0")" >/dev/null 2>&1; pwd -P)"
-cd "$SCRIPT_DIR/../"
+cd "$SCRIPT_DIR/.."
 
 if ! test -f src/backend/postgres; then
   echo "src/backend/postgres doesn't exists, run make-build.sh first in debug mode"
@@ -13,21 +13,7 @@ if ! test -f contrib/pg_tde/pg_tde.so; then
   exit 1
 fi
 
-objdump -W src/backend/postgres |\
-    egrep -A3 DW_TAG_typedef |\
-    perl -e ' while (<>) { chomp; @flds = split;next unless (1 < @flds);\
-        next if $flds[0]  ne "DW_AT_name" && $flds[1] ne "DW_AT_name";\
-        next if $flds[-1] =~ /^DW_FORM_str/;\
-        print $flds[-1],"\n"; }'  |\
-    sort | uniq > percona.typedefs
-
-objdump -W contrib/pg_tde/pg_tde.so |\
-    egrep -A3 DW_TAG_typedef |\
-    perl -e ' while (<>) { chomp; @flds = split;next unless (1 < @flds);\
-        next if $flds[0]  ne "DW_AT_name" && $flds[1] ne "DW_AT_name";\
-        next if $flds[-1] =~ /^DW_FORM_str/;\
-        print $flds[-1],"\n"; }'  |\
-    sort | uniq > tde.typedefs
+src/tools/find_typedef src/backend contrib/pg_tde > percona.typedefs
 
 # Combine with original typedefs
-cat percona.typedefs tde.typedefs src/tools/pgindent/typedefs.list | sort | uniq > combined.typedefs
+cat percona.typedefs src/tools/pgindent/typedefs.list | sort -u > combined.typedefs
