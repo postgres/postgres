81a4b01e64a881a395bf722eb1d9af88b4123bb7
pmp-p <mail.peny@free.fr>
2025-04-29 14:58:32 +0200

    paths

--- wasm-build/build-with-docker.sh
+++ wasm-build/build-with-docker.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export WORKSPACE=${GITHUB_WORKSPACE:-/workspace}
-
 # we are using a custom emsdk to build pglite wasm
 # this is available as a docker image under electricsql/pglite-builder
 IMG_NAME="electricsql/pglite-builder"
@@ -15,14 +13,18 @@ cat .buildconfig
 
 [ -f postgres-pglite/configure ] || ln -s . postgres-pglite
 
+cd $(realpath ${WORKSPACE}/postgres-pglite)
+
+export WORKSPACE=${GITHUB_WORKSPACE:-$(pwd)}
+
 mkdir -p dist/pglite dist/extensions-emsdk
 
 docker run \
   --rm \
   --env-file .buildconfig \
   --workdir=/workspace \
-  -v ${WORKSPACE}:/workspace:rw \
-  -v ${WORKSPACE}/dist:/tmp/sdk/dist:rw \
+  -v ${WORKSPACE}/postgres-pglite:/workspace:rw \
+  -v ${WORKSPACE}/postgres-pglite/dist:/tmp/sdk/dist:rw \
   $IMG_NAME:$IMG_TAG \
   bash -c "source ${SDKROOT}/wasm32-bi-emscripten-shell.sh && ./wasm-build.sh ${WHAT:-\"contrib extra\"}"
 
