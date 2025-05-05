3f2ab73934ab1e27151ecd14fd7d8ef602555093
Noah Misch <noah@leadboat.com>
2025-05-05 04:52:04 -0700

    With GB18030, prevent SIGSEGV from reading past end of allocation.
    With GB18030 as source encoding, applications could crash the server via
    SQL functions convert() or convert_from().  Applications themselves
    could crash after passing unterminated GB18030 input to libpq functions
    PQescapeLiteral(), PQescapeIdentifier(), PQescapeStringConn(), or
    PQescapeString().  Extension code could crash by passing unterminated
    GB18030 input to jsonapi.h functions.  All those functions have been
    intended to handle untrusted, unterminated input safely.
    
    A crash required allocating the input such that the last byte of the
    allocation was the last byte of a virtual memory page.  Some malloc()
    implementations take measures against that, making the SIGSEGV hard to
    reach.  Back-patch to v13 (all supported versions).
    
    Author: Noah Misch <noah@leadboat.com>
    Author: Andres Freund <andres@anarazel.de>
    Reviewed-by: Masahiko Sawada <sawada.mshk@gmail.com>
    Backpatch-through: 13
    Security: CVE-2025-4207

--- src/interfaces/libpq/fe-exec.c
+++ src/interfaces/libpq/fe-exec.c
@@ -3980,7 +3980,8 @@ PQescapeStringInternal(PGconn *conn,
 		}
 
 		/* Slow path for possible multibyte characters */
-		charlen = pg_encoding_mblen(encoding, source);
+		charlen = pg_encoding_mblen_or_incomplete(encoding,
+												  source, remaining);
 
 		if (remaining < charlen ||
 			pg_encoding_verifymbchar(encoding, source, charlen) == -1)
@@ -4124,7 +4125,8 @@ PQescapeInternal(PGconn *conn, const char *str, size_t len, bool as_ident)
 			int			charlen;
 
 			/* Slow path for possible multibyte characters */
-			charlen = pg_encoding_mblen(conn->client_encoding, s);
+			charlen = pg_encoding_mblen_or_incomplete(conn->client_encoding,
+													  s, remaining);
 
 			if (charlen > remaining)
 			{
