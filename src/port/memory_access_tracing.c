f4bf17a3de045e425ecb2700095f3a98023133f2
Igor Demushkin <owltop@quasar-infra-dev.vla.yp-c.yandex.net>
2025-05-15 23:32:30 +0300

    some fixes

--- src/port/memory_access_tracing.c
+++ src/port/memory_access_tracing.c
@@ -9,13 +9,13 @@
 
 #pragma clang diagnostic ignored "-Wreserved-identifier"
 
+#define NO_SANITIZE __attribute__((no_sanitize("all")))
+
 
 int ENABLE_TRACE = 1;
 static int trace_fd = -1;
 
 int memory_access_counter = 0;
-
-#define NO_SANITIZE __attribute__((no_sanitize("all")))
 
 NO_SANITIZE
 __attribute__((constructor))
@@ -58,129 +58,128 @@ void resetMemoryAccessCount()
 }
 
 
-    NO_SANITIZE
-    void __sanitizer_cov_trace_pc()
-    {
-        // Just need this
-    }
+NO_SANITIZE
+void __sanitizer_cov_trace_pc()
+{
+    // Just need this
+}
 
-    NO_SANITIZE
-    void __sanitizer_cov_load1(uint8* addr)
-    {
-        if (ENABLE_TRACE == 1) {
-            ++memory_access_counter;
-            char buffer[128];
-            int len = snprintf(buffer, sizeof(buffer), "l1\t%lu\t%lu\t%lu\n", 
-                            addr, pthread_self(), __builtin_return_address(0));
-            write(trace_fd, buffer, len);
-        }
+NO_SANITIZE
+void __sanitizer_cov_load1(uint8* addr)
+{
+    if (ENABLE_TRACE == 1) {
+        ++memory_access_counter;
+        char buffer[128];
+        int len = snprintf(buffer, sizeof(buffer), "l1\t%lu\t%lu\t%lu\n", 
+                        addr, pthread_self(), __builtin_return_address(0));
+        write(trace_fd, buffer, len);
     }
+}
 
-    NO_SANITIZE
-    void __sanitizer_cov_load2(uint16 * addr)
-    {
-        if (ENABLE_TRACE == 1) {
-            ++memory_access_counter;
-            char buffer[128];
-            int len = snprintf(buffer, sizeof(buffer), "l2\t%lu\t%lu\t%lu\n", 
-                            addr, pthread_self(), __builtin_return_address(0));
-            write(trace_fd, buffer, len);
-        }
+NO_SANITIZE
+void __sanitizer_cov_load2(uint16 * addr)
+{
+    if (ENABLE_TRACE == 1) {
+        ++memory_access_counter;
+        char buffer[128];
+        int len = snprintf(buffer, sizeof(buffer), "l2\t%lu\t%lu\t%lu\n", 
+                        addr, pthread_self(), __builtin_return_address(0));
+        write(trace_fd, buffer, len);
     }
+}
 
-    NO_SANITIZE
-    void __sanitizer_cov_load4(uint32 * addr)
-    {
-        if (ENABLE_TRACE == 1) {
-            ++memory_access_counter;
-            char buffer[128];
-            int len = snprintf(buffer, sizeof(buffer), "l4\t%lu\t%lu\t%lu\n", 
-                            addr, pthread_self(), __builtin_return_address(0));
-            write(trace_fd, buffer, len);
-        }
+NO_SANITIZE
+void __sanitizer_cov_load4(uint32 * addr)
+{
+    if (ENABLE_TRACE == 1) {
+        ++memory_access_counter;
+        char buffer[128];
+        int len = snprintf(buffer, sizeof(buffer), "l4\t%lu\t%lu\t%lu\n", 
+                        addr, pthread_self(), __builtin_return_address(0));
+        write(trace_fd, buffer, len);
     }
+}
 
-    NO_SANITIZE
-    void __sanitizer_cov_load8(uint64 * addr)
-    {
-        if (ENABLE_TRACE == 1) {
-            ++memory_access_counter;
-            char buffer[128];
-            int len = snprintf(buffer, sizeof(buffer), "l8\t%lu\t%lu\t%lu\n",
-                            addr, pthread_self(), __builtin_return_address(0));
-            write(trace_fd, buffer, len);
-        }
-        printf("%d\n", memory_access_counter);
+NO_SANITIZE
+void __sanitizer_cov_load8(uint64 * addr)
+{
+    if (ENABLE_TRACE == 1) {
+        ++memory_access_counter;
+        char buffer[128];
+        int len = snprintf(buffer, sizeof(buffer), "l8\t%lu\t%lu\t%lu\n",
+                        addr, pthread_self(), __builtin_return_address(0));
+        write(trace_fd, buffer, len);
     }
+}
 
-    NO_SANITIZE
-    void __sanitizer_cov_load16(__int128 * addr)
-    {
-        if (ENABLE_TRACE == 1) {
-            ++memory_access_counter;
-            char buffer[128];
-            int len = snprintf(buffer, sizeof(buffer), "l16\t%lu\t%lu\t%lu\n", 
-                            addr, pthread_self(), __builtin_return_address(0));
-            write(trace_fd, buffer, len);
-        }
+NO_SANITIZE
+void __sanitizer_cov_load16(__int128 * addr)
+{
+    if (ENABLE_TRACE == 1) {
+        ++memory_access_counter;
+        char buffer[128];
+        int len = snprintf(buffer, sizeof(buffer), "l16\t%lu\t%lu\t%lu\n", 
+                        addr, pthread_self(), __builtin_return_address(0));
+        write(trace_fd, buffer, len);
     }
+}
 
-    NO_SANITIZE
-    void __sanitizer_cov_store1(uint8 * addr)
-    {
-        if (ENABLE_TRACE == 1) {
-            ++memory_access_counter;
-            char buffer[128];
-            int len = snprintf(buffer, sizeof(buffer), "s1\t%lu\t%lu\t%lu\n", 
-                            addr, pthread_self(), __builtin_return_address(0));
-            write(trace_fd, buffer, len);
-        }
+NO_SANITIZE
+void __sanitizer_cov_store1(uint8 * addr)
+{
+    if (ENABLE_TRACE == 1) {
+        ++memory_access_counter;
+        char buffer[128];
+        int len = snprintf(buffer, sizeof(buffer), "s1\t%lu\t%lu\t%lu\n", 
+                        addr, pthread_self(), __builtin_return_address(0));
+        write(trace_fd, buffer, len);
     }
+}
 
-    NO_SANITIZE
-    void __sanitizer_cov_store2(uint16 * addr)
-    {
-        if (ENABLE_TRACE == 1) {
-            ++memory_access_counter;
-            char buffer[128];
-            int len = snprintf(buffer, sizeof(buffer), "s2\t%lu\t%lu\t%lu\n", 
-                            addr, pthread_self(), __builtin_return_address(0));
-            write(trace_fd, buffer, len);
-        }
+NO_SANITIZE
+void __sanitizer_cov_store2(uint16 * addr)
+{
+    if (ENABLE_TRACE == 1) {
+        ++memory_access_counter;
+        char buffer[128];
+        int len = snprintf(buffer, sizeof(buffer), "s2\t%lu\t%lu\t%lu\n", 
+                        addr, pthread_self(), __builtin_return_address(0));
+        write(trace_fd, buffer, len);
     }
+}
 
-    NO_SANITIZE
-    void __sanitizer_cov_store4(uint32 * addr)
-    {
-        if (ENABLE_TRACE == 1) {
-            ++memory_access_counter;
-            char buffer[128];
-            int len = snprintf(buffer, sizeof(buffer), "s4\t%lu\t%lu\t%lu\n", 
-                            addr, pthread_self(), __builtin_return_address(0));
-            write(trace_fd, buffer, len);
-        }
+NO_SANITIZE
+void __sanitizer_cov_store4(uint32 * addr)
+{
+    if (ENABLE_TRACE == 1) {
+        ++memory_access_counter;
+        char buffer[128];
+        int len = snprintf(buffer, sizeof(buffer), "s4\t%lu\t%lu\t%lu\n", 
+                        addr, pthread_self(), __builtin_return_address(0));
+        write(trace_fd, buffer, len);
     }
+}
 
-    NO_SANITIZE
-    void __sanitizer_cov_store8(uint64 * addr)
-    {
-        if (ENABLE_TRACE == 1) {
-            ++memory_access_counter;
-            char buffer[128];
-            int len = snprintf(buffer, sizeof(buffer), "s8\t%lu\t%lu\t%lu\n", 
-                            addr, pthread_self(), __builtin_return_address(0));
-            write(trace_fd, buffer, len);
-        }
+NO_SANITIZE
+void __sanitizer_cov_store8(uint64 * addr)
+{
+    if (ENABLE_TRACE == 1) {
+        ++memory_access_counter;
+        char buffer[128];
+        int len = snprintf(buffer, sizeof(buffer), "s8\t%lu\t%lu\t%lu\n", 
+                        addr, pthread_self(), __builtin_return_address(0));
+        write(trace_fd, buffer, len);
     }
+}
 
-    NO_SANITIZE
-    void __sanitizer_cov_store16(__int128 * addr)
-    {
-        if (ENABLE_TRACE == 1) {
-            ++memory_access_counter;
-            char buffer[128];
-            int len = snprintf(buffer, sizeof(buffer), "s16\t%lu\t%lu\t%lu\n", 
-                            addr, pthread_self(), __builtin_return_address(0));
-            write(trace_fd, buffer, len);
-        }
+NO_SANITIZE
+void __sanitizer_cov_store16(__int128 * addr)
+{
+    if (ENABLE_TRACE == 1) {
+        ++memory_access_counter;
+        char buffer[128];
+        int len = snprintf(buffer, sizeof(buffer), "s16\t%lu\t%lu\t%lu\n", 
+                        addr, pthread_self(), __builtin_return_address(0));
+        write(trace_fd, buffer, len);
     }
+}
