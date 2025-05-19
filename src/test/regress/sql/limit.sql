0420b24fee5e6483e1aa032f75edf340c14d4109
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2025-05-19 18:50:26 +0300

    Fix deparsing FETCH FIRST <expr> ROWS WITH TIES
    In the grammar, <expr> is a c_expr, which accepts only a limited set
    of integer literals and simple expressions without parens. The
    deparsing logic didn't quite match the grammar rule, and failed to use
    parens e.g. for "5::bigint".
    
    To fix, always surround the expression with parens. Would be nice to
    omit the parens in simple cases, but unfortunately it's non-trivial to
    detect such simple cases. Even if the expression is a simple literal
    123 in the original query, after parse analysis it becomes a FuncExpr
    with COERCE_IMPLICIT_CAST rather than a simple Const.
    
    Reported-by: yonghao lee
    Backpatch-through: 13
    Discussion: https://www.postgresql.org/message-id/18929-077d6b7093b176e2@postgresql.org

--- src/test/regress/sql/limit.sql
+++ src/test/regress/sql/limit.sql
@@ -196,6 +196,9 @@ CREATE VIEW limit_thousand_v_3 AS SELECT thousand FROM onek WHERE thousand < 995
 		ORDER BY thousand FETCH FIRST (NULL+1) ROWS WITH TIES;
 \d+ limit_thousand_v_3
 CREATE VIEW limit_thousand_v_4 AS SELECT thousand FROM onek WHERE thousand < 995
-		ORDER BY thousand FETCH FIRST NULL ROWS ONLY;
+		ORDER BY thousand FETCH FIRST (5::bigint) ROWS WITH TIES;
 \d+ limit_thousand_v_4
+CREATE VIEW limit_thousand_v_5 AS SELECT thousand FROM onek WHERE thousand < 995
+		ORDER BY thousand FETCH FIRST NULL ROWS ONLY;
+\d+ limit_thousand_v_5
 -- leave these views
