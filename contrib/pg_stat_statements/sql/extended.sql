7e8b44f4e0e630971d912628f07a38a7d3a6fae8
Michael Paquier <michael@paquier.xyz>
2025-05-29 11:26:27 +0900

    pg_stat_statements: Fix parameter number gaps in normalized queries
    pg_stat_statements anticipates that certain constant locations may be
    recorded multiple times and attempts to avoid calculating a length for
    these locations in fill_in_constant_lengths().
    
    However, during generate_normalized_query() where normalized query
    strings are generated, these locations are not excluded from
    consideration.  This could increment the parameter number counter for
    every recorded occurrence at such a location, leading to an incorrect
    normalization in certain cases with gaps in the numbers reported.
    
    For example, take this query:
    SELECT WHERE '1' IN ('2'::int, '3'::int::text)
    Before this commit, it would be normalized like that, with gaps in the
    parameter numbers:
    SELECT WHERE $1 IN ($3::int, $4::int::text)
    However the correct, less confusing one should be like that:
    SELECT WHERE $1 IN ($2::int, $3::int::text)
    
    This commit fixes the computation of the parameter numbers to track the
    number of constants replaced with an $n by a separate counter instead of
    the iterator used to loop through the list of locations.
    
    The underlying query IDs are not changed, neither are the normalized
    strings for existing PGSS hash entries.  New entries with fresh
    normalized queries would automatically get reshaped based on the new
    parameter numbering.
    
    Issue discovered while discussing a separate problem for HEAD, but this
    affects all the stable branches.
    
    Author: Sami Imseih <samimseih@gmail.com>
    Discussion: https://postgr.es/m/CAA5RZ0tzxvWXsacGyxrixdhy3tTTDfJQqxyFBRFh31nNHBQ5qA@mail.gmail.com
    Backpatch-through: 13

--- contrib/pg_stat_statements/sql/extended.sql
+++ contrib/pg_stat_statements/sql/extended.sql
@@ -5,3 +5,19 @@ SET pg_stat_statements.track_utility = FALSE;
 -- This test checks that an execute message sets a query ID.
 SELECT query_id IS NOT NULL AS query_id_set
   FROM pg_stat_activity WHERE pid = pg_backend_pid() \bind \g
+
+-- Various parameter numbering patterns
+SELECT pg_stat_statements_reset() IS NOT NULL AS t;
+-- Unique query IDs with parameter numbers switched.
+SELECT WHERE ($1::int, 7) IN ((8, $2::int), ($3::int, 9)) \bind '1' '2' '3' \g
+SELECT WHERE ($2::int, 10) IN ((11, $3::int), ($1::int, 12)) \bind '1' '2' '3' \g
+SELECT WHERE $1::int IN ($2::int, $3::int) \bind '1' '2' '3' \g
+SELECT WHERE $2::int IN ($3::int, $1::int) \bind '1' '2' '3' \g
+SELECT WHERE $3::int IN ($1::int, $2::int) \bind '1' '2' '3' \g
+-- Two groups of two queries with the same query ID.
+SELECT WHERE '1'::int IN ($1::int, '2'::int) \bind '1' \g
+SELECT WHERE '4'::int IN ($1::int, '5'::int) \bind '2' \g
+SELECT WHERE $2::int IN ($1::int, '1'::int) \bind '1' '2' \g
+SELECT WHERE $2::int IN ($1::int, '2'::int) \bind '3' '4' \g
+
+SELECT query, calls FROM pg_stat_statements ORDER BY query COLLATE "C";
