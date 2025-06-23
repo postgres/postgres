43da394304fba820830da2cef2c0214fe292c037
John Naylor <john.naylor@postgresql.org>
2025-06-23 18:03:56 +0700

    Properly fix AVX-512 CRC calculation bug
    The problem that led to the workaround in f83f14881c7 was not in fact
    a compiler bug, but a failure to zero the upper bits of the vector
    register containing the initial scalar CRC value. Fix that and revert
    the workaround.
    
    Diagnosed-by: Nathan Bossart <nathandbossart@gmail.com>
    Diagnosed-by: Raghuveer Devulapalli <raghuveer.devulapalli@intel.com>
    Tested-by: Andy Fan <zhihuifan1213@163.com>
    Tested-by: Soumyadeep Chakraborty <soumyadeep2007@gmail.com>
    Reviewed-by: Nathan Bossart <nathandbossart@gmail.com>
    Reviewed-by: Raghuveer Devulapalli <raghuveer.devulapalli@intel.com>
    Discussion: https://postgr.es/m/PH8PR11MB82866B07AA6758D12F699C00FB70A@PH8PR11MB8286.namprd11.prod.outlook.com

--- src/port/pg_crc32c_sse42.c
+++ src/port/pg_crc32c_sse42.c
@@ -123,7 +123,7 @@ pg_comp_crc32c_avx512(pg_crc32c crc, const void *data, size_t len)
 		__m512i		k;
 
 		k = _mm512_broadcast_i32x4(_mm_setr_epi32(0x740eef02, 0, 0x9e4addf8, 0));
-		x0 = _mm512_xor_si512(_mm512_castsi128_si512(_mm_cvtsi32_si128(crc0)), x0);
+		x0 = _mm512_xor_si512(_mm512_zextsi128_si512(_mm_cvtsi32_si128(crc0)), x0);
 		buf += 64;
 
 		/* Main loop. */
