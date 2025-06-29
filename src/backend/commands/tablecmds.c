25cab4473525cd3705fc8ee524b85721db967b65
Tom Lane <tgl@sss.pgh.pa.us>
2025-06-29 13:56:03 -0400

    Obtain required table lock during cross-table constraint updates.
    Sometimes a table's constraint may depend on a column of another
    table, so that we have to update the constraint when changing the
    referenced column's type.  We need to have lock on the constraint's
    table to do that.  ATPostAlterTypeCleanup believed that this case
    was only possible for FOREIGN KEY constraints, but it's wrong at
    least for CHECK and EXCLUDE constraints; and in general, we'd
    probably need exclusive lock to alter any sort of constraint.
    So just remove the contype check and acquire lock for any other
    table.  This prevents a "you don't have lock" assertion failure,
    though no ill effect is observed in production builds.
    
    We'll error out later anyway because we don't presently support
    physically altering column types within stored composite columns.
    But the catalog-munging is basically all there, so we may as well
    make that part work.
    
    Bug: #18970
    Reported-by: Alexander Lakhin <exclusion@gmail.com>
    Diagnosed-by: jian he <jian.universality@gmail.com>
    Author: Tom Lane <tgl@sss.pgh.pa.us>
    Discussion: https://postgr.es/m/18970-a7d1cfe1f8d5d8d9@postgresql.org
    Backpatch-through: 13

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -12894,9 +12894,12 @@ ATPostAlterTypeCleanup(List **wqueue, AlteredTableInfo *tab, LOCKMODE lockmode)
 	/*
 	 * Re-parse the index and constraint definitions, and attach them to the
 	 * appropriate work queue entries.  We do this before dropping because in
-	 * the case of a FOREIGN KEY constraint, we might not yet have exclusive
-	 * lock on the table the constraint is attached to, and we need to get
-	 * that before reparsing/dropping.
+	 * the case of a constraint on another table, we might not yet have
+	 * exclusive lock on the table the constraint is attached to, and we need
+	 * to get that before reparsing/dropping.  (That's possible at least for
+	 * FOREIGN KEY, CHECK, and EXCLUSION constraints; in non-FK cases it
+	 * requires a dependency on the target table's composite type in the other
+	 * table's constraint expressions.)
 	 *
 	 * We can't rely on the output of deparsing to tell us which relation to
 	 * operate on, because concurrent activity might have made the name
@@ -12912,7 +12915,6 @@ ATPostAlterTypeCleanup(List **wqueue, AlteredTableInfo *tab, LOCKMODE lockmode)
 		Form_pg_constraint con;
 		Oid			relid;
 		Oid			confrelid;
-		char		contype;
 		bool		conislocal;
 
 		tup = SearchSysCache1(CONSTROID, ObjectIdGetDatum(oldId));
@@ -12929,7 +12931,6 @@ ATPostAlterTypeCleanup(List **wqueue, AlteredTableInfo *tab, LOCKMODE lockmode)
 				elog(ERROR, "could not identify relation associated with constraint %u", oldId);
 		}
 		confrelid = con->confrelid;
-		contype = con->contype;
 		conislocal = con->conislocal;
 		ReleaseSysCache(tup);
 
@@ -12946,12 +12947,12 @@ ATPostAlterTypeCleanup(List **wqueue, AlteredTableInfo *tab, LOCKMODE lockmode)
 			continue;
 
 		/*
-		 * When rebuilding an FK constraint that references the table we're
-		 * modifying, we might not yet have any lock on the FK's table, so get
-		 * one now.  We'll need AccessExclusiveLock for the DROP CONSTRAINT
-		 * step, so there's no value in asking for anything weaker.
+		 * When rebuilding another table's constraint that references the
+		 * table we're modifying, we might not yet have any lock on the other
+		 * table, so get one now.  We'll need AccessExclusiveLock for the DROP
+		 * CONSTRAINT step, so there's no value in asking for anything weaker.
 		 */
-		if (relid != tab->relid && contype == CONSTRAINT_FOREIGN)
+		if (relid != tab->relid)
 			LockRelationOid(relid, AccessExclusiveLock);
 
 		ATPostAlterTypeParse(oldId, relid, confrelid,
