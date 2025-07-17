9dcd1aa815f5911bc7d526cdfee85ae493b5e12c
Tom Lane <tgl@sss.pgh.pa.us>
2025-07-17 12:46:38 -0400

    Fix PQport to never return NULL unless the connection is NULL.
    This is the documented behavior, and it worked that way before
    v10.  However, addition of the connhost[] array created cases
    where conn->connhost[conn->whichhost].port is NULL.  The rest
    of libpq is careful to substitute DEF_PGPORT[_STR] for a null
    or empty port string, but we failed to do so here, leading to
    possibly returning NULL.  As of v18 that causes psql's \conninfo
    command to segfault.  Older psql versions avoid that, but it's
    pretty likely that other clients have trouble with this,
    so we'd better back-patch the fix.
    
    In stable branches, just revert to our historical behavior of
    returning an empty string when there was no user-given port
    specification.  However, it seems substantially more useful and
    indeed more correct to hand back DEF_PGPORT_STR in such cases,
    so let's make v18 and master do that.
    
    Author: Daniele Varrazzo <daniele.varrazzo@gmail.com>
    Reviewed-by: Laurenz Albe <laurenz.albe@cybertec.at>
    Reviewed-by: Tom Lane <tgl@sss.pgh.pa.us>
    Discussion: https://postgr.es/m/CA+mi_8YTS8WPZPO0PAb2aaGLwHuQ0DEQRF0ZMnvWss4y9FwDYQ@mail.gmail.com
    Backpatch-through: 13

--- src/interfaces/libpq/fe-connect.c
+++ src/interfaces/libpq/fe-connect.c
@@ -6646,7 +6646,9 @@ PQport(const PGconn *conn)
 	if (!conn)
 		return NULL;
 
-	if (conn->connhost != NULL)
+	if (conn->connhost != NULL &&
+		conn->connhost[conn->whichhost].port != NULL &&
+		conn->connhost[conn->whichhost].port[0] != '\0')
 		return conn->connhost[conn->whichhost].port;
 
 	return "";
