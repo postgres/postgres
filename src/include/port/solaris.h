1ccb3851db5d29d6d6bd02f53c7e2020107ff73b
Tom Lane <tgl@sss.pgh.pa.us>
2025-07-23 15:44:29 -0400

    Fix build breakage on Solaris-alikes with late-model GCC.
    Solaris has never bothered to add "const" to the second argument
    of PAM conversation procs, as all other Unixen did decades ago.
    This resulted in an "incompatible pointer" compiler warning when
    building --with-pam, but had no more serious effect than that,
    so we never did anything about it.  However, as of GCC 14 the
    case is an error not warning by default.
    
    To complicate matters, recent OpenIndiana (and maybe illumos
    in general?) *does* supply the "const" by default, so we can't
    just assume that platforms using our solaris template need help.
    
    What we can do, short of building a configure-time probe,
    is to make solaris.h #define _PAM_LEGACY_NONCONST, which
    causes OpenIndiana's pam_appl.h to revert to the traditional
    definition, and hopefully will have no effect anywhere else.
    Then we can use that same symbol to control whether we include
    "const" in the declaration of pam_passwd_conv_proc().
    
    Bug: #18995
    Reported-by: Andrew Watkins <awatkins1966@gmail.com>
    Author: Tom Lane <tgl@sss.pgh.pa.us>
    Discussion: https://postgr.es/m/18995-82058da9ab4337a7@postgresql.org
    Backpatch-through: 13

--- src/include/port/solaris.h
+++ src/include/port/solaris.h
@@ -26,6 +26,15 @@
 #endif
 
 /*
+ * On original Solaris, PAM conversation procs lack a "const" in their
+ * declaration; but recent OpenIndiana versions put it there by default.
+ * The least messy way to deal with this is to define _PAM_LEGACY_NONCONST,
+ * which causes OpenIndiana to declare pam_conv per the Solaris tradition,
+ * and also use that symbol to control omitting the "const" in our own code.
+ */
+#define _PAM_LEGACY_NONCONST 1
+
+/*
  * Many versions of Solaris have broken strtod() --- see bug #4751182.
  * This has been fixed in current versions of Solaris:
  *
