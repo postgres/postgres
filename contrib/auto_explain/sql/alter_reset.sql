b998ce3272ab45877f760ff558906a967b6a7da6
Nathan Bossart <nathan@postgresql.org>
2025-08-01 16:52:11 -0500

    Allow resetting unknown custom GUCs with reserved prefixes.
    Currently, ALTER DATABASE/ROLE/SYSTEM RESET [ALL] with an unknown
    custom GUC with a prefix reserved by MarkGUCPrefixReserved() errors
    (unless a superuser runs a RESET ALL variant).  This is problematic
    for cases such as an extension library upgrade that removes a GUC.
    To fix, simply make sure the relevant code paths explicitly allow
    it.  Note that we require superuser or privileges on the parameter
    to reset it.  This is perhaps a bit more restrictive than is
    necessary, but it's not clear whether further relaxing the
    requirements is safe.
    
    Oversight in commit 88103567cb.  The ALTER SYSTEM fix is dependent
    on commit 2d870b4aef, which first appeared in v17.  Unfortunately,
    back-patching that commit would introduce ABI breakage, and while
    that breakage seems unlikely to bother anyone, it doesn't seem
    worth the risk.  Hence, the ALTER SYSTEM part of this commit is
    omitted on v15 and v16.
    
    Reported-by: Mert Alev <mert@futo.org>
    Reviewed-by: Laurenz Albe <laurenz.albe@cybertec.at>
    Discussion: https://postgr.es/m/18964-ba09dea8c98fccd6%40postgresql.org
    Backpatch-through: 15

---
+++ contrib/auto_explain/sql/alter_reset.sql
@@ -0,0 +1,20 @@
+--
+-- This tests resetting unknown custom GUCs with reserved prefixes.  There's
+-- nothing specific to auto_explain; this is just a convenient place to put
+-- this test.
+--
+
+SELECT current_database() AS datname \gset
+CREATE ROLE regress_ae_role;
+
+ALTER DATABASE :"datname" SET auto_explain.bogus = 1;
+ALTER ROLE regress_ae_role SET auto_explain.bogus = 1;
+ALTER ROLE regress_ae_role IN DATABASE :"datname" SET auto_explain.bogus = 1;
+
+LOAD 'auto_explain';
+
+ALTER DATABASE :"datname" RESET auto_explain.bogus;
+ALTER ROLE regress_ae_role RESET auto_explain.bogus;
+ALTER ROLE regress_ae_role IN DATABASE :"datname" RESET auto_explain.bogus;
+
+DROP ROLE regress_ae_role;
