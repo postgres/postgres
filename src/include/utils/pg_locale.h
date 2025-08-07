dd29262077e854199497a4aa42a776a2095eeec7
John Naylor <john.naylor@postgresql.org>
2025-08-07 17:12:44 +0700

    Update ICU C++ API symbols
    Recent ICU versions have added U_SHOW_CPLUSPLUS_HEADER_API, and we need
    to set this to zero as well to hide the ICU C++ APIs from pg_locale.h
    
    Per discussion, we want cpluspluscheck to work cleanly in backbranches,
    so backpatch both this and its predecessor commit ed26c4e25a4 to all
    supported versions.
    
    Reported-by: Tom Lane <tgl@sss.pgh.pa.us>
    Discussion: https://postgr.es/m/1115793.1754414782%40sss.pgh.pa.us
    Backpatch-through: 13

--- src/include/utils/pg_locale.h
+++ src/include/utils/pg_locale.h
@@ -13,6 +13,11 @@
 #define _PG_LOCALE_
 
 #ifdef USE_ICU
+/* only include the C APIs, to avoid errors in cpluspluscheck */
+#undef U_SHOW_CPLUSPLUS_API
+#define U_SHOW_CPLUSPLUS_API 0
+#undef U_SHOW_CPLUSPLUS_HEADER_API
+#define U_SHOW_CPLUSPLUS_HEADER_API 0
 #include <unicode/ucol.h>
 #endif
 
