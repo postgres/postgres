c70b6db34ffeab48beef1fb4ce61bcad3772b8dd
Dean Rasheed <dean.a.rasheed@gmail.com>
2025-09-05 08:21:35 +0100

    Fix concurrent update issue with MERGE.
    When executing a MERGE UPDATE action, if there is more than one
    concurrent update of the target row, the lock-and-retry code would
    sometimes incorrectly identify the latest version of the target tuple,
    leading to incorrect results.
    
    This was caused by using the ctid field from the TM_FailureData
    returned by table_tuple_lock() in a case where the result was TM_Ok,
    which is unsafe because the TM_FailureData struct is not guaranteed to
    be fully populated in that case. Instead, it should use the tupleid
    passed to (and updated by) table_tuple_lock().
    
    To reduce the chances of similar errors in the future, improve the
    commentary for table_tuple_lock() and TM_FailureData to make it
    clearer that table_tuple_lock() updates the tid passed to it, and most
    fields of TM_FailureData should not be relied on in non-failure cases.
    An exception to this is the "traversed" field, which is set in both
    success and failure cases.
    
    Reported-by: Dmitry <dsy.075@yandex.ru>
    Author: Yugo Nagata <nagata@sraoss.co.jp>
    Reviewed-by: Dean Rasheed <dean.a.rasheed@gmail.com>
    Reviewed-by: Chao Li <li.evan.chao@gmail.com>
    Discussion: https://postgr.es/m/1570d30e-2b95-4239-b9c3-f7bf2f2f8556@yandex.ru
    Backpatch-through: 15

--- src/include/access/tableam.h
+++ src/include/access/tableam.h
@@ -121,7 +121,9 @@ typedef enum TU_UpdateIndexes
 /*
  * When table_tuple_update, table_tuple_delete, or table_tuple_lock fail
  * because the target tuple is already outdated, they fill in this struct to
- * provide information to the caller about what happened.
+ * provide information to the caller about what happened. When those functions
+ * succeed, the contents of this struct should not be relied upon, except for
+ * `traversed`, which may be set in both success and failure cases.
  *
  * ctid is the target's ctid link: it is the same as the target's TID if the
  * target was deleted, or the location of the replacement tuple if the target
@@ -137,6 +139,9 @@ typedef enum TU_UpdateIndexes
  * tuple); otherwise cmax is zero.  (We make this restriction because
  * HeapTupleHeaderGetCmax doesn't work for tuples outdated in other
  * transactions.)
+ *
+ * traversed indicates if an update chain was followed in order to try to lock
+ * the target tuple.  (This may be set in both success and failure cases.)
  */
 typedef struct TM_FailureData
 {
@@ -1508,7 +1513,7 @@ table_tuple_update(Relation rel, ItemPointer otid, TupleTableSlot *slot,
  *
  * Input parameters:
  *	relation: relation containing tuple (caller must hold suitable lock)
- *	tid: TID of tuple to lock
+ *	tid: TID of tuple to lock (updated if an update chain was followed)
  *	snapshot: snapshot to use for visibility determinations
  *	cid: current command ID (used for visibility test, and stored into
  *		tuple's cmax if lock is successful)
@@ -1533,8 +1538,10 @@ table_tuple_update(Relation rel, ItemPointer otid, TupleTableSlot *slot,
  *	TM_WouldBlock: lock couldn't be acquired and wait_policy is skip
  *
  * In the failure cases other than TM_Invisible and TM_Deleted, the routine
- * fills *tmfd with the tuple's t_ctid, t_xmax, and, if possible, t_cmax.  See
- * comments for struct TM_FailureData for additional info.
+ * fills *tmfd with the tuple's t_ctid, t_xmax, and, if possible, t_cmax.
+ * Additionally, in both success and failure cases, tmfd->traversed is set if
+ * an update chain was followed.  See comments for struct TM_FailureData for
+ * additional info.
  */
 static inline TM_Result
 table_tuple_lock(Relation rel, ItemPointer tid, Snapshot snapshot,
