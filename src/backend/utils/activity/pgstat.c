3e6dfcfb0529a9d003e07a56f58d0ff3c46556bf
Michael Paquier <michael@paquier.xyz>
2025-09-08 15:52:51 +0900

    Fix corruption of pgstats shared hashtable due to OOM failures
    A new pgstats entry is created as a two-step process:
    - The entry is looked at in the shared hashtable of pgstats, and is
    inserted if not found.
    - When not found and inserted, its fields are then initialized.  This
    part include a DSA chunk allocation for the stats data of the new entry.
    
    As currently coded, if the DSA chunk allocation fails due to an
    out-of-memory failure, an ERROR is generated, leaving in the pgstats
    shared hashtable an inconsistent entry due to the first step, as the
    entry has already been inserted in the hashtable.  These broken entries
    can then be found by other backends, crashing them.
    
    There are only two callers of pgstat_init_entry(), when loading the
    pgstats file at startup and when creating a new pgstats entry.  This
    commit changes pgstat_init_entry() so as we use dsa_allocate_extended()
    with DSA_ALLOC_NO_OOM, making it return NULL on allocation failure
    instead of failing.  This way, a backend failing an entry creation can
    take appropriate cleanup actions in the shared hashtable before throwing
    an error.  Currently, this means removing the entry from the shared
    hashtable before throwing the error for the allocation failure.
    
    Out-of-memory errors unlikely happen in the wild, and we do not bother
    with back-patches when these are fixed, usually.  However, the problem
    dealt with here is a degree worse as it breaks the shared memory state
    of pgstats, impacting other processes that may look at an inconsistent
    entry that a different process has failed to create.
    
    Author: Mikhail Kot <mikhail.kot@databricks.com>
    Discussion: https://postgr.es/m/CAAi9E7jELo5_-sBENftnc2E8XhW2PKZJWfTC3i2y-GMQd2bcqQ@mail.gmail.com
    Backpatch-through: 15

--- src/backend/utils/activity/pgstat.c
+++ src/backend/utils/activity/pgstat.c
@@ -1649,6 +1649,16 @@ pgstat_read_statsfile(void)
 
 					header = pgstat_init_entry(key.kind, p);
 					dshash_release_lock(pgStatLocal.shared_hash, p);
+					if (header == NULL)
+					{
+						/*
+						 * It would be tempting to switch this ERROR to a
+						 * WARNING, but it would mean that all the statistics
+						 * are discarded when the environment fails on OOM.
+						 */
+						elog(ERROR,	"could not allocate entry %d/%u/%u",
+							 key.kind, key.dboid, key.objoid);
+					}
 
 					if (!read_chunk(fpin,
 									pgstat_get_entry_data(key.kind, header),
