97527a5e68d523048fd80aa1c97c3fe5bf948fb6
Fujii Masao <fujii@postgresql.org>
2025-09-16 10:37:38 +0900

    pg_restore: Fix comment handling with --no-publications / --no-subscriptions.
    Previously, pg_restore did not skip comments on publications or subscriptions
    even when --no-publications or --no-subscriptions was specified. As a result,
    it could issue COMMENT commands for objects that were never created,
    causing those commands to fail.
    
    This commit fixes the issue by ensuring that comments on publications and
    subscriptions are also skipped when the corresponding options are used.
    
    Backpatch to all supported versions.
    
    Author: Jian He <jian.universality@gmail.com>
    Co-authored-by: Fujii Masao <masao.fujii@gmail.com>
    Discussion: https://postgr.es/m/CACJufxHCt00pR9h51AVu6+yPD5J7JQn=7dQXxqacj0XyDhc-fA@mail.gmail.com
    Backpatch-through: 13

--- src/bin/pg_dump/t/002_pg_dump.pl
+++ src/bin/pg_dump/t/002_pg_dump.pl
@@ -548,6 +548,29 @@ my %pgdump_runs = (
 			'postgres',
 		],
 	},
+	no_subscriptions => {
+		dump_cmd => [
+			'pg_dump', '--no-sync',
+			'--file' => "$tempdir/no_subscriptions.sql",
+			'--no-subscriptions',
+			'postgres',
+		],
+	},
+	no_subscriptions_restore => {
+		dump_cmd => [
+			'pg_dump', '--no-sync',
+			'--format' => 'custom',
+			'--file' => "$tempdir/no_subscriptions_restore.dump",
+			'postgres',
+		],
+		restore_cmd => [
+			'pg_restore',
+			'--format' => 'custom',
+			'--file' => "$tempdir/no_subscriptions_restore.sql",
+			'--no-subscriptions',
+			"$tempdir/no_subscriptions_restore.dump",
+		],
+	},
 	no_table_access_method => {
 		dump_cmd => [
 			'pg_dump', '--no-sync',
@@ -722,6 +745,8 @@ my %full_runs = (
 	no_large_objects => 1,
 	no_owner => 1,
 	no_privs => 1,
+	no_subscriptions => 1,
+	no_subscriptions_restore => 1,
 	no_table_access_method => 1,
 	pg_dumpall_dbprivs => 1,
 	pg_dumpall_exclude => 1,
@@ -1546,6 +1571,10 @@ my %tests = (
 		regexp =>
 		  qr/^COMMENT ON SUBSCRIPTION sub1 IS 'comment on subscription';/m,
 		like => { %full_runs, section_post_data => 1, },
+		unlike => {
+			no_subscriptions => 1,
+			no_subscriptions_restore => 1,
+		},
 	},
 
 	'COMMENT ON TEXT SEARCH CONFIGURATION dump_test.alt_ts_conf1' => {
@@ -2988,6 +3017,10 @@ my %tests = (
 			\QCREATE SUBSCRIPTION sub1 CONNECTION 'dbname=doesnotexist' PUBLICATION pub1 WITH (connect = false, slot_name = 'sub1');\E
 			/xm,
 		like => { %full_runs, section_post_data => 1, },
+		unlike => {
+			no_subscriptions => 1,
+			no_subscriptions_restore => 1,
+		},
 	},
 
 	'CREATE SUBSCRIPTION sub2' => {
@@ -2999,6 +3032,10 @@ my %tests = (
 			\QCREATE SUBSCRIPTION sub2 CONNECTION 'dbname=doesnotexist' PUBLICATION pub1 WITH (connect = false, slot_name = 'sub2', origin = none);\E
 			/xm,
 		like => { %full_runs, section_post_data => 1, },
+		unlike => {
+			no_subscriptions => 1,
+			no_subscriptions_restore => 1,
+		},
 	},
 
 	'CREATE SUBSCRIPTION sub3' => {
@@ -3010,6 +3047,10 @@ my %tests = (
 			\QCREATE SUBSCRIPTION sub3 CONNECTION 'dbname=doesnotexist' PUBLICATION pub1 WITH (connect = false, slot_name = 'sub3');\E
 			/xm,
 		like => { %full_runs, section_post_data => 1, },
+		unlike => {
+			no_subscriptions => 1,
+			no_subscriptions_restore => 1,
+		},
 	},
 
 	'ALTER PUBLICATION pub1 ADD TABLE test_table' => {
@@ -3862,6 +3903,8 @@ my %tests = (
 			no_large_objects => 1,
 			no_privs => 1,
 			no_owner => 1,
+			no_subscriptions => 1,
+			no_subscriptions_restore => 1,
 			no_table_access_method => 1,
 			only_dump_test_schema => 1,
 			pg_dumpall_dbprivs => 1,
@@ -3954,6 +3997,8 @@ my %tests = (
 			no_large_objects => 1,
 			no_privs => 1,
 			no_owner => 1,
+			no_subscriptions => 1,
+			no_subscriptions_restore => 1,
 			no_table_access_method => 1,
 			pg_dumpall_dbprivs => 1,
 			pg_dumpall_exclude => 1,
