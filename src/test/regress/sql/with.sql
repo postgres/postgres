7df74e635e65e880c308d52e11d3d603aa1ee9c6
Tom Lane <tgl@sss.pgh.pa.us>
2025-09-17 16:32:57 -0400

    Calculate agglevelsup correctly when Aggref contains a CTE.
    If an aggregate function call contains a sub-select that has
    an RTE referencing a CTE outside the aggregate, we must treat
    that reference like a Var referencing the CTE's query level
    for purposes of determining the aggregate's level.  Otherwise
    we might reach the nonsensical conclusion that the aggregate
    should be evaluated at some query level higher than the CTE,
    ending in a planner error or a broken plan tree that causes
    executor failures.
    
    Bug: #19055
    Reported-by: BugForge <dllggyx@outlook.com>
    Author: Tom Lane <tgl@sss.pgh.pa.us>
    Discussion: https://postgr.es/m/19055-6970cfa8556a394d@postgresql.org
    Backpatch-through: 13

--- src/test/regress/sql/with.sql
+++ src/test/regress/sql/with.sql
@@ -1065,6 +1065,20 @@ select ( with cte(foo) as ( values(f1) )
 from int4_tbl;
 
 --
+-- test for bug #19055: interaction of WITH with aggregates
+--
+-- The reference to cte1 must determine the aggregate's level,
+-- even though it contains no Vars referencing cte1
+explain (verbose, costs off)
+select f1, (with cte1(x,y) as (select 1,2)
+            select count((select i4.f1 from cte1))) as ss
+from int4_tbl i4;
+
+select f1, (with cte1(x,y) as (select 1,2)
+            select count((select i4.f1 from cte1))) as ss
+from int4_tbl i4;
+
+--
 -- test for nested-recursive-WITH bug
 --
 WITH RECURSIVE t(j) AS (
