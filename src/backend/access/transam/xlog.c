53a2246ddf9b1e641654dd0654ff8f24e952920a
Daniel Gustafsson <dgustafsson@postgresql.org>
2025-09-25 15:27:33 +0200

    Remove preprocessor guards from injection points
    When defining an injection point there is no need to wrap the definition
    with USE_INJECTION_POINT guards, the INJECTION_POINT macro is available
    in all builds.  Remove to make the code consistent.
    
    Author: Hayato Kuroda <kuroda.hayato@fujitsu.com>
    Reviewed-by: Michael Paquier <michael@paquier.xyz>
    Reviewed-by: Daniel Gustafsson <daniel@yesql.se>
    Discussion: https://postgr.es/m/OSCPR01MB14966C8015DEB05ABEF2CE077F51FA@OSCPR01MB14966.jpnprd01.prod.outlook.com
    Backpatch-through: 17

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -7294,9 +7294,7 @@ CreateCheckPoint(int flags)
 	if (PriorRedoPtr != InvalidXLogRecPtr)
 		UpdateCheckPointDistanceEstimate(RedoRecPtr - PriorRedoPtr);
 
-#ifdef USE_INJECTION_POINTS
 	INJECTION_POINT("checkpoint-before-old-wal-removal");
-#endif
 
 	/*
 	 * Delete old log files, those no longer needed for last checkpoint to
