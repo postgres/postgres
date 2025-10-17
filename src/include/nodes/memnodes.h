86d118f9a60c6aafe1a6331fdf80455df24f32ae
David Rowley <drowley@postgresql.org>
2025-10-18 12:50:14 +1300

    Englishify comment wording
    Switch to using the English word here rather than using a verbified
    function name.
    
    The full word still fits within a single comment line, so it's probably
    better just to use that instead of trying to shorten it, which might
    cause confusion.
    
    Author: Rafia Sabih <rafia.pghackers@gmail.com>
    Discussion: https://postgr.es/m/CA+FpmFe7LnRF2NA_QfARjkSWme4mNt+Udwbh2Yb=zZm35Ji31w@mail.gmail.com

--- src/include/nodes/memnodes.h
+++ src/include/nodes/memnodes.h
@@ -120,7 +120,7 @@ typedef struct MemoryContextData
 
 	NodeTag		type;			/* identifies exact kind of context */
 	/* these two fields are placed here to minimize alignment wastage: */
-	bool		isReset;		/* T = no space alloced since last reset */
+	bool		isReset;		/* T = no space allocated since last reset */
 	bool		allowInCritSection; /* allow palloc in critical section */
 	Size		mem_allocated;	/* track memory allocated for this context */
 	const MemoryContextMethods *methods;	/* virtual function table */
