f2da499169279c5dcf0ae1acdb97ef7a4db222b4
Álvaro Herrera <alvherre@kurilemu.de>
2025-11-04 20:31:43 +0100

    Fix snapshot handling bug in recent BRIN fix
    Commit a95e3d84c0e0 added ActiveSnapshot push+pop when processing
    work-items (BRIN autosummarization), but forgot to handle the case of
    a transaction failing during the run, which drops the snapshot untimely.
    Fix by making the pop conditional on an element being actually there.
    
    Author: Álvaro Herrera <alvherre@kurilemu.de>
    Backpatch-through: 13
    Discussion: https://postgr.es/m/202511041648.nofajnuddmwk@alvherre.pgsql

--- src/backend/postmaster/autovacuum.c
+++ src/backend/postmaster/autovacuum.c
@@ -2544,7 +2544,8 @@ deleted:
 
 		PushActiveSnapshot(GetTransactionSnapshot());
 		perform_work_item(workitem);
-		PopActiveSnapshot();
+		if (ActiveSnapshotSet())	/* transaction could have aborted */
+			PopActiveSnapshot();
 
 		/*
 		 * Check for config changes before acquiring lock for further jobs.
