0f8a2b133644b3cf17a54c10153ba7941d0d8936
Andres Freund <andres@anarazel.de>
2025-11-04 13:24:57 -0500

    Backpatch: Fix warnings about declaration of environ on MinGW
    Backpatch commit 7bc9a8bdd2d to 13-17. The motivation for backpatching is that
    we want to update CI to Debian Trixie. Trixie contains a newer mingw
    installation, which would trigger the warning addressed by 7bc9a8bdd2d. The
    risk of backpatching seems fairly low, given that it did not cause issues in
    the branches the commit is already present.
    
    While CI is not present in 13-14, it seems better to be consistent across
    branches.
    
    Author: Thomas Munro <tmunro@postgresql.org>
    Discussion: https://postgr.es/m/o5yadhhmyjo53svzwvaocww6zkrp63i4f32cw3treuh46pxtza@hyqio5b2tkt6
    Backpatch-through: 13

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -859,7 +859,9 @@ PostmasterMain(int argc, char *argv[])
 
 	/* For debugging: display postmaster environment */
 	{
+#if !defined(WIN32) || defined(_MSC_VER)
 		extern char **environ;
+#endif
 		char	  **p;
 
 		ereport(DEBUG3,
