a95e3d84c0e0ffd1e27c185dd69d053e43f2f8b5
Álvaro Herrera <alvherre@kurilemu.de>
2025-11-04 13:23:26 +0100

    BRIN autosummarization may need a snapshot
    It's possible to define BRIN indexes on functions that require a
    snapshot to run, but the autosummarization feature introduced by commit
    7526e10224f0 fails to provide one.  This causes autovacuum to leave a
    BRIN placeholder tuple behind after a failed work-item execution, making
    such indexes less efficient.  Repair by obtaining a snapshot prior to
    running the task, and add a test to verify this behavior.
    
    Author: Álvaro Herrera <alvherre@kurilemu.de>
    Reported-by: Giovanni Fabris <giovanni.fabris@icon.it>
    Reported-by: Arthur Nascimento <tureba@gmail.com>
    Backpatch-through: 13
    Discussion: https://postgr.es/m/202511031106.h4fwyuyui6fz@alvherre.pgsql

--- src/backend/postmaster/autovacuum.c
+++ src/backend/postmaster/autovacuum.c
@@ -2556,7 +2556,9 @@ deleted:
 		workitem->avw_active = true;
 		LWLockRelease(AutovacuumLock);
 
+		PushActiveSnapshot(GetTransactionSnapshot());
 		perform_work_item(workitem);
+		PopActiveSnapshot();
 
 		/*
 		 * Check for config changes before acquiring lock for further jobs.
