3995e4a9d8c6870aa3bc817c62a1b6678f3ad318
Thomas Munro <tmunro@postgresql.org>
2025-11-18 10:30:34 +1300

    Define PS_USE_CLOBBER_ARGV on GNU/Hurd.
    This is a backpatch of 32b23664 to v14 and v15.
    
    * Remove ancient test for __hurd__, which intended to activate
      PS_USE_CHANGE_ARGV in these branches but turned out not to be
      defined on modern systems.
    
    * Add new test for__GNU__ to activate PS_USE_CLOBBER_ARGV, like the
      newer branches.
    
    Author: Michael Banck <mbanck@debian.org>
    Discussion: https://postgr.es/m/CA%2BhUKGJMNGUAqf27WbckYFrM-Mavy0RKJvocfJU%3DJ2XcAZyv%2Bw%40mail.gmail.com
    Backpatch-through: 14-15

--- src/backend/utils/misc/ps_status.c
+++ src/backend/utils/misc/ps_status.c
@@ -73,9 +73,9 @@ bool		update_process_title = true;
 #define PS_USE_PSTAT
 #elif defined(HAVE_PS_STRINGS)
 #define PS_USE_PS_STRINGS
-#elif (defined(BSD) || defined(__hurd__)) && !defined(__darwin__)
+#elif defined(BSD) && !defined(__darwin__)
 #define PS_USE_CHANGE_ARGV
-#elif defined(__linux__) || defined(_AIX) || defined(__sgi) || (defined(sun) && !defined(BSD)) || defined(__svr5__) || defined(__darwin__)
+#elif defined(__linux__) || defined(_AIX) || defined(__sgi) || (defined(sun) && !defined(BSD)) || defined(__svr5__) || defined(__darwin__) || defined(__GNU__)
 #define PS_USE_CLOBBER_ARGV
 #elif defined(WIN32)
 #define PS_USE_WIN32
@@ -85,7 +85,7 @@ bool		update_process_title = true;
 
 
 /* Different systems want the buffer padded differently */
-#if defined(_AIX) || defined(__linux__) || defined(__darwin__)
+#if defined(_AIX) || defined(__linux__) || defined(__darwin__) || defined(__GNU__)
 #define PS_PADDING '\0'
 #else
 #define PS_PADDING ' '
