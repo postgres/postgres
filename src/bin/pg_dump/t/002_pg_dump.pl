33bb3dc2d855c0d4472372f0eb7877725af67a39
Álvaro Herrera <alvherre@kurilemu.de>
2025-11-25 19:08:36 +0100

    pg_dump tests: don't put dumps in stdout
    This bloats the regression log files for no reason.
    
    Backpatch to 18; no further only because it fails to apply cleanly.
    (It's just whitespace change that conflicts, but I don't think this
    warrants more effort than this.)
    
    Discussion: https://postgr.es/m/202511251218.zfs4nu2qnh2m@alvherre.pgsql

--- src/bin/pg_dump/t/002_pg_dump.pl
+++ src/bin/pg_dump/t/002_pg_dump.pl
@@ -5019,7 +5019,12 @@ $node->command_fails_like(
 # Test dumping pg_catalog (for research -- cannot be reloaded)
 
 $node->command_ok(
-	[ 'pg_dump', '--port' => $port, '--schema' => 'pg_catalog' ],
+	[
+		'pg_dump',
+		'--port' => $port,
+		'--schema' => 'pg_catalog',
+		'--file' => "$tempdir/pgdump_pgcatalog.dmp"
+	],
 	'pg_dump: option -n pg_catalog');
 
 #########################################
@@ -5029,7 +5034,8 @@ $node->command_ok(
 	[
 		'pg_dumpall',
 		'--port' => $port,
-		'--exclude-database' => '"myhost.mydb"'
+		'--exclude-database' => '"myhost.mydb"',
+		'--file' => "$tempdir/pgdumpall.dmp"
 	],
 	'pg_dumpall: option --exclude-database handles database names with embedded dots'
 );
