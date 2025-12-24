9f7565c6c2d45df9705556c60f41394a8356cdf9
Tom Lane <tgl@sss.pgh.pa.us>
2025-12-23 21:38:43 -0500

    Fix another case of indirectly casting away const.
    This one was missed in 8f1791c61, because the machines that
    detected those issues don't compile this function.
    
    Author: Bertrand Drouvot <bertranddrouvot.pg@gmail.com>
    Reviewed-by: Tom Lane <tgl@sss.pgh.pa.us>
    Discussion: https://postgr.es/m/1324889.1764886170@sss.pgh.pa.us

--- src/port/getopt.c
+++ src/port/getopt.c
@@ -72,7 +72,7 @@ int
 getopt(int nargc, char *const *nargv, const char *ostr)
 {
 	static char *place = EMSG;	/* option letter processing */
-	char	   *oli;			/* option letter list index */
+	const char *oli;			/* option letter list index */
 
 	if (!*place)
 	{							/* update scanning pointer */
