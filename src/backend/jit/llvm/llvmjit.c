6377b17257c69c6c87b9aa1da3fac62bd91345eb
Thomas Munro <tmunro@postgresql.org>
2025-12-31 13:24:17 +1300

    jit: Fix jit_profiling_support when unavailable.
    jit_profiling_support=true captures profile data for Linux perf.  On
    other platforms, LLVMCreatePerfJITEventListener() returns NULL and the
    attempt to register the listener would crash.
    
    Fix by ignoring the setting in that case.  The documentation already
    says that it only has an effect if perf support is present, and we
    already did the same for older LLVM versions that lacked support.
    
    No field reports, unsurprisingly for an obscure developer-oriented
    setting.  Noticed in passing while working on commit 1a28b4b4.
    
    Backpatch-through: 14
    Reviewed-by: Matheus Alcantara <matheusssilv97@gmail.com>
    Reviewed-by: Andres Freund <andres@anarazel.de>
    Discussion: https://postgr.es/m/CA%2BhUKGJgB6gvrdDohgwLfCwzVQm%3DVMtb9m0vzQn%3DCwWn-kwG9w%40mail.gmail.com

--- src/backend/jit/llvm/llvmjit.c
+++ src/backend/jit/llvm/llvmjit.c
@@ -1193,7 +1193,8 @@ llvm_create_object_layer(void *Ctx, LLVMOrcExecutionSessionRef ES, const char *T
 	{
 		LLVMJITEventListenerRef l = LLVMCreatePerfJITEventListener();
 
-		LLVMOrcRTDyldObjectLinkingLayerRegisterJITEventListener(objlayer, l);
+		if (l)
+			LLVMOrcRTDyldObjectLinkingLayerRegisterJITEventListener(objlayer, l);
 	}
 #endif
 
