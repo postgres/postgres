84b787ae66e6736685d3275b0c7c9becd2a82508
David Rowley <drowley@postgresql.org>
2026-01-09 11:03:24 +1300

    Fix possible incorrect column reference in ERROR message
    When creating a partition for a RANGE partitioned table, the reporting
    of errors relating to converting the specified range values into
    constant values for the partition key's type could display the name of a
    previous partition key column when an earlier range was specified as
    MINVALUE or MAXVALUE.
    
    This was caused by the code not correctly incrementing the index that
    tracks which partition key the foreach loop was working on after
    processing MINVALUE/MAXVALUE ranges.
    
    Fix by using foreach_current_index() to ensure the index variable is
    always set to the List element being worked on.
    
    Author: myzhen <zhenmingyang@yeah.net>
    Reviewed-by: zhibin wang <killerwzb@gmail.com>
    Discussion: https://postgr.es/m/273cab52.978.19b96fc75e7.Coremail.zhenmingyang@yeah.net
    Backpatch-through: 14

--- src/backend/parser/parse_utilcmd.c
+++ src/backend/parser/parse_utilcmd.c
@@ -4146,11 +4146,13 @@ transformPartitionRangeBounds(ParseState *pstate, List *blist,
 	int			i,
 				j;
 
-	i = j = 0;
+	j = 0;
 	foreach(lc, blist)
 	{
 		Node	   *expr = lfirst(lc);
 		PartitionRangeDatum *prd = NULL;
+
+		i = foreach_current_index(lc);
 
 		/*
 		 * Infinite range bounds -- "minvalue" and "maxvalue" -- get passed in
@@ -4229,7 +4231,6 @@ transformPartitionRangeBounds(ParseState *pstate, List *blist,
 			prd = makeNode(PartitionRangeDatum);
 			prd->kind = PARTITION_RANGE_DATUM_VALUE;
 			prd->value = (Node *) value;
-			++i;
 		}
 
 		prd->location = exprLocation(expr);
