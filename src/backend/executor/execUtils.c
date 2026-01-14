ed425b5a200ac00488940f2354d3cf9f11091926
Jeff Davis <jdavis@postgresql.org>
2026-01-14 12:01:36 -0800

    Remove redundant assignment in CreateWorkExprContext
    In CreateWorkExprContext(), maxBlockSize is initialized to
    ALLOCSET_DEFAULT_MAXSIZE, and it then immediately reassigned,
    thus the initialization is a redundant.
    
    Author: Andreas Karlsson <andreas@proxel.se>
    Reported-by: Chao Li <lic@highgo.com>
    Reviewed-by: Tom Lane <tgl@sss.pgh.pa.us>
    Discussion: https://postgr.es/m/83a14f3c-f347-4769-9c01-30030b31f1eb@gmail.com

--- src/backend/executor/execUtils.c
+++ src/backend/executor/execUtils.c
@@ -321,7 +321,7 @@ CreateExprContext(EState *estate)
 ExprContext *
 CreateWorkExprContext(EState *estate)
 {
-	Size		maxBlockSize = ALLOCSET_DEFAULT_MAXSIZE;
+	Size		maxBlockSize;
 
 	maxBlockSize = pg_prevpower2_size_t(work_mem * (Size) 1024 / 16);
 
