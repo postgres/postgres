74a116a79b47631e163c9814f39f5d218834e94c
Michael Paquier <michael@paquier.xyz>
2026-02-06 15:38:16 +0900

    Fix some error message inconsistencies
    These errors are very unlikely going to show up, but in the event that
    they happen, some incorrect information would have been provided:
    - In pg_rewind, a stat() failure was reported as an open() failure.
    - In pg_combinebackup, a check for the new directory of a tablespace
    mapping was referred as the old directory.
    - In pg_combinebackup, a failure in reading a source file when copying
    blocks referred to the destination file.
    
    The changes for pg_combinebackup affect v17 and newer versions.  For
    pg_rewind, all the stable branches are affected.
    
    Author: Man Zeng <zengman@halodbtech.com>
    Discussion: https://postgr.es/m/tencent_1EE1430B1E6C18A663B8990F@qq.com
    Backpatch-through: 14

--- src/bin/pg_combinebackup/pg_combinebackup.c
+++ src/bin/pg_combinebackup/pg_combinebackup.c
@@ -501,7 +501,7 @@ add_tablespace_mapping(cb_options *opt, char *arg)
 				 tsmap->old_dir);
 
 	if (!is_absolute_path(tsmap->new_dir))
-		pg_fatal("old directory is not an absolute path in tablespace mapping: %s",
+		pg_fatal("new directory is not an absolute path in tablespace mapping: %s",
 				 tsmap->new_dir);
 
 	/* Canonicalize paths to avoid spurious failures when comparing. */
