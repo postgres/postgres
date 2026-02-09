b764b26f2e0a0a57057f301ca87e6a604f5e708b
Tom Lane <tgl@sss.pgh.pa.us>
2026-02-09 10:07:31 -0500

    Require superuser to install a non-built-in selectivity estimator.
    Selectivity estimators come in two flavors: those that make specific
    assumptions about the data types they are working with, and those
    that don't.  Most of the built-in estimators are of the latter kind
    and are meant to be safely attachable to any operator.  If the
    operator does not behave as the estimator expects, you might get a
    poor estimate, but it won't crash.
    
    However, estimators that do make datatype assumptions can malfunction
    if they are attached to the wrong operator, since then the data they
    get from pg_statistic may not be of the type they expect.  This can
    rise to the level of a security problem, even permitting arbitrary
    code execution by a user who has the ability to create SQL objects.
    
    To close this hole, establish a rule that built-in estimators are
    required to protect themselves against being called on the wrong type
    of data.  It does not seem practical however to expect estimators in
    extensions to reach a similar level of security, at least not in the
    near term.  Therefore, also establish a rule that superuser privilege
    is required to attach a non-built-in estimator to an operator.
    We expect that this restriction will have little negative impact on
    extensions, since estimators generally have to be written in C and
    thus superuser privilege is required to create them in the first
    place.
    
    This commit changes the privilege checks in CREATE/ALTER OPERATOR
    to enforce the rule about superuser privilege, and fixes a couple
    of built-in estimators that were making datatype assumptions without
    sufficiently checking that they're valid.
    
    Reported-by: Daniel Firer as part of zeroday.cloud
    Author: Tom Lane <tgl@sss.pgh.pa.us>
    Reviewed-by: Noah Misch <noah@leadboat.com>
    Security: CVE-2026-2004
    Backpatch-through: 14

--- src/backend/tsearch/ts_selfuncs.c
+++ src/backend/tsearch/ts_selfuncs.c
@@ -109,12 +109,14 @@ tsmatchsel(PG_FUNCTION_ARGS)
 	 * OK, there's a Var and a Const we're dealing with here.  We need the
 	 * Const to be a TSQuery, else we can't do anything useful.  We have to
 	 * check this because the Var might be the TSQuery not the TSVector.
+	 *
+	 * Also check that the Var really is a TSVector, in case this estimator is
+	 * mistakenly attached to some other operator.
 	 */
-	if (((Const *) other)->consttype == TSQUERYOID)
+	if (((Const *) other)->consttype == TSQUERYOID &&
+		vardata.vartype == TSVECTOROID)
 	{
 		/* tsvector @@ tsquery or the other way around */
-		Assert(vardata.vartype == TSVECTOROID);
-
 		selec = tsquerysel(&vardata, ((Const *) other)->constvalue);
 	}
 	else
