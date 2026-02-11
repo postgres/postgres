ce4b7e3a1058eb3edd7511f2770379d7f78bd3a0
Tom Lane <tgl@sss.pgh.pa.us>
2026-02-11 16:53:14 -0500

    Fix plpgsql's handling of "return simple_record_variable".
    If the variable's value is null, exec_stmt_return() missed filling
    in estate->rettype.  This is a pretty old bug, but we'd managed not
    to notice because that value isn't consulted for a null result ...
    unless we have to cast it to a domain.  That case led to a failure
    with "cache lookup failed for type 0".
    
    The correct way to assign the data type is known by exec_eval_datum.
    While we could copy-and-paste that logic, it seems like a better
    idea to just invoke exec_eval_datum, as the ROW case already does.
    
    Reported-by: Pavel Stehule <pavel.stehule@gmail.com>
    Author: Tom Lane <tgl@sss.pgh.pa.us>
    Discussion: https://postgr.es/m/CAFj8pRBT_ahexDf-zT-cyH8bMR_qcySKM8D5nv5MvTWPiatYGA@mail.gmail.com
    Backpatch-through: 14

--- src/pl/plpgsql/src/sql/plpgsql_domain.sql
+++ src/pl/plpgsql/src/sql/plpgsql_domain.sql
@@ -277,3 +277,13 @@ $$ LANGUAGE plpgsql;
 SELECT * FROM test_assign_ordered_named_pairs(1,2,3);
 SELECT * FROM test_assign_ordered_named_pairs(2,1,3);
 SELECT * FROM test_assign_ordered_named_pairs(1,2,0);  -- should fail someday
+
+CREATE FUNCTION test_null_ordered_named_pair()
+  RETURNS ordered_named_pair AS $$
+declare v ordered_named_pair;
+begin
+return v;
+end
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM test_null_ordered_named_pair();
