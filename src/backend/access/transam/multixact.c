23064542f8bdcbc4b6a513cac8ceed67c0d2336e
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2026-02-16 17:16:59 +0200

    Don't reset 'latest_page_number' when replaying multixid truncation
    'latest_page_number' is set to the correct value, according to
    nextOffset, early at system startup. Contrary to the comment, it hence
    should be set up correctly by the time we get to WAL replay.
    
    This fixes a failure to replay WAL generated on older minor versions,
    before commit 789d65364c (18.2, 17.8, 16.12, 15.16, 14.21). The
    failure occurs after a truncation record has been replayed and looks
    like this:
    
        FATAL:  could not access status of transaction 858112
        DETAIL:  Could not read from file "pg_multixact/offsets/000D" at offset 24576: read too few bytes.
        CONTEXT:  WAL redo at 3/2A3AB408 for MultiXact/CREATE_ID: 858111 offset 6695072 nmembers 5: 1048228 (sh) 1048271 (keysh) 1048316 (sh) 1048344 (keysh) 1048370 (sh)
    
    Reported-by: Sebastian Webber <sebastian@swebber.me>
    Reviewed-by: Andrey Borodin <x4mmm@yandex-team.ru>
    Reviewed-by: Kirill Reshke <reshkekirill@gmail.com>
    Discussion: https://www.postgresql.org/message-id/20260214090150.GC2297@p46.dedyn.io;lightning.p46.dedyn.io
    Backpatch-through: 14-18

--- src/backend/access/transam/multixact.c
+++ src/backend/access/transam/multixact.c
@@ -3434,7 +3434,6 @@ multixact_redo(XLogReaderState *record)
 	else if (info == XLOG_MULTIXACT_TRUNCATE_ID)
 	{
 		xl_multixact_truncate xlrec;
-		int			pageno;
 
 		memcpy(&xlrec, XLogRecGetData(record),
 			   SizeOfMultiXactTruncate);
@@ -3459,14 +3458,6 @@ multixact_redo(XLogReaderState *record)
 		SetMultiXactIdLimit(xlrec.endTruncOff, xlrec.oldestMultiDB, false);
 
 		PerformMembersTruncation(xlrec.startTruncMemb, xlrec.endTruncMemb);
-
-		/*
-		 * During XLOG replay, latest_page_number isn't necessarily set up
-		 * yet; insert a suitable value to bypass the sanity test in
-		 * SimpleLruTruncate.
-		 */
-		pageno = MultiXactIdToOffsetPage(xlrec.endTruncOff);
-		MultiXactOffsetCtl->shared->latest_page_number = pageno;
 		PerformOffsetsTruncation(xlrec.startTruncOff, xlrec.endTruncOff);
 
 		LWLockRelease(MultiXactTruncationLock);
