9a42888a32fd2ebc3d8e39bbc450556c326c9db7
Tom Lane <tgl@sss.pgh.pa.us>
2026-03-05 14:43:21 -0500

    Exit after fatal errors in client-side compression code.
    It looks like whoever wrote the astreamer (nee bbstreamer) code
    thought that pg_log_error() is equivalent to elog(ERROR), but
    it's not; it just prints a message.  So all these places tried to
    continue on after a compression or decompression error return,
    with the inevitable result being garbage output and possibly
    cascading error messages.  We should use pg_fatal() instead.
    
    These error conditions are probably pretty unlikely in practice,
    which no doubt accounts for the lack of field complaints.
    
    Author: Tom Lane <tgl@sss.pgh.pa.us>
    Reviewed-by: Chao Li <li.evan.chao@gmail.com>
    Discussion: https://postgr.es/m/1531718.1772644615@sss.pgh.pa.us
    Backpatch-through: 15

--- src/bin/pg_basebackup/bbstreamer_zstd.c
+++ src/bin/pg_basebackup/bbstreamer_zstd.c
@@ -165,8 +165,8 @@ bbstreamer_zstd_compressor_content(bbstreamer *streamer,
 								 &inBuf, ZSTD_e_continue);
 
 		if (ZSTD_isError(yet_to_flush))
-			pg_log_error("could not compress data: %s",
-						 ZSTD_getErrorName(yet_to_flush));
+			pg_fatal("could not compress data: %s",
+					 ZSTD_getErrorName(yet_to_flush));
 	}
 }
 
@@ -207,8 +207,8 @@ bbstreamer_zstd_compressor_finalize(bbstreamer *streamer)
 											&in, ZSTD_e_end);
 
 		if (ZSTD_isError(yet_to_flush))
-			pg_log_error("could not compress data: %s",
-						 ZSTD_getErrorName(yet_to_flush));
+			pg_fatal("could not compress data: %s",
+					 ZSTD_getErrorName(yet_to_flush));
 
 	} while (yet_to_flush > 0);
 
@@ -313,8 +313,8 @@ bbstreamer_zstd_decompressor_content(bbstreamer *streamer,
 									&mystreamer->zstd_outBuf, &inBuf);
 
 		if (ZSTD_isError(ret))
-			pg_log_error("could not decompress data: %s",
-						 ZSTD_getErrorName(ret));
+			pg_fatal("could not decompress data: %s",
+					 ZSTD_getErrorName(ret));
 	}
 }
 
