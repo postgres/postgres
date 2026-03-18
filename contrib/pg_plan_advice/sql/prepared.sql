b335fe56f35506348a567648e3a7ac1b3ac0baf3
Robert Haas <rhaas@postgresql.org>
2026-03-18 18:15:36 -0400

    pg_plan_advice: Fix multiple copy-and-paste-errors in test case.
    The second half of this file is meant to test feedback, not
    generated advice, and is meant to use the statements that it
    prepares, not leftover prepared statements from earlier in the
    file.
    
    These mistakes resulted in failures under debug_discard_caches = 1,
    because re-executing pt2 instead of executing pt4 for the first
    time resulted in different output depending on whether the query
    was replanned.
    
    Reported-by: Tom Lane <tgl@sss.pgh.pa.us> (per BF member avocet)

--- contrib/pg_plan_advice/sql/prepared.sql
+++ contrib/pg_plan_advice/sql/prepared.sql
@@ -25,12 +25,12 @@ SET pg_plan_advice.advice = 'SEQ_SCAN(ptab)';
 EXPLAIN (COSTS OFF)
 SELECT * FROM ptab;
 
--- Prepared, so advice should not be generated.
+-- Prepared, so feedback should not be generated.
 PREPARE pt3 AS SELECT * FROM ptab;
-EXPLAIN (COSTS OFF) EXECUTE pt1;
+EXPLAIN (COSTS OFF) EXECUTE pt3;
 
 SET pg_plan_advice.always_store_advice_details = true;
 
 -- Prepared, but always_store_advice_details = true, so should show feedback.
 PREPARE pt4 AS SELECT * FROM ptab;
-EXPLAIN (COSTS OFF, PLAN_ADVICE) EXECUTE pt2;
+EXPLAIN (COSTS OFF, PLAN_ADVICE) EXECUTE pt4;
