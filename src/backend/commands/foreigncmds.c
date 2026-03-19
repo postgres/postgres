c6f369e585e93f73cec21aff8974a8610572f446
Jeff Davis <jdavis@postgresql.org>
2026-03-19 14:59:07 -0700

    Fix dependency on FDW handler.
    ALTER FOREIGN DATA WRAPPER could drop the dependency on the handler
    function if it wasn't explicitly specified.
    
    Reviewed-by: Nathan Bossart <nathandbossart@gmail.com>
    Discussion: https://postgr.es/m/35c44a4b7fb76d35418c4d66b775a88f4ce60c86.camel@j-davis.com
    Backpatch-through: 14

--- src/backend/commands/foreigncmds.c
+++ src/backend/commands/foreigncmds.c
@@ -744,6 +744,11 @@ AlterForeignDataWrapper(AlterFdwStmt *stmt)
 		ereport(WARNING,
 				(errmsg("changing the foreign-data wrapper handler can change behavior of existing foreign tables")));
 	}
+	else
+	{
+		/* handler unchanged */
+		fdwhandler = fdwForm->fdwhandler;
+	}
 
 	if (validator_given)
 	{
