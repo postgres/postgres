2497dac55648e738b643577e807bfd2a3ac4c6e2
Fujii Masao <fujii@postgresql.org>
2026-03-30 14:37:33 +0900

    Fix FK triggers losing DEFERRABLE/INITIALLY DEFERRED when marked ENFORCED again
    Previously, a foreign key defined as DEFERRABLE INITIALLY DEFERRED could
    behave as NOT DEFERRABLE after being set to NOT ENFORCED and then back
    to ENFORCED.
    
    This happened because recreating the FK triggers on re-enabling the constraint
    forgot to restore the tgdeferrable and tginitdeferred fields in pg_trigger.
    
    Fix this bug by properly setting those fields when the foreign key constraint
    is marked ENFORCED again and its triggers are recreated, so the original
    DEFERRABLE and INITIALLY DEFERRED properties are preserved.
    
    Backpatch to v18, where NOT ENFORCED foreign keys were introduced.
    
    Author: Yasuo Honda <yasuo.honda@gmail.com>
    Reviewed-by: Fujii Masao <masao.fujii@gmail.com>
    Discussion: https://postgr.es/m/CAKmOUTms2nkxEZDdcrsjq5P3b2L_PR266Hv8kW5pANwmVaRJJQ@mail.gmail.com
    Backpatch-through: 18

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -12558,6 +12558,8 @@ ATExecAlterFKConstrEnforceability(List **wqueue, ATAlterConstraint *cmdcon,
 		fkconstraint->fk_matchtype = currcon->confmatchtype;
 		fkconstraint->fk_upd_action = currcon->confupdtype;
 		fkconstraint->fk_del_action = currcon->confdeltype;
+		fkconstraint->deferrable = currcon->condeferrable;
+		fkconstraint->initdeferred = currcon->condeferred;
 
 		/* Create referenced triggers */
 		if (currcon->conrelid == fkrelid)
