69c11f0545a027fdcb32254b546a0cd431823e4f
Álvaro Herrera <alvherre@kurilemu.de>
2026-04-05 00:21:53 +0200

    Modernize struct declarations in snapbuild.h
    Just a cosmetic cleanup.

--- src/include/replication/snapbuild.h
+++ src/include/replication/snapbuild.h
@@ -16,6 +16,14 @@
 #include "utils/snapmgr.h"
 
 /*
+ * forward declarations in this file
+ */
+typedef struct ReorderBuffer ReorderBuffer;
+typedef struct SnapBuild SnapBuild;
+typedef struct xl_heap_new_cid xl_heap_new_cid;
+typedef struct xl_running_xacts xl_running_xacts;
+
+/*
  * Please keep get_snapbuild_state_desc() (located in the pg_logicalinspect
  * module) updated if a change needs to be made to SnapBuildState.
  */
@@ -50,20 +58,11 @@ typedef enum
 	SNAPBUILD_CONSISTENT = 2,
 } SnapBuildState;
 
-/* forward declare so we don't have to include snapbuild_internal.h */
-struct SnapBuild;
-typedef struct SnapBuild SnapBuild;
 
-/* forward declare so we don't have to include reorderbuffer.h */
-struct ReorderBuffer;
-
-/* forward declare so we don't have to include heapam_xlog.h */
-struct xl_heap_new_cid;
-struct xl_running_xacts;
 
 extern void CheckPointSnapBuild(void);
 
-extern SnapBuild *AllocateSnapshotBuilder(struct ReorderBuffer *reorder,
+extern SnapBuild *AllocateSnapshotBuilder(ReorderBuffer *reorder,
 										  TransactionId xmin_horizon, XLogRecPtr start_lsn,
 										  bool need_full_snapshot,
 										  bool in_slot_creation,
@@ -91,9 +90,9 @@ extern bool SnapBuildProcessChange(SnapBuild *builder, TransactionId xid,
 								   XLogRecPtr lsn);
 extern void SnapBuildProcessNewCid(SnapBuild *builder, TransactionId xid,
 								   XLogRecPtr lsn,
-								   struct xl_heap_new_cid *xlrec);
+								   xl_heap_new_cid *xlrec);
 extern void SnapBuildProcessRunningXacts(SnapBuild *builder, XLogRecPtr lsn,
-										 struct xl_running_xacts *running);
+										 xl_running_xacts *running);
 extern void SnapBuildSerializationPoint(SnapBuild *builder, XLogRecPtr lsn);
 
 extern bool SnapBuildSnapshotExists(XLogRecPtr lsn);
