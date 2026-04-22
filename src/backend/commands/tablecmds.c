0859000d0d7195738b1c52e430b25fc72642cd73
Michael Paquier <michael@paquier.xyz>
2026-04-22 10:34:38 +0900

    Allow ALTER INDEX .. ATTACH PARTITION to validate a parent index
    This commit tweaks ALTER INDEX .. ATTACH PARTITION to attempt a
    validation of a parent index in the case where an index is already
    attached but the parent is not yet valid.  This occurs in cases where a
    parent index was created invalid such as with CREATE INDEX ONLY, but was
    left invalid after an invalid child index was attached (partitioned
    indexes set indisvalid to false if at least one partition is
    !indisvalid, indisvalid is true in a partitioned table iff all
    partitions are indisvalid).  This could leave a partition tree in a
    situation where a user could not bring the parent index back to valid
    after fixing the child index, as there is no built-in mechanism to do
    so.  This commit relies on the fact that repeated ATTACH PARTITION
    commands on the same index silently succeed.
    
    An invalid parent index is more than just a passive issue.  It causes
    for example ON CONFLICT on a partitioned table if the invalid parent
    index is used to enforce a unique constraint.
    
    Some test cases are added to track some of problematic patterns, using a
    set of partition trees with combinations of invalid indexes and ATTACH
    PARTITION.
    
    Reported-by: Mohamed Ali <moali.pg@gmail.com>
    Author: Sami Imseih <sanmimseih@gmail.com>
    Reviewed-by: Michael Paquier <michael@paquier.xyz>
    Reviewed-by: Haibo Yan <tristan.yim@gmail.com>
    Discussion: http://postgr.es/m/CAGnOmWqi1D9ycBgUeOGf6mOCd2Dcf=6sKhbf4sHLs5xAcKVCMQ@mail.gmail.com
    Backpatch-through: 14

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -19433,7 +19433,10 @@ ATExecAttachPartitionIdx(List **wqueue, Relation parentIdx, RangeVar *name)
 
 	ObjectAddressSet(address, RelationRelationId, RelationGetRelid(partIdx));
 
-	/* Silently do nothing if already in the right state */
+	/*
+	 * Check if the index is already attached to the correct parent,
+	 * ultimately attempting one round of validation if already the case.
+	 */
 	currParent = partIdx->rd_rel->relispartition ?
 		get_partition_parent(partIdxId, false) : InvalidOid;
 	if (currParent != RelationGetRelid(parentIdx))
@@ -19532,6 +19535,14 @@ ATExecAttachPartitionIdx(List **wqueue, Relation parentIdx, RangeVar *name)
 
 		free_attrmap(attmap);
 
+		validatePartitionedIndex(parentIdx, parentTbl);
+	}
+	else if (!parentIdx->rd_index->indisvalid)
+	{
+		/*
+		 * The index is attached, but the parent is still invalid; see if it
+		 * can be validated now.
+		 */
 		validatePartitionedIndex(parentIdx, parentTbl);
 	}
 
