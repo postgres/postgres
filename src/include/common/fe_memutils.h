fe2720c450655a9986dd731a62e476ea3e1313b0
Tom Lane <tgl@sss.pgh.pa.us>
2026-05-11 05:13:48 -0700

    Make palloc_array() and friends safe against integer overflow.
    Sufficiently large "count" arguments could result in undetected
    overflow, causing the allocated memory chunk to be much smaller
    than what the caller will subsequently write into it.  This is
    unlikely to be a hazard with 64-bit size_t but can sometimes
    happen on 32-bit builds, primarily where a function allocates
    workspace that's significantly larger than its input data.
    Rather than trying to patch the at-risk callers piecemeal,
    let's just redefine these macros so that they always check.
    
    To do that, move the longstanding add_size() and mul_size() functions
    into palloc.h and mcxt.c, and adjust them to not be specific to
    shared-memory allocation.  Then invent palloc_mul(), palloc0_mul(),
    palloc_mul_extended() to use these functions.  Actually, the latter
    use inlined copies to save one function call.  repalloc_array() gets
    similar treatment.  I didn't bother trying to inline the calls for
    repalloc0_array() though.
    
    In v14 and v15, this also adds repalloc_extended(), which previously
    was only available in v16 and up.
    
    We need copies of all this in fe_memutils.[hc] as well, since that
    module also provides palloc_array() etc.
    
    Reported-by: Xint Code
    Author: Tom Lane <tgl@sss.pgh.pa.us>
    Reviewed-by: Masahiko Sawada <sawada.mshk@gmail.com>
    Backpatch-through: 14
    Security: CVE-2026-6473

--- src/include/common/fe_memutils.h
+++ src/include/common/fe_memutils.h
@@ -30,6 +30,16 @@ extern void *pg_realloc(void *ptr, size_t size);
 extern void pg_free(void *ptr);
 
 /*
+ * Support for safe calculation of memory request sizes
+ */
+extern Size add_size(Size s1, Size s2);
+extern Size mul_size(Size s1, Size s2);
+extern void *pg_malloc_mul(Size s1, Size s2);
+extern void *pg_malloc0_mul(Size s1, Size s2);
+extern void *pg_malloc_mul_extended(Size s1, Size s2, int flags);
+extern void *pg_realloc_mul(void *p, Size s1, Size s2);
+
+/*
  * Variants with easier notation and more type safety
  */
 
@@ -42,14 +52,15 @@ extern void pg_free(void *ptr);
 /*
  * Allocate space for "count" objects of type "type"
  */
-#define pg_malloc_array(type, count) ((type *) pg_malloc(sizeof(type) * (count)))
-#define pg_malloc0_array(type, count) ((type *) pg_malloc0(sizeof(type) * (count)))
+#define pg_malloc_array(type, count) ((type *) pg_malloc_mul(sizeof(type), count))
+#define pg_malloc0_array(type, count) ((type *) pg_malloc0_mul(sizeof(type), count))
+#define pg_malloc_array_extended(type, count, flags) ((type *) pg_malloc_mul_extended(sizeof(type), count, flags))
 
 /*
  * Change size of allocation pointed to by "pointer" to have space for "count"
  * objects of type "type"
  */
-#define pg_realloc_array(pointer, type, count) ((type *) pg_realloc(pointer, sizeof(type) * (count)))
+#define pg_realloc_array(pointer, type, count) ((type *) pg_realloc_mul(pointer, sizeof(type), count))
 
 /* Equivalent functions, deliberately named the same as backend functions */
 extern char *pstrdup(const char *in);
@@ -59,12 +70,17 @@ extern void *palloc0(Size size);
 extern void *palloc_extended(Size size, int flags);
 extern void *repalloc(void *pointer, Size size);
 extern void pfree(void *pointer);
+extern void *palloc_mul(Size s1, Size s2);
+extern void *palloc0_mul(Size s1, Size s2);
+extern void *palloc_mul_extended(Size s1, Size s2, int flags);
+extern void *repalloc_mul(void *p, Size s1, Size s2);
 
 #define palloc_object(type) ((type *) palloc(sizeof(type)))
 #define palloc0_object(type) ((type *) palloc0(sizeof(type)))
-#define palloc_array(type, count) ((type *) palloc(sizeof(type) * (count)))
-#define palloc0_array(type, count) ((type *) palloc0(sizeof(type) * (count)))
-#define repalloc_array(pointer, type, count) ((type *) repalloc(pointer, sizeof(type) * (count)))
+#define palloc_array(type, count) ((type *) palloc_mul(sizeof(type), count))
+#define palloc0_array(type, count) ((type *) palloc0_mul(sizeof(type), count))
+#define palloc_array_extended(type, count, flags) ((type *) palloc_mul_extended(sizeof(type), count, flags))
+#define repalloc_array(pointer, type, count) ((type *) repalloc_mul(pointer, sizeof(type), count))
 
 /* sprintf into a palloc'd buffer --- these are in psprintf.c */
 extern char *psprintf(const char *fmt,...) pg_attribute_printf(1, 2);
