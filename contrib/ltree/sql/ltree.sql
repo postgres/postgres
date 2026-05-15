092b570a72dd71544090f62fdda6e7e7702df2b8
Michael Paquier <michael@paquier.xyz>
2026-05-15 18:02:54 +0900

    Re-add regression tests for ltree and intarray
    These tests have been removed by 906ea101d0d5, due to some of them being
    unstable in the buildfarm with low max_stack_depth values.  They are now
    reworked so as they should be more portable.
    
    The tests to cover the findoprnd() overflows use a balanced tree to
    avoid using too much stack, per a suggestion and an investigation by Tom
    Lane.
    
    Note: This is initially applied only on HEAD; a backpatch will follow
    should the buildfarm be fine with the situation.
    
    Discussion: https://postgr.es/m/agZc6XecyE7E7fep@paquier.xyz
    Backpatch-through: 14

--- contrib/ltree/sql/ltree.sql
+++ contrib/ltree/sql/ltree.sql
@@ -246,6 +246,19 @@ SELECT 'tree.awdfg'::ltree @ 'tree & aWdfg@'::ltxtquery;
 SELECT 'tree.awdfg_qwerty'::ltree @ 'tree & aw_qw%*'::ltxtquery;
 SELECT 'tree.awdfg_qwerty'::ltree @ 'tree & aw_rw%*'::ltxtquery;
 
+-- Test for overflow of the int16 "left" field in findoprnd().
+-- This query uses a balanced binary tree to avoid using too much stack.
+DO $$
+DECLARE
+  e text := 'a';
+BEGIN
+  FOR i IN 1..14 LOOP
+    e := '(' || e || '&' || e || ')';
+  END LOOP;
+  PERFORM ('b|' || e)::ltxtquery;
+END;
+$$;
+
 --arrays
 
 SELECT '{1.2.3}'::ltree[] @> '1.2.3.4';
@@ -384,3 +397,10 @@ SELECT count(*) FROM _ltreetest WHERE t ~ '23.*{1}.1' ;
 SELECT count(*) FROM _ltreetest WHERE t ~ '23.*.1' ;
 SELECT count(*) FROM _ltreetest WHERE t ~ '23.*.2' ;
 SELECT count(*) FROM _ltreetest WHERE t ? '{23.*.1,23.*.2}' ;
+
+-- Test for overflow of lquery_level.totallen.
+SELECT (repeat('x', 255) || repeat('|' || repeat('x', 255), 256))::lquery;
+
+--- Test for overflow of lquery_level.numvar, with a set of single-char
+--- variants in one level.
+SELECT (repeat('a|', 65535) || 'a')::lquery;
