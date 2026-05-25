130396e6c034c26617c8a73be44f9640082fd490
Fujii Masao <fujii@postgresql.org>
2026-05-26 00:51:18 +0900

    dblink: Give user mapping precedence for use_scram_passthrough
    Commit 97f6fc10fff changed postgres_fdw so that user-mapping settings
    override foreign server settings for use_scram_passthrough. This commit
    applies the same behavior to dblink.
    
    Backpatch to v18, where use_scram_passthrough was introduced.
    
    Author: Matheus Alcantara <matheusssilv97@gmail.com>
    Reviewed-by: Fujii Masao <masao.fujii@gmail.com>
    Discussion: https://postgr.es/m/CAHGQGwEJ8rZjmbOvCicyr4vbuLio082bNTde0WNoSWaWr9wVcg@mail.gmail.com
    Backpatch-through: 18

--- contrib/dblink/dblink.c
+++ contrib/dblink/dblink.c
@@ -3248,12 +3248,18 @@ appendSCRAMKeysInfo(StringInfo buf)
 }
 
 
+/*
+ * Return whether SCRAM pass-through is enabled.
+ *
+ * If use_scram_passthrough is specified in both the foreign server
+ * and the user mapping, the user mapping setting takes precedence.
+ */
 static bool
 UseScramPassthrough(ForeignServer *foreign_server, UserMapping *user)
 {
 	ListCell   *cell;
 
-	foreach(cell, foreign_server->options)
+	foreach(cell, user->options)
 	{
 		DefElem    *def = lfirst(cell);
 
@@ -3261,7 +3267,7 @@ UseScramPassthrough(ForeignServer *foreign_server, UserMapping *user)
 			return defGetBoolean(def);
 	}
 
-	foreach(cell, user->options)
+	foreach(cell, foreign_server->options)
 	{
 		DefElem    *def = (DefElem *) lfirst(cell);
 
