b4bd1385043c4664a7b8894a811ba91a61c6e07f
Michael Paquier <michael@paquier.xyz>
2026-06-11 17:29:38 +0900

    Fix race with timeline selection in logical decoding during promotion
    During promotion, there is a window where RecoveryInProgress() returns
    true but the WAL segments of the old timeline have already been removed.
    A logical decoding could pick up the old timeline in this window when
    reading a page, failing with the following error:
    ERROR: requested WAL segment ... has already been removed
    
    This issue does not lead to any data correctness issue, as retrying to
    decode the data works in follow-up decoding attempts.  It impacts
    availability, though.  Other WAL page read callbacks have a similar
    issue, this commit takes care of what should be the noisiest code path:
    logical decoding with START_REPLICATION in a WAL sender.
    
    A TAP test, based on an injection point waiting in the startup process
    after the segments have been removed/recycled, is added.  This part is
    backpatched down to v17.
    
    This issue has been causing sporadic failures in the buildfarm, and
    was reproducible manually.  This issue happens since logical decoding on
    standbys exists, down to v16.
    
    Reported-by: Alexander Lakhin <exclusion@gmail.com>
    Author: Bertrand Drouvot <bertranddrouvot.pg@gmail.com>
    Reviewed-by: Hayato Kuroda <kuroda.hayato@fujitsu.com>
    Reviewed-by: Xuneng Zhou <xunengzhou@gmail.com>
    Discussion: https://postgr.es/m/7daef094-abf3-4672-bc23-3df4763b16a3@gmail.com
    Backpatch-through: 16

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -6180,6 +6180,8 @@ StartupXLOG(void)
 	if (ArchiveRecoveryRequested)
 		CleanupAfterArchiveRecovery(EndOfLogTLI, EndOfLog, newTLI);
 
+	INJECTION_POINT("promotion-after-wal-segment-cleanup", NULL);
+
 	/*
 	 * Local WAL inserts enabled, so it's time to finish initialization of
 	 * commit timestamp.
