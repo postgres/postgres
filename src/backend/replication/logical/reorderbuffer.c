ac445069002c9e8c91878c2ce04d4e6a12398fc6
Álvaro Herrera <alvherre@kurilemu.de>
2026-06-16 18:13:15 +0200

    logical decoding: Correctly free speculative insertion
    The error path in ReorderBufferProcessTXN was not freeing
    (reorderbuffer.c's representation of) a speculative insertion record
    correctly.  In assert-enabled builds, this leads to an assertion
    failure.  In production builds, I see no effect; there may be a small
    transient leak, but in an improbable code path such as this, such a leak
    is not of any significance.  For users running with assertions enabled,
    the crash is annoying.
    
    Fix by having ReorderBufferProcessTXN() free the speculative insert
    ahead of freeing the rest of the transaction, and no longer try to
    handle that insert as a separate argument to ReorderBufferResetTXN().
    
    This code came in with commit 7259736a6e5b (14-era).  Backpatch all the
    way back.
    
    In branches 14-16, also backpatch the assertion that originally fails in
    the problem scenario, which was added by dbed2e36625d (originally
    backpatched to 17), that at the end of ReorderBufferReturnTXN() the
    in-memory size of the transaction is zero.
    
    Author: Vishal Prasanna <vishal.g@zohocorp.com>
    Reviewed-by: Hayato Kuroda <kuroda.hayato@fujitsu.com>
    Backpatch-through: 14
    Discussion: https://postgr.es/m/19c7623e882.4080fd5426212.311756747309556767@zohocorp.com

--- src/backend/replication/logical/reorderbuffer.c
+++ src/backend/replication/logical/reorderbuffer.c
@@ -2081,21 +2081,13 @@ static void
 ReorderBufferResetTXN(ReorderBuffer *rb, ReorderBufferTXN *txn,
 					  Snapshot snapshot_now,
 					  CommandId command_id,
-					  XLogRecPtr last_lsn,
-					  ReorderBufferChange *specinsert)
+					  XLogRecPtr last_lsn)
 {
 	/* Discard the changes that we just streamed */
 	ReorderBufferTruncateTXN(rb, txn, rbtxn_prepared(txn));
 
 	/* Free all resources allocated for toast reconstruction */
 	ReorderBufferToastReset(rb, txn);
-
-	/* Return the spec insert change if it is not NULL */
-	if (specinsert != NULL)
-	{
-		ReorderBufferReturnChange(rb, specinsert, true);
-		specinsert = NULL;
-	}
 
 	/*
 	 * For the streaming case, stop the stream and remember the command ID and
@@ -2360,7 +2352,7 @@ ReorderBufferProcessTXN(ReorderBuffer *rb, ReorderBufferTXN *txn,
 					 * CheckTableNotInUse() and locking.
 					 */
 
-					/* clear out a pending (and thus failed) speculation */
+					/* clear out a pending (= failed) speculative insertion */
 					if (specinsert != NULL)
 					{
 						ReorderBufferReturnChange(rb, specinsert, true);
@@ -2663,6 +2655,13 @@ ReorderBufferProcessTXN(ReorderBuffer *rb, ReorderBufferTXN *txn,
 		if (using_subtxn)
 			RollbackAndReleaseCurrentSubTransaction();
 
+		/* Free the specinsert change before freeing the ReorderBufferTXN */
+		if (specinsert != NULL)
+		{
+			ReorderBufferReturnChange(rb, specinsert, true);
+			specinsert = NULL;
+		}
+
 		/*
 		 * The error code ERRCODE_TRANSACTION_ROLLBACK indicates a concurrent
 		 * abort of the (sub)transaction we are streaming or preparing. We
@@ -2689,8 +2688,7 @@ ReorderBufferProcessTXN(ReorderBuffer *rb, ReorderBufferTXN *txn,
 
 			/* Reset the TXN so that it is allowed to stream remaining data. */
 			ReorderBufferResetTXN(rb, txn, snapshot_now,
-								  command_id, prev_lsn,
-								  specinsert);
+								  command_id, prev_lsn);
 		}
 		else
 		{
