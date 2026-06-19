dc5116780846951a409d788479a9e9fa6edd9f07
David Rowley <drowley@postgresql.org>
2026-06-19 15:26:18 +1200

    Update JIT tuple deforming code for virtual generated columns
    The JIT deforming code contains an optimization that determines which
    columns are guaranteed to exist in the tuple.  That's used to allow
    skipping of reading the tuple's natts when the code only needs to deform
    attributes that are guaranteed to always exist in all tuples.  83ea6c540
    missed updating this code to account for VIRTUAL generated columns.
    These are stored as NULLs in the tuple, but may be defined as NOT NULL.
    This could result in the code thinking more columns are guaranteed to
    exist than actually do.
    
    Author: David Rowley <dgrowleyml@gmail.com>
    Reviewed-by: Chao Li <li.evan.chao@gmail.com>
    Backpatch-through: 18
    Discussion: https://postgr.es/m/1151393.1781734980@sss.pgh.pa.us

--- src/test/regress/sql/generated_virtual.sql
+++ src/test/regress/sql/generated_virtual.sql
@@ -380,6 +380,21 @@ INSERT INTO gtest21c (a, c) VALUES (10, 42);
 SELECT a, b, c FROM gtest21c;
 DROP TABLE gtest21c;
 
+-- try adding a virtual generated column to an existing table with tuples,
+-- then try adding an atthasmissing column before adding a normal nullable
+-- column.
+CREATE TABLE gtest21d (a int NOT NULL);
+INSERT INTO gtest21d (a) VALUES(10);
+ALTER TABLE gtest21d ADD COLUMN b INT GENERATED ALWAYS AS (a * 10) VIRTUAL NOT NULL;
+SELECT * FROM gtest21d ORDER BY a;
+INSERT INTO gtest21d (a) VALUES(20);
+ALTER TABLE gtest21d ADD COLUMN c INT NOT NULL DEFAULT 1234;
+SELECT * FROM gtest21d ORDER BY a;
+ALTER TABLE gtest21d ADD COLUMN d INT;
+INSERT INTO gtest21d (a, c, d) VALUES(30, 12345, 100);
+SELECT * FROM gtest21d ORDER BY a;
+DROP TABLE gtest21d;
+
 -- not-null constraint with partitioned table
 CREATE TABLE gtestnn_parent (
     f1 int,
