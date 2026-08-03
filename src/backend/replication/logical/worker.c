e7e04e1d3c54466194ee6d1687fcbd4254f65ab5
Jeff Davis <jdavis@postgresql.org>
2026-08-03 11:34:58 -0700

    Do not log subscription conninfo.
    Logging connection information, even at DEBUG1, creates unnecessary
    risks. Remove the entire log message because it had no other useful
    content.
    
    Addresses finding 14 in report from linked discussion.
    
    Reported-by: Noah Misch <noah@leadboat.com>
    Discussion: https://postgr.es/m/20260710195902.4f.noahmisch@microsoft.com
    Backpatch-through: 14

--- src/backend/replication/logical/worker.c
+++ src/backend/replication/logical/worker.c
@@ -3703,10 +3703,6 @@ ApplyWorkerMain(Datum main_arg)
 
 	CommitTransactionCommand();
 
-	/* Connect to the origin and start the replication. */
-	elog(DEBUG1, "connecting to publisher using connection string \"%s\"",
-		 MySubscription->conninfo);
-
 	if (am_tablesync_worker())
 	{
 		start_table_sync(&origin_startpos, &myslotname);
