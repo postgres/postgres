7213cbfa02144b3a79a19cfae6964afd8219ecde
Etsuro Fujita <efujita@postgresql.org>
2026-08-07 17:30:05 +0900

    Drain pending asynchronous requests during ExecReScanAppend.
    The logic for asynchronous Append assumes that pending requests made for
    subplans of an Append are drained during ExecReScanAppend.  To ensure
    that, commit 9e283fc85 modified postgresReScanForeignScan to drain such
    a request if any, but failed to take into account that if such a request
    was made for a subplan that is re-scanned with parameter changes or
    pruned in the next round by runtime pruning, the postgres_fdw callback
    function is called after ExecReScanAppend or never called, respectively.
    This would cause such a request to remain even after ExecReScanAppend,
    leading to incorrect results, an infinite loop, or an assertion failure.
    
    To fix, modify ExecReScanAppend to, for each of the pending requests,
    give the FDW a chance to drain that request using the existing
    ForeignAsyncConfigureWait/ForeignAsyncNotify callback functions.  This
    makes the change made to postgresReScanForeignScan useless, so remove it
    as well.
    
    Back-patch to v14 where asynchronous Append was added.
    
    Reported-by: Alexander Korotkov <aekorotkov@gmail.com>
    Co-authored-by: Alexander Korotkov <aekorotkov@gmail.com>
    Co-authored-by: Gleb Kashkin <g.kashkin@postgrespro.ru>
    Co-authored-by: Etsuro Fujita <etsuro.fujita@gmail.com>
    Reviewed-by: Alexander Pyhalov <a.pyhalov@postgrespro.ru>
    Reviewed-by: Gleb Kashkin <g.kashkin@postgrespro.ru>
    Discussion: https://postgr.es/m/CAPpHfduMOTnV5Zj2KGJ7zanL_10QvccZHtPUaDfJvBhsh9axnQ%40mail.gmail.com
    Backpatch-through: 14

--- contrib/postgres_fdw/sql/postgres_fdw.sql
+++ contrib/postgres_fdw/sql/postgres_fdw.sql
@@ -3567,6 +3567,16 @@ INSERT INTO result_tbl SELECT * FROM async_pt WHERE b === 505;
 SELECT * FROM result_tbl ORDER BY a;
 DELETE FROM result_tbl;
 
+-- Test ExecAppendAsyncReset code path that drains outstanding async requests
+-- (case where subplans are re-scanned with parameter changes)
+EXPLAIN (VERBOSE, COSTS OFF)
+SELECT o.x FROM (VALUES (2505), (3505)) o(x), LATERAL (SELECT a FROM async_pt WHERE a = o.x OR a = 1505 LIMIT 1) s ORDER BY o.x;
+SELECT o.x FROM (VALUES (2505), (3505)) o(x), LATERAL (SELECT a FROM async_pt WHERE a = o.x OR a = 1505 LIMIT 1) s ORDER BY o.x;
+
+EXPLAIN (VERBOSE, COSTS OFF)
+SELECT o.x FROM (VALUES (2505), (3505)) o(x), LATERAL (SELECT a FROM async_pt WHERE a = o.x OR (a = 1505 AND o.x = 2505) LIMIT 1) s ORDER BY o.x;
+SELECT o.x FROM (VALUES (2505), (3505)) o(x), LATERAL (SELECT a FROM async_pt WHERE a = o.x OR (a = 1505 AND o.x = 2505) LIMIT 1) s ORDER BY o.x;
+
 DROP FOREIGN TABLE async_p3;
 DROP TABLE base_tbl3;
 
@@ -3794,8 +3804,8 @@ DROP TABLE base_tbl2;
 DROP TABLE result_tbl;
 DROP TABLE join_tbl;
 
--- Test that an asynchronous fetch is processed before restarting the scan in
--- ReScanForeignScan
+-- Test ExecAppendAsyncReset code path that drains outstanding async requests
+-- (case where subplans are re-scanned without parameter changes)
 CREATE TABLE base_tbl (a int, b int);
 INSERT INTO base_tbl VALUES (1, 11), (2, 22), (3, 33);
 CREATE FOREIGN TABLE foreign_tbl (b int)
