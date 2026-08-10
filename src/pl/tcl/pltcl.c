60e7329b9b1dd1ca7bb2a39cf7d95e1d5b8b3a39
Tom Lane <tgl@sss.pgh.pa.us>
2026-08-10 06:37:48 -0700

    Protect some fixed-size arrays that have FUNC_MAX_ARGS elements.
    The maximum number of arguments allowed for an aggregate function
    is FUNC_MAX_ARGS-1 (since the underlying transfn and/or finalfn
    will be called with one more argument).  parse_func.c failed to
    enforce this, allowing construction of calls that would try to
    pass FUNC_MAX_ARGS+1 to the underlying functions, resulting in
    a memory stomp in the executor.  Add correct checking there.
    
    Since it's possible that a bad call has been stored in a view or
    SQL function, also add checks in various aggregate-related and
    window-function-related code that there are not more than
    FUNC_MAX_ARGS arguments.  These will also protect us against the
    possibility that we're trying to run a stored view that was made
    by a server executable with different FUNC_MAX_ARGS.  (Arguably,
    that scenario does not qualify as a security problem.  But let's
    just tighten up all of this while we're here, rather than split
    hairs over whether an overrun is reachable.)
    
    Likewise check in compute_function_hashkey.  Here the hazard is
    directly from a pg_proc row, but the scenario is the same.
    
    PL/Tcl has a similar issue with a fixed-size string buffer.
    Let's just replace that buffer with a Tcl_DString, removing the
    whole issue and making the code look more like what's around it.
    
    There are a lot of other FUNC_MAX_ARGS-sized arrays, but the rest
    have nearby guards already, some with comments explicitly pointing
    out the hazard of FUNC_MAX_ARGS changing.
    
    I also used palloc_array() in a few related places in funcapi.c.
    Those aren't live hazards AFAICS, but nearby code has been
    palloc_array-ified already, so it seemed inconsistent to not use
    it here.
    
    Reported-by: Masahiko Sawada <sawada.mshk@gmail.com>
    Author: Tom Lane <tgl@sss.pgh.pa.us>
    Reviewed-by: Masahiko Sawada <sawada.mshk@gmail.com>
    Backpatch-through: 14
    Security: CVE-2026-14679

--- src/pl/tcl/pltcl.c
+++ src/pl/tcl/pltcl.c
@@ -1430,6 +1430,7 @@ compile_pltcl_function(Oid fn_oid, Oid tgreloid,
 	Tcl_DString proc_internal_def;
 	Tcl_DString proc_internal_name;
 	Tcl_DString proc_internal_body;
+	Tcl_DString proc_internal_args;
 
 	/* We'll need the pg_proc tuple in any case... */
 	procTup = SearchSysCache1(PROCOID, ObjectIdGetDatum(fn_oid));
@@ -1479,6 +1480,7 @@ compile_pltcl_function(Oid fn_oid, Oid tgreloid,
 	Tcl_DStringInit(&proc_internal_def);
 	Tcl_DStringInit(&proc_internal_name);
 	Tcl_DStringInit(&proc_internal_body);
+	Tcl_DStringInit(&proc_internal_args);
 	PG_TRY();
 	{
 		bool		is_trigger = OidIsValid(tgreloid);
@@ -1488,10 +1490,9 @@ compile_pltcl_function(Oid fn_oid, Oid tgreloid,
 		bool		need_underscore;
 		HeapTuple	typeTup;
 		Form_pg_type typeStruct;
-		char		proc_internal_args[33 * FUNC_MAX_ARGS];
 		Datum		prosrcdatum;
 		char	   *proc_source;
-		char		buf[48];
+		char		buf[64];
 		pltcl_interp_desc *interp_desc;
 		Tcl_Interp *interp;
 		int			i;
@@ -1660,7 +1661,6 @@ compile_pltcl_function(Oid fn_oid, Oid tgreloid,
 		 ************************************************************/
 		if (!is_trigger && !is_event_trigger)
 		{
-			proc_internal_args[0] = '\0';
 			for (i = 0; i < prodesc->nargs; i++)
 			{
 				Oid			argtype = procStruct->proargtypes.values[i];
@@ -1693,8 +1693,8 @@ compile_pltcl_function(Oid fn_oid, Oid tgreloid,
 				}
 
 				if (i > 0)
-					strcat(proc_internal_args, " ");
-				strcat(proc_internal_args, buf);
+					Tcl_DStringAppend(&proc_internal_args, " ", -1);
+				Tcl_DStringAppend(&proc_internal_args, buf, -1);
 
 				ReleaseSysCache(typeTup);
 			}
@@ -1702,13 +1702,14 @@ compile_pltcl_function(Oid fn_oid, Oid tgreloid,
 		else if (is_trigger)
 		{
 			/* trigger procedure has fixed args */
-			strcpy(proc_internal_args,
-				   "TG_name TG_relid TG_table_name TG_table_schema TG_relatts TG_when TG_level TG_op __PLTcl_Tup_NEW __PLTcl_Tup_OLD args");
+			Tcl_DStringAppend(&proc_internal_args,
+							  "TG_name TG_relid TG_table_name TG_table_schema TG_relatts TG_when TG_level TG_op __PLTcl_Tup_NEW __PLTcl_Tup_OLD args",
+							  -1);
 		}
 		else if (is_event_trigger)
 		{
 			/* event trigger procedure has fixed args */
-			strcpy(proc_internal_args, "TG_event TG_tag");
+			Tcl_DStringAppend(&proc_internal_args, "TG_event TG_tag", -1);
 		}
 
 		/************************************************************
@@ -1721,7 +1722,8 @@ compile_pltcl_function(Oid fn_oid, Oid tgreloid,
 		 ************************************************************/
 		Tcl_DStringAppendElement(&proc_internal_def, "proc");
 		Tcl_DStringAppendElement(&proc_internal_def, internal_proname);
-		Tcl_DStringAppendElement(&proc_internal_def, proc_internal_args);
+		Tcl_DStringAppendElement(&proc_internal_def,
+								 Tcl_DStringValue(&proc_internal_args));
 
 		/************************************************************
 		 * prefix procedure body with
@@ -1802,6 +1804,7 @@ compile_pltcl_function(Oid fn_oid, Oid tgreloid,
 		Tcl_DStringFree(&proc_internal_def);
 		Tcl_DStringFree(&proc_internal_name);
 		Tcl_DStringFree(&proc_internal_body);
+		Tcl_DStringFree(&proc_internal_args);
 		PG_RE_THROW();
 	}
 	PG_END_TRY();
@@ -1831,6 +1834,7 @@ compile_pltcl_function(Oid fn_oid, Oid tgreloid,
 	Tcl_DStringFree(&proc_internal_def);
 	Tcl_DStringFree(&proc_internal_name);
 	Tcl_DStringFree(&proc_internal_body);
+	Tcl_DStringFree(&proc_internal_args);
 
 	ReleaseSysCache(procTup);
 
