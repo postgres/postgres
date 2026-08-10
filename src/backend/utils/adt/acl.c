17b6083dbce2ba10ea5b5cf0156ec77cfa681eb5
Nathan Bossart <nathan@postgresql.org>
2026-08-10 06:38:30 -0700

    Invalidate plan cache after role changes.
    Role membership, role attribute, and database ownership changes may
    impact the expected behavior of row-level security policies, but
    currently the plan cache doesn't take notice.  To fix, register
    syscache callbacks on pg_auth_members, pg_authid, and pg_database
    that invalidate the role-dependent plans.  Changes to other
    databases' pg_database rows are ignored.
    
    Reported-by: Ilya Staroverov <i.staroverov@ftdata.ru>
    Reported-by: Shinya Kato <shinya11.kato@gmail.com>
    Author: Ilya Staroverov <i.staroverov@ftdata.ru>
    Author: Shinya Kato <shinya11.kato@gmail.com>
    Co-authored-by: Nathan Bossart <nathandbossart@gmail.com>
    Reviewed-by: Tom Lane <tgl@sss.pgh.pa.us>
    Security: CVE-2026-14666
    Backpatch-through: 14

--- src/backend/utils/adt/acl.c
+++ src/backend/utils/adt/acl.c
@@ -71,7 +71,7 @@ enum RoleRecurseType
 };
 static Oid	cached_role[] = {InvalidOid, InvalidOid};
 static List *cached_roles[] = {NIL, NIL};
-static uint32 cached_db_hash;
+uint32		cached_db_hash;
 
 
 static const char *getid(const char *s, char *n);
