8646d1d5904503f7e8cbb7166e857fed63c6e49c
Peter Eisentraut <peter@eisentraut.org>
2026-08-27 10:42:14 +0200

    Fix inferred property graph keys with INCLUDE columns
    Inferred property graph keys used all attributes stored in the primary
    key index, causing non-key INCLUDE columns to become part of the graph
    key. Use only the index's key attributes.
    
    Author: Muhammad Taha Naveed <m.taha.naveed27@gmail.com>
    Discussion: https://www.postgresql.org/message-id/flat/CAPTqav%2BVUjYgm1jZy0Scy%3D1-PfdgznVj2%3DPwH8disiXF76HEow%40mail.gmail.com

--- src/backend/commands/propgraphcmds.c
+++ src/backend/commands/propgraphcmds.c
@@ -342,7 +342,8 @@ propgraph_element_get_key(ParseState *pstate, const List *key_clause, Relation e
 			Relation	indexDesc;
 
 			indexDesc = index_open(pkidx, AccessShareLock);
-			a = array_from_attnums(indexDesc->rd_index->indkey.dim1, indexDesc->rd_index->indkey.values);
+			a = array_from_attnums(IndexRelationGetNumberOfKeyAttributes(indexDesc),
+								   indexDesc->rd_index->indkey.values);
 			index_close(indexDesc, NoLock);
 		}
 	}
