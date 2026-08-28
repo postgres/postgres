6e5d5680b555523a3d5c547aa22df0267860344a
David Rowley <drowley@postgresql.org>
2026-08-28 13:10:21 +1200

    Fix incorrect multi-column RANGE partition pruning
    When performing partition pruning with a RANGE partitioned table where
    the pruning quals are only present for a leading prefix of the partition
    key, it was possible that partition pruning would accidentally prune away
    some partitions which shouldn't be pruned and include some partitions that
    were not needed.
    
    This happened due to an incorrectly coded loop bound which was
    terminating the loop when the bound reached the first or last element in
    the partition bound array.  This resulted in those end elements not being
    checked in cases where they should be checked.  It appears that it might
    have been coded this way to avoid stepping off the array, but that was
    done incorrectly as it failed to take into account the direction of travel
    through the array (the loop can go forwards or backwards).  I.e., it's
    valid to loop when 'off' is the last element if we're going backwards
    through the array, and valid to loop if 'off' is 0 and we're looping
    forward through the array, but the code as it was didn't allow that.
    
    Here we fix this by moving the loop condition check to after we've
    calculated the array element to process, and break from the loop if that
    element is beyond either end of the array.
    
    Example of accidentally pruned partition:
    
    p: partition by range (a, b);
    p1: for values from (1, 4) to (1, 7);
    p2: for values from (1, 7) to (3, 8);
    p3: for values from (4, 8) to (6, 9);
    def: default;
    
    select * from p where a <= 1;
    
    Here p2 was pruned by mistake.
    
    Example of accidentally not pruning a partition:
    
    p: partition by range (a, b);
    p1: for values from (7, 2) to (7, 7);
    def: default;
    
    select * from p where a > 7;
    
    No partitions would be pruned in this case, despite it being impossible
    for matching rows to exist in p1.
    
    Author: David Rowley <dgrowleyml@gmail.com>
    Reviewed-by: Ayush Tiwari <ayushtiwari.slg01@gmail.com>
    Reviewed-by: Tender Wang <tndrwang@gmail.com>
    Discussion: https://postgr.es/m/CAApHDvp5ne9AWaH-tG1Lke-USLz3NwWLWTUdP5NT7ypKtcFqcg@mail.gmail.com
    Backpatch-through: 14

--- src/backend/partitioning/partprune.c
+++ src/backend/partitioning/partprune.c
@@ -3206,12 +3206,15 @@ get_matching_range_bounds(PartitionPruneContext *context,
 					 * of smallest such bound) or find the smallest one that's
 					 * greater than the lookup values and set minoff to that.
 					 */
-					while (off >= 1 && off < boundinfo->ndatums - 1)
+					while (true)
 					{
 						int32		cmpval;
 						int			nextoff;
 
 						nextoff = inclusive ? off - 1 : off + 1;
+
+						if (nextoff < 0 || nextoff >= boundinfo->ndatums)
+							break;
 						cmpval =
 							partition_rbound_datum_cmp(partsupfunc,
 													   partcollation,
@@ -3265,16 +3268,21 @@ get_matching_range_bounds(PartitionPruneContext *context,
 			if (off >= 0)
 			{
 				/*
-				 * See the comment above.
+				 * As above, check adjacent bounds to see if the bound is
+				 * equal to the lookup value.
 				 */
 				if (is_equal && nvalues < partnatts)
 				{
-					while (off >= 1 && off < boundinfo->ndatums - 1)
+					while (true)
 					{
 						int32		cmpval;
 						int			nextoff;
 
 						nextoff = inclusive ? off + 1 : off - 1;
+
+						if (nextoff < 0 || nextoff >= boundinfo->ndatums)
+							break;
+
 						cmpval = partition_rbound_datum_cmp(partsupfunc,
 															partcollation,
 															boundinfo->datums[nextoff],
