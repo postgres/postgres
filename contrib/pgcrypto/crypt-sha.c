2a2dab67dc40a41a003545308423c1d3def02dea
Peter Eisentraut <peter@eisentraut.org>
2026-09-01 11:58:20 +0200

    Fix memset invocation
    It was setting sha_buf_tmp but sizing it by sizeof sha_buf.  These
    happen to be the same size, so it's not an actual problem, but for
    correctness make the sizeof match.
    
    Reported-by: Sehrope Sarkuni <sehrope@jackdb.com>
    Discussion: https://www.postgresql.org/message-id/flat/a035c67f-4aa8-42e0-bb24-7b5abe927af4%40eisentraut.org

--- contrib/pgcrypto/crypt-sha.c
+++ contrib/pgcrypto/crypt-sha.c
@@ -477,7 +477,7 @@ px_crypt_shacrypt(const char *pw, const char *salt, char *passwd, unsigned dstle
 	memcpy(cp, sha_buf_tmp, block);
 
 	/* Make sure we don't leave something important behind */
-	px_memset(&sha_buf_tmp, 0, sizeof sha_buf);
+	px_memset(&sha_buf_tmp, 0, sizeof sha_buf_tmp);
 
 	/*-
 	 * 21. Repeat a loop according to the number specified in the rounds=<N>
