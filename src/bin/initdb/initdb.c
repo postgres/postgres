c0584c27f4f70935b3ccf31e58edb8e175fec447
Nathan Bossart <nathan@postgresql.org>
2026-09-02 09:22:40 -0500

    initdb: Pad rewritten GUC lines with spaces instead of tabs.
    Commit f63ae72bbc converted postgresql.conf.sample to spaces so
    that it lines up at any tab width, but replace_guc_value() still
    pads with tabs when it re-aligns the trailing comment of a
    rewritten setting.  Fix that.
    
    Oversight in commit f63ae72bbc.
    
    Reviewed-by: Chao Li <li.evan.chao@gmail.com>
    Discussion: https://postgr.es/m/apRezMywHRuR5h-6%40nathan
    Backpatch-through: 19

--- src/bin/initdb/initdb.c
+++ src/bin/initdb/initdb.c
@@ -587,37 +587,16 @@ replace_guc_value(char **lines, const char *guc_name, const char *guc_value,
 		{
 			/*
 			 * We try to preserve original indentation, which is tedious.
-			 * oldindent and newindent are measured in de-tab-ified columns.
 			 */
-			const char *ptr;
-			int			oldindent = 0;
-			int			newindent;
+			int			oldindent = (int) (where - lines[i]);
+			int			newindent = newline->len;
 
-			for (ptr = lines[i]; ptr < where; ptr++)
-			{
-				if (*ptr == '\t')
-					oldindent += 8 - (oldindent % 8);
-				else
-					oldindent++;
-			}
-			/* ignore the possibility of tabs in guc_value */
-			newindent = newline->len;
-			/* append appropriate tabs and spaces, forcing at least one */
+			/* append appropriate spaces, forcing at least one */
 			oldindent = Max(oldindent, newindent + 1);
 			while (newindent < oldindent)
 			{
-				int			newindent_if_tab = newindent + 8 - (newindent % 8);
-
-				if (newindent_if_tab <= oldindent)
-				{
-					appendPQExpBufferChar(newline, '\t');
-					newindent = newindent_if_tab;
-				}
-				else
-				{
-					appendPQExpBufferChar(newline, ' ');
-					newindent++;
-				}
+				appendPQExpBufferChar(newline, ' ');
+				newindent++;
 			}
 			/* and finally append the old comment */
 			appendPQExpBufferStr(newline, where);
