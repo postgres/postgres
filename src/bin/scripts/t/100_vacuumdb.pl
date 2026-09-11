d33449024c8cb3420a664a598430125dca59cfa5
Nathan Bossart <nathan@postgresql.org>
2026-09-11 15:18:40 -0500

    Fix option argument lookup in in-tree getopt_long().
    The in-tree getopt_long() moves each non-option to the end of argv,
    which might put it right where an option's argument lookup expects
    to find the argument.  For example, "vacuumdb postgres --jobs"
    takes "postgres" as the number of jobs instead of complaining that
    --jobs is missing its argument.  To fix, stop the argument lookups
    at the start of the moved non-options, which we already track to
    know when to stop scanning.
    
    Oversight in commit 411b720343.
    
    Author: Sehrope Sarkuni <sehrope@jackdb.com>
    Reviewed-by: solai v <solai.cdac@gmail.com>
    Discussion: https://postgr.es/m/CAH7T-arxDuVCSkorO%3Dk7%2BM-_JV0JFzMpN_EtKMyD2K0RDqZ2OA%40mail.gmail.com
    Backpatch-through: 17

--- src/bin/scripts/t/100_vacuumdb.pl
+++ src/bin/scripts/t/100_vacuumdb.pl
@@ -236,6 +236,10 @@ $node->command_fails_like(
 	[ 'vacuumdb', '--all', 'postgres' ],
 	qr/cannot vacuum all databases and a specific one at the same time/,
 	'cannot use option --all and a dbname as argument at the same time');
+$node->command_fails_like(
+	[ 'vacuumdb', 'postgres', '--jobs' ],
+	qr/requires an argument/,
+	'option missing its argument after a non-option');
 
 $node->safe_psql('postgres', q|
   CREATE TABLE regression_vacuumdb_test AS select generate_series(1, 10) a, generate_series(2, 11) b;
