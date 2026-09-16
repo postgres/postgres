0cf920d058a9de6a5e0520a5c1916cdbc9d16689
Peter Eisentraut <peter@eisentraut.org>
2026-09-16 09:34:06 +0200

    Message style improvement

--- src/backend/libpq/be-secure-openssl.c
+++ src/backend/libpq/be-secure-openssl.c
@@ -228,7 +228,7 @@ be_tls_init(bool isServerStart)
 		{
 			ereport(isServerStart ? FATAL : LOG,
 					errcode(ERRCODE_CONFIG_FILE_ERROR),
-					errmsg("could not load \"%s\": %s", HostsFileName,
+					errmsg("could not load file \"%s\": %s", HostsFileName,
 						   err_msg ? err_msg : "unknown error"));
 			goto error;
 		}
@@ -364,7 +364,7 @@ be_tls_init(bool isServerStart)
 				errcode(ERRCODE_CONFIG_FILE_ERROR),
 				errmsg("no SSL configurations loaded"),
 		/*- translator: The two %s contain filenames */
-				errhint("If ssl_sni is enabled then add configuration to \"%s\", else \"%s\".",
+				errhint("If ssl_sni is enabled then add configuration to file \"%s\", else file \"%s\".",
 						HostsFileName, "postgresql.conf"));
 		goto error;
 	}
