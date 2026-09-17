84287ab890ffed4cfe334019c2cb6abdaaf7c97f
Fujii Masao <fujii@postgresql.org>
2026-09-17 14:25:43 +0900

    Fix assertion after aborting internal subtransaction at transaction end
    Previously, aborting an internal subtransaction during COMMIT or
    PREPARE TRANSACTION could cause the following assertion failure.
    This could happen, for example, when a deferred constraint trigger fired
    at COMMIT and its PL/pgSQL exception block caught an error raised
    while executing the trigger function.
    
        TRAP: failed Assert("s->blockState == TBLOCK_SUBINPROGRESS || s->blockState
        == TBLOCK_INPROGRESS || s->blockState == TBLOCK_IMPLICIT_INPROGRESS ||
        s->blockState == TBLOCK_PARALLEL_INPROGRESS || s->blockState ==
        TBLOCK_STARTED"), File: "xact.c", Line: 4851, PID: 73455
    
    An internal subtransaction should be able to be aborted while the parent
    transaction is in the COMMIT or PREPARE TRANSACTION phase. However,
    RollbackAndReleaseCurrentSubTransaction()'s assertion check previously
    did not allow TBLOCK_END and TBLOCK_PREPARE as parent transaction
    states, causing the assertion failure.
    
    This commit fixes the assertion check by allowing those two parent
    transaction states.
    
    Backpatch to all supported versions.
    
    Reported-by: Fabrízio de Royes Mello <fabrizio@planetscale.com>
    Author: Patrick Reynolds <piki@planetscale.com>
    Reviewed-by: Fujii Masao <masao.fujii@gmail.com>
    Reviewed-by: Chao Li <li.evan.chao@gmail.com>
    Reviewed-by: Fabrízio de Royes Mello <fabrizio@planetscale.com>
    Discussion: https://postgr.es/m/CABo-N97AeMbWuYTWg-3%3D2DkTR3EkvS%2BFt%3DyEaWB181STsR1mBg%40mail.gmail.com
    Backpatch-through: 14

--- src/backend/access/transam/xact.c
+++ src/backend/access/transam/xact.c
@@ -4807,6 +4807,8 @@ RollbackAndReleaseCurrentSubTransaction(void)
 		   s->blockState == TBLOCK_INPROGRESS ||
 		   s->blockState == TBLOCK_IMPLICIT_INPROGRESS ||
 		   s->blockState == TBLOCK_PARALLEL_INPROGRESS ||
+		   s->blockState == TBLOCK_END ||
+		   s->blockState == TBLOCK_PREPARE ||
 		   s->blockState == TBLOCK_STARTED);
 }
 
