ab713cdcdbfd05b72a6959ff17aad56cf075ae31
Michael Paquier <michael@paquier.xyz>
2026-09-19 20:13:51 +0900

    Fix PASSING of toasted text values in JSON query functions
    The PASSING arguments of JSON_EXISTS(), JSON_QUERY(), JSON_VALUE() and
    JSON_TABLE() are evaluated by plain expression steps and given to the
    jsonpath executor as-is.
    
    When dealing with a varlena, JsonItemFromDatum() was building a
    jbvString directly the contents of a varlena.  For a varatt_external
    (varatt_external[oid/oid8] on HEAD), this ignored that the contents of
    the varlena need to be detoasted, leading to incorrect contents
    returned.
    
    Let's detoast the value, like the jsonb and json cases.  b0feda79fdf0
    has fixed the same issue for subscript values in jsonb.
    
    Note that the results of DatumGetTextPP() are allocated in the memory
    context used for the evaluation of the expression, which is reset once
    per row.
    
    Oversight in 6185c9737cf4.
    
    Author: Chaitanya Choudhary <chaitanyyachoudhary@gmail.com>
    Author: Shihao Zhong <zhong950419@gmail.com>
    Discussion: https://postgr.es/m/19693-2ecd2b52c838b3e5@postgresql.org
    Backpatch-through: 17

--- src/backend/utils/adt/jsonpath_exec.c
+++ src/backend/utils/adt/jsonpath_exec.c
@@ -3346,10 +3346,14 @@ JsonItemFromDatum(Datum val, Oid typid, int32 typmod, JsonbValue *res)
 			break;
 		case TEXTOID:
 		case VARCHAROID:
-			res->type = jbvString;
-			res->val.string.val = VARDATA_ANY(DatumGetPointer(val));
-			res->val.string.len = VARSIZE_ANY_EXHDR(DatumGetPointer(val));
-			break;
+			{
+				text	   *txt = DatumGetTextPP(val);
+
+				res->type = jbvString;
+				res->val.string.val = VARDATA_ANY(txt);
+				res->val.string.len = VARSIZE_ANY_EXHDR(txt);
+				break;
+			}
 		case DATEOID:
 		case TIMEOID:
 		case TIMETZOID:
