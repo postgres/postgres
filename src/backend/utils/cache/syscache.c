5d17c0c02f3074a8273e020e3b9ffa4a41bea325
Peter Eisentraut <peter@eisentraut.org>
2026-09-22 19:30:29 +0200

    Silence warnings about unused global variables
    Similar to commit "Remove unused global variables", silence
    -Wunused-but-set-global warnings from clang 23 (included in -Wall).
    These are variables that are used in assert-enabled builds only, so
    annotate them with PG_USED_FOR_ASSERTS_ONLY.

--- src/backend/utils/cache/syscache.c
+++ src/backend/utils/cache/syscache.c
@@ -1012,7 +1012,7 @@ static const struct cachedesc cacheinfo[] = {
 
 static CatCache *SysCache[SysCacheSize];
 
-static bool CacheInitialized = false;
+static bool CacheInitialized PG_USED_FOR_ASSERTS_ONLY = false;
 
 /* Sorted array of OIDs of tables that have caches on them */
 static Oid	SysCacheRelationOid[SysCacheSize];
