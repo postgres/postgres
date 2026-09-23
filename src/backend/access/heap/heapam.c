cd7d3e129b79ee41c03001550b43b160e10c2f30
David Rowley <drowley@postgresql.org>
2026-09-24 01:21:15 +1200

    Fix incorrect block accounting in TID Range Scans
    heapgettup_advance_block() (or heapgettup() and heapgettup_pagemode() in
    versions before v16) would incorrectly decrement rs_numblocks when
    advancing to the next block.  Ordinarily this was fine, but if the TID
    Range scan plan was used by a scrollable cursor and the cursor was fetched
    backwards enough to result in another block being required, then
    rs_numblocks may have reached 0 prematurely resulting in the scan ending
    prematurely.
    
    Reported-by: Jeff Davis <pgsql@j-davis.com>
    Author: Ewan Young <kdbase.hack@gmail.com>
    Discussion: https://postgr.es/m/6b50233fcc0f7d6f4845f083777fa9702d9612e4.camel@j-davis.com
    Backpatch-through: 14

--- src/backend/access/heap/heapam.c
+++ src/backend/access/heap/heapam.c
@@ -800,8 +800,14 @@ heapgettup(HeapScanDesc scan,
 		 */
 		if (backward)
 		{
-			finished = (page == scan->rs_startblock) ||
-				(scan->rs_numblocks != InvalidBlockNumber ? --scan->rs_numblocks == 0 : false);
+			/*
+			 * We're done if the last block is the start position.  No need to
+			 * check if rs_numblocks was set by heap_setscanlimits() as that
+			 * only changes the end block.  The start block is the same with
+			 * or without scan limits.
+			 */
+			finished = (page == scan->rs_startblock);
+
 			if (page == 0)
 				page = scan->rs_nblocks;
 			page--;
@@ -822,8 +828,19 @@ heapgettup(HeapScanDesc scan,
 			page++;
 			if (page >= scan->rs_nblocks)
 				page = 0;
-			finished = (page == scan->rs_startblock) ||
-				(scan->rs_numblocks != InvalidBlockNumber ? --scan->rs_numblocks == 0 : false);
+
+			if (page == scan->rs_startblock)
+				finished = true;
+			else if (scan->rs_numblocks != InvalidBlockNumber)
+			{
+				BlockNumber endblock;
+
+				endblock = (scan->rs_startblock + scan->rs_numblocks) %
+					scan->rs_nblocks;
+				finished = (page == endblock);
+			}
+			else
+				finished = false;
 
 			/*
 			 * Report our new scan position for synchronization purposes. We
@@ -1109,8 +1126,13 @@ heapgettup_pagemode(HeapScanDesc scan,
 		 */
 		if (backward)
 		{
-			finished = (page == scan->rs_startblock) ||
-				(scan->rs_numblocks != InvalidBlockNumber ? --scan->rs_numblocks == 0 : false);
+			/*
+			 * We're done if the last block is the start position.  No need to
+			 * check if rs_numblocks was set by heap_setscanlimits() as that
+			 * only changes the end block.  The start block is the same with
+			 * or without scan limits.
+			 */
+			finished = (page == scan->rs_startblock);
 			if (page == 0)
 				page = scan->rs_nblocks;
 			page--;
@@ -1131,8 +1153,18 @@ heapgettup_pagemode(HeapScanDesc scan,
 			page++;
 			if (page >= scan->rs_nblocks)
 				page = 0;
-			finished = (page == scan->rs_startblock) ||
-				(scan->rs_numblocks != InvalidBlockNumber ? --scan->rs_numblocks == 0 : false);
+			if (page == scan->rs_startblock)
+				finished = true;
+			else if (scan->rs_numblocks != InvalidBlockNumber)
+			{
+				BlockNumber endblock;
+
+				endblock = (scan->rs_startblock + scan->rs_numblocks) %
+					scan->rs_nblocks;
+				finished = (page == endblock);
+			}
+			else
+				finished = false;
 
 			/*
 			 * Report our new scan position for synchronization purposes. We
