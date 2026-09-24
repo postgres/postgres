6210f00cac2fa62b24881b31130bdb2224f7b950
Álvaro Herrera <alvherre@kurilemu.de>
2026-09-24 11:52:47 +0200

    Wait for transactions of an initial decoding snapshot to commit
    SnapBuildInitialSnapshot() converts the snapshot builder's list of
    committed transactions into a regular MVCC snapshot, which is then used
    with HeapTupleSatisfiesMVCC().  However, a snapshot produced that way
    and used as an MVCC snapshot can potentially cause data corruption, if
    it captures a transaction after it writes its commit WAL record but
    before it has updated its CLOG entry: it will be incorrectly used to set
    hint bits as if that transaction had aborted.
    
    Fix by having SnapBuildInitialSnapshot() wait until such transactions
    have removed themselves from procarray, which guarantees correct
    visibility.
    
    Other uses of SnapBuildBuildSnapshot only use the snapshot as
    historical, so they don't need the waits, but add commentary in that
    function to alert possible future callers.
    
    While at this, backpatch commit 504fe10d1d26, "Fix misplaced comment in
    snapbuild.c."  The comment it moved was nearby the place we modify.
    
    This bug was diagnosed by stress-testing of REPACK (CONCURRENTLY) in
    branch 19, so add a test case that tickles it using that feature in that
    branch and master.  However, the bug exists in all branches, and in
    branch 18 we can add a test with regular logical decoding using
    injection points that tickles it.  Unfortunately that test cannot be
    backpatched to earlier branches for lack of facilities.
    
    Reported-by: Mihail Nikalayeu <mihailnikalayeu@gmail.com>
    Author: Antonin Houska <ah@cybertec.at>
    Author: Rui Zhao <zhaorui126@gmail.com>
    Backpatch-through: 14
    Discussion: https://postgr.es/m/85833.1768840165@localhost
    Discussion: https://postgr.es/m/CADzfLwU78as45To9a%3D-Qkr5jEg3tMxc5rUtdKy2MTv4r_SDGng%40mail.gmail.com

--- src/backend/access/transam/xact.c
+++ src/backend/access/transam/xact.c
@@ -64,6 +64,7 @@
 #include "utils/builtins.h"
 #include "utils/combocid.h"
 #include "utils/guc.h"
+#include "utils/injection_point.h"
 #include "utils/inval.h"
 #include "utils/memutils.h"
 #include "utils/relmapper.h"
@@ -1347,6 +1348,9 @@ RecordTransactionCommit(void)
 													 &RelcacheInitFileInval);
 	wrote_xlog = (XactLastRecEnd != 0);
 
+	/* Load the injection point before entering the critical section */
+	INJECTION_POINT_LOAD("commit-before-clog-update");
+
 	/*
 	 * If we haven't been assigned an XID yet, we neither can, nor do we want
 	 * to write a COMMIT record.
@@ -1500,6 +1504,12 @@ RecordTransactionCommit(void)
 		forceSyncCommit || nrels > 0)
 	{
 		XLogFlush(XactLastRecEnd);
+
+		/*
+		 * The commit record is on disk, but not in CLOG yet.  A test can stop
+		 * here to see what others make of the transaction meanwhile.
+		 */
+		INJECTION_POINT_CACHED("commit-before-clog-update", NULL);
 
 		/*
 		 * Now we may update the CLOG, if we wrote a COMMIT record above
